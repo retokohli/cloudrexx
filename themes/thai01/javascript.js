53b0c227cf496a3f76b12840f04c48eed28c3014
Thomas Daeppen <thomas.daeppen@cloudrexx.com>
2007-03-22 15:43:41 +0000

    moved several directories of trunk to contrexx/static
    git-svn-id: http://svn.contrexxlabs.com/contrexx/trunk@385 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- themes/thai01/javascript.js
+++
@@ -1,9 +0,0 @@
-browserName = navigator.appName;
-browserVer = parseInt(navigator.appVersion);
-var msie4 = (browserName == "Microsoft Internet Explorer" && browserVer >= 4);
-if ((browserName == "Netscape" && browserVer >= 3) || msie4 || browserName=="Konqueror" || browserName=="Opera") {version = "n3";} else {version = "n2";}
-// Blurring links:
-function blurLink(theObject)	{	//
-if (msie4)	{theObject.blur();}
-}
-
