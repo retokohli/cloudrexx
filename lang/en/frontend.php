5deaf83c1796052a10da7e09dc30bce006d49cd7
lang <lang@comvation.com>
2007-03-26 15:49:33 +0000

    adding lang files
    git-svn-id: http://svn.contrexxlabs.com/contrexx/trunk@418 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

---
+++ lang/en/frontend.php
@@ -0,0 +1,81 @@
+<?php
+/**
+* Contrexx CMS
+* generated date Mon, 26 Mar 2007 17:31:21 +0200
+**/
+
+$_ARRAYLANG['TXT_PASSWORD_MINIMAL_CHARACTERS'] = "(min. 6 characters)";
+$_ARRAYLANG['TXT_ACTIVE'] = "Active";
+$_ARRAYLANG['TXT_ALL_FIELDS_REQUIRED'] = "These fields are required.";
+$_ARRAYLANG['TXT_PASSWORD_NOT_USERNAME_TEXT'] = "The password and the login name cannot be identical";
+$_ARRAYLANG['TXT_SAVE_CHANGES'] = "Save changes";
+$_ARRAYLANG['TXT_LOGGED_IN_AS'] = "You are logged in as";
+$_ARRAYLANG['TXT_USER'] = "User";
+$_ARRAYLANG['TXT_ADD_USER'] = "Add user";
+$_ARRAYLANG['TXT_USERNAME'] = "Username";
+$_ARRAYLANG['TXT_DESCRIPTION'] = "Description";
+$_ARRAYLANG['TXT_DATE'] = "Date";
+$_ARRAYLANG['TXT_EMAIL'] = "Email address";
+$_ARRAYLANG['TXT_END_DATE'] = "End date";
+$_ARRAYLANG['TXT_ADD'] = "Add";
+$_ARRAYLANG['TXT_MODULE'] = "Module";
+$_ARRAYLANG['TXT_NAME'] = "Name";
+$_ARRAYLANG['TXT_PASSWORD'] = "Password";
+$_ARRAYLANG['TXT_PAGE'] = "Site";
+$_ARRAYLANG['TXT_BROWSERLANGUAGE'] = "Language";
+$_ARRAYLANG['TXT_FROM'] = "from";
+$_ARRAYLANG['TXT_PASSWORD_FIELD_EMPTY'] = "If the password field remains empty, the current password is maintained!";
+$_ARRAYLANG['TXT_SIZE'] = "File size";
+$_ARRAYLANG['TXT_DATABASE_QUERY_ERROR'] = "Database query error!";
+$_ARRAYLANG['TXT_LOGTIME'] = "Timestamp";
+$_ARRAYLANG['TXT_USERAGENT'] = "Browser and User agents";
+$_ARRAYLANG['TXT_TO'] = "to";
+$_ARRAYLANG['TXT_MONTH_ARRAY'] = "January,February,March,April,May,June,July,August,September,October,November,December";
+$_ARRAYLANG['TXT_DAY_ARRAY'] = "Sunday,Monday,Tuesday,Wednesday,Thursday,Friday,Saturday";
+$_ARRAYLANG['TXT_HOSTNAME'] = "Hostname";
+$_ARRAYLANG['TXT_COLLAPS_LINK'] = "close";
+$_ARRAYLANG['TXT_FILL_OUT_ALL_REQUIRED_FIELDS'] = "Please fill out all required fields.";
+$_ARRAYLANG['TXT_LOG_OUT'] = "Logout";
+$_ARRAYLANG['TXT_EXPAND_LINK'] = "Expand";
+$_ARRAYLANG['TXT_THIS_MODULE_DOESNT_EXISTS'] = "This module does not exist.";
+$_ARRAYLANG['TXT_STATUS'] = "Status";
+$_ARRAYLANG['TXT_DESELECT_ALL'] = "Delete mark";
+$_ARRAYLANG['TXT_OVERVIEW'] = "Overview";
+$_ARRAYLANG['TXT_FORUM'] = "Forum";
+$_ARRAYLANG['TXT_VERIFY_PASSWORD'] = "Verify Password";
+$_ARRAYLANG['TXT_TYPE'] = "Type";
+$_ARRAYLANG['TXT_SECURITY_CODE_IS_INCORRECT'] = "The security code is incorrect.";
+$_ARRAYLANG['TXT_PASSWORD_OR_USERNAME_IS_INCORRECT'] = "Your password or username is incorrect.";
+$_ARRAYLANG['TXT_SEARCH'] = "Search";
+$_ARRAYLANG['TXT_ADMIN_STATUS'] = "Administrator";
+$_ARRAYLANG['TXT_USER_NAME'] = "Username";
+$_ARRAYLANG['TXT_MORE_NEWS'] = "More News";
+$_ARRAYLANG['TXT_LOGIN'] = "Login";
+$_ARRAYLANG['TXT_BACK'] = "back";
+$_ARRAYLANG['TXT_PW_DONOT_MATCH'] = "The two passwords do not match.";
+$_ARRAYLANG['TXT_EMAIL_NOT_SENT'] = "Cannot send email to %EMAIL%!";
+$_ARRAYLANG['TXT_RESET_PASSWORD'] = "Reset password";
+$_ARRAYLANG['TXT_LOST_PASSWORD_TEXT'] = "Please enter your email address. A message will be sent to you shortly with further instructions.";
+$_ARRAYLANG['TXT_RESTORE_PASSWORD_MAIL'] = "Dear %USERNAME%,\r\n\r\nTo choose a new password, please go to the following URL and enter your new password.\r\n\r\nPLEASE NOTE: This URL will expire in 60 minutes from the time the email was sent. If you need more time, simply enter your email address again.rnrnYour URL is:\r\n%URL%\r\n\r\n\r\nRegards,\r\n%SENDER%";
+$_ARRAYLANG['TXT_LOST_PASSWORD_MAIL_SENT'] = "An email with further instructions has been sent to %EMAIL%.";
+$_ARRAYLANG['TXT_ACCOUNT_WITH_EMAIL_DOES_NOT_EXIST'] = "A user account with this email address does not exist!";
+$_ARRAYLANG['TXT_SET_PASSWORD_TEXT'] = "Please fill in your desired password, and repeat it for confirmation.";
+$_ARRAYLANG['TXT_INVALID_USER_ACCOUNT'] = "Invalid user account specified";
+$_ARRAYLANG['TXT_SET_NEW_PASSWORD'] = "Set new password";
+$_ARRAYLANG['TXT_INVALID_PASSWORD'] = "Password is invalid (min. 6 chars).";
+$_ARRAYLANG['TXT_PASSWORD_CHANGED_SUCCESSFULLY'] = "Your password has benn successfully changed.";
+$_ARRAYLANG['TXT_PASSWORD_LOST'] = "Forgot password?";
+$_ARRAYLANG['TXT_NOT_ALLOWED_TO_ACCESS'] = "You are not allowed to access to this part of the website!";
+$_ARRAYLANG['TXT_SAVE'] = "Save";
+$_ARRAYLANG['TXT_CANCEL'] = "Cancel";
+$_ARRAYLANG['TXT_CATEGORY'] = "Category";
+$_ARRAYLANG['TXT_MARKET_MODULE_DESCRIPTION'] = "Advertisement";
+$_ARRAYLANG['TXT_MARKET_TITLE'] = "Advertisement";
+$_ARRAYLANG['TXT_RESERVATION'] = "Reservations";
+$_ARRAYLANG['TXT_RESERVATION_MODULE'] = "Reservation system";
+$_ARRAYLANG['TXT_COMPANY'] = "Company";
+$_ARRAYLANG['TXT_EGOVERNMENT'] = "Online-Desk";
+$_ARRAYLANG['TXT_IMAGE'] = "Image";
+$_ARRAYLANG['TXT_EXP_SEARCH'] = "expanded Search";
+$_ARRAYLANG['TXT_POPUP_MODULE_DESCRIPTION'] = "Setup Pop-up windows";
+?>
