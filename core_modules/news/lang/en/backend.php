c280d3ab3914fa25741559b07bd31b5e5af83810
lang <lang@comvation.com>
2007-03-28 09:30:08 +0000

    automated language files update (LanguageManager)
    git-svn-id: http://svn.contrexxlabs.com/contrexx/trunk@455 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- core_modules/news/lang/en/backend.php
+++ core_modules/news/lang/en/backend.php
@@ -1,7 +1,7 @@
 <?php
 /**
 * Contrexx CMS
-* generated date Wed, 28 Mar 2007 11:18:07 +0200
+* generated date Wed, 28 Mar 2007 11:29:33 +0200
 **/
 
 $_ARRAYLANG['TXT_DELETE_MARKED'] = "Delete marked";
