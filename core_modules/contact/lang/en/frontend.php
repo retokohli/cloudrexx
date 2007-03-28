07de386fa9541539de357d142b98b9cab6ec56b9
lang <lang@comvation.com>
2007-03-28 09:30:08 +0000

    automated language files update (LanguageManager)
    git-svn-id: http://svn.contrexxlabs.com/contrexx/trunk@455 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- core_modules/contact/lang/en/frontend.php
+++ core_modules/contact/lang/en/frontend.php
@@ -1,7 +1,7 @@
 <?php
 /**
 * Contrexx CMS
-* generated date Wed, 28 Mar 2007 11:18:07 +0200
+* generated date Wed, 28 Mar 2007 11:29:33 +0200
 **/
 
 $_ARRAYLANG['TXT_CONTACT_TRANSFERED_DATA_FROM'] = "These are the informations transferred from the contact form of the website of ";
