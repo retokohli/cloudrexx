1915d9d2c3b3bcee5289dfbf2c17397b3c352ccb
Janik Tschanz <janik.tschanz@comvation.com>
2007-03-29 10:26:35 +0000

    directory update script
    git-svn-id: http://svn.contrexxlabs.com/contrexx/trunk@467 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- changelog/version_contrexx_v1.0.9/DB_1.0.9.11/directory.sql
+++ changelog/version_contrexx_v1.0.9/DB_1.0.9.11/directory.sql
@@ -1,4 +1,4 @@
-ALTER TABLE `contrexx_module_directory_levels` ADD `onlyentries` INT( 1 ) NOT NULL AFTER `showcategories` ;
+/*ALTER TABLE `contrexx_module_directory_levels` ADD `onlyentries` INT( 1 ) NOT NULL AFTER `showcategories` ;
 
 INSERT INTO `contrexx_module_directory_settings` ( `setid` , `setname` , `setvalue` , `setdescription` , `settyp` )
 VALUES (
@@ -24,10 +24,12 @@ INSERT INTO `contrexx_module_directory_inputfields` ( `id` , `typ` , `name` , `t
 VALUES (
 NULL , '7', 'spez_field_16', '', '0', '0', '0', '0', '0', '0', '0'
 );
+
 INSERT INTO `contrexx_module_directory_inputfields` ( `id` , `typ` , `name` , `title` , `active` , `active_backend` , `is_required` , `read_only` , `sort` , `exp_search` , `is_search` )
 VALUES (
 NULL , '7', 'spez_field_17', '', '0', '0', '0', '0', '0', '0', '0'
 );
+
 INSERT INTO `contrexx_module_directory_inputfields` ( `id` , `typ` , `name` , `title` , `active` , `active_backend` , `is_required` , `read_only` , `sort` , `exp_search` , `is_search` )
 VALUES (
 NULL , '7', 'spez_field_18', '', '0', '0', '0', '0', '0', '0', '0'
@@ -41,13 +43,17 @@ VALUES (
 NULL , '7', 'spez_field_20', '', '0', '0', '0', '0', '0', '0', '0'
 );
 
+
+
 ALTER TABLE `contrexx_module_directory_dir` ADD `spez_field_16` VARCHAR( 255 ) NOT NULL ,
 ADD `spez_field_17` VARCHAR( 255 ) NOT NULL ,
 ADD `spez_field_18` VARCHAR( 255 ) NOT NULL ,
 ADD `spez_field_19` VARCHAR( 255 ) NOT NULL ,
 ADD `spez_field_20` VARCHAR( 255 ) NOT NULL ;
 
-UPDATE `contrexx_module_directory_settings` SET `setname` = 'spez_field_22' WHERE `contrexx_module_directory_settings`.`setid` =32 LIMIT 1 ;
+
+
+UPDATE `contrexx_module_directory_settings` SET `setname` = 'spez_field_21' WHERE `contrexx_module_directory_settings`.`setid` =32 LIMIT 1 ;
 
 UPDATE `contrexx_module_directory_settings` SET `setname` = 'spez_field_22' WHERE `contrexx_module_directory_settings`.`setid` =33 LIMIT 1 ;
 
@@ -70,6 +76,7 @@ INSERT INTO `contrexx_module_directory_inputfields` ( `id` , `typ` , `name` , `t
 VALUES (
 NULL , '9', 'spez_field_24', '', '0', '0', '0', '0', '0', '1', '0'
 );
+
 
 ALTER TABLE `contrexx_module_directory_dir` ADD `spez_field_23` VARCHAR( 255 ) NOT NULL ,
 ADD `spez_field_24` VARCHAR( 255 ) NOT NULL ;
@@ -99,6 +106,8 @@ VALUES (
 NULL , '10', 'spez_field_29', '', '0', '0', '0', '0', '0', '0', '0'
 );
 
+
+
 ALTER TABLE `contrexx_module_directory_dir` ADD `spez_field_25` VARCHAR( 255 ) NOT NULL ,
 ADD `spez_field_26` VARCHAR( 255 ) NOT NULL ,
 ADD `spez_field_27` VARCHAR( 255 ) NOT NULL ,
@@ -107,5 +116,5 @@ ADD `spez_field_29` VARCHAR( 255 ) NOT NULL ;
 
 UPDATE `contrexx_module_directory_settings` SET `setdescription` = 'Index-Ansicht für Kategorien' WHERE `contrexx_module_directory_settings`.`setid` =31 LIMIT 1 ;
 
-UPDATE `contrexx_module_directory_settings` SET `setdescription` = 'Einträge nach Alphabet sortieren',
-`settyp` = '2' WHERE `contrexx_module_directory_settings`.`setid` =35 LIMIT 1 ;
+UPDATE `contrexx_module_directory_settings` SET `setdescription` = 'Einträge nach Alphabet sortieren',`settyp` = '2' WHERE `contrexx_module_directory_settings`.`setid` =35 LIMIT 1 ;
+*/
