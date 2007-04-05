1891819ab88a1ee792d8057b4a245cf4641190cc
lang <lang@comvation.com>
2007-04-05 15:44:36 +0000

    automated language files update (LanguageManager)
    git-svn-id: http://svn.contrexxlabs.com/contrexx/trunk@523 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- modules/calendar/lang/dk/backend.php
+++ modules/calendar/lang/dk/backend.php
@@ -1,7 +1,7 @@
 <?php
 /**
 * Contrexx CMS
-* generated date Thu, 05 Apr 2007 17:42:58 +0200
+* generated date Thu, 05 Apr 2007 17:43:55 +0200
 **/
 
 $_ARRAYLANG['TXT_CALENDAR_DATE'] = "Termindato";
