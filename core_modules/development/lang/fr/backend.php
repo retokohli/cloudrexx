79340fc7a395a3fca5a4000ed115532d9ddc18b5
lang <lang@comvation.com>
2007-04-11 09:28:10 +0000

    automated language files update (LanguageManager)
    git-svn-id: http://svn.contrexxlabs.com/contrexx/trunk@529 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- core_modules/development/lang/fr/backend.php
+++ core_modules/development/lang/fr/backend.php
@@ -1,7 +1,7 @@
 <?php
 /**
 * Contrexx CMS
-* generated date Thu, 05 Apr 2007 17:43:55 +0200
+* generated date Wed, 11 Apr 2007 11:25:51 +0200
 **/
 
 $_ARRAYLANG['TXT_BANNER_GROUP_DELETE_JS'] = "M&ouml;chten Sie diese Bannergruppe l&ouml;schen?";
