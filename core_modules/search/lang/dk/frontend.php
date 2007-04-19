74dd841d66c1d52b531a63b32beddd86d3777eeb
lang <lang@comvation.com>
2007-04-19 08:52:03 +0000

    automated language files update (LanguageManager)
    git-svn-id: http://svn.contrexxlabs.com/contrexx/trunk@628 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- core_modules/search/lang/dk/frontend.php
+++ core_modules/search/lang/dk/frontend.php
@@ -1,7 +1,7 @@
 <?php
 /**
 * Contrexx CMS
-* generated date Thu, 19 Apr 2007 10:12:16 +0200
+* generated date Thu, 19 Apr 2007 10:51:17 +0200
 **/
 
 $_ARRAYLANG['TXT_SEARCH_RESULTS'] = "Søgeresultater";
