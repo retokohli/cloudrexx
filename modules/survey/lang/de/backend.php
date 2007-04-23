2ee348285a4f86aae89434431b50e9a9696565b7
lang <lang@comvation.com>
2007-04-23 15:36:54 +0000

    automated language files update (LanguageManager)
    git-svn-id: http://svn.contrexxlabs.com/contrexx/trunk@671 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- modules/survey/lang/de/backend.php
+++ modules/survey/lang/de/backend.php
@@ -1,7 +1,7 @@
 <?php
 /**
 * Contrexx CMS
-* generated date Mon, 23 Apr 2007 16:53:51 +0200
+* generated date Mon, 23 Apr 2007 17:35:58 +0200
 **/
 
 $_ARRAYLANG['TXT_SURVEY_INSERT_SUCCESS_VOTING'] = "Die Abstimmung wurde erfolgreich hinzugefügt.";
