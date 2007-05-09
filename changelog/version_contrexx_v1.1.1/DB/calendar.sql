e996b66cb11cecbc66458ff73cf5e276e9527e42
Janik Tschanz <janik.tschanz@comvation.com>
2007-05-09 09:24:00 +0000

    new version folder for 1.1.1
    git-svn-id: http://svn.contrexxlabs.com/contrexx/trunk@798 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

---
+++ changelog/version_contrexx_v1.1.1/DB/calendar.sql
@@ -0,0 +1,19 @@
+ALTER TABLE `contrexx_module_calendar` CHANGE `info` `link` VARCHAR( 255 ) NOT NULL DEFAULT 'http://'
+
+ALTER TABLE `contrexx_module_calendar` ADD `pic` VARCHAR( 255 ) NOT NULL AFTER `link` ,
+ADD `attachment` VARCHAR( 255 ) NOT NULL AFTER `pic` ;
+
+ALTER TABLE `contrexx_module_calendar` CHANGE `place` `placeName` VARCHAR( 25 ) NOT NULL
+
+ALTER TABLE `contrexx_module_calendar` ADD `placeStreet` VARCHAR( 255 ) NOT NULL AFTER `attachment` ,
+ADD `placeZip` VARCHAR( 255 ) NOT NULL AFTER `placeStreet` ,
+ADD `placeLink` VARCHAR( 255 ) NOT NULL AFTER `placeZip` ,
+ADD `placeMap` VARCHAR( 255 ) NOT NULL AFTER `placeLink` ;
+
+ALTER TABLE `contrexx_module_calendar` ADD `access` INT( 1 ) NOT NULL DEFAULT '0' AFTER `priority` ;
+
+ALTER TABLE `contrexx_module_calendar` ADD `organizerName` VARCHAR( 255 ) NOT NULL AFTER `placeMap` ,
+ADD `organizerStreet` VARCHAR( 255 ) NOT NULL AFTER `organizerName` ,
+ADD `organizerZip` VARCHAR( 255 ) NOT NULL AFTER `organizerStreet` ,
+ADD `organizerMail` VARCHAR( 255 ) NOT NULL AFTER `organizerZip` ,
+ADD `organizerLink` VARCHAR( 255 ) NOT NULL AFTER `organizerMail` ;
