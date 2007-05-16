8f686eca7e98f4ac8978dc090af746a4f83157c1
lang <lang@comvation.com>
2007-05-16 16:04:09 +0000

    automated language files update (LanguageManager)
    git-svn-id: http://svn.contrexxlabs.com/contrexx/trunk@823 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- modules/guestbook/lang/dk/backend.php
+++ modules/guestbook/lang/dk/backend.php
@@ -1,7 +1,7 @@
 <?php
 /**
 * Contrexx CMS
-* generated date Thu, 10 May 2007 09:35:53 +0200
+* generated date Wed, 16 May 2007 18:01:49 +0200
 **/
 
 $_ARRAYLANG['TXT_ADD_GUESTBOOK_ENTRY'] = "Tilføj postering til gæstebogen";
