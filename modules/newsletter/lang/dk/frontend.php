7e1ddaa9f63e3c8b7d567bbf91d124382d2d8506
lang <lang@comvation.com>
2007-07-23 19:02:21 +0000

    automated language files update (LanguageManager) triggered by cron(root)
    git-svn-id: http://svn.contrexxlabs.com/contrexx/trunk@1121 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- modules/newsletter/lang/dk/frontend.php
+++ modules/newsletter/lang/dk/frontend.php
@@ -1,7 +1,7 @@
 <?php
 /**
 * Contrexx CMS
-* generated date Wed, 11 Jul 2007 15:00:45 +0200
+* generated date Mon, 23 Jul 2007 10:01:49 +0200
 **/
 
 $_ARRAYLANG['TXT_NEWSLETTER_CONFIRMATION_SUCCESSFUL'] = "Succesfuld bekræftelse! Du vil fremover modtage nyhedsbrevet.";
