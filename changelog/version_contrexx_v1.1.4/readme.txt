e83c9a2b20a03c9359fe8faf882ac73fca49a1b9
Thomas Kaelin <thomas.kaelin@comvation.com>
2007-08-07 16:04:45 +0000

    Captcha-Schutz für Seitenempfehlungen hinzugefügt.
    git-svn-id: http://svn.contrexxlabs.com/contrexx/trunk@1172 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- changelog/version_contrexx_v1.1.4/readme.txt
+++ changelog/version_contrexx_v1.1.4/readme.txt
@@ -1,9 +1,28 @@
 
-01. 05.2007 Shop - Hersteler
-----------------------------
+01. 05.2007 Shop - Hersteller
+-----------------------------
 Neue MARKER für Frontend:
 - SHOP_MANUFACTURER_NAME
 - SHOP_MANUFACTURER_URL
 
 
+07.08.2007 Seitenempfehlungen - Captcha
+---------------------------------------
+Neue MARKER für Frontend:
+- RECOM_TXT_CAPTCHA
+- RECOM_CAPTCHA_URL
+- RECOM_CAPTCHA_ALT
+- RECOM_CAPTCHA_OFFSET
 
+Nachfolgend noch ein Codebeispiel zum Einbau des neuen Captcha: 
+
+[HTML]
+<tr>
+    <td>[[RECOM_TXT_CAPTCHA]]:</td>
+    <td>
+      <img src="[[RECOM_CAPTCHA_URL]]" alt="[[RECOM_CAPTCHA_ALT]]" title="[[RECOM_CAPTCHA_ALT]]" />
+      <br /><br />
+      <input type="text" name="captchaCode" /><input type="hidden" value="[[RECOM_CAPTCHA_OFFSET]]" name="captchaOffset" />
+    </td>
+</tr>
+[/HTML]
--- modules/recommend/index.class.php
+++ modules/recommend/index.class.php
@@ -4,8 +4,8 @@
  *
  * Recommend module class
  *
- * @copyright   CONTREXX CMS - COMVATION AG
- * @author		Comvation Development Team <info@comvation.com>
+ * @copyright   CONTREXX CMS - ASTALAVISTA IT AG
+ * @author		Astalavista Development Team <thun@astalvista.ch>
  * @access		public
  * @version		1.0.0
  * @package     contrexx
@@ -59,7 +59,7 @@ class Recommend extends RecommendLibrary
 	function getPage()
 	{
 		switch ($_GET['act']) {
-			case "sendRecomm":
+			case 'sendRecomm':
 				$this->_sendRecomm();
 				break;
 			default:
@@ -78,23 +78,42 @@ class Recommend extends RecommendLibrary
 		global $_LANGID, $_ARRAYLANG;
 
 		$this->_objTpl->setVariable(array(
-			"RECOM_REFERER"			=> $_SERVER['HTTP_REFERER'],
-			"RECOM_FEMALE_CHECKED"  => "checked",
-			"RECOM_SCRIPT"			=> $this->getJs(),
-			"RECOM_PREVIEW"			=> $this->getMessageBody($_LANGID),
-			"RECOM_FEMALE_SALUTATION_TEXT" => $this->getFemaleSalutation($_LANGID),
-			"RECOM_MALE_SALUTATION_TEXT" => $this->getMaleSalutation($_LANGID),
-			"RECOM_TXT_RECEIVER_NAME"	=> $_ARRAYLANG['TXT_RECEIVERNAME_FRONTEND'],
-			"RECOM_TXT_RECEIVER_MAIL"	=> $_ARRAYLANG['TXT_RECEIVERMAIL_FRONTEND'],
-			"RECOM_TXT_GENDER"			=> $_ARRAYLANG['TXT_GENDER_FRONTEND'],
-			"RECOM_TXT_SENDER_NAME"		=> $_ARRAYLANG['TXT_SENDERNAME_FRONTEND'],
-			"RECOM_TXT_SENDER_MAIL"		=> $_ARRAYLANG['TXT_SENDERMAIL_FRONTEND'],
-			"RECOM_TXT_COMMENT"			=> $_ARRAYLANG['TXT_COMMENT_FRONTEND'],
-			"RECOM_TXT_PREVIEW"			=> $_ARRAYLANG['TXT_PREVIEW_FRONTEND'],
-			"RECOM_TXT_FEMALE"			=> $_ARRAYLANG['TXT_FEMALE_FRONTEND'],
-			"RECOM_TXT_MALE"			=> $_ARRAYLANG['TXT_MALE_FRONTEND'],
-			"RECOM_TEXT"				=> $_ARRAYLANG['TXT_INTRODUCTION']
-			));
+			'RECOM_TEXT'					=> $_ARRAYLANG['TXT_INTRODUCTION'],
+			'RECOM_TXT_RECEIVER_NAME'		=> $_ARRAYLANG['TXT_RECEIVERNAME_FRONTEND'],
+			'RECOM_TXT_RECEIVER_MAIL'		=> $_ARRAYLANG['TXT_RECEIVERMAIL_FRONTEND'],
+			'RECOM_TXT_GENDER'				=> $_ARRAYLANG['TXT_GENDER_FRONTEND'],
+			'RECOM_TXT_SENDER_NAME'			=> $_ARRAYLANG['TXT_SENDERNAME_FRONTEND'],
+			'RECOM_TXT_SENDER_MAIL'			=> $_ARRAYLANG['TXT_SENDERMAIL_FRONTEND'],
+			'RECOM_TXT_COMMENT'				=> $_ARRAYLANG['TXT_COMMENT_FRONTEND'],
+			'RECOM_TXT_PREVIEW'				=> $_ARRAYLANG['TXT_PREVIEW_FRONTEND'],
+			'RECOM_TXT_FEMALE'				=> $_ARRAYLANG['TXT_FEMALE_FRONTEND'],
+			'RECOM_TXT_MALE'				=> $_ARRAYLANG['TXT_MALE_FRONTEND'],
+			'RECOM_TXT_CAPTCHA'				=> $_ARRAYLANG['TXT_RECOMMEND_CAPTCHA']
+		));
+			
+		$this->_objTpl->setVariable(array(
+			'RECOM_REFERER'					=> $_SERVER['HTTP_REFERER'],
+			'RECOM_FEMALE_CHECKED'  		=> 'checked',
+			'RECOM_SCRIPT'					=> $this->getJs(),
+			'RECOM_PREVIEW'					=> $this->getMessageBody($_LANGID),
+			'RECOM_FEMALE_SALUTATION_TEXT' 	=> $this->getFemaleSalutation($_LANGID),
+			'RECOM_MALE_SALUTATION_TEXT' 	=> $this->getMaleSalutation($_LANGID)
+		));
+		
+		//Spam-Protection
+		require_once ASCMS_LIBRARY_PATH . '/spamprotection/captcha.class.php';
+		$objCaptcha = new Captcha();
+
+		$offset = $objCaptcha->getOffset();
+		$alt = $objCaptcha->getAlt();
+		$url = $objCaptcha->getUrl();
+		
+		$this->_objTpl->setVariable(array(
+			'RECOM_CAPTCHA_URL'		=> $url,
+			'RECOM_CAPTCHA_ALT'		=> $alt,
+			'RECOM_CAPTCHA_OFFSET'	=> $offset
+		));
+		
 		$this->_objTpl->parse('recommend_form');
 	}
 
@@ -111,27 +130,33 @@ class Recommend extends RecommendLibrary
 		$empty_error = array();
 		$mail_error = array();
 		if (empty($_POST['receivername'])) {
-			$this->_pageMessage .= $_ARRAYLANG['TXT_STATUS_RECEIVER_NAME']." ".$_ARRAYLANG['TXT_IS_EMPTY']."<br />";
+			$this->_pageMessage .= $_ARRAYLANG['TXT_STATUS_RECEIVER_NAME'].' '.$_ARRAYLANG['TXT_IS_EMPTY'].'<br />';
 		}
 
 		if (empty($_POST['receivermail'])) {
-			$this->_pageMessage .= $_ARRAYLANG['TXT_STATUS_RECEIVER_MAIL']." ".$_ARRAYLANG['TXT_IS_EMPTY']."<br />";
+			$this->_pageMessage .= $_ARRAYLANG['TXT_STATUS_RECEIVER_MAIL'].' '.$_ARRAYLANG['TXT_IS_EMPTY'].'<br />';
 		} elseif (!$this->isEmail($_POST['receivermail'])) {
-			$this->_pageMessage .= $_ARRAYLANG['TXT_STATUS_RECEIVER_MAIL']." ".$_ARRAYLANG['TXT_IS_INVALID']."<br />";
+			$this->_pageMessage .= $_ARRAYLANG['TXT_STATUS_RECEIVER_MAIL'].' '.$_ARRAYLANG['TXT_IS_INVALID'].'<br />';
 		}
 
 		if (empty($_POST['sendername'])) {
-			$this->_pageMessage .= $_ARRAYLANG['TXT_STATUS_SENDER_NAME']." ".$_ARRAYLANG['TXT_IS_EMPTY']."<br />";
+			$this->_pageMessage .= $_ARRAYLANG['TXT_STATUS_SENDER_NAME'].' '.$_ARRAYLANG['TXT_IS_EMPTY'].'<br />';
 		}
 
 		if (empty($_POST['sendermail'])) {
-			$this->_pageMessage .= $_ARRAYLANG['TXT_STATUS_SENDER_MAIL']." ".$_ARRAYLANG['TXT_IS_EMPTY']."<br />";
+			$this->_pageMessage .= $_ARRAYLANG['TXT_STATUS_SENDER_MAIL'].' '.$_ARRAYLANG['TXT_IS_EMPTY'].'<br />';
 		} elseif (!$this->isEmail($_POST['sendermail'])) {
-			$this->_pageMessage .= $_ARRAYLANG['TXT_STATUS_SENDER_MAIL']." ".$_ARRAYLANG['TXT_IS_INVALID']."<br />";
+			$this->_pageMessage .= $_ARRAYLANG['TXT_STATUS_SENDER_MAIL'].' '.$_ARRAYLANG['TXT_IS_INVALID'].'<br />';
 		}
 
 		if (empty($_POST['comment'])) {
-			$this->_pageMessage .= $_ARRAYLANG['TXT_STATUS_COMMENT']." ".$_ARRAYLANG['TXT_IS_EMPTY']."<br />";
+			$this->_pageMessage .= $_ARRAYLANG['TXT_STATUS_COMMENT'].' '.$_ARRAYLANG['TXT_IS_EMPTY'].'<br />';
+		}
+		
+		require_once ASCMS_LIBRARY_PATH . '/spamprotection/captcha.class.php';
+		$objCaptcha = new Captcha();
+		if (!$objCaptcha->compare($_POST['captchaCode'], $_POST['captchaOffset'])) {
+			$this->_pageMessage .= $_ARRAYLANG['TXT_RECOMMEND_CAPTCHA'].' '.$_ARRAYLANG['TXT_IS_INVALID'];
 		}
 
 		$receivername 	= $_POST['receivername'];
@@ -142,41 +167,56 @@ class Recommend extends RecommendLibrary
 
 		if (!empty($this->_pageMessage)) {
 			//something's missing or wrong
-			$this->_objTpl->setVariable("RECOM_STATUS", "<div style=\"color: red\">".$this->_pageMessage."</div>");
-			$this->_objTpl->setCurrentBlock("recommend_form");
+			$this->_objTpl->setVariable('RECOM_STATUS', '<div style="color: red">'.$this->_pageMessage.'</div>');
+			$this->_objTpl->setCurrentBlock('recommend_form');
 			$this->_objTpl->setVariable(array(
-				"RECOM_SCRIPT"				=> $this->getJs(),
-				"RECOM_RECEIVER_NAME"		=> stripslashes($receivername),
-				"RECOM_RECEIVER_MAIL"		=> stripslashes($receivermail),
-				"RECOM_SENDER_NAME"			=> stripslashes($sendername),
-				"RECOM_SENDER_MAIL"			=> stripslashes($sendermail),
-				"RECOM_COMMENT"				=> stripslashes($comment),
-				"RECOM_PREVIEW"					=> $this->getMessageBody($_LANGID),
-				"RECOM_FEMALE_SALUTATION_TEXT" 	=> $this->getFemaleSalutation($_LANGID),
-				"RECOM_MALE_SALUTATION_TEXT" 	=> $this->getMaleSalutation($_LANGID),
-				"RECOM_TXT_RECEIVER_NAME"	=> $_ARRAYLANG['TXT_RECEIVERNAME_FRONTEND'],
-				"RECOM_TXT_RECEIVER_MAIL"	=> $_ARRAYLANG['TXT_RECEIVERMAIL_FRONTEND'],
-				"RECOM_TXT_GENDER"			=> $_ARRAYLANG['TXT_GENDER_FRONTEND'],
-				"RECOM_TXT_SENDER_NAME"		=> $_ARRAYLANG['TXT_SENDERNAME_FRONTEND'],
-				"RECOM_TXT_SENDER_MAIL"		=> $_ARRAYLANG['TXT_SENDERMAIL_FRONTEND'],
-				"RECOM_TXT_COMMENT"			=> $_ARRAYLANG['TXT_COMMENT_FRONTEND'],
-				"RECOM_TXT_PREVIEW"			=> $_ARRAYLANG['TXT_PREVIEW_FRONTEND'],
-				"RECOM_TXT_FEMALE"			=> $_ARRAYLANG['TXT_FEMALE_FRONTEND'],
-				"RECOM_TXT_MALE"			=> $_ARRAYLANG['TXT_MALE_FRONTEND'],
-				"RECOM_TEXT"				=> $_ARRAYLANG['TXT_INTRODUCTION']
-				));
-			$this->_objTpl->parseCurrentBlock("recommend_form");
+				'RECOM_SCRIPT'					=> $this->getJs(),
+				'RECOM_RECEIVER_NAME'			=> stripslashes($receivername),
+				'RECOM_RECEIVER_MAIL'			=> stripslashes($receivermail),
+				'RECOM_SENDER_NAME'				=> stripslashes($sendername),
+				'RECOM_SENDER_MAIL'				=> stripslashes($sendermail),
+				'RECOM_COMMENT'					=> stripslashes($comment),
+				'RECOM_PREVIEW'					=> $this->getMessageBody($_LANGID),
+				'RECOM_FEMALE_SALUTATION_TEXT' 	=> $this->getFemaleSalutation($_LANGID),
+				'RECOM_MALE_SALUTATION_TEXT' 	=> $this->getMaleSalutation($_LANGID)
+			));
+			
+			$this->_objTpl->setVariable(array(
+				'RECOM_TXT_RECEIVER_NAME'		=> $_ARRAYLANG['TXT_RECEIVERNAME_FRONTEND'],
+				'RECOM_TXT_RECEIVER_MAIL'		=> $_ARRAYLANG['TXT_RECEIVERMAIL_FRONTEND'],
+				'RECOM_TXT_GENDER'				=> $_ARRAYLANG['TXT_GENDER_FRONTEND'],
+				'RECOM_TXT_SENDER_NAME'			=> $_ARRAYLANG['TXT_SENDERNAME_FRONTEND'],
+				'RECOM_TXT_SENDER_MAIL'			=> $_ARRAYLANG['TXT_SENDERMAIL_FRONTEND'],
+				'RECOM_TXT_COMMENT'				=> $_ARRAYLANG['TXT_COMMENT_FRONTEND'],
+				'RECOM_TXT_PREVIEW'				=> $_ARRAYLANG['TXT_PREVIEW_FRONTEND'],
+				'RECOM_TXT_FEMALE'				=> $_ARRAYLANG['TXT_FEMALE_FRONTEND'],
+				'RECOM_TXT_MALE'				=> $_ARRAYLANG['TXT_MALE_FRONTEND'],
+				'RECOM_TEXT'					=> $_ARRAYLANG['TXT_INTRODUCTION'],
+				'RECOM_TXT_CAPTCHA'				=> $_ARRAYLANG['TXT_RECOMMEND_CAPTCHA']
+			));
+				
+			$offset = $objCaptcha->getOffset();
+			$alt = $objCaptcha->getAlt();
+			$url = $objCaptcha->getUrl();
+			
+			$this->_objTpl->setVariable(array(
+				'RECOM_CAPTCHA_URL'		=> $url,
+				'RECOM_CAPTCHA_ALT'		=> $alt,
+				'RECOM_CAPTCHA_OFFSET'	=> $offset
+			));			
+			
+			$this->_objTpl->parseCurrentBlock('recommend_form');
 			$this->_objTpl->parse();
 		} else {
 			//data is valid
 
 			if (empty($_POST['uri'])) {
-				$url = ASCMS_PROTOCOL."://".$_SERVER['HTTP_HOST'].ASCMS_PATH_OFFSET;
+				$url = ASCMS_PROTOCOL.'://'.$_SERVER['HTTP_HOST'].ASCMS_PATH_OFFSET;
 			} else {
 				$url = $_POST['uri'];
 			}
 
-			if ($_POST['gender'] == "male") {
+			if ($_POST['gender'] == 'male') {
 				$salutation = $this->getMaleSalutation($_LANGID);
 			} else {
 				$salutation = $this->getFemaleSalutation($_LANGID);
@@ -184,23 +224,23 @@ class Recommend extends RecommendLibrary
 
 			$body = $this->getMessageBody($_LANGID);
 
-			$body = preg_replace("/<SENDER_NAME>/", $sendername, $body);
-			$body = preg_replace("/<SENDER_MAIL>/", $sendermail, $body);
-			$body = preg_replace("/<RECEIVER_NAME>/", $receivername, $body);
-			$body = preg_replace("/<RECEIVER_MAIL>/", $receivermail, $body);
-			$body = preg_replace("/<URL>/", $url, $body);
-			$body = preg_replace("/<COMMENT>/", $comment, $body);
-			$body = preg_replace("/<SALUTATION>/", $salutation, $body);
+			$body = preg_replace('/<SENDER_NAME>/', $sendername, $body);
+			$body = preg_replace('/<SENDER_MAIL>/', $sendermail, $body);
+			$body = preg_replace('/<RECEIVER_NAME>/', $receivername, $body);
+			$body = preg_replace('/<RECEIVER_MAIL>/', $receivermail, $body);
+			$body = preg_replace('/<URL>/', $url, $body);
+			$body = preg_replace('/<COMMENT>/', $comment, $body);
+			$body = preg_replace('/<SALUTATION>/', $salutation, $body);
 
 			$subject = $this->getMessageSubject($_LANGID);
 
-			$subject = preg_replace("/<SENDER_NAME>/", $sendername, $subject);
-			$subject = preg_replace("/<SENDER_MAIL>/", $sendermail, $subject);
-			$subject = preg_replace("/<RECEIVER_NAME>/", $receivername, $subject);
-			$subject = preg_replace("/<RECEIVER_MAIL>/", $receivermail, $subject);
-			$subject = preg_replace("/<URL>/", $url, $subject);
-			$subject = preg_replace("/<COMMENT>/", $comment, $subject);
-			$subject = preg_replace("/<SALUTATION>/", $salutation, $subject);
+			$subject = preg_replace('/<SENDER_NAME>/', $sendername, $subject);
+			$subject = preg_replace('/<SENDER_MAIL>/', $sendermail, $subject);
+			$subject = preg_replace('/<RECEIVER_NAME>/', $receivername, $subject);
+			$subject = preg_replace('/<RECEIVER_MAIL>/', $receivermail, $subject);
+			$subject = preg_replace('/<URL>/', $url, $subject);
+			$subject = preg_replace('/<COMMENT>/', $comment, $subject);
+			$subject = preg_replace('/<SALUTATION>/', $salutation, $subject);
 
 			if (@include_once ASCMS_LIBRARY_PATH.'/phpmailer/class.phpmailer.php') {
 				$objMail = new phpmailer();
@@ -231,7 +271,7 @@ class Recommend extends RecommendLibrary
 			}
 
 
-			$this->_objTpl->setVariable("RECOM_STATUS", $_ARRAYLANG['TXT_SENT_OK']);
+			$this->_objTpl->setVariable('RECOM_STATUS', $_ARRAYLANG['TXT_SENT_OK']);
 			$this->_objTpl->parse();
 		}
 	}
@@ -244,10 +284,10 @@ class Recommend extends RecommendLibrary
 	 */
 	function isEmail($string)
 	{
-		if (eregi("^" . "[a-z0-9]+([_\\.-][a-z0-9]+)*" .	//user
-			"@" . "([a-z0-9]+([\.-][a-z0-9]+)*)+" .			//domain
-			"\\.[a-z]{2,4}" . 								//sld, tld
-			"$", $string)) {
+		if (eregi('^' . '[a-z0-9]+([_\\.-][a-z0-9]+)*' .	//user
+			'@' . '([a-z0-9]+([\.-][a-z0-9]+)*)+' .			//domain
+			'\\.[a-z]{2,4}' . 								//sld, tld
+			'$', $string)) {
 	        return true;
 		} else {
 		    return false;
