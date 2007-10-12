585f5950c635656dc5b209b33c8c4c20002ce862
lang <lang@comvation.com>
2007-10-12 19:03:05 +0000

    automated language files update (LanguageManager) triggered by cron(root)
    git-svn-id: http://svn.contrexxlabs.com/contrexx/trunk@1402 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- lang/dk/frontend.php
+++ lang/dk/frontend.php
@@ -1,7 +1,7 @@
 <?php
 /**
 * Contrexx CMS
-* generated date Mon, 08 Oct 2007 18:05:02 +0200
+* generated date Fri, 12 Oct 2007 14:05:51 +0200
 **/
 
 $_ARRAYLANG['TXT_PASSWORD_MINIMAL_CHARACTERS'] = "(min. 6 skrifttegn)";
@@ -84,4 +84,5 @@ $_ARRAYLANG['TXT_PRIVACY_MODULE_DESCRIPTION'] = "Privacy";
 $_ARRAYLANG['TXT_DATABASE_DOES_NOT_EXISTS'] = "Database not available!";
 $_ARRAYLANG['TXT_CANNOT_CONNECT_TO_DB_SERVER'] = "Unable to connect to database server!";
 $_ARRAYLANG['TXT_DELETED'] = "deleted";
+$_ARRAYLANG['TXT_PRIVACY_STATEMENT'] = "Privacy Statement";
 ?>
