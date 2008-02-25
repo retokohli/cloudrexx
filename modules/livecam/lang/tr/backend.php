2fa059f5da27467f0feececbc332adda73386703
lang <lang@comvation.com>
2008-02-25 17:40:24 +0000

    automated language files update (LanguageManager) triggered by dahasvn
    git-svn-id: http://svn.contrexxlabs.com/contrexx/trunk@1752 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- modules/livecam/lang/tr/backend.php
+++ modules/livecam/lang/tr/backend.php
@@ -12,4 +12,6 @@ $_ARRAYLANG['TXT_EMPTY_FIELDS'] = "Bazı Bölümler boş!";
 $_ARRAYLANG['TXT_UPDATE_FAILED'] = "Update(ithal) başarısız";
 $_ARRAYLANG['TXT_ACTIVATE_LIGHTBOX'] = "activate LightBox";
 $_ARRAYLANG['TXT_ACTIVATE_LIGHTBOX_INFO'] = "Damit die LightBox funktioniert, muss im Designtemplate folgender Platzhalter im Header der index.htm Datei eingefügt werden: [[JAVASCRIPT_LIGHTBOX]]";
+$_ARRAYLANG['TXT_THUMBNAIL_MAX_SIZE'] = "max. Breite der Thumbnails (in Pixel)";
+$_ARRAYLANG['TXT_CURRENT_IMAGE_MAX_SIZE'] = "max. Breite des aktuellen Bildes (in Pixel)";
 ?>
