91b9750bc40245976f422963a5e5e2d2d304b186
lang <lang@comvation.com>
2008-04-30 19:01:24 +0000

    automated language files update (LanguageManager) triggered by cron(root)
    git-svn-id: http://svn.contrexxlabs.com/contrexx/trunk@2016 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- modules/livecam/lang/fr/backend.php
+++ modules/livecam/lang/fr/backend.php
@@ -6,20 +6,20 @@
  * @package     contrexx
  * @subpackage  module_livecam
  */
-$_ARRAYLANG['TXT_SETTINGS'] = "Settings";
-$_ARRAYLANG['TXT_ARCHIVE_PATH'] = "Archive Path";
-$_ARRAYLANG['TXT_SAVE'] = "Save";
-$_ARRAYLANG['TXT_CURRENT_IMAGE_URL'] = "URL To The Current Image";
-$_ARRAYLANG['TXT_THUMBNAIL_PATH'] = "Thumbnail Path";
-$_ARRAYLANG['TXT_EMPTY_FIELDS'] = "Some Fields Are Empty!";
-$_ARRAYLANG['TXT_UPDATE_FAILED'] = "Update Failed";
-$_ARRAYLANG['TXT_ACTIVATE_LIGHTBOX'] = "activate LightBox";
-$_ARRAYLANG['TXT_ACTIVATE_LIGHTBOX_INFO'] = "Damit die LightBox funktioniert, muss im Designtemplate folgender Platzhalter im Header der index.htm Datei eingefügt werden: [[JAVASCRIPT_LIGHTBOX]]";
-$_ARRAYLANG['TXT_THUMBNAIL_MAX_SIZE'] = "max. Breite der Thumbnails (in Pixel)";
-$_ARRAYLANG['TXT_CURRENT_IMAGE_MAX_SIZE'] = "max. Breite des aktuellen Bildes (in Pixel)";
-$_ARRAYLANG['TXT_CAM'] = "Kamera";
-$_ARRAYLANG['TXT_CAMS'] = "Kameras";
-$_ARRAYLANG['TXT_LIVECAM_NUMBER_OF_CAMS'] = "Anzahl Kameras";
-$_ARRAYLANG['TXT_LIVECAM_TO_MODULE'] = "Zur Frontend-Seite";
-$_ARRAYLANG['TXT_MAKE_A_FRONTEND_PAGE'] = "Für jede Webcam müssen Sie eine Content-Seite erstellen mit der Kamera-Nummber als cmd.";
+$_ARRAYLANG['TXT_SETTINGS'] = "Paramètres";
+$_ARRAYLANG['TXT_ARCHIVE_PATH'] = "Chemin archives";
+$_ARRAYLANG['TXT_SAVE'] = "Enregistrer";
+$_ARRAYLANG['TXT_CURRENT_IMAGE_URL'] = "URL vers l'image courante";
+$_ARRAYLANG['TXT_THUMBNAIL_PATH'] = "Chemin vignette";
+$_ARRAYLANG['TXT_EMPTY_FIELDS'] = "Veuillez compléter les champs";
+$_ARRAYLANG['TXT_UPDATE_FAILED'] = "Erreur lors de la mise à jour";
+$_ARRAYLANG['TXT_ACTIVATE_LIGHTBOX'] = "Activer LightBox";
+$_ARRAYLANG['TXT_ACTIVATE_LIGHTBOX_INFO'] = "La variable [[JAVASCRIPT_LIGHTBOX]] doit être insérée dans le Header du fichier index.htm, afin que la LightBox fonctionne";
+$_ARRAYLANG['TXT_THUMBNAIL_MAX_SIZE'] = "Largeur maximale de la vignette (Pixel)";
+$_ARRAYLANG['TXT_CURRENT_IMAGE_MAX_SIZE'] = "Largeur maximale de l'image courante (Pixel)";
+$_ARRAYLANG['TXT_CAM'] = "Caméra";
+$_ARRAYLANG['TXT_CAMS'] = "Caméras";
+$_ARRAYLANG['TXT_LIVECAM_NUMBER_OF_CAMS'] = "Nombre de caméras";
+$_ARRAYLANG['TXT_LIVECAM_TO_MODULE'] = "Vers le Frontend";
+$_ARRAYLANG['TXT_MAKE_A_FRONTEND_PAGE'] = "Pour chaque caméra, créer une page de contenu avec le numéro de caméra comme cmd.";
 ?>
