59eadb976d4952b2f238a3177e0b333a577300b6
David Vogt <david.vogt@comvation.com>
2008-06-06 11:43:32 +0000

    variables are now in lang/*/backend.php
    git-svn-id: http://svn.contrexxlabs.com/contrexx/trunk@2191 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- modules/data/admin.class.php
+++ modules/data/admin.class.php
@@ -1,43 +1,5 @@
 <?php
 
-$_ARRAYLANG['TXT_DATA_PARENT_CAT'] = "Elternkategorie";
-$_ARRAYLANG['TXT_DATA_PLACEHOLDER'] = "Platzhalter";
-$_ARRAYLANG['TXT_TOP_LEVEL'] = "Keine Oberkategorie";
-$_ARRAYLANG['TXT_DATA_TEMPLATE_CATEGORY'] = "Kategorien-Template";
-$_ARRAYLANG['TXT_DATA_TEMPLATE_ENTRY'] = "Eintrag-Template";
-$_ARRAYLANG['TXT_DATA_SETTINGS_CONTENT_PAGE'] = "Normale Seite";
-$_ARRAYLANG['TXT_DATA_SETTINGS_BOX'] = "Overlay-Box";
-$_ARRAYLANG['TXT_DATA_SETTINGS_ACTION'] = "Darstellung der Detailansicht";
-$_ARRAYLANG['TXT_DATA_SETTINGS_USE_DATALIST'] = "Datenlisten verwenden";
-$_ARRAYLANG['TXT_DATA_ALL'] = "Alle";
-$_ARRAYLANG['TXT_FRONTEND_PAGE'] = "Frontend-Seite";
-$_ARRAYLANG['TXT_GENERAL_ACTION_HELP'] = "Diese Einstellung bezieht sich auf diejenigen Einträge,die einzeln auf der Webseite eingefügt werden. Für Kategorien kann die Einstellung separat gemacht werden.";
-$_ARRAYLANG['TXT_SUBCATEGORIES'] = "Unterkategorien";
-$_ARRAYLANG['TXT_DATA_SHOW_ALL'] = "Alle anzeigen";
-$_ARRAYLANG['TXT_DATA_OVERLAY_HEIGHT'] = "H&ouml;he der Overlay-Box";
-$_ARRAYLANG['TXT_DATA_OVERLAY_WIDTH'] = "Breite der Overlay-Box";
-$_ARRAYLANG['TXT_DATA_SETTINGS_THICKBOX_TEMPLATE'] = "Thickbox-Template";
-$_ARRAYLANG['TXT_ADV_SETTINGS'] = "Erweiterte Einstellungen";
-$_ARRAYLANG['TXT_GENERAL_BOX_WIDTH'] = "Allgemeine Boxbreite";
-$_ARRAYLANG['TXT_GENERAL_BOX_HEIGHT'] = "Allgemeine Boxhöhe";
-$_ARRAYLANG['TXT_TEMPLATE'] = "Template";
-$_ARRAYLANG['TXT_DATA_ATTACHMENT'] = "Anhang";
-$_ARRAYLANG['TXT_DATA_ENTRY_MODE'] = "Modus";
-$_ARRAYLANG['TXT_DATA_ENTRY_MODE_NORMAL'] = "Normal";
-$_ARRAYLANG['TXT_DATA_ENTRY_MODE_FORWARD'] = "Weiterleitung";
-$_ARRAYLANG['TXT_FORWARD_URL'] = "Weiterleitungsadresse";
-$_ARRAYLANG['TXT_DATA_CATEGORY'] = "Kategorie";
-$_ARRAYLANG['TXT_DISPLAY_MODE'] = "Darstellung";
-$_ARRAYLANG['TXT_REDIRECT_HELP'] = "Falls Sie auf eine externe Seite verlinken wollen, müssen Sie unbedingt http:// in der URL verwenden. ";
-$_ARRAYLANG['TXT_TARGET_WINDOW'] = "Zielfenster";
-$_ARRAYLANG['TXT_TARGET_BLANK'] = "Neues Fenster";
-$_ARRAYLANG['TXT_TARGET_PARENT'] = "Oberes Fenster";
-$_ARRAYLANG['TXT_TARGET_SELF'] = "Gleiches Fenster";
-$_ARRAYLANG['TXT_TARGET_TOP'] = "Oberstes Fenster";
-$_ARRAYLANG['TXT_ACTIVATE_RELEASE_TIME'] = "Zu einem bestimmten Zeitpunkt veröffentlichen";
-$_ARRAYLANG['TXT_RELEASE_TIME'] = "Veröffentlichungsdatum";
-$_ARRAYLANG['TXT_ENDLESS'] = "Ohne Enddatum (endlos)";
-
 /**
  * Data
  * @copyright   CONTREXX CMS - COMVATION AG
