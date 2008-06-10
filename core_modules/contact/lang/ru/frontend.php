db0a0a85d980052bc12e657c4aabd47e71087bf3
lang <lang@comvation.com>
2008-06-10 08:44:42 +0000

    automated language files update (LanguageManager) triggered by Dave
    git-svn-id: http://svn.contrexxlabs.com/contrexx/trunk@2204 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- core_modules/contact/lang/ru/frontend.php
+++ core_modules/contact/lang/ru/frontend.php
@@ -6,28 +6,28 @@
  * @package     contrexx
  * @subpackage  core_module_contact
  */
-$_ARRAYLANG['TXT_CONTACT_TRANSFERED_DATA_FROM'] = "Hier sind die übermittelten Informationen vom Kontaktformular der Webseite von";
-$_ARRAYLANG['TXT_CONTACT_FORM'] = "Neuer Kontaktformulareintrag auf";
+$_ARRAYLANG['TXT_CONTACT_TRANSFERED_DATA_FROM'] = "Здесь переданные сведения контактной формы с вебсайта от";
+$_ARRAYLANG['TXT_CONTACT_FORM'] = "Новая контатная форма";
 $_ARRAYLANG['TXT_CONTACT_DATE'] = "Дата";
 $_ARRAYLANG['TXT_CONTACT_HOSTNAME'] = "Имя компьютера";
 $_ARRAYLANG['TXT_CONTACT_IP_ADDRESS'] = "IP Адрес";
 $_ARRAYLANG['TXT_CONTACT_BROWSER_LANGUAGE'] = "Язык Обозревателя";
 $_ARRAYLANG['TXT_CONTACT_BROWSER_VERSION'] = "Версия Обозревателя";
-$_ARRAYLANG['TXT_CONTACT_YES'] = "Ja";
+$_ARRAYLANG['TXT_CONTACT_YES'] = "Да";
 $_ARRAYLANG['TXT_CONTACT_NO'] = "Nein";
-$_ARRAYLANG['TXT_CONTACT_UPLOADS'] = "Uploads";
-$_ARRAYLANG['TXT_NEW_ENTRY_ERORR'] = "Please check your input!";
+$_ARRAYLANG['TXT_CONTACT_UPLOADS'] = "Закачка";
+$_ARRAYLANG['TXT_NEW_ENTRY_ERORR'] = "Пожалуйста проверьте свой ввод!";
 $_ARRAYLANG['TXT_FEEDBACK_ERROR'] = "Ein oder mehrere Fehler sind aufgetreten. Benützen Sie die \"Zurück\"-Schaltfläche Ihres Browser um zurück zum Formular zu gelangen und überprüfen Sie ihre Angaben!";
-$_ARRAYLANG['TXT_CONTACT_LANG'] = "Language";
+$_ARRAYLANG['TXT_CONTACT_LANG'] = "Язык";
 $_ARRAYLANG['TXT_CONTACT_ENTRIE_DETAILS'] = "Детали";
-$_ARRAYLANG['TXT_CONTACT_FORM_ENTRIES'] = "Entries";
-$_ARRAYLANG['TXT_CONTACT_FILE_UPLOAD_FAILED'] = "The file %s couldn't be transmitted due to an error that occured!";
-$_ARRAYLANG['TXT_CONTACT_FILE_EXTENSION_NOT_ALLOWED'] = "The file %s was refused due its file extension which is not allowed!";
-$_ARRAYLANG['TXT_CONTACT_FAILED_SUBMIT_REQUEST'] = "The request wasn't transmitted due to an error that occured!";
-$_ARRAYLANG['TXT_CONTACT_FILE_SIZE_EXCEEDS_LIMIT'] = "Die Datei %s wurde nicht hinauf geladen, da Sie grösser als die maximal erlaubte Dateigrösse von %s Bytes ist!";
-$_ARRAYLANG['TXT_CONTACT_FILE_TOO_LARGE'] = "Die Datei %s wurde nicht hinauf geladen, da Sie zu gross ist!";
-$_ARRAYLANG['TXT_CONTACT_FILE_CORRUPT'] = "Die Datei %s wurde nicht komplet hinauf geladen!";
+$_ARRAYLANG['TXT_CONTACT_FORM_ENTRIES'] = "Входы";
+$_ARRAYLANG['TXT_CONTACT_FILE_UPLOAD_FAILED'] = "Файл не бы передан из-за возникшей ошибки!";
+$_ARRAYLANG['TXT_CONTACT_FILE_EXTENSION_NOT_ALLOWED'] = "Файл не был передан из-за недопустимого расширения!";
+$_ARRAYLANG['TXT_CONTACT_FAILED_SUBMIT_REQUEST'] = "Запрос не был передан из-за возникшей ошибки!";
+$_ARRAYLANG['TXT_CONTACT_FILE_SIZE_EXCEEDS_LIMIT'] = "Файл %s не был передан, из-за превышения максимального размера в  %s байтах!";
+$_ARRAYLANG['TXT_CONTACT_FILE_TOO_LARGE'] = "Файл %s не был передан, так как он слишком большой!";
+$_ARRAYLANG['TXT_CONTACT_FILE_CORRUPT'] = "Файл %s не был полностью закачан!";
 $_ARRAYLANG['TXT_CONTACT_FILE_NOT_UPLOADED'] = "Eine Datei wurde nicht hinauf geladen!";
-$_ARRAYLANG['TXT_CONTACT_CAPTCHA_DESCRIPTION'] = "Tippen Sie bitte die Zeichenfolge im dargestellten Bild in das Textfeld ein. Dies ist Notwendig, damit das System Sie von einem Spambot unterscheiden kann.";
-$_ARRAYLANG['TXT_CONTACT_INVALID_CAPTCHA_CODE'] = "The specified character string was wrong!";
+$_ARRAYLANG['TXT_CONTACT_CAPTCHA_DESCRIPTION'] = "Для активации этой опции пользователь должен ввести код изображенный на картинке, чтобы иметь возможность отправть контактную форму. Это необходимо для блокирования автоматических записей генерируемых спамботами.";
+$_ARRAYLANG['TXT_CONTACT_INVALID_CAPTCHA_CODE'] = "Указанная строка символов была неправильной!";
 ?>
