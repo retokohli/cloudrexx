3c0b59a59951e60336afec6ae265c1d6e2638fbc
lang <lang@comvation.com>
2008-06-11 08:25:12 +0000

    automated language files update (LanguageManager) triggered by Dave V.
    git-svn-id: http://svn.contrexxlabs.com/contrexx/trunk@2240 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- modules/data/lang/de/frontend.php
+++ modules/data/lang/de/frontend.php
@@ -1,8 +1,11 @@
 <?php
 /**
-* Contrexx CMS
-**/
-
+ * @copyright   CONTREXX CMS - COMVATION AG
+ * @author      Comvation Development Team <info@comvation.com>
+ * @access      public
+ * @package     contrexx
+ * @subpackage  module_data
+ */
 $_ARRAYLANG['TXT_DATA_FRONTEND_OVERVIEW_VOTING'] = "Bewertung";
 $_ARRAYLANG['TXT_DATA_FRONTEND_OVERVIEW_VOTING_DO'] = "Bewertung abgeben";
 $_ARRAYLANG['TXT_DATA_FRONTEND_OVERVIEW_COMMENTS'] = "Kommentare";
