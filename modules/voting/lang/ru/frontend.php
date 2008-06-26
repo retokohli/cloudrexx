279a32b5f3411a5b1efa67edd62e786e9a08383f
lang <lang@comvation.com>
2008-06-26 19:00:44 +0000

    automated language files update (LanguageManager) triggered by cron(root)
    git-svn-id: http://svn.contrexxlabs.com/contrexx/trunk@2360 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- modules/voting/lang/ru/frontend.php
+++ modules/voting/lang/ru/frontend.php
@@ -6,27 +6,27 @@
  * @package     contrexx
  * @subpackage  module_voting
  */
-$_ARRAYLANG['TXT_NAME'] = "Name";
-$_ARRAYLANG['TXT_RESET'] = "Zurücksetzen";
-$_ARRAYLANG['TXT_DATA_RECORD_STORED_SUCCESSFUL'] = "Der Datensatz wurde erfolgreich gespeichert";
-$_ARRAYLANG['TXT_VOTES'] = "Stimmen";
-$_ARRAYLANG['TXT_VOTING_TOTAL'] = "Total Stimmen";
-$_ARRAYLANG['TXT_DATE'] = "Datum";
-$_ARRAYLANG['TXT_TITLE'] = "Titel";
-$_ARRAYLANG['TXT_SUBMIT'] = "Absenden";
-$_ARRAYLANG['TXT_VOTING_NOT_AVAILABLE'] = "Es gibt zur Zeit keine Umfragen.";
-$_ARRAYLANG['TXT_VOTING_OLDER'] = "Ältere Umfragen";
-$_ARRAYLANG['TXT_VOTING_ENTRIES'] = "Umfragen";
-$_ARRAYLANG['TXT_VOTING_INVALID_EMAIL_ERROR'] = "Geben Sie bitte eine gültige E-Mail Adresse an!";
-$_ARRAYLANG['TXT_VOTING_ALREADY_VOTED'] = "Sie haben bereits an dieser Umfrage teilgenommen!";
-$_ARRAYLANG['TXT_VOTING_NONEXISTENT_EMAIL'] = "Ihre E-Mail Adresse existiert nicht!";
-$_ARRAYLANG['TXT_ADDITIONAL_NICKNAME'] = "Übername";
-$_ARRAYLANG['TXT_ADDITIONAL_FORENAME'] = "Vorname";
-$_ARRAYLANG['TXT_ADDITIONAL_EMAIL'] = "E-Mail-Adresse";
-$_ARRAYLANG['TXT_ADDITIONAL_SURNAME'] = "Nachname";
-$_ARRAYLANG['TXT_ADDITIONAL_PHONE'] = "Telefonnummer";
-$_ARRAYLANG['TXT_ADDITIONAL_STREET'] = "Strasse";
-$_ARRAYLANG['TXT_ADDITIONAL_ZIP'] = "PLZ";
-$_ARRAYLANG['TXT_ADDITIONAL'] = "Zusätzliche Eingabefelder für die Benutzer";
-$_ARRAYLANG['TXT_ADDITIONAL_CITY'] = "Ort";
+$_ARRAYLANG['TXT_NAME'] = "Имя";
+$_ARRAYLANG['TXT_RESET'] = "Возвращать";
+$_ARRAYLANG['TXT_DATA_RECORD_STORED_SUCCESSFUL'] = "Набор данных успешно сохранялся";
+$_ARRAYLANG['TXT_VOTES'] = "Соответствовать";
+$_ARRAYLANG['TXT_VOTING_TOTAL'] = "Совершенно голоса";
+$_ARRAYLANG['TXT_DATE'] = "Дата";
+$_ARRAYLANG['TXT_TITLE'] = "Заголовок";
+$_ARRAYLANG['TXT_SUBMIT'] = "Отправление";
+$_ARRAYLANG['TXT_VOTING_NOT_AVAILABLE'] = "Не имеется в настоящее время никаких опросов.";
+$_ARRAYLANG['TXT_VOTING_OLDER'] = "Более старые опросы";
+$_ARRAYLANG['TXT_VOTING_ENTRIES'] = "Опросы";
+$_ARRAYLANG['TXT_VOTING_INVALID_EMAIL_ERROR'] = "Укажите, пожалуйста, действительный электронный адрес!";
+$_ARRAYLANG['TXT_VOTING_ALREADY_VOTED'] = "Они приняли участие уже в этом опросе!";
+$_ARRAYLANG['TXT_VOTING_NONEXISTENT_EMAIL'] = "Ваш электронный адрес не существует!";
+$_ARRAYLANG['TXT_ADDITIONAL_NICKNAME'] = "Передача";
+$_ARRAYLANG['TXT_ADDITIONAL_FORENAME'] = "Имя";
+$_ARRAYLANG['TXT_ADDITIONAL_EMAIL'] = "Электронный адрес";
+$_ARRAYLANG['TXT_ADDITIONAL_SURNAME'] = "Фамилия";
+$_ARRAYLANG['TXT_ADDITIONAL_PHONE'] = "Номер телефона";
+$_ARRAYLANG['TXT_ADDITIONAL_STREET'] = "Улица";
+$_ARRAYLANG['TXT_ADDITIONAL_ZIP'] = "Индекс";
+$_ARRAYLANG['TXT_ADDITIONAL'] = "Дополнительные поля ввода для пользователей";
+$_ARRAYLANG['TXT_ADDITIONAL_CITY'] = "Место";
 ?>
