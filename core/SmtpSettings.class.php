fab5dbd6779694741856f64e02f7cf53f854fd13
Reto Kohli <reto.kohli@comvation.com>
2008-07-08 11:51:28 +0000

    core/SmtpSettings.class.php Shortened some lines Corrected result set variable names
    git-svn-id: http://svn.contrexxlabs.com/contrexx/trunk@2386 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- core/SmtpSettings.class.php
+++ core/SmtpSettings.class.php
@@ -9,7 +9,7 @@
  * @subpackage  core
  * @todo        Edit PHP DocBlocks!
  */
- 
+
 /**
  * SMTP Settings
  * @copyright   CONTREXX CMS - COMVATION AG
@@ -37,17 +37,26 @@ class SmtpSettings
      * @return mixed Array with account details if $accountId is a valid account ID, otherwise FALSE
      * @static
      */
-    static function _getSmtpAccount($accountId = 0, $getPassword = false)
+    static function _getSmtpAccount($accountId=0, $getPassword=false)
     {
         global $objDatabase;
 
-        if ($accountId && ($objSmtp = $objDatabase->SelectLimit('SELECT `name`, `hostname`, `port`, `username`, '.($getPassword ? '`password`' : 'CHAR_LENGTH(`password`) AS \'password\'').' FROM `'.DBPREFIX.'settings_smtp` WHERE `id` = '.$accountId, 1)) !== false && $objSmtp->RecordCount() == 1) {
+        $objResult = $objDatabase->Execute("
+            SELECT `name`, `hostname`, `port`, `username`, ".
+            ($getPassword
+                ? '`password`'
+                : 'CHAR_LENGTH(`password`) AS \'password\''
+            )."
+              FROM `'.DBPREFIX.'settings_smtp`
+             WHERE `id`=".intval($accountId)
+        );
+        if ($objResult && $objResult->RecordCount() == 1) {
             return array(
-                'name'        => $objSmtp->fields['name'],
-                'hostname'    => $objSmtp->fields['hostname'],
-                'port'        => $objSmtp->fields['port'],
-                'username'    => $objSmtp->fields['username'],
-                'password'    => $objSmtp->fields['password']
+                'name'        => $objResult->fields['name'],
+                'hostname'    => $objResult->fields['hostname'],
+                'port'        => $objResult->fields['port'],
+                'username'    => $objResult->fields['username'],
+                'password'    => $objResult->fields['password']
             );
         }
         return false;
@@ -71,15 +80,15 @@ class SmtpSettings
 
         $arrSmtp[0] = SmtpSettings::getSystemSmtpAccount();
 
-        $objSmtp = $objDatabase->Execute('SELECT `id`, `name`, `hostname`, `username` FROM `'.DBPREFIX.'settings_smtp` ORDER BY `name`');
-        if ($objSmtp !== false) {
-            while (!$objSmtp->EOF) {
-                $arrSmtp[$objSmtp->fields['id']] = array(
-                    'name'        => $objSmtp->fields['name'],
-                    'hostname'    => $objSmtp->fields['hostname'],
-                    'username'    => $objSmtp->fields['username']
+        $objResult = $objDatabase->Execute('SELECT `id`, `name`, `hostname`, `username` FROM `'.DBPREFIX.'settings_smtp` ORDER BY `name`');
+        if ($objResult !== false) {
+            while (!$objResult->EOF) {
+                $arrSmtp[$objResult->fields['id']] = array(
+                    'name'        => $objResult->fields['name'],
+                    'hostname'    => $objResult->fields['hostname'],
+                    'username'    => $objResult->fields['username']
                 );
-                $objSmtp->MoveNext();
+                $objResult->MoveNext();
             }
         }
         return $arrSmtp;
@@ -87,11 +96,9 @@ class SmtpSettings
 
 
     /**
-     * Get konfigured system SMTP account
-     *
-     * Returns the local konfigured SMTP account of the current system.
-     * @global array $_CORELANG
-     * @return array Array with the SMTP account details
+     * Returns the local configured SMTP account of the current system.
+     * @global  array $_CORELANG
+     * @return  array             Array with the SMTP account details
      * @static
      */
     static function getSystemSmtpAccount()
@@ -99,12 +106,12 @@ class SmtpSettings
         global $_CORELANG;
 
         return array(
-            'name'        => $_CORELANG['TXT_SETTINGS_SERVER_CONFIGURATION'],
-            'hostname'    => $_CORELANG['TXT_SETTINGS_SMTP_SERVER_DAEMON'],
-            'port'        => '',
-            'username'    => '',
-            'password'    => '',
-            'system'    => 1
+            'name'     => $_CORELANG['TXT_SETTINGS_SERVER_CONFIGURATION'],
+            'hostname' => $_CORELANG['TXT_SETTINGS_SMTP_SERVER_DAEMON'],
+            'port'     => '',
+            'username' => '',
+            'password' => '',
+            'system'   => 1,
         );
     }
 
@@ -153,7 +160,7 @@ class SmtpSettings
 
         if ($objDatabase->Execute("INSERT INTO `".DBPREFIX."settings_smtp` (`name`, `hostname`, `port`, `username`, `password`) VALUES ('".addslashes($arrSmtp['name'])."', '".addslashes($arrSmtp['hostname'])."', ".$arrSmtp['port'].", '".addslashes($arrSmtp['username'])."', '".addslashes($arrSmtp['password'])."')") !== false) {
             return true;
-        } 
+        }
         return false;
     }
 
@@ -173,10 +180,10 @@ class SmtpSettings
     {
         global $objDatabase;
 
-        $objSmtp = $objDatabase->SelectLimit("SELECT 1 FROM `".DBPREFIX."settings_smtp` WHERE `name` = '".addslashes($name)."' AND `id` !=".$id, 1);
-        if ($objSmtp !== false && $objSmtp->RecordCount() == 0) {
+        $objResult = $objDatabase->SelectLimit("SELECT 1 FROM `".DBPREFIX."settings_smtp` WHERE `name` = '".addslashes($name)."' AND `id` !=".$id, 1);
+        if ($objResult !== false && $objResult->RecordCount() == 0) {
             return true;
-        } 
+        }
         return false;
     }
 
@@ -201,17 +208,17 @@ class SmtpSettings
      * Returns the details of the SMTP account specified by $accountId.
      * If $accountId is either FALSE or not a valid account ID then FALSE will
      * be returned instead.
-     * @param integer $accountId
      * @see _getSmtpAccount()
-     * @return mixed Array with the details of the requested account or FALSE if the account doesn't exist
+     * @param   integer $accountId  The ID of the account settings
+     * @return  mixed           Array with the details of the requested account
+     *                          on success, false otherwise
      * @static
      */
-    static function getSmtpAccount($accountId = 0)
+    static function getSmtpAccount($accountId=0)
     {
-        if ($accountId && ($arrSmtp = SmtpSettings::getSmtpAccount($accountId, true)) !== false) {
-            return $arrSmtp;
-        }
-        return false;
+        global $objDatabase;
+
+        return SmtpSettings::_getSmtpAccount($accountId);
     }
 }
 
