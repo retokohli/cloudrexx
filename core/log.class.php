a25778770859fd3b439adee2c03995b529781fa2
David Vogt <david.vogt@comvation.com>
2008-08-13 12:47:29 +0000

    Fixes log view (invalid sql query)
    git-svn-id: http://svn.contrexxlabs.com/contrexx/trunk@2492 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- core/log.class.php
+++ core/log.class.php
@@ -5,8 +5,8 @@
  * Class to see logging
  *
  * @copyright   CONTREXX CMS - COMVATION AG
- * @author		Comvation Development Team <info@comvation.com>
- * @version		1.0.0
+ * @author        Comvation Development Team <info@comvation.com>
+ * @version        1.0.0
  * @package     contrexx
  * @subpackage  core
  * @todo        Edit PHP DocBlocks!
@@ -18,9 +18,9 @@
  * Class to see logging
  *
  * @copyright   CONTREXX CMS - COMVATION AG
- * @author		Comvation Development Team <info@comvation.com>
- * @access		public
- * @version		1.0.0
+ * @author        Comvation Development Team <info@comvation.com>
+ * @access        public
+ * @version        1.0.0
  * @package     contrexx
  * @subpackage  core
  */
@@ -36,170 +36,171 @@ class logmanager
     */
     function __construct()
     {
-    	global $_CORELANG, $objTemplate;
+        global $_CORELANG, $objTemplate;
 
-    	$objTemplate->setVariable(array(
-    	    'CONTENT_TITLE'      => $_CORELANG['TXT_LOG_ADMINISTRATION'],
-    	    'CONTENT_NAVIGATION' => "<a href='?cmd=log'>[".$_CORELANG['TXT_LOG_INDEX']."]</a>"
-    	));
+        $objTemplate->setVariable(array(
+            'CONTENT_TITLE'      => $_CORELANG['TXT_LOG_ADMINISTRATION'],
+            'CONTENT_NAVIGATION' => "<a href='?cmd=log'>[".$_CORELANG['TXT_LOG_INDEX']."]</a>"
+        ));
     }
 
     function getLogPage()
     {
-    	global $_CORELANG, $objTemplate;
+        global $_CORELANG, $objTemplate;
 
         switch($_GET['act']){
-		    case "del":
-			    $this->deleteLog();
+            case "del":
+                $this->deleteLog();
                 $action = $this->showLogs();
-			    break;
+                break;
 
-		    case "details":
+            case "details":
                 $action = $this->showDetails();
-			    break;
+                break;
 
-		    case "stats":
+            case "stats":
                 $action = $this->showStats();
-			    break;
+                break;
 
-			default:
+            default:
                 $action = $this->showLogs();
-			    break;
-		}
+                break;
+        }
 
-		$objTemplate->setVariable(array(
-			'CONTENT_TITLE'				=> $_CORELANG['TXT_LOG_ADMINISTRATION'],
-			'CONTENT_STATUS_MESSAGE'	=> trim($this->statusMessage)
-		));
+        $objTemplate->setVariable(array(
+            'CONTENT_TITLE'                => $_CORELANG['TXT_LOG_ADMINISTRATION'],
+            'CONTENT_STATUS_MESSAGE'    => trim($this->statusMessage)
+        ));
     }
 
 
     function deleteLog()
     {
-    	global $objDatabase, $_CORELANG;
+        global $objDatabase, $_CORELANG;
 
-		if(!empty($_REQUEST['logId'])) {
-			if ($objDatabase->Execute("DELETE FROM ".DBPREFIX."log WHERE id=".intval($_REQUEST['logId'])) === false) {
-		    	$this->errorHandling();
-		        return false;
-	    	}
-	    	return true;
-		} else {
-			return false;
-		}
+        if(!empty($_REQUEST['logId'])) {
+            if ($objDatabase->Execute("DELETE FROM ".DBPREFIX."log WHERE id=".intval($_REQUEST['logId'])) === false) {
+                $this->errorHandling();
+                return false;
+            }
+            return true;
+        } else {
+            return false;
+        }
     }
 
 
 
-	/*
-	* Show Logs
-	*
+    /*
+    * Show Logs
+    *
     */
     function showLogs()
     {
-    	global $objDatabase, $_CORELANG, $_CONFIG, $objTemplate;
+        global $objDatabase, $_CORELANG, $_CONFIG, $objTemplate;
 
-    	$objTemplate->addBlockfile('ADMIN_CONTENT', 'log', 'log.html');
-    	$objTemplate->setVariable(array(
-			'TXT_CONFIRM_DELETE_DATA'    => $_CORELANG['TXT_CONFIRM_DELETE_DATA'],
-			'TXT_ACTION_IS_IRREVERSIBLE' => $_CORELANG['TXT_ACTION_IS_IRREVERSIBLE'],
-			'TXT_HOSTNAME'               => $_CORELANG['TXT_HOSTNAME'],
-			'TXT_USER_NAME'			     => $_CORELANG['TXT_USERNAME'],
-			'TXT_LOGTIME'                => $_CORELANG['TXT_LOGTIME'],
-			'TXT_USERAGENT'              => $_CORELANG['TXT_USERAGENT'],
-			'TXT_BROWSERLANGUAGE'        => $_CORELANG['TXT_BROWSERLANGUAGE'],
-			'TXT_ACTION'                 => $_CORELANG['TXT_ACTION'],
-			'TXT_SEARCH'				 => $_CORELANG['TXT_SEARCH']
-		));
+        $objTemplate->addBlockfile('ADMIN_CONTENT', 'log', 'log.html');
+        $objTemplate->setVariable(array(
+            'TXT_CONFIRM_DELETE_DATA'    => $_CORELANG['TXT_CONFIRM_DELETE_DATA'],
+            'TXT_ACTION_IS_IRREVERSIBLE' => $_CORELANG['TXT_ACTION_IS_IRREVERSIBLE'],
+            'TXT_HOSTNAME'               => $_CORELANG['TXT_HOSTNAME'],
+            'TXT_USER_NAME'                 => $_CORELANG['TXT_USERNAME'],
+            'TXT_LOGTIME'                => $_CORELANG['TXT_LOGTIME'],
+            'TXT_USERAGENT'              => $_CORELANG['TXT_USERAGENT'],
+            'TXT_BROWSERLANGUAGE'        => $_CORELANG['TXT_BROWSERLANGUAGE'],
+            'TXT_ACTION'                 => $_CORELANG['TXT_ACTION'],
+            'TXT_SEARCH'                 => $_CORELANG['TXT_SEARCH']
+        ));
 
-		$objFWUser = FWUser::getFWUserObject();
+        $objFWUser = FWUser::getFWUserObject();
 
-		$term = contrexx_addslashes(contrexx_strip_tags(trim($_REQUEST['term'])));
-    	$objTemplate->setVariable('LOG_SEARCHTERM', $term);
-		$q_search = "";
-		if(!empty($term)){
-		   $q_search = "log.id LIKE '%$term%'
-	                   OR log.userid LIKE '%$term%'
-	                   OR log.useragent LIKE '%$term%'
-	                   OR log.userlanguage LIKE '%$term%'
-	                   OR log.remote_addr LIKE '%$term%'
-	                   OR log.remote_host LIKE '%$term%'
-	                   OR log.http_via LIKE '%$term%'
-	                   OR log.http_client_ip LIKE '%$term%'
-	                   OR log.http_x_forwarded_for LIKE '%$term%'
-	                   OR log.referer LIKE '%$term%'";
-			if ($objUser = $objFWUser->objUser->getUsers(array('username' => "%$term%"))) {
-				while (!$objUser->EOF) {
-					$q_search .= ' OR log.userid='.$objUser->getId();
-					$objUser->next();
-				}
-			}
-		}
+        $term = contrexx_addslashes(contrexx_strip_tags(trim($_REQUEST['term'])));
+        $objTemplate->setVariable('LOG_SEARCHTERM', $term);
+        $q_search = "";
+        if(!empty($term)){
+           $q_search = "WHERE log.id LIKE '%$term%'
+                       OR log.userid LIKE '%$term%'
+                       OR log.useragent LIKE '%$term%'
+                       OR log.userlanguage LIKE '%$term%'
+                       OR log.remote_addr LIKE '%$term%'
+                       OR log.remote_host LIKE '%$term%'
+                       OR log.http_via LIKE '%$term%'
+                       OR log.http_client_ip LIKE '%$term%'
+                       OR log.http_x_forwarded_for LIKE '%$term%'
+                       OR log.referer LIKE '%$term%'";
+            if ($objUser = $objFWUser->objUser->getUsers(array('username' => "%$term%"))) {
+                while (!$objUser->EOF) {
+                    $q_search .= ' OR log.userid='.$objUser->getId();
+                    $objUser->next();
+                }
+            }
+        }
 
-		$q = "SELECT log.id AS id,
-					 log.userid AS userid,
-		             log.datetime AS datetime,
-					 log.useragent AS useragent,
-					 log.userlanguage AS userlanguage,
-					 log.remote_addr AS remote_addr,
-					 log.remote_host AS remote_host,
-					 log.http_via AS http_via,
-					 log.http_client_ip AS http_client_ip,
-					 log.http_x_forwarded_for AS http_x_forwarded_for,
-					 log.referer AS referer
-		        FROM ".DBPREFIX."log AS log
-		       WHERE " . $q_search .
-		    "ORDER BY log.id DESC";
+        $q = "SELECT log.id AS id,
+                     log.userid AS userid,
+                     log.datetime AS datetime,
+                     log.useragent AS useragent,
+                     log.userlanguage AS userlanguage,
+                     log.remote_addr AS remote_addr,
+                     log.remote_host AS remote_host,
+                     log.http_via AS http_via,
+                     log.http_client_ip AS http_client_ip,
+                     log.http_x_forwarded_for AS http_x_forwarded_for,
+                     log.referer AS referer
+                FROM ".DBPREFIX."log AS log
+                 $q_search 
+                ORDER BY log.id DESC
+         ";
 
-		$objResult = $objDatabase->Execute($q);
-		if ($objResult === false) {
-		    $this->errorHandling();
-			return false;
-		}
+        $objResult = $objDatabase->Execute($q);
+        if ($objResult === false) {
+            $this->errorHandling();
+            return false;
+        }
 
-		$pos = intval($_GET[pos]);
-		$count = $objResult->RecordCount();
+        $pos = intval($_GET[pos]);
+        $count = $objResult->RecordCount();
 
-		if(!empty($term)) {
-		    $paging = getPaging($count, $pos, "&cmd=log&amp;term=$term", "<b>".$_CORELANG['TXT_LOG_ENTRIES']."</b>", true);
-		} else {
-		    $paging = getPaging($count, $pos, "&amp;cmd=log", "<b>".$_CORELANG['TXT_LOG_ENTRIES']."</b>", true);
-		}
+        if(!empty($term)) {
+            $paging = getPaging($count, $pos, "&cmd=log&amp;term=$term", "<b>".$_CORELANG['TXT_LOG_ENTRIES']."</b>", true);
+        } else {
+            $paging = getPaging($count, $pos, "&amp;cmd=log", "<b>".$_CORELANG['TXT_LOG_ENTRIES']."</b>", true);
+        }
 
-		$objResult = $objDatabase->SelectLimit($q, $_CONFIG['corePagingLimit'], $pos);
-		if ($objResult === false) {
-		    $this->errorHandling();
-			return false;
-		}
+        $objResult = $objDatabase->SelectLimit($q, $_CONFIG['corePagingLimit'], $pos);
+        if ($objResult === false) {
+            $this->errorHandling();
+            return false;
+        }
 
-		$objTemplate->setVariable(array(
-			'LOG_PAGING'	=> $paging,
-			'LOG_TOTAL'		=> $count
-		));
+        $objTemplate->setVariable(array(
+            'LOG_PAGING'    => $paging,
+            'LOG_TOTAL'        => $count
+        ));
 
-		while (!$objResult->EOF) {
-			$objUser = $objFWUser->objUser->getUser($objResult->fields['userid']);
+        while (!$objResult->EOF) {
+            $objUser = $objFWUser->objUser->getUser($objResult->fields['userid']);
 
-			if (($i % 2) == 0) {$class="row1";} else {$class="row2";}
-			$objTemplate->setVariable(array(
-			    'LOG_ROWCLASS' 		  => $class,
-			    'LOG_ID' 			  => $objResult->fields['id'],
-			    'LOG_USERID' 		  => $objUser ? $objUser->getId() : 0,
-			    'LOG_USERNAME'	 	  => $objUser ? htmlentities($objUser->getUsername(), ENT_QUOTES, CONTREXX_CHARSET) : '',
-			    'LOG_TIME' 		 	  => $objResult->fields['datetime'],
-			    'LOG_USERAGENT'	 	  => substr_replace($objResult->fields['useragent'],' ...', 90),
-			    'LOG_USERLANGUAGE' 	  => substr_replace($objResult->fields['userlanguage'],' ...', 20),
-			    'LOG_REMOTE_ADDR'	  => $objResult->fields['remote_addr'],
-			    'LOG_REMOTE_HOST'	  => substr_replace($objResult->fields['remote_host'],'', 60),
-			    'LOG_HTTP_VIA'     	  => $objResult->fields['http_via'],
-			    'LOG_CLIENT_IP'	  	  => $objResult->fields['http_client_ip'],
-			    'LOG_X_FORWARDED_FOR' => $objResult->fields['http_x_forwarded_for'],
-			    'LOG_REFERER' 		  => $objResult->fields['referer']
+            if (($i % 2) == 0) {$class="row1";} else {$class="row2";}
+            $objTemplate->setVariable(array(
+                'LOG_ROWCLASS'           => $class,
+                'LOG_ID'               => $objResult->fields['id'],
+                'LOG_USERID'           => $objUser ? $objUser->getId() : 0,
+                'LOG_USERNAME'           => $objUser ? htmlentities($objUser->getUsername(), ENT_QUOTES, CONTREXX_CHARSET) : '',
+                'LOG_TIME'                => $objResult->fields['datetime'],
+                'LOG_USERAGENT'           => substr_replace($objResult->fields['useragent'],' ...', 90),
+                'LOG_USERLANGUAGE'       => substr_replace($objResult->fields['userlanguage'],' ...', 20),
+                'LOG_REMOTE_ADDR'      => $objResult->fields['remote_addr'],
+                'LOG_REMOTE_HOST'      => substr_replace($objResult->fields['remote_host'],'', 60),
+                'LOG_HTTP_VIA'           => $objResult->fields['http_via'],
+                'LOG_CLIENT_IP'            => $objResult->fields['http_client_ip'],
+                'LOG_X_FORWARDED_FOR' => $objResult->fields['http_x_forwarded_for'],
+                'LOG_REFERER'           => $objResult->fields['referer']
              ));
-			$objTemplate->parse("logRow");
-			$i++;
-			$objResult->MoveNext();
-		}
+            $objTemplate->parse("logRow");
+            $i++;
+            $objResult->MoveNext();
+        }
     }
 
 
@@ -211,7 +212,7 @@ class logmanager
     * @global    array
     */
     function errorHandling(){
-    	global $_CORELANG;
+        global $_CORELANG;
         $this->statusMessage .= $_CORELANG['TXT_DATABASE_QUERY_ERROR']."<br>";
     }
 }
