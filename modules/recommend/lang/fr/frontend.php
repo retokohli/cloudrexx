33a103fd39338de9daa0776dbf4c2608aae9d679
lang <lang@comvation.com>
2008-10-17 19:00:55 +0000

    automated language files update (LanguageManager) triggered by cron(root)
    git-svn-id: http://svn.contrexxlabs.com/contrexx/trunk@2614 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- modules/recommend/lang/fr/frontend.php
+++ modules/recommend/lang/fr/frontend.php
@@ -9,20 +9,20 @@
 $_ARRAYLANG['TXT_STATUS_RECEIVER_NAME'] = "Nom destinataire";
 $_ARRAYLANG['TXT_STATUS_RECEIVER_MAIL'] = "Adresse destinataire";
 $_ARRAYLANG['TXT_STATUS_COMMENT'] = "Commentaire";
-$_ARRAYLANG['TXT_STATUS_SENDER_MAIL'] = "Votre adresse e-mail";
+$_ARRAYLANG['TXT_STATUS_SENDER_MAIL'] = "Votre adresse E-mail";
 $_ARRAYLANG['TXT_STATUS_SENDER_NAME'] = "Votre nom";
 $_ARRAYLANG['TXT_SENT_OK'] = "Recomandation envoyée.";
 $_ARRAYLANG['TXT_IS_EMPTY'] = "est vide!";
 $_ARRAYLANG['TXT_IS_INVALID'] = "est invalide!";
-$_ARRAYLANG['TXT_RECEIVERMAIL_FRONTEND'] = "Adresse e-mail du destinataire";
+$_ARRAYLANG['TXT_RECEIVERMAIL_FRONTEND'] = "Adresse E-mail du destinataire";
 $_ARRAYLANG['TXT_RECEIVERNAME_FRONTEND'] = "Nom destinataire§";
 $_ARRAYLANG['TXT_GENDER_FRONTEND'] = "Sexe du destinataire";
 $_ARRAYLANG['TXT_SENDERNAME_FRONTEND'] = "Votre nom";
-$_ARRAYLANG['TXT_SENDERMAIL_FRONTEND'] = "Votre adresse e-mail";
+$_ARRAYLANG['TXT_SENDERMAIL_FRONTEND'] = "Votre adresse E-mail";
 $_ARRAYLANG['TXT_COMMENT_FRONTEND'] = "Commentaire";
 $_ARRAYLANG['TXT_PREVIEW_FRONTEND'] = "Aperçu";
 $_ARRAYLANG['TXT_FEMALE_FRONTEND'] = "Féminin";
 $_ARRAYLANG['TXT_MALE_FRONTEND'] = "Masculin";
-$_ARRAYLANG['TXT_INTRODUCTION'] = "Pour recommander cette page, remplir simplement le formulaire suivant et cliquer finalement sur \"envoyer\". Un e-mail sera envoyé au destinataire pour lui signaler cette page.";
+$_ARRAYLANG['TXT_INTRODUCTION'] = "Pour recommander cette page, remplir simplement le formulaire suivant et cliquer finalement sur \"envoyer\". Un E-mail sera envoyé au destinataire pour lui signaler cette page.";
 $_ARRAYLANG['TXT_RECOMMEND_CAPTCHA'] = "Code sécurité";
 ?>
