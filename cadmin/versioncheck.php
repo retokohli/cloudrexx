70aad0447f796287c404c2fa51bb25cca3a8b3d0
David Vogt <david.vogt@comvation.com>
2008-11-05 15:42:08 +0000

    merge from branch (r2702): version check in backend
    git-svn-id: http://svn.contrexxlabs.com/contrexx/trunk@2703 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

---
+++ cadmin/versioncheck.php
@@ -0,0 +1,33 @@
+<?PHP
+error_reporting(0);
+ini_set('display_errors', 0);
+
+require_once("../config/version.php");
+if ($_SERVER['HTTP_X_PROTOTYPE_VERSION']) {
+    $v = preg_split('#\.#', $_CONFIG['coreCmsVersion']);
+    $version = $v[0]  * 10000 + $v[1]  * 100 + $v[2];
+    $link = fsockopen('www.contrexx.com',80);
+    $r = $_SERVER['PHP_SELF'];
+    fwrite($link, 
+        "GET /updatecenter/check.php?v=$version HTTP/1.1\n".
+        "Host: www.contrexx.com\n".
+        "Referer: $r\n".
+        "Connection: close\n\n"
+    );
+
+    $r = array();
+
+    while ($l = fgets($link)) {
+        if (preg_match('#NVERSION=(\d+\.\d+\.\d+)#', $l, $r)) {
+            $newversion = $r[1];
+            echo '(Update: <a href="http://www.contrexx.com" target="_new">'.$newversion.'</a>)';
+            fclose($link);
+            exit;
+        }
+    }
+    fclose($link);
+}
+else {
+    echo $_CONFIG['coreCmsVersion']." ".$_CONFIG['coreCmsEdition'];
+}
+
