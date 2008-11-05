15929bafcacd8efa6ec5aaa961f76e0371f8e020
David Vogt <david.vogt@comvation.com>
2008-11-05 15:59:29 +0000

    merge from branch (r2704): better text, failure tolerance
    git-svn-id: http://svn.contrexxlabs.com/contrexx/trunk@2705 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- cadmin/versioncheck.php
+++ cadmin/versioncheck.php
@@ -4,9 +4,13 @@ ini_set('display_errors', 0);
 
 require_once("../config/version.php");
 if ($_SERVER['HTTP_X_PROTOTYPE_VERSION']) {
+    // Check if there's a new version
     $v = preg_split('#\.#', $_CONFIG['coreCmsVersion']);
     $version = $v[0]  * 10000 + $v[1]  * 100 + $v[2];
     $link = fsockopen('www.contrexx.com',80);
+    if (!$link) {
+        exit;
+    }
     $r = $_SERVER['PHP_SELF'];
     fwrite($link, 
         "GET /updatecenter/check.php?v=$version HTTP/1.1\n".
@@ -20,7 +24,7 @@ if ($_SERVER['HTTP_X_PROTOTYPE_VERSION']) {
     while ($l = fgets($link)) {
         if (preg_match('#NVERSION=(\d+\.\d+\.\d+)#', $l, $r)) {
             $newversion = $r[1];
-            echo '(Update: <a href="http://www.contrexx.com" target="_new">'.$newversion.'</a>)';
+            echo '(New Version available: <a style="color:black;" href="http://www.contrexx.com" target="_new">'.$newversion.'</a>)';
             fclose($link);
             exit;
         }
