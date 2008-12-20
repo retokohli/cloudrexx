1a28c93c67bc54c131a55866e50283fdcbd3b738
lang <lang@comvation.com>
2008-12-20 22:58:18 +0000

    automated language files update (LanguageManager) triggered by dahasvn
    git-svn-id: http://svn.contrexxlabs.com/contrexx/trunk@2831 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- modules/downloads/lang/ES/backend.php
+++ modules/downloads/lang/ES/backend.php
@@ -134,4 +134,5 @@ $_ARRAYLANG['TXT_DOWNLOADS_DELETE_SUBCATEGORIES'] = "Do you want to delete the s
 $_ARRAYLANG['TXT_DOWNLOADS_CONFIRM_DELETE_CATEGORIES'] = "Are you sure you want to delete the selected categories?\nThe containing files won't be removed.";
 $_ARRAYLANG['TXT_DOWNLOADS_CATEGORY_ORDER_SET_SUCCESS'] = "The order of the categories has successfully been updated.";
 $_ARRAYLANG['TXT_DOWNLOADS_CATEGORY_ORDER_SET_FAILED'] = "The order of the category(-ies) <strong>%s</strong> couldn't be updated!";
+$_ARRAYLANG['TXT_DOWNLOADS_DELETE_SUBCATEGORIES_MULTI'] = "Do you want to delete the subcategories of the selected categories as well?";
 ?>
