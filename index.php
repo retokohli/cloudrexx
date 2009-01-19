6976d6c7a0555128205c33ca157affd1a6268bd3
ben <ben@comvation.com>
2009-01-19 14:41:40 +0000

    set "display_errors"
    git-svn-id: http://svn.contrexxlabs.com/contrexx/trunk@2868 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- index.php
+++ index.php
@@ -67,7 +67,7 @@ define('_DEBUG', 0);
 //-------------------------------------------------------
 if (_DEBUG) {
     error_reporting(E_ALL);
-    ini_set('display_errors', 0);
+    ini_set('display_errors', 1);
 } else {
     error_reporting(0);
     ini_set('display_errors', 0);
