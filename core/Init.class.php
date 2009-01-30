de76db09c596306106617fbf42f31077979db287
David Vogt <david.vogt@comvation.com>
2009-01-30 09:03:42 +0000

    $notice--
    git-svn-id: http://svn.contrexxlabs.com/contrexx/trunk@2897 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- core/Init.class.php
+++ core/Init.class.php
@@ -761,11 +761,12 @@ class InitCMS
      */
     function _is_mobile_phone() {
         $isMobile = false;
-
+        $old_er = error_reporting(0);
         $op = strtolower($_SERVER['HTTP_X_OPERAMINI_PHONE']);
         $ua = strtolower($_SERVER['HTTP_USER_AGENT']);
         $ac = strtolower($_SERVER['HTTP_ACCEPT']);
-        $ip = $_SERVER['REMOTE_ADDR'];
+
+        error_reporting($old_er);
 
         $isMobile = strpos($ac, 'application/vnd.wap.xhtml+xml') !== false
             || $op != ''
