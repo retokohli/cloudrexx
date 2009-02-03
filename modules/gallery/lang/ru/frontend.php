3af5b180d18d9face966e0e5c50b6a566c4fb97e
lang <lang@comvation.com>
2009-02-03 20:00:43 +0000

    automated language files update (LanguageManager) triggered by cron(root)
    git-svn-id: http://svn.contrexxlabs.com/contrexx/trunk@2907 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- modules/gallery/lang/ru/frontend.php
+++ modules/gallery/lang/ru/frontend.php
@@ -20,7 +20,7 @@ $_ARRAYLANG['TXT_USER_DEFINED'] = "Benutzerdefiniert";
 $_ARRAYLANG['TXT_IMAGE_NAME'] = "Bildname";
 $_ARRAYLANG['TXT_IMAGE_COUNT'] = "Anzahl Bilder";
 $_ARRAYLANG['TXT_CATEGORY'] = "Kategorie";
-$_ARRAYLANG['TXT_GALLERY'] = "Galerie";
+$_ARRAYLANG['TXT_GALLERY'] = "Bildergalerie";
 $_ARRAYLANG['TXT_SIZE'] = "Dateigrösse";
 $_ARRAYLANG['TXT_VOTING_TITLE'] = "Bildbewertungen";
 $_ARRAYLANG['TXT_VOTING_STATS_ACTUAL'] = "Aktuelle Bewertung";
@@ -39,6 +39,6 @@ $_ARRAYLANG['TXT_VOTING_RATE'] = "Bild bewerten";
 $_ARRAYLANG['TXT_VOTING_ALREADY_VOTED'] = "Sie haben dieses Bild bereits bewertet. Ihre Bewertung:";
 $_ARRAYLANG['TXT_VOTING_SCORE'] = "Bewertung";
 $_ARRAYLANG['TXT_GALLERY_IMAGE_DESCRIPTION'] = "Beschreibung: ";
-$_ARRAYLANG['TXT_IMAGES'] = "Images";
-$_ARRAYLANG['TXT_GALLERY_CATEGORY_HINT_HIERARCHY'] = "You are here:";
+$_ARRAYLANG['TXT_IMAGES'] = "Bilder";
+$_ARRAYLANG['TXT_GALLERY_CATEGORY_HINT_HIERARCHY'] = "Sie sind hier:";
 ?>
