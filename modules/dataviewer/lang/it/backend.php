11f6aefce66b46a9ca9263f3fbb090b9733b26c1
lang <lang@comvation.com>
2009-02-16 00:41:48 +0000

    automated language files update (LanguageManager) triggered by kon
    git-svn-id: http://svn.contrexxlabs.com/contrexx/trunk@4088 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

---
+++ modules/dataviewer/lang/it/backend.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * @copyright   CONTREXX CMS - COMVATION AG
+ * @author      Comvation Development Team <info@comvation.com>
+ * @access      public
+ * @package     contrexx
+ * @subpackage  module_dataviewer
+ */
+$_ARRAYLANG['TXT_DATAVIEWER_ADMINISTRATION'] = "Verwaltung";
+$_ARRAYLANG['TXT_DATAVIEWER_SETTINGS'] = "Einstellungen";
+$_ARRAYLANG['TXT_PROJECTNAME'] = "Projektname";
+$_ARRAYLANG['TXT_PROJECTDESCRIPTION'] = "Beschreibung";
+$_ARRAYLANG['TXT_PROJECTSTATUS'] = "Status";
+$_ARRAYLANG['TXT_FUNCTIONS'] = "Funktionen";
+$_ARRAYLANG['TXT_DATAVIEWER_NEW_PROJECT'] = "Projekt erfassen";
+$_ARRAYLANG['TXT_SELECTION_FILE'] = "Auswahl Datei";
+$_ARRAYLANG['TXT_CONTINUE'] = "Weiter";
+$_ARRAYLANG['TXT_CREATE_PROJECT'] = "Projekt erstellen";
+$_ARRAYLANG['TXT_DATAVIEWER_IMPORT_DATA'] = "Daten importieren";
+$_ARRAYLANG['TXT_SAVE_FILTER'] = "Filter speichern";
+$_ARRAYLANG['TXT_CREATE_FILTER_FOR'] = "Filter für";
+$_ARRAYLANG['TXT_CHOOSE_PROJECT'] = "Bitte Projekt wählen";
+$_ARRAYLANG['TXT_SELECTION_COUNTRY'] = "Auswahl Land";
+$_ARRAYLANG['TXT_START_IMPORT'] = "Importvorgang starten";
+$_ARRAYLANG['TXT_SAVE'] = "Speichern";
+$_ARRAYLANG['TXT_DATAVIEWER_OVERVIEW'] = "Übersicht";
+$_ARRAYLANG['TXT_NO_FILTER'] = "Kein Filter";
+$_ARRAYLANG['TXT_PLACEHOLDER'] = "Platzhalter";
+$_ARRAYLANG['TXT_NO_ASSIGNMENT'] = "Keine Zuordnung";
+$_ARRAYLANG['TXT_NO_DOUBLE_ASSIGNMENTS'] = "Es können keine zweit Werte gleichzeitig zugewiesen werden!";
+$_ARRAYLANG['TXT_ADMINISTRATE_PROJECTS'] = "Projekte verwalten";
+$_ARRAYLANG['TXT_LANGUAGE'] = "Sprache";
+$_ARRAYLANG['TXT_COUNTRY_BASED'] = "Länderbasiert";
+$_ARRAYLANG['TXT_FILTER'] = "Filter";
+$_ARRAYLANG['TXT_DELETE_CONFIRMATION'] = "Möchten Sie das Projekt wirklich löschen?";
+$_ARRAYLANG['TXT_PREVIEW'] = "Vorschau";
+$_ARRAYLANG['TXT_SHOW_PREVIEW'] = "Vorschau zeigen";
+$_ARRAYLANG['TXT_ONE_COLUMN_OR_MORE'] = "Es muss mindestens eine Spalte eingegeben werden!";
+$_ARRAYLANG['TXT_COLUMNS_MUSTNT_BE_EMPTY'] = "Spaltennamen dürfen nicht leer sein!";
+$_ARRAYLANG['TXT_ENTER_A_NAME'] = "Bitte geben Sie einen Namen ein!";
+$_ARRAYLANG['TXT_CHOOSE_FRONTENT_LANG'] = "Bitte geben Sie eine Frontendsprache ein!";
+$_ARRAYLANG['TXT_ENTER_DESCRIPTION'] = "Bitte geben Sie eine Beschreibung ein!";
+$_ARRAYLANG['TXT_CHOOSE_COUNTRYBASED'] = "Bitte wählen Sie ob das Projekt länderbasiert sein soll!";
+$_ARRAYLANG['TXT_CHOOSE_STATUS'] = "Bitte wählen Sie einen Status!";
+$_ARRAYLANG['TXT_PROJECT_COLUMNS'] = "Projektspalten";
+$_ARRAYLANG['TXT_FRONTEND_LANGUAGE'] = "Frontend Sprache";
+$_ARRAYLANG['TXT_YES'] = "Ja";
+$_ARRAYLANG['TXT_NO'] = "Nein";
+$_ARRAYLANG['TXT_ACTIVE'] = "Aktiv";
+$_ARRAYLANG['TXT_INACTIVE'] = "Inaktiv";
+$_ARRAYLANG['TXT_ADD_COLUMN'] = "Spalte hinzufügen";
+$_ARRAYLANG['TIP_EVERY_FILTER'] = "Sie können jedem Filter eine Spalte zuweisen, nach der gefiltert werden soll.";
+$_ARRAYLANG['TIP_PLACEHOLDER'] = "Sie können jedem Platzhalter eine Spalte zuweisen.<br />Der Inhalt wird dann anstelle des Platzhalters ausgegeben.";
+$_ARRAYLANG['TIP_PROJECTNAME'] = "Dieser Projektname ist für den User sichtbar.<br />Es sind nur Buchstaben, Zahlen, Leerschläge sowie die Umlaute (Ä,ä,Ü,ü,Ö,ö) erlaubt.";
+$_ARRAYLANG['TIP_FRONTEND_LANG'] = "Gibt an, in welchen Sprachen dieses Projekt verfügbar ist.<br />Die Auswahl ist abhängig von den aktivierten Sprachen unter Spracheinstellungen.";
+$_ARRAYLANG['TIP_COUNTRYBASED'] = "Ist diese Option aktiviert, so können die importieren Daten innerhalb<br />eines Projektes einem Land zugewiesen werden.";
+$_ARRAYLANG['TIP_DESCRIPTION'] = "Die Beschreibung ist bei den Projektauflistungen für den User ersichtlich.";
+$_ARRAYLANG['TIP_STATUS'] = "Ist der Status auf inaktiv, wird das Projekt nicht angezeigt";
+$_ARRAYLANG['TIP_COLUMN_NAME'] = "Geben Sie den Spaltenname, wie er in der CSV-Datei vorkommt ein.<br />Der Spaltenname darf keine Sonderzeichen, Leerschläge und Umlaute enthalten.";
+$_ARRAYLANG['TXT_PROJECT_DELETED'] = "Projekt wurde erfolgreich gelöscht.";
+$_ARRAYLANG['TXT_PROJECT_COULDN_BE_DELETED'] = "Projekt konnte nicht gelöscht werden!";
+$_ARRAYLANG['TXT_FILTER_UPDATED'] = "Filter wurden erfolgreich geupdatet.";
+$_ARRAYLANG['TXT_FILTER_COULDNT_BE_UPDATED'] = "Filter konnten nicht geupdatet werden!<br />";
+$_ARRAYLANG['TXT_ORDER_HAS_BEEN_CHANGED'] = "<br />Die Reihenfolge wurde angepasst.";
+$_ARRAYLANG['TXT_PLACEHOLDER_UPDATED'] = "Platzhalter wurde erfolgreich geupdatet.";
+$_ARRAYLANG['TXT_PLACEHOLDER_COULDNT_BE_UPDATED'] = "Platzhalter konnten nicht geupdatet werden!<br />";
+$_ARRAYLANG['TXT_CHOOSE_COUNTRY'] = "Bitte wählen Sie ein Land aus";
+$_ARRAYLANG['TXT_PROJECT_IS_NOT_COUNTRYBASED'] = "Diese Projekt ist nicht länderbasiert.";
+$_ARRAYLANG['TXT_CHOOSE_COUNTRY_WHICH_DATA_FOR'] = "Bitte wählen Sie ein Land aus, für die die Daten bereit gestellt werden.<br />";
+$_ARRAYLANG['TXT_WRONG_FILETYPE'] = "Dateiformat konnte nicht erkannt werden.<br />Bitte wählen Sie eine CSV Datei aus.";
+$_ARRAYLANG['TXT_FILE_COULDNT_BE_UPLOADED'] = "Datei konnte nicht heraufgeladen werden.<br />Bitte versuchen Sie es nocheinmal.";
+$_ARRAYLANG['TXT_ERROR_AT_IMPORT'] = "Fehler beim Importvorgang.<br />";
+$_ARRAYLANG['TXT_DATA_IMPORTED'] = "Daten erfolgreich importiert";
+?>
