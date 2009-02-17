88b85f4497c21ddaa3acc06d0755b1d2fab6e7b3
David Vogt <david.vogt@comvation.com>
2009-02-17 12:38:55 +0000

    Update script fixes:
    Fix DBPREFIX in calendar update, create livecam table if
    it doesn't exist.
    
    git-svn-id: http://svn.contrexxlabs.com/contrexx/trunk@4128 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- update/updates/2.1.0/components/module/livecam.php
+++ update/updates/2.1.0/components/module/livecam.php
@@ -6,8 +6,30 @@ function _livecamUpdate()
 
 	$arrColumns = $objDatabase->MetaColumns(DBPREFIX.'module_livecam');
 	if ($arrColumns === false) {
-		setUpdateMsg(sprintf($_ARRAYLANG['TXT_UNABLE_GETTING_DATABASE_TABLE_STRUCTURE'], DBPREFIX.'module_livecam'));
-		return false;
+        // try to create the table before bitching around
+        $query = "
+              CREATE TABLE `".DBPREFIX."module_livecam` (
+                  `id` int(10) unsigned NOT NULL default '1', 
+                  `currentImagePath` varchar(255)     NOT NULL default '/webcam/cam1/current.jpg',
+                  `archivePath`      varchar(255)     NOT NULL default '/webcam/cam1/archive/', 
+                  `thumbnailPath`    varchar(255)     NOT NULL default '/webcam/cam1/thumbs/',
+                  `maxImageWidth`    int(10) unsigned NOT NULL default '400',
+                  `thumbMaxSize`     int(10) unsigned NOT NULL default '200', 
+                  `lightboxActivate` set('1','0')     NOT NULL default '1',
+                  `showFrom`         int(14)          NOT NULL,
+                  `showTill`         int(14)          NOT NULL, 
+                  PRIMARY KEY  (`id`)
+            ) ENGINE=MyISAM
+        " ;
+		if ($objDatabase->Execute($query) === false) {
+			return _databaseError($query, $objDatabase->ErrorMsg());
+		}
+        // try again
+        $arrColumns = $objDatabase->MetaColumns(DBPREFIX.'module_livecam');
+        if ($arrColumns === false) {
+            setUpdateMsg(sprintf($_ARRAYLANG['TXT_UNABLE_GETTING_DATABASE_TABLE_STRUCTURE'], DBPREFIX.'module_livecam'));
+            return false;
+        }
 	}
 
 	if (!isset($arrColumns['SHOWFROM'])) {
