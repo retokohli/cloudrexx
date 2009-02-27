e3ec996bfd9a6c99d27cd339c93c2ef7ffffd386
lang <lang@comvation.com>
2009-02-27 15:28:28 +0000

    automated language files update (LanguageManager) triggered by dave
    git-svn-id: http://svn.contrexxlabs.com/contrexx/trunk@4406 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- modules/memberdir/lang/en/frontend.php
+++ modules/memberdir/lang/en/frontend.php
@@ -10,6 +10,6 @@ $_ARRAYLANG['TXT_OPEN_TICKET'] = "Create Ticket";
 $_ARRAYLANG['TXT_DIRECTORY'] = "Directory";
 $_ARRAYLANG['TXT_SEARCH'] = "Search";
 $_ARRAYLANG['TXT_OVERVIEW'] = "Overview";
-$_ARRAYLANG['TXT_SUBDIRECTORIES'] = "Subdirectories:";
+$_ARRAYLANG['TXT_SUBDIRECTORIES'] = "Subdirectories";
 $_ARRAYLANG['TXT_MEMBERDIR_EXPORT_CONTACT_AS_VCARD'] = "Export Contact As VCard";
 ?>
