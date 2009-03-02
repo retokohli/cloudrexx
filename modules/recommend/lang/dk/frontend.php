9d30a519d98c41842f574681ae96348dd4a495ae
lang <lang@comvation.com>
2009-03-02 07:12:44 +0000

    automated language files update (LanguageManager) triggered by kon
    git-svn-id: http://svn.contrexxlabs.com/contrexx/trunk@4409 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- modules/recommend/lang/dk/frontend.php
+++ modules/recommend/lang/dk/frontend.php
@@ -6,23 +6,23 @@
  * @package     contrexx
  * @subpackage  module_recommend
  */
-$_ARRAYLANG['TXT_STATUS_RECEIVER_NAME'] = "Receiver name";
-$_ARRAYLANG['TXT_STATUS_RECEIVER_MAIL'] = "Receiver e-mail address";
-$_ARRAYLANG['TXT_STATUS_COMMENT'] = "Comment";
-$_ARRAYLANG['TXT_STATUS_SENDER_MAIL'] = "Your e-mail address";
-$_ARRAYLANG['TXT_STATUS_SENDER_NAME'] = "Your name";
-$_ARRAYLANG['TXT_SENT_OK'] = "Recommendation successfully sent.";
-$_ARRAYLANG['TXT_IS_EMPTY'] = "is empty";
-$_ARRAYLANG['TXT_IS_INVALID'] = "is invalid!";
-$_ARRAYLANG['TXT_RECEIVERMAIL_FRONTEND'] = "Receiver e-mail address";
-$_ARRAYLANG['TXT_RECEIVERNAME_FRONTEND'] = "Receiver name";
-$_ARRAYLANG['TXT_GENDER_FRONTEND'] = "Gender of the receiver";
-$_ARRAYLANG['TXT_SENDERNAME_FRONTEND'] = "Your Name";
+$_ARRAYLANG['TXT_STATUS_RECEIVER_NAME'] = "Modtagerens navn";
+$_ARRAYLANG['TXT_STATUS_RECEIVER_MAIL'] = "Modtagerens emailadresse";
+$_ARRAYLANG['TXT_STATUS_COMMENT'] = "Kommentar";
+$_ARRAYLANG['TXT_STATUS_SENDER_MAIL'] = "Din emailadresse";
+$_ARRAYLANG['TXT_STATUS_SENDER_NAME'] = "Dit navn";
+$_ARRAYLANG['TXT_SENT_OK'] = "Anbefalingen er blevet sendt med sucess";
+$_ARRAYLANG['TXT_IS_EMPTY'] = "er tom!";
+$_ARRAYLANG['TXT_IS_INVALID'] = "ugyldig!";
+$_ARRAYLANG['TXT_RECEIVERMAIL_FRONTEND'] = "Modtagerens emailadresse";
+$_ARRAYLANG['TXT_RECEIVERNAME_FRONTEND'] = "Modtagerens navn";
+$_ARRAYLANG['TXT_GENDER_FRONTEND'] = "Køn af modtageren";
+$_ARRAYLANG['TXT_SENDERNAME_FRONTEND'] = "Dit navn";
 $_ARRAYLANG['TXT_SENDERMAIL_FRONTEND'] = "Your E-Mail Address";
-$_ARRAYLANG['TXT_COMMENT_FRONTEND'] = "Comment";
+$_ARRAYLANG['TXT_COMMENT_FRONTEND'] = "Kommentar";
 $_ARRAYLANG['TXT_PREVIEW_FRONTEND'] = "Preview";
 $_ARRAYLANG['TXT_FEMALE_FRONTEND'] = "Female";
 $_ARRAYLANG['TXT_MALE_FRONTEND'] = "Male";
-$_ARRAYLANG['TXT_INTRODUCTION'] = "Möchten Sie diese Seite weiterempfehlen, füllen Sie einfach das folgende Formular aus und klicken anschliessend auf \"Absenden\". Der/die Empfänger/in wird per E-Mail auf diese Seite aufmerksam gemacht.";
-$_ARRAYLANG['TXT_RECOMMEND_CAPTCHA'] = "Security Code";
+$_ARRAYLANG['TXT_INTRODUCTION'] = "Vil du anbefale denne side skal du blot udfylde følgende formular og klikke på \"send\". Modtageren bliver gjort opmærksom på siden ved en emailbesked";
+$_ARRAYLANG['TXT_RECOMMEND_CAPTCHA'] = "Sikkerhedskode";
 ?>
--- modules/voting/lang/dk/frontend.php
+++ modules/voting/lang/dk/frontend.php
@@ -6,28 +6,28 @@
  * @package     contrexx
  * @subpackage  module_voting
  */
-$_ARRAYLANG['TXT_NAME'] = "Name";
-$_ARRAYLANG['TXT_RESET'] = "Zurücksetzen";
-$_ARRAYLANG['TXT_DATA_RECORD_STORED_SUCCESSFUL'] = "Der Datensatz wurde erfolgreich gespeichert";
-$_ARRAYLANG['TXT_VOTES'] = "Stimmen";
-$_ARRAYLANG['TXT_VOTING_TOTAL'] = "Total Stimmen";
-$_ARRAYLANG['TXT_DATE'] = "Datum";
+$_ARRAYLANG['TXT_NAME'] = "Navn";
+$_ARRAYLANG['TXT_RESET'] = "Fortryd";
+$_ARRAYLANG['TXT_DATA_RECORD_STORED_SUCCESSFUL'] = "Data er blevet gemt";
+$_ARRAYLANG['TXT_VOTES'] = "Stemmer";
+$_ARRAYLANG['TXT_VOTING_TOTAL'] = "Total stemmer";
+$_ARRAYLANG['TXT_DATE'] = "Dato";
 $_ARRAYLANG['TXT_TITLE'] = "Titel";
-$_ARRAYLANG['TXT_SUBMIT'] = "Absenden";
-$_ARRAYLANG['TXT_VOTING_NOT_AVAILABLE'] = "Es gibt zur Zeit keine Umfragen.";
-$_ARRAYLANG['TXT_VOTING_OLDER'] = "Ältere Umfragen";
-$_ARRAYLANG['TXT_VOTING_ENTRIES'] = "Umfragen";
-$_ARRAYLANG['TXT_VOTING_INVALID_EMAIL_ERROR'] = "Geben Sie bitte eine gültige E-Mail Adresse an!";
-$_ARRAYLANG['TXT_VOTING_ALREADY_VOTED'] = "Sie haben bereits an dieser Umfrage teilgenommen!";
-$_ARRAYLANG['TXT_VOTING_NONEXISTENT_EMAIL'] = "Ihre E-Mail Adresse existiert nicht!";
-$_ARRAYLANG['TXT_ADDITIONAL_NICKNAME'] = "Übername";
-$_ARRAYLANG['TXT_ADDITIONAL_FORENAME'] = "Vorname";
+$_ARRAYLANG['TXT_SUBMIT'] = "Send";
+$_ARRAYLANG['TXT_VOTING_NOT_AVAILABLE'] = "Der findes ingen rundspørgelser i øjeblikket";
+$_ARRAYLANG['TXT_VOTING_OLDER'] = "Ældre rundspørgelser";
+$_ARRAYLANG['TXT_VOTING_ENTRIES'] = "Rundspørgelser";
+$_ARRAYLANG['TXT_VOTING_INVALID_EMAIL_ERROR'] = "Indtast venligst en gyldig emailadresse";
+$_ARRAYLANG['TXT_VOTING_ALREADY_VOTED'] = "Du har allerede stemt!";
+$_ARRAYLANG['TXT_VOTING_NONEXISTENT_EMAIL'] = "Din emailadresse eksisterer ikke!";
+$_ARRAYLANG['TXT_ADDITIONAL_NICKNAME'] = "Nickname";
+$_ARRAYLANG['TXT_ADDITIONAL_FORENAME'] = "Fornavn";
 $_ARRAYLANG['TXT_ADDITIONAL_EMAIL'] = "E-Mail-Adresse";
-$_ARRAYLANG['TXT_ADDITIONAL_SURNAME'] = "Nachname";
+$_ARRAYLANG['TXT_ADDITIONAL_SURNAME'] = "Efternavn";
 $_ARRAYLANG['TXT_ADDITIONAL_PHONE'] = "Telefonnummer";
-$_ARRAYLANG['TXT_ADDITIONAL_STREET'] = "Strasse";
-$_ARRAYLANG['TXT_ADDITIONAL_ZIP'] = "PLZ";
-$_ARRAYLANG['TXT_ADDITIONAL'] = "Zusätzliche Eingabefelder für die Benutzer";
-$_ARRAYLANG['TXT_ADDITIONAL_CITY'] = "Ort";
+$_ARRAYLANG['TXT_ADDITIONAL_STREET'] = "Gade / vej";
+$_ARRAYLANG['TXT_ADDITIONAL_ZIP'] = "Postnr.";
+$_ARRAYLANG['TXT_ADDITIONAL'] = "Yderlige felter for brugeren";
+$_ARRAYLANG['TXT_ADDITIONAL_CITY'] = "By";
 $_ARRAYLANG['TXT_ADDITIONAL_COMMENT'] = "Kommentar";
 ?>
