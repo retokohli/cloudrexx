ffb1a4cfdce53d0b96ee2462816d145f305f5189
David Vogt <david.vogt@comvation.com>
2009-03-05 15:28:31 +0000

    recognize some new (and some old) spiders
    git-svn-id: http://svn.contrexxlabs.com/contrexx/trunk@4557 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- core_modules/stats/lib/spiders.inc.php
+++ core_modules/stats/lib/spiders.inc.php
@@ -11,9 +11,9 @@
  */
 
 //defined robots
-$arrRobots=array("google", "msn","agent","bot","crawl","find","seek","seer","search","spider","spyder","seek","info","about",
-"altavista","aport","appie","arachnoidea","askjeeves","asterias","atomz","bigfoot.com","digout4u","phx.gbl",
-"ezresult","flunky","gazz/1.0","gulliver","webtop","ia_archiver","incywincy","indy library","ip3000.com",
+$arrRobots=array("google", "alexa", "msn","agent","bot","crawl","find","seek","seer","search","spider","spyder","seek","info","about",
+"altavista","aport","appie","arachnoidea","teoma", "askjeeves","asterias","atomz","bigfoot.com","digout4u","phx.gbl",
+"ezresult","flunky","gazz/1.0","gulliver","webtop","ia_archiver","incywincy","indy library","ip3000.com","yahoo"
 "fireball","libwww-perl/5.47","linkwalker","LWP::Simple","lwp-trivial","SurveyBot","Rumours-Agent",
 "MarkWatch","mercator","mozilla/4.05","muscatferret","nameprotect","nazilla","netmechanic",
 "nico","roach.smo.av.com-1.0","robozilla","rotondo","scooter","scoutAbout",
