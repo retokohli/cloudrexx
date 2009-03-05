595c5b2e38d6f07d803f81d3a35e5939b9892107
Reto Kohli <reto.kohli@comvation.com>
2009-03-05 13:15:57 +0000

    lib/DBG.php - Added constant DBG_NONE - Applied some formatting
    git-svn-id: http://svn.contrexxlabs.com/contrexx/trunk@4538 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- lib/DBG.php
+++ lib/DBG.php
@@ -1,132 +1,165 @@
-<?PHP
+<?php
 
-define('DBG_PHP'           , 1);
-define('DBG_ADODB'         , 2);
-define('DBG_ADODB_TRACE'   , 4);
-define('DBG_LOG_FILE'      , 8);
-define('DBG_LOG_FIREPHP'   , 16);
-define('DBG_ALL'           , 31);
+define('DBG_NONE'       ,  0);
+define('DBG_PHP'        ,  1);
+define('DBG_ADODB'      ,  2);
+define('DBG_ADODB_TRACE',  4);
+define('DBG_LOG_FILE'   ,  8);
+define('DBG_LOG_FIREPHP', 16);
+define('DBG_ALL'        , 31);
 
 // Redirect ADODB output to us instead of STDOUT.
 define('ADODB_OUTP', 'DBG_log_adodb');
 
-class DBG {
-	private static $dbg_fh = null;
+class DBG
+{
+    private static $dbg_fh = null;
     private static $fileskiplength = 0;
-
-	private static $enable_msg   = null;
-	private static $enable_trace = null;
-	private static $enable_dump  = null;
-	private static $enable_time  = null;
+    private static $enable_msg   = null;
+    private static $enable_trace = null;
+    private static $enable_dump  = null;
+    private static $enable_time  = null;
     private static $firephp      = null;
     private static $last_time    = null;
     private static $start_time   = null;
 
-    public function __construct($enable_firephp = true) {
+    public function __construct()
+    {
         throw new Exception('This is a static class! No need to create an object!');
     }
 
-    static function enable_firephp() {
+
+    static function enable_firephp()
+    {
         if (require_once('firephp/FirePHP.class.php')) {
             self::$firephp = FirePHP::getInstance(true);
         }
     }
 
-    static function enable_all() {
+
+    static function enable_all()
+    {
         self::enable_msg  ();
         self::enable_trace();
         self::enable_dump ();
         self::enable_time ();
     }
 
-    static function time() {
-		if(self::$enable_time) {
+
+    static function time()
+    {
+        if(self::$enable_time) {
             $t = self::$last_time;
             self::$last_time = microtime(true);
             $diff_last  = round(self::$last_time  - $t, 5);
             $diff_start = round(self::$last_time-self::$start_time, 5);
-			$callers = debug_backtrace();
-			$f = self::_cleanfile($callers[0]['file']);
-			$l = $callers[0]['line'];
+            $callers = debug_backtrace();
+            $f = self::_cleanfile($callers[0]['file']);
+            $l = $callers[0]['line'];
             $d = date('H:i:s');
 
-			self::_log("TIME AT: $f:$l $d (diff: $diff_last, startdiff: $diff_start)");
-		}
+            self::_log("TIME AT: $f:$l $d (diff: $diff_last, startdiff: $diff_start)");
+        }
     }
 
-	static function setup($file, $mode='a') {
-		if (self::$dbg_fh) fclose(self::$dbg_fh);
-	    if(!is_null(self::$firephp)) return true; //no need to setup ressources, we're using firephp
-		self::$dbg_fh = fopen($file, $mode);
+
+    static function setup($file, $mode='a')
+    {
+        if (self::$dbg_fh) fclose(self::$dbg_fh);
+        if(!is_null(self::$firephp)) return true; //no need to setup ressources, we're using firephp
+        self::$dbg_fh = fopen($file, $mode);
         $cinfo = debug_backtrace();
 
         self::$fileskiplength = strlen(dirname($cinfo[0]['file'])) +1;
-	}
+        return true;
+    }
 
-	static function enable_trace() {
+
+    static function enable_trace()
+    {
         self::_log('XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX ENABLING TRACE XXXXXXXXXXXXXXXX');
-		self::$enable_trace = 1;
-	}
+        self::$enable_trace = 1;
+    }
 
-	static function disable_trace() {
-		self::$enable_trace = 0;
-	}
 
-	static function enable_time() {
+    static function disable_trace()
+    {
+        self::$enable_trace = 0;
+    }
+
+
+    static function enable_time()
+    {
         self::_log('XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX ENABLING TIME XXXXXXXXXXXXXXXXX');
         if (!self::$enable_time) {
             self::$enable_time = 1;
             self::$start_time = microtime(true);
             self::$last_time  = microtime(true);
         }
-	}
+    }
 
-	static function enable_dump() {
+
+    static function enable_dump()
+    {
         self::_log('XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX ENABLING DUMP XXXXXXXXXXXXXXXXX');
-		self::$enable_dump = 1;
-	}
+        self::$enable_dump = 1;
+    }
 
-	static function disable_dump() {
-		self::$enable_dump = 0;
-	}
 
-	static function enable_msg() {
+    static function disable_dump()
+    {
+        self::$enable_dump = 0;
+    }
+
+
+    static function enable_msg()
+    {
         self::_log('XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX ENABLING MSG XXXXXXXXXXXXXXXXXX');
-		self::$enable_msg = 1;
-	}
+        self::$enable_msg = 1;
+    }
 
-	static function disable_msg() {
-		self::$enable_msg = 0;
-	}
 
-    static function _cleanfile($f) {
+    static function disable_msg()
+    {
+        self::$enable_msg = 0;
+    }
+
+
+    static function _cleanfile($f)
+    {
         return substr($f, self::$fileskiplength);
     }
 
-	static function trace($level=0) {
-		if (self::$enable_trace) {
-			$callers = debug_backtrace();
-			$f = self::_cleanfile($callers[$level]['file']);
-			$l = $callers[$level]['line'];
-			self::_log("TRACE:  $f : $l");
-		}
-	}
 
-    static function calltrace() {
-		if (self::$enable_trace) {
-			$level = 1;
-			$callers = debug_backtrace();
-			$c = $callers[$level]['class'];
-			$f = $callers[$level]['function'];
+    static function trace($level=0)
+    {
+        if (self::$enable_trace) {
+            $callers = debug_backtrace();
+            $f = self::_cleanfile($callers[$level]['file']);
+            $l = $callers[$level]['line'];
+            self::_log("TRACE:  $f : $l");
+        }
+    }
+
+
+    static function calltrace()
+    {
+        if (self::$enable_trace) {
+            $level = 1;
+            $callers = debug_backtrace();
+            $c = $callers[$level]['class'];
+            $f = $callers[$level]['function'];
             self::trace($level);
 
             $sf = self::_cleanfile($callers[$level]['file']);
             $sl = $callers[$level]['line'];
-			self::_log("        $c::$f FROM $sf : $sl");
-		}
+            self::_log("        $c::$f FROM $sf : $sl");
+        }
     }
 
-    static function dump($val){
+
+    static function dump($val)
+    {
         if (!self::$enable_dump) return;
 
         if(!is_null(self::$firephp)){
@@ -141,7 +174,9 @@ class DBG {
         self::_log('DUMP:   '.$out);
     }
 
-    static function stack(){
+
+    static function stack()
+    {
         $callers = debug_backtrace();
         self::_log("TRACE:  === STACKTRACE BEGIN ===");
         $err = error_reporting(E_ALL ^ E_NOTICE);
@@ -156,18 +191,24 @@ class DBG {
         self::_log("        === STACKTRACE END ====");
     }
 
-	static function msg($message) {
-		if(self::$enable_msg) {
-			self::_log('LOGMSG: '.$message);
-		}
 
-	}
+    static function msg($message)
+    {
+        if(self::$enable_msg) {
+            self::_log('LOGMSG: '.$message);
+        }
 
-	static function log($text, $firephp_action = 'log', $additional_args = null) {
-	    self::_log($text, $firephp_action, $additional_args);
-	}
+    }
 
-	private static function _log($text, $firephp_action = 'log', $additional_args = null) {
+
+    static function log($text, $firephp_action = 'log', $additional_args = null)
+    {
+        self::_log($text, $firephp_action, $additional_args);
+    }
+
+
+    private static function _log($text, $firephp_action = 'log', $additional_args = null)
+    {
         if(!is_null(self::$firephp) && method_exists(self::$firephp, $firephp_action)){
             self::$firephp->$firephp_action($additional_args, $text);
         }else{
@@ -175,10 +216,13 @@ class DBG {
                 fputs(self::$dbg_fh, $text."\n");
             }
         }
-	}
+    }
+
 }
 
-function DBG_log_adodb($msg, $newline) {
+
+function DBG_log_adodb($msg)
+{
     DBG::log(strip_tags($msg));
 }
 
