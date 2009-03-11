be5eff05cabe051ab9ff37aee4f9cab7a9767140
David Vogt <david.vogt@comvation.com>
2009-03-11 14:27:49 +0000

    send edition and remote addr - improved customer stats
    git-svn-id: http://svn.contrexxlabs.com/contrexx/trunk@4693 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- cadmin/versioncheck.php
+++ cadmin/versioncheck.php
@@ -7,16 +7,21 @@ require_once("../config/version.php");
 if ($_SERVER['HTTP_X_PROTOTYPE_VERSION']) {
     // Check if there's a new version
     $v = preg_split('#\.#', $_CONFIG['coreCmsVersion']);
+    $e = $_CONFIG['coreCmsEdition'];
+
     $version = $v[0]  * 10000 + $v[1]  * 100 + $v[2];
     $link = @fsockopen('www.contrexx.com',80);
     if (!$link) {
         exit;
     }
-    $r = $_SERVER['PHP_SELF'];
+    $r = $_SERVER['HTTP_REFERER'];
+    $a = $_SERVER['REMOTE_ADDR'];
     fwrite($link,
         "GET /updatecenter/check.php?v=$version HTTP/1.1\n".
         "Host: www.contrexx.com\n".
         "Referer: $r\n".
+        "X-Edition: $e\n".
+        "X-Remote-Addr: $a\n".
         "Connection: close\n\n"
     );
 
@@ -25,7 +30,7 @@ if ($_SERVER['HTTP_X_PROTOTYPE_VERSION']) {
     while ($l) {
         if (preg_match('#NVERSION=(\d+\.\d+\.\d+)#', $l, $r)) {
             $newversion = $r[1];
-            echo '(New Version available: <a style="color:black;" href="http://www.contrexx.com" target="_new">'.$newversion.'</a>)';
+            echo '(Update: <a style="color:black;" href="http://www.contrexx.com" target="_new">'.$newversion.'</a>)';
             fclose($link);
             exit;
         }
