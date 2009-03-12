140eac5b67e84a60d087a06a51df027018b98d2b
David Vogt <david.vogt@comvation.com>
2009-03-12 11:30:09 +0000

    (AUTOMATIC) update Trunk's installer dumps from contrexxlabs_contrexx_trunk
    git-svn-id: http://svn.contrexxlabs.com/contrexx/trunk@4726 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- installer/data/contrexx_dump_data.sql
+++ installer/data/contrexx_dump_data.sql
@@ -3736,6 +3736,7 @@ INSERT INTO `contrexx_modules` (`id`, `name`, `description_variable`, `status`, 
 INSERT INTO `contrexx_modules` (`id`, `name`, `description_variable`, `status`, `is_required`, `is_core`) VALUES (57,'jobs','TXT_JOBS_MODULE_DESCRIPTION','y',0,0);
 INSERT INTO `contrexx_modules` (`id`, `name`, `description_variable`, `status`, `is_required`, `is_core`) VALUES (58,'auction','TXT_AUCTION_MODULE_DESCRIPTION','y',0,0);
 INSERT INTO `contrexx_modules` (`id`, `name`, `description_variable`, `status`, `is_required`, `is_core`) VALUES (59,'partners','TXT_PARTNERS_MODULE_DESCRIPTION','y',0,0);
+INSERT INTO `contrexx_sessions` (`sessionid`, `startdate`, `lastupdated`, `status`, `user_id`, `datavalue`) VALUES ('fb2a7d09c937607d7226433f5a8768d0','1236855645','1236855645','frontend',0,'');
 INSERT INTO `contrexx_sessions` (`sessionid`, `startdate`, `lastupdated`, `status`, `user_id`, `datavalue`) VALUES ('45b691cb772f03fb92af707dae1652c4','1236854625','1236854642','frontend',1,'');
 INSERT INTO `contrexx_sessions` (`sessionid`, `startdate`, `lastupdated`, `status`, `user_id`, `datavalue`) VALUES ('9a211f1c46afd9070c4924aa4fac7018','1236854937','1236854944','frontend',0,'');
 INSERT INTO `contrexx_settings` (`setid`, `setname`, `setvalue`, `setmodule`) VALUES (3,'dnsServer','ns1.contrexxhosting.com',1);
