648d6f7a516396442eb772e186f672bd67a3f814
David Vogt <david.vogt@comvation.com>
2009-03-27 14:58:34 +0000

    UpdateUtil for making database structure changes simpler.. fixes in downloads, guestbook updates, new jobs update (using UpdateUtils)
    git-svn-id: http://svn.contrexxlabs.com/contrexx/branches/contrexx_2_1@5007 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

---
+++ update/UpdateUtil.php
@@ -0,0 +1,189 @@
+<?PHP
+class UpdateException extends Exception {};
+
+class Update_DatabaseException extends UpdateException {
+    public $sql;
+    function __construct($message, $sql = null) {
+        parent::__construct($message);
+        $this->sql = $sql;
+    }
+}
+
+class UpdateUtil {
+    /**
+     * Creates or modifies a table to the given specification.
+     * 
+     * @param string name - the name of the table. do not forget DBPREFIX!
+     * @param array struc - the structure of the columns. This is an associative
+     *     array with the keys being the column names and the values being an array
+     *     with the following keys:
+     *       array(
+     *           'type'            => 'INT', # or VARCHAR(30) or whatever
+     *           'notnull'         => true/false, # optional, defaults to true
+     *           'auto_increment'  => true/false, # optional, defaults to false
+     *           'default'         => 'value',    # optional, defaults to '' (or 0 if type is INT)
+     *           'primary'         => true/false, # optional, defaults to false
+     *           'renamefrom'      => 'a_name'    # optional. Use this if the column existed previously with another name
+     *       )
+     * @param array idx - optional. Additional index specification. This is an associative array
+     *     where the keys are index names and the values are arrays with the following
+     *     keys:
+     *        array(
+     *            'fields' => array('field1', 'field2', ..), # field names to be indexed
+     *            'type'   => 'UNIQUE/FULLTEXT', # optional. If left out, a normal search index is created
+     *        )
+     */
+    public static function table($name, array $struc, array $idx = array()) {
+        global $_CORELANG, $objDatabase, $_ARRAYLANG;
+        $tableinfo = $objDatabase->MetaTables();
+        if ($tableinfo === false) {
+            throw new UpdateException(sprintf($_ARRAYLANG['TXT_UNABLE_GETTING_DATABASE_TABLE_STRUCTURE'], $name));
+        }
+        
+        DBG::dump($tableinfo);
+        if (in_array($name, $tableinfo)) {
+            DBG::msg("table exists, checking cols, indexes: $name");
+            self::check_columns($name, $struc);
+            self::check_indexes($name, $idx);
+        }
+        else {
+            DBG::msg("not in above array: $name");
+            self::create_table($name, $struc, $idx);
+        }
+    }
+
+    private static function create_table($name, array $struc, $idx) {
+        global $_CORELANG, $objDatabase, $_ARRAYLANG;
+
+        // create table statement
+        $cols = array();
+        foreach ($struc as $col => $spec) {
+            $cols[] = "`$col` ". self::_colspec($spec);
+        }
+        $colspec    = join(",\n", $cols);
+        $primaries  = join(",\n", self::_getprimaries($struc));
+
+        $table_stmt = "CREATE TABLE `$name`(
+            $colspec,
+            PRIMARY KEY ($primaries)
+        )";
+
+        if ($objDatabase->Execute($table_stmt) === false) {
+            throw new Update_DatabaseException($objDatabase->ErrorMsg, $table_stmt);
+        }
+        // index statements. as we just created the table
+        // we can now just do check_indexes() to take care
+        // of the "problem".
+        self::check_indexes($name, $idx);
+    }
+
+    private function check_columns($name, $struc) {
+        global $objDatabase;
+        $col_info = $objDatabase->MetaColumns($name);
+        if ($col_info === false) {
+            throw new UpdateException(sprintf($_ARRAYLANG['TXT_UNABLE_GETTING_DATABASE_TABLE_STRUCTURE'], $name));
+        }
+        foreach ($struc as $col => $spec) {
+            self::_check_column($name, $col_info, $col, $spec);
+        }
+    }
+
+    private function _check_column($name, $col_info, $col, $spec) {
+        global $objDatabase;
+        if (!isset($col_info[strtoupper($col)])) {
+            $colspec = self::_colspec($spec);
+            // check if we need to rename the column
+            if (isset($spec['renamefrom']) and isset($col_info[strtoupper($spec['renamefrom'])])) {
+                // rename requested and possible.
+                $from = $spec['renamefrom'];
+                $query = "ALTER TABLE `$name` CHANGE `$from` `$col` $colspec";
+            }
+            else {
+                // rename not possible or not requested. create the new column!
+                // TODO: maybe we should somehow notify the caller if
+                //       rename was requested but not possible?
+                $query = "ALTER TABLE `$name` ADD `$col` $colspec";
+            }
+ 
+            if ($objDatabase->Execute($query) === false) {
+                throw new Update_DatabaseException($objDatabase->ErrorMsg, $query);
+            }
+		}
+
+        // TODO: maybe we should check for the type of the
+        // existing column here and adjust it too?
+    }
+
+    private function check_indexes($name, $idx) {
+        global $objDatabase;
+        # mysql> show index from contrexx_access_user_mail;
+        $key_qry = "SHOW INDEX FROM `$name`";
+        $keyinfo = $objDatabase->Execute($key_qry);
+        if ($keyinfo === false) {
+            throw new Update_DatabaseException($objDatabase->ErrorMsg, $key_qry);
+        }
+        DBG::msg("key info query = $key_qry");
+
+        // Find already existing keys, drop unused keys
+        while (!$keyinfo->EOF) {
+            DBG::dump($keyinfo->fields);
+            if (array_key_exists($keyinfo->fields['Key_name'], $idx)) {
+                $idx[$keyinfo->fields['Key_name']]['exists'] = true;
+            }
+            elseif($keyinfo->fields['Key_name'] != 'PRIMARY') {
+                DBG::msg("index ". $keyinfo->fields['Key_name'] . 'is not primary and not in IDX list.. dropping');
+                DBG::dump($idx);
+                // primary keys should NOT be dropped :P
+                $drop_st = self::_dropkey($name, $keyinfo->fields['Key_name']);
+                if($objDatabase->Execute($drop_st)===false) {
+                    throw new Update_DatabaseException($objDatabase->ErrorMsg, $drop_st);
+                }
+            }
+            $keyinfo->MoveNext();
+        }
+
+        // create new keys
+        foreach ($idx as $keyname => $spec) {
+            if (!array_key_exists('exists', $spec)) {
+                $new_st = self::_keyspec($name, $keyname, $spec);
+                if($objDatabase->Execute($new_st)===false) {
+                    throw new Update_DatabaseException($objDatabase->ErrorMsg, $new_st);
+                }
+            }
+        }
+        // okay, that's it, have a nice day!
+    }
+
+    private function _dropkey($table, $name) {
+        return "DROP INDEX `$name` ON `$table`";
+    }
+
+    private function _keyspec($table, $name, $spec) {
+        $fields = '`'. join('`, `', $spec['fields']) . '`';
+        $type   = array_key_exists('type', $spec) ? $spec['type'] : '';
+
+        $descr  = "CREATE $type INDEX ON $table ($fields)";
+    }
+    private function _colspec($spec) {
+        $notnull = (array_key_exists('notnull',        $spec)) ? $spec['notnull']        : true;
+        $autoinc = (array_key_exists('auto_increment', $spec)) ? $spec['auto_increment'] : false;
+        $default = (array_key_exists('default',        $spec)) ? addslashes($spec['default']) : '';
+
+        $descr  = $spec['type'];
+        $descr .= $notnull ? " NOT NULL" : '';
+        $descr .= $autoinc ? " auto_increment" : '';
+        $descr .= $default ? " DEFAULT '$default'" : '';
+        return $descr;
+    }
+    private function _getprimaries($struc) {
+        $primaries = array();
+        foreach ($struc as $name => $spec) {
+            $is_primary = (array_key_exists('primary', $spec)) ? $spec['primary'] : false;
+            if ($is_primary) {
+                $primaries[] = $name;
+            }
+        }
+        return $primaries;
+    }
+}
+
---
+++ update/updates/2.1.0/components/module/jobs.php
@@ -0,0 +1,69 @@
+<?php
+function _jobsUpdate() {
+	global $_ARRAYLANG;
+
+    try {
+        UpdateUtil::table(
+            DBPREFIX . 'module_jobs',
+            array(
+                'id'         => array('type' => 'INT(6)',       'notnull' => true,  'primary' => true, 'auto_increment' => true),
+                'date'       => array('type' => 'INT(14)',      'notnull' => false),
+                'title'      => array('type' => 'VARCHAR(250)', 'notnull' => true,  'default' => ''),
+                'author'     => array('type' => 'VARCHAR(150)', 'notnull' => true,  'default' => ''),
+                'workloc'    => array('type' => 'VARCHAR(250)', 'notnull' => true,  'default' => ''),
+                'workload'   => array('type' => 'VARCHAR(250)', 'notnull' => true,  'default' => ''),
+                'work_start' => array('type' => 'INT(14)',      'notnull' => true,  'default' => 0),
+                'catid'      => array('type' => 'INT(2)',       'notnull' => true,  'default' => 0),
+                'lang'       => array('type' => 'INT(2)',       'notnull' => true,  'default' => 0),
+                'userid'     => array('type' => 'INT(6)',       'notnull' => true,  'default' => 0),
+                'startdate'  => array('type' => 'DATE',         'notnull' => true,  'default' => '0000-00-00'),
+                'enddate'    => array('type' => 'DATE',         'notnull' => true,  'default' => '0000-00-00'),
+                'status'     => array('type' => 'INT(4)',       'notnull' => true,  'default' => 1),
+                'changelog'  => array('type' => 'INT(14)',      'notnull' => true,  'default' => 0)
+            )
+        );
+        UpdateUtil::table(
+            DBPREFIX . 'module_jobs_categories',
+            array(
+                'catid'      => array('type' => 'INT(2)',           'primary' => true, 'auto_increment' => true),
+                'name'       => array('type' => 'VARCHAR(100)',                        'default'        => ''),
+                'lang'       => array('type' => 'INT(2)',                              'default'        => 0),
+                'sort_style' => array('type' => "ENUM('alpha', 'date', 'date_alpha')", 'default'        => 'alpha')
+            )
+        );
+        UpdateUtil::table(
+            DBPREFIX . 'module_jobs_location',
+            array(
+                'id'   => array('type' => 'INT(10)',      'primary' => true, 'auto_increment' => true),
+                'name' => array('type' => 'VARCHAR(100)', 'default' => '')
+            )
+        );
+        UpdateUtil::table(
+            DBPREFIX . 'module_jobs_rel_loc_jobs',
+            array(
+                'job'      => array('type' => 'INT(10)', 'primary' => true),
+                'location' => array('type' => 'INT(10)', 'primary' => true)
+            )
+        );
+        UpdateUtil::table(
+            DBPREFIX . 'module_jobs_settings',
+            array(
+                'id'    => array('type' => 'INT(10)',      'primary' => true),
+                'name'  => array('type' => 'VARCHAR(250)', 'default' => ''),
+                'value' => array('type' => 'TEXT',         'default' => '')
+            )
+        );
+
+    }
+    catch (UpdateException $e) {
+        if ($e instanceof Update_DatabaseException) {
+            return _databaseError($e->sql, $e->getMessage());
+        }
+		setUpdateMsg($e->getMessage());
+		return false;
+    }
+	
+	// Everything went fine. Return without any errors.
+    return true;
+}
+
