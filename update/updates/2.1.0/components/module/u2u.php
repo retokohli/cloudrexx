589b633a334d788a69497c35e3f255cc706272c0
David Vogt <david.vogt@comvation.com>
2009-04-08 14:54:55 +0000

    table structure for U2U. UpdateUtil now correctly handles default, default_expr, even with TEXT and BLOB fields
    git-svn-id: http://svn.contrexxlabs.com/contrexx/branches/contrexx_2_1@5043 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- update/updates/2.1.0/components/module/u2u.php
+++ update/updates/2.1.0/components/module/u2u.php
@@ -3,29 +3,51 @@
 function _u2uUpdate()
 {
     try{
-        #UpdateUtil::table(
-        #    DBPREFIX . 'voting_additionaldata',
-        #    array(
-        #        'id'               => array('type' => 'INT',         'notnull' => true,  'primary'     => true,    'auto_increment' => true),
-        #        'voting_system_id' => array('type' => 'INT(14)',     'notnull' => true,                            'renamefrom'   => 'voting_sytem_id'),
-        #        'nickname'         => array('type' => 'VARCHAR(80)', 'notnull' => true,  'default'     => '',      'renamefrom'   => 'name'),
-        #        'date_entered'     => array('type' => 'TIMESTAMP',   'notnull' => false, 'default_expr'=> 'CURRENT_TIMESTAMP'),
-        #        'surname'          => array('type' => 'VARCHAR(80)', 'notnull' => true,  'default'     => ''),
-        #        'forename'         => array('type' => 'VARCHAR(80)', 'notnull' => true,  'default'     => ''),
-        #        'phone'            => array('type' => 'VARCHAR(80)', 'notnull' => true,  'default'     => ''),
-        #        'street'           => array('type' => 'VARCHAR(80)', 'notnull' => true,  'default'     => ''),
-        #        'zip'              => array('type' => 'VARCHAR(80)', 'notnull' => true,  'default'     => ''),
-        #        'city'             => array('type' => 'VARCHAR(80)', 'notnull' => true,  'default'     => ''),
-        #        'email'            => array('type' => 'VARCHAR(80)', 'notnull' => true,  'default'     => ''),
-        #        'comment'          => array('type' => 'TEXT',        'notnull' => true,  'default'     => ''),
-        #    ),
-        #    array( # indexes
-        #        'voting_system_id' => array(
-        #            'fields'=>array('voting_system_id')
-        #        )
-        #    )
-        #);
-
+        UpdateUtil::table(
+            DBPREFIX . 'module_u2u_address_list',
+            array(
+                'id'               => array('type' => 'INT(11)',     'notnull' => true, 'primary' => true, 'auto_increment' => true),
+                'user_id'          => array('type' => 'INT(11)',     'notnull' => true),
+                'buddies_id'       => array('type' => 'INT(11)',     'notnull' => true),
+            )
+        );
+        UpdateUtil::table(
+            DBPREFIX . 'module_u2u_message_log',
+            array(
+                'message_id'       => array('type' => 'INT(11) UNSIGNED','notnull' => true, 'primary' => true, 'auto_increment' => true),
+                'message_text'     => array('type' => 'TEXT',            'notnull' => true),
+                'message_title'    => array('type' => 'TEXT',            'notnull' => true),
+            )
+        );
+        UpdateUtil::table(
+            DBPREFIX . 'module_u2u_sent_messages',
+            array(
+                'id'                  => array('type' => 'INT(11) UNSIGNED',      'notnull' => true, 'primary' => true, 'auto_increment' => true),
+                'userid'              => array('type' => 'INT(11) UNSIGNED',      'notnull' => true),
+                'message_id'          => array('type' => 'INT(11) UNSIGNED',      'notnull' => true),
+                'receiver_id'         => array('type' => 'INT(11) UNSIGNED',      'notnull' => true),
+                'mesage_open_status'  => array('type' => "ENUM('0','1')",         'notnull' => true, 'default' => '0'),
+                'date_time'           => array('type' => "DATETIME",              'notnull' => true),
+            )
+        );
+        UpdateUtil::table(
+            DBPREFIX . 'module_u2u_settings',
+            array(
+                'id'                  => array('type' => 'INT(11) UNSIGNED','notnull' => true, 'primary' => true, 'auto_increment' => true),
+                'name'                => array('type' => 'VARCHAR(50)',     'notnull' => true),
+                'value'               => array('type' => 'TEXT',            'notnull' => true),
+            )
+        );
+        UpdateUtil::table(
+            DBPREFIX . 'module_u2u_user_log',
+            array(
+                'id'                  => array('type' => 'INT(11) UNSIGNED', 'notnull' => true, 'primary' => true, 'auto_increment' => true),
+                'userid'              => array('type' => 'INT(11) UNSIGNED', 'notnull' => true),
+                'user_sent_items'     => array('type' => 'INT(11) UNSIGNED', 'notnull' => true),
+                'user_unread_items'   => array('type' => 'INT(11) UNSIGNED', 'notnull' => true),
+                'user_status'         => array('type' => "ENUM('0','1')",    'notnull' => true, 'default' => '1'),
+            )
+        );
     }
     catch (UpdateException $e) {
         // we COULD do something else here..
