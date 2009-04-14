785a6f47153c71679bb03b498114a3e7d1bd6c14
David Vogt <david.vogt@comvation.com>
2009-04-14 07:00:12 +0000

    (AUTOMATIC) update Trunk's installer dumps from contrexxlabs_contrexx_trunk
    git-svn-id: http://svn.contrexxlabs.com/contrexx/trunk@5047 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- installer/data/contrexx_dump_structure.sql
+++ installer/data/contrexx_dump_structure.sql
@@ -1386,9 +1386,8 @@ CREATE TABLE `contrexx_module_gallery_votes` (
 CREATE TABLE `contrexx_module_guestbook` (
   `id` int(6) unsigned NOT NULL auto_increment,
   `status` tinyint(1) unsigned NOT NULL default '0',
-  `nickname` tinytext NOT NULL,
-  `name` tinytext NOT NULL,
-  `forename` tinytext NOT NULL,
+  `name` varchar(255) NOT NULL default '',
+  `forename` varchar(255) NOT NULL default '',
   `gender` char(1) NOT NULL default '',
   `url` tinytext NOT NULL,
   `email` tinytext NOT NULL,
