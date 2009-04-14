1049f672e3394b5fe1ba95759043d35afbb0fcc0
Thomas Däppen <thomas.daeppen@cloudrexx.com>
2009-04-14 07:41:49 +0000

    update: database structure bugfixes (synchronization with the full version dump)
    git-svn-id: http://svn.contrexxlabs.com/contrexx/branches/contrexx_2_1@5049 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- update/updates/2.1.0/components/module/knowledge.php
+++ update/updates/2.1.0/components/module/knowledge.php
@@ -6,11 +6,11 @@ function _knowledgeUpdate()
         UpdateUtil::table(
             DBPREFIX . 'module_knowledge_article_content',
             array(
-                'id'       => array('type' => 'INT',  'notnull' => true, 'primary' => true, 'auto_increment' => true),
-                'article'  => array('type' => 'INT',  'notnull' => true, 'default' => 0),
-                'lang'     => array('type' => 'INT',  'notnull' => true, 'default' => 0),
-                'question' => array('type' => 'TEXT', 'notnull' => true, 'default' => 0),
-                'answer'   => array('type' => 'TEXT', 'notnull' => true, 'default' => 0),
+                'id'       => array('type' => 'INT(10)',  'notnull' => true, 'primary' => true, 'auto_increment' => true, 'unsigned' => true),
+                'article'  => array('type' => 'INT(10)',  'notnull' => true, 'default' => 0, 'unsigned' => true),
+                'lang'     => array('type' => 'INT(10)',  'notnull' => true, 'default' => 0, 'unsigned' => true),
+                'question' => array('type' => 'TEXT',     'notnull' => true, 'default' => 0),
+                'answer'   => array('type' => 'TEXT',     'notnull' => true, 'default' => 0),
             ),
             array( # indexes
                 'module_knowledge_article_content_lang'    => array( 'fields'=>array('lang')),
@@ -20,24 +20,24 @@ function _knowledgeUpdate()
         UpdateUtil::table(
             DBPREFIX . 'module_knowledge_articles',
             array(
-                'id'           => array('type' => 'INT',    'notnull' => true, 'primary' => true, 'auto_increment' => true),
-                'category'     => array('type' => 'INT',    'notnull' => true, 'default' => 0),
-                'active'       => array('type' => 'INT(1)', 'notnull' => true, 'default' => 1),
-                'hits'         => array('type' => 'INT',    'notnull' => true, 'default' => 0),
-                'votes'        => array('type' => 'INT',    'notnull' => true, 'default' => 0),
-                'votevalue'    => array('type' => 'INT',    'notnull' => true, 'default' => 0),
-                'sort'         => array('type' => 'INT',    'notnull' => true, 'default' => 0),
-                'date_created' => array('type' => 'INT',    'notnull' => true, 'default' => 0),
-                'date_updated' => array('type' => 'INT',    'notnull' => true, 'default' => 0),
+                'id'           => array('type' => 'INT(10)',    'notnull' => true, 'primary' => true, 'auto_increment' => true, 'unsigned' => true),
+                'category'     => array('type' => 'INT(10)',    'notnull' => true, 'default' => 0, 'unsigned' => true),
+                'active'       => array('type' => 'TINYINT(1)', 'notnull' => true, 'default' => 1),
+                'hits'         => array('type' => 'INT',        'notnull' => true, 'default' => 0),
+                'votes'        => array('type' => 'INT',        'notnull' => true, 'default' => 0),
+                'votevalue'    => array('type' => 'INT',        'notnull' => true, 'default' => 0),
+                'sort'         => array('type' => 'INT',        'notnull' => true, 'default' => 0),
+                'date_created' => array('type' => 'INT(14)',    'notnull' => true, 'default' => 0),
+                'date_updated' => array('type' => 'INT(14)',    'notnull' => true, 'default' => 0),
             )
         );
         UpdateUtil::table(
             DBPREFIX . 'module_knowledge_categories',
             array(
-                'id'           => array('type' => 'INT',    'notnull' => true, 'primary' => true, 'auto_increment' => true),
-                'active'       => array('type' => 'INT(1)', 'notnull' => true, 'default' => 1),
-                'parent'       => array('type' => 'INT',    'notnull' => true, 'default' => 0),
-                'sort'         => array('type' => 'INT',    'notnull' => true, 'default' => 0),
+                'id'           => array('type' => 'INT(10)',    'notnull' => true, 'primary' => true, 'auto_increment' => true, 'unsigned' => true),
+                'active'       => array('type' => 'TINYINT(1)', 'notnull' => true, 'default' => 1, 'unsigned' => true),
+                'parent'       => array('type' => 'INT(10)',    'notnull' => true, 'default' => 0, 'unsigned' => true),
+                'sort'         => array('type' => 'INT(10)',    'notnull' => true, 'default' => 1, 'unsigned' => true),
             ),
             array( # indexes
                 'module_knowledge_categories_sort'   => array( 'fields'=>array('sort')),
@@ -45,10 +45,19 @@ function _knowledgeUpdate()
             )
         );
         UpdateUtil::table(
+            DBPREFIX . 'module_knowledge_categories_content',
+            array(
+                'id'           => array('type' => 'INT(10)',    'notnull' => true, 'primary' => true, 'auto_increment' => true, 'unsigned' => true),
+                'category'     => array('type' => 'INT(10)', 'notnull' => true, 'default' => 0, 'unsigned' => true),
+                'name'         => array('type' => 'VARCHAR(255)',    'notnull' => true),
+                'lang'         => array('type' => 'INT(11)',    'notnull' => true, 'default' => 1),
+            )
+        );
+        UpdateUtil::table(
             DBPREFIX . 'module_knowledge_settings',
             array(
-                'id'    => array('type' => 'INT',          'notnull' => true, 'primary' => true, 'auto_increment' => true),
-                'name'  => array('type' => 'VARCHAR(255)', 'notnull' => true, 'default' => 1),
+                'id'    => array('type' => 'INT(10)',          'notnull' => true, 'primary' => true, 'auto_increment' => true, 'unsigned' => true),
+                'name'  => array('type' => 'VARCHAR(255)', 'notnull' => true),
                 'value' => array('type' => 'TEXT',         'notnull' => true, 'default' => 0),
             ),
             array( # indexes
@@ -58,9 +67,9 @@ function _knowledgeUpdate()
         UpdateUtil::table(
             DBPREFIX . 'module_knowledge_tags',
             array(
-                'id'    => array('type' => 'INT',          'notnull' => true, 'primary' => true, 'auto_increment' => true),
+                'id'    => array('type' => 'INT(10)',          'notnull' => true, 'primary' => true, 'auto_increment' => true, 'unsigned' => true),
                 'name'  => array('type' => 'VARCHAR(255)', 'notnull' => true, 'default' => 1),
-                'lang'  => array('type' => 'INT',          'notnull' => true, 'default' => 0),
+                'lang'  => array('type' => 'INT(10)',          'notnull' => true, 'default' => 1, 'unsigned' => true),
             ),
             array( # indexes
                 'module_knowledge_tags_name'   => array( 'fields'=>array('name'))
@@ -69,9 +78,9 @@ function _knowledgeUpdate()
         UpdateUtil::table(
             DBPREFIX . 'module_knowledge_tags_articles',
             array(
-                'id'      => array('type' => 'INT',  'notnull' => true, 'primary' => true, 'auto_increment' => true),
-                'article' => array('type' => 'INT',  'notnull' => true, 'default' => 0),
-                'tag'     => array('type' => 'INT',  'notnull' => true, 'default' => 0),
+                'id'      => array('type' => 'INT(10)',  'notnull' => true, 'primary' => true, 'auto_increment' => true, 'unsigned' => true),
+                'article' => array('type' => 'INT(10)',  'notnull' => true, 'default' => 0, 'unsigned' => true),
+                'tag'     => array('type' => 'INT(10)',  'notnull' => true, 'default' => 0, 'unsigned' => true),
             ),
             array( # indexes
                 'module_knowledge_tags_articles_tag'     => array( 'fields'=>array('tag')),
