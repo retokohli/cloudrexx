3f69e85e1377b8e13415ae46a2057bc4095e5eeb
Thomas Däppen <thomas.daeppen@cloudrexx.com>
2009-04-20 10:39:20 +0000

    update: script bugfixes
    git-svn-id: http://svn.contrexxlabs.com/contrexx/branches/contrexx_2_1@5082 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- update/updates/2.1.0/components/core_module/alias.php
+++ update/updates/2.1.0/components/core_module/alias.php
@@ -1,27 +1,23 @@
 <?php
 function _aliasUpdate() {
-	global $objDatabase;
-
-	// Find out if the alias_source table is already present
-	$tables = $objDatabase->MetaTables('TABLES');
-	if (!$tables) {
-		setUpdateMsg($_ARRAYLANG['TXT_UNABLE_DETERMINE_DATABASE_STRUCTURE']);
-		return false;
-	}
-
-	// Add the isdefault column
-	$qry_add_isdefault = "ALTER TABLE `".DBPREFIX."module_alias_source` ADD `isdefault` BOOL NOT NULL DEFAULT '0'";
-	if (!$objDatabase->Execute($qry_add_isdefault)) {
-		$msg = $objDatabase->ErrorMsg();
-		if (!preg_match('/duplicate\s+column/i', $msg))
-			return _databaseError($qry_add_isdefault, $msg);
-	}
-	
-	// Index the isdefault column
-	$qry_add_isdefault_idx = "ALTER TABLE `".DBPREFIX."module_alias_source` ADD INDEX ( `isdefault` )";
-	if (!$objDatabase->Execute($qry_add_isdefault_idx)) {
-		return _databaseError($qry_add_isdefault_idx, $objDatabase->ErrorMsg());
-	}
+    try {
+        UpdateUtil::table(
+            DBPREFIX.'module_alias_source',
+            array(
+                'id'         => array('type' => 'INT(10)', 'unsigned' => true, 'notnull' => true, 'auto_increment' => true, 'primary' => true),
+                'target_id'  => array('type' => 'INT(10)', 'unsigned' => true),
+                'url'        => array('type' => 'VARCHAR(255)'),
+                'isdefault'  => array('type' => 'TINYINT(1)', 'notnull' => true, 'default' => '0')
+            ),
+            array(
+                'url'        => array('fields' => array('url'), 'type' => 'UNIQUE'),
+                'isdefault'  => array('fields' => array('isdefault'))
+            )
+        );
+    }
+    catch (UpdateException $e) {
+        return UpdateUtil::DefaultActionHandler($e);
+    }
 	return true;
 }
 
