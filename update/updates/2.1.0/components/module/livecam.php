da5c1ef145039732b4da28c247dd7c34c0bf5c05
Thomas Däppen <thomas.daeppen@cloudrexx.com>
2009-04-22 17:47:41 +0000

    update: script bugfix
    git-svn-id: http://svn.contrexxlabs.com/contrexx/branches/contrexx_2_1@5092 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- update/updates/2.1.0/components/module/livecam.php
+++ update/updates/2.1.0/components/module/livecam.php
@@ -15,8 +15,17 @@ function _livecamUpdate()
                 'maxImageWidth'      => array('type' => 'INT(10)', 'unsigned' => true, 'notnull' => true, 'default' => '400'),
                 'thumbMaxSize'       => array('type' => 'INT(10)', 'unsigned' => true, 'notnull' => true, 'default' => '200'),
                 'shadowboxActivate'  => array('type' => 'SET(\'1\',\'0\')', 'notnull' => true, 'default' => '1', 'renamefrom' => 'lightboxActivate'),
-                'showFrom'           => array('type' => 'INT(14)'),
-                'showTill'           => array('type' => 'INT(14)')
+                'showFrom'           => array('type' => 'INT(14)', 'notnull' => true, 'default' => ''),
+                'showTill'           => array('type' => 'INT(14)', 'notnull' => true, 'default' => '')
+            )
+        );
+
+        UpdateUtil::table(
+            DBPREFIX.'module_livecam_settings',
+            array(
+                'setid'      => array('type' => 'INT(10)', 'unsigned' => true, 'notnull' => true, 'auto_increment' => true, 'primary' => true),
+                'setname'    => array('type' => 'VARCHAR(255)', 'notnull' => true, 'default' => ''),
+                'setvalue'   => array('type' => 'TEXT')
             )
         );
     }
@@ -27,8 +36,22 @@ function _livecamUpdate()
 	$defaultFrom = mktime(0, 0);
 	$defaultTill = mktime(23, 59);
     //set new default settings
-	$query = "UPDATE `".DBPREFIX."module_livecam` SET `showFrom`=$defaultFrom, `showTill`=$defaultTill WHERE 1";
+	$query = "UPDATE `".DBPREFIX."module_livecam` SET `showFrom`=$defaultFrom, `showTill`=$defaultTill WHERE `showFrom` = ''";
 	if ($objDatabase->Execute($query) === false) {
+		return _databaseError($query, $objDatabase->ErrorMsg());
+	}
+
+    $query = "SELECT 1 FROM `".DBPREFIX."module_livecam_settings` WHERE `setname` = 'amount_of_cams'";
+    $objResult = $objDatabase->SelectLimit($query, 1);
+    if ($objResult !== false) {
+        if ($objResult->RecordCount() == 0) {
+            $query = "INSERT INTO `".DBPREFIX."module_livecam_settings` (`setid`, `setname`, `setvalue`) VALUES
+('1', 'amount_of_cams', '1')";
+            if ($objDatabase->Execute($query) === false) {
+                return _databaseError($query, $objDatabase->ErrorMsg());
+            }
+        }
+    } else {
 		return _databaseError($query, $objDatabase->ErrorMsg());
 	}
 
