f1c47f6a503b3108f892b9c00a53072285c0ec0d
David Vogt <david.vogt@comvation.com>
2009-05-12 05:30:12 +0000

    (AUTOMATIC) update Trunk's installer dumps from contrexxlabs_contrexx_trunk
    git-svn-id: http://svn.contrexxlabs.com/contrexx/trunk@5149 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

---
+++ installer/data/contrexx_dump_structure.sql
@@ -0,0 +1,19 @@
+CREATE TABLE `contrexx_access_group_dynamic_ids` (
+  `access_id` int(11) unsigned NOT NULL default '0',
+  `group_id` int(11) unsigned NOT NULL default '0'
+) TYPE=MyISAM;
+CREATE TABLE `contrexx_access_group_static_ids` (
+  `access_id` int(11) unsigned NOT NULL default '0',
+  `group_id` int(11) unsigned NOT NULL default '0'
+) TYPE=MyISAM;
+CREATE TABLE `contrexx_access_rel_user_group` (
+  `user_id` int(10) unsigned NOT NULL default '0',
+  `group_id` int(10) unsigned NOT NULL default '0',
+  PRIMARY KEY  (`user_id`,`group_id`)
+) TYPE=InnoDB;
+CREATE TABLE `contrexx_access_settings` (
+  `key` varchar(32) NOT NULL default '',
+  `value` varchar(255) NOT NULL default '',
+  `status` tinyint(1) unsigned NOT NULL default '0',
+  UNIQUE KEY `key` (`key`)
+) TYPE=InnoDB;
