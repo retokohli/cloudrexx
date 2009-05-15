0f21db5c9f9203e5ec4ef1ca58e8aa5482a759f3
David Vogt <david.vogt@comvation.com>
2009-05-15 07:30:22 +0000

    (AUTOMATIC) update 2_1_1's installer dumps from contrexxlabs_contrexx_2_1_1
    git-svn-id: http://svn.contrexxlabs.com/contrexx/branches/contrexx_2_1@5183 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- installer/data/contrexx_dump_structure.sql
+++ installer/data/contrexx_dump_structure.sql
@@ -553,14 +553,6 @@ CREATE TABLE `contrexx_module_calendar` (
   PRIMARY KEY  (`id`),
   FULLTEXT KEY `name` (`name`,`comment`,`placeName`)
 ) TYPE=MyISAM ;
-CREATE TABLE `contrexx_module_calendar_access` (
-  `id` int(11) unsigned NOT NULL auto_increment,
-  `name` varchar(64) NOT NULL default '',
-  `description` varchar(255) NOT NULL default '',
-  `access_id` int(11) unsigned NOT NULL default '0',
-  `type` enum('global','frontend','backend') NOT NULL default 'global',
-  PRIMARY KEY  (`id`)
-) TYPE=MyISAM ;
 CREATE TABLE `contrexx_module_calendar_categories` (
   `id` int(5) NOT NULL auto_increment,
   `name` varchar(150) NOT NULL default '',
