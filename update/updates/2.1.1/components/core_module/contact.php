431a875d9b00bb833f470f9081a177f21645a0e5
David Vogt <david.vogt@comvation.com>
2009-05-20 07:01:07 +0000

    fix for #286 - recipient field type on contact module
    git-svn-id: http://svn.contrexxlabs.com/contrexx/branches/contrexx_2_1@5213 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- update/updates/2.1.1/components/core_module/contact.php
+++ update/updates/2.1.1/components/core_module/contact.php
@@ -13,6 +13,22 @@ function _contactUpdate()
                 'sort'   => array('type' => 'INT(11)',      'notnull' => true, 'default'     => 0),
             )
         );
+
+
+        $type_t = "ENUM('text','label','checkbox','checkboxGroup','date','file','hidden','password','radio','select','textarea','recipient')";
+        UpdateUtil::table(
+            DBPREFIX . 'module_contact_form_field',
+            array(
+                'id'          => array('type' => 'INT',          'notnull' => true, 'primary' => true,      'auto_increment' => true),
+                'id_form'     => array('type' => 'INT(11)',      'notnull' => true, 'default' => 0),
+                'name'        => array('type' => 'VARCHAR(250)', 'notnull' => true, 'default' => ''),
+                'type'        => array('type' => $type_t,        'notnull' => true, 'default' => 'text'),
+                'is_required' => array('type' => 'TEXT',         'notnull' => true),
+                'check_type'  => array('type' => 'INT(3)',       'notnull' => true, 'default' => 0),
+                'order_id'    => array('type' => 'INT(5)',       'notnull' => true, 'default' => 0)
+            )
+        );
+
     }
     catch (UpdateException $e) {
         // we COULD do something else here..
