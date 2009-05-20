8a35c7010fd251f9fb12f1b9a5dbdc0d542d7db6
David Vogt <david.vogt@comvation.com>
2009-05-20 13:52:02 +0000

    fixing bug #331
    git-svn-id: http://svn.contrexxlabs.com/contrexx/branches/contrexx_2_1@5236 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- update/updates/2.1.1/components/module/ecard.php
+++ update/updates/2.1.1/components/module/ecard.php
@@ -2,6 +2,7 @@
 
 function _ecardUpdate()
 {
+    global $objDatabase;
     try{
         UpdateUtil::table(
             DBPREFIX . 'module_ecard_ecards',
@@ -27,6 +28,37 @@ function _ecardUpdate()
     }
     catch (UpdateException $e) {
         return UpdateUtil::DefaultActionHandler($e);
+    }
+
+    # INSERT IGNORE doesn't do anything if it would result in a duplicate key.
+    # Therefore, it's safe to just overwrite, as it actually DOESN't overwrite :D
+    $ins_tpl = "INSERT IGNORE INTO " .DBPREFIX."module_ecard_settings (setting_name, setting_value) VALUES ('%s', '%s');";
+    $insert_values = array(
+        array( 'maxCharacters'  , '100'),
+        array( 'maxLines'       , '50'),
+        array( 'motive_0'       , 'Bild_001.jpg'),
+        array( 'motive_1'       , 'Bild_002.jpg'),
+        array( 'motive_2'       , ''),
+        array( 'motive_3'       , ''),
+        array( 'motive_4'       , ''),
+        array( 'motive_5'       , ''),
+        array( 'motive_6'       , ''),
+        array( 'motive_7'       , ''),
+        array( 'motive_8'       , ''),
+        array( 'maxHeight'      , '300'),
+        array( 'validdays'      , '30'),
+        array( 'maxWidth'       , '300'),
+        array( 'maxHeightThumb' , '80'),
+        array( 'maxWidthThumb'  , '80'),
+        array( 'subject'        , 'Sie haben eine E-Card erhalten!'),
+        array( 'emailText'      , "[[ECARD_SENDER_NAME]] hat Ihnen eine E-Card geschickt.<br />\n Sie können diese während den nächsten [[ECARD_VALID_DAYS]] Tagen unter [[ECARD_URL]] abrufen.")
+    );
+
+    foreach ($insert_values as $setting){
+            $query = sprintf($ins_tpl, addslashes($setting[0]), addslashes($setting[1]));
+            if (! $objDatabase->Execute($query)) {
+                return _databaseError($query, $objDatabase->ErrorMsg());
+            }
     }
 
     return true;
