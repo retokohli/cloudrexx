81bd864f69401b0850e8e154f6981e799e7af3e7
David Vogt <david.vogt@comvation.com>
2009-05-20 13:17:13 +0000

    fixes bug #324
    git-svn-id: http://svn.contrexxlabs.com/contrexx/branches/contrexx_2_1@5232 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- update/updates/2.1.1/components/module/gallery.php
+++ update/updates/2.1.1/components/module/gallery.php
@@ -38,16 +38,29 @@ function _galleryUpdate()
 	}
 
 	$arrSettings = array(
-					'22' => array(
-						'name' 	=> 'show_file_name',
-						'value' => 'off'),
-					'23' => array(
-						'name' 	=> 'slide_show',
-						'value' => 'off'),
-					'24' => array(
-						'name' 	=> 'slide_show_seconds',
-						'value' => '3'),
-					);
+        '1'  => array( 'name' => 'max_images_upload',        'value' => '10'),
+        '2'  => array( 'name' => 'standard_quality',         'value' => '95'),
+        '3'  => array( 'name' => 'standard_size_proz',       'value' => '25'),
+        '4'  => array( 'name' => 'standard_width_abs',       'value' => '140'),
+        '6'  => array( 'name' => 'standard_height_abs',      'value' => '0'),
+        '7'  => array( 'name' => 'standard_size_type',       'value' => 'abs'),
+        '8'  => array( 'name' => 'validation_show_limit',    'value' => '10'),
+        '9'  => array( 'name' => 'validation_standard_type', 'value' => 'all'),
+        '11' => array( 'name' => 'show_names',               'value' => 'off'),
+        '12' => array( 'name' => 'quality',                  'value' => '95'),
+        '13' => array( 'name' => 'show_comments',            'value' => 'off'),
+        '14' => array( 'name' => 'show_voting',              'value' => 'off'),
+        '15' => array( 'name' => 'enable_popups',            'value' => 'on'),
+        '16' => array( 'name' => 'image_width',              'value' => '1200'),
+        '17' => array( 'name' => 'paging',                   'value' => '30'),
+        '18' => array( 'name' => 'show_latest',              'value' => 'on'),
+        '19' => array( 'name' => 'show_random',              'value' => 'on'),
+        '20' => array( 'name' => 'header_type',              'value' => 'hierarchy'),
+        '21' => array( 'name' => 'show_ext',                 'value' => 'off'),
+        '22' => array( 'name' => 'show_file_name',           'value' => 'on'),
+        '23' => array( 'name' => 'slide_show',               'value' => 'slideshow'),
+        '24' => array( 'name' => 'slide_show_seconds',       'value' => '3')
+    );
 
 	foreach ($arrSettings as $id => $arrSetting) {
 		$query = "SELECT 1 FROM `".DBPREFIX."module_gallery_settings` WHERE `name`= '".$arrSetting['name']."'" ;
@@ -66,4 +79,4 @@ function _galleryUpdate()
 
     return true;
 }
-?>
+
