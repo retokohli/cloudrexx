5aaa0c28a287ac1e2dea46d7cb239af96a064dc5
ss4u <comvation@softsolutions4u.info>
2009-05-25 14:11:24 +0000

    ss4u
    git-svn-id: http://svn.contrexxlabs.com/contrexx/trunk@5263 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- core_modules/access/lib/AccessLib.class.php
+++ core_modules/access/lib/AccessLib.class.php
@@ -1,4 +1,8 @@
 <?php
+
+   	$_ARRAYLANG['TXT_ACCESS_SESSION_ON_INTERVAL'] = "SESSION Intervals";
+   	$_ARRAYLANG['TXT_ACCESS_SESSION_TITLE'] = "SESSION";
+   	
 /**
  * User Management
  * @copyright   CONTREXX CMS - COMVATION AG
