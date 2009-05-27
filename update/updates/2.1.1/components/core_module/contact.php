f4e4fb388990fadbe8643fbc77314d1ac7124903
Thomas Däppen <thomas.daeppen@cloudrexx.com>
2009-05-27 18:23:57 +0000

    update: update script bugfixes (contact.php / news.php / access.php)
    git-svn-id: http://svn.contrexxlabs.com/contrexx/branches/contrexx_2_1@5283 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- update/updates/2.1.1/components/core_module/contact.php
+++ update/updates/2.1.1/components/core_module/contact.php
@@ -15,20 +15,19 @@ function _contactUpdate()
         );
 
 
-        $type_t = "ENUM('text','label','checkbox','checkboxGroup','date','file','hidden','password','radio','select','textarea','recipient')";
         UpdateUtil::table(
-            DBPREFIX . 'module_contact_form_field',
+            DBPREFIX.'module_contact_form_field',
             array(
-                'id'          => array('type' => 'INT',          'notnull' => true, 'primary' => true,      'auto_increment' => true),
-                'id_form'     => array('type' => 'INT(11)',      'notnull' => true, 'default' => 0),
-                'name'        => array('type' => 'VARCHAR(250)', 'notnull' => true, 'default' => ''),
-                'type'        => array('type' => $type_t,        'notnull' => true, 'default' => 'text'),
-                'is_required' => array('type' => 'TEXT',         'notnull' => true),
-                'check_type'  => array('type' => 'INT(3)',       'notnull' => true, 'default' => 0),
-                'order_id'    => array('type' => 'INT(5)',       'notnull' => true, 'default' => 0)
+                'id'             => array('type' => 'INT(10)', 'unsigned' => true, 'notnull' => true, 'auto_increment' => true, 'primary' => true),
+                'id_form'        => array('type' => 'INT(10)', 'unsigned' => true, 'notnull' => true, 'default' => '0'),
+                'name'           => array('type' => 'VARCHAR(255)', 'notnull' => true, 'default' => ''),
+                'type'           => array('type' => 'ENUM(\'text\',\'label\',\'checkbox\',\'checkboxgroup\',\'date\',\'file\',\'hidden\',\'password\',\'radio\',\'select\',\'textarea\')', 'notnull' => true, 'default' => 'text'),
+                'attributes'     => array('type' => 'TEXT'),
+                'is_required'    => array('type' => 'SET(\'0\',\'1\')', 'notnull' => true, 'default' => '0'),
+                'check_type'     => array('type' => 'INT(3)', 'notnull' => true, 'default' => '1'),
+                'order_id'       => array('type' => 'SMALLINT(5)', 'unsigned' => true, 'notnull' => true, 'default' => '0')
             )
         );
-
     }
     catch (UpdateException $e) {
         // we COULD do something else here..
