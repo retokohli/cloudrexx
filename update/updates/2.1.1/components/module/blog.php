fdaf1cbd490eb03ec0f2fb5aad5fd3eadb4baf08
Thomas Däppen <thomas.daeppen@cloudrexx.com>
2009-05-28 17:24:35 +0000

    update: script bugfixes (de.lang.php / blog.php / version.php / stats.php / access.php)
    git-svn-id: http://svn.contrexxlabs.com/contrexx/branches/contrexx_2_1@5300 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- update/updates/2.1.1/components/module/blog.php
+++ update/updates/2.1.1/components/module/blog.php
@@ -3,20 +3,20 @@ function _blogUpdate() {
 	global $objDatabase, $_ARRAYLANG;
 
 	/*
-	* Check for missing setting "blog_comments_editor" in database. In the update-package for 1.2 this value somehow 
+	* Check for missing setting "blog_comments_editor" in database. In the update-package for 1.2 this value somehow
 	* got lost.
 	*/
 	$query = '	SELECT 	name
 				FROM	`'.DBPREFIX.'module_blog_settings`
 				WHERE	name="blog_comments_editor"
 				LIMIT	1';
-	
+
 	$objResult = $objDatabase->Execute($query);
-	
+
 	if ($objResult !== false) {
 		if ($objResult->RecordCount() == 0) {
 			$query = "INSERT INTO `".DBPREFIX."module_blog_settings` ( `name` , `value` ) VALUES ('blog_comments_editor', 'wysiwyg')";
-			
+
 			if ($objDatabase->Execute($query) === false) {
 				return _databaseError($query, $objDatabase->ErrorMsg());
 			}
@@ -24,7 +24,31 @@ function _blogUpdate() {
 	} else {
 		return _databaseError($query, $objDatabase->ErrorMsg());
 	}
-	
+
+	/************************************************
+	* BUGFIX:	Set write access to the upload dir  *
+	************************************************/
+	require_once ASCMS_FRAMEWORK_PATH.'/File.class.php';
+	$objFile = new File();
+	if (is_writeable(ASCMS_BLOG_IMAGES_PATH) || $objFile->setChmod(ASCMS_BLOG_IMAGES_PATH, ASCMS_BLOG_IMAGES_WEB_PATH, '')) {
+    	if ($mediaDir = @opendir(ASCMS_BLOG_IMAGES_PATH)) {
+    		while($file = readdir($mediaDir)) {
+    			if ($file != '.' && $file != '..') {
+    				if (!is_writeable(ASCMS_BLOG_IMAGES_PATH.'/'.$file) && !$objFile->setChmod(ASCMS_BLOG_IMAGES_PATH.'/', ASCMS_BLOG_IMAGES_WEB_PATH.'/', $file)) {
+    					setUpdateMsg(sprintf($_ARRAYLANG['TXT_SET_WRITE_PERMISSON_TO_FILE'], ASCMS_BLOG_IMAGES_PATH.'/'.$file, $_CORELANG['TXT_UPDATE_TRY_AGAIN']), 'msg');
+    					return false;
+    				}
+    			}
+			}
+    	} else {
+    		setUpdateMsg(sprintf($_ARRAYLANG['TXT_SET_WRITE_PERMISSON_TO_DIR_AND_CONTENT'], ASCMS_BLOG_IMAGES_PATH.'/', $_CORELANG['TXT_UPDATE_TRY_AGAIN']), 'msg');
+    		return false;
+		}
+    } else {
+    	setUpdateMsg(sprintf($_ARRAYLANG['TXT_SET_WRITE_PERMISSON_TO_DIR_AND_CONTENT'], ASCMS_BLOG_IMAGES_PATH.'/', $_CORELANG['TXT_UPDATE_TRY_AGAIN']), 'msg');
+    	return false;
+    }
+
 	/**
 	 * Everything went fine. Return without any errors.
 	 */
