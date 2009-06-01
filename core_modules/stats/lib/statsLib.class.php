5c0f5ba09a2307af24998094d21626b5d950ee06
Reto Kohli <reto.kohli@comvation.com>
2009-06-01 12:15:33 +0000

    lib/statsLib.class.php - Fixed javascript code containing HTML tags
    git-svn-id: http://svn.contrexxlabs.com/contrexx/branches/contrexx_2_1@5307 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- core_modules/stats/lib/statsLib.class.php
+++ core_modules/stats/lib/statsLib.class.php
@@ -119,7 +119,7 @@ class statsLibrary
                     else {
                         c = screen.pixelDepth;
                     }
-                    document.write('<script type=\"text/javascript\" src=\"$ascms_core_module_web_path/stats/counter.php?mode=script&referer='+referer+'&pageId=$pageId&screen=' + screen.width + 'x' + screen.height + '&color_depth=' + c + '$searchTerm\" ></s'+'cript>');
+                    document.write('<script type=\"text/javascript\" src=\"$ascms_core_module_web_path/stats/counter.php?mode=script&referer='+referer+'&pageId=$pageId&screen=' + screen.width + 'x' + screen.height + '&color_depth=' + c + '$searchTerm\" ><\/script>');
                     // -->
                     </script>
                     <noscript><div><img src=\"$ascms_core_module_web_path/stats/counter.php?mode=noscript&amp;referer=$referer&amp;pageId=$pageId.$searchTerm\" alt=\" \" width=\"1\" height=\"1\" /></div></noscript>
