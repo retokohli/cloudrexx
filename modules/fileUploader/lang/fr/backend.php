6a058bcc40b30768c01a7867e8078167ef6add1a
lang <lang@comvation.com>
2009-06-12 11:06:54 +0000

    automated language files update (LanguageManager) triggered by dahasvn
    git-svn-id: http://svn.contrexxlabs.com/contrexx/trunk@5338 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

---
+++ modules/fileUploader/lang/fr/backend.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @copyright   CONTREXX CMS - COMVATION AG
+ * @author      Comvation Development Team <info@comvation.com>
+ * @access      public
+ * @package     contrexx
+ * @subpackage  module_fileUploader
+ */
+$_ARRAYLANG['TXT_FILEUPLOADER_CLOSE'] = "Close";
+?>
