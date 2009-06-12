de6883486a00846340d108496e11c7a2574baeb8
lang <lang@comvation.com>
2009-06-12 15:19:10 +0000

    automated language files update (LanguageManager) triggered by dahasvn
    git-svn-id: http://svn.contrexxlabs.com/contrexx/trunk@5342 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- modules/fileUploader/lang/fr/frontend.php
+++ modules/fileUploader/lang/fr/frontend.php
@@ -7,4 +7,5 @@
  * @subpackage  module_fileUploader
  */
 $_ARRAYLANG['TXT_FILEUPLOADER_CLOSE'] = "Close";
+$_ARRAYLANG['TXT_FILEUPLOADER_FILE_EXTENSION_NOT_ALLOWED'] = "Erreur: le fichier %s ne peut pas être accepté, car son extension n'est pas autorisée";
 ?>
