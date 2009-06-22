def50df80f1c1082b2272ca677a8ca08c9b5a03c
lang <lang@comvation.com>
2009-06-22 04:08:19 +0000

    automated language files update (LanguageManager) triggered by cron(root)
    git-svn-id: http://svn.contrexxlabs.com/contrexx/trunk@5370 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- lang/de/backend.php
+++ lang/de/backend.php
@@ -1026,4 +1026,10 @@ $_ARRAYLANG['TXT_MANAGE_CONFIGURATION'] = "Einstellungen verwalten";
 $_ARRAYLANG['TXT_ADD_FILES'] = "Dateien hinzufügen";
 $_ARRAYLANG['TXT_ACCESS_COMMUNITY_EVENTS'] = "Community Events einsehen";
 $_ARRAYLANG['TXT_MEDIADIR_MODULE'] = "Medien Verzeichnis";
+$_ARRAYLANG['TXT_LANGUAGES'] = "Sprachen";
+$_ARRAYLANG['TXT_CONTENT_NO_TITLE'] = "Unbenannt";
+$_ARRAYLANG['TXT_ERROR_COULD_NOT_INSERT_PAGE'] = "Seite konnte nicht in die Datenbank eingefügt werden";
+$_ARRAYLANG['TXT_SUCCESS_PAGE_SAVED'] = "Seite gespeichert:";
+$_ARRAYLANG['TXT_ERROR_COULD_NOT_GET_DATA'] = "Daten konnten nicht vom Server geholt werden";
+$_ARRAYLANG['TXT_CONTENT_PLEASE_WAIT'] = "Bitte warten...";
 ?>
--- lang/fr/backend.php
+++ lang/fr/backend.php
@@ -1027,4 +1027,10 @@ $_ARRAYLANG['TXT_MANAGE_CONFIGURATION'] = "Gérer les paramètres";
 $_ARRAYLANG['TXT_ADD_FILES'] = "Nouveaux fichiers";
 $_ARRAYLANG['TXT_ACCESS_COMMUNITY_EVENTS'] = "Accéder aux manifestations communautaires";
 $_ARRAYLANG['TXT_MEDIADIR_MODULE'] = "Meda Directory";
+$_ARRAYLANG['TXT_LANGUAGES'] = "Sprachen";
+$_ARRAYLANG['TXT_CONTENT_NO_TITLE'] = "Unbenannt";
+$_ARRAYLANG['TXT_ERROR_COULD_NOT_INSERT_PAGE'] = "Seite konnte nicht in die Datenbank eingefügt werden";
+$_ARRAYLANG['TXT_SUCCESS_PAGE_SAVED'] = "Seite gespeichert:";
+$_ARRAYLANG['TXT_ERROR_COULD_NOT_GET_DATA'] = "Daten konnten nicht vom Server geholt werden";
+$_ARRAYLANG['TXT_CONTENT_PLEASE_WAIT'] = "Bitte warten...";
 ?>
