8d258bc39abd40fe4ce66655d48a5346b71de0ec
David Vogt <david.vogt@comvation.com>
2009-07-03 09:24:32 +0000

    fix upload whitelist.. make a blacklist instead!
    git-svn-id: http://svn.contrexxlabs.com/contrexx/branches/contrexx_2_1@5422 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- modules/downloads/lib/downloadsLib.class.php
+++ modules/downloads/lib/downloadsLib.class.php
@@ -21,6 +21,7 @@ require_once dirname(__FILE__).'/Download.class.php';
  * @ignore
  */
 require_once ASCMS_FRAMEWORK_PATH.'/System.class.php';
+require_once ASCMS_LIBRARY_PATH.'/FRAMEWORK/Validator.class.php';
 
 /**
  * Digital Asset Management Library
@@ -32,26 +33,6 @@ require_once ASCMS_FRAMEWORK_PATH.'/System.class.php';
  */
 class DownloadsLibrary
 {
-    /**
-     * File extensions that are allowed to upload
-     *
-     * This array contains all file extensions that are allowed
-     * to be uploaded. If a file's file extensions is not listed
-     * in this array then the contact request will be blocked and
-     * a error message will be return instead.
-     */
-    public  $enabledUploadFileExtensions = array(
-        "txt","doc","xls","pdf","ppt","gif","jpg","png","xml",
-        "odt","ott","sxw","stw","dot","rtf","sdw","wpd","jtd",
-        "jtt","hwp","wps","ods","ots","sxc","stc","dif","dbf",
-        "xlw","xlt","sdc","vor","sdc","cvs","slk","wk1","wks",
-        "123","odp","otp","sxi","sti","pps","pot","sxd","sda",
-        "sdd","sdp","cgm","odg","otg","sxd","std","dxf","emf",
-        "eps","met","pct","sgf","sgv","svm","wmf","bmp","jpeg",
-        "jfif","jif","jpe","pbm","pcx","pgm","ppm","psd","ras",
-        "tga","tif","tiff","xbm","xpm","pcd","oth","odm","sxg",
-        "sgl","odb","odf","sxm","smf","mml","zip","rar"
-    );
     protected $defaultCategoryImage = array();
     protected $defaultDownloadImage = array();
     protected $arrPermissionTypes = array(
