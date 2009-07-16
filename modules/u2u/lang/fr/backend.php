4d70d908708c3176abc4a57f216aa2b49496ca12
Thomas Däppen <thomas.daeppen@cloudrexx.com>
2009-07-16 08:34:41 +0000

    u2u / access: - synchronized u2u language files with trunk - link 'add to addressbook' on the members list page is hidden for users that are already part of the addressbook
    git-svn-id: http://svn.contrexxlabs.com/contrexx/branches/contrexx_2_1@5483 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- modules/u2u/lang/fr/backend.php
+++ modules/u2u/lang/fr/backend.php
@@ -6,33 +6,33 @@
  * @package     contrexx
  * @subpackage  module_u2u
  */
-$_ARRAYLANG['TXT_OVERVIEW'] = "Overview";
-$_ARRAYLANG['TXT_WELCOME_MSG'] = "Hello world";
-$_ARRAYLANG['TXT_U2U_SETTINGS'] = "Settings";
-$_ARRAYLANG['TXT_U2U_MAX_POSTING_SIZE_INBOX'] = "Maximum Posting size";
-$_ARRAYLANG['TXT_U2U_MAX_CHARS_PER_MSG'] = "Maximum Number of Characters Per Message";
-$_ARRAYLANG['TXT_U2U_VALUE'] = "Value";
-$_ARRAYLANG['TXT_U2U_OVERVIEW_TITLE'] = "Overview";
+$_ARRAYLANG['TXT_OVERVIEW'] = "Aperçu";
+$_ARRAYLANG['TXT_WELCOME_MSG'] = "Bonjour Monde";
+$_ARRAYLANG['TXT_U2U_SETTINGS'] = "Paramètres";
+$_ARRAYLANG['TXT_U2U_MAX_POSTING_SIZE_INBOX'] = "Taille maximum de la boîte";
+$_ARRAYLANG['TXT_U2U_MAX_CHARS_PER_MSG'] = "Nombre maximal de caractères par message";
+$_ARRAYLANG['TXT_U2U_VALUE'] = "Valeur";
+$_ARRAYLANG['TXT_U2U_OVERVIEW_TITLE'] = "Aperçu";
 $_ARRAYLANG['TXT_U2U_ACCESS_ID'] = "ID";
-$_ARRAYLANG['TXT_U2U_ACCESS_STATUS'] = "Status";
-$_ARRAYLANG['TXT_U2U_ACCESS_USERNAME'] = "Username";
+$_ARRAYLANG['TXT_U2U_ACCESS_STATUS'] = "Statut";
+$_ARRAYLANG['TXT_U2U_ACCESS_USERNAME'] = "Nom utilisateur";
 $_ARRAYLANG['TXT_U2U_ACCESS_EMAIL'] = "E-mail";
-$_ARRAYLANG['TXT_U2U_ACCESS_REGISTERED_SINCE'] = "Registered Since";
-$_ARRAYLANG['TXT_U2U_ACCESS_LAST_ACTIVITY'] = "Last Activity";
-$_ARRAYLANG['TXT_U2U_ACCESS_VALIDITY_EXPIRATION'] = "Validity Expiration";
-$_ARRAYLANG['TXT_U2U_ACCESS_CONFIRM_USER_ACCOUNT_STATUS_CHANGED'] = "Are you sure want to change the status of the user";
-$_ARRAYLANG['TXT_U2U_STATUS_UPDATED'] = "Status Successfully Updated";
-$_ARRAYLANG['TXT_U2U_ENTER_THE_SUBJECT_OF_THE_MAIL'] = "Enter the subject of the mail";
-$_ARRAYLANG['TXT_U2U_ENTER_THE_FROM_ADDRESS'] = "Enter the 'From' address for the mail";
-$_ARRAYLANG['TXT_U2U_NOTIFICATIION_EMAILS'] = "Edit the content of Notification Emails";
-$_ARRAYLANG['TXT_U2U_SUBJECT'] = "Subject";
-$_ARRAYLANG['TXT_U2U_FROM_LINE'] = "From Line";
+$_ARRAYLANG['TXT_U2U_ACCESS_REGISTERED_SINCE'] = "Enregistré depuis";
+$_ARRAYLANG['TXT_U2U_ACCESS_LAST_ACTIVITY'] = "Dernière activité";
+$_ARRAYLANG['TXT_U2U_ACCESS_VALIDITY_EXPIRATION'] = "Expiration";
+$_ARRAYLANG['TXT_U2U_ACCESS_CONFIRM_USER_ACCOUNT_STATUS_CHANGED'] = "Voulez-vous vraiment changer le statut de cet utilisateur?";
+$_ARRAYLANG['TXT_U2U_STATUS_UPDATED'] = "Statut mis à jour";
+$_ARRAYLANG['TXT_U2U_ENTER_THE_SUBJECT_OF_THE_MAIL'] = "Veuillez saisir un sujet.";
+$_ARRAYLANG['TXT_U2U_ENTER_THE_FROM_ADDRESS'] = "Veuillez saisir un expéditeur valide";
+$_ARRAYLANG['TXT_U2U_NOTIFICATIION_EMAILS'] = "Editer le contenu des E-mail de notification";
+$_ARRAYLANG['TXT_U2U_SUBJECT'] = "Sujet";
+$_ARRAYLANG['TXT_U2U_FROM_LINE'] = "De ligne";
 $_ARRAYLANG['TXT_U2U_MESSAGE'] = "Message";
 $_ARRAYLANG['TXT_U2U_NOTE'] = "Note";
-$_ARRAYLANG['TXT_U2U_SENDER_NAME_CONTENT'] = "Use [senderName] for display the sender name in the E-mail";
-$_ARRAYLANG['TXT_U2U_RECEIVER_NAME_CONTENT'] = "Use [receiverName] for print the receiver name in the E-mail";
-$_ARRAYLANG['TXT_U2U_SUCCESS'] = "Success";
-$_ARRAYLANG['TXT_U2U_NOT_SUCCESS'] = "No Success";
+$_ARRAYLANG['TXT_U2U_SENDER_NAME_CONTENT'] = "Utiliser la variable [senderName] pour afficher le nom de l'expéditeur dans l'E-mail";
+$_ARRAYLANG['TXT_U2U_RECEIVER_NAME_CONTENT'] = "Utiliser la variable [receiverName] pour afficher le nom du destinataire dans l'E-mail";
+$_ARRAYLANG['TXT_U2U_SUCCESS'] = "Effectué";
+$_ARRAYLANG['TXT_U2U_NOT_SUCCESS'] = "Echec";
 $_ARRAYLANG['TXT_U2U_MODULE'] = "U2U";
-$_ARRAYLANG['TXT_U2U_DOMAIN_NAME_CONTENT'] = "Use [domainName] to display the domain of your website in the E-Mail";
+$_ARRAYLANG['TXT_U2U_DOMAIN_NAME_CONTENT'] = "Utiliser la variable [domainName] pour afficher le'URL de votre site dans l'E-mail";
 ?>
