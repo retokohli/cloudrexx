871382fa373b07a7200ac1c03812745deae6554b
lang <lang@comvation.com>
2009-08-13 17:17:39 +0000

    automated language files update (LanguageManager) triggered by kon
    git-svn-id: http://svn.contrexxlabs.com/contrexx/trunk@5995 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

---
+++ modules/printshop/lang/CZ/frontend.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * @copyright   CONTREXX CMS - COMVATION AG
+ * @author      Comvation Development Team <info@comvation.com>
+ * @access      public
+ * @package     contrexx
+ * @subpackage  module_printshop
+ */
+$_ARRAYLANG['TXT_PRINTSHOP_SUMMARY'] = "Zusammenfassung";
+$_ARRAYLANG['TXT_PRINTSHOP_PRICE_PER_PIECE'] = "Preis pro Stück";
+$_ARRAYLANG['TXT_PRINTSHOP_AMOUNT'] = "Exemplare";
+$_ARRAYLANG['TXT_PRINTSHOP_TYPE'] = "Drucksache";
+$_ARRAYLANG['TXT_PRINTSHOP_EXCL_TAX'] = "Exkl. MwSt.";
+$_ARRAYLANG['TXT_PRINTSHOP_DATA_PREPARATION'] = "Datenaufbereitung";
+$_ARRAYLANG['TXT_PRINTSHOPT_COMMIT_ORDER'] = "Auftrag aufgeben";
+$_ARRAYLANG['TXT_PRINTSHOP_TYPE_TITLE'] = "Typ";
+$_ARRAYLANG['TXT_PRINTSHOP_FORMAT_TITLE'] = "Format";
+$_ARRAYLANG['TXT_PRINTSHOP_FRONT_TITLE'] = "Vorderseite";
+$_ARRAYLANG['TXT_PRINTSHOP_BACK_TITLE'] = "Rückseite";
+$_ARRAYLANG['TXT_PRINTSHOP_WEIGHT_TITLE'] = "Grammatur";
+$_ARRAYLANG['TXT_PRINTSHOP_PAPER_TITLE'] = "Papier";
+$_ARRAYLANG['TXT_PRINTSHOP_PRICE_TITLE'] = "Preis";
+$_ARRAYLANG['TXT_PRINTSHOP_FACTOR_TITLE'] = "Faktor";
+?>
