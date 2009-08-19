6a83cafbd8e1a56d64a2c8ed4072c21d3c7627d0
Reto Kohli <reto.kohli@comvation.com>
2009-08-19 12:07:29 +0000

    core_modules/access/lib/blocks.class.php - Removed PHP 4 constructor - Fixed several warnings
    git-svn-id: http://svn.contrexxlabs.com/contrexx/branches/contrexx_2_1@6475 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- core_modules/access/lib/blocks.class.php
+++ core_modules/access/lib/blocks.class.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * User Management
  * @copyright   CONTREXX CMS - COMVATION AG
@@ -23,232 +24,236 @@ require_once ASCMS_CORE_MODULE_PATH.'/access/lib/AccessLib.class.php';
  */
 class Access_Blocks extends AccessLib
 {
-	public function Access()
-	{
-		$this->__construct();
-	}
+    public function __construct()
+    {
+        global $objTemplate;
 
-	public function __construct()
-	{
-		global $objTemplate;
+        parent::__construct();
 
-		parent::__construct();
+        $this->_objTpl = &$objTemplate;
+    }
 
-		$this->_objTpl = &$objTemplate;
-	}
 
-	function setCurrentlyOnlineUsers($gender = null)
-	{
-		$objFWUser = FWUser::getFWUserObject();
-		$arrSettings = User_Setting::getSettings();
+    function setCurrentlyOnlineUsers($gender=null)
+    {
+        $objFWUser = FWUser::getFWUserObject();
+        $arrSettings = User_Setting::getSettings();
 
-		$filter = array(
-			'active'	=> true,
-			'last_activity' => array(
-				'>' => (time()-3600)
-			)
-		);
-		if ($arrSettings['block_currently_online_users_pic']['status']) {
-			$filter['picture'] = array('!=' => '');
-		}
+        $filter = array(
+            'active'    => true,
+            'last_activity' => array(
+                '>' => (time()-3600)
+            )
+        );
+        if ($arrSettings['block_currently_online_users_pic']['status']) {
+            $filter['picture'] = array('!=' => '');
+        }
 
-		if (!empty($gender)) {
-			$filter['gender'] = 'gender_'.$gender;
-		}
+        if (!empty($gender)) {
+            $filter['gender'] = 'gender_'.$gender;
+        }
 
-		if ($objUser = $objFWUser->objUser->getUsers(
-			$filter,
-			null,
-			array(
-				'last_activity'	=> 'desc',
-				'username'		=> 'asc'
-			),
-			null,
-			$limit = $arrSettings['block_currently_online_users']['value']
-		)) {
-			while (!$objUser->EOF) {
-				$this->_objTpl->setVariable(array(
-					'ACCESS_USER_ID'	=> $objUser->getId(),
-					'ACCESS_USER_USERNAME'	=> htmlentities($objUser->getUsername(), ENT_QUOTES, CONTREXX_CHARSET)
-				));
+        $objUser = $objFWUser->objUser->getUsers(
+            $filter,
+            null,
+            array(
+                'last_activity'    => 'desc',
+                'username'        => 'asc'
+            ),
+            null,
+            $arrSettings['block_currently_online_users']['value']
+        );
+        if ($objUser) {
+            while (!$objUser->EOF) {
+                $this->_objTpl->setVariable(array(
+                    'ACCESS_USER_ID'    => $objUser->getId(),
+                    'ACCESS_USER_USERNAME'    => htmlentities($objUser->getUsername(), ENT_QUOTES, CONTREXX_CHARSET)
+                ));
 
-				$objUser->objAttribute->first();
-				while (!$objUser->objAttribute->EOF) {
-					$objAttribute = $objUser->objAttribute->getById($objUser->objAttribute->getId());
-					$this->parseAttribute($objUser, $objAttribute->getId(), 0, false, false, false, false, false);
-					$objUser->objAttribute->next();
-				}
+                $objUser->objAttribute->first();
+                while (!$objUser->objAttribute->EOF) {
+                    $objAttribute = $objUser->objAttribute->getById($objUser->objAttribute->getId());
+                    $this->parseAttribute($objUser, $objAttribute->getId(), 0, false, false, false, false, false);
+                    $objUser->objAttribute->next();
+                }
 
-				$this->_objTpl->parse('access_currently_online_'.(!empty($gender) ? $gender.'_' : '').'members');
+                $this->_objTpl->parse('access_currently_online_'.(!empty($gender) ? $gender.'_' : '').'members');
 
-				$objUser->next();
-			}
-		} else {
-			$this->_objTpl->hideBlock('access_currently_online_'.(!empty($gender) ? $gender.'_' : '').'members');
-		}
-	}
+                $objUser->next();
+            }
+        } else {
+            $this->_objTpl->hideBlock('access_currently_online_'.(!empty($gender) ? $gender.'_' : '').'members');
+        }
+    }
 
-	function setLastActiveUsers($gender = null)
-	{
-		$arrSettings = User_Setting::getSettings();
 
-		$filter['active'] = true;
-		if ($arrSettings['block_last_active_users_pic']['status']) {
-			$filter['picture'] = array('!=' => '');
-		}
+    function setLastActiveUsers($gender = null)
+    {
+        $arrSettings = User_Setting::getSettings();
 
-		if (!empty($gender)) {
-			$filter['gender'] = 'gender_'.$gender;
-		}
+        $filter['active'] = true;
+        if ($arrSettings['block_last_active_users_pic']['status']) {
+            $filter['picture'] = array('!=' => '');
+        }
 
-		$objFWUser = FWUser::getFWUserObject();
-		if ($objUser = $objFWUser->objUser->getUsers(
-			$filter,
-			null,
-			array(
-				'last_activity'	=> 'desc',
-				'username'		=> 'asc'
-			),
-			null,
-			$limit = $arrSettings['block_last_active_users']['value']
-		)) {
-			while (!$objUser->EOF) {
-				$this->_objTpl->setVariable(array(
-					'ACCESS_USER_ID'	=> $objUser->getId(),
-					'ACCESS_USER_USERNAME'	=> htmlentities($objUser->getUsername(), ENT_QUOTES, CONTREXX_CHARSET)
-				));
+        if (!empty($gender)) {
+            $filter['gender'] = 'gender_'.$gender;
+        }
 
-				$objUser->objAttribute->first();
-				while (!$objUser->objAttribute->EOF) {
-					$objAttribute = $objUser->objAttribute->getById($objUser->objAttribute->getId());
-					$this->parseAttribute($objUser, $objAttribute->getId(), 0, false, false, false, false, false);
-					$objUser->objAttribute->next();
-				}
+        $objFWUser = FWUser::getFWUserObject();
+        $objUser = $objFWUser->objUser->getUsers(
+            $filter,
+            null,
+            array(
+                'last_activity'    => 'desc',
+                'username'        => 'asc'
+            ),
+            null,
+            $arrSettings['block_last_active_users']['value']
+        );
+        if ($objUser) {
+            while (!$objUser->EOF) {
+                $this->_objTpl->setVariable(array(
+                    'ACCESS_USER_ID'    => $objUser->getId(),
+                    'ACCESS_USER_USERNAME'    => htmlentities($objUser->getUsername(), ENT_QUOTES, CONTREXX_CHARSET)
+                ));
 
-				$this->_objTpl->parse('access_last_active_'.(!empty($gender) ? $gender.'_' : '').'members');
+                $objUser->objAttribute->first();
+                while (!$objUser->objAttribute->EOF) {
+                    $objAttribute = $objUser->objAttribute->getById($objUser->objAttribute->getId());
+                    $this->parseAttribute($objUser, $objAttribute->getId(), 0, false, false, false, false, false);
+                    $objUser->objAttribute->next();
+                }
 
-				$objUser->next();
-			}
-		} else {
-			$this->_objTpl->hideBlock('access_last_active_'.(!empty($gender) ? $gender.'_' : '').'members');
-		}
-	}
+                $this->_objTpl->parse('access_last_active_'.(!empty($gender) ? $gender.'_' : '').'members');
 
-	function setLatestRegisteredUsers($gender = null)
-	{
-		$arrSettings = User_Setting::getSettings();
+                $objUser->next();
+            }
+        } else {
+            $this->_objTpl->hideBlock('access_last_active_'.(!empty($gender) ? $gender.'_' : '').'members');
+        }
+    }
 
-		$filter['active'] = true;
-		if ($arrSettings['block_latest_reg_users_pic']['status']) {
-			$filter['picture'] = array('!=' => '');
-		}
 
-		if (!empty($gender)) {
-			$filter['gender'] = 'gender_'.$gender;
-		}
+    function setLatestRegisteredUsers($gender = null)
+    {
+        $arrSettings = User_Setting::getSettings();
 
-		$objFWUser = FWUser::getFWUserObject();
-		if ($objUser = $objFWUser->objUser->getUsers(
-			$filter,
-			null,
-			array(
-				'regdate'	=> 'desc',
-				'username'	=> 'asc'
-			),
-			null,
-			$limit = $arrSettings['block_latest_reg_users']['value']
-		)) {
-			while (!$objUser->EOF) {
-				$this->_objTpl->setVariable(array(
-					'ACCESS_USER_ID'	=> $objUser->getId(),
-					'ACCESS_USER_USERNAME'	=> htmlentities($objUser->getUsername(), ENT_QUOTES, CONTREXX_CHARSET)
-				));
+        $filter['active'] = true;
+        if ($arrSettings['block_latest_reg_users_pic']['status']) {
+            $filter['picture'] = array('!=' => '');
+        }
 
-				$objUser->objAttribute->first();
-				while (!$objUser->objAttribute->EOF) {
-					$objAttribute = $objUser->objAttribute->getById($objUser->objAttribute->getId());
-					$this->parseAttribute($objUser, $objAttribute->getId(), 0, false, false, false, false, false);
-					$objUser->objAttribute->next();
-				}
+        if (!empty($gender)) {
+            $filter['gender'] = 'gender_'.$gender;
+        }
 
-				$this->_objTpl->parse('access_latest_registered_'.(!empty($gender) ? $gender.'_' : '').'members');
+        $objFWUser = FWUser::getFWUserObject();
+        $objUser = $objFWUser->objUser->getUsers(
+            $filter,
+            null,
+            array(
+                'regdate'    => 'desc',
+                'username'    => 'asc'
+            ),
+            null,
+            $arrSettings['block_latest_reg_users']['value']
+        );
+        if ($objUser) {
+            while (!$objUser->EOF) {
+                $this->_objTpl->setVariable(array(
+                    'ACCESS_USER_ID'    => $objUser->getId(),
+                    'ACCESS_USER_USERNAME'    => htmlentities($objUser->getUsername(), ENT_QUOTES, CONTREXX_CHARSET)
+                ));
 
-				$objUser->next();
-			}
-		} else {
-			$this->_objTpl->hideBlock('access_latest_registered_'.(!empty($gender) ? $gender.'_' : '').'members');
-		}
-	}
+                $objUser->objAttribute->first();
+                while (!$objUser->objAttribute->EOF) {
+                    $objAttribute = $objUser->objAttribute->getById($objUser->objAttribute->getId());
+                    $this->parseAttribute($objUser, $objAttribute->getId(), 0, false, false, false, false, false);
+                    $objUser->objAttribute->next();
+                }
 
-	function setBirthdayUsers($gender = null)
-	{
-		$arrSettings = User_Setting::getSettings();
+                $this->_objTpl->parse('access_latest_registered_'.(!empty($gender) ? $gender.'_' : '').'members');
 
-		$filter = array(
-			'active'	=> true,
+                $objUser->next();
+            }
+        } else {
+            $this->_objTpl->hideBlock('access_latest_registered_'.(!empty($gender) ? $gender.'_' : '').'members');
+        }
+    }
+
+
+    function setBirthdayUsers($gender = null)
+    {
+        $arrSettings = User_Setting::getSettings();
+
+        $filter = array(
+            'active'    => true,
             'birthday_day'      => date('j'),
             'birthday_month'    => date('n')
-		);
-		if ($arrSettings['block_birthday_users_pic']['status']) {
-			$filter['picture'] = array('!=' => '');
-		}
+        );
+        if ($arrSettings['block_birthday_users_pic']['status']) {
+            $filter['picture'] = array('!=' => '');
+        }
 
-		if (!empty($gender)) {
-			$filter['gender'] = 'gender_'.$gender;
-		}
+        if (!empty($gender)) {
+            $filter['gender'] = 'gender_'.$gender;
+        }
 
-		$objFWUser = FWUser::getFWUserObject();
-		if ($objUser = $objFWUser->objUser->getUsers(
-			$filter,
-			null,
-			array(
-				'regdate'	=> 'desc',
-				'username'	=> 'asc'
-			),
-			null,
-			$limit = $arrSettings['block_birthday_users']['value']
-		)) {
-			while (!$objUser->EOF) {
-				$this->_objTpl->setVariable(array(
-					'ACCESS_USER_ID'	=> $objUser->getId(),
-					'ACCESS_USER_USERNAME'	=> htmlentities($objUser->getUsername(), ENT_QUOTES, CONTREXX_CHARSET)
-				));
+        $objFWUser = FWUser::getFWUserObject();
+        $objUser = $objFWUser->objUser->getUsers(
+            $filter,
+            null,
+            array(
+                'regdate'    => 'desc',
+                'username'    => 'asc'
+            ),
+            null,
+            $arrSettings['block_birthday_users']['value']
+        );
+        if ($objUser) {
+            while (!$objUser->EOF) {
+                $this->_objTpl->setVariable(array(
+                    'ACCESS_USER_ID'    => $objUser->getId(),
+                    'ACCESS_USER_USERNAME'    => htmlentities($objUser->getUsername(), ENT_QUOTES, CONTREXX_CHARSET)
+                ));
 
-				$objUser->objAttribute->first();
-				while (!$objUser->objAttribute->EOF) {
-					$objAttribute = $objUser->objAttribute->getById($objUser->objAttribute->getId());
-					$this->parseAttribute($objUser, $objAttribute->getId(), 0, false, false, false, false, false);
-					$objUser->objAttribute->next();
-				}
+                $objUser->objAttribute->first();
+                while (!$objUser->objAttribute->EOF) {
+                    $objAttribute = $objUser->objAttribute->getById($objUser->objAttribute->getId());
+                    $this->parseAttribute($objUser, $objAttribute->getId(), 0, false, false, false, false, false);
+                    $objUser->objAttribute->next();
+                }
 
-				$this->_objTpl->parse('access_birthday_'.(!empty($gender) ? $gender.'_' : '').'members');
+                $this->_objTpl->parse('access_birthday_'.(!empty($gender) ? $gender.'_' : '').'members');
 
-				$objUser->next();
-			}
-		} else {
-			$this->_objTpl->hideBlock('access_birthday_'.(!empty($gender) ? $gender.'_' : '').'members');
-		}
-	}
+                $objUser->next();
+            }
+        } else {
+            $this->_objTpl->hideBlock('access_birthday_'.(!empty($gender) ? $gender.'_' : '').'members');
+        }
+    }
 
-	function isSomeonesBirthdayToday()
-	{
-		$arrSettings = User_Setting::getSettings();
+
+    function isSomeonesBirthdayToday()
+    {
+        $arrSettings = User_Setting::getSettings();
 
         $filter = array(
             'active'            => true,
             'birthday_day'      => date('j'),
             'birthday_month'    => date('n')
-		);
-		if ($arrSettings['block_birthday_users_pic']['status']) {
-			$filter['picture'] = array('!=' => '');
-		}
+        );
+        if ($arrSettings['block_birthday_users_pic']['status']) {
+            $filter['picture'] = array('!=' => '');
+        }
 
-		$objFWUser = FWUser::getFWUserObject();
-		if ($objFWUser->objUser->getUsers($filter, null, null, null, $limit = 1)) {
-			return true;
-		} else {
-			return false;
-		}
-	}
+        $objFWUser = FWUser::getFWUserObject();
+        if ($objFWUser->objUser->getUsers($filter, null, null, null, 1))
+            return true;
+        return false;
+    }
 
 }
+
+?>
