c480968b19d6569d4e100d02406cc81305fe6954
lang <lang@comvation.com>
2009-08-24 12:11:47 +0000

    automated language files update (LanguageManager) triggered by dahasvn
    git-svn-id: http://svn.contrexxlabs.com/contrexx/trunk@6500 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

---
+++ modules/partners/lang/ja/frontend.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * @copyright   CONTREXX CMS - COMVATION AG
+ * @author      Comvation Development Team <info@comvation.com>
+ * @access      public
+ * @package     contrexx
+ * @subpackage  module_partners
+ */
+$_ARRAYLANG['TXT_PARTNERS_ALL'] = "All";
+$_ARRAYLANG['TXT_PARTNERS_PHONES'] = "Phone:";
+$_ARRAYLANG['TXT_HELLO_WORLD'] = "Hello World";
+$_ARRAYLANG['TXT_NEWSLETTER_ALL'] = "ALL";
+$_ARRAYLANG['PARTNERS_DETAIL_TITLE'] = "Company Title";
+$_ARRAYLANG['PARTNERS_DETAIL_CONTACT_TXT'] = "Contact Information";
+$_ARRAYLANG['PARTNERS_DETAIL_PHONE_TXT'] = "Phone";
+$_ARRAYLANG['PARTNERS_DETAIL_FAX_TXT'] = "Fax";
+$_ARRAYLANG['PARTNERS_DETAIL_WEBSITE_TXT'] = "Website";
+$_ARRAYLANG['PARTNERS_COUNT'] = "Listing";
+$_ARRAYLANG['PARTNERS_NAME'] = "Partners";
+$_ARRAYLANG['PARTNER_NAME'] = "Partner";
+$_ARRAYLANG['TXT_PARTNERS_PHONES_PLUS'] = "+";
+$_ARRAYLANG['PARTNERS_COUNT_ERROR'] = "No Partners found matching your criteria.";
+$_ARRAYLANG['TXT_PARTNERS_LEVEL'] = "Level";
+$_ARRAYLANG['TXT_PARTNERS_PROFILE'] = "Profile";
+$_ARRAYLANG['TXT_PARTNERS_COUNTRY'] = "Country";
+$_ARRAYLANG['TXT_PARTNERS_VERTICAL'] = "Vertical";
+$_ARRAYLANG['TXT_PARTNERS_INACTIVE'] = "Inactive";
+$_ARRAYLANG['TXT_ENTRIES_TITLE'] = "Entries";
+?>
---
+++ modules/printshop/lang/ja/backend.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * @copyright   CONTREXX CMS - COMVATION AG
+ * @author      Comvation Development Team <info@comvation.com>
+ * @access      public
+ * @package     contrexx
+ * @subpackage  module_printshop
+ */
+$_ARRAYLANG['TXT_PRINTSHOP_OVERVIEW'] = "Übersicht";
+$_ARRAYLANG['TXT_PRINTSHOP_SETTINGS_TITLE'] = "Einstellungen";
+$_ARRAYLANG['TXT_PRINTSHOP_NO_ENTRY'] = "Keine Einträge vorhanden";
+$_ARRAYLANG['TXT_PRINTSHOP_ADD_ATTRIBUTE'] = "Attribut hinzufügen";
+$_ARRAYLANG['TXT_PRINTSHOP_UPDATE_ATTRIBUTE'] = "Attribut aktualisieren";
+$_ARRAYLANG['TXT_PRINTSHOP_ENTER_ATTRIBUTE'] = "Name eingeben:";
+$_ARRAYLANG['TXT_PRINTSHOP_SUBTITLE_ACTIONS'] = "Funktionen";
+$_ARRAYLANG['TXT_PRINTSHOP_ATTRIBUTE_NAME'] = "Attributname";
+$_ARRAYLANG['TXT_PRINTSHOP_ATTRIBUTE_STILL_USED'] = "Attribut %s wird noch verwendet und wurde deshalb nicht gelöscht.";
+$_ARRAYLANG['TXT_PRINTSHOP_ATTRIBUTE_DELETED'] = "Attribut %s wurde gelöscht.";
+$_ARRAYLANG['TXT_PRINTSHOP_DELETE_ENTRY'] = "Soll dieser Eintrag wirklich gelöscht werden? Dieser Vorgang kann nicht rückgängig gemacht werden!";
+$_ARRAYLANG['TXT_PRINTSHOP_DELETE_SELECTED'] = "Sollen wirklich alle markierten Einträge gelöscht werden? Dieser Vorgang kann nicht rückgängig gemacht werden!";
+$_ARRAYLANG['TXT_PRINTSHOP_ALREADY_EXISTS'] = "Dieser Eintrag existiert bereits.";
+$_ARRAYLANG['TXT_PRINTSHOP_EMAIL_HELP'] = "An diese E-Mail Adresse werden die online Bestellungen geschickt.";
+$_ARRAYLANG['TXT_PRINTSHOP_ORDER_EMAIL'] = "E-Mail für Bestellungen";
+$_ARRAYLANG['TXT_PRINTSHOP_ORDERS_TITLE'] = "Bestellungen";
+$_ARRAYLANG['TXT_PRINTSHOP_ADD'] = "Hinzufügen";
+$_ARRAYLANG['TXT_PRINTSHOP_ADD_PRODUCT'] = "Produkt hinzufügen";
+$_ARRAYLANG['TXT_PRINTSHOP_PRODUCTS'] = "Produkte";
+$_ARRAYLANG['TXT_PRINTSHOP_CHOOSE'] = "Wählen";
+$_ARRAYLANG['TXT_PRINTSHOP_FIELDS_REQUIRED'] = "Fehlende Angaben, bitte wählen Sie alle Kriterien aus.";
+$_ARRAYLANG['TXT_PRINTSHOP_ENTRY_ADDED'] = "Eintrag hinzugefügt.";
+$_ARRAYLANG['TXT_PRINTSHOP_ENTRY_DELETED'] = "Eintrag wurde gelöscht.";
+$_ARRAYLANG['TXT_PRINTSHOP_ENTRY_UPDATED'] = "Eintrag wurde aktualisiert.";
+$_ARRAYLANG['TXT_PRINTSHOP_FADE_OUT'] = "Meldung ausblenden";
+$_ARRAYLANG['TXT_PRINTSHOP_SAVE'] = "Speichern";
+$_ARRAYLANG['TXT_PRINTSHOP_ENTRIES_PER_PAGE'] = "Produkte pro Seite (Übersicht)";
+$_ARRAYLANG['TXT_PRINTSHOP_SETTINGS_SAVE_SUCCESSFUL'] = "Einstellungen gespeichert.";
+$_ARRAYLANG['TXT_PRINTSHOP_ENTRY'] = "Einträge";
+$_ARRAYLANG['TXT_PRINTSHOP_TYPE_TITLE'] = "Typ";
+$_ARRAYLANG['TXT_PRINTSHOP_FORMAT_TITLE'] = "Format";
+$_ARRAYLANG['TXT_PRINTSHOP_FRONT_TITLE'] = "Vorderseite";
+$_ARRAYLANG['TXT_PRINTSHOP_BACK_TITLE'] = "Rückseite";
+$_ARRAYLANG['TXT_PRINTSHOP_WEIGHT_TITLE'] = "Grammatur";
+$_ARRAYLANG['TXT_PRINTSHOP_PAPER_TITLE'] = "Papier";
+$_ARRAYLANG['TXT_PRINTSHOP_PRICE_TITLE'] = "Preis";
+$_ARRAYLANG['TXT_PRINTSHOP_FACTOR_TITLE'] = "Faktor";
+?>
