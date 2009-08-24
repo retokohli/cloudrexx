63a79bf2b8f6387ab9a50f6ca486ed0b40575020
lang <lang@comvation.com>
2009-08-24 12:11:47 +0000

    automated language files update (LanguageManager) triggered by dahasvn
    git-svn-id: http://svn.contrexxlabs.com/contrexx/trunk@6500 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

---
+++ modules/block/lang/ja/frontend.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @copyright   CONTREXX CMS - COMVATION AG
+ * @author      Comvation Development Team <info@comvation.com>
+ * @access      public
+ * @package     contrexx
+ * @subpackage  module_block
+ */
+$_ARRAYLANG['TXT_BLOCK_OVERVIEW'] = "Overview";
+$_ARRAYLANG['TXT_BLOCK_MARKET_DETAIL_ID'] = "more";
+$_ARRAYLANG['TXT_BLOCK_MARKET'] = "other advertisements";
+$_ARRAYLANG['TXT_BLOCK_MARKET_DETAIL'] = "all advertisements";
+$_ARRAYLANG['TXT_BLOCK_MARKET_ERROR_TITLE'] = "Advertisement";
+$_ARRAYLANG['TXT_BLOCK_MARKET_ERROR'] = "Actually there is no advertisement posted.";
+?>
