ee21c9929af82afbc6eb12b9ba4d328afdfc12e5
Reto Kohli <reto.kohli@comvation.com>
2009-08-26 15:03:26 +0000

    themes/* - Stripped whitespace, replaced tabs, added linefeed at EOF - Added LAST_MODIFICATION_PAGE placeholder - Added mobileview theme
    git-svn-id: http://svn.contrexxlabs.com/contrexx/trunk@6533 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- themes/contrexx_premium/banner/rotator.php
+++ themes/contrexx_premium/banner/rotator.php
@@ -1,15 +1,15 @@
 <?php
-$files = array(); 
+$files = array();
 $folder = './';
 
 $handle = opendir($folder);
 $exts = explode(' ','jpg jpeg png gif');
 while (false !== ($file = readdir($handle))) {
-	foreach($exts as $ext) { // for each extension check the extension
-		if (preg_match('/\.'.$ext.'$/i', $file, $test)) { // faster than ereg, case insensitive
-			$files[] = $file; // it's good
-		}
-	}
+    foreach($exts as $ext) { // for each extension check the extension
+        if (preg_match('/\.'.$ext.'$/i', $file, $test)) { // faster than ereg, case insensitive
+            $files[] = $file; // it's good
+        }
+    }
 }
 closedir($handle); // We're not using it anymore
 $rand = array_rand($files);
