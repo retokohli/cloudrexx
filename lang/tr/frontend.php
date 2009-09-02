af8dade6cd73b2c21a9723b2c54138e3724dbb10
David Vogt <david.vogt@comvation.com>
2009-09-02 12:11:57 +0000

    CSRF texts.. and some more (sync from trunk)
    git-svn-id: http://svn.contrexxlabs.com/contrexx/branches/contrexx_2_1@6605 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- lang/tr/frontend.php
+++ lang/tr/frontend.php
@@ -190,5 +190,7 @@ $_ARRAYLANG['TXT_AUCTION_MODULE_DESCRIPTION'] = "Auktionen";
 $_ARRAYLANG['TXT_AUCTION_TITLE'] = "Auktionen";
 $_ARRAYLANG['TXT_MARKET_LATEST'] = "Latest Ads";
 $_ARRAYLANG['TXT_CORE_LAST_MODIFIED_PAGE'] = "Geändert am";
-$_ARRAYLANG['TXT_HOTELCARD'] = "Hotelcard";
+$_ARRAYLANG['TXT_CSRF_TITLE'] = "Ein Fehler ist aufgetreten";
+$_ARRAYLANG['TXT_CSRF_DESCR'] = "Aus Sicherheitsgründen wurde diese Aktion blockiert. Falls diese Meldung wiederholt auftaucht, versuchen Sie, weniger Browserfenster offen zu halten.";
+$_ARRAYLANG['TXT_CSRF_BUTTON'] = "Klicken Sie hier, um fortzufahren.";
 ?>
