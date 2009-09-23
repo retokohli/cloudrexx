2465a95660af092930e2bac4b31daf9ef3518fe5
lang <lang@comvation.com>
2009-09-23 19:16:37 +0000

    automated language files update (LanguageManager) triggered by kon
    git-svn-id: http://svn.contrexxlabs.com/contrexx/trunk@6741 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- core_modules/search/lang/ru/frontend.php
+++ core_modules/search/lang/ru/frontend.php
@@ -6,13 +6,13 @@
  * @package     contrexx
  * @subpackage  core_module_search
  */
-$_ARRAYLANG['TXT_SEARCH_RESULTS'] = "Suchresultate";
-$_ARRAYLANG['TXT_SEARCH_RESULTS_ORDER_BY_RELEVANCE'] = "Ваш поиск к <strong> %s </strong> давал в итоге %d ответ(отсортировано после значения)";
-$_ARRAYLANG['TXT_RELEVANCE'] = "Значение";
-$_ARRAYLANG['TXT_NO_SEARCH_RESULTS'] = "Mit dem Suchbegriff <strong>%s</strong> wurden keine Resultate gefunden.";
-$_ARRAYLANG['TXT_SEARCH'] = "Suchen";
-$_ARRAYLANG['TXT_PLEASE_ENTER_SEARCHTERM'] = "Bitte geben Sie ein Suchwort ein.";
-$_ARRAYLANG['TXT_UNTITLED'] = "Ohne Titel";
+$_ARRAYLANG['TXT_SEARCH_RESULTS'] = "Risultati della ricerca";
+$_ARRAYLANG['TXT_SEARCH_RESULTS_ORDER_BY_RELEVANCE'] = "La ricerca di <strong>%s</strong> fornisce %d risultato(s) (separati da importanza)";
+$_ARRAYLANG['TXT_RELEVANCE'] = "Importanza";
+$_ARRAYLANG['TXT_NO_SEARCH_RESULTS'] = "Nessun documento corrisponde ai criteri di ricerca <strong>%s</strong>.";
+$_ARRAYLANG['TXT_SEARCH'] = "Ricercare";
+$_ARRAYLANG['TXT_PLEASE_ENTER_SEARCHTERM'] = "Volete osservare i criteri di ricerca.";
+$_ARRAYLANG['TXT_UNTITLED'] = "Senza titolo";
 $_ARRAYLANG['TXT_SEARCH_RESULTS_ORDER_BY_HITS'] = "Ihre Suche nach <strong>%s</strong> ergab %d Treffer";
 $_ARRAYLANG['TXT_SEARCH_RESULTS_SORT_BY'] = "sortiert nach";
 $_ARRAYLANG['TXT_HITS'] = "Treffer";
--- modules/printshop/lang/bg/frontend.php
+++ modules/printshop/lang/bg/frontend.php
@@ -7,7 +7,7 @@
  * @subpackage  module_printshop
  */
 $_ARRAYLANG['TXT_PRINTSHOP_SUMMARY'] = "Zusammenfassung";
-$_ARRAYLANG['TXT_PRINTSHOP_PRICE_PER_PIECE'] = "Preis pro Stück";
+$_ARRAYLANG['TXT_PRINTSHOP_PRICE_PER_PIECE'] = "Stückpreis";
 $_ARRAYLANG['TXT_PRINTSHOP_AMOUNT'] = "Exemplare";
 $_ARRAYLANG['TXT_PRINTSHOP_TYPE'] = "Drucksache";
 $_ARRAYLANG['TXT_PRINTSHOP_EXCL_TAX'] = "Exkl. MwSt.";
@@ -22,11 +22,11 @@ $_ARRAYLANG['TXT_PRINTSHOP_PAPER_TITLE'] = "Papier";
 $_ARRAYLANG['TXT_PRINTSHOP_PRICE_TITLE'] = "Preis";
 $_ARRAYLANG['TXT_PRINTSHOP_FACTOR_TITLE'] = "Faktor";
 $_ARRAYLANG['TXT_PRINTSHOP_COMMIT_ORDER'] = "Auftrag aufgeben";
-$_ARRAYLANG['TXT_PRINTSHOP_PRICES_THRESHOLD_AMOUNT'] = "Obige Tabelle zeigt Preis pro Stück ab bestellter Menge";
+$_ARRAYLANG['TXT_PRINTSHOP_PRICES_THRESHOLD_AMOUNT'] = "Die Tabelle zeigt den Preis pro Stück ab bestellter Menge";
 $_ARRAYLANG['TXT_PRINTSHOP_FILE'] = "Datei";
 $_ARRAYLANG['TXT_PRINTSHOP_CHOOSE_FILE'] = "Datei auswählen";
 $_ARRAYLANG['TXT_PRINTSHOP_UPLOAD_DATA_TITLE'] = "Daten hochladen";
-$_ARRAYLANG['TXT_PRINTSHOP_UPLOAD_DATA_DESC'] = "Bitte laden Sie Ihre Daten auf unseren Server. (gif, png, jpg)";
+$_ARRAYLANG['TXT_PRINTSHOP_UPLOAD_DATA_DESC'] = "Bitte laden Sie Ihre Bilder, die gedruck werden sollen, auf unseren Server. (max. 2MB, erlaubte Dateiendungen: gif, png, jpg)";
 $_ARRAYLANG['TXT_PRINTSHOP_SUBJECT'] = "Betreff";
 $_ARRAYLANG['TXT_PRINTSHOP_ORDERDETAILS'] = "Auftragsangaben";
 $_ARRAYLANG['TXT_PRINTSHOP_AMOUNT_COST_SHIP'] = "Auflage/Kosten/Versand";
@@ -49,7 +49,7 @@ $_ARRAYLANG['TXT_PRINTSHOP_CONTACT'] = "Kontakt";
 $_ARRAYLANG['TXT_PRINTSHOP_COMMENT'] = "Bemerkungen";
 $_ARRAYLANG['TXT_PRINTSHOP_EMAIL'] = "E-Mail";
 $_ARRAYLANG['TXT_PRINTSHOP_TELEPHONE'] = "Telefon";
-$_ARRAYLANG['TXT_PRINTSHOP_ACCEPT_TERMS'] = "Ich bin mit den AGB einverstanden";
+$_ARRAYLANG['TXT_PRINTSHOP_ACCEPT_TERMS'] = "Ich bin mit den %s AGB %s einverstanden";
 $_ARRAYLANG['TXT_PRINTSHOP_SUBMIT_ORDER'] = "Bestellung abschicken";
 $_ARRAYLANG['TXT_PRINTSHOP_MISSING_FIELDS'] = "Einige Angaben sind unvollständig, bitte füllen Sie alle Felder aus";
 $_ARRAYLANG['TXT_PRINTSHOP_ORDER_SAVE_ERROR'] = "Beim Speichern der Bestellung ist ein Fehler aufgetreten, falls das Problem nach erneutem Versuchen besteht, wenden Sie sich bitte an den Webmaster.";
@@ -57,9 +57,11 @@ $_ARRAYLANG['TXT_PRINTSHOP_INVALID_EMAIL'] = "Ungültige E-Mail Adresse";
 $_ARRAYLANG['TXT_PRINTSHOP_SHIPMENT_PICKUP'] = "Abholung";
 $_ARRAYLANG['TXT_PRINTSHOP_SHIPMENT_MESSENGER'] = "Kurier";
 $_ARRAYLANG['TXT_PRINTSHOP_SHIPMENT_MAIL'] = "Post";
+$_ARRAYLANG['TXT_PRINTSHOP_SHIPMENT_TYPE'] = "Lieferart";
 $_ARRAYLANG['TXT_PRINTSHOP_INVALID_PHONE'] = "Ungültige Telefonnummer";
 $_ARRAYLANG['TXT_PRINTSHOP_MISSING_IMAGE_UPLOAD_FRONT'] = "Sie müssen mindestens eine Bild-Datei als Druckvorlage hochladen. Es können nur Dateien mit der Endung .JPG, .GIF oder .PNG hochgeladen werden.";
 $_ARRAYLANG['TXT_PRINTSHOP_MISSING_IMAGE_UPLOAD_BACK'] = "Sie müssen mindestens zwei Bild-Dateien als Druckvorlage hochladen (Vorder- und Rückseite). Es können nur Dateien mit der Endung .JPG, .GIF oder .PNG hochgeladen werden.";
 $_ARRAYLANG['TXT_PRINTSHOP_INVALID_EXTENSION'] = "Ungültiger Dateityp. Es können nur Dateien mit der Endung .JPG, .GIF oder .PNG hochgeladen werden.";
 $_ARRAYLANG['TXT_PRINTSHOP_ERROR_UPLOADING_FILE'] = "Beim Hochladen der Datei ist ein Fehler aufgetreten. Bitte versuchen Sie es erneut oder wenden Sie sich an den Webmaster, falls das Problem besteht.";
+$_ARRAYLANG['TXT_PRINTSHOP_UPLOAD_DATA_DESC2'] = "Sie können uns die Bilder auch per %s oder %s nachsenden.";
 ?>
