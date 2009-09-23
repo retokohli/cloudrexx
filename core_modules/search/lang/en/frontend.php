48e4e6b235678d86008e5c26f969d9328efdd7d3
lang <lang@comvation.com>
2009-09-23 19:16:37 +0000

    automated language files update (LanguageManager) triggered by kon
    git-svn-id: http://svn.contrexxlabs.com/contrexx/trunk@6741 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- core_modules/search/lang/en/frontend.php
+++ core_modules/search/lang/en/frontend.php
@@ -6,13 +6,13 @@
  * @package     contrexx
  * @subpackage  core_module_search
  */
-$_ARRAYLANG['TXT_SEARCH_RESULTS'] = "Search Results";
-$_ARRAYLANG['TXT_SEARCH_RESULTS_ORDER_BY_RELEVANCE'] = "Your search for <strong>%s</strong> resulted in %d hits (sorted by relevance)";
-$_ARRAYLANG['TXT_RELEVANCE'] = "Relevance";
-$_ARRAYLANG['TXT_NO_SEARCH_RESULTS'] = "The search key <strong>%s</strong> did not yield any results.";
-$_ARRAYLANG['TXT_SEARCH'] = "Search";
-$_ARRAYLANG['TXT_PLEASE_ENTER_SEARCHTERM'] = "Please enter a keyword.";
-$_ARRAYLANG['TXT_UNTITLED'] = "Untitled";
+$_ARRAYLANG['TXT_SEARCH_RESULTS'] = "Risultati della ricerca";
+$_ARRAYLANG['TXT_SEARCH_RESULTS_ORDER_BY_RELEVANCE'] = "La ricerca di <strong>%s</strong> fornisce %d risultato(s) (separati da importanza)";
+$_ARRAYLANG['TXT_RELEVANCE'] = "Importanza";
+$_ARRAYLANG['TXT_NO_SEARCH_RESULTS'] = "Nessun documento corrisponde ai criteri di ricerca <strong>%s</strong>.";
+$_ARRAYLANG['TXT_SEARCH'] = "Ricercare";
+$_ARRAYLANG['TXT_PLEASE_ENTER_SEARCHTERM'] = "Volete osservare i criteri di ricerca.";
+$_ARRAYLANG['TXT_UNTITLED'] = "Senza titolo";
 $_ARRAYLANG['TXT_SEARCH_RESULTS_ORDER_BY_HITS'] = "Your search for <strong>%s</strong> resulted in %d hits";
 $_ARRAYLANG['TXT_SEARCH_RESULTS_SORT_BY'] = "Sorted by";
 $_ARRAYLANG['TXT_HITS'] = "Hits";
--- modules/recommend/lang/ES/frontend.php
+++ modules/recommend/lang/ES/frontend.php
@@ -6,23 +6,23 @@
  * @package     contrexx
  * @subpackage  module_recommend
  */
-$_ARRAYLANG['TXT_STATUS_RECEIVER_NAME'] = "Nombre del Receptor";
-$_ARRAYLANG['TXT_STATUS_RECEIVER_MAIL'] = "E-Mail del Receptor";
-$_ARRAYLANG['TXT_STATUS_COMMENT'] = "Comentario";
-$_ARRAYLANG['TXT_STATUS_SENDER_MAIL'] = "Tu direcci&oacute;n de E-Mail";
-$_ARRAYLANG['TXT_STATUS_SENDER_NAME'] = "Tu Nombre";
-$_ARRAYLANG['TXT_SENT_OK'] = "Recomendaci&oacute;n enviada con &eacute;xito.";
-$_ARRAYLANG['TXT_IS_EMPTY'] = "est&aacute; vac&iacute;o";
-$_ARRAYLANG['TXT_IS_INVALID'] = "no es v&aacute;lido";
-$_ARRAYLANG['TXT_RECEIVERMAIL_FRONTEND'] = "E-Mail del Receptor";
-$_ARRAYLANG['TXT_RECEIVERNAME_FRONTEND'] = "Nombre del Receptor";
-$_ARRAYLANG['TXT_GENDER_FRONTEND'] = "G&eacute;nero del Receptor";
-$_ARRAYLANG['TXT_SENDERNAME_FRONTEND'] = "Tu Nombre";
-$_ARRAYLANG['TXT_SENDERMAIL_FRONTEND'] = "E-Mail del Receptor";
-$_ARRAYLANG['TXT_COMMENT_FRONTEND'] = "Comentario";
-$_ARRAYLANG['TXT_PREVIEW_FRONTEND'] = "Vista Previa";
-$_ARRAYLANG['TXT_FEMALE_FRONTEND'] = "Femenino";
-$_ARRAYLANG['TXT_MALE_FRONTEND'] = "Masculino";
-$_ARRAYLANG['TXT_INTRODUCTION'] = "Si quieres recomendar esta p&aacute;gina, rellena el formulario y haz click en el bot&oacute;n enviar. El receptor recibir&aacute; un e-mail con tu recomendaci&oacute;n.";
-$_ARRAYLANG['TXT_RECOMMEND_CAPTCHA'] = "C&oacute;digo de Seguridad";
+$_ARRAYLANG['TXT_STATUS_RECEIVER_NAME'] = "Nome destinatario";
+$_ARRAYLANG['TXT_STATUS_RECEIVER_MAIL'] = "Indirizzo e-mail destinatario";
+$_ARRAYLANG['TXT_STATUS_COMMENT'] = "Commento";
+$_ARRAYLANG['TXT_STATUS_SENDER_MAIL'] = "Il vostro indirizzo e-mail";
+$_ARRAYLANG['TXT_STATUS_SENDER_NAME'] = "Il vostro nome";
+$_ARRAYLANG['TXT_SENT_OK'] = "Raccomandazione inviata con successo.";
+$_ARRAYLANG['TXT_IS_EMPTY'] = "est vuoto";
+$_ARRAYLANG['TXT_IS_INVALID'] = "est invalido!";
+$_ARRAYLANG['TXT_RECEIVERMAIL_FRONTEND'] = "Indirizzo E del destinatario";
+$_ARRAYLANG['TXT_RECEIVERNAME_FRONTEND'] = "Nome del destinatario";
+$_ARRAYLANG['TXT_GENDER_FRONTEND'] = "Sesso del destinatario";
+$_ARRAYLANG['TXT_SENDERNAME_FRONTEND'] = "Il vostro nome";
+$_ARRAYLANG['TXT_SENDERMAIL_FRONTEND'] = "Il vostro indirizzo e-mail";
+$_ARRAYLANG['TXT_COMMENT_FRONTEND'] = "Commento";
+$_ARRAYLANG['TXT_PREVIEW_FRONTEND'] = "Preview";
+$_ARRAYLANG['TXT_FEMALE_FRONTEND'] = "femminile";
+$_ARRAYLANG['TXT_MALE_FRONTEND'] = "maschile";
+$_ARRAYLANG['TXT_INTRODUCTION'] = "Per raccomandare questa pagina, riempire semplicemente il formulario seguente e premere finalmente su /Inviare/. Un e-mail sarà inviato al destinatario per segnalargli questa pagina..";
+$_ARRAYLANG['TXT_RECOMMEND_CAPTCHA'] = "Codice di sicurezza";
 ?>
