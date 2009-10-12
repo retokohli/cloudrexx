ae9e04f668251ecbd18d86cbaa613b204513b09b
lang <lang@comvation.com>
2009-10-12 15:28:29 +0000

    automated language files update (LanguageManager) triggered by dahasvn
    git-svn-id: http://svn.contrexxlabs.com/contrexx/trunk@6815 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- lang/de/frontend.php
+++ lang/de/frontend.php
@@ -194,4 +194,5 @@ $_ARRAYLANG['TXT_ACCESS_PRIMARY_GROUP'] = "Primäre Benutzergruppe";
 $_ARRAYLANG['TXT_CSRF_TITLE'] = "Ein Fehler ist aufgetreten";
 $_ARRAYLANG['TXT_CSRF_DESCR'] = "Aus Sicherheitsgründen wurde diese Aktion blockiert. Falls diese Meldung wiederholt auftaucht, versuchen Sie, weniger Browserfenster offen zu halten.";
 $_ARRAYLANG['TXT_CSRF_BUTTON'] = "Klicken Sie hier, um fortzufahren.";
+$_ARRAYLANG['TXT_ACCESS_PLEASE_SELECT'] = "Bitte wählen...";
 ?>
