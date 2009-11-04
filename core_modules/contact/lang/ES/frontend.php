60a8b768c1463f32e0e608ff3eef5681cc1faaab
lang <lang@comvation.com>
2009-11-04 14:48:58 +0000

    revert fuckup from langmanager (dave)
    git-svn-id: http://svn.contrexxlabs.com/contrexx/trunk@6890 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- core_modules/contact/lang/ES/frontend.php
+++ core_modules/contact/lang/ES/frontend.php
@@ -6,31 +6,31 @@
  * @package     contrexx
  * @subpackage  core_module_contact
  */
-$_ARRAYLANG['TXT_CONTACT_TRANSFERED_DATA_FROM'] = "Dati trasmessi del formulario di contatto del sito web";
-$_ARRAYLANG['TXT_CONTACT_FORM'] = "Nuovo messaggio del formulario di contatto aspro";
-$_ARRAYLANG['TXT_CONTACT_DATE'] = "Data";
-$_ARRAYLANG['TXT_CONTACT_HOSTNAME'] = "Server";
-$_ARRAYLANG['TXT_CONTACT_IP_ADDRESS'] = "Indirizzo IP ";
-$_ARRAYLANG['TXT_CONTACT_BROWSER_LANGUAGE'] = "Lingua del Browser";
-$_ARRAYLANG['TXT_CONTACT_BROWSER_VERSION'] = "Versione del Browser";
-$_ARRAYLANG['TXT_CONTACT_SUBMIT'] = "Inviare";
-$_ARRAYLANG['TXT_CONTACT_YES'] = "Sì";
+$_ARRAYLANG['TXT_CONTACT_TRANSFERED_DATA_FROM'] = "Esta es la informaci&oacute;n enviada por el formulario de contacto de la Web ";
+$_ARRAYLANG['TXT_CONTACT_FORM'] = "Nuevo formulario de contacto en la entrada";
+$_ARRAYLANG['TXT_CONTACT_DATE'] = "Fecha";
+$_ARRAYLANG['TXT_CONTACT_HOSTNAME'] = "Nombre del host";
+$_ARRAYLANG['TXT_CONTACT_IP_ADDRESS'] = "Direcci&oacute;n IP";
+$_ARRAYLANG['TXT_CONTACT_BROWSER_LANGUAGE'] = "Idioma del navegador";
+$_ARRAYLANG['TXT_CONTACT_BROWSER_VERSION'] = "Versi&oacute;n del navegador";
+$_ARRAYLANG['TXT_CONTACT_SUBMIT'] = "Enviar";
+$_ARRAYLANG['TXT_CONTACT_YES'] = "S&iacute;";
 $_ARRAYLANG['TXT_CONTACT_NO'] = "No";
-$_ARRAYLANG['TXT_CONTACT_UPLOADS'] = "Teletrasmissioni";
-$_ARRAYLANG['TXT_NEW_ENTRY_ERORR'] = "Volete verificare la vostra battitura!";
-$_ARRAYLANG['TXT_FEEDBACK_ERROR'] = "Uno o più errori si sono verificati. Utilizzate la chiave \\\" Precedente \\\" del vostro navigatore per tornare al formulario e verificate le vostre battiture!";
-$_ARRAYLANG['TXT_CONTACT_LANG'] = "Lingua";
-$_ARRAYLANG['TXT_CONTACT_ENTRIE_DETAILS'] = "Dettagli";
-$_ARRAYLANG['TXT_CONTACT_FORM_ENTRIES'] = "Messaggi";
-$_ARRAYLANG['TXT_CONTACT_FILE_UPLOAD_FAILED'] = "Errore in occasione di l' spedizione dell'archivio %s!";
-$_ARRAYLANG['TXT_CONTACT_FILE_EXTENSION_NOT_ALLOWED'] = "Errore: l'archivio %s non può essere accettato, poiché la sua estensione n' non è autorizzato!";
-$_ARRAYLANG['TXT_CONTACT_FAILED_SUBMIT_REQUEST'] = "Errore in occasione della trasmissione della richiesta!";
-$_ARRAYLANG['TXT_CONTACT_FILE_SIZE_EXCEEDS_LIMIT'] = "Errore in occasione di l' spedizione dell'archivio: la dimensione dell'archivio non può eccedere %s Bytes!";
-$_ARRAYLANG['TXT_CONTACT_FILE_TOO_LARGE'] = "Errore in occasione di l' spedizione dell'archivio: la dimensione dell'archivio è troppo importante!";
-$_ARRAYLANG['TXT_CONTACT_FILE_CORRUPT'] = "Errore: l'archivio %s n' a non potuto essere inviato completamente!";
-$_ARRAYLANG['TXT_CONTACT_FILE_NOT_UPLOADED'] = "Un archivio n' a non potuto essere importato!";
-$_ARRAYLANG['TXT_CONTACT_CAPTCHA_DESCRIPTION'] = "Volete osservare la sequenza di caratteri di l' immagine nel campo qui di seguito. (Egli s' agisce d' una protezione anti-spambot)";
-$_ARRAYLANG['TXT_CONTACT_INVALID_CAPTCHA_CODE'] = "Battitura erronea del codice CAPTCHA!";
-$_ARRAYLANG['TXT_CONTACT_RESET'] = "Cancellare";
-$_ARRAYLANG['TXT_CONTACT_RECEIVER_ADDRESSES_SELECTION'] = "Elenco destinatari";
+$_ARRAYLANG['TXT_CONTACT_UPLOADS'] = "Subidas";
+$_ARRAYLANG['TXT_NEW_ENTRY_ERORR'] = "\¡Por favor, compruebe los campos!";
+$_ARRAYLANG['TXT_FEEDBACK_ERROR'] = "\¡Se han producido uno o m&aacute;s errores. Por favor, utilice el bot&oacute;n \"volver\" de su navegador para regresar al formulario y comprobar sus campos!";
+$_ARRAYLANG['TXT_CONTACT_LANG'] = "Idioma";
+$_ARRAYLANG['TXT_CONTACT_ENTRIE_DETAILS'] = "Detalles";
+$_ARRAYLANG['TXT_CONTACT_FORM_ENTRIES'] = "Entradas";
+$_ARRAYLANG['TXT_CONTACT_FILE_UPLOAD_FAILED'] = "\¡El fichero %s no pudo ser enviado a causa de un error!";
+$_ARRAYLANG['TXT_CONTACT_FILE_EXTENSION_NOT_ALLOWED'] = "\¡El fichero %s fue rechazado debido a que tiene una extensi&oacute;n no permitida!";
+$_ARRAYLANG['TXT_CONTACT_FAILED_SUBMIT_REQUEST'] = "\¡La solicitud no pudo ser enviada debido a un error!";
+$_ARRAYLANG['TXT_CONTACT_FILE_SIZE_EXCEEDS_LIMIT'] = "\¡El fichero %s no fue subido debido a que el tama&ntilde;o era mayor que el permitido (%s)!";
+$_ARRAYLANG['TXT_CONTACT_FILE_TOO_LARGE'] = "\¡El fichero no pudo ser subido ya que era demasiado grande!";
+$_ARRAYLANG['TXT_CONTACT_FILE_CORRUPT'] = "\¡El fichero %s no fue subido correctamente!";
+$_ARRAYLANG['TXT_CONTACT_FILE_NOT_UPLOADED'] = "\¡El archivo no se ha subido correctamente!";
+$_ARRAYLANG['TXT_CONTACT_CAPTCHA_DESCRIPTION'] = "Por favor, escriba los caracteres de la imagen en el campo de texto destinado para ello. Esto es necesario para que el sistema sepa que usted no es un robot de spam.";
+$_ARRAYLANG['TXT_CONTACT_INVALID_CAPTCHA_CODE'] = "\¡La cadena de caracteres introducida no es v&aacute;lida!";
+$_ARRAYLANG['TXT_CONTACT_RESET'] = "Reiniciar";
+$_ARRAYLANG['TXT_CONTACT_RECEIVER_ADDRESSES_SELECTION'] = "Seleccionar direcci&oacute;n del destinatario";
 ?>
