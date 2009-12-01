76675563ecf0b19b8e7269af154afae4b857ef2d
David Vogt <david.vogt@comvation.com>
2009-12-01 18:00:17 +0000

    (AUTOMATIC) update trunk's installer dumps from contrexxlabs_contrexx_trunk
    git-svn-id: http://svn.contrexxlabs.com/contrexx/trunk@6962 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- installer/data/contrexx_dump_data.sql
+++ installer/data/contrexx_dump_data.sql
@@ -2331,7 +2331,7 @@ INSERT INTO `contrexx_module_newsletter_settings` (`setid`, `setname`, `setvalue
 INSERT INTO `contrexx_module_newsletter_settings` (`setid`, `setname`, `setvalue`, `status`) VALUES (5,'text_break_after','100',1);
 INSERT INTO `contrexx_module_newsletter_settings` (`setid`, `setname`, `setvalue`, `status`) VALUES (6,'test_mail','info@example.com',1);
 INSERT INTO `contrexx_module_newsletter_settings` (`setid`, `setname`, `setvalue`, `status`) VALUES (7,'overview_entries_limit','10',1);
-INSERT INTO `contrexx_module_newsletter_settings` (`setid`, `setname`, `setvalue`, `status`) VALUES (8,'rejected_mail_operation','deactivate',1);
+INSERT INTO `contrexx_module_newsletter_settings` (`setid`, `setname`, `setvalue`, `status`) VALUES (8,'rejected_mail_operation','delete',1);
 INSERT INTO `contrexx_module_newsletter_settings` (`setid`, `setname`, `setvalue`, `status`) VALUES (9,'defUnsubscribe','0',1);
 INSERT INTO `contrexx_module_newsletter_settings` (`setid`, `setname`, `setvalue`, `status`) VALUES (10,'notifyOnUnsubscribe','1',1);
 INSERT INTO `contrexx_module_newsletter_template` (`id`, `name`, `description`, `html`, `text`, `required`) VALUES (1,'Standard','Standard Template','<html>\r\n<head>\r\n<title>[[subject]]</title>\r\n</head>\r\n<body>\r\n[[content]]\r\n<br /><br />\r\n[[profile_setup]]<br/>\r\n[[unsubscribe]]\r\n</body>\r\n</html>','[[content]]\r\n\r\n[[profile_setup]]\r\n[[unsubscribe]]',1);
