0db294c42ddfbcc8dc1c230db8ca889b7025194d
lang <lang@comvation.com>
2010-01-12 10:22:15 +0000

    automated language files update (LanguageManager) triggered by dahasvn
    git-svn-id: http://svn.contrexxlabs.com/contrexx/trunk@7038 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

---
+++ core_modules/login/lang/ja/frontend.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @copyright   CONTREXX CMS - COMVATION AG
+ * @author      Comvation Development Team <info@comvation.com>
+ * @access      public
+ * @package     contrexx
+ * @subpackage  core_module_login
+ */
+$_ARRAYLANG['TXT_DIR_F_LINK'] = "Link";
+?>
