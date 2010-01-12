e03b51d1d707bbdf75638e29eacbdd2f33b3c8e1
lang <lang@comvation.com>
2010-01-12 10:54:33 +0000

    lang: removed obsolete lang SE
    git-svn-id: http://svn.contrexxlabs.com/contrexx/trunk@7045 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- core_modules/development/lang/SE/backend.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
-* Contrexx CMS
-* generated date Tue, 23 Jan 2007 14:44:20 +0100
-**/
-
-$_ARRAYLANG['TXT_BANNER_GROUP_DELETE_JS'] = "M&ouml;chten Sie diese Bannergruppe l&ouml;schen?";
-?>
