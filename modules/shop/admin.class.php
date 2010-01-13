73a8cfc72601ae6a13e833ded93b02f4392ab5da
Thomas Däppen <thomas.daeppen@cloudrexx.com>
2010-01-13 15:47:35 +0000

    shop:
    - support for images containing special character in the filename
    - umlauts support in comments of orders
    
    
    git-svn-id: http://svn.contrexxlabs.com/contrexx/trunk@7054 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- modules/shop/admin.class.php
+++ modules/shop/admin.class.php
@@ -3163,16 +3163,28 @@ class shopmanager extends ShopLibrary
             'SHOP_STOCK_VISIBILITY' => ($objProduct->isStockVisible() ? ' checked="checked"' : ''),
             'SHOP_MANUFACTURER_MENUOPTIONS' =>
                 Manufacturer::getMenuoptions($objProduct->getManufacturerId()),
-            'SHOP_PICTURE1_IMG_SRC' => (!empty($arrImages[1]['img']) && is_file(ASCMS_SHOP_IMAGES_PATH.'/'.$arrImages[1]['img'].self::thumbnailSuffix)
-                    ? ASCMS_SHOP_IMAGES_WEB_PATH.'/'.$arrImages[1]['img'].self::thumbnailSuffix
+            'SHOP_PICTURE1_IMG_SRC' =>
+                (   !empty($arrImages[1]['img'])
+                 && is_file(ASCMS_SHOP_IMAGES_PATH.'/'.
+                        ImageManager::getThumbnailFilename($arrImages[1]['img']))
+                    ? FWValidator::getEscapedSource(ASCMS_SHOP_IMAGES_WEB_PATH.'/'.
+                          ImageManager::getThumbnailFilename($arrImages[1]['img']))
                     : self::$defaultImage
                 ),
-            'SHOP_PICTURE2_IMG_SRC' => (!empty($arrImages[2]['img']) && is_file(ASCMS_SHOP_IMAGES_PATH.'/'.$arrImages[2]['img'].self::thumbnailSuffix)
-                    ? ASCMS_SHOP_IMAGES_WEB_PATH.'/'.$arrImages[2]['img'].self::thumbnailSuffix
+            'SHOP_PICTURE2_IMG_SRC' =>
+                (   !empty($arrImages[2]['img'])
+                 && is_file(ASCMS_SHOP_IMAGES_PATH.'/'.
+                        ImageManager::getThumbnailFilename($arrImages[2]['img']))
+                    ? FWValidator::getEscapedSource(ASCMS_SHOP_IMAGES_WEB_PATH.'/'.
+                      ImageManager::getThumbnailFilename($arrImages[2]['img']))
                     : self::$defaultImage
                 ),
-            'SHOP_PICTURE3_IMG_SRC' => (!empty($arrImages[3]['img']) && is_file(ASCMS_SHOP_IMAGES_PATH.'/'.$arrImages[3]['img'].self::thumbnailSuffix)
-                    ? ASCMS_SHOP_IMAGES_WEB_PATH.'/'.$arrImages[3]['img'].self::thumbnailSuffix
+            'SHOP_PICTURE3_IMG_SRC' =>
+                (   !empty($arrImages[3]['img'])
+                 && is_file(ASCMS_SHOP_IMAGES_PATH.'/'.
+                        ImageManager::getThumbnailFilename($arrImages[3]['img']))
+                    ? FWValidator::getEscapedSource(ASCMS_SHOP_IMAGES_WEB_PATH.'/'.
+                      ImageManager::getThumbnailFilename($arrImages[3]['img']))
                     : self::$defaultImage
                 ),
             'SHOP_PICTURE1_IMG_SRC_NO_THUMB' => (!empty($arrImages[1]['img']) && is_file(ASCMS_SHOP_IMAGES_PATH.'/'.$arrImages[1]['img'])
@@ -3474,7 +3486,7 @@ class shopmanager extends ShopLibrary
                         'SHOP_ORDERID' => $orderId,
                         'SHOP_TIP_ID' => $orderId,
                         'SHOP_TIP_NOTE' => ereg_replace(
-                                "\r\n|\n|\r", '<br />', htmlentities(strip_tags($tipNote))
+                                "\r\n|\n|\r", '<br />', htmlentities(strip_tags($tipNote), ENT_QUOTES, CONTREXX_CHARSET)
                             ),
                         'SHOP_TIP_LINK' => $tipLink,
                         'SHOP_DATE' => $objResult->fields['order_date'],
