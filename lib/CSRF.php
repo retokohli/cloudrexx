186e4d50fbc9e22b20e4da6b9a33590dad1a654a
Thomas Däppen <thomas.daeppen@cloudrexx.com>
2010-01-13 09:42:12 +0000

    csrf: minor improvements
    git-svn-id: http://svn.contrexxlabs.com/contrexx/trunk@7047 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- lib/CSRF.php
+++ lib/CSRF.php
@@ -1,6 +1,16 @@
 <?php
 
 /**
+ * Protect against CSRF attacks
+ * @version     2.1.3
+ * @since       2.1.3
+ * @package     contrexx
+ * @subpackage  lib
+ * @copyright   CONTREXX CMS - COMVATION AG
+ * @author      David Vogt <david.vogt@comvation.com>
+ */
+
+/**
  * This class provides protection against CSRF attacks.
  *
  * call CSRF::add_code() if the page contains vulnerable
@@ -9,7 +19,12 @@
  *
  * This class expects that the session has been set up
  * correctly and can be used through $_SESSION.
- *
+ * @version     2.1.3
+ * @since       2.1.3
+ * @package     contrexx
+ * @subpackage  lib
+ * @copyright   CONTREXX CMS - COMVATION AG
+ * @author      David Vogt <david.vogt@comvation.com>
  */
 class CSRF {
 
@@ -52,15 +67,18 @@ class CSRF {
 
     private static $current_code = NULL;
 
-    private static function __get_code() {
-        if (!empty(CSRF::$current_code)) {
-            return CSRF::$current_code;
+
+    private static function __get_code()
+    {
+        if (!empty(self::$current_code)) {
+            return self::$current_code;
         }
-        CSRF::$current_code = base64_encode(rand(1000000000000,9999999999999));
-        CSRF::$current_code = preg_replace('#[\'"=%]#', '_', CSRF::$current_code);
-        CSRF::__setkey(CSRF::$current_code, CSRF::$validity_count);
-        return CSRF::$current_code;
+        self::$current_code = base64_encode(rand(1000000000000, 9999999999999));
+        self::$current_code = preg_replace('#[\'"=%]#', '_', self::$current_code);
+        self::__setkey(self::$current_code, self::$validity_count);
+        return self::$current_code;
     }
+
 
     /**
      * An utility function to patch URLs specifically in
@@ -71,42 +89,47 @@ class CSRF {
      * Example: __enhance_header('Location: index.php')
      * --> "Location: index.php?__csrf__=xxxxx"
      */
-    private static function __enhance_header($header) {
+    private static function __enhance_header($header)
+    {
+        if (!self::__is_logged_in()) return $header;
         $result = array();
         if (!preg_match('#^(\w+):\s*(.*)$#i', $header, $result)) {
-            # don't know what to do with it
+            // don't know what to do with it
             return $header;
         }
         $hdr = $result[1];
-        $url = CSRF::enhanceURI($result[2]);
-
+        $url = self::enhanceURI($result[2]);
         return "$hdr: $url";
     }
+
 
     /**
      * Acts as a replacement for header() calls that handle URLs.
      * Only use it for headers in the form "Foo: an_url", for
      * instance "Location: index.php?foo=bar".
      */
-    public static function header($header) {
-        header(CSRF::__enhance_header($header));
+    public static function header($header)
+    {
+        header(self::__enhance_header($header));
     }
 
+
     /**
-	 * Adds the CSRF protection code to the URI specified by $uri.
+     * Adds the CSRF protection code to the URI specified by $uri.
+     *
+     * Note: This adds a simple ampersand (&), not the HTML entity &amp;.
+     * Thus, it is only suitable for modifying header() parameters and
+     * URIs within javascript.  For URIs to be embedded into HTML,
+     * you *SHOULD* htmlentities() it first!
      */
     public static function enhanceURI($uri)
     {
-        $key = CSRF::$formkey;
-        $val = CSRF::__get_code();
-        if (strstr($uri, '?')) {
-            $uri .= "&$key=$val";
-        }
-        else {
-            $uri .= "?$key=$val";
-        }
+        $key = self::$formkey;
+        $val = self::__get_code();
+        $uri .= (strstr($uri, '?') ? '&' : '?')."$key=$val";
         return $uri;
     }
+
 
     /**
      * Call this to add a CSRF protection code to all the
@@ -114,213 +137,233 @@ class CSRF {
      * you don't need to pass any content, and nothing is
      * returned - this function uses PHP to change it's
      * output so as to insert the data.
+     *
+     * Note: output_add_rewrite_var() used in here does a really bad job
+     * on your URIs within the HTML.  It adds parameters without considering
+     * whether it should use '&' or '&amp;'.  This results in invalid HTML!
      */
-	public static function add_code() {
-        if (!CSRF::__is_logged_in())   { return; }
-        if (CSRF::$already_added_code) { return; }
-        CSRF::$already_added_code = true;
-        $code = CSRF::__get_code();
-        output_add_rewrite_var(CSRF::$formkey, $code);
-	}
+    public static function add_code()
+    {
+        if (!self::__is_logged_in()) return;
+        if (self::$already_added_code) return;
+        self::$already_added_code = true;
+        $code = self::__get_code();
+        output_add_rewrite_var(self::$formkey, $code);
+    }
+
 
     /**
      * Adds a placeholder for the CSRF code to the given template.
      * This is so you can easily patch javascript code that handles
      * URLs, as this cannot be done by add_code().
-     *
      * @param $tpl Template object
      */
-    public static function add_placeholder($tpl) {
-        if (!CSRF::__is_logged_in())   { return; }
+    public static function add_placeholder($tpl)
+    {
+        if (!self::__is_logged_in()) return true;
         if (!is_object($tpl)) {
-            DBG::msg("CSRF::add_placeholder(): fix this call, that ain't a template object! (Stack follows)");
+            DBG::msg("self::add_placeholder(): fix this call, that ain't a template object! (Stack follows)");
             DBG::stack();
         }
-        $code = CSRF::__get_code();
+        $code = self::__get_code();
         $tpl->setGlobalVariable(array(
-            "CSRF_PARAM"    => CSRF::param(),
-            "CSRF_KEY"      => "$code"
+            "CSRF_PARAM" => self::param(),
+            "CSRF_KEY"   => "$code",
         ));
         return true;
     }
+
 
     /**
      * Returns the anti-CSRF code's form key.
      * You can build your own URLs together
      * with CSRF::code()
      */
-    public static function key() {
-        return CSRF::$formkey;
+    public static function key()
+    {
+        return self::$formkey;
     }
+
 
     /**
      * Returns the anti-CSRF code for the current
      * request. You can build your own URLs together
      * with CSRF::key()
      */
-    public static function code() {
-        return CSRF::__get_code();
+    public static function code()
+    {
+        return self::__get_code();
     }
+
 
     /**
      * Returns a key/value pair ready to use in an URL.
      */
-    public static function param() {
-        if (!CSRF::__is_logged_in()) return '';
-
-        return CSRF::key().'='.CSRF::code();
+    public static function param()
+    {
+        if (!self::__is_logged_in()) return '';
+        return self::key().'='.self::code();
     }
+
 
     /**
      * Call this if you need to protect critical work.
      * This function will stop the request if it cannot
      * find a valid anti-CSRF code in the request.
      */
-    public static function check_code() {
-        if (!CSRF::__is_logged_in()) { return; }
-        if (CSRF::$already_checked)  { return; }
-
-        CSRF::$already_checked = true;
-
-        # do not check if it's an AJAX request.. they're secure
-        # by definition and also, they're much more delicate in
-        # what can be returned - and they usually exceed the
-        # request amount limit pretty quickly (see active_decrease etc)
-        if (CSRF::__is_ajax()) {
+    public static function check_code()
+    {
+        if (!self::__is_logged_in()) return;
+        if (self::$already_checked) return;
+        self::$already_checked = true;
+        // do not check if it's an AJAX request.  They're secure
+        // by definition and also, they're much more delicate in
+        // what can be returned - and they usually exceed the
+        // request amount limit pretty quickly (see active_decrease etc)
+        if (self::__is_ajax()) {
             return;
         }
 
         $code = ($_SERVER['REQUEST_METHOD'] == 'GET')
-            ? (isset($_GET [CSRF::$formkey]) ? $_GET[CSRF::$formkey] : '')
-            : (isset($_POST[CSRF::$formkey]) ? $_POST[CSRF::$formkey] : '')
-        ;
+            ? (isset($_GET [self::$formkey]) ? $_GET[self::$formkey] : '')
+            : (isset($_POST[self::$formkey]) ? $_POST[self::$formkey] : '');
 
-        CSRF::__cleanup();
-        if(! CSRF::__getkey($code)) {
-            CSRF::__kill();
-        }
-        else {
-            CSRF::__reduce($code);
-
-            if (CSRF::__getkey($code) < 0) {
-                CSRF::__kill();
+        self::__cleanup();
+        if(! self::__getkey($code)) {
+            self::__kill();
+        } else {
+            self::__reduce($code);
+            if (self::__getkey($code) < 0) {
+                self::__kill();
             }
         }
     }
 
-    private static function __kill() {
+
+    private static function __kill()
+    {
         global $_CORELANG;
 
-        $data = ($_SERVER['REQUEST_METHOD'] == 'GET')
+        $data = ($_SERVER['REQUEST_METHOD'] == 'GET'
             ? $_GET
             : $_POST
-        ;
-        CSRF::add_code();
+        );
+        self::add_code();
 
         // TODO: make this a nice little template
         $html = '
-            <html><head>
-            <title>'.$_CORELANG['TXT_CSRF_TITLE'].'</title>
-            <style type="text/css">
-                * {
-                    font-family: Arial,Helvetica,sans-serif;
-                }
-                div#message {
-                    margin-left: auto;
-                    margin-right: auto;
-                    width: 500px;
-                    border: 1px solid red;
-                    padding: 5px;
-                    background-color: #ffefef;
-                    margin-top: 100px;
-                }
-            </style>
-            </head>
-            <body>
-            <div id="message">
-                <h2>'.$_CORELANG['TXT_CSRF_TITLE'].'</h2>
-                '.$_CORELANG['TXT_CSRF_DESCR'].'
+<html><head>
+<title>'.$_CORELANG['TXT_CSRF_TITLE'].'</title>
+<style type="text/css">
+  * {
+    font-family: Arial,Helvetica,sans-serif;
+  }
+  div#message {
+    margin-left: auto;
+    margin-right: auto;
+    width: 500px;
+    border: 1px solid red;
+    padding: 5px;
+    background-color: #ffefef;
+    margin-top: 100px;
+  }
+</style>
+</head>
+<body>
+<div id="message">
+  <h2>'.$_CORELANG['TXT_CSRF_TITLE'].'</h2>
+  '.$_CORELANG['TXT_CSRF_DESCR'].'
 
-                <p/>
-                <form method="'.$_SERVER['REQUEST_METHOD'].'">
-                _____ELEMENTS___
-                <input type="submit" value="'.$_CORELANG['TXT_CSRF_BUTTON'].'" />
-                </form>
-            </div>
-            </body>
-            </html>
-        ';
+  <p/>
+  <form method="'.$_SERVER['REQUEST_METHOD'].'">
+  _____ELEMENTS___
+  <input type="submit" value="'.$_CORELANG['TXT_CSRF_BUTTON'].'" />
+  </form>
+</div>
+</body>
+</html>
+';
         $form = '';
         foreach ($data as $key => $value) {
-            if ($key == CSRF::$formkey or $key == 'amp;'.CSRF::$formkey) {
+            if ($key == self::$formkey or $key == 'amp;'.self::$formkey) {
                 continue;
             }
-            $form .= CSRF::parseRequestParametersForForm($key, $value);
+            $form .= self::parseRequestParametersForForm($key, $value);
         }
         $html = str_replace('_____ELEMENTS___', $form, $html);
         die($html);
     }
 
-    private static function parseRequestParametersForForm($key, $value, $arrSubKeys = array())
+
+    private static function parseRequestParametersForForm($key, $value, $arrSubKeys=array())
     {
+        $elem = '';
         if (is_array($value)) {
             foreach ($value as $subKey => $subValue) {
-                $elem .= CSRF::parseRequestParametersForForm($key, $subValue, array_merge($arrSubKeys, array($subKey)));
+                $elem .= self::parseRequestParametersForForm($key, $subValue, array_merge($arrSubKeys, array($subKey)));
             }
         } else {
             $elem = '<input type="hidden" name="_N_" value="_V_" />';
-            $elem = str_replace('_N_', CSRF::__formval($key.(!empty($arrSubKeys) ? '['.implode('][', $arrSubKeys).']' : '')),  $elem);
-            $elem = str_replace('_V_', CSRF::__formval($value),$elem);
+            $elem = str_replace('_N_', self::__formval($key.(!empty($arrSubKeys) ? '['.implode('][', $arrSubKeys).']' : '')),  $elem);
+            $elem = str_replace('_V_', self::__formval($value), $elem);
         }
-
         return $elem;
     }
 
-    private static function __formval($str) {
+
+    private static function __formval($str)
+    {
         return htmlspecialchars(contrexx_stripslashes($str), ENT_QUOTES, CONTREXX_CHARSET);
     }
 
-    private static function __reduce($code) {
-        foreach (array_keys($_SESSION[CSRF::$sesskey]) as $key) {
-            $reduce = ($code == $key)
-                ? CSRF::$active_decrease
-                : CSRF::$unused_decrease
-            ;
-            $_SESSION[CSRF::$sesskey][$key] -= $reduce;
+
+    private static function __reduce($code)
+    {
+        foreach (array_keys($_SESSION[self::$sesskey]) as $key) {
+            $_SESSION[self::$sesskey][$key] -=
+                ($code == $key
+                    ? self::$active_decrease : self::$unused_decrease);
         }
     }
 
-    private static function __is_ajax() {
-        return (isset($_SERVER['HTTP_X_REQUESTED_WITH']) &&
-            ($_SERVER['HTTP_X_REQUESTED_WITH'] == 'XMLHttpRequest'));
+
+    private static function __is_ajax()
+    {
+        return
+            (   isset($_SERVER['HTTP_X_REQUESTED_WITH'])
+             && $_SERVER['HTTP_X_REQUESTED_WITH'] == 'XMLHttpRequest');
     }
 
-    private static function __cleanup() {
-        $del_candidates = array();
-        foreach ($_SESSION[CSRF::$sesskey] as $key => $count) {
+
+    private static function __cleanup()
+    {
+        foreach ($_SESSION[self::$sesskey] as $key => $count) {
             if ($count < 0) {
-                $del_candidates[] = $key;
+                unset($_SESSION[self::$sesskey][$key]);
             }
         }
-        foreach ($del_candidates as $cand) {
-            unset($_SESSION[CSRF::$sesskey][$cand]);
-        }
-
     }
 
-    private function __getkey($key) {
-        return !empty($_SESSION[CSRF::$sesskey][$key]);
+
+    private static function __getkey($key)
+    {
+        return !empty($_SESSION[self::$sesskey][$key]);
     }
-    private function __setkey($key, $value) {
-        if (!isset($_SESSION[CSRF::$sesskey])) {
-            $_SESSION[CSRF::$sesskey] = array();
+
+
+    private static function __setkey($key, $value)
+    {
+        if (!isset($_SESSION[self::$sesskey])) {
+            $_SESSION[self::$sesskey] = array();
         }
-        $csrfdata                 = $_SESSION[CSRF::$sesskey];
+        $csrfdata                 = $_SESSION[self::$sesskey];
         $csrfdata[$key]           = $value;
-        $_SESSION[CSRF::$sesskey] = $csrfdata;
+        $_SESSION[self::$sesskey] = $csrfdata;
     }
 
-    private function __is_logged_in() {
+
+    private static function __is_logged_in()
+    {
         if (class_exists('FWUser')) {
             $objFWUser = FWUser::getFWUserObject();
             if ($objFWUser->objUser->login()) {
@@ -330,12 +373,19 @@ class CSRF {
         return false;
     }
 
+
     /**
-     * Removed the CSRF protection parameter from the query string and referer
+     * Remove the CSRF protection parameter from the query string and referrer
      */
     public static function cleanRequestURI()
     {
-        $csrfUrlModifierPattern = '#(\&|\?)?'.CSRF::$formkey.'\=[a-zA-Z0-9_]+#';
+// This will remove the parameter from the first position in the query string
+// and leave an URI like "index.php&name=value", which is invalid
+        //$csrfUrlModifierPattern = '#(?:\&(?:amp\;)?|\?)?'.self::$formkey.'\=[a-zA-Z0-9_]+#';
+// Better cut the parameter plus trailing ampersand, if any.
+        $csrfUrlModifierPattern = '/'.self::$formkey.'\=[a-zA-Z0-9_]+\&?/';
+// This will leave the URI valid, even if it's the last parameter;
+// a trailing question mark or ampersand does no harm.
         !empty($_SERVER['QUERY_STRING']) ? $_SERVER['QUERY_STRING'] = preg_replace($csrfUrlModifierPattern, '', $_SERVER['QUERY_STRING'])    : false;
         !empty($_SERVER['REQUEST_URI'])  ? $_SERVER['REQUEST_URI']  = preg_replace($csrfUrlModifierPattern, '', $_SERVER['REQUEST_URI'])     : false;
         !empty($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] = preg_replace($csrfUrlModifierPattern, '', $_SERVER['HTTP_REFERER'])    : false;
@@ -344,3 +394,5 @@ class CSRF {
 }
 
 CSRF::cleanRequestURI();
+
+?>
