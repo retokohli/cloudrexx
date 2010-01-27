b20b0dcbbdc95ba6f83d5273e620843bc4b1189e
Reto Kohli <reto.kohli@comvation.com>
2010-01-27 10:59:32 +0000

    modules/knowledge/search.php - Disabled CSRF
    git-svn-id: http://svn.contrexxlabs.com/contrexx/branches/contrexx_2_1@7143 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- modules/knowledge/search.php
+++ modules/knowledge/search.php
@@ -17,9 +17,9 @@ require_once "../../core/validator.inc.php";
 require_once "../../core/database.php";
 require_once "lib/databaseError.class.php";
 
-require_once '../../lib/CSRF.php';
+//require_once '../../lib/CSRF.php';
 // Temporary fix until all GET operation requests will be replaced by POSTs
-CSRF::setFrontendMode();
+//CSRF::setFrontendMode();
 
 require_once ASCMS_LIBRARY_PATH.'/PEAR/HTML/Template/Sigma/Sigma.php';
 require_once ASCMS_LIBRARY_PATH.'/adodb/adodb.inc.php';
