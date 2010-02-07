93ccd908d7fa42b28ae7a848cb232fb483a65ad9
David Vogt <david.vogt@comvation.com>
2010-02-07 22:02:31 +0000

    layout fixes in backend, code cleanup
    git-svn-id: http://svn.contrexxlabs.com/contrexx/trunk@7191 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- modules/partners/PartnersBase.php
+++ modules/partners/PartnersBase.php
@@ -133,6 +133,7 @@ class PartnersBase {
         $this->_objTpl->add_tr('TXT_PARTNERS_IMAGE_SCALE_WIDTH');
         $this->_objTpl->add_tr('TXT_PARTNERS_RESET_SEARCH');
         $this->_objTpl->add_tr('TXT_PARTNERS_HIDE_EMPTY_LABELS');
+        $this->_objTpl->add_tr('TXT_PARTNERS_SEARCH_TEXT');
         $this->_objTpl->PARTNERS_DROPDOWN_JS_LOCATION     = ASCMS_MODULE_WEB_PATH . '/partners/js/dropdown.js';
         $this->_objTpl->PARTNERS_LABELBROWSER_JS_LOCATION = ASCMS_MODULE_WEB_PATH . '/partners/js/labelbrowser.js';
         $this->_objTpl->PARTNERS_LABEL_JS_LOCATION        = ASCMS_MODULE_WEB_PATH . '/partners/js/labels.js';
