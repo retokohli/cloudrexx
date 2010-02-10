e172f0d28b3af08044ec1f778d6e1da4754f46e8
David Vogt <david.vogt@comvation.com>
2010-02-10 21:46:17 +0000

    make label browser actually usable with keyboard
    git-svn-id: http://svn.contrexxlabs.com/contrexx/trunk@7207 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- modules/partners/js/labelbrowser.js
+++ modules/partners/js/labelbrowser.js
@@ -21,6 +21,10 @@ var LabelBrowser = (function(){
         return find_in_tree(id, elem.parentid);
     };
 
+    /**
+     * Returns the element in the treedata with the given id-number, specified by elem_id.
+     * The first parameter specifies the browser object, as with all other functions here.
+     */
     var find_in_tree = function(id, elem_id) {
         var td = treedata[id];
 
@@ -49,15 +53,102 @@ var LabelBrowser = (function(){
         return found;
     };
 
+    /**
+     * Returns a list of dropdowns that need to be drawn.
+     * The first parameter, id, is the id of the browser itself (and DOM id of the wrapping div).
+     * The second parameter, current, is the id uf the newly selected entry.
+     *
+     * The resulting data structure is a list, of objects of the following form:
+     *     {list: <what to display>, selected: <id of the selected entry>}
+     */
+    var draw_data = function(id,current) {
+        var treestructure = [];
+        var curr;
+        if (current > 0) {
+            curr = find_in_tree(id, current);
+            while (true) {
+                if (curr.parentid) {
+                    var par = find_in_tree(id, curr.parentid);
+                    treestructure.push({'list': par.children, 'selected': curr.id});
+                    curr = par;
+                }
+                else {
+                    break;
+                }
+            }
+        }
+        treestructure.push({'list': treedata[id], 'selected': curr ? curr.id : 0});
+
+        treestructure.reverse();
+        return treestructure;
+    };
+
+    /**
+     * Draws all select boxes from the root node, up to the given "current" entry.
+     */
+    var draw = function(id, current) {
+        var treestructure = draw_data(id, current);
+
+        treestructure.each(function(elem){
+                add(id, elem.list, elem.selected);
+        });
+    };
+
+    /**
+     * after a change-event, this function removes all the select boxes right of
+     * the just-clicked one while keeping the clicked one and the ones left of it.
+     * Then, it draws a new select box right of it if the currently selected entry
+     * has children.
+     * This is different from draw(), which just paints all the select boxes up from
+     * the root node.
+     */
+    var redraw = function(id, prev, current) {
+
+        var treestructure = draw_data(id, current);
+
+        var keepers = [];
+        var finders = [];
+
+        var t = keepers;
+        treestructure.each(function(e){
+            t.push(e);
+            if(e.selected == current)
+                t = finders;
+        });
+
+        var dropdowns = $$('#'+id + "_container select");
+        dropdowns.each(function(dd){
+            if (!keepers.find(function(e) {return e.selected == dd.value})) {
+                var p;
+                try{
+                    p = dd.parentNode;
+                    p.removeChild(dd);
+                }
+                catch (e) {
+                    console.dir(e);
+                }
+            }
+        });
+
+        finders.each(function(e){
+            add(id, e.list, e.selected);
+        });
+
+    };
+
     var changed = function(id, current) {
         var children = find_in_tree(id, current).children;
 
         var target = $(id);
+
+        var prev = target.value;
+
         target.value = current;
 
+
+        redraw(id,prev, current);
+
         if (children.length) {
-			clear(id);
-			draw(id, current);
             add(id, children, 0);
         }
     };
@@ -87,35 +178,6 @@ var LabelBrowser = (function(){
         });
     };
 
-    var clear = function(id) {
-        var c = $(id + "_container");
-        c.update('');
-    };
-
-    var draw = function(id, current) {
-        var treestructure = [];
-        var curr;
-        if (current > 0) {
-            curr = find_in_tree(id, current);
-            while (true) {
-                if (curr.parentid) {
-                    var par = find_in_tree(id, curr.parentid);
-                    treestructure.push({'list': par.children, 'selected': curr.id});
-                    curr = par;
-                }
-                else {
-                    break;
-                }
-            }
-        }
-        treestructure.push({'list': treedata[id], 'selected': curr ? curr.id : 0});
-
-        treestructure.reverse();
-
-        treestructure.each(function(elem){
-                add(id, elem.list, elem.selected);
-        });
-    };
 
     var register = function(id, current, tree) {
         var target = $(id);
