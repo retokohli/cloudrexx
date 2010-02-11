33f57f7ec6ff230fd3f574410cb5f364d0c3ddb5
lang <lang@comvation.com>
2010-02-11 07:33:51 +0000

    automated language files update (LanguageManager) triggered by Dave for partners module
    git-svn-id: http://svn.contrexxlabs.com/contrexx/trunk@7209 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- lang/de/frontend.php
+++ lang/de/frontend.php
@@ -201,4 +201,5 @@ $_ARRAYLANG['TXT_CSRF_TITLE'] = "Ein Fehler ist aufgetreten";
 $_ARRAYLANG['TXT_CSRF_DESCR'] = "Aus Sicherheitsgründen wurde diese Aktion blockiert. Falls diese Meldung wiederholt auftaucht, versuchen Sie, weniger Browserfenster offen zu halten.";
 $_ARRAYLANG['TXT_CSRF_BUTTON'] = "Klicken Sie hier, um fortzufahren.";
 $_ARRAYLANG['TXT_ACCESS_PLEASE_SELECT'] = "Bitte wählen...";
+$_ARRAYLANG['TXT_PARTNERS_MODULE_DESCRIPTION'] = "Verwaltung und Präsentation von Geschäftspartnern";
 ?>
