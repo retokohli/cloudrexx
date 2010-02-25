cf48dc9af5fff3ad5648eef7d59a17c87a482519
David Vogt <david.vogt@comvation.com>
2010-02-25 09:30:17 +0000

    (AUTOMATIC) update 2_1_4's installer dumps from contrexxlabs_contrexx_2_1_4
    git-svn-id: http://svn.contrexxlabs.com/contrexx/branches/contrexx_2_1@7264 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- installer/data/contrexx_dump_data.sql
+++ installer/data/contrexx_dump_data.sql
@@ -4121,5 +4121,5 @@ INSERT INTO `contrexx_voting_system` (`id`, `date`, `title`, `question`, `status
 		      frontend_lang_id, backend_lang_id, active, profile_access, restore_key,
 		      restore_key_time, u2u_active) 
 	VALUES (1,1,'system','e10adc3949ba59abbe56e057f20f883e',0,0,0,0,0,'','nobody',
-		      1,1,1,'members_only','',0,'0'); 
+		      0,0,1,'members_only','',0,'0'); 
     
