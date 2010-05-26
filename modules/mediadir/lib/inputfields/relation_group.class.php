c10d4158b1a9bce8f6b7eea3234ac0d56d769a7d
Janik Tschanz <janik.tschanz@comvation.com>
2010-05-26 13:24:58 +0000

    selector added
    git-svn-id: http://svn.contrexxlabs.com/contrexx/trunk@7456 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- modules/mediadir/lib/inputfields/relation_group.class.php
+++ modules/mediadir/lib/inputfields/relation_group.class.php
@@ -13,6 +13,7 @@
  */
 require_once ASCMS_MODULE_PATH . '/mediadir/lib/inputfields/inputfield.interface.php';
 require_once ASCMS_MODULE_PATH . '/mediadir/lib/lib.class.php';
+require_once ASCMS_MODULE_PATH . '/mediadir/lib/entry.class.php';
 
 class mediaDirectoryInputfieldRelation_group extends mediaDirectoryLibrary implements inputfield
 {
@@ -56,28 +57,51 @@ class mediaDirectoryInputfieldRelation_group extends mediaDirectoryLibrary imple
                             entry_id=".$intEntryId."
                         LIMIT 1
                     ");
-                    $strValue = intval($objInputfieldValue->fields['value']);
+                    $strValue = htmlspecialchars($objInputfieldValue->fields['value'], ENT_QUOTES, CONTREXX_CHARSET);
                 } else {
                     $strValue = null;
                 }
                 
                 $strFormType = empty($arrInputfield['default_value'][$_LANGID]) ? $arrInputfield['default_value'][0] : $arrInputfield['default_value'][$_LANGID];
                 $arrSelectorOptions = array();
+                $arrValue = explode(",",$strValue);
                 
-                for($i=0;$i <= 20; $i++) {
-                    $arrSelectorOptions['not_selected'] .= '<option  value="'.$i.'">Test '.$i.'</option>';
+                $objEntries = new mediaDirectoryEntry();
+                $objEntries->getEntries();
+                
+                foreach($objEntries->arrEntries as $intKey => $arrEntry) {
+                	if(in_array($intKey,$arrValue)) {
+                		$arrSelectorOptions['selected'][] = '<option  value="'.$arrEntry['entryId'].'">'.$arrEntry['entryFields'][0].'</option>';
+                    } else {
+                        $arrSelectorOptions['not_selected'][] = '<option  value="'.$arrEntry['entryId'].'">'.$arrEntry['entryFields'][0].'</option>';
+                	}
                 }
+                
+                asort($arrSelectorOptions['selected']);
+                asort($arrSelectorOptions['not_selected']);
+                
+                $strSelectorSelected = join("", $arrSelectorOptions['selected']);
+                $strSelectorNotSelected = join("", $arrSelectorOptions['not_selected']);
                 
                 $strInputfield .= <<<EOF
 <script language="JavaScript" type="text/javascript">
 /* <![CDATA[ */
 
-function sayHallo(){
-    searchElement("Hallo Chrigu");
-}
+function searchElement(elementId, term){
+    elmSelector = document.getElementById(elementId);
 
-function searchElement(term){
-    alert(term);
+    var pattern = term.toLowerCase()
+    var reg = new RegExp(pattern);
+    
+    for (i = 0; i < elmSelector.length; ++i) {
+        var text = elmSelector.options[i].text.toLowerCase()
+    
+        if (text.match(reg)) {
+            elmSelector.options[i].selected = true;
+        } else {
+            elmSelector.options[i].selected = false;
+        }
+    }
 }
 
 /* ]]> */
@@ -86,27 +110,18 @@ EOF;
                 
                 $strInputfield .= '<div class="'.$this->moduleName.'Selector" style="float: left; height: auto !important;">';
                 $strInputfield .= '<div class="'.$this->moduleName.'SelectorLeft" style="float: left; height: auto !important;"><select id="'.$this->moduleName.'Inputfield_deselected_'.$intId.'" name="'.$this->moduleName.'Inputfield[deselected_'.$intId.'][]" size="12" multiple="multiple" style="width: 180px;">';
-                $strInputfield .= $arrSelectorOptions['not_selected'];
-                $strInputfield .= '</select><br /><input class="'.$this->moduleName.'SelectorSearch" type="text" onclick="this.value=\'\';" onkeyup="searchElement(this.value);" value="Suchbegriff..."  style="width: 178px;"/></div>';
+                $strInputfield .= $strSelectorNotSelected;
+                $strInputfield .= '</select><br /><input class="'.$this->moduleName.'SelectorSearch" type="text" onclick="this.value=\'\';" onkeyup="searchElement(\''.$this->moduleName.'Inputfield_deselected_'.$intId.'\', this.value);" value="Suchbegriff..."  style="width: 178px;"/></div>';
                 $strInputfield .= '<div class="'.$this->moduleName.'SelectorCenter" style="float: left; height: 100px; padding: 60px 10px 0px 10px;">';
                 $strInputfield .= '<input style="width: 40px; min-width: 40px;" value=" &gt;&gt; " name="addElement" onclick="moveElement(document.entryModfyForm.elements[\''.$this->moduleName.'Inputfield_deselected_'.$intId.'\'],document.entryModfyForm.elements[\''.$this->moduleName.'Inputfield_'.$intId.'\'],addElement,removeElement);" type="button">';
                 $strInputfield .= '<br />';
                 $strInputfield .= '<input style="width: 40px; min-width: 40px;" value=" &lt;&lt; " name="removeElement" onclick="moveElement(document.entryModfyForm.elements[\''.$this->moduleName.'Inputfield_'.$intId.'\'],document.entryModfyForm.elements[\''.$this->moduleName.'Inputfield_deselected_'.$intId.'\'],removeElement,addElement);" type="button">';
                 $strInputfield .= '</div>';
                 $strInputfield .= '<div class="'.$this->moduleName.'SelectorRight" style="float: left; height: auto !important;"><select id="'.$this->moduleName.'Inputfield_'.$intId.'" name="'.$this->moduleName.'Inputfield['.$intId.'][]" size="12" multiple="multiple" style="width: 180px;">';
-                $strInputfield .= $arrSelectorOptions['selected'];
-                $strInputfield .= '</select></div>';
+                $strInputfield .= $strSelectorSelected;
+                $strInputfield .= '</select><br /><input class="'.$this->moduleName.'SelectorNew" style="float: right;" type="button" value="'.$_ARRAYLANG['TXT_MEDIADIR_ADD_ENTRY'].'"/></div>';
                 $strInputfield .= '</div>';
                 
-                $strInputfield .= <<<EOF
-<script language="JavaScript" type="text/javascript">
-/* <![CDATA[ */
-
-sayHallo();
-
-/* ]]> */
-</script>
-EOF;
                 return $strInputfield;
 
                 break;
