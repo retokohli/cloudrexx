e6b471ba24dba279995e0b1d50c2d54a74b1f1e1
lang <lang@comvation.com>
2010-07-20 12:11:14 +0000

    automated language files update (LanguageManager) triggered by dahasvn
    git-svn-id: http://svn.contrexxlabs.com/contrexx/trunk@7923 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- core_modules/access/lang/de/frontend.php
+++ core_modules/access/lang/de/frontend.php
@@ -75,7 +75,7 @@ $_ARRAYLANG['TXT_ACCESS_YOUR_ACCOUNT_SUCCSESSFULLY_DELETED'] = "Ihr Konto wurde 
 $_ARRAYLANG['TXT_ACCESS_SAVE'] = "Speichern";
 $_ARRAYLANG['TXT_ACCESS_USER_ACCOUNT_STORED_SUCCESSFULLY'] = "Das Benutzerkonto wurde erfolgreich gespeichert";
 $_ARRAYLANG['TXT_ACCESS_CAPTCHA_DESCRIPTION'] = "Tippen Sie bitte den Sicherheitscode im dargestellten Bild in das Textfeld ein. Dies ist notwendig, damit das System Sie als natürliche Person identifizieren kann.";
-$_ARRAYLANG['TXT_ACCESS_INVALID_CAPTCHA_CODE'] = "Der eingegebene Sicerheitscode war falsch!";
+$_ARRAYLANG['TXT_ACCESS_INVALID_CAPTCHA_CODE'] = "Der eingegebene Sicherheitscode war falsch!";
 $_ARRAYLANG['TXT_ACCESS_ACCEPT_TOS'] = "Ich bestätige die Richtigkeit der gemachten Angaben und die <a href=\"%s\" target=\"_blank\" title=\"Geschäftsbedingungen\">Geschäftsbedingungen</a> zu kennen und akzeptiere diese.";
 $_ARRAYLANG['TXT_ACCESS_TOS'] = "Allgemeine Geschäftsbedingungen";
 $_ARRAYLANG['TXT_ACCESS_TOS_NOT_CHECKED'] = "Sie müssen die Allgemeinen Geschäftsbedingungen akzeptieren!";
