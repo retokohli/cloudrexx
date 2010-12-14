cb4da1d0c3b34d28e1b20cc6bc903c1ec94af107
lang <lang@comvation.com>
2010-12-14 20:01:28 +0000

    automated language files update (LanguageManager) triggered by cron(root)
    git-svn-id: http://svn.contrexxlabs.com/contrexx/trunk@8746 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- modules/elearning/lang/en/frontend.php
+++ modules/elearning/lang/en/frontend.php
@@ -7,29 +7,29 @@
  * @subpackage  module_elearning
  */
 $_ARRAYLANG['TXT_ELEARNING_MIN_POINTS'] = "Min. benötigte Punkte";
-$_ARRAYLANG['TXT_ELEARNING_ANSWERED'] = "Beantwortete Prüfungsfragen";
+$_ARRAYLANG['TXT_ELEARNING_ANSWERED'] = "Beantwortete Selbstevaluationsfragen";
 $_ARRAYLANG['TXT_ELEARNING_POINTS'] = "Punkte";
 $_ARRAYLANG['TXT_ELEARNING_LEXICON'] = "Lexikon";
 $_ARRAYLANG['TXT_ELEARNING_SAVE_LEXICON_ERROR'] = "Fehler beim Speichern des Lexikoneintrages.";
 $_ARRAYLANG['TXT_ELEARNING_QUESTIONS_COUNT'] = "Anzahl Fragen";
 $_ARRAYLANG['TXT_ELEARNING_SEARCH_TERM'] = "Begriff suchen";
 $_ARRAYLANG['TXT_ELEARNING_EVALUATION'] = "Auswertung";
-$_ARRAYLANG['TXT_ELEARNING_EXAM'] = "Prüfung";
+$_ARRAYLANG['TXT_ELEARNING_EXAM'] = "Selbstevaluation";
 $_ARRAYLANG['TXT_ELEARNING_HELP'] = "Hilfe";
 $_ARRAYLANG['TXT_ELEARNING_INCORRECT_UNIT_ERROR'] = "Die Lerneinheit ist nicht dem aktuellen Kurs zugeordnet oder die Lerneinheit existiert nicht.";
 $_ARRAYLANG['TXT_ELEARNING_UNKNOWN_UNIT_ERROR'] = "Die Lerneinheit ist nicht bekannt.";
 $_ARRAYLANG['TXT_ELEARNING_ALL'] = "Alle";
-$_ARRAYLANG['TXT_ELEARNING_START_EXAM'] = "Prüfung starten";
+$_ARRAYLANG['TXT_ELEARNING_START_EXAM'] = "Selbstevaluation starten";
 $_ARRAYLANG['TXT_ELEARNING_NEXT'] = "weiter";
 $_ARRAYLANG['TXT_ELEARNING_EXAM_RUN'] = "Durchgang";
 $_ARRAYLANG['TXT_ELEARNING_EXAM_RUNS'] = "Durchgänge";
 $_ARRAYLANG['TXT_ELEARNING_MAX_EXAM_RUNS'] = "Max. erlaubte Durchgänge";
-$_ARRAYLANG['TXT_ELEARNING_EXAM_QUESTIONS_COUNT'] = "Anzahl Prüfungsfragen";
+$_ARRAYLANG['TXT_ELEARNING_EXAM_QUESTIONS_COUNT'] = "Anzahl Selbstevaluationsfragen";
 $_ARRAYLANG['TXT_ELEARNING_UNDEFINED_STATUS'] = "Nicht gestartet";
 $_ARRAYLANG['TXT_ELEARNING_STARTED_STATUS'] = "Gestartet";
 $_ARRAYLANG['TXT_ELEARNING_PASSED_STATUS'] = "Bestanden";
 $_ARRAYLANG['TXT_ELEARNING_NOT_PASSED_STATUS'] = "Nicht bestanden";
-$_ARRAYLANG['TXT_ELEARNING_EXAM_STATUS'] = "Prüfungs-Status";
+$_ARRAYLANG['TXT_ELEARNING_EXAM_STATUS'] = "Selbstevaluations-Status";
 $_ARRAYLANG['TXT_ELEARNING_VOCABULARY'] = "Wortschatz";
 $_ARRAYLANG['TXT_ELEARNING_DOWNLOADS'] = "Downloads";
 ?>
