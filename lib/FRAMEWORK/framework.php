d583fcedbc536abfd27c7a49626c4c51bd482936
Severin Raez <severin.raez@comvation.com>
2011-02-15 11:36:44 +0000

    core-framework(enhancement): Link to SQL class
    git-svn-id: http://svn.contrexxlabs.com/contrexx/branches/contrexx_2_1@9493 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- lib/FRAMEWORK/framework.php
+++ lib/FRAMEWORK/framework.php
@@ -15,4 +15,5 @@ require_once dirname(__FILE__).'/Validator.class.php';
 require_once dirname(__FILE__).'/Language.class.php';
 // require_once dirname(__FILE__).'/CsvCreator.class.php';
 require_once dirname(__FILE__).'/FWUser.class.php';
+require_once dirname(__FILE__).'/SQL.class.php';
 ?>
