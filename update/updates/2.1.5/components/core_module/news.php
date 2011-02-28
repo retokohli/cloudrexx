7a2fb296f6f71aff8d8d458a58b0f02229546212
Thomas Däppen <thomas.daeppen@cloudrexx.com>
2011-02-28 07:23:18 +0000

    news (update bugfix): minor sql + syntax bugfix in update script
    git-svn-id: http://svn.contrexxlabs.com/contrexx/branches/contrexx_2_1@9676 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- update/updates/2.1.5/components/core_module/news.php
+++ update/updates/2.1.5/components/core_module/news.php
@@ -248,18 +248,18 @@ function _newsUpdate() {
     //encoding was a little messy in 2.1.4. convert titles and teasers to their raw representation
     if($_CONFIG['coreCmsVersion'] == "2.1.4") {
         try{
-            $res = UpdateUtil::sql('SELECT id, title, teaser_text from '.DBPREFIX.'module_news');
+            $res = UpdateUtil::sql('SELECT `id`, `title`, `teaser_text` FROM `'.DBPREFIX.'module_news` WHERE `changelog` > '.mktime(0,0,0,12,15,2010));
             while($res->MoveNext()) {
                 $title = $res->fields['title'];
                 $teaserText = $res->fields['teaser_text'];
-                $id = $res->id['title'];
+                $id = $res->fields['id'];
 
                 //title is html entity style
-                $title = html_entity_decode($title, ENT_QUOTES, CONTREXX_CHARSET));
+                $title = html_entity_decode($title, ENT_QUOTES, CONTREXX_CHARSET);
                 //teaserText is html entity style, but no contrexx was specified on encoding
                 $teaserText = html_entity_decode($teaserText);
 
-                UpdateUtil::sql('UPDATE '.DBPREFIX.'module_news SET title="'.addslashes($title).'" teaser_text="'.addslashes($teaserText).'" where id='.$id);
+                UpdateUtil::sql('UPDATE `'.DBPREFIX.'module_news` SET `title`="'.addslashes($title).'", `teaser_text`="'.addslashes($teaserText).'" where `id`='.$id);
             }
         }
         catch (UpdateException $e) {
