b904c033385ba640e9d1c851f85236c2f410755a
Ueli Kramer <ueli.kramer@comvation.com>
2011-03-04 13:10:56 +0000

    Ueli Kramer - Unnötige Erweiterung löschen
    git-svn-id: http://svn.contrexxlabs.com/contrexx/branches/contrexx_2_1@9845 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- modules/livecam/index.class.php
+++ modules/livecam/index.class.php
@@ -305,9 +305,6 @@ class Livecam extends LivecamLibrary
             $this->_objTpl->parse('livecamArchive');
         } else {
             $this->statusMessage = $_ARRAYLANG['TXT_LIVECAM_NO_PICTURES_OF_SELECTED_DAY'];
-            $this->_objTpl->setVariable(array(
-                'LIVECAM_STATUSMESSAGE' => $this->statusMessage
-            ));
         }
     }
 
