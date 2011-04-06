ae4a309fc6a5e55bdba89565ee26c00a6fc7ab62
Reto Kohli <reto.kohli@comvation.com>
2011-04-06 13:11:57 +0000

    lib/FRAMEWORK/Javascript.class.php - Added code for jQueryUI and jcrop (still commented out -- coming soon) lib/FRAMEWORK/User/User.class.php lib/FRAMEWORK/User/User_Profile_Attribute.class.php lib/FRAMEWORK/User/UserGroup.class.php - Formatted for readability (wrapped long lines)
    git-svn-id: http://svn.contrexxlabs.com/contrexx/trunk@10643 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- lib/FRAMEWORK/Javascript.class.php
+++ lib/FRAMEWORK/Javascript.class.php
@@ -1,12 +1,24 @@
 <?php
-//this is needed for ContrexxJS (activated with 'cx')
-require_once ASCMS_LIBRARY_PATH.'/FRAMEWORK/ContrexxJavascript.php';
+
 /**
  * Javascript
  * @author      Stefan Heinemann <sh@comvation.com>
  * @copyright   CONTREXX CMS - COMVATION AG
  * @package     contrexx
- * @subpackage  module_feed
+ * @subpackage  framework
+ * @todo        Edit PHP DocBlocks!
+ */
+
+// TODO: This is awkward here!  Find a way to include it just when it's needed.
+// Required by ContrexxJS (activated with 'cx')
+require_once ASCMS_LIBRARY_PATH.'/FRAMEWORK/ContrexxJavascript.php';
+
+/**
+ * Javascript
+ * @author      Stefan Heinemann <sh@comvation.com>
+ * @copyright   CONTREXX CMS - COMVATION AG
+ * @package     contrexx
+ * @subpackage  framework
  * @todo        Edit PHP DocBlocks!
  */
 class JS
@@ -35,7 +47,6 @@ class JS
 
     /**
      * Holding the last error
-     *
      * @access private
      * @static
      * @var string
@@ -109,8 +120,59 @@ class JS
             ),
             'specialcode'  => 'var $J = jQuery.noConflict();',
         ),
-        'cx'         => array(
-            'jsfiles'      => array(
+        'jquery-tools' => array(
+            'jsfiles' => array(
+                'lib/javascript/jquery/tools/jquery.tools.min.js',
+            ),
+            'dependencies' => array('jquery')
+        ),
+/*
+Coming soon
+        'jqueryui'     => array(
+            'jsfiles'       => array(
+                'lib/javascript/jqueryUI/js/jquery-ui-1.8.2.custom.min.js'
+            ),
+            'cssfiles'      => array(
+                'lib/javascript/jqueryUI/css/blitzer/jquery-ui-1.8.2.custom.css',
+            ),
+            'dependencies'  => array(
+                'jquery',
+            ),
+        ),
+*/
+/*
+Coming soon
+        'jcrop' => array(
+            'jsfiles'       => array(
+                'lib/javascript/jcrop/js/jquery.Jcrop.min.js'
+            ),
+            'cssfiles'      => array(
+                'lib/javascript/jcrop/css/jquery.Jcrop.css',
+            ),
+            'dependencies'  => array(
+                'jquery',
+            ),
+            // When invoking jcrop, add code like this to create the widget:
+            // jQuery(window).load(function(){
+            //   jQuery("#my_image").Jcrop({ [option: value, ...] });
+            // });
+            // where option may be any of
+            // aspectRatio   decimal
+            //    Aspect ratio of w/h (e.g. 1 for square)
+            // minSize       array [ w, h ]
+            //    Minimum width/height, use 0 for unbounded dimension
+            // maxSize       array [ w, h ]
+            //    Maximum width/height, use 0 for unbounded dimension
+            // setSelect     array [ x, y, x2, y2 ]
+            //    Set an initial selection area
+            // bgColor       color value
+            //    Set color of background container
+            // bgOpacity     decimal 0 - 1
+            //    Opacity of outer image when cropping
+        ),
+*/
+        'cx' => array(
+            'jsfiles' => array(
                 'lib/javascript/cx/contrexxJs.js',
                 'lib/javascript/cx/contrexxJs-tools.js',
                 'lib/javascript/jquery/jquery.includeMany-1.2.2.min.js' //to dynamically include javascript files
@@ -118,17 +180,10 @@ class JS
             'dependencies' => array('jquery')
             //we insert the specialCode for the Contrexx-API later in getCode()
         ),
-        'jquery-tools'     => array(
-            'jsfiles'       => array(
-                'lib/javascript/jquery/tools/jquery.tools.min.js',
-            ),
-            'dependencies' => array('jquery')
-        ),
     );
 
     /**
      * Holds the custom JS files
-     *
      * @static
      * @access private
      * @var array
@@ -137,7 +192,6 @@ class JS
 
     /**
      * The custom CSS files
-     *
      * @static
      * @access private
      * @var array
@@ -146,7 +200,6 @@ class JS
 
     /**
      * The custom Code
-     *
      * @static
      * @access private
      * @var array
@@ -162,8 +215,7 @@ class JS
     private static $shadowBoxPlayers = array('img', 'swf', 'flv', 'qt', 'wmp', 'iframe','html');
 
     /**
-     * The language of the shadobox to be used
-     *
+     * The language of the shadowbox to be used
      * @access private
      * @static
      * @var string
@@ -179,6 +231,10 @@ class JS
      */
     private static $registeredJsFiles = array();
 
+    private static $re_name_postfix = 1;
+    private static $comment_dict = array();
+
+
     /**
      * Set the offset parameter
      * @param string
@@ -190,9 +246,9 @@ class JS
         if (!preg_match('/\/$/', $offset)) {
             $offset .= '/';
         }
-
         self::$offset = $offset;
     }
+
 
     /**
      * Activate an available js file
@@ -200,11 +256,11 @@ class JS
      * The options parameter is specific for the chosen
      * library. The library must define callback methods for
      * the options to be used.
-     * @param string $name
-     * @param array $options
-     * @param bool $dependencies
      * @access public
      * @static
+     * @param  string  $name
+     * @param  array   $options
+     * @param  bool    $dependencies
      * @return bool
      */
     public static function activate($name, $options = null, $dependencies = true)
@@ -215,38 +271,33 @@ class JS
                 an available javascript type';
             return false;
         }
-
         $data = self::$available[$name];
-		if (!empty($data['ref'])) {
-			$name = $data['ref'];
-			if (array_key_exists($name, self::$available)) {
-				$data = self::$available[$name];
-			} else {
-				self::$error = $name.' unknown reference';
-				return false;
-			}
-		}
-
+        if (!empty($data['ref'])) {
+            $name = $data['ref'];
+            if (array_key_exists($name, self::$available)) {
+                $data = self::$available[$name];
+            } else {
+                self::$error = $name.' unknown reference';
+                return false;
+            }
+        }
         if (!empty($data['dependencies']) && $dependencies) {
             foreach ($data['dependencies'] as $dep) {
                 self::activate($dep);
             }
         }
-
         if (isset($data['loadcallback']) && isset($options)) {
             self::$data['loadcallback']($options);
         }
-
         if (array_search($name, self::$active) === false) {
             self::$active[] = $name;
         }
-
         return true;
     }
 
+
     /**
      * Deactivate a previously activated js file
-     *
      * @param string $name
      * @access public
      * @static
@@ -263,9 +314,9 @@ class JS
             return false;
         }
         unset(self::$active[$searchResult]);
-
         return true;
     }
+
 
     /**
      * Register a custom js file
@@ -289,19 +340,15 @@ class JS
             self::activate($basename);
             return true;
         }
-
         if (!preg_match('#^https?://#', $file)) {
             if (!file_exists(($file[0] == '/' ? ASCMS_PATH : ASCMS_DOCUMENT_ROOT.'/').$file)) {
                 self::$error .= "The file ".$file." doesn't exist\n";
                 return false;
             }
         }
-
         if (array_search($file, self::$customJS) === false) {
-
             self::$customJS[] = $file;
         }
-
         return true;
     }
 
@@ -324,13 +371,12 @@ class JS
         if (array_search($file, self::$customCSS) === false) {
             self::$customCSS[] = $file;
         }
-
         return true;
     }
 
+
     /**
      * Register special code
-     *
      * Add special code to the List
      * @static
      * @access public
@@ -343,14 +389,12 @@ class JS
         if (array_search($code, self::$customCode) === false) {
             self::$customCode[] = $code;
         }
-
         return true;
     }
 
 
     /**
      * Return the code for the placeholder
-     *
      * @access public
      * @static
      * @return string
@@ -361,47 +405,40 @@ class JS
         $cssfiles = array();
         $specialcode = array();
         $retstring  = '';
-
         if (count(self::$active) > 0) {
             foreach (self::$active as $name) {
                 $data = self::$available[$name];
                 if (!isset($data['jsfiles'])) {
-                    self::$error = "A JS entry should at least contain
-                        one js file...";
+                    self::$error = "A JS entry should at least contain one js file...";
                     return false;
                 }
                 $retstring .= self::makeJSFiles($data['jsfiles']);
-
                 if (!empty($data['cssfiles'])) {
                     $cssfiles = array_merge($cssfiles, $data['cssfiles']);
                 }
-
                 if (isset($data['specialcode']) && strlen($data['specialcode']) > 0) {
                     $retstring .= self::makeSpecialCode(array($data['specialcode']));
                 }
-
                 if (isset($data['makecallback'])) {
                     self::$data['makecallback']();
                 }
-
-                //special case contrexx-API: fetch specialcode if activated
-                if($name == 'cx') {
-                    $retstring .= self::makeSpecialCode(array(ContrexxJavascript::getInstance()->initJs()));
+                // Special case contrexx-API: fetch specialcode if activated
+                if ($name == 'cx') {
+                    $retstring .= self::makeSpecialCode(
+                        array(ContrexxJavascript::getInstance()->initJs()));
                 }                  
             }
         }
-
         $retstring .= self::makeJSFiles(self::$customJS);
         $retstring .= self::makeCSSFiles($cssfiles);
         $retstring .= self::makeCSSFiles(self::$customCSS);
         $retstring .= self::makeSpecialCode(self::$customCode);
-
         return $retstring;
     }
 
+
     /**
      * Return the last error
-     *
      * @return string
      * @static
      * @access public
@@ -411,9 +448,9 @@ class JS
         return self::$error;
     }
 
+
     /**
      * Return the available libs
-     *
      * @access public
      * @static
      * @return array
@@ -426,7 +463,6 @@ class JS
 
     /**
      * Make the code for the Javascript files
-     *
      * @param array $files
      * @return string
      * @static
@@ -437,19 +473,18 @@ class JS
         $code = "";
 
         foreach ($files as $file) {
-            //file has already been added to the js list
-            if(array_search($file, self::$registeredJsFiles) !== false)
+            // The file has already been added to the js list
+            if (array_search($file, self::$registeredJsFiles) !== false)
                 continue;
             self::$registeredJsFiles[] = $file;
             $code .= "<script type=\"text/javascript\" src=\"".self::$offset.$file."\"></script>\n\t";
         }
-
         return $code;
     }
 
+
     /**
      * Make the code for the CSS files
-     *
      * @param array $files
      * @return string
      * @static
@@ -458,17 +493,15 @@ class JS
     private static function makeCSSFiles($files)
     {
         $code = "";
-
         foreach ($files as $file) {
             $code .= "<link rel=\"stylesheet\" type=\"text/css\" href=\"".self::$offset.$file."\" />\n\t";
         }
-
         return $code;
     }
 
+
     /**
      * Make the code section for
-     *
      * @access private
      * @param array $code
      * @return string
@@ -479,16 +512,14 @@ class JS
         $retcode = "";
         if (!empty($code)) {
             $retcode .= "<script type=\"text/javascript\">\n/* <![CDATA[ */\n";
-
             foreach ($code as $segment) {
-                $retcode .= $segment."\n\t";
+                $retcode .= $segment."\n";
             }
-
             $retcode .= "\n/* ]]> */\n</script>\n";
         }
-
         return $retcode;
     }
+
 
     /**
      * Callback function for the shadowbox library
@@ -542,6 +573,7 @@ class JS
         }
     }
 
+
     /**
      * Callback function for the shadowbox library
      *
@@ -570,7 +602,7 @@ class JS
         }
 
         // make the code for loading the skins
-        $skindir = "lib/javascript/shadowbox/skin";
+        $skindir = self::$offset."lib/javascript/shadowbox/skin";
         $skin = 'standard';
         if ($objInit->mode == "frontend") {
             $themePath = $objInit->getCurrentThemesPath();
@@ -579,8 +611,7 @@ class JS
             }
         }
         self::$customCode[] = "Shadowbox.loadSkin('".$skin."', '".self::$offset.$skindir."');";
-
-        // make the code for loading the language
+        // Make the code for loading the language
         self::$customCode[] = "Shadowbox.loadLanguage('".self::$shadowBoxLanguage."', '".self::$offset."lib/javascript/shadowbox/lang')";
     }
 
@@ -599,46 +630,50 @@ class JS
      * @param string $content - Reference to the HTML content. Note that it
      *                          WILL be modified in-place.
      */
-    public function findJavascripts(&$content) {
+    public function findJavascripts(&$content)
+    {
         JS::grabComments($content);
         $content = preg_replace_callback('/<script .*?src=(?:"|\')([^"\']*)(?:"|\').*?\/?>(?:<\/script>)?/i', array('JS', 'registerFromRegex'), $content);
         JS::restoreComments($content);
     }
 
 
-    private static $comment_dict = array();
     /**
      * Grabs all comments in the given HTML and replaces them with a
      * temporary string. Modifies the given HTML in-place.
      * @param string $content
      */
-    private static function grabComments(&$content) {
+    private static function grabComments(&$content)
+    {
         $content = preg_replace_callback('#<!--.*?-->#ms', array('JS', '_storeComment'), $content);
     }
+
 
     /**
      * Restores all grabbed comments (@see JS::grabComments()) and
      * puts them back in the given content. Modifies the given HTML in-place.
      * @param string $content
      */
-    private static function restoreComments(&$content) {
+    private static function restoreComments(&$content)
+    {
         krsort(self::$comment_dict);
         foreach (self::$comment_dict as $key => $value) {
             $content = str_replace($key, $value, $content);
         }
     }
 
-    private static $re_name_postfix = 1;
+
     /**
      * Internal helper for replacing comments. @see JS::grabComments()
      */
-    private static function _storeComment($re) {
-
+    private static function _storeComment($re)
+    {
         $name = 'saved_comment_'.self::$re_name_postfix;
         self::$comment_dict[$name] = $re[0];
         self::$re_name_postfix++;
         return $name;
     }
+
 }
 
 ?>
