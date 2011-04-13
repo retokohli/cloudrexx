c1b945bee12a220096b246094e3a3ba63002103b
Thomas Däppen <thomas.daeppen@cloudrexx.com>
2011-04-13 08:54:08 +0000

    alias (update): extended update script for multi language support
    git-svn-id: http://svn.contrexxlabs.com/contrexx/trunk@10712 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- update/updates/2.2/components/core_module/alias.php
+++ update/updates/2.2/components/core_module/alias.php
@@ -1,5 +1,7 @@
 <?php
 function _aliasUpdate() {
+    global $objUpdate;
+
     try {
         UpdateUtil::table(
             DBPREFIX.'module_alias_source',
@@ -27,6 +29,15 @@ function _aliasUpdate() {
                 'url'    => array('fields' => array('url'), 'type' => 'UNIQUE')
             )
         );
+
+        // Since version 2.2, aliases are unique within a language. Therefore every alias must be associated to its content page's language.
+        if ($objUpdate->_isNewerVersion($_CONFIG['coreCmsVersion'], '2.2.0')) {
+            UpdateUtil::sql('UPDATE `'.DBPREFIX.'module_alias_source` AS tblAS
+                         INNER JOIN `'.DBPREFIX.'module_alias_target` AS tblAT ON tblAT.`id` = tblAS.`target_id`
+                         INNER JOIN `'.DBPREFIX.'content_navigation`  AS tblN  ON tblN.`catid` = tblAT.`url`
+                                SET tblAS.`lang_id` = tblN.`lang`
+                              WHERE tblAT.`type` = \'local\'');
+        }
     }
     catch (UpdateException $e) {
         return UpdateUtil::DefaultActionHandler($e);
