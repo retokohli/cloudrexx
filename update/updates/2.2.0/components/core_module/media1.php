969bfee21d30fa342c9d20320006c0921f82ecb4
Severin Raez <severin.raez@comvation.com>
2011-04-19 12:08:12 +0000

    media(bugfix): renamed update file
    git-svn-id: http://svn.contrexxlabs.com/contrexx/trunk@10856 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- update/updates/2.2.0/components/core_module/media1.php
+++ update/updates/2.2.0/components/core_module/media1.php
@@ -4,7 +4,7 @@ function _mediaUpdate()
 {
     global $_ARRAYLANG, $_CORELANG;
 
-	require_once ASCMS_FRAMEWORK_PATH.'/File.class.php';
+    /*	require_once ASCMS_FRAMEWORK_PATH.'/File.class.php';
 	$objFile = new File();
 
     $paths = glob(ASCMS_DOCUMENT_ROOT.'/media/archive*');
@@ -17,7 +17,7 @@ function _mediaUpdate()
             setUpdateMsg(sprintf($_ARRAYLANG['TXT_SET_WRITE_PERMISSON_TO_DIR_AND_CONTENT'], "<pre>$web_path</pre>", $_CORELANG['TXT_UPDATE_TRY_AGAIN']));
             return false;
         }
-    }
+        }*/
 
     try {
         UpdateUtil::table(
