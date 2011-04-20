c2665767ef47e468391825e7eb634725fe838d32
Thomas Däppen <thomas.daeppen@cloudrexx.com>
2011-04-20 08:46:10 +0000

    update (calendar): added default value for contrexx_module_calendar.series_pattern_begin
    git-svn-id: http://svn.contrexxlabs.com/contrexx/trunk@10884 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- update/updates/2.2.0/components/module/calendar.php
+++ update/updates/2.2.0/components/module/calendar.php
@@ -1,84 +1,70 @@
 <?php
 function _calendarUpdate()
 {
-	global $objDatabase, $_ARRAYLANG;
-
-	$arrColumns = $objDatabase->MetaColumns(DBPREFIX.'module_calendar');
-	if ($arrColumns === false) {
-		setUpdateMsg(sprintf($_ARRAYLANG['TXT_UNABLE_GETTING_DATABASE_TABLE_STRUCTURE'], DBPREFIX.'module_calendar'));
-		return false;
-	}
-
-
-	$arrNewCols = array(
-	   'series_status' => array(
-			'type' 		=> 'TINYINT',
-			'after' 	=> 'notification_address',
-    	),
-    	'series_type' => array(
-			'type' 		=> 'INT (11)',
-			'after' 	=> 'series_status',
-    	),
-    	'series_pattern_count' => array(
-			'type' 		=> 'INT',
-			'after' 	=> 'series_type',
-    	),
-    	'series_pattern_weekday' => array(
-			'type' 		=> 'VARCHAR(7)',
-			'after' 	=> 'series_pattern_count',
-    	),
-    	'series_pattern_day' => array(
-			'type' 		=> 'INT',
-			'after' 	=> 'series_pattern_weekday',
-    	),
-    	'series_pattern_week' => array(
-			'type' 		=> 'INT',
-			'after' 	=> 'series_pattern_day',
-    	),
-    	'series_pattern_month' => array(
-			'type' 		=> 'INT',
-			'after' 	=> 'series_pattern_week',
-    	),
-    	'series_pattern_type' => array(
-			'type' 		=> 'INT',
-			'after' 	=> 'series_pattern_month',
-    	),
-    	'series_pattern_dourance_type' => array(
-			'type' 		=> 'INT',
-			'after' 	=> 'series_pattern_type',
-    	),
-    	'series_pattern_end' => array(
-			'type' 		=> 'INT',
-			'after' 	=> 'series_pattern_dourance_type',
-    	),
-    	'series_pattern_begin' => array(
-			'type' 		=> 'INT',
-			'after' 	=> 'series_pattern_end',
-    	),
-    	'series_pattern_exceptions' => array(
-			'type' 		=> 'longtext',
-			'after' 	=> 'series_pattern_begin',
-    	)
-	);
-
-	foreach ($arrNewCols as $col => $arrAttr) {
-		if (!array_key_exists(strtoupper($col), $arrColumns)) {
-		    $query = "ALTER TABLE `".DBPREFIX."module_calendar` ADD `".$col."` ".$arrAttr['type']." NOT NULL AFTER `".$arrAttr['after']."`";
-		    if ($objDatabase->Execute($query) === false) {
-				return _databaseError($query, $objDatabase->ErrorMsg());
-			}
-		}
-	}
+	global $objDatabase;
 
     try{
-        // delete obsolete table  contrexx_module_calendar_access
-        UpdateUtil::drop_table(DBPREFIX.'module_calendar_access');
+        UpdateUtil::table(
+            DBPREFIX.'module_calendar',
+            array(
+                'id'                                 => array('type' => 'INT(11)', 'notnull' => true, 'primary' => true, 'auto_increment' => true),
+                'active'                             => array('type' => 'TINYINT(1)', 'notnull' => true, 'default' => '1', 'after' => 'id'),
+                'catid'                              => array('type' => 'INT(11)', 'notnull' => true, 'default' => '0', 'after' => 'active'),
+                'startdate'                          => array('type' => 'INT(14)', 'notnull' => false, 'after' => 'catid'),
+                'enddate'                            => array('type' => 'INT(14)', 'notnull' => false, 'after' => 'startdate'),
+                'priority'                           => array('type' => 'INT(1)', 'notnull' => true, 'default' => '3', 'after' => 'enddate'),
+                'access'                             => array('type' => 'INT(1)', 'notnull' => true, 'default' => '0', 'after' => 'priority'),
+                'name'                               => array('type' => 'VARCHAR(100)', 'notnull' => true, 'default' => '', 'after' => 'access'),
+                'comment'                            => array('type' => 'text', 'after' => 'name'),
+                'placeName'                          => array('type' => 'VARCHAR(255)', 'after' => 'comment'),
+                'link'                               => array('type' => 'VARCHAR(255)', 'notnull' => true, 'default' => 'http://', 'after' => 'placeName'),
+                'pic'                                => array('type' => 'VARCHAR(255)', 'notnull' => true, 'default' => '', 'after' => 'link'),
+                'attachment'                         => array('type' => 'VARCHAR(255)', 'notnull' => true, 'default' => '', 'after' => 'pic'),
+                'placeStreet'                        => array('type' => 'VARCHAR(255)', 'notnull' => true, 'default' => '', 'after' => 'attachment'),
+                'placeZip'                           => array('type' => 'VARCHAR(255)', 'notnull' => true, 'default' => '', 'after' => 'placeStreet'),
+                'placeCity'                          => array('type' => 'VARCHAR(255)', 'notnull' => true, 'default' => '', 'after' => 'placeZip'),
+                'placeLink'                          => array('type' => 'VARCHAR(255)', 'notnull' => true, 'default' => '', 'after' => 'placeCity'),
+                'placeMap'                           => array('type' => 'VARCHAR(255)', 'notnull' => true, 'default' => '', 'after' => 'placeLink'),
+                'organizerName'                      => array('type' => 'VARCHAR(255)', 'notnull' => true, 'default' => '', 'after' => 'placeMap'),
+                'organizerStreet'                    => array('type' => 'VARCHAR(255)', 'notnull' => true, 'default' => '', 'after' => 'organizerName'),
+                'organizerZip'                       => array('type' => 'VARCHAR(255)', 'notnull' => true, 'default' => '', 'after' => 'organizerStreet'),
+                'organizerPlace'                     => array('type' => 'VARCHAR(255)', 'notnull' => true, 'default' => '', 'after' => 'organizerZip'),
+                'organizerMail'                      => array('type' => 'VARCHAR(255)', 'notnull' => true, 'default' => '', 'after' => 'organizerPlace'),
+                'organizerLink'                      => array('type' => 'VARCHAR(255)', 'notnull' => true, 'default' => '', 'after' => 'organizerMail'),
+                'key'                                => array('type' => 'VARCHAR(255)', 'notnull' => true, 'default' => '', 'after' => 'organizerLink'),
+                'num'                                => array('type' => 'INT(5)', 'notnull' => true, 'default' => '0', 'after' => 'key'),
+                'mailTitle'                          => array('type' => 'VARCHAR(255)', 'notnull' => true, 'default' => '', 'after' => 'num'),
+                'mailContent'                        => array('type' => 'text', 'after' => 'mailTitle'),
+                'registration'                       => array('type' => 'INT(1)', 'notnull' => true, 'default' => '0', 'after' => 'mailContent'),
+                'groups'                             => array('type' => 'text', 'after' => 'registration'),
+                'all_groups'                         => array('type' => 'INT(1)', 'notnull' => true, 'default' => '0', 'after' => 'groups'),
+                'public'                             => array('type' => 'INT(1)', 'notnull' => true, 'default' => '0', 'after' => 'all_groups'),
+                'notification'                       => array('type' => 'INT(1)', 'after' => 'public'),
+                'notification_address'               => array('type' => 'VARCHAR(255)', 'notnull' => true, 'default' => '', 'after' => 'notification'),
+                'series_status'                      => array('type' => 'TINYINT(4)', 'after' => 'notification_address'),
+                'series_type'                        => array('type' => 'INT(11)', 'after' => 'series_status'),
+                'series_pattern_count'               => array('type' => 'INT(11)', 'after' => 'series_type'),
+                'series_pattern_weekday'             => array('type' => 'VARCHAR(7)', 'after' => 'series_pattern_count'),
+                'series_pattern_day'                 => array('type' => 'INT(11)', 'after' => 'series_pattern_weekday'),
+                'series_pattern_week'                => array('type' => 'INT(11)', 'after' => 'series_pattern_day'),
+                'series_pattern_month'               => array('type' => 'INT(11)', 'after' => 'series_pattern_week'),
+                'series_pattern_type'                => array('type' => 'INT(11)', 'after' => 'series_pattern_month'),
+                'series_pattern_dourance_type'       => array('type' => 'INT(11)', 'after' => 'series_pattern_type'),
+                'series_pattern_end'                 => array('type' => 'INT(11)', 'after' => 'series_pattern_dourance_type'),
+                'series_pattern_begin'               => array('type' => 'INT(11)', 'notnull' => true, 'default' => '0', 'after' => 'series_pattern_end'),
+                'series_pattern_exceptions'          => array('type' => 'longtext', 'after' => 'series_pattern_begin')
+            ),
+            array(
+                'name'                               => array('fields' => array('name','comment','placeName'), 'type' => 'FULLTEXT')
+            )
+        );
     }
     catch (UpdateException $e) {
         // we COULD do something else here..
         DBG::trace();
         return UpdateUtil::DefaultActionHandler($e);
     }
+
 
     //2.1.1
 
