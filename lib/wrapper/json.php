69cdf9e05dd07286dcacc4f61deab657cdb47953
Severin Raez <severin.raez@comvation.com>
2011-04-28 12:36:09 +0000

    lib(bugfix): syntax fixes in json wrapper
    git-svn-id: http://svn.contrexxlabs.com/contrexx/trunk@10963 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- lib/wrapper/json.php
+++ lib/wrapper/json.php
@@ -1,35 +1,34 @@
 <?php
-if(function_exists("json_encode")) {
-    return;
-}
-require_once(ASCMS_LIBRARY_PATH.'/PEAR/Services/JSON.php');
+if(!function_exists("json_encode")) {
+    require_once(ASCMS_LIBRARY_PATH.'/PEAR/Services/JSON.php');
 
-class JsonWrapper {
-    protected $pearJSON;
-    protected static $instance;
-    protected function __construct() {
-        $this->pearJSON = new Services_JSON();
+    class JsonWrapper {
+        protected $pearJSON;
+        protected static $instance;
+        protected function __construct() {
+            $this->pearJSON = new Services_JSON();
+        }
+
+        public static function getInstance() {
+            if(!self::$instance)
+                self::$instance = new JsonWrapper();
+
+            return self::$instance;
+        }
+
+        public function encode($obj) {
+            return $this->pearJSON->encode($obj);
+        }
+        public function decode($str) {
+            return $this->pearJSON->decode($str);
+        }
     }
 
-    public static function getInstance() {
-        if(!$this->instance)
-            $this->instance = new JsonWrapper();
-
-        return $this->instance;
+    function json_encode($obj) {
+        return JsonWrapper::getInstance()->encode($obj);
     }
 
-    public function encode($obj) {
-        return $this->pearJSON->encode($obj);
+    function json_decode($str) {
+        return JsonWrapper::getInstance()->encode($str);
     }
-    public function decode($str) {
-        return $this->pearJSON->decode($str);
-    }
-}
-
-function json_encode($obj) {
-    return JsonWrapper::getInstance()->encode($obj);
-}
-
-function json_decode($str) {
-    return JsonWrapper::getInstance()->encode($str);
 }
