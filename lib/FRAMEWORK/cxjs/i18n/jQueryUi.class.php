ac36dd64bf1261efb05634ca6bb4ff58809cd7c7
Severin Raez <severin.raez@comvation.com>
2011-05-11 08:24:49 +0000

    lib-cxjs(enhancement): allows chained dynamical inclusion, fixes media dir bug: localisation not always loaded
    git-svn-id: http://svn.contrexxlabs.com/contrexx/trunk@11107 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- lib/FRAMEWORK/cxjs/i18n/jQueryUi.class.php
+++ lib/FRAMEWORK/cxjs/i18n/jQueryUi.class.php
@@ -2,8 +2,8 @@
 class JQueryUiI18nProvider implements ContrexxJavascriptI18nProvider {
     public function getVariables($langCode) {
         $vars = array();
-        $datePickerFile = '/lib/javascript/jquery/ui/i18n/jquery.ui.datepicker-'.$langCode.'.js';
-        if(file_exists(ASCMS_DOCUMENT_ROOT.$datePickerFile))
+        $datePickerFile = 'lib/javascript/jquery/ui/i18n/jquery.ui.datepicker-'.$langCode.'.js';
+        if(file_exists(ASCMS_DOCUMENT_ROOT.'/'.$datePickerFile))
             $vars['datePickerI18nFile'] = $datePickerFile;
         return $vars;
     }
