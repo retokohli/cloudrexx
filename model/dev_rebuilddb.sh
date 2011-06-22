1cd150caf04905eb3d7e11c28a17fcfb4c3f362a
Severin Raez <severin.raez@comvation.com>
2011-06-22 14:14:52 +0000

    testing(enhancement): absolute paths for scripts
    git-svn-id: http://svn.contrexxlabs.com/contrexx/branches/work/cm_2_3@11693 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- model/dev_rebuilddb.sh
+++ model/dev_rebuilddb.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-
+cd $(dirname $0)
 ./doctrine orm:schema-tool:drop --force
 ./doctrine orm:schema-tool:create
--- testing/run.sh
+++ testing/run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+cd $(dirname $0)
 dir=$(pwd)
 cd PHPUnit
 php phpunit.php $dir/tests
