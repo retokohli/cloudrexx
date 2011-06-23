9073cec93e688ab1aa3fd2f8438db79d289a2ba3
Severin Raez <severin.raez@comvation.com>
2011-06-23 13:44:59 +0000

    doctrine(enhancement): working tree/join query
    git-svn-id: http://svn.contrexxlabs.com/contrexx/branches/work/cm_2_3@11710 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- testing/tests/model/contentManager/PageRepositoryTest.php
+++ testing/tests/model/contentManager/PageRepositoryTest.php
@@ -5,7 +5,6 @@ include_once('../testCases/DoctrineTestCase.php');
 
 class PageRepositoryTest extends DoctrineTestCase
 {
-
     public function testTree() {
         $repo = self::$em->getRepository('Cx\Model\ContentManager\Page');
         
@@ -20,19 +19,55 @@ class PageRepositoryTest extends DoctrineTestCase
         $n4->setParent($n1);
         $n5->setParent($n2);
 
+        $p1 = new \Cx\Model\ContentManager\Page();     
+        $p1->setLang(1);
+        $p1->setTitle('testpage1');
+        $p1->setNode($n1);
+        $p1->setUser(1);
+
+        $p2 = new \Cx\Model\ContentManager\Page();     
+        $p2->setLang(1);
+        $p2->setTitle('testpage2');
+        $p2->setNode($n1);
+        $p2->setUser(1);
+
+        $p3 = new \Cx\Model\ContentManager\Page();     
+        $p3->setLang(2);
+        $p3->setTitle('testpage3');
+        $p3->setNode($n1);
+        $p3->setUser(1);
+
         self::$em->persist($n1);
         self::$em->persist($n2);
         self::$em->persist($n3);
         self::$em->persist($n4);
         self::$em->persist($n5);
 
+        self::$em->persist($p1);
+        self::$em->persist($p2);
+        self::$em->persist($p3);
+
         self::$em->flush();
 
-        $childs = $repo->getTreeByLanguage();
+        //make sure we re-fetch a correct state
+        self::$em->clear();
 
-        $nRepo = self::$em->getRepository('Cx\Model\ContentManager\Node');
-        $nRepo->moveDown($childs[1],1);
-        DoctrineDebug::dump($childs[0]->getChildren());
+        $tree = $repo->getTree(1);
+
+        //check if we get only desired language back
+        $pages = $tree[0]->getPages();
+        foreach($pages as $page) 
+            $this->assertEquals(1, $page->getLang());
+
+        DoctrineDebug::dump($tree);
+                
+        //page count as expected?
+        $this->assertEquals(2, count($tree[0]->getPages()));
+        //all pages fetched?
+        $this->assertEquals(5, count($tree));
+        //children assigned as expected?
+        $children = $tree[0]->getChildren();
+        $this->assertEquals(2, count($tree[0]->getChildren()));
 
         return true;
     }
