5d2c3b6c734d95ecd0b609f29710918d4c8e8351
Severin Raez <severin.raez@comvation.com>
2011-06-28 13:45:16 +0000

    model(enhancement): validations work
    git-svn-id: http://svn.contrexxlabs.com/contrexx/branches/work/cm_2_3@11774 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- model/entities/Cx/Model/Base/EntityBase.php
+++ model/entities/Cx/Model/Base/EntityBase.php
@@ -2,13 +2,41 @@
 namespace Cx\Model\Base;
 
 /**
+ * Thrown by @link EntityBase::validate() if validation errors occur.
+ */
+class ValidationException extends \Exception {
+    protected $errors;
+
+    public function __construct(array $errors) {
+        parent::__construct();
+        $this->errors = $errors;
+        $this->assignMessage();
+    }
+
+    private function assignMessage() {
+        $str = '';
+        foreach($this->errors as $field => $details) {
+            $str .= $field.":\n";
+            foreach($details as $id => $message) {
+                $str .= "    $id: $message\n";
+            }
+        }
+        $this->message = $str;
+    }
+
+    public function getErrors() {
+        return $this->errors;
+    }
+}
+
+/**
  * This class provides the magic of being validatable.
  *
  * See EntityBase::$validators if you want to subclass it.
  */
 class EntityBase {
     /**
-     * Initialize this array as 
+     * Initialize this array as follows:
      * array(
      *     'columName' => Zend_Validate
      * )
@@ -17,18 +45,23 @@ class EntityBase {
     protected $validators = null;
 
     /**
+     * @throws ValidationException
      * @return null | array( 'field' => array( 'errorid' => 'errormessage') )
+     * @prePersis
      */
     public function validate() {
         $errors = array();
-        foreach($this->$validators as $field => $validator) {
-            if(!$validator->isValid($this->$field)) {
-                $errors[$field] = $validator->getMessages();
+        foreach($this->validators as $field => $validator) {
+            $methodName = 'get'.ucfirst($field);
+            $val = $this->$methodName();
+            if($val) {
+                if(!$validator->isValid($val)) {
+                    $errors[$field] = $validator->getMessages();
+                }
             }
         }
-        if(count($errors) == 0)
-            return null;
-        return $errors;
+        if(count($errors) > 0)
+            throw new ValidationException($errors);
     }
 }
 ?>
