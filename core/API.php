c64d360f3edbb2b6f958c62040b2830240b5a7ef
Severin Raez <severin.raez@comvation.com>
2011-06-28 09:05:21 +0000

    lib(enhancement): integrated Zend Form, Validator, Locale
    git-svn-id: http://svn.contrexxlabs.com/contrexx/branches/work/cm_2_3@11768 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- core/API.php
+++ core/API.php
@@ -116,6 +116,10 @@ if (isset($adminPage) && $adminPage ) {
     require_once ASCMS_CORE_PATH.'/Security.class.php';
 }
 
+//zend.
+set_include_path(implode(PATH_SEPARATOR, array(get_include_path(),ASCMS_LIBRARY_PATH)));
+require_once ASCMS_LIBRARY_PATH.'/Zend/Loader/Autoloader.php';
+
 //wrappers providing php functions via PEAR and other third party libraries if they're not found.
 $wrapperDir = ASCMS_LIBRARY_PATH.'/wrapper/';
 require_once $wrapperDir.'json.php';
