96b5a6a7ed94920451e60ee474741827385ddfb6
Severin Raez <severin.raez@comvation.com>
2011-06-30 09:19:41 +0000

    testing(enhancement): now using a test database instance
    git-svn-id: http://svn.contrexxlabs.com/contrexx/branches/work/cm_2_3@11788 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

---
+++ testing/copyDbToTestDb.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+user="root"
+pass="1234"
+
+cd $(dirname $0)
+dir=$(pwd)
+db=$(awk -f getDBName.awk ../config/configuration.php)
+testdb=$db"_testing"
+
+echo "dumping original db..."
+mysqldump -u$user -p$pass $db > /tmp/cxdb.sql 2>/dev/null
+if [ "$?" != "0" ]; then
+    echo "could not dump db, check user and pass provided in script."
+    rm /tmp/cxdb.sql
+    exit
+fi
+
+echo "creating test db..."
+mysql -uroot -p1234 -e"DROP DATABASE $testdb" 2>/dev/null
+mysql -uroot -p1234 -e"CREATE DATABASE $testdb"
+echo "initializing test db..."
+mysql -uroot -p1234 $testdb < /tmp/cxdb.sql
+
+rm /tmp/cxdb.sql
