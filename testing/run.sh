b36c352c14daded73ca8915ae8c40afb425a466a
Severin Raez <severin.raez@comvation.com>
2011-06-30 09:19:41 +0000

    testing(enhancement): now using a test database instance
    git-svn-id: http://svn.contrexxlabs.com/contrexx/branches/work/cm_2_3@11788 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- testing/run.sh
+++ testing/run.sh
@@ -1,5 +1,25 @@
 #!/bin/bash
 cd $(dirname $0)
 dir=$(pwd)
+
+cd ../config
+if [ ! -f configuration_testing.php ]; then #we have to create the test config
+    echo "could not find test config, creating it..."
+    awk -f ../testing/createTestConfig.awk configuration.php > configuration_testing.php
+fi
+
+echo "activating test configuration"
+mv configuration.php configuration_original.php
+mv configuration_testing.php configuration.php
+
+
+echo "starting unit testing"
+cd $dir
 cd PHPUnit
 php ../cx_bootstrap.php $dir/tests
+
+cd $dir
+cd ../config
+echo "deactivating test configuration"
+mv configuration.php configuration_testing.php
+mv configuration_original.php configuration.php
