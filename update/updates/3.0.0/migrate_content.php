9fb806feabadbc24fb68d5172f4c4d04017736b0
ss4u <comvation@softsolutions4u.info>
2011-08-03 09:49:37 +0000

    Migartion script ==========     Added node array to store nodes  objects.     If the parcat value is zero set the root as a parent else set the node object of parent     category form old architecture as parent.     Mapped all values form old table structure to new tables.
    git-svn-id: http://svn.contrexxlabs.com/contrexx/branches/work/cm_2_3@12026 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- update/updates/3.0.0/migrate_content.php
+++ update/updates/3.0.0/migrate_content.php
@@ -28,32 +28,55 @@ class Contrexx_Content_migration
     public function migrate() 
     {
         global $objDatabase;
-                
-        $objResult = $objDatabase->Execute('SELECT content.*, nav.* FROM `'.DBPREFIX.'content` as content LEFT JOIN `'.DBPREFIX.'content_navigation` as nav ON content.id = nav.catid');
+        $nodeArr = array ();
+        $root = new \Cx\Model\ContentManager\Node();
         
+        $objResult = $objDatabase->Execute('SELECT content.*, nav.* 
+                                            FROM `'.DBPREFIX.'content` AS content 
+                                            INNER JOIN `'.DBPREFIX.'content_navigation` AS nav
+                                            ON content.id = nav.catid
+                                            ORDER BY nav.parcat ASC, nav.displayorder ASC');
+
         while (!$objResult->EOF) {
-            $root = new \Cx\Model\ContentManager\Node();
-            $n    = new \Cx\Model\ContentManager\Node();        
-            $n->setParent($root);
-
-            $p = new \Cx\Model\ContentManager\Page();            
+            $n    = new \Cx\Model\ContentManager\Node(); 
             
+            $nodeArr[$objResult->fields['id']] = $n;
+            
+            if ($objResult->fields['parcat'] == 0) {          
+                $n->setParent($root);
+                self::$em->persist($root);
+            } else {
+                $n->setParent($nodeArr[$objResult->fields['parcat']]);
+            }          
+
+            $p = new \Cx\Model\ContentManager\Page();
+            
+            $p->setNode($n); 
             $p->setLang($objResult->fields['lang']);
+            $p->setCaching($objResult->fields['cachingstatus']);
             $p->setTitle($objResult->fields['title']);
-            $p->setContent($objResult->fields['content']);
-            $p->setNode($n);
+            $p->setContent($objResult->fields['content']);            
+            $p->setCustomContent($objResult->fields['custom_content']);
+            $p->setCssName($objResult->fields['css_name']);
+            $p->setMetatitle($objResult->fields['metatitle']);
+            $p->setMetadesc($objResult->fields['metadesc']);
+            $p->setMetakeys($objResult->fields['metakeys']);
+            $p->setMetarobots($objResult->fields['metarobots']);
             $p->setUsername($objResult->fields['username']);
-
-            self::$em->persist($root);
-            self::$em->persist($n);
-
+            $p->setDisplay(($objResult->fields['displaystatus'] === 'on' ? 1 : 0));
+            $p->setActive($objResult->fields['activestatus']);
+            $p->setTarget($objResult->fields['target']);
+            $p->setModule($objResult->fields['module']);
+            
+            self::$em->persist($n);            
             self::$em->persist($p);
-
+            
             self::$em->flush();
 
-            $objResult->MoveNext();        
+            $objResult->MoveNext();
         }       
     }    
 }
-
+$obj = new Contrexx_Content_migration();
+$obj->migrate();
 ?>
