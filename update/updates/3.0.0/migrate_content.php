b62767dd29844c68c8c117ff2a97b70cace240e6
ss4u <comvation@softsolutions4u.info>
2011-08-06 13:38:56 +0000

    Creating Log: ========   We have truncated the 'ext_log_table' after the content migration.   We have fetched the logid, action, history_id from the content_logfile table,username   and change log from the navigation_history table using the $objDatabase inserted into   the corresponding columns to the 'ext_log_entries'.
    git-svn-id: http://svn.contrexxlabs.com/contrexx/branches/work/cm_2_3@12035 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- update/updates/3.0.0/migrate_content.php
+++ update/updates/3.0.0/migrate_content.php
@@ -1,12 +1,11 @@
 <?php
-
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-require('../../../config/configuration.php');
-require('../../../core/API.php');
-require('../../../config/doctrine.php');
+require_once '../../../config/configuration.php';
+require_once '../../../core/API.php';
+require_once '../../../config/doctrine.php';
 
 class Contrexx_Content_migration
 {
@@ -25,38 +24,67 @@ class Contrexx_Content_migration
         
     }
    
-    public function migrate() 
+    public function migrate()
     {
         global $objDatabase;
         $nodeArr = array ();
         $root = new \Cx\Model\ContentManager\Node();
+        self::$em->persist($root);
         
         $objDatabase->Execute('TRUNCATE TABLE `pages`');
-        $objDatabase->Execute('TRUNCATE TABLE `nodes`');        
-        $objResult = $objDatabase->Execute('SELECT content.*, nav.* 
+        $objDatabase->Execute('TRUNCATE TABLE `nodes`');  
+
+        $objNodeResult = $objDatabase->Execute('SELECT catid FROM `'.DBPREFIX.'content_navigation` ORDER BY catid ASC');
+
+        while (!$objNodeResult->EOF) {
+            
+            $nodeArr[$objNodeResult->fields['catid']] = new \Cx\Model\ContentManager\Node();             
+            
+            self::$em->persist($nodeArr[$objNodeResult->fields['catid']]);
+            
+            $objNodeResult->MoveNext();
+        }
+
+        $objResult = $objDatabase->Execute('SELECT content.content,
+                                            content.title,
+                                            content.metatitle,
+                                            content.metadesc,
+                                            content.metakeys,
+                                            content.metarobots,
+                                            content.css_name,
+                                            nav.catid,
+                                            nav.parcat,
+                                            nav.lang,
+                                            nav.cachingstatus,
+                                            nav.changelog,
+                                            nav.custom_content,
+                                            nav.startdate,
+                                            nav.enddate,
+                                            nav.username,
+                                            nav.displaystatus,
+                                            nav.activestatus,
+                                            nav.target,
+                                            nav.module,
+                                            nav.cmd
                                             FROM `'.DBPREFIX.'content` AS content 
                                             INNER JOIN `'.DBPREFIX.'content_navigation` AS nav
                                             ON content.id = nav.catid
-                                            ORDER BY nav.parcat ASC, nav.displayorder ASC');        
+                                            ORDER BY nav.parcat ASC, nav.displayorder ASC');
         
         while (!$objResult->EOF) {
-            $n    = new \Cx\Model\ContentManager\Node(); 
-            
-            $nodeArr[$objResult->fields['id']] = $n;
-            
-            if ($objResult->fields['parcat'] == 0) {          
-                $n->setParent($root);
-                self::$em->persist($root);
+
+            if ($objResult->fields['parcat'] == 0) {
+                $nodeArr[$objResult->fields['catid']]->setParent($root);                
             } else {
-                $n->setParent($nodeArr[$objResult->fields['parcat']]);
-            }          
+                $nodeArr[$objResult->fields['catid']]->setParent($nodeArr[$objResult->fields['parcat']]);
+            }
 
             $p = new \Cx\Model\ContentManager\Page();
             
             // Convert the changelog value from Unix time stamp to date for UpdatedAt function
             $updatedDate = Date('Y-m-d H:i:s',$objResult->fields['changelog']);
 
-            $p->setNode($n); 
+            $p->setNode($nodeArr[$objResult->fields['catid']]); 
             $p->setLang($objResult->fields['lang']);
             $p->setCaching($objResult->fields['cachingstatus']);
             $p->setUpdatedAt(new DateTime($updatedDate));
@@ -75,14 +103,40 @@ class Contrexx_Content_migration
             $p->setActive($objResult->fields['activestatus']);
             $p->setTarget($objResult->fields['target']);
             $p->setModule($objResult->fields['module']);
-            
-            self::$em->persist($n);            
+
             self::$em->persist($p);
 
             self::$em->flush();
 
             $objResult->MoveNext();
-        }       
-    }    
+        }
+        
+        $objDatabase->Execute('TRUNCATE TABLE `ext_log_entries`');     
+
+        $actionArr = array(
+                      'new'    => 'create',
+                      'update' => 'update',
+                      'delete' => 'remove',
+                     );
+          
+        $objLog = $objDatabase->Execute('SELECT cnlog.id AS logId,
+                                         cnlog.action AS action,
+                                         cnlog.history_id AS historyId,
+                                         navHis.username,
+                                         navHis.changelog,
+                                         cnHis.page_id
+                                         FROM `'.DBPREFIX.'content_logfile` AS cnlog
+                                         LEFT JOIN `'.DBPREFIX.'content_navigation_history` AS navHis ON navHis.id = cnlog.history_id
+                                         LEFT JOIN `'.DBPREFIX.'content_history` AS cnHis ON navHis.id = cnHis.id');
+        while (!$objLog->EOF) {
+            $objDatabase->Execute('INSERT INTO `ext_log_entries` SET
+                                     `id`        = '.$objLog->fields['logid'].',
+                                     `action`    = "'.$actionArr[$objLog->fields['action']].'",
+                                     `logged_at` = '.date('Y m d H:i:s',$objLog->fields['changelog']).',
+                                     `version`   = '.$objLog->fields['historyId'].',
+                                     `username`  = "'.$objLog->fields['username'].'"');
+            $objLog->MoveNext();
+        }                
+    }
 }
 ?>
