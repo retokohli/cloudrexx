e42f79f2ff52fa82ba5d4bacac5e12c5cb3297b2
ss4u <comvation@softsolutions4u.info>
2011-08-08 13:41:59 +0000

    Migration script ==========     Fetched the values from navigaiton_history, content_history, content_logfile and     migrated into pages, node tables.     Created the pages depends on the action varible in logfile table.     Created the _setPageRecords() function to set the page values.
    git-svn-id: http://svn.contrexxlabs.com/contrexx/branches/work/cm_2_3@12041 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- update/updates/3.0.0/migrate_content.php
+++ update/updates/3.0.0/migrate_content.php
@@ -32,9 +32,12 @@ class Contrexx_Content_migration
         self::$em->persist($root);
         
         $objDatabase->Execute('TRUNCATE TABLE `pages`');
-        $objDatabase->Execute('TRUNCATE TABLE `nodes`');  
-
-        $objNodeResult = $objDatabase->Execute('SELECT catid FROM `'.DBPREFIX.'content_navigation` ORDER BY catid ASC');
+        $objDatabase->Execute('TRUNCATE TABLE `nodes`');          
+        $objDatabase->Execute('TRUNCATE TABLE `ext_log_entries`');
+        
+        $objNodeResult = $objDatabase->Execute('SELECT DISTINCT catid
+                                                FROM `'.DBPREFIX.'content_navigation_history`
+                                                ORDER BY catid ASC');
 
         while (!$objNodeResult->EOF) {
             
@@ -44,32 +47,15 @@ class Contrexx_Content_migration
             
             $objNodeResult->MoveNext();
         }
-
-        $objResult = $objDatabase->Execute('SELECT content.content,
-                                            content.title,
-                                            content.metatitle,
-                                            content.metadesc,
-                                            content.metakeys,
-                                            content.metarobots,
-                                            content.css_name,
-                                            nav.catid,
-                                            nav.parcat,
-                                            nav.lang,
-                                            nav.cachingstatus,
-                                            nav.changelog,
-                                            nav.custom_content,
-                                            nav.startdate,
-                                            nav.enddate,
-                                            nav.username,
-                                            nav.displaystatus,
-                                            nav.activestatus,
-                                            nav.target,
-                                            nav.module,
-                                            nav.cmd
-                                            FROM `'.DBPREFIX.'content` AS content 
-                                            INNER JOIN `'.DBPREFIX.'content_navigation` AS nav
-                                            ON content.id = nav.catid
-                                            ORDER BY nav.parcat ASC, nav.displayorder ASC');
+        
+        $objResult = $objDatabase->Execute('SELECT cn.*,
+               nav.*,
+               cnlog.*
+               FROM `'.DBPREFIX.'content_history` AS cn
+               INNER JOIN `'.DBPREFIX.'content_navigation_history` AS nav
+               ON cn.id=nav.id
+               INNER JOIN `'.DBPREFIX.'content_logfile` AS cnlog
+               ON cn.id = cnlog.history_id ORDER BY cnlog.history_id ASC');
         
         while (!$objResult->EOF) {
 
@@ -79,64 +65,98 @@ class Contrexx_Content_migration
                 $nodeArr[$objResult->fields['catid']]->setParent($nodeArr[$objResult->fields['parcat']]);
             }
 
-            $p = new \Cx\Model\ContentManager\Page();
+            switch ($objResult->fields['action']) {
+                case 'new':
+                    $p[$objResult->fields['page_id']] = new \Cx\Model\ContentManager\Page();
+                    $page = $p[$objResult->fields['page_id']];
+                    break;
+                case 'update':
+                    $page = isset($p[$objResult->fields['page_id']]) ? $p[$objResult->fields['page_id']] : new \Cx\Model\ContentManager\Page();
+                    break;
+                case 'delete':
+                    self::$em->remove($p[$objResult->fields['page_id']]);
+                    break;
+            }
+                        
+            $this->_setPageRecords($objResult, $nodeArr[$objResult->fields['catid']], $page);
             
-            // Convert the changelog value from Unix time stamp to date for UpdatedAt function
-            $updatedDate = Date('Y-m-d H:i:s',$objResult->fields['changelog']);
-
-            $p->setNode($nodeArr[$objResult->fields['catid']]); 
-            $p->setLang($objResult->fields['lang']);
-            $p->setCaching($objResult->fields['cachingstatus']);
-            $p->setUpdatedAt(new DateTime($updatedDate));
-            $p->setTitle($objResult->fields['title']);
-            $p->setContent($objResult->fields['content']);            
-            $p->setCustomContent($objResult->fields['custom_content']);
-            $p->setCssName($objResult->fields['css_name']);
-            $p->setMetatitle($objResult->fields['metatitle']);
-            $p->setMetadesc($objResult->fields['metadesc']);
-            $p->setMetakeys($objResult->fields['metakeys']);
-            $p->setMetarobots($objResult->fields['metarobots']);
-            $p->setStart(new DateTime($objResult->fields['startdate']));
-            $p->setEnd(new DateTime($objResult->fields['enddate']));
-            $p->setUsername($objResult->fields['username']);
-            $p->setDisplay(($objResult->fields['displaystatus'] === 'on' ? 1 : 0));
-            $p->setActive($objResult->fields['activestatus']);
-            $p->setTarget($objResult->fields['target']);
-            $p->setModule($objResult->fields['module']);
-
-            self::$em->persist($p);
+            self::$em->persist($page);
 
             self::$em->flush();
 
             $objResult->MoveNext();
-        }
+        }             
         
-        $objDatabase->Execute('TRUNCATE TABLE `ext_log_entries`');     
+        // Check for unmatched record in contents table and pages table
+        $objRecords = $objDatabase->Execute('SELECT * 
+                                             FROM `'.DBPREFIX.'content` AS cn
+                                             INNER JOIN `'.DBPREFIX.'content_navigation` AS nav
+                                             ON cn.id = nav.catid
+                                             WHERE cn.id
+                                             NOT IN (
+                                                SELECT pages.id
+                                                FROM pages
+                                                WHERE cn.id = pages.id)'
+                                            );
+        
+        if ($objRecords->RecordCount() > 0) {            
+            $objNodeResult = $objDatabase->Execute('SELECT catid FROM `'.DBPREFIX.'content_navigation` ORDER BY catid ASC');
 
-        $actionArr = array(
-                      'new'    => 'create',
-                      'update' => 'update',
-                      'delete' => 'remove',
-                     );
-          
-        $objLog = $objDatabase->Execute('SELECT cnlog.id AS logId,
-                                         cnlog.action AS action,
-                                         cnlog.history_id AS historyId,
-                                         navHis.username,
-                                         navHis.changelog,
-                                         cnHis.page_id
-                                         FROM `'.DBPREFIX.'content_logfile` AS cnlog
-                                         LEFT JOIN `'.DBPREFIX.'content_navigation_history` AS navHis ON navHis.id = cnlog.history_id
-                                         LEFT JOIN `'.DBPREFIX.'content_history` AS cnHis ON navHis.id = cnHis.id');
-        while (!$objLog->EOF) {
-            $objDatabase->Execute('INSERT INTO `ext_log_entries` SET
-                                     `id`        = '.$objLog->fields['logid'].',
-                                     `action`    = "'.$actionArr[$objLog->fields['action']].'",
-                                     `logged_at` = '.date('Y m d H:i:s',$objLog->fields['changelog']).',
-                                     `version`   = '.$objLog->fields['historyId'].',
-                                     `username`  = "'.$objLog->fields['username'].'"');
-            $objLog->MoveNext();
-        }                
+            while (!$objNodeResult->EOF) {
+                if (empty ($nodeArr[$objNodeResult->fields['catid']])) {
+                    $nodeArr[$objNodeResult->fields['catid']] = new \Cx\Model\ContentManager\Node();             
+            
+                    self::$em->persist($nodeArr[$objNodeResult->fields['catid']]);
+                }            
+                $objNodeResult->MoveNext();
+            }
+        }
+
+        while (!$objRecords->EOF) {
+
+            if ($objRecords->fields['parcat'] == 0) { 
+                $nodeArr[$objRecords->fields['catid']]->setParent($root);                
+            } else {
+                $nodeArr[$objRecords->fields['catid']]->setParent($nodeArr[$objRecords->fields['parcat']]);
+            }
+            $page = new \Cx\Model\ContentManager\Page();
+
+            $this->_setPageRecords($objRecords, $nodeArr[$objRecords->fields['catid']], $page);
+
+            self::$em->persist($page);
+
+            self::$em->flush();
+            
+            $objRecords->MoveNext();
+        }
+    }
+    
+    function _setPageRecords($objResult, $node, $page)
+    {
+
+            // Convert the changelog value from Unix time stamp to date for UpdatedAt function
+            $updatedDate = Date('Y-m-d H:i:s',$objResult->fields['changelog']);
+
+            $page->setNode($node); 
+            $page->setLang($objResult->fields['lang']);
+            $page->setCaching($objResult->fields['cachingstatus']);
+            $page->setUpdatedAt(new DateTime($updatedDate));
+            $page->setTitle($objResult->fields['title']);
+            $page->setContent($objResult->fields['content']);            
+            $page->setCustomContent($objResult->fields['custom_content']);
+            $page->setCssName($objResult->fields['css_name']);
+            $page->setMetatitle($objResult->fields['metatitle']);
+            $page->setMetadesc($objResult->fields['metadesc']);
+            $page->setMetakeys($objResult->fields['metakeys']);
+            $page->setMetarobots($objResult->fields['metarobots']);
+            $page->setStart(new DateTime($objResult->fields['startdate']));
+            $page->setEnd(new DateTime($objResult->fields['enddate']));
+            $page->setUsername($objResult->fields['username']);
+            $page->setDisplay(($objResult->fields['displaystatus'] === 'on' ? 1 : 0));
+            $page->setActive($objResult->fields['activestatus']);
+            $page->setTarget($objResult->fields['target']);
+            $page->setModule($objResult->fields['module']);     
+            
     }
 }
 ?>
