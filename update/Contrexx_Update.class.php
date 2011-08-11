888fc3e50f4f1e4db8d9bcd1eb18af0ca1d4bcf0
Thomas Däppen <thomas.daeppen@cloudrexx.com>
2011-08-11 15:40:00 +0000

    core_DBG: - moved adodb debug mode setting into new DBG method set_adobe_debug_mode() (addition to revision r12069)
    git-svn-id: http://svn.contrexxlabs.com/contrexx/trunk@12070 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- update/Contrexx_Update.class.php
+++ update/Contrexx_Update.class.php
@@ -42,14 +42,7 @@ class Contrexx_Update
         if (!$objDatabase) {
             die($errorMsg);
         }
-
-        if (DBG::getMode() & DBG_ADODB_TRACE) {
-            DBG::enable_adodb_debug(true);
-        } elseif (DBG::getMode() & DBG_ADODB || DBG::getMode() & DBG_ADODB_ERROR) {
-            DBG::enable_adodb_debug();
-        } else {
-            DBG::disable_adodb_debug();
-        }
+        DBG::set_adodb_debug_mode();
 
         $this->_loadLanguage();
         if (!empty($_GET['ajax'])) {
