40f310baba492205bc588d810fa2a39336987d8b
Severin Raez <severin.raez@comvation.com>
2011-08-17 11:54:03 +0000

    model(enhancement): table prefixes
    git-svn-id: http://svn.contrexxlabs.com/contrexx/branches/work/cm_2_3@12105 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- config/doctrine.php
+++ config/doctrine.php
@@ -22,6 +22,9 @@ $classLoader->register();
 $classLoader = new \Doctrine\Common\ClassLoader('Cx\Model\Proxies', ASCMS_MODEL_PROXIES_PATH);
 $classLoader->register();
 
+$classLoader = new \Doctrine\Common\ClassLoader('DoctrineExtension', ASCMS_MODEL_PATH.'/extensions');
+$classLoader->register();
+
 $classLoader = new \Doctrine\Common\ClassLoader('Gedmo', $doctrineDir);
 $classLoader->register();
 
@@ -67,6 +70,10 @@ $evm->addEventSubscriber($loggableListener);
 $treeListener = new \Gedmo\Tree\TreeListener();
 $evm->addEventSubscriber($treeListener);
 $config->setMetadataDriverImpl($chainDriverImpl);
+
+//table prefix
+$prefixListener = new \DoctrineExtension\TablePrefixListener($_DBCONFIG['tablePrefix']);
+$evm->addEventListener(\Doctrine\ORM\Events::loadClassMetadata, $prefixListener);
 
 //$config->setSqlLogger(new Doctrine\DBAL\Logging\EchoSQLLogger());
 
