9b0c751136f116b665394e218a06a32cbd5680ce
Severin Raez <severin.raez@comvation.com>
2011-08-29 14:19:11 +0000

    config(bugfix): reverting unintentional checkin from r12778
    git-svn-id: http://svn.contrexxlabs.com/contrexx/branches/work/cm_2_3@12779 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- config/configuration.php
+++ config/configuration.php
@@ -21,20 +21,20 @@ define('CONTEXX_INSTALLED', true);
 * -------------------------------------------------------------------------
 */
 $_DBCONFIG['host'] = 'localhost'; // This is normally set to localhost
-$_DBCONFIG['database'] = 'cm_2_3';
+$_DBCONFIG['database'] = 'cm2'; // Database name
 $_DBCONFIG['tablePrefix'] = 'contrexx_'; // Database table prefix
-$_DBCONFIG['user'] = 'root';
-$_DBCONFIG['password'] = '1234';
+$_DBCONFIG['user'] = 'root'; // Database username
+$_DBCONFIG['password'] = ''; // Database password
 $_DBCONFIG['dbType'] = 'mysql';	// Database type (e.g. mysql,postgres ..)
-$_DBCONFIG['charset'] = 'utf8';
+$_DBCONFIG['charset'] = 'default'; // Charset (default, latin1, utf8, ..)
 
 /**
 * -------------------------------------------------------------------------
 * Site path specific configuration
 * -------------------------------------------------------------------------
 */
-$_PATHCONFIG['ascms_root'] = '/home/srz/web/root';
-$_PATHCONFIG['ascms_root_offset'] = '/cm_2_3';
+$_PATHCONFIG['ascms_root'] = 'C:/xampp/htdocs';
+$_PATHCONFIG['ascms_root_offset'] = '/cm2/cm_2_3'; // example: '/cms';
 
 /**
 * -------------------------------------------------------------------------
@@ -61,8 +61,8 @@ $_CONFIGURATION['custom']['shopJsCart'] = false; // true|false
 /**
 * Set character encoding
 */
-$_CONFIG['coreCharacterEncoding'] = 'UTF-8';
-$_CONFIG['coreCharacterEncoding'] = 'UTF-8';
+$_CONFIG['coreCharacterEncoding'] = ''; // example 'UTF-8'
+@ini_set('default_charset', $_CONFIG['coreCharacterEncoding']);
 
 /**
 * Set output url seperator
@@ -80,4 +80,9 @@ $_CONFIG['coreCharacterEncoding'] = 'UTF-8';
 * -------------------------------------------------------------------------
 */
 require_once dirname(__FILE__).'/set_constants.php';
+/**
+ * Initialize doctrine
+ */
+require_once dirname(__FILE__).'/doctrine.php';
+
 ?>
