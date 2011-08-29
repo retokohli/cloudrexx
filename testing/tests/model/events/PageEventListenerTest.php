ff7a3e2f9ca24607859e6ec766dcd30e43ba9bd8
Severin Raez <severin.raez@comvation.com>
2011-08-29 14:02:19 +0000

    model(enhancement): slug generation tests
    git-svn-id: http://svn.contrexxlabs.com/contrexx/branches/work/cm_2_3@12776 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- testing/tests/model/events/PageEventListenerTest.php
+++ testing/tests/model/events/PageEventListenerTest.php
@@ -5,8 +5,11 @@ class PageEventListenerTest extends DoctrineTestCase
 {
     public function testUniqueSlugGeneration() {
         $root = new \Cx\Model\ContentManager\Node();
+
         $n1 = new \Cx\Model\ContentManager\Node();
+        $n1->setParent($root);
         $n2 = new \Cx\Model\ContentManager\Node();
+        $n2->setParent($root);
 
         $p1 = new \Cx\Model\ContentManager\Page();
         $p1->setLang(1);
@@ -18,18 +21,29 @@ class PageEventListenerTest extends DoctrineTestCase
         $p2 = new \Cx\Model\ContentManager\Page();
         $p2->setLang(1);
         $p2->setTitle('testpage');
-        $p2->setNode($n1);
+        $p2->setNode($n2);
         $p2->setUsername('user');
+
+        //different language, shouldn't conflict
+        $p3 = new \Cx\Model\ContentManager\Page();
+        $p3->setLang(2);
+        $p3->setTitle('testpage');
+        $p3->setNode($n1);
+        $p3->setUsername('user');
 
         self::$em->persist($root);
         self::$em->persist($n1);
         self::$em->persist($n2);
+        self::$em->flush();return;
         self::$em->persist($p1);
         self::$em->persist($p2);
+        self::$em->persist($p3);
         self::$em->flush();
 
         //see whether the listener changed the slug as we expect him to do.
         $this->assertEquals('testpage', $p1->getSlug());
         $this->assertEquals('testpage-1', $p2->getSlug());
+        //check whether slug uniqueness was checked only language-wide
+        $this->assertEquals('testpage', $p3->getSlug());
     }
 }
