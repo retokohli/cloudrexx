586dd15ccdca909a4e59b4d7ba504b9581aec0a6
Severin Raez <severin.raez@comvation.com>
2011-08-30 13:18:37 +0000

    model(enhancement): finally: slugging works (tests included)
    git-svn-id: http://svn.contrexxlabs.com/contrexx/branches/work/cm_2_3@12791 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- testing/tests/model/events/PageEventListenerTest.php
+++ testing/tests/model/events/PageEventListenerTest.php
@@ -58,4 +58,65 @@ class PageEventListenerTest extends DoctrineTestCase
         //check whether slug uniqueness was checked only language-wide
         $this->assertEquals('testpage', $p3->getSlug());
     }
+
+    public function testUniqueSlugGenerationWithPersistedNodes() {
+        $root = new \Cx\Model\ContentManager\Node();
+
+        $n1 = new \Cx\Model\ContentManager\Node();
+        $n1->setParent($root);
+        $n2 = new \Cx\Model\ContentManager\Node();
+        $n2->setParent($root);
+
+        $p1 = new \Cx\Model\ContentManager\Page();
+        $p1->setLang(1);
+        $p1->setTitle('testpage');
+        $p1->setNode($n1);
+        $p1->setUsername('user');
+
+        self::$em->persist($root);
+        self::$em->persist($n1);
+        self::$em->persist($n2);
+        self::$em->persist($p1);
+        self::$em->flush();
+
+        $id = $n2->getId();
+       
+        self::$em->clear();
+
+        $node = self::$em->find('Cx\Model\ContentManager\Node', $id);
+
+        //provocate a slug conflict
+        $p2 = new \Cx\Model\ContentManager\Page();
+        $p2->setLang(1);
+        $p2->setTitle('testpage');
+        $p2->setNode($node);
+        $p2->setUsername('user');
+
+        //different language, shouldn't conflict
+        $p3 = new \Cx\Model\ContentManager\Page();
+        $p3->setLang(2);
+        $p3->setTitle('testpage');
+        $p3->setNode($node);
+        $p3->setUsername('user');
+
+        $newNode = new \Cx\Model\ContentManager\Node();
+        $newNode->setParent($node->getParent());
+        //mixing in a conflict inside the new persists
+        $p4 = new \Cx\Model\ContentManager\Page();
+        $p4->setLang(1);
+        $p4->setTitle('testpage');
+        $p4->setNode($newNode);
+        $p4->setUsername('user');
+
+
+        self::$em->persist($p2);
+        self::$em->persist($p3);
+        self::$em->persist($newNode);
+        self::$em->persist($p4);
+        self::$em->flush();
+
+        $this->assertEquals('testpage-1', $p2->getSlug());
+        $this->assertEquals('testpage', $p3->getSlug());
+        $this->assertEquals('testpage-2', $p4->getSlug());
+    }
 }
