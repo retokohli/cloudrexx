8ebb33fbb74906a17a6cf18c3fceefd93e09234f
Severin Raez <severin.raez@comvation.com>
2011-09-15 14:50:52 +0000

    core(enhancement): contentmanager loads content via sigma template instead of ajaxing it now.
    git-svn-id: http://svn.contrexxlabs.com/contrexx/branches/work/cm_2_3@12978 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- core/ContentManager2.class.php
+++ core/ContentManager2.class.php
@@ -31,6 +31,8 @@ class ContentManager {
 
 		$objTemplate->addBlockfile('ADMIN_CONTENT', 'content_manager', 'content_manager.html');
 		$objTemplate->touchBlock('content_manager');
+		$objTemplate->addBlockfile('CONTENT_MANAGER_MEAT', 'content_manager_meat', 'cm.html');
+		$objTemplate->touchBlock('content_manager_meat');
 
         // TODO: move including of add'l JS dependencies to cx obj from /cadmin/index.html
         $objTemplate->setVariable('CXJS_INIT_JS', ContrexxJavascript::getInstance()->initJs());
