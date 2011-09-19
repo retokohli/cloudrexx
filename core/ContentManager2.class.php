438d6e89f98de3b1613166b9ce2835dcee58b7b1
Severin Raez <severin.raez@comvation.com>
2011-09-19 09:19:33 +0000

    core(enhancement): German language variables for content manager.
    git-svn-id: http://svn.contrexxlabs.com/contrexx/branches/work/cm_2_3@13023 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- core/ContentManager2.class.php
+++ core/ContentManager2.class.php
@@ -15,11 +15,11 @@ class ContentManager {
 	
 	var $em = null;
 
-	function __construct() {
+	public function __construct() {
 		$this->em = Env::em();
 	}
 
-	function renderCM() {
+	public function renderCM() {
         JS::activate('cx');
         JS::activate('ckeditor');
         JS::activate('jqueryui');
@@ -34,9 +34,32 @@ class ContentManager {
 		$objTemplate->addBlockfile('CONTENT_MANAGER_MEAT', 'content_manager_meat', 'cm.html');
 		$objTemplate->touchBlock('content_manager_meat');
 
+        $this->setLanguageVars(array(
+            //langs
+            'TXT_GERMAN', 'TXT_FRENCH', 'TXT_ENGLISH',
+            //categories
+            'TXT_SITE_TYPE', 'TXT_SITE_CONTENT', 'TXT_SITE_ACCESS', 'TXT_SITE_SETTINGS', 'TXT_SITE_HISTORY',
+            //type tab
+            'TXT_TYPE_EXPLANATION', 'TXT_TYPE_CONTENT', 'TXT_TYPE_CONTENT_DESCRIPTION', 'TXT_TYPE_REDIRECTION', 'TXT_TYPE_REDIRECTION_DESCRIPTION', 'TXT_TYPE_APP', 'TXT_TYPE_APP_DESCRIPTION', 
+            //content tab
+            'TXT_ACTIVE_FROM_TO', 'TXT_ACTIVE_FROM', 'TXT_ACTIVE_TO', 'TXT_META_INFORMATION', 'TXT_META_TITLE',  'TXT_META_KWORDS', 'TXT_META_DESC', 'TXT_META_ROBOTS',
+            //settings tab
+            'TXT_MODULE_CMD', 'TXT_MODULE', 'TXT_CMD', 'TXT_OPTICS_STYLE', 'TXT_SKIN', 'TXT_SPECIAL_CONTENT_PAGE', 'TXT_CUSTOMCONTENT', 'TXT_REDIRECTION', 'TXT_TARGET', 'TXT_PERFORMANCE_OPTIMIZATION', 'TXT_CACHING', 'TXT_LINK', 'TXT_SLUG',
+            //bottom buttons
+            'TXT_PREVIEW', 'TXT_SAVE_PUBLISH', 'TXT_SAVE'
+        ));
+
         // TODO: move including of add'l JS dependencies to cx obj from /cadmin/index.html
         $objTemplate->setVariable('CXJS_INIT_JS', ContrexxJavascript::getInstance()->initJs());
 	}
+
+    protected function setLanguageVars($ids) {
+        global $_CORELANG;
+        global $objTemplate;
+        foreach($ids as $id) {
+            $objTemplate->setVariable($id, $_CORELANG[$id]);
+        }
+    }
 
 }
 
