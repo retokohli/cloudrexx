3a1c23d1d5142c2e4b751a35c06f476fd4044496
Severin Raez <severin.raez@comvation.com>
2011-09-20 09:28:04 +0000

    core(enhancement): ContentManager refactoring
    git-svn-id: http://svn.contrexxlabs.com/contrexx/branches/work/cm_2_3@13046 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

---
+++ core/Module.class.php
@@ -0,0 +1,30 @@
+<?php
+
+class ModuleException extends Exception {}
+
+class Module {
+    protected $defaultAct = '';
+
+    public function __construct($act, $template) {
+        $this->act = $act;
+        $this->template = $template;
+	}
+
+    public function getPage() {
+        if($this->act == '') {
+            $this->act = $this->defaultAct;
+        }
+
+        //prevent execution of non-act methods.
+        if(substr($this->act, 0, 3) != 'act') {
+            throw new ModuleException('acts start with "act", "' . $this->act . '" given');
+        }
+
+        //call the right act.
+        $act = $this->act;
+        if(method_exists($this, $act))
+            $this->$act();
+        else
+            throw new ModuleException('unknown act: "' . $this->act . '"');
+    }
+}
