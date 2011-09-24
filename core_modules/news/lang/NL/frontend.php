9ce1166903256fc0b78f622dc813d79c1489e545
Thomas Däppen <thomas.daeppen@cloudrexx.com>
2011-09-24 15:14:10 +0000

    news: added version 3 from branches/work/module_news_2_2
    git-svn-id: http://svn.contrexxlabs.com/contrexx/branches/work/cm_2_3@13125 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

---
+++ core_modules/news/lang/NL/frontend.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ * @copyright   CONTREXX CMS - COMVATION AG
+ * @author      Comvation Development Team <info@comvation.com>
+ * @access      public
+ * @package     contrexx
+ * @subpackage  core_module_news
+ */
+$_ARRAYLANG['TXT_DATE'] = "Datum";
+$_ARRAYLANG['TXT_TITLE'] = "Titel";
+$_ARRAYLANG['TXT_CATEGORY'] = "Categorie";
+$_ARRAYLANG['TXT_NEWS_MESSAGES'] = "Nieuwsberichten";
+$_ARRAYLANG['TXT_NEWS_MESSAGE'] = "Nieuwsbericht";
+$_ARRAYLANG['TXT_HYPERLINKS'] = "Hyperlinks";
+$_ARRAYLANG['TXT_EXTERNAL_SOURCE'] = "Externe bronnen";
+$_ARRAYLANG['TXT_LINK'] = "Link";
+$_ARRAYLANG['TXT_NEWS_NEWS_CONTENT'] = "Nieuwsinhoud";
+$_ARRAYLANG['TXT_IMPORTANT_HYPERLINKS'] = "Gerelateerde hyperlinks";
+$_ARRAYLANG['TXT_NEWS_SOURCE'] = "Bron";
+$_ARRAYLANG['TXT_LAST_UPDATE'] = "Laatste update";
+$_ARRAYLANG['TXT_PERFORM'] = "Uitvoeren";
+$_ARRAYLANG['TXT_NO_NEWS_FOUND'] = "Er is geen nieuws gevonden!";
+$_ARRAYLANG['TXT_NEWS_FEED_TITLE'] = "Niewsfeed titel";
+$_ARRAYLANG['TXT_NEWS_FEED_DESCRIPTION'] = "Niewsfeed omschrijving";
+$_ARRAYLANG['TXT_SUBMIT_NEWS'] = "Nieuwsbericht toevoegen";
+$_ARRAYLANG['TXT_NEWS_SUCCESSFULLY_SUBMITED'] = "Het nieuwsbericht is succesvol toegevoegd!";
+$_ARRAYLANG['TXT_NEWS_SUBMIT_ERROR'] = "Er is een onbekende fout opgetreden bij het toevoegen van het nieuwsbericht!<br />Probeer het nogmaals.";
+$_ARRAYLANG['TXT_SET_NEWS_TITLE_AND_TEXT_OR_REDIRECT'] = "Er moet minimaal een titel en enige tekst aan het nieuwsbericht worden toegevoegd of een redirect worden ingesteld!";
+$_ARRAYLANG['TXT_NEWS_SHOW_NEWS'] = "Nieuws weergeven";
+$_ARRAYLANG['TXT_NEWS_OPTIONAL_VARS'] = "Deze variabele zijn optioneel";
+$_ARRAYLANG['TXT_NEWS_FONT_COLOR'] = "Tekstkleur";
+$_ARRAYLANG['TXT_NEWS_FONT_SIZE'] = "Tekstgrootte";
+$_ARRAYLANG['TXT_NEWS_FONT'] = "Lettertype";
+$_ARRAYLANG['TXT_NEWS_DISPLAY_LIMIT'] = "Aantal getoonde nieuwsberichten";
+$_ARRAYLANG['TXT_NEWS_SHOW_NEWS_DATE'] = "Datum van het nieuwsbericht weergeven";
+$_ARRAYLANG['TXT_NEWS_REDIRECT'] = "Een link naar een extern nieuwsbericht opnemen kan gerealiseerd worden door de betreffende link in de pagina in te voegen.";
+$_ARRAYLANG['TXT_NEWS_TEASER_TEXT'] = "Teaser tekst";
+$_ARRAYLANG['TXT_NEWS_TEASER_IMAGE'] = "Teaser afbeelding";
+$_ARRAYLANG['TXT_NEWS_NEWS_URL'] = "Nieuws URL";
+$_ARRAYLANG['TXT_SUBMIT'] = "Toevoegen";
+$_ARRAYLANG['TXT_NOTIFY_ADDRESS'] = "Hallo";
+$_ARRAYLANG['TXT_NOTIFY_SUBJECT'] = "News-Meldung zu bestätigen";
+$_ARRAYLANG['TXT_NOTIFY_MESSAGE'] = "Es wurde eine neue News-Meldung eingetragen, die noch freigeschalten werden muss. Unter nachfolgender URL können Sie dies tun:";
+$_ARRAYLANG['TXT_CAPTCHA_ERROR'] = "Der eingegebene Captcha-Code ist nicht korrekt!";
+$_ARRAYLANG['TXT_CAPTCHA'] = "Bitte geben Sie die abgebildeten Zeichen ein:";
+$_ARRAYLANG['TXT_NEWS_MORE'] = "more";
+$_ARRAYLANG['TXT_NEWS_COMMENTS_NONE_EXISTING'] = "No comments found";
+$_ARRAYLANG['TXT_NEWS_LIB_POSTED_BY'] = "posted by [USER] at [DATE]";
+$_ARRAYLANG['TXT_NEWS_COMMENTS_SAVING_ERROR'] = "Comment saving error";
+$_ARRAYLANG['TXT_NEWS_COMMENTS_NOT_VALID_TITLE'] = "Not valid comment title";
+$_ARRAYLANG['TXT_NEWS_COMMENTS_NOT_VALID_MESSAGE'] = "Not valid comment message";
+$_ARRAYLANG['TXT_NEWS_COMMENTS_NOT_VALID_AUTHOR'] = "Not valid author name";
+$_ARRAYLANG['TXT_NEWS_COMMENTS_NOT_VALID_CAPTCHA'] = "Not valid code";
+$_ARRAYLANG['TXT_NEWS_COMMENTS_SPAM_PREVENT'] = "Spam prevent";
+$_ARRAYLANG['TXT_NEWS_COMMENTS_ADD_COMMENT'] = "Add comment";
+$_ARRAYLANG['TXT_NEWS_COMMENTS_AUTHOR'] = "Name";
+$_ARRAYLANG['TXT_NEWS_COMMENTS_TITLE'] = "Title";
+$_ARRAYLANG['TXT_NEWS_COMMENTS_MESSAGE'] = "Message";
+$_ARRAYLANG['TXT_NEWS_COMMENTS_ADD'] = "Add";
+$_ARRAYLANG['TXT_NEWS_FRONTEND_DETAILS_COMMENT_INSERT_ERROR_ACTIVATED'] = "The comment-function is currently deactivated.";
+$_ARRAYLANG['TXT_NEWS_FRONTEND_DETAILS_COMMENT_INSERT_MAIL_SUBJECT'] = "New comment with title \\\"[SUBJECT]\\\"";
+$_ARRAYLANG['TXT_NEWS_FRONTEND_DETAILS_COMMENT_INSERT_MAIL_BODY'] = "[USERNAME] has added a new comment to the news with the title \\\"[SUBJECT]\\\". The message is:\n\n----------\n[COMMENT]\n----------\n\nDepending on your system-settings you have to activate this comment in your controlpanel before it is visible to your visitors.";
+$_ARRAYLANG['TXT_NEWS_FRONTEND_DETAILS_COMMENT_INSERT_ERROR_TIMEOUT'] = "Before writing another comment you have to wait [SECONDS] seconds.";
+$_ARRAYLANG['TXT_NEWS_COMMENTS'] = "Comments";
+$_ARRAYLANG['TXT_NEWS_MONTH_1'] = "January";
+$_ARRAYLANG['TXT_NEWS_MONTH_2'] = "February";
+$_ARRAYLANG['TXT_NEWS_MONTH_3'] = "March";
+$_ARRAYLANG['TXT_NEWS_MONTH_4'] = "";
+$_ARRAYLANG['TXT_NEWS_MONTH_5'] = "May";
+$_ARRAYLANG['TXT_NEWS_MONTH_6'] = "June";
+$_ARRAYLANG['TXT_NEWS_MONTH_7'] = "July";
+$_ARRAYLANG['TXT_NEWS_MONTH_8'] = "August";
+$_ARRAYLANG['TXT_NEWS_MONTH_9'] = "September";
+$_ARRAYLANG['TXT_NEWS_MONTH_10'] = "Oktober";
+$_ARRAYLANG['TXT_NEWS_MONTH_11'] = "November";
+$_ARRAYLANG['TXT_NEWS_MONTH_12'] = "Desember";
+$_ARRAYLANG['TXT_NEWS_NOT_FOUND'] = "News not found";
+?>
---
+++ core_modules/news/lib/headlines.class.php
@@ -0,0 +1,176 @@
+<?php
+
+/**
+ * News headlines
+ * @copyright   CONTREXX CMS - COMVATION AG
+ * @author Comvation Development Team <info@comvation.com>
+ * @version 1.0.0
+ * @package     contrexx
+ * @subpackage  core_module_news
+ * @todo        Edit PHP DocBlocks!
+ */
+
+/**
+ * @ignore
+ */
+require_once ASCMS_CORE_MODULE_PATH . '/news/lib/newsLib.class.php';
+
+/**
+ * News headlines
+ *
+ * Gets all the news headlines
+ * @copyright   CONTREXX CMS - COMVATION AG
+ * @author Comvation Development Team <info@comvation.com>
+ * @access public
+ * @version 1.0.0
+ * @package     contrexx
+ * @subpackage  core_module_news
+ */
+class newsHeadlines extends newsLibrary
+{
+    public $_pageContent;
+    public $_objTemplate;
+    public $arrSettings = array();
+
+    function __construct($pageContent)
+    {
+        $this->getSettings();
+        $this->_pageContent = $pageContent;
+        $this->_objTemplate = new HTML_Template_Sigma('.');
+        CSRF::add_placeholder($this->_objTemplate);
+    }
+
+
+    function getSettings()
+    {
+        global $objDatabase;
+
+        $objResult = $objDatabase->Execute("
+            SELECT name, value FROM ".DBPREFIX."module_news_settings");
+        if ($objResult !== false) {
+            while (!$objResult->EOF) {
+                $this->arrSettings[$objResult->fields['name']] = $objResult->fields['value'];
+                $objResult->MoveNext();
+            }
+        }
+    }
+
+
+    function getHomeHeadlines($catId=0)
+    {
+        global $_CORELANG, $objDatabase, $_LANGID;
+
+        $i = 0;
+        $catId= intval($catId);
+
+        $this->_objTemplate->setTemplate($this->_pageContent,true,true);
+        $this->_objTemplate->setCurrentBlock('headlines_row');
+
+        $newsLimit = intval($this->arrSettings['news_headlines_limit']);
+        if ($newsLimit>50) { //limit to a maximum of 50 news
+            $newsLimit=50;
+        }
+
+        if ($newsLimit<1) { //do not get any news if 0 was specified as the limit.
+            $objResult=false;
+        } else {//fetch news
+            $objResult = $objDatabase->SelectLimit("
+                SELECT tblN.id AS id,
+                       tblN.`date`, 
+                       tblN.teaser_image_path,
+                       tblN.teaser_image_thumbnail_path,
+                       tblN.redirect,
+                       tblN.publisher,
+                       tblN.publisher_id,
+                       tblN.author,
+                       tblN.author_id,
+                       tblL.title AS title, 
+                       tblL.teaser_text
+                  FROM ".DBPREFIX."module_news AS tblN
+            INNER JOIN ".DBPREFIX."module_news_locale AS tblL ON tblL.news_id=tblN.id
+                  WHERE tblN.status=1".
+                   ($catId > 0 ? " AND tblN.catid=$catId" : '')."
+                   AND tblN.teaser_only='0'
+                   AND tblL.lang_id=".$_LANGID."
+                   AND (startdate<='".date('Y-m-d H:i:s')."' OR startdate='0000-00-00 00:00:00')
+                   AND (enddate>='".date('Y-m-d H:i:s')."' OR enddate='0000-00-00 00:00:00')".
+                   ($this->arrSettings['news_message_protection'] == '1' && !Permission::hasAllAccess()
+                      ? (($objFWUser = FWUser::getFWUserObject()) && $objFWUser->objUser->login()
+                          ? " AND (frontend_access_id IN (".
+                            implode(',', array_merge(array(0), $objFWUser->objUser->getDynamicPermissionIds())).
+                            ") OR userid=".$objFWUser->objUser->getId().") "
+                          : " AND frontend_access_id=0 ")
+                      : '').
+                   "ORDER BY date DESC", $newsLimit);
+        }
+
+        if ($objResult !== false && $objResult->RecordCount() >= 0) {
+            while (!$objResult->EOF) {
+                $newsid    = $objResult->fields['id'];
+                $newstitle = $objResult->fields['title'];
+                $newsUrl    = empty($objResult->fields['redirect'])
+                                ? CONTREXX_SCRIPT_PATH.'?section=news&cmd=details&newsid='.$newsid
+                                : $objResult->fields['redirect'];
+                $htmlLink   = self::parseLink($newsUrl, $newstitle, contrexx_raw2xhtml($newstitle), 'headlineLink');
+
+                list($image, $htmlLinkImage, $imageSource) = self::parseImageThumbnail($objResult->fields['teaser_image_path'],
+                                                                                       $objResult->fields['teaser_image_thumbnail_path'],
+                                                                                       $newstitle,
+                                                                                       $newsUrl);
+
+                $author     = FWUser::getParsedUserTitle($objResult->fields['author_id'], $objResult->fields['author']);
+                $publisher  = FWUser::getParsedUserTitle($objResult->fields['publisher_id'], $objResult->fields['publisher']);
+
+                $this->_objTemplate->setVariable(array(
+                    'NEWS_ID'           => $newsid,
+                    'NEWS_CSS'          => 'row'.($i % 2 + 1),
+                    'NEWS_DATE'         => date(ASCMS_DATE_SHORT_FORMAT, $objResult->fields['date']),
+                    'NEWS_LONG_DATE'    => date(ASCMS_DATE_FORMAT, $objResult->fields['date']),
+                    'NEWS_TITLE'        => contrexx_raw2xhtml($newstitle),
+                    'NEWS_TEASER'       => nl2br($objResult->fields['teaser_text']),
+                    'NEWS_LINK'         => $htmlLink,
+                    'NEWS_LINK_URL'     => contrexx_raw2xhtml($newsUrl),
+                    'NEWS_AUTHOR'       => contrexx_raw2xhtml($author),
+                    'NEWS_PUBLISHER'    => contrexx_raw2xhtml($publisher),
+
+                    // Backward compatibility for templates pre 3.0
+                    'HEADLINE_ID'       => $newsid,
+                    'HEADLINE_DATE'     => date(ASCMS_DATE_SHORT_FORMAT, $objResult->fields['date']),
+                    'HEADLINE_TEXT'     => nl2br($objResult->fields['teaser_text']),
+                    'HEADLINE_LINK'     => $htmlLink,
+                    'HEADLINE_AUTHOR'   => contrexx_raw2xhtml($author),
+                ));
+
+                if (!empty($image)) {
+                    $this->_objTemplate->setVariable(array(
+                        'NEWS_IMAGE'         => $image,
+                        'NEWS_IMAGE_SRC'     => contrexx_raw2xhtml($imageSource),
+                        'NEWS_IMAGE_ALT'     => contrexx_raw2xhtml($newstitle),
+                        'NEWS_IMAGE_LINK'    => $htmlLinkImage,
+
+                        // Backward compatibility for templates pre 3.0
+                        'HEADLINE_IMAGE_PATH'=> contrexx_raw2xhtml($imageSource),
+                    ));
+
+                    if ($this->_objTemplate->blockExists('news_image')) {
+                        $this->_objTemplate->parse('news_image');
+                    }
+                } else {
+                    if ($this->_objTemplate->blockExists('news_image')) {
+                        $this->_objTemplate->hideBlock('news_image');
+                    }
+                }
+
+                $this->_objTemplate->parseCurrentBlock();
+                $i++;
+                $objResult->MoveNext();
+            }
+        } else {
+            $this->_objTemplate->hideBlock('headlines_row');
+        }
+        $this->_objTemplate->setVariable("TXT_MORE_NEWS", $_CORELANG['TXT_MORE_NEWS']);
+        return $this->_objTemplate->get();
+    }
+}
+
+?>
