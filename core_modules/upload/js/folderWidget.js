f4f2d1c17cdd3ff6901581007f280f3ac806493e
Thomas Däppen <thomas.daeppen@cloudrexx.com>
2011-11-14 10:32:24 +0000

    upload: added newest version from trunk
    git-svn-id: http://svn.contrexxlabs.com/contrexx/branches/work/cm_2_3@14687 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

---
+++ core_modules/upload/js/folderWidget.js
@@ -0,0 +1,65 @@
+var FolderWidget = function(options) {
+    var container = options.container; //the DOM container where we populate the list into
+    var id = options.id; //the unique widget id
+    var refreshUrl = options.refreshUrl; //the url to get fresh folder data from
+    var deleteUrl = options.deleteUrl; //the url used to send a delete request for a file
+    var files = []; //the files in target folder
+    var $ = $J; //jquery at $ locally
+
+    var refresh = function() {
+        $.getJSON(
+            refreshUrl,
+            {
+                folderWidgetId: id
+            },
+            function(json) {
+                container.empty();
+                var ul = $J('<ul></ul>');
+                files = json;
+                $.each(files, function(index, file) {
+                    var span = $J('<span></span>').html(file);
+                    var li = $J('<li></li>').append(span);
+                    var del = $J(' &nbsp; <a></a>');
+                    del.html('delete');
+                    del.addClass('deleteIcon');
+                    del.attr('href','');
+                    del.bind('click', function() {
+                        var fileElement = li;
+                        var fileName = file;
+                        $.get(deleteUrl,
+                            {
+                                file: file,
+                                folderWidgetId: id
+                            },
+                            function() {                               
+                                fileElement.remove(); //remove the li elem representing the file
+                                //remove the file from our local files array
+                                //this makes sure isEmpty() doesn't lie 
+                                for(index in files) {
+                                    var file = files[index];
+                                    if(file == fileName)
+                                        files.splice(index, 1);
+                                }
+                            });
+                        return false;
+                    });
+                    del.appendTo(li);
+                    li.appendTo(ul);
+                });
+                ul.appendTo(container);
+            }
+        );
+    };
+
+    //load the files
+    refresh();
+
+    var isEmpty = function() {
+        return files.length == 0;
+    };
+
+    return {
+        refresh: refresh,
+        isEmpty: isEmpty
+    };
+};
---
+++ core_modules/upload/lib/formUploader.class.php
@@ -0,0 +1,129 @@
+<?php
+require_once ASCMS_CORE_MODULE_PATH.'/upload/lib/uploader.class.php';
+/**
+ * FormUploader - Class for upload via HTML input-tags.
+ */
+class FormUploader extends Uploader
+{
+    /**
+     * @override
+     */     
+    public function handleRequest()
+    {
+        global $sessionObj;
+        global $_FILES;
+
+        //get a writable directory
+        $targetDir = '/upload_'.$this->uploadId;
+        $tempPath = $sessionObj->getTempPath();
+        $webTempPath = $sessionObj->getWebTempPath();
+
+        //create a file manager
+        require_once ASCMS_FRAMEWORK_PATH.'/File.class.php';
+        $fm = new File();
+        //make sure target directory exists
+        if(!file_exists($tempPath.$targetDir))
+            $fm->mkdir($tempPath, $webTempPath, $targetDir);
+
+        //move all uploaded file to this upload's temp directory
+        foreach($_FILES["uploaderFiles"]["error"] as $key => $error) {
+            if($error == UPLOAD_ERR_OK) {
+                $tmpName = $_FILES["uploaderFiles"]["tmp_name"][$key];
+                $name = $_FILES["uploaderFiles"]["name"][$key];
+
+                if (!FWValidator::is_file_ending_harmless($name)) {
+                    die('Error:'.sprintf('The file %s was refused due to its file extension which is not allowed!', htmlentities($name, ENT_QUOTES, CONTREXX_CHARSET)));
+                }
+
+                //TODO: Uploader::addChunk does this also -> centralize in function
+                // remember the "raw" file name, we want to store all original
+                // file names in the session.
+                $originalFileName = $name;
+                // Clean the fileName for security reasons
+                $name = preg_replace('/[^\w\._]+/', '', $name);
+                $sessionKey = 'upload_originalFileNames_'.$this->uploadId;
+                $originalFileNames = array();
+                if(isset($_SESSION[$sessionKey]))
+                    $originalFileNames = $_SESSION[$sessionKey];
+                $originalFileNames[$name] = $originalFileName;
+                $_SESSION[$sessionKey] = $originalFileNames;
+                //end of TODO-region
+                
+                //move file somewhere we know both the web- and normal path...
+                @move_uploaded_file($tmpName,ASCMS_TEMP_PATH.'/'.$name);    
+                //...then do a safe-mode-safe (yeah) move operation
+                $fm->moveFile(ASCMS_TEMP_PATH, ASCMS_TEMP_WEB_PATH, '/'.$name, $tempPath.$targetDir, $webTempPath.$targetDir);
+            }
+        }
+
+        //and call back.
+        $this->notifyCallback();
+        //redirect the user where he belongs
+        $this->redirect();
+    }
+
+    /**
+     * @override
+     */     
+    public function getXHtml()
+    {
+        $iframeUrl = '';   
+        if($this->isBackendRequest)
+            $iframeUrl = ASCMS_ADMIN_WEB_PATH.'/index.php?cmd=upload&act=formUploaderFrame&uploadId='.$this->uploadId;
+        else
+            $iframeUrl = CONTREXX_SCRIPT_PATH.'?section=upload&cmd=formUploaderFrame&uploadId='.$this->uploadId;
+      
+        $tpl = new HTML_Template_Sigma(ASCMS_CORE_MODULE_PATH.'/upload/template/uploaders');
+        $tpl->setErrorHandling(PEAR_ERROR_DIE);
+
+        $tpl->loadTemplateFile('form.html');
+        $tpl->setVariable('IFRAME_URL', $iframeUrl);
+        
+        return $tpl->get();
+    }
+
+    public function getFrameXHtml() {
+		global $_CORELANG;
+	
+        //JS / CSS dependencies
+        JS::activate('cx');
+        JS::registerCSS('core_modules/upload/css/uploaders/form/formUploader.css');
+        JS::registerJS('core_modules/upload/js/uploaders/form/formUploader.js');
+        
+        $uploadPath = $this->getUploadPath('form');
+
+        $redirectUrl = '';   
+        if($this->isBackendRequest)
+            $redirectUrl = ASCMS_ADMIN_WEB_PATH.'/index.php?cmd=upload&act=formUploaderFrameFinished&uploadId='.$this->uploadId;
+        else
+            $redirectUrl = ASCMS_PATH_OFFSET.'/index.php?section=upload&cmd=formUploaderFrameFinished&uploadId='.$this->uploadId;
+        $this->setRedirectUrl($redirectUrl);
+      
+        $tpl = new HTML_Template_Sigma(ASCMS_CORE_MODULE_PATH.'/upload/template/uploaders');
+        $tpl->setErrorHandling(PEAR_ERROR_DIE);
+        
+        $tpl->loadTemplateFile('formFrame.html');
+        $tpl->setVariable('UPLOAD_URL', $uploadPath);
+        $tpl->setVariable('INCLUDES', JS::getCode());
+        $tpl->setVariable('CXJS_INIT_JS', ContrexxJavascript::getInstance()->initJs());
+		$tpl->setVariable('UPLOAD_FORM_ADD', $_CORELANG['UPLOAD_FORM_ADD']);
+		$tpl->setVariable('UPLOAD', $_CORELANG['UPLOAD']);
+        
+        require_once ASCMS_FRAMEWORK_PATH.'/System.class.php';
+        $tpl->setVariable('MAX_FILE_SIZE', FWSystem::getMaxUploadFileSize()-1000);
+        
+        return $tpl->get();
+    }
+
+    public function getFrameFinishedXHtml() {
+        global $_CORELANG;
+        $tpl = new HTML_Template_Sigma(ASCMS_CORE_MODULE_PATH.'/upload/template/uploaders');
+        $tpl->setErrorHandling(PEAR_ERROR_DIE);
+
+        $tpl->loadTemplateFile('formFrameFinished.html');
+
+        $tpl->setVariable('FINISHED_MESSAGE',htmlentities($_CORELANG['UPLOAD_FINISHED'], ENT_QUOTES, CONTREXX_CHARSET));        
+       
+        return $tpl->get();
+    }
+}
