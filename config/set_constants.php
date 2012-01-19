5bae426a2f829e9cfc2370c2cebf4a42501d1b8a
Michael Raess <michael.raess@comvation.com>
2012-01-19 13:08:38 +0000

    mediadir (bugfix #844): - added missing constants
    git-svn-id: http://svn.contrexxlabs.com/contrexx/trunk@16026 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- config/set_constants.php
+++ config/set_constants.php
@@ -107,4 +107,6 @@ define('ASCMS_ECARD_THUMBNAIL_PATH',	    ASCMS_DOCUMENT_ROOT.'/images/modules/ec
 define('ASCMS_ECARD_THUMBNAIL_WEB_PATH',	ASCMS_PATH_OFFSET.'/images/modules/ecard/thumbnails');
 define('ASCMS_PARTNERS_IMAGES_PATH',        ASCMS_DOCUMENT_ROOT.'/images/partners');
 define('ASCMS_PARTNERS_IMAGES_WEB_PATH',    ASCMS_PATH_OFFSET.'/images/partners');
+define('ASCMS_MEDIADIR_IMAGES_PATH',        ASCMS_DOCUMENT_ROOT.'/images/mediadir');
+define('ASCMS_MEDIADIR_IMAGES_WEB_PATH',    ASCMS_PATH_OFFSET.'/images/mediadir');
 ?>
