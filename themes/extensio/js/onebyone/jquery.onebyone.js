981b206ff1a8e776c283d9cf5dcb77ab351e42de
ss4u <comvation@softsolutions4u.info>
2012-06-12 04:33:24 +0000

    Changed the theme name contrexx_theme_new to extensio edited the design of headlines.html added the content blocks for testimonial and twitter in home page
    git-svn-id: http://svn.contrexxlabs.com/contrexx/trunk@19157 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- themes/extensio/js/onebyone/jquery.onebyone.js
+++ themes/extensio/js/onebyone/jquery.onebyone.js
@@ -2,13 +2,10 @@
 	Author: http://codecanyon.net/user/sike
 */
 ;(function($) {	
- 	
 	// the available ease animation style
 	var easeTypeArr = ['rollIn', 'fadeIn', 'fadeInUp', 'fadeInDown', 'fadeInLeft', 'fadeInRight', 'fadeInRight', 'bounceIn', 'bounceInDown', 'bounceInUp', 'bounceInLeft', 'bounceInRight', 'rotateIn', 'rotateInDownLeft', 'rotateInDownRight', 'rotateInUpLeft', 'rotateInUpRight'];
 	var easeTypeNum = easeTypeArr.length;			    
-	
     $.fn.oneByOne = function(options) {  
-	
 	   	// plugin default options
 		var settings = { 
 			className: 'oneByOne', 
@@ -24,16 +21,13 @@
 			slideShow: false,
 			slideShowDelay: 3000
 		}; 	          	
-
 		// extends settings with options provided
         if (options) {
 			$.extend(settings, options);
 		}                    
-		
 		var parent;
 		var _this;	
 		var currentBannerNum = -1;	 
-	
 		var bannerWidth = settings.width; 
 		var bannerHeight = settings.height;
 		var xdiff = 0;    
@@ -42,18 +36,13 @@
 		// store the ease animation style of each slider
 		var realEaseTypeArr = []; 
 		var _easeType;
-		                               		
 		var bannerArr = [];     
 		var bannerNum = 0;    
 		var _preBannerNum = 0, buttonArea, buttonCon, arrowButton; 		             		    
-		
-		
 		_this = this; 	        
 		_this.wrap('<div class="' + settings.className + '"/>');  	 
 		parent = _this.parent();                                 
-		
 		parent.css('overflow', 'hidden')
-		
 		_this.find('.'+settings.sliderClassName).each(function(index) { 
 			/*				
 			if($(this).children().length>1){        
@@ -66,9 +55,7 @@
 			bannerNum++;		
 			$(this).css('left', bannerWidth*index)
 			bannerArr[index] = $(this);  
-
 		});     
-		
 	    // add the mobile touch screen support 
 	   	_this.bind('touchstart', function (event) {
             event.preventDefault();
@@ -81,7 +68,6 @@
 			if(arrowButton)arrowButton.fadeIn();
 			return false;
 		});                
-
 		_this.bind('touchmove', function (event) {
 			event.preventDefault();
 			var touch = event.originalEvent.touches[0] || event.originalEvent.changedTouches[0];
@@ -94,7 +80,6 @@
 			}
 			return false;
 		});        
-
 		_this.bind('touchend', function (event) {
 			var _n = currentBannerNum;
 			event.preventDefault();  
@@ -122,7 +107,6 @@
 			if(arrowButton)arrowButton.delay(400).fadeOut();
 			return false;            
 		})    
-		
 		// enable or disable the drag function of the slider
 		if(settings.enableDrag){
  			_this.mousedown(function (event) {  
@@ -132,7 +116,6 @@
                 }         
                 return false;  
             });                                         
-                    
             _this.mousemove(function (event) {
                 if (isMouseDown) {             
                    	xdiff = event.pageX - this.mouseX;   	
@@ -140,12 +123,9 @@
 					if (settings.slideShow) {
 						stopSlideShow();
 					} 
-					
 				}
                 return false;
             });       
-
-		
 			_this.mouseup(function (event) {
                 isMouseDown = false;     
 				var _n = currentBannerNum;
@@ -156,7 +136,6 @@
                     _n++;
                     _n = _n >= bannerNum ? bannerNum - 1 : _n;
             		tweenTo(_n);
-
                 } else if (xdiff > halfWidth - fullWidth * settings.tolerance) {
                     _n--;
                     _n = _n < 0 ? 0 : _n;
@@ -166,28 +145,22 @@
 					if (settings.slideShow) {
 						startSlideShow();
 					} 
-					
                 }
                 xdiff = 0;
                 return false;
             });  
-
             _this.mouseleave(function (event) {
                 $(this).mouseup();
             });    				
-			
 		}
- 		    
 		parent.mouseover(function (event){
 			if(buttonCon)buttonCon.fadeIn(); 
 			if(arrowButton)arrowButton.fadeIn(); 				
 		});           
-		
 		parent.mouseleave(function (event){
 			if(buttonCon)buttonCon.fadeOut(); 
 			if(arrowButton)arrowButton.fadeOut();
 		});  
-		
   		// add the circle buttons
 		if(settings.showButton){
             buttonArea = $('<div class="buttonArea"><div class="buttonCon"></div></div>');
@@ -197,16 +170,13 @@
             for(var i = 0; i < bannerNum; i++){
 					buttonCon.append('<a class="theButton" rel="'+ i +'">'+ (i + 1) +'</a>').css('cursor', 'pointer');
             }              
-                 
             $('.buttonCon a:eq('+ currentBannerNum +')', buttonArea).addClass('active');
             $('.buttonCon a', buttonArea).bind('click', function(event){        			  
                 if($(this).hasClass('active')) return false;   
 				var _n = $(this).attr('rel'); 	
 				tweenTo(_n);
             });	 
-		
 		}
-
         // add the previous/next arrow buttons
 		if(settings.showArrow){
 			arrowButton = $('<div class="arrowButton"><div class="prevArrow"></div><div class="nextArrow"></div></div>');
@@ -217,34 +187,24 @@
             var _prev = $('.prevArrow', arrowButton).bind('click', function(event) {
 				prevBanner();
             });
-
 		}                
-		
-		
-		
   		if(buttonCon)buttonCon.hide(); 
 		if(arrowButton)arrowButton.hide();
-		
 		tweenTo(0);   
-		
 		if(settings.slideShow) {
 			slideShowInt = setInterval(function() {
 				nextBanner();
 			}, settings.slideShowDelay);
 			_this.data('interval', slideShowInt);
 		} 		
-		  	    
-		
 		// move to the certain slider via a number, display the slider content one by one
 		function tweenTo(n){    
 			if (settings.slideShow) {
 				stopSlideShow();
 			}  
-
 			_this.stop(true, true).animate({
 				left: -n * bannerWidth
 			},  settings.delay, function(){
-
 				if(n!=currentBannerNum){
 				_preBannerNum = currentBannerNum;	 
 				if(bannerArr[_preBannerNum]){
@@ -252,12 +212,9 @@
 						if(!($.browser.msie||$.browser.opera)){
 						 	bannerArr[_preBannerNum].fadeOut();
 						} 
-					
 					// }       
-				
 	           		$('.buttonArea a:eq('+ _preBannerNum +')', parent).removeClass('active');	    							
 				}				
-			
 	           	$('.buttonArea a:eq('+ n +')', parent).addClass('active');
 			    if(settings.easeType.toLowerCase()!="random"){   
 	 				bannerArr[n].show().children().each(function(index) {
@@ -269,7 +226,6 @@
 						$(this).show().addClass("animate"+_n+ " " + settings.easeType);
 					});							
 				}else{ 
-				
 					_easeType = easeTypeArr[Math.floor(Math.random()*easeTypeNum)];	
 					realEaseTypeArr[n] = _easeType; 																													
 					if(bannerArr[_preBannerNum]){
@@ -279,15 +235,12 @@
 								$(this).hide();
 							} 	                                         
 						});							
-					
 					}
-
 	 				bannerArr[n].show().children().each(function(index) {								           
 						var _n = index;
 						$(this).show().addClass("animate"+_n+ " " + _easeType);
 					});							
 				}
-				
 					// isTweenning = false;     
 					_this.delay(bannerArr[n].children().length*200).queue(function(){
 						if(settings.slideShow) startSlideShow();
@@ -296,16 +249,10 @@
 					currentBannerNum = n;	                    
 		   		}  					    
 			});     		 
-			  			  	
 		}      
-
-  
-	
-	
 	   	function stopSlideShow() {
 				clearInterval(_this.data('interval'));
 			}
-
 		function startSlideShow() {
 			clearInterval(_this.data('interval'));
 			slideShowInt = setInterval(	function(){
@@ -313,28 +260,18 @@
 			},settings.slideShowDelay);
 			_this.data('interval', slideShowInt);
 		}  
-   
- 		
-	
 	  	function nextBanner(){
 			var _n = currentBannerNum;
 			_n++;      
 			_n = _n >= bannerNum ? 0 : _n;					
 			tweenTo(_n);
-		
 		}  
-	
 		function prevBanner(){
 			var _n = currentBannerNum;
 			_n--;                       
 			_n = _n < 0 ? bannerNum - 1 : _n;					
 			tweenTo(_n);
-		
 		}  		
-			                 
         return this;
-
     };           
-  	    
-
 })(jQuery);
