c4720ebd3d1901fd6f400cdc3762ba09d8519e3a
Michael Ritter <michael.ritter@cloudrexx.com>
2012-08-09 13:26:49 +0000

    ContentManager - addition - added some content to 'Blocks'-tab Contrexx - change - Moved module 'block' to core_modules
    git-svn-id: http://svn.contrexxlabs.com/contrexx/trunk@20785 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

---
+++ core_modules/block/lang/TE/frontend.php
@@ -0,0 +1,8 @@
+<?php
+/**
+* Contrexx CMS
+* generated date Tue, 23 Jan 2007 16:34:47 +0100
+**/
+
+$_ARRAYLANG['TXT_BLOCK_OVERVIEW'] = "Übersicht";
+?>
---
+++ core_modules/block/lang/en/backend.php
@@ -0,0 +1,103 @@
+<?php
+/**
+ * @copyright   CONTREXX CMS - COMVATION AG
+ * @author      Comvation Development Team <info@comvation.com>
+ * @access      public
+ * @package     contrexx
+ * @subpackage  module_block
+ */
+$_ARRAYLANG['TXT_BLOCK_MODIFY_BLOCK'] = "Modify Content Block %s";
+$_ARRAYLANG['TXT_BLOCK_ADD_BLOCK'] = "Add Content Block";
+$_ARRAYLANG['TXT_BLOCK_BLOCKS'] = "Content Blocks";
+$_ARRAYLANG['TXT_BLOCK_NAME'] = "Name";
+$_ARRAYLANG['TXT_BLOCK_PLACEHOLDER'] = "Placeholder";
+$_ARRAYLANG['TXT_BLOCK_FUNCTIONS'] = "Functions";
+$_ARRAYLANG['TXT_BLOCK_CONTENT'] = "Content";
+$_ARRAYLANG['TXT_BLOCK_SAVE'] = "Store";
+$_ARRAYLANG['TXT_BLOCK_BLOCK_UPDATED_SUCCESSFULLY'] = "The Content Block has successfully been updated.";
+$_ARRAYLANG['TXT_BLOCK_BLOCK_COULD_NOT_BE_UPDATED'] = "The Content Block couldn't be updated!";
+$_ARRAYLANG['TXT_BLOCK_SETTINGS'] = "Settings";
+$_ARRAYLANG['TXT_BLOCK_USE_BLOCK_SYSTEM'] = "Use block system";
+$_ARRAYLANG['TXT_BLOCK_OVERVIEW'] = "Overview";
+$_ARRAYLANG['TXT_BLOCK_BASIC_DATA'] = "Basic Data";
+$_ARRAYLANG['TXT_BLOCK_ADDITIONAL_OPTIONS'] = "Additional Options";
+$_ARRAYLANG['TXT_BLOCK_SUBMIT_SELECT'] = "Select action";
+$_ARRAYLANG['TXT_BLOCK_SUBMIT_DELETE'] = "Delete selected";
+$_ARRAYLANG['TXT_BLOCK_SUBMIT_ACTIVATE'] = "Activate selected";
+$_ARRAYLANG['TXT_BLOCK_SUBMIT_DEACTIVATE'] = "Deactivate selected";
+$_ARRAYLANG['TXT_BLOCK_SELECT_ALL'] = "Select all";
+$_ARRAYLANG['TXT_BLOCK_DESELECT_ALL'] = "Delete selected";
+$_ARRAYLANG['TXT_BLOCK_RANDOM'] = "Included In random generator";
+$_ARRAYLANG['TXT_BLOCK_DELETE_SELECTED_BLOCKS'] = "Do you really want to delete the selected Content Blocks?";
+$_ARRAYLANG['TXT_BLOCK_NO_NAME'] = "[No name]";
+$_ARRAYLANG['TXT_BLOCK_USE_BLOCK_RANDOM'] = "Use random generator";
+$_ARRAYLANG['TXT_BLOCK_USE_BLOCK_RANDOM_PLACEHOLDER'] = "[[BLOCK_RANDOMIZER]] Placeholder for the random generator";
+$_ARRAYLANG['TXT_BLOCK_ACTIVE'] = "Active";
+$_ARRAYLANG['TXT_BLOCK_INACTIVE'] = "Inactive";
+$_ARRAYLANG['TXT_BLOCK_FRONTEND_LANGUAGES'] = "Frontend languages";
+$_ARRAYLANG['TXT_BLOCK_BLOCK_ADDED_SUCCESSFULLY'] = "The Content Block %s has been added successfully.";
+$_ARRAYLANG['TXT_BLOCK_BLOCK_COULD_NOT_BE_ADDED'] = "An error occurred while adding the Content Block!";
+$_ARRAYLANG['TXT_BLOCK_COULD_NOT_DELETE_BLOCK'] = "An error occurred while deleting the Content Block %s!";
+$_ARRAYLANG['TXT_BLOCK_FAILED_TO_DELETE_BLOCKS'] = "An error occurred while deleting the following Content Blocks: %s";
+$_ARRAYLANG['TXT_BLOCK_SUCCESSFULLY_DELETED'] = "The Content Block %s has been deleted successfully.";
+$_ARRAYLANG['TXT_BLOCK_BLOCKS_SUCCESSFULLY_DELETED'] = "The Content Blocks have been deleted successfully.";
+$_ARRAYLANG['TXT_BLOCK_OPERATION_IRREVERSIBLE'] = "This operation is irreversible!";
+$_ARRAYLANG['TXT_BLOCK_CONFIRM_DELETE_BLOCK'] = "Do you really want to delete the Content Block %s?";
+$_ARRAYLANG['TXT_BLOCK_COPY_BLOCK'] = "Copy Content Block %s";
+$_ARRAYLANG['TXT_BLOCK_DELETE_BLOCK'] = "Delete Content Block %s";
+$_ARRAYLANG['TXT_BLOCK_BLOCK_RANDOM'] = "Placeholder for random generator";
+$_ARRAYLANG['TXT_BLOCK_BLOCK_GLOBAL'] = "Placeholder for all blocks. Within this placeholder, all page and language assignments will be considered.";
+$_ARRAYLANG['TXT_BLOCK_GLOBAL_SEPERATOR'] = "Delimitter within global block";
+$_ARRAYLANG['TXT_BLOCK_GLOBAL_SEPERATOR_INFO'] = "Delimitter between individual blocks within the global block. You can use HTML.";
+$_ARRAYLANG['TXT_BLOCK_SUBMIT_GLOBAL'] = "Add selected to the global Content Block";
+$_ARRAYLANG['TXT_BLOCK_SUBMIT_GLOBAL_OFF'] = "Delete selected from global Content Block";
+$_ARRAYLANG['TXT_BLOCK_SHOW_IN_GLOBAL'] = "Display in global block";
+$_ARRAYLANG['TXT_BLOCK_STATUS'] = "Status";
+$_ARRAYLANG['TXT_BLOCK_BLOCK'] = "Content Block";
+$_ARRAYLANG['TXT_BLOCK_ACTIVATE'] = "Activate";
+$_ARRAYLANG['TXT_BLOCK_SHOW_BLOCK_ON_ALL_'] = "Show Content Block on all pages in this language";
+$_ARRAYLANG['TXT_BLOCK_CONTENT_PAGES'] = "Frontend pages";
+$_ARRAYLANG['TXT_BLOCK_SHOW_BLOCK_IN_THIS_LANGUAGE'] = "Display Content Block in this language";
+$_ARRAYLANG['TXT_BLOCK_DEACTIVATE'] = "deactivate";
+$_ARRAYLANG['TXT_DONT_SHOW_ON_PAGES'] = "On none pages";
+$_ARRAYLANG['TXT_SHOW_ON_ALL_PAGES'] = "On all pages";
+$_ARRAYLANG['TXT_SHOW_ON_SELECTED_PAGES'] = "Select pages";
+$_ARRAYLANG['TXT_BLOCK_SHOW_ON_ALL_PAGES'] = "Show Content Block on all pages in this language";
+$_ARRAYLANG['TXT_BLOCK_FRONTEND_PAGES'] = "Frontend pages";
+$_ARRAYLANG['TXT_BLOCK_LANG_SHOW'] = "Display Content Block in this language";
+$_ARRAYLANG['TXT_BLOCK_BLOCK_NO'] = "Block #%s";
+$_ARRAYLANG['TXT_BLOCK_BLOCK_ALL'] = "alle";
+$_ARRAYLANG['TXT_BLOCK_BLOCK_NONE'] = "-";
+$_ARRAYLANG['TXT_BLOCK_CATEGORIES'] = "Categories";
+$_ARRAYLANG['TXT_BLOCK_CATEGORIES_MANAGE'] = "Manage categories";
+$_ARRAYLANG['TXT_BLOCK_CATEGORIES_ADD'] = "Add category";
+$_ARRAYLANG['TXT_BLOCK_CATEGORIES_ADD_ERROR'] = "Beim Aktualisieren/Hinzufügen der Rubriken ist ein Fehler aufgetreten.";
+$_ARRAYLANG['TXT_BLOCK_CATEGORIES_ADD_OK'] = "Die Rubrik wurde aktualisiert/hinzugefügt.";
+$_ARRAYLANG['TXT_BLOCK_PARENT'] = "Parent category";
+$_ARRAYLANG['TXT_BLOCK_NONE'] = "Keine";
+$_ARRAYLANG['TXT_BLOCK_CATEGORIES_ALL'] = "All categories";
+$_ARRAYLANG['TXT_BLOCK_NO_CATEGORIES_FOUND'] = "Keine Rubriken gefunden";
+$_ARRAYLANG['TXT_BLOCK_CATEGORIES_DELETE_ERROR'] = "Beim Löschen einer Rubrik trat ein Fehler auf.";
+$_ARRAYLANG['TXT_BLOCK_CATEGORIES_DELETE_OK'] = "Rubrik(n) erfolgreich gelöscht.";
+$_ARRAYLANG['TXT_BLOCK_CATEGORIES_EDIT'] = "Modify category";
+$_ARRAYLANG['TXT_BLOCK_CATEGORIES_DELETE_INFO_HEAD'] = "Informationen zum Löschen von Kategorien";
+$_ARRAYLANG['TXT_BLOCK_CATEGORIES_DELETE_INFO'] = "Es können nur Kategorien gelöscht werden, welche keine Unterkategorien enthalten.<br />Falls eine Kategorie gelöscht wird, welche noch bei Blöcken zugewiesen ist, wird diese Zuweisung vorher entfernt.";
+$_ARRAYLANG['TXT_BLOCK_CATEGORY'] = "Rubrik";
+$_ARRAYLANG['TXT_BLOCK_SHOW_FROM'] = "From";
+$_ARRAYLANG['TXT_BLOCK_SHOW_UNTIL'] = "Till";
+$_ARRAYLANG['TXT_BLOCK_AVAILABLE_PAGES'] = "Available pages";
+$_ARRAYLANG['TXT_BLOCK_SELECTED_PAGES'] = "Selected pages";
+$_ARRAYLANG['TXT_BLOCK_SELECT_ALL'] = "Select All";
+$_ARRAYLANG['TXT_BLOCK_UNSELECT_ALL'] = "Unselect all";
+$_ARRAYLANG['TXT_BLOCK_SHOW_ALWAYS'] = "Immer anzeigen";
+$_ARRAYLANG['TXT_BLOCK_SHOW_TIMED'] = "Zeitgesteuertes Anzeigen";
+$_ARRAYLANG['TXT_BLOCK_LANGUAGE'] = "Language";
+$_ARRAYLANG['TXT_BLOCK_GLOBAL_PLACEHOLDERS'] = "Block im globalen Platzhalter anzeigen";
+$_ARRAYLANG['TXT_BLOCK_DISPLAY_TIME'] = "Anzeigedauer";
+$_ARRAYLANG['TXT_BLOCK_CONTENT'] = "Content";
+$_ARRAYLANG['TXT_BLOCK_ORDER']   = "Order";
+$_ARRAYLANG['TXT_BLOCK_INCLUDED_IN_GLOBAL_BLOCK'] = "Included in global block";
+$_ARRAYLANG['TXT_BLOCK_DISPLAY_SELECTED_PAGE'] = "Block only gets displayed on the following pages:";
+$_ARRAYLANG['TXT_BLOCK_DISPLAY_ALL_PAGE'] = "Block gets displayed on all pages";
+$_ARRAYLANG['TXT_BLOCK_USE_WYSIWYG_EDITOR'] = "Use WYSIWYG editor";
+?>
