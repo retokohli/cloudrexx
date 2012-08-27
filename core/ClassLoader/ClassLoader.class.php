73c879084fc3aa002b078d524f31c2bb7907a658
Michael Ritter <michael.ritter@cloudrexx.com>
2012-08-27 14:25:29 +0000

    Contrexx/Core - addition - Added class autoloader
    git-svn-id: http://svn.contrexxlabs.com/contrexx/trunk@21100 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

---
+++ core/ClassLoader/ClassLoader.class.php
@@ -0,0 +1,71 @@
+<?php
+namespace Cx\Core\ClassLoader;
+
+class ClassLoader {
+    private $legacyClassLoader;
+    
+    public function __construct() {
+        spl_autoload_register(array($this, 'autoload'));
+        $this->legacyClassLoader = new LegacyClassLoader();
+    }
+    
+    private function autoload($name) {
+        if ($this->load($name, $path)) {
+            return;
+        }
+        if ($path) {
+            //echo '<b>' . $name . ': ' . $path . '</b><br />';
+        }
+        $this->loadLegacy($name);
+    }
+    
+    private function load($name, &$resolvedPath) {
+        $parts = explode('\\', $name);
+        // new classes should be in namespace \Cx\something
+        if (!in_array(current($parts), array('Cx'/*, 'Doctrine'*/, 'Gedmo'/*, 'Symfony'*/)) || count($parts) < 3) {
+            return false;
+        }
+        
+        // Exception for model, its within /model/[entities|events]/cx/model/
+        if ($parts[0] == 'Cx' && $parts[1] == 'Model') {
+            $third = 'Entities';
+            if ($parts[2] == 'Events') {
+                $third = 'Events';
+            }
+            $parts = array_merge(array('Cx', 'Model', $third), $parts);
+        }
+        
+        // we don't need the Cx part
+        unset($parts[0]);
+        // core, lib, model, etc. are lowercase by design
+        $parts[1] = strtolower($parts[1]);
+        // but we need the original class name to find the correct file name
+        $className = end($parts);
+        unset($parts[count($parts)]);
+        reset($parts);
+        
+        // find matching path
+        $path = ASCMS_DOCUMENT_ROOT;
+        foreach ($parts as $part) {
+            // Switch these lines after renaming things according to namespaces
+            $part = '/' . $part;
+            if (!is_dir($path . $part)) {
+                break;
+            }
+            $path .= $part;
+        }
+        $className = preg_replace('/Exception/', '', $className);
+        $resolvedPath = $path . '/' . $className . '.class.php';
+        if (file_exists($path . '/' . $className . '.class.php')) {
+            //echo $name . ' :: ' . $path . '/' . $className . '<br />';
+            require_once($path . '/' . $className . '.class.php');
+            return true;
+        }
+        //echo '<span style="color: red;">' . implode('\\', $parts) . '</span>';
+        return false;
+    }
+    
+    private function loadLegacy($name) {
+        $this->legacyClassLoader->autoload($name);
+    }
+}
