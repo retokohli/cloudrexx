8eff1d24be62f26ad824deb63fd0ba52986ccd1e
Michael Ritter <michael.ritter@cloudrexx.com>
2012-08-29 09:29:05 +0000

    Cadmin index.php - update - ClassLoader gets loaded now Content Manager - update - Added method cx.cm.performAction to handle actions at a single point Content Manager - update - Removed unused links from action item list Content Manager - bugfix - Actions now do update the tree and the actions menu Content Manager - bugfix - Fixed page preview link
    git-svn-id: http://svn.contrexxlabs.com/contrexx/trunk@21140 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- core/contentmanager/ActionsRenderer.class.php
+++ core/contentmanager/ActionsRenderer.class.php
@@ -16,29 +16,25 @@ class ActionsRenderer
         $actions = array();
         
         // This shows the empty editor, sets parent node and hides actions menu
-        $actions[] = sprintf('<li class="action-item new" onclick="cx.cm.showEditor();jQuery(\'#parent_node\').val(' . $page->getNode()->getId() . ');jQuery(this).parent().remove();return false;">%3$s</li>', ' new', '', $_ARRAYLANG['TXT_CORE_CM_NEW']);
+        $actions[] = sprintf('<li class="action-item new">%3$s</li>', ' new', '', $_ARRAYLANG['TXT_CORE_CM_NEW']);
 
-        if ($page->isActive()) {
-            if ($page->getEditingStatus() == 'hasDraft' || $page->getEditingStatus() == 'hasDraftWaiting') {
-                $actions[] = sprintf(self::$actionItem, ' publish', "index.php?cmd=jsondata&object=page&act=set&action=publish&pageId={$page->getId()}", $_ARRAYLANG['TXT_CORE_PUBLISH_DRAFT']);
-            } else {
-                $actions[] = sprintf(self::$actionItem, ' deactivate', "index.php?cmd=jsondata&object=page&act=set&action=deactivate&pageId={$page->getId()}", $_ARRAYLANG['TXT_CORE_CM_UNPUBLISH']);
-            }
+        if ($page->getEditingStatus() == 'hasDraft' || $page->getEditingStatus() == 'hasDraftWaiting') {
+            $actions[] = sprintf(self::$actionItem, ' publish', '', $_ARRAYLANG['TXT_CORE_PUBLISH_DRAFT']);
         } else {
-            if ($page->getEditingStatus() == 'hasDraft' || $page->getEditingStatus() == 'hasDraftWaiting') {
-                $actions[] = sprintf(self::$actionItem, ' publish', "index.php?cmd=jsondata&object=page&act=set&action=publish&pageId={$page->getId()}", $_ARRAYLANG['TXT_CORE_PUBLISH_DRAFT']);
+            if ($page->isActive()) {
+                $actions[] = sprintf(self::$actionItem, ' deactivate', '', $_ARRAYLANG['TXT_CORE_CM_UNPUBLISH']);
             } else {
-                $actions[] = sprintf(self::$actionItem, ' activate', "index.php?cmd=jsondata&object=page&act=set&action=activate&pageId={$page->getId()}", $_ARRAYLANG['TXT_CORE_CM_PUBLISH']);
+                $actions[] = sprintf(self::$actionItem, ' activate', '', $_ARRAYLANG['TXT_CORE_CM_PUBLISH']);
             }
         }
 
         if ($page->isVisible()) {
-            $actions[] = sprintf(self::$actionItem, ' hide', "index.php?cmd=jsondata&object=page&act=set&action=hide&pageId={$page->getId()}", $_ARRAYLANG['TXT_CORE_CM_HIDE']);
+            $actions[] = sprintf(self::$actionItem, ' hide', '', $_ARRAYLANG['TXT_CORE_CM_HIDE']);
         } else {
-            $actions[] = sprintf(self::$actionItem, ' show', "index.php?cmd=jsondata&object=page&act=set&action=show&pageId={$page->getId()}", $_ARRAYLANG['TXT_CORE_CM_SHOW']);
+            $actions[] = sprintf(self::$actionItem, ' show', '', $_ARRAYLANG['TXT_CORE_CM_SHOW']);
         }
 
-        $actions[] = sprintf(self::$actionItem, ' delete', "index.php?cmd=jsondata&object=node&act=delete&id={$page->getNode()->getId()}", $_ARRAYLANG['TXT_CORE_CM_DELETE']);
+        $actions[] = sprintf(self::$actionItem, ' delete', '', $_ARRAYLANG['TXT_CORE_CM_DELETE']);
 
         return self::$header.implode("\n",$actions).self::$footer;
     }
