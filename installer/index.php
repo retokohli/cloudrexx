0bb003cc879a9546d0991a0f7043601736a6cf58
Thomas Däppen <thomas.daeppen@cloudrexx.com>
2012-09-04 13:08:29 +0000

    installer: upgraded system requirements to PHP5.3
    git-svn-id: http://svn.contrexxlabs.com/contrexx/trunk@21216 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- installer/index.php
+++ installer/index.php
@@ -14,8 +14,8 @@
 @error_reporting (0);
 @ini_set('display_errors', 0);
 $php = phpversion();
-if ($php < "5.2") {
-    errorBox("Das Contrexx CMS benötigt mindestens PHP in der Version 5.2.<br>Auf Ihrem System läuft PHP ".$php);
+if ($php < "5.3") {
+	errorBox("Das Contrexx CMS benötigt mindestens PHP in der Version 5.3.<br>Auf Ihrem System läuft PHP ".$php);
 }
 
 /**
