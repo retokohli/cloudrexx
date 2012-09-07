ed880bdea09e8a5c5a7ee9bfa7b965abd21dac49
Michael Raess <michael.raess@comvation.com>
2012-09-07 06:30:48 +0000

    themes: deleted app theme
    git-svn-id: http://svn.contrexxlabs.com/contrexx/trunk@21275 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- themes/app/js/search.js
+++
@@ -1,8 +0,0 @@
-function clearText(thefield) {    
-    if (thefield.defaultValue == thefield.value)
-        thefield.value = ""
-}
-function resetText(thefield) {
-    if ("" == thefield.value)
-        thefield.value = thefield.defaultValue
-}
