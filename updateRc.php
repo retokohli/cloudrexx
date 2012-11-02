da284dfab9b6dabf4d16f148306656fdb09e853e
Yannic Tschanz <yannic.tschanz@comvation.com>
2012-11-02 18:01:15 +0000

    updateRc.php updated
    git-svn-id: http://svn.contrexxlabs.com/contrexx/trunk@22393 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- updateRc.php
+++ updateRc.php
@@ -1,11 +1,11 @@
 <?php
 
 /**
- * This file updates an RC1 installation to an RC2 installation
+ * This file updates an RC1 and RC2 installation to a stable installation
  * To update your installation perform the following
  * 1. Copy all your files to some backup folder
  * 2. Create a backup of your DB
- * 3. Copy all RC2 files to your folder
+ * 3. Copy all stable files to your folder
  * 4. Copy /config/configuration.php, /config/settings.php and /.htaccess back
  * 5. Copy your customized files back
  * 6. Execute this script
@@ -18,26 +18,58 @@ DBG::activate(DBG_PHP);
 require_once($documentRoot.'/config/settings.php');                      // needed for doctrine.php
 require_once($documentRoot.'/config/configuration.php');                 // needed for doctrine.php
 require_once($documentRoot.'/core/API.php');                             // needed for getDatabaseObject()
+require_once($documentRoot.'/core/Env.class.php');                       // needed to get EM
+require_once($documentRoot.'/core/ClassLoader/ClassLoader.class.php');
+new \Cx\Core\ClassLoader\ClassLoader($documentRoot, false);
+require_once($documentRoot.'/config/doctrine.php');
+require_once($documentRoot.'/lib/FRAMEWORK/Language.class.php');         // needed by page repo
+require_once($documentRoot.'/core/API.php');                             // needed for getDatabaseObject()
+require_once($documentRoot.'/core/Init.class.php');
+require_once($documentRoot.'/lib/FRAMEWORK/User/User_Setting_Mail.class.php');
+require_once($documentRoot.'/lib/FRAMEWORK/User/User_Setting.class.php');
+require_once($documentRoot.'/lib/FRAMEWORK/User/User_Profile_Attribute.class.php');
+require_once($documentRoot.'/lib/FRAMEWORK/User/User_Profile.class.php');
+require_once($documentRoot.'/lib/FRAMEWORK/User/UserGroup.class.php');
+require_once($documentRoot.'/lib/FRAMEWORK/User/User.class.php');
+require_once($documentRoot.'/lib/FRAMEWORK/FWUser.class.php');
+require_once($documentRoot.'/core/session.class.php');
 
 $objDatabase = getDatabaseObject($strErrMessage, true);
 
-$testQuery = '
-    SELECT
-        `target`
-    FROM
-        `'.DBPREFIX.'backend_areas`
-    WHERE
-        `area_id` = 186
-';
-$result = $objDatabase->Execute($testQuery);
-if (!$result) {
-    die('Could not execute query!');
-}
-if ($result->fields['target'] == '_blank') {
-    die('You do already have RC2!');
+// note that license does not get any language vars, so it won't be able to display the fallback message
+$license = \Cx\Core\License\License::getCached($_CONFIG, $objDatabase);
+
+$objInit = new InitCMS('backend', null);
+
+if (!isset($sessionObj) || !is_object($sessionObj)) $sessionObj = new \cmsSession();
+
+$objUser = \FWUser::getFWUserObject()->objUser;
+if (!$objUser->login(true) || !$objUser->getAdminStatus()) {
+    // do not use die() here, if we use this script as in include it won't work
+    echo '<a href="' . ASCMS_ADMIN_WEB_PATH . '/">You must log in as admin before you can update the database. Click here to log in...</a>';
+    return;
 }
 
-$updates = array(
+$objResultRc1 = $objDatabase->Execute('SELECT `target` FROM `'.DBPREFIX.'backend_areas` WHERE `area_id` = 186');
+$objResultRc2 = $objDatabase->Execute('SELECT `order_id` FROM `'.DBPREFIX.'backend_areas` WHERE `area_id` = 2');
+if (!$objResultRc1 || !$objResultRc2) {
+    die('ERROR: Could not execute query.');
+}
+if ($objResultRc1->fields['target'] != '_blank') {
+    $version = 'rc1';
+} elseif ($objResultRc2->fields['order_id'] != 6) {
+    $version = 'rc2';
+} else {
+    die('You have already installed the stable...');
+}
+
+$updatesRc1ToStable = array(
+    //rc1 to rc2
+    '
+        ALTER TABLE `'.DBPREFIX.'access_user_profile`
+        CHANGE `interests` `interests` TEXT,
+        CHANGE `signature` `signature` TEXT
+    ',
     '
         INSERT INTO
             `'.DBPREFIX.'settings`
@@ -70,6 +102,15 @@ $updates = array(
             `distributor` = \'Comvation AG\'
     ',
     '
+        ALTER TABLE `'.DBPREFIX.'module_mediadir_rel_entry_inputfields`
+        ADD UNIQUE (
+            `entry_id`,
+            `lang_id`,
+            `form_id`,
+            `field_id`
+        )
+    ',
+    '
         ALTER TABLE
             `'.DBPREFIX.'module_repository`
         DROP COLUMN
@@ -87,7 +128,59 @@ $updates = array(
         WHERE
             `area_id` = 186
     ',
+    //rc2 to stable
+    '
+        DELETE FROM `'.DBPREFIX.'backend_areas`
+        WHERE `'.DBPREFIX.'backend_areas`.`area_id` = 179
+    ',
+    '
+        INSERT INTO `'.DBPREFIX.'settings` (`setid`, `setname`, `setvalue`, `setmodule`)
+        VALUES 
+            (104, "upgradeUrl", "http://license.contrexx.com/", 66),
+            (105, "isUpgradable", " ", 66),
+            (106, "dashboardMessages", " ", 66)
+    ',
+    '
+        UPDATE `'.DBPREFIX.'backend_areas` SET `order_id` = 4
+        WHERE `'.DBPREFIX.'backend_areas`.`area_id` = 8;
+    ',
+    '
+        UPDATE `'.DBPREFIX.'backend_areas` SET `order_id` = 5
+        WHERE `'.DBPREFIX.'backend_areas`.`area_id` = 29;
+    ',
+    '
+        UPDATE `'.DBPREFIX.'backend_areas` SET `order_id` = 6
+        WHERE `'.DBPREFIX.'backend_areas`.`area_id` = 2;
+    ',
 );
+$updatesRc2ToStable = array(
+    '
+        DELETE FROM `'.DBPREFIX.'backend_areas`
+        WHERE `'.DBPREFIX.'backend_areas`.`area_id` = 179
+    ',
+    '
+        INSERT INTO `'.DBPREFIX.'settings` (`setid`, `setname`, `setvalue`, `setmodule`)
+        VALUES 
+            (104, "upgradeUrl", "http://license.contrexx.com/", 66),
+            (105, "isUpgradable", " ", 66),
+            (106, "dashboardMessages", " ", 66)
+    ',
+    '
+        UPDATE `'.DBPREFIX.'backend_areas` SET `order_id` = 4
+        WHERE `'.DBPREFIX.'backend_areas`.`area_id` = 8;
+    ',
+    '
+        UPDATE `'.DBPREFIX.'backend_areas` SET `order_id` = 5
+        WHERE `'.DBPREFIX.'backend_areas`.`area_id` = 29;
+    ',
+    '
+        UPDATE `'.DBPREFIX.'backend_areas` SET `order_id` = 6
+        WHERE `'.DBPREFIX.'backend_areas`.`area_id` = 2;
+    ',
+);
+
+$updates = ($version == 'rc1') ? $updatesRc1ToStable : $updatesRc2ToStable;
+
 foreach ($updates as $update) {
     $result = $objDatabase->Execute($update);
     if (!$result) {
@@ -123,4 +216,24 @@ if ($fp !== false) {
     die('Could not read data dump file!');
 }
 
-echo 'Update successful, you now have RC2!';
+if ($version == 'rc1') {
+    $em = \Env::em();
+    $pageRepo = $em->getRepository('Cx\Model\ContentManager\Page');
+
+    $fallbackPages = $pageRepo->findBy(array(
+        'type' => \Cx\Model\ContentManager\Page::TYPE_FALLBACK,
+    ));
+
+    foreach ($fallbackPages as $page) {
+        $page->setModule($page->getModule());
+        $page->setCmd($page->getCmd());
+        $em->persist($page);
+    }
+    $em->flush();
+}
+
+$_GET['force'] = 'true';
+$_GET['silent'] = 'true';
+require_once($documentRoot.'/core_modules/License/versioncheck.php');
+
+echo 'Update successful, you now have the stable installed!';
