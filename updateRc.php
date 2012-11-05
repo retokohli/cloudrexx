c0c845f70bebcc99a6c7c66f82ee2ec9f0ab57f5
Yannic Tschanz <yannic.tschanz@comvation.com>
2012-11-05 15:57:20 +0000

    updateRc.php updated
    git-svn-id: http://svn.contrexxlabs.com/contrexx/trunk@22409 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- updateRc.php
+++ updateRc.php
@@ -1,10 +1,10 @@
 <?php
 
 /**
- * This file updates an RC1 and RC2 installation to a stable installation
- * To update your installation perform the following
- * 1. Copy all your files to some backup folder
- * 2. Create a backup of your DB
+ * This file updates a RC1 or RC2 installation to a stable installation
+ * To update your installation perform the following:
+ * 1. Copy all your files to a backup folder
+ * 2. Create a backup of your database
  * 3. Copy all stable files to your folder
  * 4. Copy /config/configuration.php, /config/settings.php and /.htaccess back
  * 5. Copy your customized files back
@@ -12,9 +12,8 @@
  */
 
 $documentRoot = dirname(__FILE__);
-
 require_once($documentRoot.'/lib/DBG.php');
-DBG::activate(DBG_PHP);
+DBG::deactivate();
 require_once($documentRoot.'/config/settings.php');                      // needed for doctrine.php
 require_once($documentRoot.'/config/configuration.php');                 // needed for doctrine.php
 require_once($documentRoot.'/core/API.php');                             // needed for getDatabaseObject()
@@ -35,11 +34,10 @@ require_once($documentRoot.'/lib/FRAMEWORK/FWUser.class.php');
 require_once($documentRoot.'/core/session.class.php');
 
 $objDatabase = getDatabaseObject($strErrMessage, true);
+$objInit = new InitCMS('frontend', null);
+$_CORELANG = $objInit->loadLanguageData('core');
 
-// note that license does not get any language vars, so it won't be able to display the fallback message
-$license = \Cx\Core_Modules\License\License::getCached($_CONFIG, $objDatabase);
-
-$objInit = new InitCMS('backend', null);
+$license = \Cx\Core_Modules\License\License::getCached($_CONFIG, $objDatabase, $_CORELANG);
 
 if (!isset($sessionObj) || !is_object($sessionObj)) $sessionObj = new \cmsSession();
 
@@ -63,124 +61,140 @@ if ($objResultRc1->fields['target'] != '_blank') {
     die('You have already installed the stable...');
 }
 
-$updatesRc1ToStable = array(
-    //rc1 to rc2
+
+$objResult = $objDatabase->Execute('SELECT 1 FROM `'.DBPREFIX.'module_filesharing_mail_template` WHERE (`id` = 1 OR `id` = 2)');
+if (!$objResult) {
+    die('ERROR: Could not execute query.');
+}
+if ($objResult->RecordCount() == 0) {
+    $rc1UpdateFilesharing[] = '
+        INSERT INTO `'.DBPREFIX.'module_filesharing_mail_template` (`id`, `lang_id`, `subject`, `content`) VALUES (1, 1, "Jemand teilt eine Datei mit Ihnen", "Guten Tag,\r\n\r\nJemand hat auf [[DOMAIN]] eine Datei mit Ihnen geteilt.\r\n\r\n<!-- BEGIN filesharing_file -->\r\nDownload-Link: [[FILE_DOWNLOAD]]\r\n<!-- END filesharing_file -->\r\n\r\nDie Person hat eine Nachricht hinterlassen:\r\n[[MESSAGE]]\r\n\r\nFreundliche Grüsse");
+    ';
+    $rc1UpdateFilesharing[] = '
+        INSERT INTO `'.DBPREFIX.'module_filesharing_mail_template` (`id`, `lang_id`, `subject`, `content`) VALUES (2, 2, "Somebody is sharing a file with you", "Hi,\r\n\r\nSomebody shared a file with you on [[DOMAIN]].\r\n\r\n<!-- BEGIN filesharing_file -->\r\nDownload link: [[FILE_DOWNLOAD]]\r\n<!-- END filesharing_file -->\r\n\r\nThe person has left a message for you:\r\n[[MESSAGE]]\r\n\r\nBest regards");
+    ';
+} else {
+    $rc1UpdateFilesharing = null;
+}
+
+$updatesRc1ToRc2 = array(
     '
         ALTER TABLE `'.DBPREFIX.'access_user_profile`
         CHANGE `interests` `interests` TEXT,
         CHANGE `signature` `signature` TEXT
     ',
     '
-        INSERT INTO
-            `'.DBPREFIX.'settings`
-            (
-                `setid`,
-                `setname`,
-                `setvalue`,
-                `setmodule`
-            )
-        VALUES
-            (
-                103,
-                \'availableComponents\',
-                \'\',
-                66
-            )
+        UPDATE `'.DBPREFIX.'access_user_profile`
+        SET  `interests` = NULL , `signature` = NULL
+        WHERE (`interests` = "" AND `signature` = "");
     ',
     '
-        ALTER TABLE
-            `'.DBPREFIX.'modules`
-        ADD
-            `distributor` CHAR( 50 ) NOT NULL
-        AFTER
-            `name`
+        INSERT INTO `'.DBPREFIX.'settings` (`setid`, `setname`, `setvalue`, `setmodule`)
+        VALUES (103, "availableComponents", "", 66)
     ',
     '
-        UPDATE
-            `'.DBPREFIX.'modules`
-        SET
-            `distributor` = \'Comvation AG\'
+        ALTER TABLE `'.DBPREFIX.'modules`
+        ADD `distributor` CHAR( 50 ) NOT NULL
+        AFTER `name`
+    ',
+    '
+        UPDATE `'.DBPREFIX.'modules`
+        SET `distributor` = "Comvation AG"
+    ',
+    '
+        CREATE TABLE IF NOT EXISTS `'.DBPREFIX.'module_mediadir_rel_entry_inputfields_clean` (
+          `id` int(11) NOT NULL auto_increment,
+          `entry_id` int(7) NOT NULL,
+          `lang_id` int(7) NOT NULL,
+          `form_id` int(7) NOT NULL,
+          `field_id` int(7) NOT NULL,
+          `value` longtext collate utf8_unicode_ci NOT NULL,
+          PRIMARY KEY  (`id`),
+          FULLTEXT KEY `value` (`value`)
+        ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci
+    ',
+    '
+        INSERT INTO `'.DBPREFIX.'module_mediadir_rel_entry_inputfields_clean`
+        SELECT NULL, `entry_id`, `lang_id`, `form_id`, `field_id`, `value`
+        FROM `'.DBPREFIX.'module_mediadir_rel_entry_inputfields`
+        GROUP BY `entry_id`, `form_id`, `field_id`, `lang_id`, `value`
+    ',
+    '
+        TRUNCATE `'.DBPREFIX.'module_mediadir_rel_entry_inputfields`
     ',
     '
         ALTER TABLE `'.DBPREFIX.'module_mediadir_rel_entry_inputfields`
-        ADD UNIQUE (
-            `entry_id`,
-            `lang_id`,
-            `form_id`,
-            `field_id`
-        )
+        ADD UNIQUE (`entry_id`, `lang_id`, `form_id`, `field_id`)
     ',
     '
-        ALTER TABLE
-            `'.DBPREFIX.'module_repository`
-        DROP COLUMN
-            `lang`
+        INSERT IGNORE INTO `'.DBPREFIX.'module_mediadir_rel_entry_inputfields`
+        SELECT `entry_id`, `lang_id`, `form_id`, `field_id`, `value`
+        FROM `'.DBPREFIX.'module_mediadir_rel_entry_inputfields_clean`
+        ORDER BY `id` DESC
     ',
     '
-        TRUNCATE TABLE
-            `'.DBPREFIX.'module_repository`
+        DROP TABLE `'.DBPREFIX.'module_mediadir_rel_entry_inputfields_clean`
     ',
     '
-        UPDATE
-            `'.DBPREFIX.'backend_areas`
-        SET
-            `target` = \'_blank\'
-        WHERE
-            `area_id` = 186
-    ',
-    //rc2 to stable
-    '
-        DELETE FROM `'.DBPREFIX.'backend_areas`
-        WHERE `'.DBPREFIX.'backend_areas`.`area_id` = 179
+        ALTER TABLE `'.DBPREFIX.'module_repository`
+        DROP COLUMN `lang`
     ',
     '
-        INSERT INTO `'.DBPREFIX.'settings` (`setid`, `setname`, `setvalue`, `setmodule`)
-        VALUES 
-            (104, "upgradeUrl", "http://license.contrexx.com/", 66),
-            (105, "isUpgradable", " ", 66),
-            (106, "dashboardMessages", " ", 66)
+        TRUNCATE TABLE `'.DBPREFIX.'module_repository`
     ',
     '
-        UPDATE `'.DBPREFIX.'backend_areas` SET `order_id` = 4
-        WHERE `'.DBPREFIX.'backend_areas`.`area_id` = 8;
-    ',
-    '
-        UPDATE `'.DBPREFIX.'backend_areas` SET `order_id` = 5
-        WHERE `'.DBPREFIX.'backend_areas`.`area_id` = 29;
-    ',
-    '
-        UPDATE `'.DBPREFIX.'backend_areas` SET `order_id` = 6
-        WHERE `'.DBPREFIX.'backend_areas`.`area_id` = 2;
+        UPDATE `'.DBPREFIX.'backend_areas`
+        SET `target` = "_blank"
+        WHERE `area_id` = 186
     ',
 );
+if (!empty($rc1UpdateFilesharing)) {
+    $updatesRc1ToRc2 = array_merge($updatesRc1ToRc2, $rc1UpdateFilesharing);
+}
+
 $updatesRc2ToStable = array(
     '
+        UPDATE `'.DBPREFIX.'core_setting`
+        SET `value` = "off"
+        WHERE (`section` = "filesharing" AND `name` = "permission" AND `group` = "config")
+    ',
+    '
+        INSERT INTO `'.DBPREFIX.'core_text` (`id`, `lang_id`, `section`, `key`, `text`)
+        VALUES  (4, 1, "shop", "currency_name", "Euro"),
+                (5, 1, "shop", "currency_name", "United States Dollars")
+    ',
+    '
+        INSERT INTO `'.DBPREFIX.'settings` (`setid`, `setname`, `setvalue`, `setmodule`)
+        VALUES  (104, "upgradeUrl", "http://license.contrexx.com/", 66),
+                (105, "isUpgradable", "on", 66),
+                (106, "dashboardMessages", "", 66)
+    ',
+    '
         DELETE FROM `'.DBPREFIX.'backend_areas`
         WHERE `'.DBPREFIX.'backend_areas`.`area_id` = 179
     ',
     '
-        INSERT INTO `'.DBPREFIX.'settings` (`setid`, `setname`, `setvalue`, `setmodule`)
-        VALUES 
-            (104, "upgradeUrl", "http://license.contrexx.com/", 66),
-            (105, "isUpgradable", " ", 66),
-            (106, "dashboardMessages", " ", 66)
+        UPDATE `'.DBPREFIX.'backend_areas`
+        SET `order_id` = 4
+        WHERE `'.DBPREFIX.'backend_areas`.`area_id` = 8
     ',
     '
-        UPDATE `'.DBPREFIX.'backend_areas` SET `order_id` = 4
-        WHERE `'.DBPREFIX.'backend_areas`.`area_id` = 8;
+        UPDATE `'.DBPREFIX.'backend_areas`
+        SET `order_id` = 5
+        WHERE `'.DBPREFIX.'backend_areas`.`area_id` = 29
     ',
     '
-        UPDATE `'.DBPREFIX.'backend_areas` SET `order_id` = 5
-        WHERE `'.DBPREFIX.'backend_areas`.`area_id` = 29;
+        UPDATE `'.DBPREFIX.'backend_areas`
+        SET `order_id` = 6
+        WHERE `'.DBPREFIX.'backend_areas`.`area_id` = 2
     ',
     '
-        UPDATE `'.DBPREFIX.'backend_areas` SET `order_id` = 6
-        WHERE `'.DBPREFIX.'backend_areas`.`area_id` = 2;
+        TRUNCATE TABLE `'.DBPREFIX.'module_repository`
     ',
 );
 
+$updatesRc1ToStable = array_merge($updatesRc1ToRc2, $updatesRc2ToStable);
 $updates = ($version == 'rc1') ? $updatesRc1ToStable : $updatesRc2ToStable;
-
 foreach ($updates as $update) {
     $result = $objDatabase->Execute($update);
     if (!$result) {
@@ -199,13 +213,14 @@ if ($fp !== false) {
             $sqlQuery .= $buffer;
             if (preg_match('/;[ \t\r\n]*$/', $buffer)) {
                 $sqlQuery = preg_replace('/SET FOREIGN_KEY_CHECKS = 0;/', '', $sqlQuery);
-                if (substr($sqlQuery, 0, 40) != 'INSERT INTO `contrexx_module_repository`') {
+                if (substr(trim($sqlQuery), 0, 40) != 'INSERT INTO `contrexx_module_repository`') {
                     $sqlQuery = '';
                     continue;
                 }
                 $sqlQuery = preg_replace('#`contrexx_module_repository`#', '`'.DBPREFIX.'module_repository`', $sqlQuery);
                 $result = $objDatabase->Execute($sqlQuery);
                 if ($result === false) {
+                    echo $sqlQuery;
                     die('Update failed!');
                 }
                 $sqlQuery = '';
@@ -236,4 +251,4 @@ $_GET['force'] = 'true';
 $_GET['silent'] = 'true';
 require_once($documentRoot.'/core_modules/License/versioncheck.php');
 
-echo 'Update successful, you now have the stable installed!';
+echo 'Update successful. Now you have installed the stable.';
