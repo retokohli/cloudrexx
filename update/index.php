668b867b18863e7a00d0973fee5c6d54a4f25297
Michael Raess <michael.raess@comvation.com>
2012-12-04 15:33:26 +0000

    Update - update - Added some comments
    git-svn-id: http://svn.contrexxlabs.com/contrexx/trunk@22660 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- update/index.php
+++ update/index.php
@@ -10,27 +10,34 @@
  * @subpackage  update
  */
 
+// Debugging
 require_once dirname(__FILE__).'/lib/DBG.php';
 DBG::deactivate();
 
 define('UPDATE_PATH', dirname(__FILE__));
 
+// Config files
 require_once dirname(UPDATE_PATH) . '/config/configuration.php';
 require_once ASCMS_DOCUMENT_ROOT  . '/config/settings.php';
 require_once ASCMS_DOCUMENT_ROOT  . '/config/version.php';
 
+
+// Library and core files
 require_once ASCMS_LIBRARY_PATH . '/PEAR/HTML/Template/Sigma/Sigma.php';
 require_once ASCMS_LIBRARY_PATH . '/adodb/adodb.inc.php';
 require_once ASCMS_CORE_PATH    . '/database.php';
 require_once ASCMS_CORE_PATH    . '/session.class.php';
 require_once ASCMS_CORE_PATH    . '/Init.class.php';
 
+// Update files
 require_once UPDATE_PATH . '/Contrexx_Update.class.php';
 require_once UPDATE_PATH . '/lib/FRAMEWORK/UpdateUtil.class.php';
 require_once UPDATE_PATH . '/config/configuration.php';
 
+// Start session
 $sessionObj = new cmsSession();
 $sessionObj->cmsSessionStatusUpdate('backend');
 
+// Start update
 $objUpdate = new Contrexx_Update();
 die($objUpdate->getPage());
