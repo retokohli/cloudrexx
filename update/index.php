a9503b5feafde52a8fd1c0211de12a24a4ccf8d0
Michael Raess <michael.raess@comvation.com>
2012-12-04 15:31:05 +0000

    Update - bugfixes - Fixed fatal errors Update - update - Necessary files are now included and the others were removed Update - change - Changed contrexx logo
    git-svn-id: http://svn.contrexxlabs.com/contrexx/trunk@22659 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- update/index.php
+++ update/index.php
@@ -2,59 +2,35 @@
 /**
  * Contrexx Update System
  *
- * This class is used to update the system to a newer version of Contrexx
- * @copyright   CONTREXX CMS - COMVATION AG
- * @author        Comvation Development Team <info@comvation.com>
- * @version       $Id:     Exp $
+ * This class is used to update the system to a newer version of Contrexx.
+ * 
+ * @copyright   Contrexx WMS - Comvation AG
+ * @author      Comvation Development Team <info@comvation.com>
  * @package     contrexx
  * @subpackage  update
- * @todo        Edit PHP DocBlocks!
  */
 
-/**
- * Debug level, see lib/DBG.php
- *   DBG_PHP             - show PHP errors/warnings/notices
- *   DBG_ADODB           - show ADODB queries
- *   DBG_ADODB_TRACE     - show ADODB queries with backtrace
- *   DBG_ADODB_ERROR     - show ADODB queriy errors only
- *   DBG_LOG_FILE        - DBG: log to file (/dbg.log)
- *   DBG_LOG_FIREPHP     - DBG: log via FirePHP
- *
- * Use DBG::activate($level) and DBG::deactivate($level)
- * to activate/deactivate a debug level.
- * Calling these methods without specifying a debug level
- * will either activate or deactivate all levels.
- */
-include_once('../lib/DBG.php');
+require_once dirname(__FILE__).'/lib/DBG.php';
 DBG::deactivate();
-
-require_once 'UpdateUtil.php';
 
 define('UPDATE_PATH', dirname(__FILE__));
 
-if (!@include_once(UPDATE_PATH.'/../config/configuration.php')) {
-    die('Couldn\'t load configuration file <i>'.realpath(UPDATE_PATH.'/../config/configuration.php').'</i>!');
-} elseif (!@include_once(ASCMS_DOCUMENT_ROOT.'/config/version.php')) {
-    die('Couldn\'t load version file <i>'.ASCMS_DOCUMENT_ROOT.'/config/version.php'.'</i>!');
-} elseif (!@include_once(ASCMS_CORE_PATH.'/API.php')) {
-    die('Couldn\'t load contrexx API file <i>'.ASCMS_CORE_PATH.'/API.php</i>!');
-} elseif (!@include_once(UPDATE_PATH.'/Contrexx_Update.class.php')) {
-    die('Couldn\'t load contrexx update system <i>'.UPDATE_PATH.'/Contrexx_Update.class.php'.'</i>!');
-} elseif (!@include_once(UPDATE_PATH.'/config/configuration.php')) {
-    die('Couldn\'t load contrexx update system configuration file <i>'.UPDATE_PATH.'/config/configuration.php'.'</i>!');
-} else {
-    $_SYSCONFIG = false;
-    @include_once(ASCMS_DOCUMENT_ROOT.'/config/settings.php');
-    if (is_array($_SYSCONFIG)) {
-        foreach ($_SYSCONFIG as $sysconfigKey => $sysconfValue) {
-            $_CONFIG[$sysconfigKey] = $sysconfValue;
-        }
-    }
+require_once dirname(UPDATE_PATH) . '/config/configuration.php';
+require_once ASCMS_DOCUMENT_ROOT  . '/config/settings.php';
+require_once ASCMS_DOCUMENT_ROOT  . '/config/version.php';
 
-    $sessionObj = new cmsSession();
-    $sessionObj->cmsSessionStatusUpdate('backend');
+require_once ASCMS_LIBRARY_PATH . '/PEAR/HTML/Template/Sigma/Sigma.php';
+require_once ASCMS_LIBRARY_PATH . '/adodb/adodb.inc.php';
+require_once ASCMS_CORE_PATH    . '/database.php';
+require_once ASCMS_CORE_PATH    . '/session.class.php';
+require_once ASCMS_CORE_PATH    . '/Init.class.php';
 
-    $objUpdate = new Contrexx_Update();
-    die($objUpdate->getPage());
-}
-?>
+require_once UPDATE_PATH . '/Contrexx_Update.class.php';
+require_once UPDATE_PATH . '/lib/FRAMEWORK/UpdateUtil.class.php';
+require_once UPDATE_PATH . '/config/configuration.php';
+
+$sessionObj = new cmsSession();
+$sessionObj->cmsSessionStatusUpdate('backend');
+
+$objUpdate = new Contrexx_Update();
+die($objUpdate->getPage());
