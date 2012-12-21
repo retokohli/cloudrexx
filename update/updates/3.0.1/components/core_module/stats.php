db472dc436a1fd89aec4310c79ceb58fdfed0495
Yannic Tschanz <yannic.tschanz@comvation.com>
2012-12-21 19:07:47 +0000

    update updateutil: new parameter for database table comments implemented in table() update components: database installation scripts for missed modules implemented
    git-svn-id: http://svn.contrexxlabs.com/contrexx/trunk@22866 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- update/updates/3.0.1/components/core_module/stats.php
+++ update/updates/3.0.1/components/core_module/stats.php
@@ -1,120 +1,120 @@
 <?php
 function _statsUpdate()
 {
-	global $objDatabase, $objUpdate, $_CONFIG, $_ARRAYLANG;
+    global $objDatabase, $objUpdate, $_CONFIG, $_ARRAYLANG;
 
-	// remove redundancies
-	if (!isset($_SESSION['contrexx_update']['update']['update_stats'])) {
-		$_SESSION['contrexx_update']['update']['update_stats'] = array();
-	}
+    // remove redundancies
+    if (!isset($_SESSION['contrexx_update']['update']['update_stats'])) {
+        $_SESSION['contrexx_update']['update']['update_stats'] = array();
+    }
 
-	foreach (array(
-		'stats_browser' => array(
-			'obsoleteIndex'	=> 'name',
-			'unique' => array('name'),
-			'change' => "`name` `name` VARCHAR(255) BINARY NOT NULL DEFAULT ''"
-		),
-		'stats_colourdepth' => array(
-			'obsoleteIndex'	=> 'depth',
-			'unique' => array('depth')
-		),
-		'stats_country' => array(
-			'obsoleteIndex'	=> 'country',
-			'unique' => array('country'),
-			'change' => "`country` `country` VARCHAR(100) BINARY NOT NULL DEFAULT ''"
-		),
-		'stats_hostname' => array(
-			'obsoleteIndex'	=> 'hostname',
-			'unique' => array('hostname'),
-			'change' => "`hostname` `hostname` VARCHAR(255) BINARY NOT NULL DEFAULT ''"
-		),
-		'stats_operatingsystem' => array(
-			'obsoleteIndex'	=> 'name',
-			'unique' => array('name'),
-			'change' => "`name` `name` VARCHAR(255) BINARY NOT NULL DEFAULT ''"
-		),
-		'stats_referer' => array(
-			'obsoleteIndex'	=> 'uri',
-			'unique' => array('uri'),
-			'change' => "`uri` `uri` VARCHAR(255) BINARY NOT NULL DEFAULT ''"
-		),
-		'stats_requests' => array(
-			'obsoleteIndex'	=> 'page',
-			'unique' => array('page'),
-			'count' => 'visits',
-			'change' => "`page` `page` VARCHAR(255) BINARY NOT NULL DEFAULT ''"
-		),
-		'stats_requests_summary' => array(
-			'obsoleteIndex'	=> 'type',
-			'unique' => array('type', 'timestamp')
-		),
-		'stats_screenresolution' => array(
-			'obsoleteIndex'	=> 'resolution',
-			'unique' => array('resolution')
-		),
-		'stats_search' => array(
-			'change' => "`name` `name` VARCHAR(100) BINARY NOT NULL DEFAULT ''",
-			'unique' => array('name')
-		),
-		'stats_spiders' => array(
-			'obsoleteIndex'	=> 'page',
-			'unique' => array('page'),
-			'change' => "`page` `page` VARCHAR(100) BINARY DEFAULT NULL"
-		),
-		'stats_spiders_summary'	=> array(
-			'obsoleteIndex'	=> 'unqiue',
-			'unique' => array('name'),
-			'change' => "`name` `name` VARCHAR(255) BINARY NOT NULL DEFAULT ''"
-		),
-		'stats_visitors_summary' => array(
-			'obsoleteIndex'	=> 'type',
-			'unique' => array('type', 'timestamp')
-		),
+    foreach (array(
+        'stats_browser' => array(
+            'obsoleteIndex'	=> 'name',
+            'unique' => array('name'),
+            'change' => "`name` `name` VARCHAR(255) BINARY NOT NULL DEFAULT ''"
+        ),
+        'stats_colourdepth' => array(
+            'obsoleteIndex'	=> 'depth',
+            'unique' => array('depth')
+        ),
+        'stats_country' => array(
+            'obsoleteIndex'	=> 'country',
+            'unique' => array('country'),
+            'change' => "`country` `country` VARCHAR(100) BINARY NOT NULL DEFAULT ''"
+        ),
+        'stats_hostname' => array(
+            'obsoleteIndex'	=> 'hostname',
+            'unique' => array('hostname'),
+            'change' => "`hostname` `hostname` VARCHAR(255) BINARY NOT NULL DEFAULT ''"
+        ),
+        'stats_operatingsystem' => array(
+            'obsoleteIndex'	=> 'name',
+            'unique' => array('name'),
+            'change' => "`name` `name` VARCHAR(255) BINARY NOT NULL DEFAULT ''"
+        ),
+        'stats_referer' => array(
+            'obsoleteIndex'	=> 'uri',
+            'unique' => array('uri'),
+            'change' => "`uri` `uri` VARCHAR(255) BINARY NOT NULL DEFAULT ''"
+        ),
+        'stats_requests' => array(
+            'obsoleteIndex'	=> 'page',
+            'unique' => array('page'),
+            'count' => 'visits',
+            'change' => "`page` `page` VARCHAR(255) BINARY NOT NULL DEFAULT ''"
+        ),
+        'stats_requests_summary' => array(
+            'obsoleteIndex'	=> 'type',
+            'unique' => array('type', 'timestamp')
+        ),
+        'stats_screenresolution' => array(
+            'obsoleteIndex'	=> 'resolution',
+            'unique' => array('resolution')
+        ),
+        'stats_search' => array(
+            'change' => "`name` `name` VARCHAR(100) BINARY NOT NULL DEFAULT ''",
+            'unique' => array('name')
+        ),
+        'stats_spiders' => array(
+            'obsoleteIndex'	=> 'page',
+            'unique' => array('page'),
+            'change' => "`page` `page` VARCHAR(100) BINARY DEFAULT NULL"
+        ),
+        'stats_spiders_summary'	=> array(
+            'obsoleteIndex'	=> 'unqiue',
+            'unique' => array('name'),
+            'change' => "`name` `name` VARCHAR(255) BINARY NOT NULL DEFAULT ''"
+        ),
+        'stats_visitors_summary' => array(
+            'obsoleteIndex'	=> 'type',
+            'unique' => array('type', 'timestamp')
+        ),
         /************************************************
         * EXTENSION:    Unique key on sid attribte of   *
         *               table contrexx_statis_visitors  *
         * ADDED:		Contrexx v2.1.0					*
         ************************************************/
-		'stats_visitors' => array(
-			'obsoleteIndex'	=> 'sid',
+        'stats_visitors' => array(
+            'obsoleteIndex'	=> 'sid',
             'unique' => array('sid'),
             'count'  => 'timestamp'
-		)
+        )
 
-	) as $table => $arrUnique) {
-		do {
-			if (in_array($table, $_SESSION['contrexx_update']['update']['update_stats'])) {
-				break;
-			} elseif (!checkTimeoutLimit()) {
-				return false;
-			}
+    ) as $table => $arrUnique) {
+        do {
+            if (in_array($table, $_SESSION['contrexx_update']['update']['update_stats'])) {
+                break;
+            } elseif (!checkTimeoutLimit()) {
+                return false;
+            }
 
-			if (isset($arrUnique['change'])) {
-				$query = 'ALTER TABLE `'.DBPREFIX.$table.'` CHANGE '.$arrUnique['change'];
-				if ($objDatabase->Execute($query) === false) {
+            if (isset($arrUnique['change'])) {
+                $query = 'ALTER TABLE `'.DBPREFIX.$table.'` CHANGE '.$arrUnique['change'];
+                if ($objDatabase->Execute($query) === false) {
                     return _databaseError($query, $objDatabase->ErrorMsg());
-				}
-			}
+                }
+            }
 
-			$arrIndexes = $objDatabase->MetaIndexes(DBPREFIX.$table);
-			if ($arrIndexes !== false) {
-				if (isset($arrIndexes['unique'])) {
-					$_SESSION['contrexx_update']['update']['update_stats'][] = $table;
-					break;
-				} elseif (isset($arrUnique['obsoleteIndex']) && isset($arrIndexes[$arrUnique['obsoleteIndex']])) {
-					$query = 'ALTER TABLE `'.DBPREFIX.$table.'` DROP INDEX `'.$arrUnique['obsoleteIndex'].'`';
-					if ($objDatabase->Execute($query) === false) {
-						return _databaseError($query, $objDatabase->ErrorMsg());
-					}
-				}
-			} else {
-				setUpdateMsg(sprintf($_ARRAYLANG['TXT_UNABLE_GETTING_DATABASE_TABLE_STRUCTURE'], DBPREFIX.$table));
-				return false;
-			}
+            $arrIndexes = $objDatabase->MetaIndexes(DBPREFIX.$table);
+            if ($arrIndexes !== false) {
+                if (isset($arrIndexes['unique'])) {
+                    $_SESSION['contrexx_update']['update']['update_stats'][] = $table;
+                    break;
+                } elseif (isset($arrUnique['obsoleteIndex']) && isset($arrIndexes[$arrUnique['obsoleteIndex']])) {
+                    $query = 'ALTER TABLE `'.DBPREFIX.$table.'` DROP INDEX `'.$arrUnique['obsoleteIndex'].'`';
+                    if ($objDatabase->Execute($query) === false) {
+                        return _databaseError($query, $objDatabase->ErrorMsg());
+                    }
+                }
+            } else {
+                setUpdateMsg(sprintf($_ARRAYLANG['TXT_UNABLE_GETTING_DATABASE_TABLE_STRUCTURE'], DBPREFIX.$table));
+                return false;
+            }
 
             #DBG::msg("table = $table");
             #DBG::dump($arrUnique);
-			if (isset($arrUnique['unique'])) {
+            if (isset($arrUnique['unique'])) {
                 $query = 'SELECT `'.implode('`,`', $arrUnique['unique']).'`, COUNT(`id`) AS redundancy FROM `'.DBPREFIX.$table.'` GROUP BY `'.implode('`,`', $arrUnique['unique']).'` ORDER BY redundancy DESC';
                 $objEntry = $objDatabase->SelectLimit($query, 10);
                 if ($objEntry !== false) {
@@ -142,27 +142,27 @@ function _statsUpdate()
                 }
             }
 
-			if ($objEntry->RecordCount() == 0 || $lastRedundancyCount < 2) {
-				$query = 'ALTER IGNORE TABLE `'.DBPREFIX.$table.'` ADD UNIQUE `unique` (`'.implode('`,`', $arrUnique['unique']).'`)';
-				if ($objDatabase->Execute($query) == false) {
-					return _databaseError($query, $objDatabase->ErrorMsg());
-				}
-				$_SESSION['contrexx_update']['update']['update_stats'][] = $table;
-				break;
-			}
-		} while ($objEntry->RecordCount() > 1);
-	}
+            if ($objEntry->RecordCount() == 0 || $lastRedundancyCount < 2) {
+                $query = 'ALTER IGNORE TABLE `'.DBPREFIX.$table.'` ADD UNIQUE `unique` (`'.implode('`,`', $arrUnique['unique']).'`)';
+                if ($objDatabase->Execute($query) == false) {
+                    return _databaseError($query, $objDatabase->ErrorMsg());
+                }
+                $_SESSION['contrexx_update']['update']['update_stats'][] = $table;
+                break;
+            }
+        } while ($objEntry->RecordCount() > 1);
+    }
 
 
-	$arrIndexes = $objDatabase->MetaIndexes(DBPREFIX.'stats_search');
-	if ($arrIndexes !== false) {
-		if (isset($arrIndexes['unique'])) {
-			$query = 'ALTER TABLE `'.DBPREFIX.'stats_search` DROP INDEX `unique`';
-			if ($objDatabase->Execute($query) === false) {
-				return _databaseError($query, $objDatabase->ErrorMsg());
-			}
-		}
-    	if(empty($_SESSION['contrexx_update']['update']['update_stats']['utf8'])){
+    $arrIndexes = $objDatabase->MetaIndexes(DBPREFIX.'stats_search');
+    if ($arrIndexes !== false) {
+        if (isset($arrIndexes['unique'])) {
+            $query = 'ALTER TABLE `'.DBPREFIX.'stats_search` DROP INDEX `unique`';
+            if ($objDatabase->Execute($query) === false) {
+                return _databaseError($query, $objDatabase->ErrorMsg());
+            }
+        }
+        if(empty($_SESSION['contrexx_update']['update']['update_stats']['utf8'])){
             $query = "ALTER IGNORE TABLE `".DBPREFIX."stats_search` CHANGE `name` `name` VARCHAR( 100 ) CHARACTER SET latin1 COLLATE latin1_bin NOT NULL";
             if($objDatabase->Execute($query)){
                 $_SESSION['contrexx_update']['update']['update_stats']['utf8'] = 1;
@@ -172,10 +172,10 @@ function _statsUpdate()
                     $query = "ALTER IGNORE TABLE `".DBPREFIX."stats_search` CHANGE `name` `name` VARCHAR( 100 ) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL";
                     if($_SESSION['contrexx_update']['update']['update_stats']['utf8'] == 2 && $objDatabase->Execute($query)){
                         $_SESSION['contrexx_update']['update']['update_stats']['utf8'] = 3;
-                		$query = 'ALTER IGNORE TABLE `'.DBPREFIX.'stats_search` ADD UNIQUE `unique` (`name`, `external`)';
-                		if ($objDatabase->Execute($query) === false) {
-                			return _databaseError($query, $objDatabase->ErrorMsg());
-                		}
+                        $query = 'ALTER IGNORE TABLE `'.DBPREFIX.'stats_search` ADD UNIQUE `unique` (`name`, `external`)';
+                        if ($objDatabase->Execute($query) === false) {
+                            return _databaseError($query, $objDatabase->ErrorMsg());
+                        }
                     }else{
                         return _databaseError($query, $objDatabase->ErrorMsg());
                     }
@@ -186,10 +186,10 @@ function _statsUpdate()
                 return _databaseError($query, $objDatabase->ErrorMsg());
             }
         }
-	} else {
-		setUpdateMsg(sprintf($_ARRAYLANG['TXT_UNABLE_GETTING_DATABASE_TABLE_STRUCTURE'], DBPREFIX.'stats_search'));
-		return false;
-	}
+    } else {
+        setUpdateMsg(sprintf($_ARRAYLANG['TXT_UNABLE_GETTING_DATABASE_TABLE_STRUCTURE'], DBPREFIX.'stats_search'));
+        return false;
+    }
 
     // only execute this part for versions < 2.1.5
     if ($objUpdate->_isNewerVersion($_CONFIG['coreCmsVersion'], '2.1.5')) {
@@ -231,10 +231,14 @@ function _statsUpdate()
             return \Cx\Lib\UpdateUtil::DefaultActionHandler($e);
         }
     }
-		
+        
     try {
-		//2.2.0: new config option 'exclude_identifying_info'
-		\Cx\Lib\UpdateUtil::sql('INSERT IGNORE INTO '.DBPREFIX.'stats_config (id, name, value, status) VALUES (20, "exclude_identifying_info", 1, 0)');
+        //2.2.0: new config option 'exclude_identifying_info'
+        \Cx\Lib\UpdateUtil::sql('
+            INSERT INTO '.DBPREFIX.'stats_config (id, name, value, status)
+            VALUES (20, "exclude_identifying_info", 1, 0)
+            ON DUPLICATE KEY UPDATE `id` = `id`
+        ');
 
     }
     catch (\Cx\Lib\UpdateException $e) {
@@ -242,6 +246,6 @@ function _statsUpdate()
     }
 
 
-	return true;
+    return true;
 }
 ?>
