f94a7b4123b2438ab6af47d81ba5ad5f57a63577
Reto Kohli <reto.kohli@comvation.com>
2013-01-10 15:24:08 +0000

    Classloader - Fixed warning
    git-svn-id: http://svn.contrexxlabs.com/contrexx/trunk@23044 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- core/ClassLoader/LegacyClassLoader.class.php
+++ core/ClassLoader/LegacyClassLoader.class.php
@@ -8,14 +8,14 @@ class LegacyClassLoader {
     private $bytes = 0;
     private $subBytes = 0;
     private $mapTable = array();
-    
+
     public function __construct() {
         self::$instance = $this;
         if (file_exists(ASCMS_TEMP_PATH.'/legacyClassCache.tmp')) {
             $this->mapTable = unserialize(file_get_contents(ASCMS_TEMP_PATH.'/legacyClassCache.tmp'));
         }
     }
-    
+
     public function autoload($name) {
         $parts = explode('\\', $name);
         // Let doctrine handle it's includes itself
@@ -46,10 +46,17 @@ class LegacyClassLoader {
             // files in /lib/FRAMEWORK
             if ($this->testLoad(ASCMS_FRAMEWORK_PATH.'/'.preg_replace('/FW/', '', $name).'.class.php', $origName)) { return; }
             // files in /lib/PEAR
-            if ($this->testLoad(ASCMS_LIBRARY_PATH.'/PEAR/'.preg_replace('/_/', '/', preg_replace('/PEAR\//', '', $name . '/')).end(preg_split('/_/', $name)).'.php', $origName)) { return; }
+            $end = preg_split('/_/', $name);
+            if ($this->testLoad(
+                    ASCMS_LIBRARY_PATH.'/PEAR/'.
+                    preg_replace(
+                        '/_/', '/', preg_replace('/PEAR\//', '', $name . '/')).
+                    end($end).'.php', $origName)) {
+                return;
+            }
             // files in /model/entities/Cx/Model/Base
             if ($this->testLoad(ASCMS_MODEL_PATH.'/entities/Cx/Model/Base/' . $name . '.php', $origName)) { return; }
-            
+
             // core module and module libraries /[core_modules|modules]/{modulename}/lib/{modulename}Lib.class.php
             $moduleName = strtolower(preg_replace('/Library/', '', $name));
             // exception for mediadir
@@ -73,7 +80,7 @@ class LegacyClassLoader {
             if ($this->testLoad(ASCMS_MODULE_PATH.'/' . $moduleName . '/lib/Lib.class.php', $origName)) { return; }
             if ($this->testLoad(ASCMS_MODULE_PATH.'/' . $moduleName . '/lib/lib.class.php', $origName)) { return; }
             if ($this->testLoad(ASCMS_MODULE_PATH.'/' . $moduleName . '/Lib.class.php', $origName)) { return; }
-            
+
             // core module and module model /[core_modules|modules]/{modulename}/lib/
             $moduleName = current(preg_split('/[A-Z]/', lcfirst($name)));
             $nameWithoutModule = substr($name, strlen($moduleName));
@@ -89,7 +96,7 @@ class LegacyClassLoader {
 
             // core module and module model classes not containing module name
             if ($this->testLoad(ASCMS_MODULE_PATH.'/shop/lib/' . $name . '.class.php', $origName)) { return; }
-            
+
             // Temporary exceptions
             // exception for filesystem. TEMPORARY!
             if ($this->testLoad(ASCMS_FRAMEWORK_PATH.'/File/'.$name.'.class.php', $origName)) {return; }
@@ -97,17 +104,17 @@ class LegacyClassLoader {
             if ($this->testLoad(ASCMS_FRAMEWORK_PATH.'/cxjs/'.$name.'.class.php', $origName)) {return; }
             if ($this->testLoad(ASCMS_FRAMEWORK_PATH.'/cxjs/'.$name.'.interface.php', $origName)) {return; }
             if ($this->testLoad(ASCMS_FRAMEWORK_PATH.'/cxjs/i18n/'.preg_replace('/JQueryUiI18nProvider/', 'jQueryUi', $name).'.class.php', $origName)) {return; }
-            
+
             // This is sort of like giving in...
             $this->fallbackLoad($origName, $name);
         }
         $endTime = microtime(true);
         $this->timeUsed += ($endTime - $startTime);
     }
-    
+
     private function loadFromCache($name) {
         global $objInit;
-        
+
         if (isset($this->mapTable[$name])) {
             $file = $this->mapTable[$name];
             $ending = explode('/', $file);
@@ -122,7 +129,7 @@ class LegacyClassLoader {
         }
         return false;
     }
-    
+
     private function testLoad($path, $name) {
         if (file_exists($path)) {
             $path = substr($path, strlen(ASCMS_DOCUMENT_ROOT));
@@ -138,15 +145,15 @@ class LegacyClassLoader {
         }
         return false;
     }
-    
+
     /**
      * This method won't work for all files at once because max exec time is too short
      * @param type $name
-     * @param type $className 
+     * @param type $className
      */
     private function fallbackLoad($name, $className) {
         global $_CONFIG;
-        
+
         //echo $name . '<br />';
         $namespace = substr($name, 0, strlen($name) - strlen($className) - 1);
         $globDirs = array(
@@ -179,7 +186,7 @@ class LegacyClassLoader {
         }
         $this->testLoad($path, $name);
     }
-    
+
     private function searchClass($name, $namespace, $path = ASCMS_DOCUMENT_ROOT) {
         $files = glob($path . '/*.php');
         foreach ($files as $file) {
@@ -198,7 +205,7 @@ class LegacyClassLoader {
             $fcontent = file_get_contents($file);
             // match namespace too
             $matches = array();
-            
+
             //if (preg_match('/(?:namespace\s+([\\\\\w]+);[.\n\r]*?)?(?:class|interface)\s+' . $name . '\s+(?:extends|implements)?[\\\\\s\w,\n\t\r]*?\{/', $fcontent, $matches)) {
             if (preg_match('/(?:namespace ([\\\\a-zA-Z0-9_]*);[\w\W]*)?(?:class|interface) ' . $name . '(?:[ a-zA-Z0-9\n\r\t\\\\_])*\{/', $fcontent, $matches)) {
                 if (isset($matches[0]) && (!isset($matches[1]) || $matches[1] == $namespace)) {
@@ -218,10 +225,10 @@ class LegacyClassLoader {
         }
         return false;
     }
-    
+
     private function loadClass($path, $name) {
         global $_CONFIG;
-        
+
         $this->tab++;
         $bytes = memory_get_peak_usage();
         if ($_CONFIG['useCustomizings'] == 'on' && file_exists(ASCMS_CUSTOMIZING_PATH . '/' . $path)) {
@@ -245,15 +252,15 @@ class LegacyClassLoader {
             echo '<br />';
         }*/
     }
-    
+
     public static function getInstance() {
         return self::$instance;
     }
-    
+
     public function getTimeUsed() {
         return $this->timeUsed;
     }
-    
+
     public function getRamUsed() {
         return $this->bytes;
     }
