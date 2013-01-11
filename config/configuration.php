2ce0ab88d306f886bf889cb8ac154a8375b0c7f1
Thomas Däppen <thomas.daeppen@cloudrexx.com>
2013-01-11 10:55:57 +0000

    configuration.php: - removed shop config - minor optimization of file structure
    git-svn-id: http://svn.contrexxlabs.com/contrexx/trunk@23070 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- config/configuration.php
+++ config/configuration.php
@@ -25,7 +25,7 @@ $_DBCONFIG['database'] = 'dev'; // Database name
 $_DBCONFIG['tablePrefix'] = 'contrexx_'; // Database table prefix
 $_DBCONFIG['user'] = 'root'; // Database username
 $_DBCONFIG['password'] = ''; // Database password
-$_DBCONFIG['dbType'] = 'mysql';	// Database type (e.g. mysql,postgres ..)
+$_DBCONFIG['dbType'] = 'mysql';    // Database type (e.g. mysql,postgres ..)
 $_DBCONFIG['charset'] = 'utf8'; // Charset (default, latin1, utf8, ..)
 $_DBCONFIG['timezone'] = $_CONFIG['timezone']; // Timezone
 
@@ -43,8 +43,8 @@ $_PATHCONFIG['ascms_root_offset'] = '/cm_2_3'; // example: '/cms';
 * -------------------------------------------------------------------------
 */
 $_FTPCONFIG['is_activated'] = false; // Ftp support true or false
-$_FTPCONFIG['use_passive'] = false; // Use passive ftp mode
-$_FTPCONFIG['host'] = 'localhost';// This is normally set to localhost
+$_FTPCONFIG['use_passive'] = false;    // Use passive ftp mode
+$_FTPCONFIG['host']    = 'localhost';// This is normally set to localhost
 $_FTPCONFIG['port'] = 21; // Ftp remote port
 $_FTPCONFIG['username'] = ''; // Ftp login username
 $_FTPCONFIG['password'] = ''; // Ftp login password
@@ -52,32 +52,20 @@ $_FTPCONFIG['path'] = ''; // Ftp path to cms (must not include ascms_root_offset
 
 /**
 * -------------------------------------------------------------------------
-* Optional customizing exceptions
-* Shopnavbar: If set to TRUE the shopnavbar will appear on each page
+* Base setup (altering might break the system!)
 * -------------------------------------------------------------------------
 */
-$_CONFIGURATION['custom']['shopnavbar'] = false; // true|false
-$_CONFIGURATION['custom']['shopJsCart'] = false; // true|false
-
-/**
-* Set character encoding
-*/
+// Set character encoding
 $_CONFIG['coreCharacterEncoding'] = 'UTF-8'; // example 'UTF-8'
 @ini_set('default_charset', $_CONFIG['coreCharacterEncoding']);
 
-/**
-* Set output url seperator
-*/
+// Set output url seperator
 @ini_set('arg_separator.output', '&amp;');
 
-/**
-* Set url rewriter tags
-*/
+// Set url rewriter tags
 @ini_set('url_rewriter.tags', 'a=href,area=href,frame=src,iframe=src,input=src,form=,fieldset=');
 
-/**
-* Set timezone
-*/
+// Set timezone
 @ini_set('date.timezone', $_CONFIG['timezone']);
 
 /**
@@ -86,4 +74,4 @@ $_CONFIG['coreCharacterEncoding'] = 'UTF-8'; // example 'UTF-8'
 * -------------------------------------------------------------------------
 */
 require_once dirname(__FILE__).'/set_constants.php';
-?>
+
