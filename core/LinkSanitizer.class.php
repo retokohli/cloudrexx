fa3b1c4cb7d5442ca09cd9e0d5cd0d4610ba1c4d
Yannic Tschanz <yannic.tschanz@comvation.com>
2013-02-15 09:57:15 +0000

    LinkSanitizer (bugfix #1016): parameters preview and appview are now appended to all href and action attributes if passed Url (bugfix #1016): parameters preview and appview are now added by default
    git-svn-id: http://svn.contrexxlabs.com/contrexx/trunk@23510 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- core/LinkSanitizer.class.php
+++ core/LinkSanitizer.class.php
@@ -33,29 +33,80 @@ class LinkSanitizer {
      * Calculates and returns the content with all replacements done.
      */
     function replace() {
-        return preg_replace("/
-                (
-                    # match all SRC and HREF attributes 
-                    \s+(src|href|action)\s*=\s*['\"]
-                    |
-                    # or match all CSS @import statements
-                    @import\s+url\s*\(                             
-                )
+        $content = preg_replace("/
+            (
+                # match all SRC and HREF attributes 
+                \s+(src|href|action)\s*=\s*['\"]
+                |
+                # or match all CSS @import statements
+                @import\s+url\s*\(                             
+            )
 
-                # but only those who's values don't start with a slash..
-                (?=[^\/])
+            # but only those who's values don't start with a slash..
+            (?=[^\/])
 
-                # ..and neither start with a protocol (http:, ftp:, javascript:, mailto:, etc)
-                (?![a-z]+:)
+            # ..and neither start with a protocol (http:, ftp:, javascript:, mailto:, etc)
+            (?![a-zA-Z]+:)
 
-                # ..and neither start width an ampersand followed by a sharp and end with a semicolon (which would indicate that the url contains html codes for ascii characters)
-                (?!&\#\d+;)
+            # ..and neither start width an ampersand followed by a sharp and end with a semicolon (which would indicate that the url contains html codes for ascii characters)
+            (?!&\#\d+;)
 
-                # ..and neither start with a sharp
-                (?!\#)
+            # ..and neither start with a sharp
+            (?!\#)
 
-                # ..and neither start with a backslash which would indicate that the url lies within some javascript code
-                (?!\\\)
-            /x", '\1'.$this->offset, $this->content);
+            # ..and neither start with a backslash which would indicate that the url lies within some javascript code
+            (?!\\\)
+        /x", '\1'.$this->offset, $this->content);
+
+        if (!empty($_GET['preview']) || (isset($_GET['appview']) && ($_GET['appview'] == 1))) {
+            $content = preg_replace_callback("/
+                (\<(?:a|form)[^>]*?\s+(?:href|action)\s*=\s*)
+                (['\"])
+                ((?![a-zA-Z]+?:|\\\\).+?)
+                \\2
+                ([^>]*\>)
+            /x", array($this, 'appendParameters'), $content);
+        }
+
+        return $content;
     }
+
+    /**
+     * Callback method for appending preview and appview parameter to href and action attributes.
+     *
+     * @access  private
+     * @param   array       $matches    regex matches
+     * @return  string                  replacement string
+     */
+    private function appendParameters($matches) {
+        $before = $matches[1];
+        $quote = $matches[2];
+        $value = $matches[3];
+        $after = $matches[4];
+
+        if (strpos($value, '?') !== false) {
+            list($path, $query) = explode('?', $value);
+            $query = \Cx\Core\Routing\Url::params2array($query);
+        } else {
+            $path = $value;
+            $query = array();
+        }
+
+        $extension = pathinfo($path, PATHINFO_EXTENSION);
+        if (!empty($extension) && ($extension != 'php')) {
+            return $matches[0];
+        }
+
+        if (!empty($_GET['preview']) && !isset($query['preview'])) {
+            $query['preview'] = $_GET['preview'];
+        }
+        if ((isset($_GET['appview']) && ($_GET['appview'] == 1)) && !isset($query['appview'])) {
+            $query['appview'] = $_GET['appview'];
+        }
+
+        $query = \Cx\Core\Routing\Url::array2params($query);
+
+        return $before.$quote.$path.'?'.$query.$quote.$after;
+    }
+
 }
