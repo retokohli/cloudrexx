366828ecfdff6a4ba7eedc1dbf4dcab83a3736ce
Michael Raess <michael.raess@comvation.com>
2013-02-18 09:29:40 +0000

    Model Node Repository - move - Moved php code into another file and removed the old files
    git-svn-id: http://svn.contrexxlabs.com/contrexx/trunk@23559 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- model/entities/Cx/Model/ContentManager/Repository/NodeRepository.class.php
+++ model/entities/Cx/Model/ContentManager/Repository/NodeRepository.class.php
@@ -11,9 +11,12 @@
 
 namespace Cx\Model\ContentManager\Repository;
 
-use Doctrine\ORM\EntityManager;
-use Doctrine\ORM\Mapping\ClassMetadata;
-use Gedmo\Tree\Entity\Repository\CxNestedTreeRepository;
+use Doctrine\ORM\EntityManager,
+    Doctrine\ORM\Mapping\ClassMetadata,
+    Doctrine\ORM\Query,
+    Gedmo\Tree\Strategy\ORM\Nested,
+    Gedmo\Tree\Entity\Repository\NestedTreeRepository,
+    Gedmo\Exception\InvalidArgumentException;
 
 /**
  * NodeRepository
@@ -23,7 +26,7 @@ use Gedmo\Tree\Entity\Repository\CxNestedTreeRepository;
  * @package     contrexx
  * @subpackage  model_contentmanager
  */
-class NodeRepository extends CxNestedTreeRepository {
+class NodeRepository extends NestedTreeRepository {
     protected $em = null;
     const DataProperty = '__data';
 
@@ -144,6 +147,182 @@ class NodeRepository extends CxNestedTreeRepository {
         $rootNode->setRgt($left);
         $this->em->persist($rootNode);
         $this->em->flush();
+    }
+
+    /**
+     * Get the query for next siblings of the given $node
+     *
+     * @param object $node
+     * @param bool $includeSelf - include the node itself
+     * @throws \Gedmo\Exception\InvalidArgumentException - if input is invalid
+     * @return Query
+     */
+    public function getNextSiblingsQuery($node, $includeSelf = false, $skipAliasNodes = false)
+    {
+        $meta = $this->getClassMetadata();
+        if (!$node instanceof $meta->name) {
+            throw new InvalidArgumentException("Node is not related to this repository");
+        }
+        if (!$this->_em->getUnitOfWork()->isInIdentityMap($node)) {
+            throw new InvalidArgumentException("Node is not managed by UnitOfWork");
+        }
+
+        $config = $this->listener->getConfiguration($this->_em, $meta->name);
+        $parent = $meta->getReflectionProperty($config['parent'])->getValue($node);
+        if (!$parent) {
+            throw new InvalidArgumentException("Cannot get siblings from tree root node");
+        }
+        $parentId = current($this->_em->getUnitOfWork()->getEntityIdentifier($parent));
+
+        $left  = $meta->getReflectionProperty($config['left'])->getValue($node);
+        $sign  = $includeSelf ? '>=' : '>';
+        $page  = $skipAliasNodes ? ', Cx\Model\ContentManager\Page page' : '';
+        $where = $skipAliasNodes ? ' AND node.id = page.node' : '';
+        $type  = $skipAliasNodes ? ' AND page.type <> \'alias\'' : '';
+        $group = $skipAliasNodes ? ' GROUP BY node.id' : '';
+
+        $dql = "SELECT node FROM {$config['useObjectClass']} node {$page}";
+        $dql .= " WHERE node.{$config['parent']} = {$parentId}";
+        $dql .= " AND node.{$config['left']} {$sign} {$left}";
+        $dql .= $where;
+        $dql .= $type;
+        $dql .= $group;
+        $dql .= " ORDER BY node.{$config['left']} ASC";
+        return $this->_em->createQuery($dql);
+    }
+
+    /**
+     * Find the next siblings of the given $node
+     *
+     * @param object $node
+     * @param bool $includeSelf - include the node itself
+     * @return array
+     */
+    public function getNextSiblings($node, $includeSelf = false, $skipAliasNodes = false)
+    {
+        return $this->getNextSiblingsQuery($node, $includeSelf, $skipAliasNodes)->getResult();
+    }
+
+    /**
+     * Get query for previous siblings of the given $node
+     *
+     * @param object $node
+     * @param bool $includeSelf - include the node itself
+     * @throws \Gedmo\Exception\InvalidArgumentException - if input is invalid
+     * @return Query
+     */
+    public function getPrevSiblingsQuery($node, $includeSelf = false, $skipAliasNodes = false)
+    {
+        $meta = $this->getClassMetadata();
+        if (!$node instanceof $meta->name) {
+            throw new InvalidArgumentException("Node is not related to this repository");
+        }
+        if (!$this->_em->getUnitOfWork()->isInIdentityMap($node)) {
+            throw new InvalidArgumentException("Node is not managed by UnitOfWork");
+        }
+
+        $config = $this->listener->getConfiguration($this->_em, $meta->name);
+        $parent = $meta->getReflectionProperty($config['parent'])->getValue($node);
+        if (!$parent) {
+            throw new InvalidArgumentException("Cannot get siblings from tree root node");
+        }
+        $parentId = current($this->_em->getUnitOfWork()->getEntityIdentifier($parent));
+
+        $left  = $meta->getReflectionProperty($config['left'])->getValue($node);
+        $sign  = $includeSelf ? '<=' : '<';
+        $page  = $skipAliasNodes ? ', Cx\Model\ContentManager\Page page' : '';
+        $where = $skipAliasNodes ? ' AND node.id = page.node' : '';
+        $type  = $skipAliasNodes ? ' AND page.type <> \'alias\'' : '';
+        $group = $skipAliasNodes ? ' GROUP BY node.id' : '';
+
+        $dql = "SELECT node FROM {$config['useObjectClass']} node {$page}";
+        $dql .= " WHERE node.{$config['parent']} = {$parentId}";
+        $dql .= " AND node.{$config['left']} {$sign} {$left}";
+        $dql .= $where;
+        $dql .= $type;
+        $dql .= $group;
+        $dql .= " ORDER BY node.{$config['left']} ASC";
+        return $this->_em->createQuery($dql);
+    }
+
+    /**
+     * Find the previous siblings of the given $node
+     *
+     * @param object $node
+     * @param bool $includeSelf - include the node itself
+     * @return array
+     */
+    public function getPrevSiblings($node, $includeSelf = false, $skipAliasNodes = false)
+    {
+        return $this->getPrevSiblingsQuery($node, $includeSelf, $skipAliasNodes)->getResult();
+    }
+
+    /**
+     * Move the node down in the same level
+     *
+     * @param object $node
+     * @param mixed $number
+     *         integer - number of positions to shift
+     *         boolean - if "true" - shift till last position
+     * @throws RuntimeException - if something fails in transaction
+     * @return boolean - true if shifted
+     */
+    public function moveDown($node, $number = 1, $skipAliasNodes = false)
+    {
+        $result = false;
+        $meta = $this->getClassMetadata();
+        if ($node instanceof $meta->name) {
+            $config = $this->listener->getConfiguration($this->_em, $meta->name);
+            $nextSiblings = $this->getNextSiblings($node, false, $skipAliasNodes);
+            if ($numSiblings = count($nextSiblings)) {
+                $result = true;
+                if ($number === true) {
+                    $number = $numSiblings;
+                } elseif ($number > $numSiblings) {
+                    $number = $numSiblings;
+                }
+                $this->listener
+                    ->getStrategy($this->_em, $meta->name)
+                    ->updateNode($this->_em, $node, $nextSiblings[$number - 1], Nested::NEXT_SIBLING);
+            }
+        } else {
+            throw new InvalidArgumentException("Node is not related to this repository");
+        }
+        return $result;
+    }
+
+    /**
+     * Move the node up in the same level
+     *
+     * @param object $node
+     * @param mixed $number
+     *         integer - number of positions to shift
+     *         boolean - true shift till first position
+     * @throws RuntimeException - if something fails in transaction
+     * @return boolean - true if shifted
+     */
+    public function moveUp($node, $number = 1, $skipAliasNodes = false)
+    {
+        $result = false;
+        $meta = $this->getClassMetadata();
+        if ($node instanceof $meta->name) {
+            $config = $this->listener->getConfiguration($this->_em, $meta->name);
+            $prevSiblings = array_reverse($this->getPrevSiblings($node, false, $skipAliasNodes));
+            if ($numSiblings = count($prevSiblings)) {
+                $result = true;
+                if ($number === true) {
+                    $number = $numSiblings;
+                } elseif ($number > $numSiblings) {
+                    $number = $numSiblings;
+                }
+                $this->listener
+                    ->getStrategy($this->_em, $meta->name)
+                    ->updateNode($this->_em, $node, $prevSiblings[$number - 1], Nested::PREV_SIBLING);
+            }
+        } else {
+            throw new InvalidArgumentException("Node is not related to this repository");
+        }
+        return $result;
     }
 }
 
