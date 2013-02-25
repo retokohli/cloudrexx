71cfbaef38e1714cc060d30c6e1fcdb097f068ad
Ueli Kramer <ueli.kramer@comvation.com>
2013-02-25 15:43:41 +0000

    sociallogin - bugfix - all inactive users have been deleted
    git-svn-id: http://svn.contrexxlabs.com/contrexx/trunk@23759 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- lib/FRAMEWORK/User/User.class.php
+++ lib/FRAMEWORK/User/User.class.php
@@ -1666,19 +1666,9 @@ class User extends User_Profile
                   LEFT JOIN `'.DBPREFIX.'access_user_attribute_value` AS tblA ON tblA.`user_id`=tblU.`id`
                   LEFT JOIN `'.DBPREFIX.'access_user_network` AS tblN ON tblN.`user_id`=tblU.`id`
                  WHERE tblU.`active`=0
-                   AND ((tblU.`restore_key`!=\'\'
-                   AND tblU.`restore_key_time`<'.time().') OR
-                   tblU.`expiration`<'.time().')');
+                   AND tblU.`restore_key`!=\'\'
+                   AND tblU.`restore_key_time`<'.time());
         }
-        $objDatabase->Execute('
-                DELETE tblU, tblP, tblG, tblA, tblN
-                  FROM `'.DBPREFIX.'access_users` AS tblU
-                 INNER JOIN `'.DBPREFIX.'access_user_profile` AS tblP ON tblP.`user_id`=tblU.`id`
-                  LEFT JOIN `'.DBPREFIX.'access_rel_user_group` AS tblG ON tblG.`user_id`=tblU.`id`
-                  LEFT JOIN `'.DBPREFIX.'access_user_attribute_value` AS tblA ON tblA.`user_id`=tblU.`id`
-                  LEFT JOIN `'.DBPREFIX.'access_user_network` AS tblN ON tblN.`user_id`=tblU.`id`
-                 WHERE tblU.`active`=0
-                   AND tblU.`expiration`<'.time());
     }
 
 
