d0f71b7378b9f3dd66109cde543afe7080f2832a
Michael Ritter <michael.ritter@cloudrexx.com>
2013-02-25 15:27:05 +0000

    Model - bugfix - Made doctrine-cli and generate-entities.sh work (was broken because of classloader)
    git-svn-id: http://svn.contrexxlabs.com/contrexx/trunk@23756 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- model/doctrine.php
+++ model/doctrine.php
@@ -1,11 +1,9 @@
 <?php
-//get constants
-$doctrineDir = ASCMS_LIBRARY_PATH.'/doctrine/';
-
 // Variable $helperSet is defined inside cli-config.php
 require __DIR__ . '/cli-config.php';
-require_once ASCMS_CORE_PATH.'/ClassLoader/ClassLoader.class.php';
-new \Cx\Core\ClassLoader\ClassLoader(ASCMS_DOCUMENT_ROOT);
+
+//get constants
+$doctrineDir = ASCMS_LIBRARY_PATH.'/doctrine/';
 
 $cli = new \Symfony\Component\Console\Application('Doctrine Command Line Interface', Doctrine\Common\Version::VERSION);
 $cli->setCatchExceptions(true);
