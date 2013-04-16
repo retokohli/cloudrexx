acbaddc21dc2c7e3a6b77c2b1d0309a6499fb9ff
Michael Ritter <michael.ritter@cloudrexx.com>
2013-04-16 15:45:33 +0000

    core_modules Workbench - update - Updates module structure to match new definitions
    git-svn-id: http://svn.contrexxlabs.com/contrexx/trunk@24315 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

---
+++ core_modules/Workbench/Model/Entity/Command.class.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Cx\Core_Modules\Workbench\Model;
+
+class CommandException extends \Cx\Core_Modules\Workbench\Controller\WorkbenchException {};
+
+abstract class Command {
+    protected $interface;
+    protected $name;
+    protected $description;
+    protected $synopsis;
+    protected $help;
+    
+    public function __construct(UserInterface $owner) {
+        $this->interface = $owner;
+    }
+    
+    public function getName() {
+        return $this->name;
+    }
+    
+    public function getDescription() {
+        return $this->description;
+    }
+    
+    public function getSynopsis() {
+        return $this->synopsis;
+    }
+    
+    public function getHelp() {
+        return $this->help;
+    }
+    
+    public abstract function execute(array $arguments);
+}
