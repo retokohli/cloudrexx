d675c0d8cc334a60b834f40ee8be90cfbbce9131
Michael Ritter <michael.ritter@cloudrexx.com>
2013-04-26 08:29:17 +0000

    index.php - update - Since Cx class can do everything by itself, we don't have to do a lot in index.php
    git-svn-id: http://svn.contrexxlabs.com/contrexx/trunk@24418 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
  * displayed on these hostings.
  * 
  * Checks PHP version, loads debugger and initial config, checks if installed
- * and loads the Contrexx class (from customizing if configured to do so)
+ * and loads the Contrexx class
  * @version 3.1.0
  * @author Michael Ritter <michael.ritter@comvation.com>
  */
@@ -33,68 +33,8 @@ if (version_compare($php, '5.3.0') < 0) {
  * Calling these methods without specifying a debug level
  * will either activate or deactivate all levels.
  */
-require_once dirname(__FILE__).'/lib/FRAMEWORK/DBG/DBG.php';
-//\DBG::activate(DBG_PHP);
+/*require_once dirname(__FILE__).'/lib/FRAMEWORK/DBG/DBG.php';
+//\DBG::activate(DBG_PHP);*/
 
-// Initialize global vars, in order to disable notices
-// @todo: Don't use the global scope anymore
-$_DBCONFIG = $_CONFIGURATION = $_CONFIG = null;
-
-/**
- * User configuration settings
- *
- * This file is re-created by the CMS itself. It initializes the
- * {@link $_CONFIG[]} global array.
- */
-$incSettingsStatus = include_once dirname(__FILE__).'/config/settings.php';
-
-/**
- * Path, database, FTP configuration settings
- *
- * Initialises global settings array and constants.
- */
-include_once dirname(__FILE__).'/config/configuration.php';
-
-// Check if the system is installed
-if (!defined('CONTEXX_INSTALLED') || !CONTEXX_INSTALLED) {
-    header('Location: ../installer/index.php');
-    exit;
-} else if ($incSettingsStatus === false) {
-    die('System halted: Unable to load basic configuration!');
-}
-
-// Check if the system is configured with enabled customizings
-$customizing = null;
-if (isset($_CONFIG['useCustomizings']) && $_CONFIG['useCustomizings'] == 'on') {
-    $customizing = ASCMS_CUSTOMIZING_PATH;
-}
-
-/**
- * This is the old fashion way, requiring two separate files for front- and backend
- */
-/*
-if ($customizing && file_exists(ASCMS_CUSTOMIZING_PATH.'/core/initFrontend.php')) {
-    require_once(ASCMS_CUSTOMIZING_PATH.'/core/initFrontend.php');
-} else {
-    require_once(ASCMS_CORE_PATH.'/initFrontend.php');
-}//*/
-
-/**
- * This is the new way (BETA). To test this, perform the following steps:
- * 1. Comment out the old fashion code above (just remove // on line 75)
- * 2. Uncomment the code below (add // on line 88)
- * 3. Remove cadmin from whitelist in .htaccess
- */
-///*
-// Load the Contrexx class (from customizing if enabled)
-if ($customizing && file_exists(ASCMS_CUSTOMIZING_PATH.'/core/Cx.class.php')) {
-    require_once(ASCMS_CUSTOMIZING_PATH.'/core/Cx.class.php');
-} else {
-    require_once(ASCMS_CORE_PATH.'/Cx.class.php');
-}
-// load in frontend mode as long as anything else than /cadmin is requested:
-// @todo improve this rule, use constants and move to Cx class:
-$frontend = $_GET['__cap'] != ASCMS_PATH_OFFSET.'/cadmin/index.php';
-
-// Initialize the Contrexx class (we don't use the constructor in order to avoid namespaces:
-init($frontend);//*/
+require_once(dirname(__FILE__).'/core/Cx.class.php');
+init();
