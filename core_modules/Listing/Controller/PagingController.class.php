c696c1992a443b77e994fbc055fbe34463b7c1ab
Michael Ritter <michael.ritter@cloudrexx.com>
2013-04-29 20:34:48 +0000

    Core_Modules Listing - bugfix - Replaced tabs by four spaces Core_Modules Listing - update - Added some comments Core_Modules Listing - update - Passing arguments to data handlers Core_Modules Listing - update - Callback function can return a doctrine query Core_Modules Listing - update - Added abstract class ListingHandler Core_Modules Listing - update - Committing draft of PagingController
    git-svn-id: http://svn.contrexxlabs.com/contrexx/trunk@24449 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- core_modules/Listing/Controller/PagingController.class.php
+++ core_modules/Listing/Controller/PagingController.class.php
@@ -2,9 +2,21 @@
 
 namespace Cx\Core_Modules\Listing\Controller;
 
-class PagingController {
-	
-	public function handle(&$offset, &$count, &$criteria, &$order) {
-	
-	}
+class PagingController extends ListingHandler {
+    protected $countPerPage = 0;
+    protected $currentPage = 0;
+    
+    public function __construct() {
+        global $_CONFIG;
+        
+        $this->countPerPage = $_CONFIG['corePagingLimit'];
+    }
+    
+    public function handle(&$offset, &$count, &$criteria, &$order, &$args) {
+        $offset = 0;
+        if (isset($args['pos'])) {
+            $offset = $args['pos'];
+        }
+        $count = $this->countPerPage;
+    }
 }
