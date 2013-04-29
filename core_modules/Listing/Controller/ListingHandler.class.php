df6bce1ebc1e4f29f49d654c7a99ce24f5a5cbf7
Michael Ritter <michael.ritter@cloudrexx.com>
2013-04-29 20:34:48 +0000

    Core_Modules Listing - bugfix - Replaced tabs by four spaces Core_Modules Listing - update - Added some comments Core_Modules Listing - update - Passing arguments to data handlers Core_Modules Listing - update - Callback function can return a doctrine query Core_Modules Listing - update - Added abstract class ListingHandler Core_Modules Listing - update - Committing draft of PagingController
    git-svn-id: http://svn.contrexxlabs.com/contrexx/trunk@24449 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

---
+++ core_modules/Listing/Controller/ListingHandler.class.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Cx\Core_Modules\Listing\Controller;
+
+/**
+ * This class defines a handler for Listings
+ * (for example the PagingController)
+ * @author ritt0r
+ *
+ */
+abstract class ListingHandler {
+    
+    /**
+     * Constructor for the Handler. Do not
+     * do anything Listing specific (e.g.
+     * don't initialize page number here)
+     */
+    public abstract function __construct();
+    
+    /**
+     * This method is called once for each Listing
+     * @param int $offset The limit offset for the listing
+     * @param int $count The limit count for the listing
+     * @param array $criteria Criteria to be matched by the listing
+     * @param array $order Order listing by this fields
+     * @param array $args Arguments supplied for this listing
+     */
+    public abstract function handle(&$offset, &$count, &$criteria, &$order, &$args);
+}
