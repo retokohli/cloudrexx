c40478e9be84291c0457c67db6bfb6953290e413
Michael Ritter <michael.ritter@cloudrexx.com>
2013-04-30 14:56:50 +0000

    core_modules Workbench - update - Added component view to toolbox (draft) core_modules Workbench - bugfix - Fixed content navigation core_modules Workbench - update - Some cleanup in ComponentController
    git-svn-id: http://svn.contrexxlabs.com/contrexx/trunk@24479 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- core_modules/Workbench/Controller/ComponentController.class.php
+++ core_modules/Workbench/Controller/ComponentController.class.php
@@ -22,9 +22,6 @@ class ComponentController extends \Cx\Core\Component\Model\Entity\SystemComponen
     public function load(\Cx\Core\Cx $cx, \Cx\Core\ContentManager\Model\Entity\Page $page = null) {
         $objTemplate = $cx->getTemplate();
         $_ARRAYLANG = \Env::get('init')->loadLanguageData($this->getName());
-
-        $post = $_POST;
-        //\DBG::activate(DBG_PHP);
         
         // Initialize
         if (!isset($_GET['act'])) {
@@ -59,7 +56,8 @@ class ComponentController extends \Cx\Core\Component\Model\Entity\SystemComponen
                 }
             default:
                 $navEntries = array(
-                    'index.php?cmd=Workbench&amp;act=development' => 'YAML',
+                    'index.php?cmd=Workbench&amp;act=development/yaml' => 'YAML',
+                    'index.php?cmd=Workbench&amp;act=development/components' => 'Components',
                 );
                 $objTemplate->setVariable('ADMIN_CONTENT', new Toolbox($_ARRAYLANG, $act, $_POST));
                 break;
@@ -73,7 +71,7 @@ class ComponentController extends \Cx\Core\Component\Model\Entity\SystemComponen
                 'HREF' => $href,
                 'TITLE' => $title,
             ));
-            if ($title == strtoupper($act)) {
+            if (strtolower($title) == $act) {
                 $navigation->touchBlock('active');
             }
             $navigation->parse('tab_entry');
@@ -81,35 +79,25 @@ class ComponentController extends \Cx\Core\Component\Model\Entity\SystemComponen
         $objTemplate->setVariable('CONTENT_NAVIGATION', $navigation->get());
     }
 
-    public function preResolve(\Cx\Core\Cx $cx, \Cx\Core\Routing\Url $request) {
-        
-    }
+    public function preResolve(\Cx\Core\Cx $cx, \Cx\Core\Routing\Url $request) {}
 
-    public function postResolve(\Cx\Core\Cx $cx, \Cx\Core\ContentManager\Model\Entity\Page $page = null) {
-        
-    }
+    public function postResolve(\Cx\Core\Cx $cx, \Cx\Core\ContentManager\Model\Entity\Page $page = null) {}
 
-    public function preContentLoad(\Cx\Core\Cx $cx, \Cx\Core\ContentManager\Model\Entity\Page $page = null) {
-        
-    }
+    public function preContentLoad(\Cx\Core\Cx $cx, \Cx\Core\ContentManager\Model\Entity\Page $page = null) {}
 
-    public function preContentParse(\Cx\Core\Cx $cx, \Cx\Core\ContentManager\Model\Entity\Page $page = null) {
-        
-    }
+    public function preContentParse(\Cx\Core\Cx $cx, \Cx\Core\ContentManager\Model\Entity\Page $page = null) {}
 
-    public function postContentParse(\Cx\Core\Cx $cx, &$content) {
-        
-    }
+    public function postContentParse(\Cx\Core\Cx $cx, &$content) {}
 
     public function postContentLoad(\Cx\Core\Cx $cx, &$content) {
         $warning = new \Cx\Core\Html\Sigma(ASCMS_CORE_MODULE_PATH . '/Workbench/View/Template');
         $warning->loadTemplateFile('Warning.html');
-        echo $warning->get();
+        echo $warning->get() . '<div>';
     }
 
-    public function preFinalize(\Cx\Core\Cx $cx, \Cx\Core\Html\Sigma $template) {
-    }
+    public function preFinalize(\Cx\Core\Cx $cx, \Cx\Core\Html\Sigma $template) {}
 
     public function postFinalize(\Cx\Core\Cx $cx) {
+        echo '</div>';
     }
 }
--- core_modules/Workbench/Controller/Toolbox.class.php
+++ core_modules/Workbench/Controller/Toolbox.class.php
@@ -7,25 +7,101 @@ class Toolbox {
     
     public function __construct(&$language, $mode, &$arguments) {
         $this->template = new \Cx\Core\Html\Sigma(ASCMS_CORE_MODULE_PATH . '/Workbench/View/Template');
-        $this->template->loadTemplateFile('Toolbox.html');
-        $res = \Env::get('db')->Execute('SHOW TABLES');
-        while (!$res->EOF) {
-            $this->template->setVariable('TABLE', current($res->fields));
-            $this->template->parse('table_option');
-            $res->MoveNext();
+        switch ($mode) {
+            case 'yaml':
+                $this->template->loadTemplateFile('Yaml.html');
+                $res = \Env::get('db')->Execute('SHOW TABLES');
+                while (!$res->EOF) {
+                    $this->template->setVariable('TABLE', current($res->fields));
+                    $this->template->parse('table_option');
+                    $res->MoveNext();
+                }
+                //if (mode = table) {
+                $result = $this->loadSql($arguments['table']);
+                //} else {
+                //$result = sql
+                //}
+                $result = $this->sql2Yaml($result);
+                break;
+                $this->template->setVariable(array(
+                    'TXT_WORKBENCH_TOOLBOX_YAML_FROM_TABLE' => $language['TXT_WORKBENCH_TOOLBOX_YAML_FROM_TABLE'],
+                    'TXT_WORKBENCH_TOOLBOX_YAML_FROM_SQL' => $language['TXT_WORKBENCH_TOOLBOX_YAML_FROM_SQL'],
+                    'TXT_WORKBENCH_TOOLBOX_SUBMIT' => $language['TXT_WORKBENCH_TOOLBOX_SUBMIT'],
+                    'RESULT' => $result,
+                ));
+            case 'components':
+                $this->template->loadTemplateFile('Components.html');
+                $query = '
+                    SELECT
+                        `id`,
+                        `name`,
+                        `is_required`,
+                        `is_core`
+                    FROM
+                        `' . DBPREFIX . 'modules`
+                    ORDER BY
+                        `name` ASC
+                ';
+                $res = \Env::get('db')->Execute($query);
+                $modules = array();
+                while (!$res->EOF) {
+                    $modules[] = array(
+                        'id' => $res->fields['id'],
+                        'name' => $res->fields['name'],
+                        'type' => ($res->fields['is_required'] && $res->fields['is_core'] ? 'core' : ($res->fields['is_core'] ? 'core_module' : 'module')),
+                        'exists_db' => 'true',
+                        'exists_filesystem' => $this->componentExistsInFileSystem(
+                            $res->fields['is_required'],
+                            $res->fields['is_core'],
+                            $res->fields['name']
+                        ),
+                        'style' => $this->getComponentStyle(
+                            $res->fields['is_required'],
+                            $res->fields['is_core'],
+                            $res->fields['name']
+                        ),
+                    );
+                    $res->MoveNext();
+                }
+                $table = new \BackendTable(new \Cx\Core_Modules\Listing\Model\Entity\DataSet($modules));
+                $this->template->setVariable(array(
+                    'RESULT' => $table->toHtml(),
+                ));
+                break;
         }
-        //if (mode = table) {
-        $result = $this->loadSql($arguments['table']);
-        //} else {
-        //$result = sql
-        //}
-        $result = $this->sql2Yaml($result);
-        $this->template->setVariable(array(
-            'TXT_WORKBENCH_TOOLBOX_YAML_FROM_TABLE' => $language['TXT_WORKBENCH_TOOLBOX_YAML_FROM_TABLE'],
-            'TXT_WORKBENCH_TOOLBOX_YAML_FROM_SQL' => $language['TXT_WORKBENCH_TOOLBOX_YAML_FROM_SQL'],
-            'TXT_WORKBENCH_TOOLBOX_SUBMIT' => $language['TXT_WORKBENCH_TOOLBOX_SUBMIT'],
-            'RESULT' => $result,
-        ));
+    }
+    
+    protected function componentExistsInFileSystem($required, $core, $name) {
+        $path = ASCMS_MODULE_FOLDER;
+        if ($required && $core) {
+            $path = ASCMS_CORE_FOLDER;
+        } else if ($core) {
+            $path = ASCMS_CORE_MODULE_FOLDER;
+        }
+        $path .= '/' . $name;
+        if (is_dir(ASCMS_CUSTOMIZING_PATH . $path)) {
+            return 'customizing';
+        } else if (is_dir(ASCMS_DOCUMENT_ROOT . $path)) {
+            return 'true';
+        }
+        return 'false';
+    }
+    
+    protected function getComponentStyle($required, $core, $name) {
+        if ($this->componentExists($name)) {
+            return '3.1';
+        }
+        if ($this->componentExistsInFileSystem($required, $core, $name) !== 'false') {
+            if (preg_match('/[A-Z]/', $name)) {
+                return '3.0';
+            }
+        }
+        return '<= 2.2.6';
+    }
+    
+    protected function componentExists($name) {
+        $componentRepo = \Env::get('em')->getRepository('Cx\Core\Component\Model\Entity\SystemComponent');
+        return (bool) $componentRepo->findOneBy(array('name' => $name));
     }
     
     protected function loadSql($table) {
