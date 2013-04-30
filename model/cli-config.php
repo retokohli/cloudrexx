3b79cddf82dfaac3d91ee489a4b203f20cccec87
Michael Ritter <michael.ritter@cloudrexx.com>
2013-04-30 13:27:01 +0000

    model doctrine - bugfix - Loading entity manager via \Cx\Core\Db\Db
    git-svn-id: http://svn.contrexxlabs.com/contrexx/trunk@24475 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- model/cli-config.php
+++ model/cli-config.php
@@ -6,7 +6,9 @@ include('../config/settings.php');
 require_once ASCMS_CORE_PATH.'/ClassLoader/ClassLoader.class.php';
 $cl = new \Cx\Core\ClassLoader\ClassLoader(ASCMS_DOCUMENT_ROOT);
 
-include('../config/doctrine.php');
+$db = new \Cx\Core\Db\Db();
+$em = $db->getEntityManager();
+\Env::set('em', $em);
 
 $helpers = array(
     'db' => new \Doctrine\DBAL\Tools\Console\Helper\ConnectionHelper($em->getConnection()),
