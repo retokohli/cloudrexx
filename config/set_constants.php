5821a7bfe83eed1dfd4cf2ad704b7981c0ff0b3e
Michael Ritter <michael.ritter@cloudrexx.com>
2013-05-06 07:11:53 +0000

    config - update - Added $_PATHCONFIG entries and constants to differ between a full installation and a MultiSite instance
    git-svn-id: http://svn.contrexxlabs.com/contrexx/trunk@24501 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- config/set_constants.php
+++ config/set_constants.php
@@ -13,9 +13,10 @@ static $match = null;
 /**
  * Define constants
  */
-//define('ASCMS_PATH',                      dirname(dirname(dirname(realpath(__FILE__)))));
-define('ASCMS_PATH',                        $_PATHCONFIG['ascms_root']);
-define('ASCMS_PATH_OFFSET',                 $_PATHCONFIG['ascms_root_offset']); // example '/cms'
+define('ASCMS_PATH',                        $_PATHCONFIG['ascms_installation_root']);
+define('ASCMS_PATH_OFFSET',                 $_PATHCONFIG['ascms_installation_offset']); // example '/cms'
+define('ASCMS_INSTANCE_PATH',               $_PATHCONFIG['ascms_root']);
+define('ASCMS_INSTANCE_OFFSET',             $_PATHCONFIG['ascms_root_offset']);
 define('ASCMS_BACKEND_PATH',                '/cadmin');
 define('ASCMS_PROTOCOL',                    empty($_SERVER['HTTPS']) || $_SERVER['HTTPS'] == 'off' ? 'http' : 'https');
 define('ASCMS_WEBSERVER_SOFTWARE',          !empty($_SERVER['SERVER_SOFTWARE']) && stristr($_SERVER['SERVER_SOFTWARE'], 'apache') ? 'apache' : (!empty($_SERVER['SERVER_SOFTWARE']) && stristr($_SERVER['SERVER_SOFTWARE'], 'iis') ? 'iis' : ''));
@@ -27,8 +28,8 @@ define('CONTREXX_DIRECTORY_INDEX',          'index.php');
 
 define('DBPREFIX',                          $_DBCONFIG['tablePrefix']);
 define('ASCMS_DOCUMENT_ROOT',               ASCMS_PATH.ASCMS_PATH_OFFSET);
-define('ASCMS_CUSTOMIZING_PATH',            ASCMS_DOCUMENT_ROOT.'/customizing');
-define('ASCMS_CUSTOMIZING_WEB_PATH',        ASCMS_PATH_OFFSET.'/customizing');
+define('ASCMS_CUSTOMIZING_PATH',            ASCMS_INSTANCE_PATH.ASCMS_INSTANCE_OFFSET.'/customizing');
+define('ASCMS_CUSTOMIZING_WEB_PATH',        ASCMS_INSTANCE_OFFSET.'/customizing');
 
 require_once ASCMS_DOCUMENT_ROOT.'/config/settings.php';
 
