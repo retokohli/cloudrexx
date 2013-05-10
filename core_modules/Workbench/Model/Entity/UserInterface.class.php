5679485804701ea0b2095c54dcda60890272f4e2
Michael Ritter <michael.ritter@cloudrexx.com>
2013-05-10 14:25:55 +0000

    Core_module Workbench - update - Added method UserInterface->tree() to show a recursive array as tree Core_module Workbench - update - Reorganized command list in UserInterface class Core_module Workbench - update - Added draft of command 'treenav'
    git-svn-id: http://svn.contrexxlabs.com/contrexx/trunk@24520 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- core_modules/Workbench/Model/Entity/UserInterface.class.php
+++ core_modules/Workbench/Model/Entity/UserInterface.class.php
@@ -11,20 +11,26 @@ abstract class UserInterface {
         \Env::get('ClassLoader')->loadFile(ASCMS_CORE_PATH.'/Typing/Model/Entity/AutoBoxedObject.class.php');
         \Env::get('ClassLoader')->loadFile(ASCMS_CORE_PATH.'/Typing/Model/Entity/Primitives.class.php');
         $this->commands = array(
+            /* FINISHED COMMANDS */
+            
+            /* BETA COMMANDS */
             'create' => new CreateCommand($this), // create new component
             'delete' => new DeleteCommand($this), // delete a component
             'activate' => new ActivateCommand($this), // activate a component
             'deactivate' => new DeactivateCommand($this), // deactivate a component
-            //'export' => new ExportCommand($this), // export contrexx files without workbench
             //'remove' => new RemoveCommand($this), // remove workbench from installation
-            //'update' => new UpdateCommand($this), // port a component to this version of contrexx
+            'treenav' => new TreeNavCommand($this), // recursive tree view of backend navigation
             //'addnav' => new AddNavCommand($this), // add a backend navigation entry
             //'rmnav' => new RmNavCommand($this), // remove a backend navigation entry
             //'mvnav' => new MvNavCommand($this), // move a backend navigation entry (remove and add new)
             'move' => new MoveCommand($this), // convert component types (core to core_module, etc.) and rename components
-            //'publish' => new PublishCommand($this), // publish component to contrexx app repo (after successful unit testing)
             //'test' => new TestCommand($this), // run UnitTests
             //'db' => new DbCommmand($this), // wrapper for doctrine commandline tools
+            
+            /* FUTURE COMMANDS */
+            //'export' => new ExportCommand($this), // export contrexx files without workbench
+            //'publish' => new PublishCommand($this), // publish component to contrexx app repo (after successful unit testing)
+            //'update' => new UpdateCommand($this), // port a component to this version of contrexx
         );
     }
     
@@ -84,4 +90,9 @@ abstract class UserInterface {
     public abstract function yesNo($question);
     
     public abstract function show($message);
+    
+    /**
+     * Accepts an array in the form array({something}=>{title}, 'children'=>{recursion})
+     */
+    public abstract function tree(array $tree, $displayindex = 0);
 }
