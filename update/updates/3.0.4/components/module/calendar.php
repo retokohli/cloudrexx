b48305da028098708ba3e91aa73fe6f25eae9b32
Ueli Kramer <ueli.kramer@comvation.com>
2013-05-15 06:22:07 +0000

    Update 3.0.4: add update directory for sp4
    git-svn-id: http://svn.contrexxlabs.com/contrexx/trunk@24543 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

---
+++ update/updates/3.0.4/components/module/calendar.php
@@ -0,0 +1,176 @@
+<?php
+
+function _calendarUpdate() {
+    global $objDatabase;
+
+    try {
+        \Cx\Lib\UpdateUtil::table(
+            DBPREFIX . 'module_calendar', array(
+                'id' => array('type' => 'INT(11)', 'notnull' => true, 'primary' => true, 'auto_increment' => true),
+                'active' => array('type' => 'TINYINT(1)', 'notnull' => true, 'default' => '1', 'after' => 'id'),
+                'catid' => array('type' => 'INT(11)', 'notnull' => true, 'default' => '0', 'after' => 'active'),
+                'startdate' => array('type' => 'INT(14)', 'notnull' => false, 'after' => 'catid'),
+                'enddate' => array('type' => 'INT(14)', 'notnull' => false, 'after' => 'startdate'),
+                'priority' => array('type' => 'INT(1)', 'notnull' => true, 'default' => '3', 'after' => 'enddate'),
+                'access' => array('type' => 'INT(1)', 'notnull' => true, 'default' => '0', 'after' => 'priority'),
+                'name' => array('type' => 'VARCHAR(100)', 'notnull' => true, 'default' => '', 'after' => 'access'),
+                'comment' => array('type' => 'text', 'after' => 'name'),
+                'placeName' => array('type' => 'VARCHAR(255)', 'after' => 'comment'),
+                'link' => array('type' => 'VARCHAR(255)', 'notnull' => true, 'default' => 'http://', 'after' => 'placeName'),
+                'pic' => array('type' => 'VARCHAR(255)', 'notnull' => true, 'default' => '', 'after' => 'link'),
+                'attachment' => array('type' => 'VARCHAR(255)', 'notnull' => true, 'default' => '', 'after' => 'pic'),
+                'placeStreet' => array('type' => 'VARCHAR(255)', 'notnull' => true, 'default' => '', 'after' => 'attachment'),
+                'placeZip' => array('type' => 'VARCHAR(255)', 'notnull' => true, 'default' => '', 'after' => 'placeStreet'),
+                'placeCity' => array('type' => 'VARCHAR(255)', 'notnull' => true, 'default' => '', 'after' => 'placeZip'),
+                'placeLink' => array('type' => 'VARCHAR(255)', 'notnull' => true, 'default' => '', 'after' => 'placeCity'),
+                'placeMap' => array('type' => 'VARCHAR(255)', 'notnull' => true, 'default' => '', 'after' => 'placeLink'),
+                'organizerName' => array('type' => 'VARCHAR(255)', 'notnull' => true, 'default' => '', 'after' => 'placeMap'),
+                'organizerStreet' => array('type' => 'VARCHAR(255)', 'notnull' => true, 'default' => '', 'after' => 'organizerName'),
+                'organizerZip' => array('type' => 'VARCHAR(255)', 'notnull' => true, 'default' => '', 'after' => 'organizerStreet'),
+                'organizerPlace' => array('type' => 'VARCHAR(255)', 'notnull' => true, 'default' => '', 'after' => 'organizerZip'),
+                'organizerMail' => array('type' => 'VARCHAR(255)', 'notnull' => true, 'default' => '', 'after' => 'organizerPlace'),
+                'organizerLink' => array('type' => 'VARCHAR(255)', 'notnull' => true, 'default' => '', 'after' => 'organizerMail'),
+                'key' => array('type' => 'VARCHAR(255)', 'notnull' => true, 'default' => '', 'after' => 'organizerLink'),
+                'num' => array('type' => 'INT(5)', 'notnull' => true, 'default' => '0', 'after' => 'key'),
+                'mailTitle' => array('type' => 'VARCHAR(255)', 'notnull' => true, 'default' => '', 'after' => 'num'),
+                'mailContent' => array('type' => 'text', 'after' => 'mailTitle'),
+                'registration' => array('type' => 'INT(1)', 'notnull' => true, 'default' => '0', 'after' => 'mailContent'),
+                'groups' => array('type' => 'text', 'after' => 'registration'),
+                'all_groups' => array('type' => 'INT(1)', 'notnull' => true, 'default' => '0', 'after' => 'groups'),
+                'public' => array('type' => 'INT(1)', 'notnull' => true, 'default' => '0', 'after' => 'all_groups'),
+                'notification' => array('type' => 'INT(1)', 'after' => 'public'),
+                'notification_address' => array('type' => 'VARCHAR(255)', 'notnull' => true, 'default' => '', 'after' => 'notification'),
+                'series_status' => array('type' => 'TINYINT(4)', 'after' => 'notification_address'),
+                'series_type' => array('type' => 'INT(11)', 'after' => 'series_status'),
+                'series_pattern_count' => array('type' => 'INT(11)', 'after' => 'series_type'),
+                'series_pattern_weekday' => array('type' => 'VARCHAR(7)', 'after' => 'series_pattern_count'),
+                'series_pattern_day' => array('type' => 'INT(11)', 'after' => 'series_pattern_weekday'),
+                'series_pattern_week' => array('type' => 'INT(11)', 'after' => 'series_pattern_day'),
+                'series_pattern_month' => array('type' => 'INT(11)', 'after' => 'series_pattern_week'),
+                'series_pattern_type' => array('type' => 'INT(11)', 'after' => 'series_pattern_month'),
+                'series_pattern_dourance_type' => array('type' => 'INT(11)', 'after' => 'series_pattern_type'),
+                'series_pattern_end' => array('type' => 'INT(11)', 'after' => 'series_pattern_dourance_type'),
+                'series_pattern_begin' => array('type' => 'INT(11)', 'notnull' => true, 'default' => '0', 'after' => 'series_pattern_end'),
+                'series_pattern_exceptions' => array('type' => 'longtext', 'after' => 'series_pattern_begin')
+            ),
+            array(
+                'name' => array('fields' => array('name', 'comment', 'placeName'), 'type' => 'FULLTEXT')
+            )
+        );
+    } catch (\Cx\Lib\UpdateException $e) {
+        // we COULD do something else here..
+        DBG::trace();
+        return \Cx\Lib\UpdateUtil::DefaultActionHandler($e);
+    }
+
+
+    //2.1.1
+
+
+    $query = "SELECT status FROM " . DBPREFIX . "modules WHERE id='21'";
+    $objResultCheck = $objDatabase->SelectLimit($query, 1);
+
+    if ($objResultCheck !== false) {
+        if ($objResultCheck->fields['status'] == 'y') {
+            $calendarStatus = true;
+        } else {
+            $calendarStatus = false;
+        }
+    } else {
+        return _databaseError($query, $objDatabase->ErrorMsg());
+    }
+
+    if ($calendarStatus) {
+        $arrContentSites = array();
+
+        $arrContentSites[0]['module'] = 'calendar';
+        $arrContentSites[0]['cmd'] = '';
+        $arrContentSites[1]['module'] = 'calendar';
+        $arrContentSites[1]['cmd'] = 'eventlist';
+        $arrContentSites[2]['module'] = 'calendar';
+        $arrContentSites[2]['cmd'] = 'boxes';
+
+
+        //insert new link placeholder in content, if module is active
+        foreach ($arrContentSites as $key => $siteArray) {
+            
+            $module = $siteArray['module'];
+            $cmd = $siteArray['cmd'];
+            
+            try {
+                \Cx\Lib\UpdateUtil::migrateContentPage(
+                    $module,
+                    $cmd,
+                    '<a href="index.php?section=calendar&amp;cmd=event&amp;id={CALENDAR_ID}">{CALENDAR_TITLE}</a>',
+                    '{CALENDAR_DETAIL_LINK}',
+                    '3.0.0'
+                );
+            } catch (\Cx\Lib\UpdateException $e) {
+                return \Cx\Lib\UpdateUtil::DefaultActionHandler($e);
+            }
+        }
+        
+        
+        try {
+            \Cx\Lib\UpdateUtil::migrateContentPage(
+                'calendar',
+                'sign',
+                '<input type="hidden" name="id" value="{CALENDAR_NOTE_ID}" />',
+                '<input type="hidden" name="id" value="{CALENDAR_NOTE_ID}" /><input type="hidden" name="date" value="{CALENDAR_NOTE_DATE}" />',
+                '3.0.0'
+            );
+            \Cx\Lib\UpdateUtil::migrateContentPage(
+                'calendar',
+                'sign',
+                '<a href="index.php?section=calendar&amp;id={CALENDAR_NOTE_ID}">{TXT_CALENDAR_BACK}</a>',
+                '{CALENDAR_LINK_BACK}',
+                '3.0.0'
+            );
+        } catch (\Cx\Lib\UpdateException $e) {
+            return \Cx\Lib\UpdateUtil::DefaultActionHandler($e);
+        }
+    }
+
+    try {
+        // delete obsolete table  contrexx_module_calendar_access
+        \Cx\Lib\UpdateUtil::drop_table(DBPREFIX . 'module_calendar_access');
+
+        \Cx\Lib\UpdateUtil::table(
+                DBPREFIX . 'module_calendar_form_data', array(
+            'reg_id' => array('type' => 'INT(10)', 'notnull' => true, 'default' => '0'),
+            'field_id' => array('type' => 'INT(10)', 'notnull' => true, 'default' => '0'),
+            'data' => array('type' => 'TEXT', 'notnull' => true)
+                )
+        );
+
+        \Cx\Lib\UpdateUtil::table(
+                DBPREFIX . 'module_calendar_form_fields', array(
+            'id' => array('type' => 'INT(7)', 'notnull' => true, 'auto_increment' => true, 'primary' => true),
+            'note_id' => array('type' => 'INT(10)', 'notnull' => true, 'default' => '0'),
+            'name' => array('type' => 'TEXT'),
+            'type' => array('type' => 'INT(1)', 'notnull' => true, 'default' => '0'),
+            'required' => array('type' => 'INT(1)', 'notnull' => true, 'default' => '0'),
+            'order' => array('type' => 'INT(3)', 'notnull' => true, 'default' => '0'),
+            'key' => array('type' => 'INT(7)', 'notnull' => true, 'default' => '0')
+                )
+        );
+
+        \Cx\Lib\UpdateUtil::table(
+                DBPREFIX . 'module_calendar_registrations', array(
+            'id' => array('type' => 'INT(7)', 'notnull' => true, 'auto_increment' => true, 'primary' => true),
+            'note_id' => array('type' => 'INT(7)', 'notnull' => true, 'default' => '0'),
+            'note_date' => array('type' => 'INT(11)', 'notnull' => true, 'after' => 'note_id'),
+            'time' => array('type' => 'INT(14)', 'notnull' => true, 'default' => '0'),
+            'host' => array('type' => 'VARCHAR(255)'),
+            'ip_address' => array('type' => 'VARCHAR(15)'),
+            'type' => array('type' => 'INT(1)', 'notnull' => true, 'default' => '0')
+                )
+        );
+    } catch (\Cx\Lib\UpdateException $e) {
+        // we COULD do something else here..
+        DBG::trace();
+        return \Cx\Lib\UpdateUtil::DefaultActionHandler($e);
+    }
+
+    return true;
+}
---
+++ update/updates/3.0.4/config.inc.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Update config
+ *
+ * This is the configuration of the version to upgrade to.
+ */
+$arrUpdate = array(
+    'cmsName'          => 'Contrexx',
+    'cmsVersion'       => '3.0.4',
+    'cmsStatus'        => 'Stable',
+    'cmsEdition'       => 'Trial',
+    'cmsCodeName'      => 'Nikola Tesla',
+    'cmsReleaseDate'   => '12.04.2013',
+    'cmsFromVersion'   => '1.2.0',
+    'cmsRequiredPHP'   => '5.3',
+    'cmsRequiredMySQL' => '5.0',
+    'cmsRequiredGD'    => '1.6',
+    'lang'             => array('de'),
+    'script'           => 'update.php'
+);
+?>
