1053f247238325ed81cc754d47d4dab011754a4c
svnbot <svnbot@comvation.com>
2013-05-23 15:00:53 +0000

    (AUTOMATIC) update trunk's installer dumps from contrexxlabs_contrexx_trunk
    git-svn-id: http://svn.contrexxlabs.com/contrexx/trunk@24631 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- installer/data/contrexx_dump_structure.sql
+++ installer/data/contrexx_dump_structure.sql
@@ -2,14 +2,16 @@ SET @saved_cs_client     = @@character_set_client;
 SET character_set_client = utf8;
 CREATE TABLE `contrexx_access_group_dynamic_ids` (
   `access_id` int(11) unsigned NOT NULL default '0',
-  `group_id` int(11) unsigned NOT NULL default '0'
+  `group_id` int(11) unsigned NOT NULL default '0',
+  PRIMARY KEY  (`access_id`,`group_id`)
 ) ENGINE=MyISAM;
 SET character_set_client = @saved_cs_client;
 SET @saved_cs_client     = @@character_set_client;
 SET character_set_client = utf8;
 CREATE TABLE `contrexx_access_group_static_ids` (
   `access_id` int(11) unsigned NOT NULL default '0',
-  `group_id` int(11) unsigned NOT NULL default '0'
+  `group_id` int(11) unsigned NOT NULL default '0',
+  PRIMARY KEY  (`access_id`,`group_id`)
 ) ENGINE=MyISAM;
 SET character_set_client = @saved_cs_client;
 SET @saved_cs_client     = @@character_set_client;
