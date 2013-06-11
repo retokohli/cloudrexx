c609d6c357ef0c1ddb1bab398d7e6277d59d9dbc
Michael Ritter <michael.ritter@cloudrexx.com>
2013-06-11 15:46:14 +0000

    core Component - update - Moved decoration of SystemComponent from Controller to SystemComponentController core Component - update - Hook methods of SystemComponentControllers are now optional core Component - update - SystemComponentController->load() now tries to load Front- and BackendController core Component - update - SystemComponentController now knows all other Controllers for the same component core Component - update - Added SystemComponentFrontend and BackendController core Component - update - Added template for backend subnavigation
    git-svn-id: http://svn.contrexxlabs.com/contrexx/trunk@24907 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- core/Component/Model/Entity/Controller.class.php
+++ core/Component/Model/Entity/Controller.class.php
@@ -1,46 +1,38 @@
 <?php
 /**
  * This is the superclass for all Controller classes
- * Decorator for SystemComponent
+ * 
  * @author Michael Ritter <michael.ritter@comvation.com>
  */
 namespace Cx\Core\Component\Model\Entity;
 
 /**
  * This is the superclass for all Controller classes
- * Decorator for SystemComponent
+ * 
  * @author Michael Ritter <michael.ritter@comvation.com>
  */
 abstract class Controller {
-    /**
-     * @var Cx\Core\Component\Model\Entity\SystemComponent
-     */
-    protected $systemComponent;
     
     /**
-     * Initializes a controller
-     * @param \Cx\Core\Component\Model\Entity\SystemComponent $systemComponent SystemComponent to decorate
+     * SystemComponentController for this Component
+     * @var \Cx\Core\Component\Model\Entity\SystemComponentController
      */
-    public function __construct(\Cx\Core\Component\Model\Entity\SystemComponent $systemComponent) {
-        $this->systemComponent = $systemComponent;
+    private $systemComponentController = null;
+    
+    /**
+     * Creates new controller
+     * @param SystemComponentController $systemComponentController Main controller for this system component
+     */
+    public function __construct(SystemComponentController $systemComponentController) {
+        $this->systemComponentController = $systemComponentController;
+        $this->systemComponentController->registerController();
     }
     
     /**
-     * Returns the SystemComponent this Controller decorates
-     * @return \Cx\Core\Component\Model\Entity\SystemComponent
+     * Returns the main controller
+     * @return SystemComponentController Main controller for this system component
      */
-    public function getSystemComponent() {
-        return $this->systemComponent;
-    }
-    
-    /**
-     * Decoration: all methods that are not specified in this or child classes
-     * call the corresponding method of the decorated SystemComponent
-     * @param string $methodName Name of method to call
-     * @param array $arguments List of arguments for the method to call
-     * @return mixed Return value of the method to call
-     */
-    public function __call($methodName, $arguments) {
-        return call_user_func(array($this->systemComponent, $methodName), $arguments);
+    public function getSystemComponentController() {
+        return $this->systemComponentController;
     }
 }
---
+++ core/Component/Model/Entity/SystemComponentBackendController.class.php
@@ -0,0 +1,73 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+namespace Cx\Core\Component\Model\Entity;
+/**
+ * Description of SystemComponentBackendController
+ *
+ * @author ritt0r
+ */
+abstract class SystemComponentBackendController extends Controller {
+    
+    /**
+     * @return array List of acts
+     */
+    public abstract function getCommands();
+    
+    public function getPage(\Cx\Core\Cx $cx, \Cx\Core\ContentManager\Model\Entity\Page $page) {
+        global $_ARRAYLANG;
+        
+        $cmd = array('');
+        
+        if (isset($_GET['act'])) {
+            $cmd = explode('/', contrexx_input2raw($_GET['act']));
+        }
+        
+        $actTemplate = new \Cx\Core\Html\Sigma(ASCMS_CORE_MODULE_PATH . '/MultiSite/View/Template');
+        $actTemplate->loadTemplateFile();
+        $successMessage = '';
+        $failureMessage = '';
+        
+        // todo: $actTemplate->loadTemplateFile(), Messages
+        $this->parsePage($actTemplate, $cx, $cmd, $successMessage, $failureMessage);
+        
+        // set tabs
+        $navigation = new \Cx\Core\Html\Sigma(ASCMS_CORE_PATH . '/Component/View/Template');
+        $navigation->loadTemplateFile('Navigation.html');
+        foreach ($this->getCommands() as $cmd) {
+            $act = '&amp;act=' . $cmd;
+            $txt = $cmd;
+            if (empty($cmd)) {
+                $act = '';
+                $txt = 'DEFAULT';
+            }
+            $navigation->setVariable(array(
+                'HREF' => 'index.php?cmd=' . $this->getSystemComponentController()->getName() . $act,
+                'TITLE' => $_ARRAYLANG['TXT_' . strtoupper($this->getSystemComponentController()->getName() . '_ACT_' . $txt)],
+            ));
+            if (strtolower($title) == $act) {
+                $navigation->touchBlock('active');
+            }
+            $navigation->parse('tab_entry');
+        }
+        
+        $page->setContent($actTemplate->get());
+        $cx->getTemplate()->setVariable(array(
+            'CONTENT_NAVIGATION' => $navigation->get(),
+            'CONTENT_STATUS_MESSAGE' => $successMessage,
+        ));
+    }
+    
+    /**
+     * Use this to parse your backend page
+     * 
+     * You will get the template located in /View/Template/{CMD}.html
+     * @param \Cx\Core\Html\Sigma $template Template for current CMD
+     * @param \Cx\Core\Cx $cx Contrexx main class
+     * @param array $cmd CMD separated by slashes
+     */
+    public abstract function parsePage(\Cx\Core\Html\Sigma $template, \Cx\Core\Cx $cx, array $cmd, &$successMessage, &$failureMessage);
+}
