b8061b37b0bf9394bba03ee17f9f149cba03c28c
Michael Ritter <michael.ritter@cloudrexx.com>
2013-06-12 15:41:37 +0000

    core ContentManager - update - Removed unused methods in ComponentController
    git-svn-id: http://svn.contrexxlabs.com/contrexx/trunk@24929 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- core/ContentManager/Controller/ComponentController.class.php
+++ core/ContentManager/Controller/ComponentController.class.php
@@ -12,29 +12,11 @@ namespace Cx\Core\ContentManager\Controller;
 /**
  * Main controller for ContentManager
  * 
- * At the moment, this is just an empty ComponentController in order to load
- * YAML files via component framework
+ * At the moment, this is ComponentController is just used to load
+ * YAML files and JsonAdapters via component framework
  * @author Michael Ritter <michael.ritter@comvation.com>
  */
 class ComponentController extends \Cx\Core\Component\Model\Entity\SystemComponentController {
-    
-    public function load(\Cx\Core\Cx $cx, \Cx\Core\ContentManager\Model\Entity\Page $page) {}
-    
-    public function postContentLoad(\Cx\Core\Cx $cx, \Cx\Core\ContentManager\Model\Entity\Page $page) {}
-    
-    public function postContentParse(\Cx\Core\Cx $cx, \Cx\Core\ContentManager\Model\Entity\Page $page) {}
-    
-    public function postFinalize(\Cx\Core\Cx $cx) {}
-    
-    public function postResolve(\Cx\Core\Cx $cx, \Cx\Core\ContentManager\Model\Entity\Page $page) {}
-    
-    public function preContentLoad(\Cx\Core\Cx $cx, \Cx\Core\ContentManager\Model\Entity\Page $page) {}
-    
-    public function preContentParse(\Cx\Core\Cx $cx, \Cx\Core\ContentManager\Model\Entity\Page $page) {}
-    
-    public function preFinalize(\Cx\Core\Cx $cx, \Cx\Core\Html\Sigma $template) {}
-    
-    public function preResolve(\Cx\Core\Cx $cx, \Cx\Core\Routing\Url $request) {}
 
     public function getControllersAccessableByJson() {
         return array(
