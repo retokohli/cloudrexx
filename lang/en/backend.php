319ffc7e934327c76af325e207e587e46bf28117
Michael Ritter <michael.ritter@cloudrexx.com>
2013-06-12 08:53:24 +0000

    core lang - bugfix - Added missing global statement
    git-svn-id: http://svn.contrexxlabs.com/contrexx/trunk@24916 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- lang/en/backend.php
+++ lang/en/backend.php
@@ -6,6 +6,7 @@
  * @package     contrexx
  * @subpackage  lang
  */
+global $_ARRAYLANG;
 $_ARRAYLANG['TXT_INTERNAL'] = "internal";
 $_ARRAYLANG['TXT_CORE_SEARCH_USER_INFO'] = "Enter at least %s character(s).";
 $_ARRAYLANG['TXT_CORE_SEARCH_USER'] = "Search user";
