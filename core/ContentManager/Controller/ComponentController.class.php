e75285c419289fed01621d95b960e1a00687c3e9
Michael Ritter <michael.ritter@cloudrexx.com>
2013-06-18 13:04:59 +0000

    core ContentManager - update - PageEventListener now uses Contrexx event handler
    git-svn-id: http://svn.contrexxlabs.com/contrexx/trunk@24993 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- core/ContentManager/Controller/ComponentController.class.php
+++ core/ContentManager/Controller/ComponentController.class.php
@@ -17,6 +17,19 @@ namespace Cx\Core\ContentManager\Controller;
  * @author Michael Ritter <michael.ritter@comvation.com>
  */
 class ComponentController extends \Cx\Core\Core\Model\Entity\SystemComponentController {
+    
+    public function __construct(\Cx\Core\Core\Model\Entity\SystemComponent $systemComponent, \Cx\Core\Core\Controller\Cx $cx) {
+        parent::__construct($systemComponent, $cx);
+        $evm = $cx->getEvents();
+        $pageListener = new \Cx\Core\ContentManager\Model\Event\PageEventListener();
+        $evm->addModelListener(\Doctrine\ORM\Events::prePersist, 'Cx\\Core\\ContentManager\\Model\\Entity\\Page', $pageListener);
+        $evm->addModelListener(\Doctrine\ORM\Events::postPersist, 'Cx\\Core\\ContentManager\\Model\\Entity\\Page', $pageListener);
+        $evm->addModelListener(\Doctrine\ORM\Events::preUpdate, 'Cx\\Core\\ContentManager\\Model\\Entity\\Page', $pageListener);
+        $evm->addModelListener(\Doctrine\ORM\Events::postUpdate, 'Cx\\Core\\ContentManager\\Model\\Entity\\Page', $pageListener);
+        $evm->addModelListener(\Doctrine\ORM\Events::preRemove, 'Cx\\Core\\ContentManager\\Model\\Entity\\Page', $pageListener);
+        $evm->addModelListener(\Doctrine\ORM\Events::postRemove, 'Cx\\Core\\ContentManager\\Model\\Entity\\Page', $pageListener);
+        $evm->addModelListener(\Doctrine\ORM\Events::onFlush, 'Cx\\Core\\ContentManager\\Model\\Entity\\Page', $pageListener);
+    }
 
     public function getControllersAccessableByJson() {
         return array(
