ae94ecf47dd0f52621e15ff37bf3d9103f040d1b
Michael Ritter <michael.ritter@cloudrexx.com>
2013-06-25 08:29:33 +0000

    core_modules Workbench - update - Added docblocks for Commands
    git-svn-id: http://svn.contrexxlabs.com/contrexx/trunk@25093 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- core_modules/Workbench/Model/Entity/CreateCommand.class.php
+++ core_modules/Workbench/Model/Entity/CreateCommand.class.php
@@ -1,16 +1,44 @@
 <?php
+/**
+ * Command to create components
+ * @author Michael Ritter <michael.ritter@comvation.com>
+ */
 
 namespace Cx\Core_Modules\Workbench\Model\Entity;
 
+/**
+ * Command to create components
+ * @author Michael Ritter <michael.ritter@comvation.com>
+ */
 class CreateCommand extends Command {
+    
+    /**
+     * Command name
+     * @var string
+     */
     protected $name = 'create';
+    
+    /**
+     * Command description
+     * @var string
+     */
     protected $description = 'Creates a new component (core, core_module, lib, module)';
+    
+    /**
+     * Command synopsis
+     * @var string
+     */
     protected $synopsis = 'workbench(.bat) create [core|core_module|lib|module] {component_name}';
+    
+    /**
+     * Command help text
+     * @var string
+     */
     protected $help = 'Creates and activates a new component of the specified type named {component_name}.';
     
     /**
-     * @todo Activate newly created component
-     * @param array $arguments 
+     * Execute this command
+     * @param array $arguments Array of commandline arguments
      */
     public function execute(array $arguments) {
         
--- core_modules/Workbench/Model/Entity/DeleteCommand.class.php
+++ core_modules/Workbench/Model/Entity/DeleteCommand.class.php
@@ -1,16 +1,44 @@
 <?php
+/**
+ * Command to delete components
+ * @author Michael Ritter <michael.ritter@comvation.com>
+ */
 
 namespace Cx\Core_Modules\Workbench\Model\Entity;
 
+/**
+ * Command to delete components
+ * @author Michael Ritter <michael.ritter@comvation.com>
+ */
 class DeleteCommand extends Command {
+    
+    /**
+     * Command name
+     * @var string
+     */
     protected $name = 'delete';
+    
+    /**
+     * Command description
+     * @var string
+     */
     protected $description = 'Deletes a component (core, core_module, lib, module)';
+    
+    /**
+     * Command synopsis
+     * @var string
+     */
     protected $synopsis = 'workbench(.bat) delete [core|core_module|lib|module] {component_name}';
+    
+    /**
+     * Command help text
+     * @var string
+     */
     protected $help = 'Deactivates and deletes the component with the specified type named {component_name}.';
     
     /**
-     * @param array $arguments
-     * @return type 
+     * Execute this command
+     * @param array $arguments Array of commandline arguments
      */
     public function execute(array $arguments) {
         if (!$this->interface->yesNo('Do you really want to irrevocably delete ' . $arguments[2] . ' "' . $arguments[3] . '"?')) {
