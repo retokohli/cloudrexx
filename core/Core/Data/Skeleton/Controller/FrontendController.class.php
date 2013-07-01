076d96be7bfe04e6dee3a8f6b9717b1e6b091e22
Michael Ritter <michael.ritter@cloudrexx.com>
2013-07-01 06:13:51 +0000

    core Core - update - Cleaned up Data directory locations core Core - update - Added first version of Skeleton Component
    git-svn-id: http://svn.contrexxlabs.com/contrexx/trunk@25213 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- core/Core/Data/Skeleton/Controller/FrontendController.class.php
+++ core/Core/Data/Skeleton/Controller/FrontendController.class.php
@@ -1,21 +1,32 @@
 <?php
-
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * Specific FrontendController for this Component. Use this to easily create a frontent view
+ * @author Michael Ritter <michael.ritter@comvation.com>
  */
 
 namespace Cx\Modules\Skeleton\Controller;
 
+/**
+ * Specific FrontendController for this Component. Use this to easily create a frontent view
+ * @author Michael Ritter <michael.ritter@comvation.com>
+ */
 class FrontendController extends \Cx\Core\Core\Model\Entity\SystemComponentFrontendController {
     
+    /**
+     * Use this to parse your frontend page
+     * 
+     * You will get a template based on the content of the resolved page
+     * You can access Cx class using $this->cx
+     * To show messages, use \Message class
+     * @param \Cx\Core\Html\Sigma $template Template containing content of resolved page
+     */
     public function parsePage(\Cx\Core\Html\Sigma $template) {
-        $bla = new \Cx\Modules\Skeleton\Controller\FrontendController();
+        // this class inherits from Controller, therefore you can get access to
+        // Cx like this:
+        $this->cx;
         
-        $this->cx->getEvents()->addModelListener(
-            \Doctrine\ORM\Events::postPersist,
-            '\\Cx\\Modules\\Skeleton\\Model\\Entity\\Test',
-            $this
-        );
+        // Controller routes all calls to undeclared methods to your
+        // ComponentController. So you can do things like
+        $this->getName();
     }
 }
---
+++ core/Core/Data/Skeleton/lang/en/frontend.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * This is the english language file for frontend mode.
+ * This file is included by Contrexx and all entries are set as placeholder
+ * values for frontent page template by SystemComponentFrontendController
+ */
+
+global $_ARRAYLANG;
+
+// Let's start with module info:
+$_ARRAYLANG['TXT_MODULE_SKELETON'] = 'New module';
+$_ARRAYLANG['TXT_MODULE_SKELETON_DESCRIPTION'] = 'This is a new module with some sample content to show how to start.';
+
+// Now our content specific values:
+$_ARRAYLANG['TXT_MODULE_SKELETON_CONGRATULATIONS'] = 'Congratulations';
+$_ARRAYLANG['TXT_MODULE_SKELETON_SUCCESSFUL_CREATION'] = 'You successfully created a new module!';
+$_ARRAYLANG['TXT_MODULE_SKELETON_EXAMPLE_TEMPLATE'] = 'This is the default template for this component. It is used for all ACTs that do not have their own template.';
