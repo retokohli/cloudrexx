22662277787feb81257806c0145c5d92037f0915
Ueli Kramer <ueli.kramer@comvation.com>
2013-07-18 14:39:29 +0000

    REMOVED: popup module
    git-svn-id: http://svn.contrexxlabs.com/contrexx/trunk@25688 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- core/error.class.php
+++ core/error.class.php
@@ -65,7 +65,6 @@ class error
      */
     function errorHandling()
     {
-        global $_CORELANG;
         $this->_objTpl->setTemplate($this->pageContent);
 
         if (!isset($_REQUEST['id'])) {
@@ -73,16 +72,18 @@ class error
         }
 
         switch ($_REQUEST['id']){
-        case '404':
-            $errorNo = 404;
-            $errorMsg = "Not found";
-            break;
+            case '404':
+                $errorNo = 404;
+                $errorMsg = "Not found";
+                break;
 
-        default:
-            $errorNo = 404;
-            $errorMsg = "Not found";
-            break;
+            default:
+                $errorNo = 404;
+                $errorMsg = "Not found";
+                break;
         }
+
+        \header($_SERVER['SERVER_PROTOCOL'] . ' ' . $errorNo . ' ' . $errorMsg);
 
         /*
         100 Continue
@@ -131,4 +132,3 @@ class error
         return $this->_objTpl->get();
     }
 }
-?>
