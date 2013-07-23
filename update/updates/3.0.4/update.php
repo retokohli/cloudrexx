0473a7c3ae3ddea0893613ba62c8391cc39aa044
ss4u <comvation@softsolutions4u.info>
2013-07-23 11:58:15 +0000

    Update:
       Added the md5 sum of cx files after striping all whitespaces.
      Modified the verifySumOfFile() to check the rawmd5sum if the old one fails.
    
    git-svn-id: http://svn.contrexxlabs.com/contrexx/trunk@25837 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- update/updates/3.0.4/update.php
+++ update/updates/3.0.4/update.php
@@ -1453,11 +1453,12 @@ function loadMd5SumOfOriginalCxFiles()
     $arrMd5SumsOfCxFiles = array();
     $list = file($md5File);
     foreach ($list as $entry) {
-        list($file, $md5sum) = explode('|', trim($entry));
+        list($file, $md5sum, $rawMd5Sum) = explode('|', trim($entry));
         if (!isset($arrMd5SumsOfCxFiles[$file])) {
             $arrMd5SumsOfCxFiles[$file] = array();
         }
-        $arrMd5SumsOfCxFiles[$file][] = $md5sum;
+        $arrMd5SumsOfCxFiles[$file]['md5_sum']     = $md5sum;
+        $arrMd5SumsOfCxFiles[$file]['raw_md5_sum'] = $rawMd5Sum;
     }
 
     return true;
@@ -1517,7 +1518,8 @@ function verifyMd5SumOfFile($file, $newFile)
         return true;
     }
 
-    $md5 = md5_file($file);
+    $md5        = md5_file($file);
+    $rawmd5     = md5(preg_replace('/(\s|\\\[rnt]{1})/', '', file_get_contents($file)));
     $cxFilePath = substr($file, strlen(ASCMS_DOCUMENT_ROOT.'/'));
 
     // file did not exist in old version,
@@ -1527,13 +1529,14 @@ function verifyMd5SumOfFile($file, $newFile)
     }
 
     foreach ($arrMd5SumsOfCxFiles[$cxFilePath] as $validMd5Sum) {
-        if ($md5 == $validMd5Sum) {
+        if ($md5 == $validMd5Sum['md5_sum'] || $rawmd5 == $validMd5Sum['raw_md5_sum']) {
             return true;
         }
     }
 
-    $md5SumOfNewFile = md5_file($newFile);
-    if ($md5 == $md5SumOfNewFile) {
+    $md5SumOfNewFile    = md5_file($newFile);
+    $rawMd5SumOfNewFile = md5(preg_replace('/(\s|\\\[rnt]{1})/', '', file_get_contents($newFile)));
+    if ($md5 == $md5SumOfNewFile || $rawmd5 == $rawMd5SumOfNewFile) {
         return true;
     }
 
