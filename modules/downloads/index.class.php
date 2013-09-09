9d525a6397e9cda4b6e205f1f559f848340e0c2a
Kevin Riesen <kevin.riesen@comvation.com>
2013-09-09 15:02:26 +0000

    dam (fixed #1539): Removed unused parameter in getPaging().
    git-svn-id: http://svn.contrexxlabs.com/contrexx/trunk@26656 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- modules/downloads/index.class.php
+++ modules/downloads/index.class.php
@@ -1035,7 +1035,11 @@ JS_CODE;
 
             $downloadCount = $objDownload->getFilteredSearchDownloadCount();
             if ($downloadCount > $_CONFIG['corePagingLimit']) {
-                $this->objTemplate->setVariable('DOWNLOADS_FILE_PAGING', getPaging($downloadCount, $limitOffset, '&'.substr($this->moduleParamsHtml, 1).'&category='.$objCategory->getId().'&downloads_search_keyword='.htmlspecialchars($this->searchKeyword), "<b>".$_ARRAYLANG['TXT_DOWNLOADS_DOWNLOADS']."</b>"));
+                if(\Env::get('cx')->getPage()->getModule() == ''){
+                    $this->objTemplate->setVariable('DOWNLOADS_FILE_PAGING', getPaging($downloadCount, $limitOffset, '', "<b>".$_ARRAYLANG['TXT_DOWNLOADS_DOWNLOADS']."</b>"));
+                }else{
+                    $this->objTemplate->setVariable('DOWNLOADS_FILE_PAGING', getPaging($downloadCount, $limitOffset, '&'.substr($this->moduleParamsHtml, 1).'&category='.$objCategory->getId().'&downloads_search_keyword='.htmlspecialchars($this->searchKeyword), "<b>".$_ARRAYLANG['TXT_DOWNLOADS_DOWNLOADS']."</b>"));
+                }
             }
 
             $this->objTemplate->setVariable(array(
