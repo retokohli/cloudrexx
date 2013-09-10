fbd735c9eb93f62ab49968bbdeec3de989ba3c6f
Michael Raess <michael.raess@comvation.com>
2013-09-10 08:00:15 +0000

    Core Module News - enhancement - gui optimizations (fixes #1651)
    git-svn-id: http://svn.contrexxlabs.com/contrexx/trunk@26663 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- core_modules/news/lang/de/backend.php
+++ core_modules/news/lang/de/backend.php
@@ -321,4 +321,5 @@ $_ARRAYLANG['TXT_NEWS_COMMENT_BLOCK_CONTACT_FORM_CAPTCHA_CODE'] = "HTML-Code fü
 $_ARRAYLANG['TXT_NEWS_COMMENT_BLOCK_ADD_COMMENT_TITLE_BUTTON'] = "Titel für den Button Kommentar hinzufügen.";
 $_ARRAYLANG['TXT_NEWS_NO_TITLE_ENTERED'] = "Fehler beim Speichern der Newsmeldung. Es muss ein Titel angegeben werden.";
 $_ARRAYLANG['TXT_NEWS_PREVIEW'] = "Vorschau öffnen";
+$_ARRAYLANG['TXT_NEWS_NUMBER_OF_COMMENTS'] = "Anzahl Kommentare";
 ?>
