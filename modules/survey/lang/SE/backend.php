138e9a2e87a33736f026ff639be3d1daf59089ea
Thomas Däppen <thomas.daeppen@cloudrexx.com>
2013-10-07 08:58:26 +0000

    survey: added new version from branch /branches/work/module_survey
    git-svn-id: http://svn.contrexxlabs.com/contrexx/trunk@27181 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

---
+++ modules/survey/lang/SE/backend.php
@@ -0,0 +1,51 @@
+<?php
+/**
+* Contrexx CMS
+* generated date Tue, 23 Jan 2007 15:25:36 +0100
+**/
+
+$_ARRAYLANG['TXT_SURVEY_INSERT_SUCCESS_VOTING'] = "The voting has been successfully added.";
+$_ARRAYLANG['TXT_SURVEY_INSERT_SUCCESS_SURVEY'] = "The survey has been successfully added.";
+$_ARRAYLANG['TXT_SURVEY_PLACEHOLDER'] = "Placeholder";
+$_ARRAYLANG['TXT_SURVEY_PARTICIPANT'] = "Participant";
+$_ARRAYLANG['TXT_SURVEY_PARTICIPANT_LAST'] = "Last participant";
+$_ARRAYLANG['TXT_SURVEY_ACTIONS'] = "Actions";
+$_ARRAYLANG['TXT_SURVEY_DELETE_JS'] = "Do you really want to delete this survey? Attention: All collected information are gone lost.";
+$_ARRAYLANG['TXT_SURVEY_STATUS_CHANGE'] = "Change status";
+$_ARRAYLANG['TXT_SURVEY_EDIT'] = "Edit voting / survey";
+$_ARRAYLANG['TXT_SURVEY_TYPE_SURVEY'] = "Survey";
+$_ARRAYLANG['TXT_SURVEY_TYPE_VOTING'] = "Voting";
+$_ARRAYLANG['TXT_SURVEY_PLACEHOLDER_ACTIVE'] = "Activated for use in Placeholder";
+$_ARRAYLANG['TXT_SURVEY_NO_SURVEYS'] = "There are no votings / surveys existing.";
+$_ARRAYLANG['TXT_SURVEY_DELETE_ALL_JS'] = "Do you really want to delete all selected entries. Attention: This process can not be undone!";
+$_ARRAYLANG['TXT_MULTIACTION_SELECT'] = "Select action";
+$_ARRAYLANG['TXT_MULTIACTION_DELETE'] = "Delete selected entries";
+$_ARRAYLANG['TXT_MULTIACTION_ACTIVATE'] = "Activate selected entries";
+$_ARRAYLANG['TXT_MULTIACTION_DEACTIVATE'] = "Deactivate selected entries";
+$_ARRAYLANG['TXT_SURVEY_STATUS_CHANGE_SUCCESS'] = "Status has been changed successfully.";
+$_ARRAYLANG['TXT_SURVEY_DELETE_ERROR'] = "An error occured while deleting the voting / survey. Please try again.";
+$_ARRAYLANG['TXT_SURVEY_STATUS_CHANGE_ERROR'] = "An error occured while changing status.";
+$_ARRAYLANG['TXT_SURVEY_DELETE_SUCCESS'] = "The voting / survey [NAME] has been successfully deleted.";
+$_ARRAYLANG['TXT_SURVEY_REDIRECT_HELP'] = "If you wish a redirection after completation of a survey, you can define the redirecting page in this field.";
+$_ARRAYLANG['TXT_SURVEY_COMMENTS_HELP'] = "If you activate this option, there will be added an comment field at the end of the survey.";
+$_ARRAYLANG['TXT_SURVEY_HOMEBOX_HELP'] = "If you activate this option, this voting will be parsed into the system variable [[SURVEY_HOME]].";
+$_ARRAYLANG['TXT_SURVEY_QUESTION'] = "Question";
+$_ARRAYLANG['TXT_SURVEY_QUESTION_REMOVE'] = "Do you really want to remove the last question?";
+$_ARRAYLANG['TXT_SURVEY_ANSWER_REMOVE'] = "Do you really want to remove this answer?";
+$_ARRAYLANG['TXT_SURVEY_EXTENDED'] = "Extended";
+$_ARRAYLANG['TXT_SURVEY_ANSWERS'] = "Answers";
+$_ARRAYLANG['TXT_SURVEY_ANSWER_ADD'] = "Add another answer";
+$_ARRAYLANG['TXT_SURVEY_MOVE_UP'] = "Move up";
+$_ARRAYLANG['TXT_SURVEY_MOVE_DOWN'] = "Move down";
+$_ARRAYLANG['TXT_SURVEY_ANSWER_DELETE'] = "Remove answer";
+$_ARRAYLANG['TXT_SURVEY_ACTIVATED_IN'] = "Activated at";
+$_ARRAYLANG['TXT_SURVEY_TYPESELECTION'] = "Type selection";
+$_ARRAYLANG['TXT_SURVEY_COMMENTABLE'] = "Comments";
+$_ARRAYLANG['TXT_SURVEY_QUESTION_ADD'] = "Add question";
+$_ARRAYLANG['TXT_SURVEY_QUESTION_DELETE'] = "Remove question";
+$_ARRAYLANG['TXT_SETTINGS_LOGFILE'] = "Write logfile";
+$_ARRAYLANG['TXT_SETTINGS_LOGFILE_HELP'] = "If you activate this option, detailled information for every participant are collected.";
+$_ARRAYLANG['TXT_SETTINGS_ANONYMOUS'] = "Allow anonymous participant";
+$_ARRAYLANG['TXT_SETTINGS_ANONYMOUS_HELP'] = "Activating this option allows anonymous participation in surveys.";
+$_ARRAYLANG['TXT_SETTINGS_UPDATE_SUCCESSFULL'] = "Settings have been saved successfully.";
+?>
---
+++ modules/survey/lang/tr/backend.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * @copyright   CONTREXX CMS - COMVATION AG
+ * @author      Comvation Development Team <info@comvation.com>
+ * @access      public
+ * @package     contrexx
+ * @subpackage  module_survey
+ */
+$_ARRAYLANG['TXT_SURVEY_INSERT_SUCCESS_VOTING'] = "Oylama / Anket başarıyla eklendi.";
+$_ARRAYLANG['TXT_SURVEY_INSERT_SUCCESS_SURVEY'] = "Anket başarıyla eklendi.";
+$_ARRAYLANG['TXT_SURVEY_PLACEHOLDER'] = "Yertucu";
+$_ARRAYLANG['TXT_SURVEY_PARTICIPANT'] = "Katılanlar";
+$_ARRAYLANG['TXT_SURVEY_PARTICIPANT_LAST'] = "Son Katılan";
+$_ARRAYLANG['TXT_SURVEY_ACTIONS'] = "İşlem";
+$_ARRAYLANG['TXT_SURVEY_DELETE_JS'] = "Son anketi gerçekten silmek mi istiyorsunuz? Dikkat: Bütün kaydedilenler kayboluyor.";
+$_ARRAYLANG['TXT_SURVEY_STATUS_CHANGE'] = "Durum (statü) değiştir";
+$_ARRAYLANG['TXT_SURVEY_EDIT'] = "Oylama / Anket düzenle";
+$_ARRAYLANG['TXT_SURVEY_TYPE_SURVEY'] = "Anket";
+$_ARRAYLANG['TXT_SURVEY_TYPE_VOTING'] = "Oylama";
+$_ARRAYLANG['TXT_SURVEY_PLACEHOLDER_ACTIVE'] = "Yer tutucuda etkin";
+$_ARRAYLANG['TXT_SURVEY_NO_SURVEYS'] = "Oylama / Anket yok.";
+$_ARRAYLANG['TXT_SURVEY_DELETE_ALL_JS'] = "İşaretlenenleri gerçekten  silmek mi istiyorsunuz? Bu işlem geri döndürülemez!";
+$_ARRAYLANG['TXT_MULTIACTION_SELECT'] = "İşlem seç";
+$_ARRAYLANG['TXT_MULTIACTION_DELETE'] = "İşaretlenenleri sil";
+$_ARRAYLANG['TXT_MULTIACTION_ACTIVATE'] = "İşaretlenenleri etkinle";
+$_ARRAYLANG['TXT_MULTIACTION_DEACTIVATE'] = "İşaretlenenleri etkinsizle";
+$_ARRAYLANG['TXT_SURVEY_STATUS_CHANGE_SUCCESS'] = "Durum (statü) başarıyla değiştirildi.";
+$_ARRAYLANG['TXT_SURVEY_DELETE_ERROR'] = "Oylama / Anketin silinmesinde hata oluştu .";
+$_ARRAYLANG['TXT_SURVEY_STATUS_CHANGE_ERROR'] = "Durum değişikliğinde hato oluştu.";
+$_ARRAYLANG['TXT_SURVEY_DELETE_SUCCESS'] = "[NAME] Oylama / Anket başarıyla silindi.";
+$_ARRAYLANG['TXT_SURVEY_REDIRECT_HELP'] = "Bir oylamanın veya Anketin katılımından sonra eğer bir ileti isterseniz bunu burada tanımlayabilisiniz.";
+$_ARRAYLANG['TXT_SURVEY_COMMENTS_HELP'] = "Yorum fonksiyonunun etkinlemesinde anketin sonuna yorumlar için bir bölüm eklendi.";
+$_ARRAYLANG['TXT_SURVEY_HOMEBOX_HELP'] = "Yertutucu fonksiyonunun etkinlemesinde Anket [[SURVEY_HOME]] sistemtabanına bağlandı. Bunları örneğin Sidebarda kullanabilirsiniz.";
+$_ARRAYLANG['TXT_SURVEY_QUESTION'] = "Soru";
+$_ARRAYLANG['TXT_SURVEY_QUESTION_REMOVE'] = "Son soru gerçekten silinsin mi?";
+$_ARRAYLANG['TXT_SURVEY_ANSWER_REMOVE'] = "Bu cevap gerçekten silinsin mi?";
+$_ARRAYLANG['TXT_SURVEY_EXTENDED'] = "Geliştirilmiş";
+$_ARRAYLANG['TXT_SURVEY_ANSWERS'] = "Cevaplar";
+$_ARRAYLANG['TXT_SURVEY_ANSWER_ADD'] = "Başka soru ekle";
+$_ARRAYLANG['TXT_SURVEY_MOVE_UP'] = "Yukarı kaydır";
+$_ARRAYLANG['TXT_SURVEY_MOVE_DOWN'] = "Aşağıya kaydır";
+$_ARRAYLANG['TXT_SURVEY_ANSWER_DELETE'] = "Cevabı sil";
+$_ARRAYLANG['TXT_SURVEY_ACTIVATED_IN'] = "de Etkin";
+$_ARRAYLANG['TXT_SURVEY_TYPESELECTION'] = "Tip seçimi";
+$_ARRAYLANG['TXT_SURVEY_COMMENTABLE'] = "Yorumlar";
+$_ARRAYLANG['TXT_SURVEY_QUESTION_ADD'] = "Soru ekle";
+$_ARRAYLANG['TXT_SURVEY_QUESTION_DELETE'] = "Soru sil";
+$_ARRAYLANG['TXT_SETTINGS_LOGFILE'] = "Logverisi başlat";
+$_ARRAYLANG['TXT_SETTINGS_LOGFILE_HELP'] = "Bu seçeneği etkinlerseniz her katılana ayrıntılı bilgiler kaydediliyor.";
+$_ARRAYLANG['TXT_SETTINGS_ANONYMOUS'] = "Anonim katılanları kabul et";
+$_ARRAYLANG['TXT_SETTINGS_ANONYMOUS_HELP'] = "Bu seçeneğin güncellenmesinde Anketlere anonim de katılabilinir.";
+$_ARRAYLANG['TXT_SETTINGS_UPDATE_SUCCESSFULL'] = "Ayarlar başarıyla etinlendi.";
+?>
