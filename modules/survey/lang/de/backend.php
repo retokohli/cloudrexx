8a254db2a86367af1ca00ec8243b74cf7e18984e
Thomas Däppen <thomas.daeppen@cloudrexx.com>
2013-10-07 08:58:26 +0000

    survey: added new version from branch /branches/work/module_survey
    git-svn-id: http://svn.contrexxlabs.com/contrexx/trunk@27181 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

---
+++ modules/survey/lang/de/backend.php
@@ -0,0 +1,146 @@
+<?php
+/**
+* Contrexx CMS
+* generated date Fri, 8 jan 2010
+**/
+$_ARRAYLANG['TXT_ANALYSE_SURVEY_OVERVIEW']="Analyse Übersicht";
+$_ARRAYLANG['TXT_ANALYSE_SURVEY_QUESTION_TXT']="Fragenanalyse";
+
+
+$_ARRAYLANG['TXT_OVERVIEW'] = "Übersicht";
+$_ARRAYLANG['TXT_ADDRESSBOOK'] = "Adressen";
+$_ARRAYLANG['TXT_ADDITIONAL_FIELDS_LABEL'] = "Zusätzliche Teilnehmerinformationen";
+$_ARRAYLANG['TXT_ADDRESS_LIST_OVERVIEW'] = "Übersicht Adressen";
+$_ARRAYLANG['TXT_ADDRESSLIST_ADD'] = "Adresse hinzufügen";
+$_ARRAYLANG['TXT_ADD_ADDRESS_DETAILS'] = "Adressdetail erfassen";
+$_ARRAYLANG['TXT_ADDRESS_DETAILS'] = "Adressendetail";
+$_ARRAYLANG['TXT_ADDRESS_OVERVIEW'] = "Übersicht Adressen";
+$_ARRAYLANG['TXT_EMAIL_TEXT'] = "E-Mail";
+$_ARRAYLANG['TXT_LASTNAME'] = "Nachname";
+$_ARRAYLANG['TXT_FIRSTNAME'] = "Vorname";
+$_ARRAYLANG['TXT_CUSTOMDATA'] = "Kundendaten";
+$_ARRAYLANG['TXT_CREATE_SURVEY'] = "Umfrage erstellen";
+$_ARRAYLANG['TXT_SURVEYOVERVIEW'] = "Übersicht Umfrage";
+$_ARRAYLANG['TXT_WELCOME_MSG'] = "Willkommen";
+$_ARRAYLANG['TXT_ACTIVE'] = "Aktiv";
+$_ARRAYLANG['TXT_INACTIVE'] = "Inaktiv";
+$_ARRAYLANG['TXT_SELECT_QUESTION']="Bitte wählen Sie den Fragentyp";
+$_ARRAYLANG['TXT_QUESTION_TYPE']="Bitte wählen Sie den Fragentyp";
+$_ARRAYLANG['TXT_MULTIPLE_CHOICE_ONE_ANSWER']="Multiple Choice (Nur eine Antwort)";
+$_ARRAYLANG['TXT_MULTIPLE_CHOICE_MULTIPLE_ANSWER']="Multiple Choice (Mehrfache Antworten möglich)";
+$_ARRAYLANG['TXT_MATRIX_CHOICE_ONE_ANSWER_PER_ROW']="Matrix (Pro Frage eine Antwort)";
+$_ARRAYLANG['TXT_MATRIX_CHOICE_MULTIPLE_ANSWER_PER_ROW']="Matrix (Pro Frage mehrere Antworten)";
+$_ARRAYLANG['TXT_QUESTION_TEXT']="Frage";
+$_ARRAYLANG['TXT_ANSWER_CHOICE']="Antworten (pro Zeile eine Antwort)";
+$_ARRAYLANG['TXT_ADD_COMMENT']="Kommentarfeld hinzufügen";
+$_ARRAYLANG['TXT_SURVEY_COMMENT']="Kommentar";
+$_ARRAYLANG['TXT_SURVEY_ANSWER']="Antworten";
+$_ARRAYLANG['TXT_YES']="Ja";
+$_ARRAYLANG['TXT_NO']="Nein";
+$_ARRAYLANG['TXT_COLUMN_CHOICE']="Frage (Pro Zeile eine Frage)";
+$_ARRAYLANG['TXT_TITLE']="Titel";
+$_ARRAYLANG['TXT_UNIQUE_USER_VERIFICATION']="Teilnahmeüberprüfung";
+$_ARRAYLANG['TXT_COOKIE_BASED']="Cookie basiert";
+$_ARRAYLANG['TXT_EMAIL_BASED']="E-Mail basiert";
+$_ARRAYLANG['TXT_IS_HOME_BOX']="In Homebox auflisten";
+$_ARRAYLANG['TXT_DESCRIPTION']="Beschreibung";
+$_ARRAYLANG['TXT_BEGINNING_SURVEY']="Text am Anfang der Umfrage";
+$_ARRAYLANG['TXT_ADDITIONALINFO_SURVEY']="Text vor den zusätzlichen<br />Teilnehmerinformationen";
+$_ARRAYLANG['TXT_BELOW_SUBMIT']="Text unterhalb Absende-Button";
+$_ARRAYLANG['TXT_THANK_MSG']="Text für Dankes-Seite";
+$_ARRAYLANG['TXT_SALUTATION'] = "Anrede";
+$_ARRAYLANG['TXT_NICKNAME'] = "Nickname";
+$_ARRAYLANG['TXT_AGEGROUP'] = "Altersgruppe";
+$_ARRAYLANG['TXT_TELEPHONE'] = "Telefon";
+$_ARRAYLANG['TXT_STREET'] = "Strasse";
+$_ARRAYLANG['TXT_ZIPCODE'] = "Postleitzahl";
+$_ARRAYLANG['TXT_PLACEOFREC'] = "Wohnort";
+$_ARRAYLANG['TXT_VOTES'] = "Votes";
+$_ARRAYLANG['TXT_ANALYSE_QUESTION_PREVIEW']="Fragen Vorschau";
+$_ARRAYLANG['TXT_SNO']="Nummer";
+$_ARRAYLANG['TXT_QUESTION']="Frage";
+$_ARRAYLANG['TXT_ANSWERED_QUESTION']="Beantworte Fragen";
+$_ARRAYLANG['TXT_SKIPPED_QUESTION']="Übersprungene Fragen";
+$_ARRAYLANG['TXT_QUESTION_OVERVIEW']="Fragenübersicht";
+$_ARRAYLANG['TXT_SORTING']="Sortierung";
+$_ARRAYLANG['TXT_CREATED_AT']="Erstellt am";
+$_ARRAYLANG['TXT_QUESTION_TYPE']="Fragentyp";
+$_ARRAYLANG['TXT_IS_COMMENTABLE']="Kann kommentiert werden";
+$_ARRAYLANG['TXT_COUNTER']="Zähler";
+$_ARRAYLANG['TXT_FUNCTIONS']="Funktionen";
+$_ARRAYLANG['TXT_SELECT_ALL']="Alle markieren";
+$_ARRAYLANG['TXT_DESELECT_ALL']="Alle Markierungen löschen";
+$_ARRAYLANG['TXT_SELECT_ACTION']="Aktion";
+$_ARRAYLANG['TXT_SAVE_SORTING']="Sortierung speichern";
+$_ARRAYLANG['TXT_DELETE_SELECTED']="Markierte löschen";
+$_ARRAYLANG['TXT_SURVEY_OVERVIEW']="Übersicht Umfragen";
+$_ARRAYLANG['TXT_SYMBOL']="#";
+$_ARRAYLANG['TXT_STATUS']="Status";
+$_ARRAYLANG['TXT_SURVEY_TITLE']="Titel";
+$_ARRAYLANG['TXT_MODIFIED_AT']="Bearbeitet am";
+$_ARRAYLANG['TXT_ACTIVATE']="aktivieren";
+$_ARRAYLANG['TXT_DEACTIVATE']="deaktivieren";
+
+$_ARRAYLANG['TXT_TOTAL']="Total";
+$_ARRAYLANG['TXT_BACK']="Zurück";
+$_ARRAYLANG['TXT_BACK_SURVEY_TXT']="Zurück";
+$_ARRAYLANG['TXT_SINGLE_TEXTBOX']="Textfeld";
+$_ARRAYLANG['TXT_MULTIPLE_TEXTBOX']="Textfeld mit mehreren Zeilen";
+$_ARRAYLANG['TXT_TEXT_ROW']="Einfache Textzeile (Infotext)";
+$_ARRAYLANG['QUESTION_TEXT']="Frage";
+$_ARRAYLANG['SETTINGS_TEXT']="Einstellungen";
+$_ARRAYLANG['ADD_SALUTATION']="Anreden (pro Zeile eine Anrede)";
+$_ARRAYLANG['AGE_GROUP']="Altersgruppen (pro Zeile eine Altersgruppe)";
+$_ARRAYLANG['ENTER_AGE_GROUP']="Bitte definieren Sie eine Altersgruppe";
+$_ARRAYLANG['ENTER_SALUTATION']="Bitte definieren Sie eine Anrede";
+
+$_ARRAYLANG['TXT_EDIT_SURVEY_TXT']="Umfrage bearbeiten";
+$_ARRAYLANG['TXT_SAVE_TXT']="Speichern";
+$_ARRAYLANG['TXT_HELP_TXT']="Hilfe";
+$_ARRAYLANG['TXT_HELP_IMAGE_TXT']="Hilfeicon";
+$_ARRAYLANG['TXT_SURVEY_NEXT_TXT']="Weiter";
+$_ARRAYLANG['TXT_SURVEY_UPDATE_SUC_TXT']="Umfrage wurde gespeichert";
+$_ARRAYLANG['TXT_QUESTION_UPDATE_SUC_TXT']="Frage wurde gespeichert";
+$_ARRAYLANG['TXT_QUESTION_DELETED_SUC_TXT']="Frage wurde gelöscht";
+$_ARRAYLANG['TXT_QUESTION_ADD_TXT']="Neue Frage hinzufügen";
+$_ARRAYLANG['TXT_SURVEY_SORTING_SAVE_TXT']="Sortierreihenfolge wurde geändert";
+$_ARRAYLANG['TXT_EDIT_QUESTION_TXT']="Frage bearbeiten";
+$_ARRAYLANG['TXT_NO_DESCRIPTION_TXT']="Keine Beschreibung";
+$_ARRAYLANG['TXT_SURVEY_ACTIVATED_SUCC_TXT']="Umfrage wurde aktiviert";
+$_ARRAYLANG['TXT_SURVEY_DEACTIVATED_SUCC_TXT']="Umfrage wurde deaktiviert";
+$_ARRAYLANG['TXT_SURVEY_DELETE_SUC_TXT']="Umfrage wurde gelöscht";
+$_ARRAYLANG['TXT_SURVEY_RESETED_SUC_TXT']="Umfrage wurde zurückgesetzt";
+$_ARRAYLANG['TXT_SURVEY_ADD_TXT']="Umfrage hinzufügen";
+$_ARRAYLANG['TXT_SURVEY_ADDQUESTION_TXT']="Frage hinzufügen";
+$_ARRAYLANG['TXT_SURVEY_CREATEQUESTION_TXT']="Frage erstellen";
+$_ARRAYLANG['TXT_SURVEY_ADDED_SUC_TXT']="Umfrage wurde hinzugefügt";
+$_ARRAYLANG['TXT_SURVEY_ADDQUESTION_ANOTHER_TXT']="Neue Frage hinzufügen";
+$_ARRAYLANG['TXT_SURVEY_DONE_TXT']="Fertig";
+$_ARRAYLANG['TXT_SURVEY_PREVIEW_TXT']="Vorschau";
+$_ARRAYLANG['TXT_SURVEY_ANALYSE_TXT']="Analyse";
+$_ARRAYLANG['TXT_SURVEY_REFRESH_TXT']="zurücksetzen";
+$_ARRAYLANG['TXT_SURVEY_EDIT_TXT']="bearbeiten";
+$_ARRAYLANG['TXT_SURVEY_DELETE_TXT']="löschen";
+$_ARRAYLANG['TXT_SURVEY_CREATE_TXT']="erstellen";
+$_ARRAYLANG['TXT_SURVEY_SETTING_SUCC_TXT']="Einstellungen wurden gespeichert";
+$_ARRAYLANG['TXT_SURVEY_HOME_SUCC_TXT']="Die Standard-Umfrage wurde erfolgreich aktiviert";
+$_ARRAYLANG['TXT_SURVEY_HOME_ERR_TXT']="Die Standard-Umfrage muss zuerst aktiviert werden";
+$_ARRAYLANG['TXT_HOME']="Standard";
+
+$_ARRAYLANG['TXT_SURVEY_NOITEM_SELECTED_ERR']="Bitte markieren Sie die gewünschten Fragen";
+$_ARRAYLANG['TXT_SURVEY_CONFIRM_DELETE_ERR']="Möchten Sie diesen Eintrag wirklich löschen?";
+$_ARRAYLANG['TXT_SURVEY_SORTING_NUMBER_ERR']="Bitte geben Sie die Sortiernummer ein";
+$_ARRAYLANG['TXT_SURVEY_SORTING_NUMBER_NUM_ERR']="Bitte geben Sie die Sortiernummer ein (Zahl)";
+$_ARRAYLANG['TXT_SURVEY_SORTING_NUMBER_NOTSAME_ERR']="Ein oder mehrere Sortiernummern sind doppelt";
+$_ARRAYLANG['TXT_SURVEY_SELECT_ANSWER_INPUT_ERR']="Bitte wählen Sie den Fragentyp";
+$_ARRAYLANG['TXT_SURVEY_ENTER_QUESTION_ERR']="Bitte geben Sie die Frage ein";
+$_ARRAYLANG['TXT_SURVEY_ENTER_ANSWER_ERR']="Bitte geben Sie die Antwort auf die Frage ein";
+$_ARRAYLANG['TXT_SURVEY_ENTER_COLUMN_ERR']="Bitte geben Sie die Zeilen ein";
+
+$_ARRAYLANG['TXT_SURVEY_ENTER_TITLE_ERR']="Bitte geben Sie den Titel ein";
+$_ARRAYLANG['TXT_SURVEY_HOMEBOX_ERR']="Es ist bereits eine Umfrage für die Homebox definiert worden.";
+$_ARRAYLANG['TXT_SURVEY_SELECT_EMAIL_ERR'] = "Bitte wählen Sie das Feld E-Mail in den zusätzlichen Teilnehmerinformationen";
+
+$_ARRAYLANG['TXT_SHOW']="Anzeigen";
+$_ARRAYLANG['TXT_HIDE']="Verbergen";
+?>
