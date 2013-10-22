41baa7b461eeaa480b3cf869f1bf7e320a315e62
Michael Raess <michael.raess@comvation.com>
2013-10-22 12:10:52 +0000

    Lib Framework FileSystem - optimization - United all replaceCharacters and cleanFileName methods into one method and removed redundant code Lib Framework FileSystem - addition - Added new methods to sanitize paths and file names
    git-svn-id: http://svn.contrexxlabs.com/contrexx/trunk@27373 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- core_modules/fileBrowser/admin.class.php
+++ core_modules/fileBrowser/admin.class.php
@@ -577,7 +577,7 @@ class FileBrowser {
             //skip . and ..
             if($file == '.' || $file == '..') { continue; }
 
-			$file = self::cleanFileName($file);
+			$file = \Cx\Lib\FileSystem\FileSystem::replaceCharacters($file);
 
 			//delete potentially malicious files
             if(!FWValidator::is_file_ending_harmless($file)) {
@@ -627,35 +627,6 @@ class FileBrowser {
            we can now simply return the desired target path, as only valid
            files are present in $tempPath */	 
         return array($path, $webPath);
-    }
-
-	protected static function cleanFileName($string) {
-        //contrexx file name policies
-        $string = FWValidator::getCleanFileName($string);
-
-        //media library special changes; code depends on those
-        // replace $change with ''
-        $change = array('+');
-        // replace $signs1 with $signs
-        $signs1 = array(' ', 'ä', 'ö', 'ü', 'ç');
-        $signs2 = array('_', 'ae', 'oe', 'ue', 'c');
-
-        foreach ($change as $str) {
-            $string = str_replace($str, '_', $string);
-        }
-        for ($x = 0; $x < count($signs1); $x++) {
-            $string = str_replace($signs1[$x], $signs2[$x], $string);
-        }
-        $string = str_replace('__', '_', $string);
-        if (strlen($string) > 60) {
-            $info       = pathinfo($string);
-            $stringExt  = $info['extension'];
-
-            $stringName = substr($string, 0, strlen($string) - (strlen($stringExt) + 1));
-            $stringName = substr($stringName, 0, 60 - (strlen($stringExt) + 1));
-            $string     = $stringName.'.'.$stringExt;
-        }
-        return $string;
     }
 
 
