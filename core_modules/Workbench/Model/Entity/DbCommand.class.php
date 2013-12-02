3135fd42622d71d590159d9cf0d76f06ebce0cde
Michael Ritter <michael.ritter@cloudrexx.com>
2013-12-02 09:24:16 +0000

    core_modules Workbench - update - Added draft for shortcuts in DB command
    git-svn-id: http://svn.contrexxlabs.com/contrexx/trunk@28432 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- core_modules/Workbench/Model/Entity/DbCommand.class.php
+++ core_modules/Workbench/Model/Entity/DbCommand.class.php
@@ -22,25 +22,66 @@ class DbCommand extends Command {
      * Command description
      * @var string
      */
-    protected $description = 'Allows access to doctrine command line tools';
+    protected $description = 'Allows access to doctrine command line tools and adds some handy shortcuts';
     
     /**
      * Command synopsis
      * @var string
      */
-    protected $synopsis = 'workbench(.bat) db {doctrine syntax}';
+    protected $synopsis = 'workbench(.bat) db [doctrine {doctrine syntax}|{command}]';
     
     /**
      * Command help text
      * @var string
      */
-    protected $help = 'Gives access to doctrine command line tools. Use "help" to see what commands doctrine provides.';
+    protected $help = 'Gives access to doctrine command line tools and other db management commands. Use "help" to see what commands are available.';
     
     /**
      * Execute this command
      * @param array $arguments Array of commandline arguments
      */
     public function execute(array $arguments) {
+        $arguments = array_slice($arguments, 1);
+        
+        switch ($arguments[1]) {
+            // empty /model/entities and /model/repositories folders
+            case 'cleanup':
+                $this->cleanup();
+                break;
+            // update database for component
+            case 'update':
+                // empty /model/entities and /model/repositories folders
+                $this->cleanup();
+                // doctrine orm:validate-schema
+                $this->executeDoctrine(array('', 'doctrine', 'orm:validate-schema'));
+                // doctrine orm:generate-entities --filter="{component filter}" entities
+                $this->executeDoctrine(array('', 'doctrine', 'orm:generate-entities', '--filter="{component filter}"', 'entities'));
+                // move entities to component directory and add .class extension
+                // todo
+                // doctrine orm:generate-repositories --filter="{component filter}" repositories
+                $this->executeDoctrine(array('', 'doctrine', 'orm:generate-repositories', '--filter="{component filter}"', 'repositories'));
+                // move repositories to component directory and add .class extension
+                // todo
+                // doctrine orm:schema-tool:create --dump-sql
+                // remove component tables
+                // execute sql statements from db dump
+                // empty /model/entities and /model/repositories folders
+                $this->cleanup();
+                break;
+            case 'doctrine':
+                $this->executeDoctrine($arguments);
+                break;
+            case 'help':
+            default:
+                echo 'Command `' . $this->getName() . "`\r\n" .
+                        $this->getDescription() . "\r\n\r\n" .
+                        $this->getSynopsis() . "\r\n\r\n" .
+                        $this->getHelp() . "\r\n";
+                break;
+        }
+    }
+    
+    public function executeDoctrine(array $arguments) {
         $_SERVER['argv'] = array_slice($arguments, 1);
         
         $cli = new \Symfony\Component\Console\Application('Doctrine Command Line Interface', \Doctrine\Common\Version::VERSION);
@@ -75,5 +116,9 @@ class DbCommand extends Command {
 
         ));
         $cli->run();
+    }
+    
+    protected function cleanup() {
+        // leave /model/entities/Cx/Model/Base/EntityBase.class.php where it is!
     }
 }
