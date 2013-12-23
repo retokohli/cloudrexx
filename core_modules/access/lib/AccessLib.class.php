736cce4430b9efdc5d6f244a2ab5654a56477232
Michael Raess <michael.raess@comvation.com>
2013-12-23 07:48:56 +0000

    Merged revision(s) 28884 from branches/contrexx_3_1/core_modules/access/lib/AccessLib.class.php: Core Module Access - bugfix - Fixes #1871
    git-svn-id: http://svn.contrexxlabs.com/contrexx/trunk@28885 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- core_modules/access/lib/AccessLib.class.php
+++ core_modules/access/lib/AccessLib.class.php
@@ -600,15 +600,6 @@ class AccessLib
                         continue 2;
                     }
                     break;
-
-                case 'frontend_language':
-                    if (count(FWLanguage::getActiveFrontendLanguages()) == 1) {
-                        if ($this->_objTpl->blockExists('access_user_'.$attributeId)) {
-                            $this->_objTpl->hideBlock('access_user_'.$attributeId);
-                        }
-                        continue 2;
-                    }
-                    break;
             }
             $this->parseAccountAttribute($objUser, $attributeId, $edit);
         }
