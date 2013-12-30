aa04cefb37d8d41945984afdaf56de677a098ecd
Kevin Riesen <kevin.riesen@comvation.com>
2013-12-30 09:27:26 +0000

    fixed #1887: Taking BACKEND_LANG_ID if in backend, FRONTEND_LANG_ID otherwise
    git-svn-id: http://svn.contrexxlabs.com/contrexx/branches/contrexx_3_1@28985 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- core_modules/news/lib/newsLib.class.php
+++ core_modules/news/lib/newsLib.class.php
@@ -142,7 +142,12 @@ class newsLibrary
                 continue;
             }
             $selected = $category['id'] == $selectedCategory ? 'selected="selected"' : '';
-            $options .= '<option value="'.$category['id'].'" '.$selected.'>'.str_repeat('&nbsp;&nbsp;&nbsp;&nbsp;', ($category['level'] - $level)).contrexx_raw2xhtml($categoriesLang[$category['id']][BACKEND_LANG_ID]).'</option>';
+            $options .= '<option value="'.$category['id'].'" '.$selected.'>'
+                    .str_repeat('&nbsp;&nbsp;&nbsp;&nbsp;', ($category['level'] - $level))
+                    .contrexx_raw2xhtml($categoriesLang[$category['id']][
+                        \Env::get('cx')->getMode() == \Cx\Core\Core\Controller\Cx::MODE_BACKEND ? BACKEND_LANG_ID : FRONTEND_LANG_ID
+                    ])
+                    .'</option>';
         }
 
         return $options;
