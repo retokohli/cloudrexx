f3286a505f71060a07394acc6b6b70e103fbc503
Michael Raess <michael.raess@comvation.com>
2014-01-15 09:48:07 +0000

    Core Module Media - bugfix - Encode file names (special characters) in utf8 (fixes #1929)
    git-svn-id: http://svn.contrexxlabs.com/contrexx/branches/contrexx_3_1@29181 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- core_modules/media/admin.class.php
+++ core_modules/media/admin.class.php
@@ -595,7 +595,7 @@ class MediaManager extends MediaLibrary
                     $this->_objTpl->setVariable(array(  // file
                         'MEDIA_DIR_TREE_ROW'  => $class,
                         'MEDIA_FILE_ICON'     => self::_getIconWebPath().$dirTree[$key]['icon'][$x].'.png',
-                        'MEDIA_FILE_NAME'     => $fileName,
+                        'MEDIA_FILE_NAME'     => utf8_encode($fileName),
                         'MEDIA_FILE_SIZE'     => $this->_formatSize($dirTree[$key]['size'][$x]),
                         'MEDIA_FILE_TYPE'     => $this->_formatType($dirTree[$key]['type'][$x]),
                         'MEDIA_FILE_DATE'     => $this->_formatDate($dirTree[$key]['date'][$x]),
