cb3306d577944878ce161a55f9c287df381d2052
Michael Raess <michael.raess@comvation.com>
2014-01-21 07:36:59 +0000

    Merged revision(s) 29307 from branches/contrexx_3_1: Lib Framework System - change - Cosmetic adjustments Core Module Media - bugfix - Encode also directories (fixes #1929)
    git-svn-id: http://svn.contrexxlabs.com/contrexx/trunk@29308 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- lib/FRAMEWORK/System.class.php
+++ lib/FRAMEWORK/System.class.php
@@ -128,19 +128,22 @@ class FWSystem
     /**
      * Checks if the given string contains utf8 characters
      *
-     * @param   string  $string
-     * @return  int     0 (no match) or 1 (match)
+     * @param   string   $string
+     * @return  boolean  false (no match) or true (match)
      */
-    public static function detectUTF8($string) {
-        return preg_match('%(?:
-        [\xC2-\xDF][\x80-\xBF]              # non-overlong 2-byte
-        |\xE0[\xA0-\xBF][\x80-\xBF]         # excluding overlongs
-        |[\xE1-\xEC\xEE\xEF][\x80-\xBF]{2}  # straight 3-byte
-        |\xED[\x80-\x9F][\x80-\xBF]         # excluding surrogates
-        |\xF0[\x90-\xBF][\x80-\xBF]{2}      # planes 1-3
-        |[\xF1-\xF3][\x80-\xBF]{3}          # planes 4-15
-        |\xF4[\x80-\x8F][\x80-\xBF]{2}      # plane 16
-        )+%xs', $string);
+    public static function detectUtf8($string) {
+        return (bool) preg_match(
+            '%(?:
+                [\xC2-\xDF][\x80-\xBF]              # non-overlong 2-byte
+                |\xE0[\xA0-\xBF][\x80-\xBF]         # excluding overlongs
+                |[\xE1-\xEC\xEE\xEF][\x80-\xBF]{2}  # straight 3-byte
+                |\xED[\x80-\x9F][\x80-\xBF]         # excluding surrogates
+                |\xF0[\x90-\xBF][\x80-\xBF]{2}      # planes 1-3
+                |[\xF1-\xF3][\x80-\xBF]{3}          # planes 4-15
+                |\xF4[\x80-\x8F][\x80-\xBF]{2}      # plane 16
+            )+%xs',
+            $string
+        );
     }
 
 }
