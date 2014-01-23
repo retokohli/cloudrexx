5bfdf720838ae4630e411f67996b67aafb75a291
ss4u <comvation@softsolutions4u.info>
2014-01-23 06:23:39 +0000

    News : Added NEWS_COMMENT_LINK placeholder in news_recent_comments.html
    git-svn-id: http://svn.contrexxlabs.com/contrexx/trunk@29350 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- core_modules/news/lib/newsRecentComments.class.php
+++ core_modules/news/lib/newsRecentComments.class.php
@@ -47,62 +47,78 @@ class newsRecentComments extends newsLibrary
 
         // abort if commenting system is not active
         if (!$this->arrSettings['news_comments_activated']) {
-            $this->_objTemplate->hideBlock('news_comments');
-            return;
-        }
-        
-        $_ARRAYLANG = \Env::get('init')->loadLanguageData('news');
-        $commentsCount = (int) $this->arrSettings['recent_news_message_limit'];
-        
-        $query = "SELECT  `title`,
-                          `date`,
-                          `poster_name`,
-                          `userid`,
-                          `text`
-                    FROM  
-                          `".DBPREFIX."module_news_comments`
-                    WHERE       
-                          `is_active` = '1'
-                    ORDER BY
-                          `date` DESC 
-                    LIMIT 0, $commentsCount";
+            $this->_objTemplate->hideBlock('news_comments');            
+        } else {
+            $_ARRAYLANG = \Env::get('init')->loadLanguageData('news');
+            $commentsCount = (int) $this->arrSettings['recent_news_message_limit'];
 
-        $objResult = $objDatabase->Execute($query);
+            $query = "SELECT  `nComment`.`title`,
+                              `nComment`.`date`,
+                              `nComment`.`poster_name`,
+                              `nComment`.`userid`,
+                              `nComment`.`text`,
+                              `news`.`id`,
+                              `news`.`catid`
+                        FROM  
+                              `".DBPREFIX."module_news_comments` AS nComment
+                        LEFT JOIN 
+                              `".DBPREFIX."module_news` AS news
+                        ON
+                            `nComment`.newsid = `news`.id
+                        WHERE
+                            `news`.status = 1
+                        AND
+                            `news`.allow_comments = 1
+                        AND
+                            `nComment`.`is_active` = '1'
+                        ORDER BY
+                              `date` DESC 
+                        LIMIT 0, $commentsCount";
 
-        // no comments for this message found
-        if (!$objResult || $objResult->EOF) {
-            if ($this->_objTemplate->blockExists('news_no_comment')) {
-                $this->_objTemplate->setVariable('TXT_NEWS_COMMENTS_NONE_EXISTING', $_ARRAYLANG['TXT_NEWS_COMMENTS_NONE_EXISTING']);
-                $this->_objTemplate->parse('news_no_comment');
+            $objResult = $objDatabase->Execute($query);
+
+            // no comments for this message found
+            if (!$objResult || $objResult->EOF) {
+                if ($this->_objTemplate->blockExists('news_no_comment')) {
+                    $this->_objTemplate->setVariable('TXT_NEWS_COMMENTS_NONE_EXISTING', $_ARRAYLANG['TXT_NEWS_COMMENTS_NONE_EXISTING']);
+                    $this->_objTemplate->parse('news_no_comment');
+                }
+
+                $this->_objTemplate->hideBlock('news_comment_list');
+                $this->_objTemplate->parse('news_comments');
+
+                return $this->_objTemplate->get();
             }
 
-            $this->_objTemplate->hideBlock('news_comment_list');
-            $this->_objTemplate->parse('news_comments');
+            $i = 0;
+            while (!$objResult->EOF) {
+                self::parseUserAccountData($this->_objTemplate, $objResult->fields['userid'], $objResult->fields['poster_name'], 'news_comments_poster');
+                
+                $commentTitle = $objResult->fields['title'];
+                
+                $newsUrl  = \Cx\Core\Routing\Url::fromModuleAndCmd('news', $this->findCmdById('details', $objResult->fields['catid']), FRONTEND_LANG_ID, array('newsid' => $objResult->fields['id']));
+                $newsLink = self::parseLink($newsUrl, $commentTitle, contrexx_raw2xhtml($commentTitle));
+                
+                $this->_objTemplate->setVariable(array(
+                   'NEWS_COMMENTS_CSS'          => 'row'.($i % 2 + 1),
+                   'NEWS_COMMENTS_TITLE'        => contrexx_raw2xhtml($commentTitle),
+                   'NEWS_COMMENTS_MESSAGE'      => nl2br(contrexx_raw2xhtml($objResult->fields['text'])),
+                   'NEWS_COMMENTS_LONG_DATE'    => date(ASCMS_DATE_FORMAT, $objResult->fields['date']),
+                   'NEWS_COMMENTS_DATE'         => date(ASCMS_DATE_FORMAT_DATE, $objResult->fields['date']),
+                   'NEWS_COMMENTS_TIME'         => date(ASCMS_DATE_FORMAT_TIME, $objResult->fields['date']),
+                   'NEWS_COMMENT_LINK'          => $newsLink,
+                   'NEWS_COMMENT_URL'           => $newsUrl
+                ));
 
-            return $this->_objTemplate->get();
+                $this->_objTemplate->parse('news_comment');
+                $i++;
+                $objResult->MoveNext();
+            }
+
+            $this->_objTemplate->parse('news_comment_list');
+            $this->_objTemplate->hideBlock('news_no_comment');
         }
-
-        $i = 0;
-        while (!$objResult->EOF) {
-            self::parseUserAccountData($this->_objTemplate, $objResult->fields['userid'], $objResult->fields['poster_name'], 'news_comments_poster');
-
-            $this->_objTemplate->setVariable(array(
-               'NEWS_COMMENTS_CSS'          => 'row'.($i % 2 + 1),
-               'NEWS_COMMENTS_TITLE'        => contrexx_raw2xhtml($objResult->fields['title']),
-               'NEWS_COMMENTS_MESSAGE'      => nl2br(contrexx_raw2xhtml($objResult->fields['text'])),
-               'NEWS_COMMENTS_LONG_DATE'    => date(ASCMS_DATE_FORMAT, $objResult->fields['date']),
-               'NEWS_COMMENTS_DATE'         => date(ASCMS_DATE_FORMAT_DATE, $objResult->fields['date']),
-               'NEWS_COMMENTS_TIME'         => date(ASCMS_DATE_FORMAT_TIME, $objResult->fields['date']),
-            ));
-
-            $this->_objTemplate->parse('news_comment');
-            $i++;
-            $objResult->MoveNext();
-        }
-
-        $this->_objTemplate->parse('news_comment_list');
-        $this->_objTemplate->hideBlock('news_no_comment');
-        
+         
         return $this->_objTemplate->get();
     }
 }
