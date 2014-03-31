fd5adc256793e5fe944bd618dd07308f06c5eb8b
Michael Raess <michael.raess@comvation.com>
2014-03-31 09:00:26 +0000

    Core Module Media - bugfix - Don't rotate image if value is empty (fixes #2021)
    git-svn-id: http://svn.contrexxlabs.com/contrexx/branches/contrexx_3_1@29968 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- core_modules/media/mediaLib.class.php
+++ core_modules/media/mediaLib.class.php
@@ -468,7 +468,7 @@ class MediaLibrary
             }
             
             // Rotate image
-            if (isset($arrData['d'])) {
+            if (!empty($arrData['d'])) {
                 $this->_objImage->rotateImage(intval($arrData['d']));
             }
             
