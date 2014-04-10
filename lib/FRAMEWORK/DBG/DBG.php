2f14698527f4e3260b368431871b9cd3c58e9006
Michael Ritter <michael.ritter@cloudrexx.com>
2014-04-10 14:10:15 +0000

    lib DBG - update - Added activateIf() and some conditionals
    git-svn-id: http://svn.contrexxlabs.com/contrexx/trunk@30059 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- lib/FRAMEWORK/DBG/DBG.php
+++ lib/FRAMEWORK/DBG/DBG.php
@@ -103,6 +103,30 @@ class DBG
             self::stack();
         }
     }
+    
+    public static function activateIf($condition, $mode = null) {
+        if (
+            (!is_callable($condition) && $condition) ||
+            (is_callable($condition) && $condition())
+        ) {
+            static::activate($mode);
+        }
+    }
+    
+    public static function isIp($ip) {
+        return $_SERVER['REMOTE_ADDR'] == $ip;
+    }
+    
+    public static function hasCookie($cookieName) {
+        return isset($_COOKIE[$cookieName]);
+    }
+    
+    public static function hasCookieValue($cookieName, $cookieValue) {
+        if (!static::hasCookie($cookieName)) {
+            return false;
+        }
+        return $_COOKIE[$cookieName] == $cookieValue;
+    }
 
 
     /**
