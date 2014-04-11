b5e2565bea59363851f9377de76f791b40211610
Robin Glauser <robin.glauser@cloudrexx.com>
2014-04-11 15:28:29 +0000

    (fixed #2052): File Extension interchangeable
    git-svn-id: http://svn.contrexxlabs.com/contrexx/branches/contrexx_3_1@30064 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- core_modules/media/mediaLib.class.php
+++ core_modules/media/mediaLib.class.php
@@ -333,18 +333,18 @@ class MediaLibrary
 
         $obj_file = new File();
         // file or dir
+        $fileName = !empty($_POST['renName']) ? $_POST['renName'] : 'empty';
         if (empty($_POST['oldExt'])) {
-            $fileName = $_POST['renName'];
-            $oldName  = $_POST['oldName'];
+            $oldName = $_POST['oldName'];
         } else {
-            // TODO: $_POST['renName'] may be empty
-            $ext      =
-                (   !empty($_POST['renExt'])
-                 && FWValidator::is_file_ending_harmless(
-                        $_POST['renName'].$_POST['renExt'])
-                    ? $_POST['renExt'] : 'txt');
-            $fileName = $_POST['renName'].'.'.$ext;
-            $oldName  = $_POST['oldName'].'.'.$_POST['oldExt'];
+            $ext
+                = (!empty($_POST['renExt'])
+            && FWValidator::is_file_ending_harmless(
+                $_POST['renName'] . '.' . $_POST['renExt']
+            )
+                ? $_POST['renExt'] : 'txt');
+            $fileName = $fileName . '.' . $ext;
+            $oldName = $_POST['oldName'] . '.' . $_POST['oldExt'];
         }
 
         \Cx\Lib\FileSystem\FileSystem::clean_path($fileName);
