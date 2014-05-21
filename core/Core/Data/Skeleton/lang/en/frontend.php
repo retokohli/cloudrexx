17854b3a9fdc747ade6d35f5108a95d3a89288ca
Robin Glauser <robin.glauser@cloudrexx.com>
2014-05-21 13:32:42 +0000

    Revert: First version of the new MediaBrowser
    git-svn-id: http://svn.contrexxlabs.com/contrexx/trunk@30505 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- core/Core/Data/Skeleton/lang/en/frontend.php
+++ core/Core/Data/Skeleton/lang/en/frontend.php
@@ -13,10 +13,10 @@
 global $_ARRAYLANG;
 
 // Let's start with module info:
-$_ARRAYLANG['TXT_CORE_MODULE_MEDIABROWSER'] = 'FilesBrowser';
-$_ARRAYLANG['TXT_CORE_MODULE_MEDIABROWSER_DESCRIPTION'] = 'This is a new module with some sample content to show how to start.';
+$_ARRAYLANG['TXT_MODULE_SKELETON'] = '{COMPONENT_NAME}';
+$_ARRAYLANG['TXT_MODULE_SKELETON_DESCRIPTION'] = 'This is a new module with some sample content to show how to start.';
 
 // Now our content specific values:
-$_ARRAYLANG['TXT_CORE_MODULE_MEDIABROWSER_CONGRATULATIONS'] = 'Congratulations';
-$_ARRAYLANG['TXT_CORE_MODULE_MEDIABROWSER_SUCCESSFUL_CREATION'] = 'You successfully created a new module!';
-$_ARRAYLANG['TXT_CORE_MODULE_MEDIABROWSER_EXAMPLE_TEMPLATE'] = 'This is the default template for this component. It is used for all ACTs that do not have their own template.';
+$_ARRAYLANG['TXT_MODULE_SKELETON_CONGRATULATIONS'] = 'Congratulations';
+$_ARRAYLANG['TXT_MODULE_SKELETON_SUCCESSFUL_CREATION'] = 'You successfully created a new module!';
+$_ARRAYLANG['TXT_MODULE_SKELETON_EXAMPLE_TEMPLATE'] = 'This is the default template for this component. It is used for all ACTs that do not have their own template.';
