128c2ca6f83d828e2ddd882232f96bed47426d1f
Sudhir Cdnsol <sudhir.cdnsol@comvation.com>
2014-06-11 10:58:19 +0000

    commiting aftre changing instance t website
    git-svn-id: http://svn.contrexxlabs.com/contrexx/branches/contrexx_4_0@31020 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- core/Core/init.php
+++ core/Core/init.php
@@ -1,4 +1,5 @@
 <?php
+@ini_set('date.timezone', 'Europe/Zurich');
 /**
  * This file loads everything needed to load Contrexx. Just require this file
  * and execute \init($mode); while $mode is optional. $mode can be one of
@@ -62,21 +63,21 @@ require_once dirname(dirname(dirname(__FILE__))).'/lib/FRAMEWORK/DBG/DBG.php';
 //\DBG::activate(DBG_PHP);
 
 
-	require_once $_PATHCONFIG['ascms_installation_root'].$_PATHCONFIG['ascms_installation_offset'].'/core_modules/MultiSite/Model/Repository/InstanceRepository.class.php';
+	require_once $_PATHCONFIG['ascms_installation_root'].$_PATHCONFIG['ascms_installation_offset'].'/core_modules/MultiSite/Model/Repository/WebsiteRepository.class.php';
 	require_once $_PATHCONFIG['ascms_installation_root'].$_PATHCONFIG['ascms_installation_offset'].'/core/Core/Model/Entity/EntityBase.class.php';
 	require_once $_PATHCONFIG['ascms_installation_root'].$_PATHCONFIG['ascms_installation_offset'].'/core_modules/License/Person.class.php';
-	require_once $_PATHCONFIG['ascms_installation_root'].$_PATHCONFIG['ascms_installation_offset'].'/core_modules/MultiSite/Model/Entity/Instance.class.php';
-	$multiSiteRepo = new \Cx\Core_Modules\MultiSite\Model\Repository\InstanceRepository();
+	require_once $_PATHCONFIG['ascms_installation_root'].$_PATHCONFIG['ascms_installation_offset'].'/core_modules/MultiSite/Model/Entity/Website.class.php';
+	$multiSiteRepo = new \Cx\Core_Modules\MultiSite\Model\Repository\WebsiteRepository();
 	$subdomain = current(explode('.', $_SERVER['HTTP_HOST']));
-	global $multiSiteInstanceOffset, $multiSiteInstanceName;
-	$multiSiteInstanceOffset = '';
-	$multiSiteInstanceName = '';
-	foreach ($multiSiteRepo->findAll($_PATHCONFIG['ascms_root'].$_PATHCONFIG['ascms_root_offset'].'/instances') as $instance) {
+	global $multiSiteWebsiteOffset, $multiSiteWebsiteName;
+	$multiSiteWebsiteOffset = '';
+	$multiSiteWebsiteName = '';
+	foreach ($multiSiteRepo->findAll($_PATHCONFIG['ascms_root'].$_PATHCONFIG['ascms_root_offset'].'/websites') as $instance) {
 		if ($subdomain == strtolower($instance->getName())) {
 			//\DBG::activate(DBG_PHP);
-			$_PATHCONFIG['ascms_root_offset'] .= '/instances/'.$instance->getName();
-			$multiSiteInstanceOffset .= '/instances/'.$instance->getName();
-			$multiSiteInstanceName = $instance->getName();
+			$_PATHCONFIG['ascms_root_offset'] .= '/websites/'.$instance->getName();
+			$multiSiteWebsiteOffset .= '/websites/'.$instance->getName();
+			$multiSiteWebsiteName = $instance->getName();
 			$pathconfigBackup = $_PATHCONFIG;
 			require_once $_PATHCONFIG['ascms_root'].$_PATHCONFIG['ascms_root_offset'].'/config/configuration.php';
 			//$_PATHCONFIG = $pathconfigBackup;
