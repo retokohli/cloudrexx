4775624faa119f0a30ecd932777635cb112c2f33
Kevin Riesen <kevin.riesen@comvation.com>
2014-07-02 07:47:54 +0000

    fixed: DB-Structure differences
    git-svn-id: http://svn.contrexxlabs.com/contrexx/branches/contrexx_3_2@31536 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- update/updates/3.2.0/update3.php
+++ update/updates/3.2.0/update3.php
@@ -853,7 +853,7 @@ $updates310To310Sp1 = array(
             'contact_customer'       => array('type' => 'INT(11)', 'notnull' => false, 'after' => 'contact_role'),
             'contact_language'       => array('type' => 'INT(11)', 'notnull' => false, 'after' => 'contact_customer'),
             'gender'                 => array('type' => 'TINYINT(2)', 'after' => 'contact_language'),
-            'notes'                  => array('type' => 'text', 'notnull' => true, 'after' => 'gender'),
+            'notes'                  => array('type' => 'text', 'notnull' => false, 'after' => 'gender'),
             'industry_type'          => array('type' => 'INT(11)', 'notnull' => false, 'after' => 'notes'),
             'contact_type'           => array('type' => 'TINYINT(2)', 'notnull' => false, 'after' => 'industry_type'),
             'user_account'           => array('type' => 'INT(11)', 'notnull' => false, 'after' => 'contact_type'),
@@ -894,7 +894,7 @@ $updates310To310Sp1 = array(
             'notes_type_id'      => array('type' => 'INT(1)', 'after' => 'customer_id'),
             'user_id'            => array('type' => 'INT(11)', 'after' => 'notes_type_id'),
             'date'               => array('type' => 'date', 'after' => 'user_id'),
-            'comment'            => array('type' => 'text', 'after' => 'date'),
+            'comment'            => array('type' => 'text', 'notnull' => false, 'after' => 'date'),
             'added_date'         => array('type' => 'datetime', 'notnull' => false, 'after' => 'comment'),
             'updated_by'         => array('type' => 'INT(11)', 'notnull' => false, 'after' => 'added_date'),
             'updated_on'         => array('type' => 'datetime', 'notnull' => false, 'after' => 'updated_by')
@@ -933,7 +933,7 @@ $updates310To310Sp1 = array(
             'id'             => array('type' => 'INT(11)', 'notnull' => true, 'auto_increment' => true, 'primary' => true),
             'email'          => array('type' => 'VARCHAR(256)', 'after' => 'id'),
             'email_type'     => array('type' => 'TINYINT(4)', 'after' => 'email'),
-            'is_primary'     => array('type' => 'ENUM(\'0\',\'1\')', 'notnull' => true, 'default' => '0', 'after' => 'email_type'),
+            'is_primary'     => array('type' => 'ENUM(\'0\',\'1\')', 'notnull' => false, 'default' => '0', 'after' => 'email_type'),
             'contact_id'     => array('type' => 'INT(11)', 'after' => 'is_primary')
         ),
         'keys' => array(
@@ -948,7 +948,7 @@ $updates310To310Sp1 = array(
             'id'             => array('type' => 'INT(11)', 'notnull' => true, 'auto_increment' => true, 'primary' => true),
             'phone'          => array('type' => 'VARCHAR(256)', 'after' => 'id'),
             'phone_type'     => array('type' => 'TINYINT(4)', 'after' => 'phone'),
-            'is_primary'     => array('type' => 'ENUM(\'0\',\'1\')', 'notnull' => true, 'default' => '0', 'after' => 'phone_type'),
+            'is_primary'     => array('type' => 'ENUM(\'0\',\'1\')', 'notnull' => false, 'default' => '0', 'after' => 'phone_type'),
             'contact_id'     => array('type' => 'INT(11)', 'after' => 'is_primary')
         ),
         'keys' => array(
@@ -963,7 +963,7 @@ $updates310To310Sp1 = array(
             'id'             => array('type' => 'INT(11)', 'notnull' => true, 'auto_increment' => true, 'primary' => true),
             'url'            => array('type' => 'VARCHAR(256)', 'after' => 'id'),
             'url_profile'    => array('type' => 'TINYINT(4)', 'after' => 'url'),
-            'is_primary'     => array('type' => 'ENUM(\'0\',\'1\')', 'notnull' => true, 'default' => '0', 'after' => 'url_profile'),
+            'is_primary'     => array('type' => 'ENUM(\'0\',\'1\')', 'notnull' => false, 'default' => '0', 'after' => 'url_profile'),
             'contact_id'     => array('type' => 'INT(11)', 'after' => 'is_primary')
         ),
         'keys' => array(
@@ -979,7 +979,7 @@ $updates310To310Sp1 = array(
             'url'            => array('type' => 'VARCHAR(256)', 'after' => 'id'),
             'url_type'       => array('type' => 'TINYINT(4)', 'after' => 'url'),
             'url_profile'    => array('type' => 'TINYINT(4)', 'after' => 'url_type'),
-            'is_primary'     => array('type' => 'ENUM(\'0\',\'1\')', 'notnull' => true, 'default' => '0', 'after' => 'url_profile'),
+            'is_primary'     => array('type' => 'ENUM(\'0\',\'1\')', 'notnull' => false, 'default' => '0', 'after' => 'url_profile'),
             'contact_id'     => array('type' => 'INT(11)', 'after' => 'is_primary')
         ),
         'keys' => array(
