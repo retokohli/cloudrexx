babbb35eccec913ce91c0905631e5a49505dfeef
Raj ss4u <rajkumar@softsolutions4u.info>
2014-07-09 13:59:48 +0000

    In method Cx\Core\Json\JsonData::data() passed all arguments (not only POST) to hasAccess() method.
    git-svn-id: http://svn.contrexxlabs.com/contrexx/branches/contrexx_4_0@31695 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- core/Json/JsonData.class.php
+++ core/Json/JsonData.class.php
@@ -170,7 +170,7 @@ class JsonData {
         }
         
         if ($objPermission && ($objPermission instanceof \Cx\Core\Access\Model\Entity\Permission)) {
-            if (!$objPermission->hasAccess($arguments['post']))
+            if (!$objPermission->hasAccess($arguments))
                 return $this->getErrorData('JsonData-request to method ' . $realMethod . ' of adapter ' . $adapter->getName() . ' has been rejected by not complying to the permission requirements of the requested method.');
         }
         
