cc8323be5f78e7d28ae57c31bf36bf4c35947b85
Thomas Däppen <thomas.daeppen@cloudrexx.com>
2014-07-14 18:04:51 +0000

    Cx\Core_Modules\Listing: added newest version from branch contrexx_4_0
    git-svn-id: http://svn.contrexxlabs.com/contrexx/trunk@31736 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- core/Html/Controller/ViewGenerator.class.php
+++ core/Html/Controller/ViewGenerator.class.php
@@ -17,10 +17,12 @@ class ViewGenerator {
     /**
      *
      * @param mixed $object Array, instance of DataSet, instance of EntityBase, object
+     * @param $options is functions array 
      * @throws ViewGeneratorException 
      */
     public function __construct($object, $options = array()) {
         $this->options = $options;
+        $entityNS=null;
         if (is_array($object)) {
             $object = new \Cx\Core_Modules\Listing\Model\Entity\DataSet($object);
         }
@@ -35,12 +37,70 @@ class ViewGenerator {
             // render form
             $this->object = $object;
         }
-        
+        // get entity name space
+        $entityNS = $this->object->getDataType();
+        if (empty($entityNS)) {
+            \Message::add('Cannot load, Invalid name space', \Message::CLASS_ERROR);
+            return;
+        }
+        /** 
+         *  postSave event
+         *  execute save if entry is a doctrine entity (or execute callback if specified in configuration)
+         */
+        $add=(!empty($_GET['add'])? contrexx_input2raw($_GET['add']):null);
+        if (!empty($add) && !empty($this->options['functions']['add'])) {
+            $form = $this->renderFormForEntry(null);
+            if ($form === false) {
+                // cannot save, no such entry
+                \Message::add('Cannot save, no such entry', \Message::CLASS_ERROR);
+                return;
+            }
+            if (!$form->isValid()) {
+                // data validation failed, stay in add view
+                \Message::add('Cannot save, validation failed', \Message::CLASS_ERROR);
+                return;
+            }
+            if (!empty($_POST)) {
+                $post=$_POST;
+                unset($post['csrf']);
+                $blankPost=true;
+                if (!empty($post)) {
+                    foreach($post as $value) {
+                        if ($value) $blankPost=false;
+                    }
+                }
+                if ($blankPost) {
+                    \Message::add('Cannot save, You should fill any one field!', \Message::CLASS_ERROR);
+                    return;
+                }
+                $entityObject = \Env::get('em')->getClassMetadata($entityNS);  
+                $primaryKeyName =$entityObject->getSingleIdentifierFieldName(); //get primary key name  
+                $getAllField = $entityObject->getColumnNames(); //get all field names  
+                //add new entry
+                $entityObj = new $entityNS();
+                foreach($getAllField as $entity) {
+                    if (isset($_POST[$entity]) && $entity!=$primaryKeyName) {
+                        $name='set'.$entity;
+                        $entityObj->$name(contrexx_input2raw($_POST[$entity]));
+                    }
+                }
+                \Env::get('em')->persist($entityObj);
+                \Env::get('em')->flush();
+                \Message::add('Entity have been added sucessfully!');   
+                $actionUrl = clone \Env::get('Resolver')->getRequest->getUrl();
+                $actionUrl->setParam('add', null);
+                \Cx\Core\Csrf\Controller\ComponentController::redirect($actionUrl);
+            }
+        }
+        /** 
+         *  postEdit event
+         *  execute edit if entry is a doctrine entity (or execute callback if specified in configuration)
+         */
+       // $entityId = (isset($_POST['editid'])? contrexx_input2raw($_POST['editid']):null);
         if (isset($_POST['editid'])) {
-            // render form for editid
             $entityId = contrexx_input2raw($_POST['editid']);
+            // render form for editid
             $form = $this->renderFormForEntry($entityId);
-            // form->isValid()?
             if ($form === false) {
                 // cannot save, no such entry
                 \Message::add('Cannot save, no such entry', \Message::CLASS_ERROR);
@@ -49,119 +109,95 @@ class ViewGenerator {
             if (!$form->isValid()) {
                 // data validation failed, stay in edit view
                 \Message::add('Cannot save, validation failed', \Message::CLASS_ERROR);
-                $_GET['editid'] = $_POST['editid'];
                 return;
             }
-            // get form data
-            $dataSet = $form->getData();
-            
-            /**
-             * TODO:
-             * - trigger pre- and postSave event
-             * - execute save if entry is a doctrine entity (or execute callback if specified in configuration)
-             * 
-             * - trigger pre- and postRemove event
-             * - execute remove if entry is a doctrine entity (or execute callback if specified in configuration)
-             * 
-             * - trigger pre- and postCreate event
-             * - execute create if entity class is a doctrine entity (or execute callback if specified in configuration)
-             */
-            
-            //echo '<pre>';var_dump($dataSet->toArray());die();
-            // save form data
-            // CUSTOMIZING FOR PAYCLOUD.CH
-            $instanceUrl = 'https://' . contrexx_input2raw($_POST['editid']) . '.' . substr($_SERVER['SERVER_NAME'], 0);
-            $data = $dataSet->toArray();
-            
-            // modify data for checkout json adapter of instance
-            $data['firstName'] = $data['firstname'];
-            $data['name'] = $data['lastname'];
-            $data['plz'] = $data['zip'];
-            $data['ort'] = $data['location'];
-            $data['emailId'] = $data['email'];
-            $data['phoneNumber'] = $data['phone'];
-            $data['poBox'] = $data['pOBox'];
-            $data['gender'] = $data['salutation'] === 'Mrs.' ? 1 : 2;
-            $data['newsletterStatus'] = $data['newsletterStatus'] === 'yes' ? 1 : 0;
-            
-            $jd = new \Cx\Core\Json\JsonData();
-            $jd->getJson(
-                $instanceUrl.'/cadmin/index.php?cmd=JsonData&object=user&act=loginUser',
-                array(
-                    'USERNAME' => 'O5vie5gOnIMY3Xbi@payrexx.com',
-                    'PASSWORD' => 'NwIoOKjujfMlLcMnZCoSxFyZ6hmKnRxa',
-                )
-            );
-            // get new username and password
-            $passwordHasChanged = false;
-            $username = $data['email'];
-            $password = contrexx_input2raw($_POST['password1']);
-            if (!empty($_POST['password1']) || !empty($_POST['password2'])) {
-                if (empty($_POST['password2']) || $_POST['password1'] != $_POST['password2']) {
-                    \Message::add('blabla', \Message::CLASS_ERROR);
-                    return;
+            $entityObject=array();
+            if ($this->object->entryExists($entityId)) {
+                $entityObject = $this->object->getEntry($entityId);
+            }
+            if (empty($entityObject)) {
+                \Message::add('Cannot save, Invalid entry', \Message::CLASS_ERROR);
+                return;
+            }
+            $isUpdate=false; 
+            $updateArray=array();
+            $entityObj = \Env::get('em')->getClassMetadata($entityNS);  
+            $primaryKeyName =$entityObj->getSingleIdentifierFieldName(); //get primary key name  
+            //$getAllField = $entityObj->getColumnNames(); //get all field names 
+            $id=$entityObject[$primaryKeyName]; //get primary key value  
+            $classMethods = get_class_methods(new $entityNS());
+            foreach ($entityObject as $name=>$value) {
+                if (isset ($_POST[$name])) { 
+                    if ($_POST[$name] != $value) {
+                        $isUpdate=true;
+                        if (in_array('set'.ucfirst($name), $classMethods)) {
+                            $updateArray['set'.ucfirst($name)]=contrexx_input2raw($_POST[$name]);
+                        }
+                    } 
                 }
-                $passwordHasChanged = true;
             }
-            // set password using access json adapter
-            if ($username != '' || $passwordHasChanged) {
-                $jd->getJson(
-                    $instanceUrl.'/cadmin/index.php?cmd=JsonData&object=user&act=setPassword',
-                    array(
-                        'userId' => $username,
-                        'password' => $password,
-                        'repeatPassword' => $password,
-                    )
-                );
+            if (!empty($updateArray) && !empty($id) 
+                && !empty($isUpdate)) {
+                $entityObj=\Env::get('em')->getRepository($entityNS)->find($id);
+                if (!empty($entityObj)) {
+                    foreach($updateArray as $key=>$value) {
+                        $entityObj->$key($value);
+                    }
+                    \Env::get('em')->flush();    
+                    \Message::add('Entity have been updated sucessfully!');   
+                } else {
+                    \Message::add('Cannot save, Invalid argument!', \Message::CLASS_ERROR);
+                }
+            } 
+            $actionUrl = clone \Env::get('Resolver')->getRequest->getUrl();
+            $actionUrl->setParam('editid', null);
+            \Cx\Core\Csrf\Controller\ComponentController::redirect($actionUrl);
+        }
+        /**
+         * 
+         * trigger pre- and postRemove event
+         * execute remove if entry is a doctrine entity (or execute callback if specified in configuration)
+         */
+        $deleteId = (!empty($_GET['deleteid'])? contrexx_input2raw($_GET['deleteid']):null);
+        if (!empty($deleteId)) {
+            $entityObject = $this->object->getEntry($deleteId);
+            if (empty($entityObject)) {
+                \Message::add('Cannot save, Invalid entry', \Message::CLASS_ERROR);
+                return;
             }
-            
-            if (isset($_POST['activeStatus'])){
-                $jd->getJson(
-                    $instanceUrl.'/cadmin/index.php?cmd=JsonData&object=checkout&act=activateInstance'
-                );
-                $jd->getJson(
-                    $instanceUrl.'/cadmin/index.php?cmd=JsonData&object=checkout&act=setMessage',
-                    array(
-                        'message' => contrexx_input2raw($_POST['instanceMessage']),
-                    )
-                );
-            } else {
-                $jd->getJson(
-                    $instanceUrl.'/cadmin/index.php?cmd=JsonData&object=checkout&act=disableInstance'
-                );
+            $entityObj = \Env::get('em')->getClassMetadata($entityNS);  
+            $primaryKeyName =$entityObj->getSingleIdentifierFieldName(); //get primary key name  
+            $id=$entityObject[$primaryKeyName]; //get primary key value  
+            if (!empty($id)) {
+                $entityObj=\Env::get('em')->getRepository($entityNS)->find($id);
+                if (!empty($entityObj)) {
+                    \Env::get('em')->remove($entityObj);
+                    \Env::get('em')->flush();
+                    \Message::add('Entity have been deleted sucessfully!');   
+                }
             }
-            
-            // unset password in rest of data
-            //unset($data['email']);
-            //unset($data['password']);
-            // submit rest of data
-            $jd->getJson(
-                $instanceUrl.'/cadmin/index.php?cmd=JsonData&object=checkout&act=saveMasterData',
-                $data
-            );
-            $jd->getJson(
-                $instanceUrl.'/cadmin/index.php?cmd=JsonData&object=user&act=logoutUser'
-            );
-            //
-            \Cx\Core\Csrf\Controller\ComponentController::redirect(\Env::get('cx')->getRequest()->getUrl());
-            // END CUSTOMIZING FOR PAYCLOUD.CH
+            $actionUrl = clone \Env::get('Resolver')->getRequest->getUrl();
+            $actionUrl->setParam('deleteid', null);
+            \Cx\Core\Csrf\Controller\ComponentController::redirect($actionUrl);
         }
     }
     
     public function render(&$isSingle = false) {
+        if (!empty($_GET['add']) 
+            && !empty($this->options['functions']['add'])) {
+            $isSingle = true;
+            return $this->renderFormForEntry(null);
+        }
         $renderObject = $this->object;
         $entityClass = get_class($this->object);
-        if (
-            $this->object instanceof \Cx\Core_Modules\Listing\Model\Entity\DataSet
-            && isset($_GET['editid'])
-        ) {
+        if ($this->object instanceof \Cx\Core_Modules\Listing\Model\Entity\DataSet
+            && isset($_GET['editid'])) {
             $entityClass = $this->object->getDataType();
             $entityId = contrexx_input2raw($_GET['editid']);
             if ($this->object->entryExists($entityId)) {
                 $renderObject = $this->object->getEntry($entityId);
             }
         }
-        
         if ($renderObject instanceof \Cx\Core_Modules\Listing\Model\Entity\DataSet) {
             if (!count($renderObject) || !count(current($renderObject))) {
                 // make this configurable
@@ -169,9 +205,15 @@ class ViewGenerator {
                 $tpl->loadTemplateFile('NoEntries.html');
                 return $tpl->get();
             }
-            $listingController = new \Cx\Core_Modules\Listing\Controller\ListingController($renderObject);
+            $listingController = new \Cx\Core_Modules\Listing\Controller\ListingController($renderObject, array(), $this->options['functions']);
             $renderObject = $listingController->getData();
-            return new \BackendTable($renderObject, $this->options) . '<br />' . $listingController;
+            $backendTable = new \BackendTable($renderObject, $this->options) . '<br />' . $listingController;
+            if (!empty($this->options['functions']['add'])) {
+                $actionUrl = clone \Env::get('cx')->getRequest()->getUrl();
+                $actionUrl->setParam('add', 1);
+                $backendTable .= '<br /><br /><input type="button" name="addEtity" value="Add" onclick="location.href='."'".$actionUrl."&csrf=".\Cx\Core\Csrf\Controller\ComponentController::code()."'".'" />'; 
+            }
+            return $backendTable;
         } else {
             $isSingle = true;
             return $this->renderFormForEntry($entityId);
@@ -179,19 +221,37 @@ class ViewGenerator {
     }
     
     protected function renderFormForEntry($entityId) {
-        $renderObject = $this->object;
+        $renderArray=array();
         $entityClass = get_class($this->object);
+        $actionUrl = clone \Env::get('cx')->getRequest()->getUrl();
         if ($this->object instanceof \Cx\Core_Modules\Listing\Model\Entity\DataSet) {
             $entityClass = $this->object->getDataType();
-            if (!$this->object->entryExists($entityId)) {
-                // no such entry
-                return false;
+            $entityObject = \Env::get('em')->getClassMetadata($entityClass);  
+            $primaryKeyName =$entityObject->getSingleIdentifierFieldName(); //get primary key name
+            if (!empty($_GET['add']) && !empty($this->options['functions']['add'])) {
+                $title='Add Entity';
+                $actionUrl->setParam('add', 1);
+                $getAllField = $entityObject->getColumnNames(); //get all field names  
+                if (empty($getAllField)) return false;
+                foreach($getAllField as $name) {
+                    if ($name!=$primaryKeyName) {
+                        $renderArray[$name]="";    
+                    }
+                }
+            } else {
+                if (!$this->object->entryExists($entityId)) return false;
+                $title='Edit Entity';
+                $actionUrl->setParam('editid', null);
+                $renderObject = $this->object->getEntry($entityId);
+                if (empty($renderObject)) return false;
+                foreach($renderObject as $name=>$value) {
+                    if ($name!=$primaryKeyName) {
+                        $renderArray[$name]=$value;
+                    }
+                }
             }
-            $renderObject = $this->object->getEntry($entityId);
         }
-        $actionUrl = clone \Env::get('cx')->getRequest()->getUrl();
-        $actionUrl->setParam('editid', null);
-        return new FormGenerator($renderObject, $actionUrl, $entityClass, $this->options);
+        return new FormGenerator($renderArray, $actionUrl,$title, $this->options);
     }
     
     public function __toString() {
