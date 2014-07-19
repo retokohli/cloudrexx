9720be6f161574a44e092e85d3591bb94892de38
Thomas Däppen <thomas.daeppen@cloudrexx.com>
2014-07-19 15:33:06 +0000

    Backend (addition to r31827): Added Home to the white-list (fixes CSRF issue when requesting the backend
    git-svn-id: http://svn.contrexxlabs.com/contrexx/trunk@31830 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- core/Csrf/Controller/ComponentController.class.php
+++ core/Csrf/Controller/ComponentController.class.php
@@ -94,7 +94,7 @@ class ComponentController extends \Cx\Core\Core\Model\Entity\SystemComponentCont
         // The CSRF code needn't to be checked in the login module
         // because the user isn't logged in at this point.
         // TODO: Why is upload excluded? The CSRF check doesn't take place in the upload module!
-        if (!empty($plainCmd) && !empty($cmd) and !in_array($plainCmd, array('FileBrowser', 'upload', 'Login'))) {
+        if (!empty($plainCmd) && !empty($cmd) and !in_array($plainCmd, array('FileBrowser', 'upload', 'Login', 'Home'))) {
             // Since language initialization in in the same hook as this
             // and we cannot define the order of module-processing,
             // we need to check if language is already initialized:
