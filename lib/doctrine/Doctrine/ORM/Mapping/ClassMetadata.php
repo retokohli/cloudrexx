a8665f3b75afcac996c8e3d1577621203c6c4899
Sudhir Cdnsol <sudhir.cdnsol@comvation.com>
2014-07-21 11:46:08 +0000

    newInstance method fixed
    git-svn-id: http://svn.contrexxlabs.com/contrexx/trunk@31879 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- lib/doctrine/Doctrine/ORM/Mapping/ClassMetadata.php
+++ lib/doctrine/Doctrine/ORM/Mapping/ClassMetadata.php
@@ -369,8 +369,14 @@ class ClassMetadata extends ClassMetadataInfo
     public function newInstance()
     {
         if ($this->_prototype === null) {
-            $this->_prototype = unserialize(sprintf('O:%d:"%s":0:{}', strlen($this->name), $this->name));
+            // $this->_prototype = unserialize(sprintf('O:%d:"%s":0:{}', strlen($this->name), $this->name));
+            if (PHP_VERSION_ID === 50429 || PHP_VERSION_ID === 50513) {
+                $this->_prototype = $this->reflClass->newInstanceWithoutConstructor();
+            } else {
+                $this->_prototype = unserialize(sprintf('O:%d:"%s":0:{}', strlen($this->name), $this->name));
+            }
         }
         return clone $this->_prototype;
     }
+
 }
