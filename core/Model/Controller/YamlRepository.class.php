ec80260f0b8e5c796c4e7df43167e31d1013f0f5
Thomas Däppen <thomas.daeppen@cloudrexx.com>
2014-07-23 06:38:45 +0000

    Cx\Core\Model: added YamlRepository to manage entities (YamlEntity) in a YAML-file in the file system
    git-svn-id: http://svn.contrexxlabs.com/contrexx/trunk@31932 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

---
+++ core/Model/Controller/YamlRepository.class.php
@@ -0,0 +1,275 @@
+<?php
+/**
+ * YAML Repository
+ *
+ * A entity repository with its storage being in a YAML-file.
+ *
+ * @copyright   CONTREXX CMS - COMVATION AG
+ * @author      Thomas Däppen <thomas.daeppen@comvation.com>
+ * @package     contrexx
+ * @subpackage  core_model
+ */
+
+namespace Cx\Core\Model\Controller;
+
+/**
+ * YAML Repository Exception
+ *
+ * @copyright   CONTREXX CMS - COMVATION AG
+ * @author      Thomas Däppen <thomas.daeppen@comvation.com>
+ * @package     contrexx
+ * @subpackage  core_model
+ */
+class YamlRepositoryException extends \Exception {};
+
+/**
+ * YAML Repository
+ *
+ * A entity repository with its storage being in a YAML-file.
+ *
+ * @copyright   CONTREXX CMS - COMVATION AG
+ * @author      Thomas Däppen <thomas.daeppen@comvation.com>
+ * @package     contrexx
+ * @subpackage  core_model
+ */
+class YamlRepository {
+    /**
+     * Absolute path to the YAML-repository
+     * @var string
+     */
+    protected $repositoryPath;
+
+    /**
+     * All entities currently loaded/attached to the repository
+     * @var array
+     */
+    protected $entities;
+
+    /**
+     * Auto-increment value used for the primary identifier
+     * when adding a new entity to the repository
+     * @var integer
+     */
+    protected $entityAutoIncrement;
+
+    /**
+     * Primary identifier attribute of the entity model
+     * @var string
+     */
+    protected $entityIdentifier;
+    
+    /**
+     * Definition of the unique key attributes of the entity model
+     * @var array
+     */
+    protected $entityUniqueKeys;
+
+    /**
+     * Initialize a YAML-Repository.
+     * Load the entities stored in the file specified by $repositoryPath.
+     * @param   string  $repositoryPath Absolute path in the file system to the YAML-repository
+     */
+    public function __construct($repositoryPath) {
+        if (empty($repositoryPath)) {
+            throw new YamlRepositoryException('No repository specified!');
+        }
+
+        $this->repositoryPath = $repositoryPath;
+        $this->load();
+    }
+
+    /**
+     * Reset the repository. Unload all data.
+     * @access private
+     */
+    private function reset() {
+        $this->entities = array();
+        $this->entityAutoIncrement = 1;
+        $this->entityIdentifier = null;
+        $this->entityUniqueKeys = array();
+    }
+
+    /**
+     * Load repository from file system specified by $this->repositoryPath
+     * @access private
+     */
+    private function load() {
+        $this->reset();
+
+        $repository = \Cx\Core_Modules\Listing\Model\Entity\DataSet::load($this->repositoryPath);
+        if (!$repository->entryExists('meta')) {
+            throw new YamlRepositoryException("Unable to load repository $this->repositoryPath. Repository is missing meta description!");
+        }
+
+        $meta = $repository->getEntry('meta');
+        $this->entityAutoIncrement = $meta['auto_increment'];
+        $this->entityIdentifier = $meta['identifier'];
+        $this->entityUniqueKeys = $meta['unique_keys'];
+
+        if (!$repository->entryExists('data')) {
+            \DBG::msg("YamlRepository: Empty repository $this->repositoryPath loaded.");
+            return;
+        }
+
+        $data = $repository->getEntry('data');
+        foreach ($data as $entity) {
+            if (!($entity instanceof \Cx\Core\Model\Model\Entity\YamlEntity)) {
+                throw new YamlRepositoryException("Unable to load repository $this->repositoryPath. Repository contains invalid entity: ".serialize($entity));
+            }
+            $id = $this->getIdentifierOfEntity($entity);
+            $this->entities[$id] = $entity;
+        }
+    }
+
+    /**
+     * Return all entities from repository
+     */
+    public function findAll() {
+        return $this->entities;
+    }
+
+    /**
+     * Return single entity specified by primary identifier $id
+     * @param   mixed   Value of primary identifier
+     * @return  YamlEntity  Object from repository identified by primary identifier $id
+     */
+    public function findById($id) {
+        if (isset($this->entities[$id])) {
+            return $this->entities[$id];
+        }
+
+        throw new YamlRepositoryException("No entity found by $this->entityIdentifier = $id!");
+    }
+
+    /**
+     * Return an array of entities matching the criterias specified by $criteria.
+     * @param   array   $criteria Arguments to filter the returned entities by
+     * @return  array   Array containing the objects from the repository matching the filter criterias
+     */
+    public function findBy(array $criteria = array()) {
+        $matchedEntities = array();
+
+        foreach ($this->entities as $entity) {
+            foreach ($criteria as $key => $value) {
+                if (call_user_func(array($entity, "get".ucfirst($key))) != $value) {
+                    continue 2;
+                }
+            }
+            $matchedEntities[] = $entity;
+        }
+
+        return $matchedEntities;
+    }
+
+    /**
+     * Add new entity to the repository
+     * Method {@see flush()} must triggered to persist the entity in the repository.
+     * @param   YamlEntity  Entity to add to the repository
+     */
+    public function add($entity) {
+        if (!($entity instanceof \Cx\Core\Model\Model\Entity\YamlEntity)) {
+            throw new YamlRepositoryException("Entity must be of type YamlEntity!");
+        }
+
+        // check if entity already has an ID and if so if there is already
+        // an other entity present in the repository with the same ID.
+        $id = $this->getIdentifierOfEntity($entity);
+        if ($id && isset($this->entities[$id])) {
+            throw new \YamlRepositoryException("Unable to add new entity to repository. A entity by the specified primary identifier {$this->getIdentifierOfEntity($entity)} is already present in the repository.");
+        } else {
+            // assign new ID to entity
+            call_user_func(array($entity, "set".ucfirst($this->entityIdentifier)), $this->getNewEntityIdentifier());
+        }
+        $this->validate($entity);
+        $this->entities[$this->getIdentifierOfEntity($entity)] = $entity;
+    }
+
+    /**
+     * Remove entity from repository.
+     * Entity will not be removed until the method {@see flush()} has been triggered.
+     * @param   YamlEntity $entity The entity to remove from the repository.
+     */
+    public function remove($entity) {
+        unset($this->entities[$this->getIdentifierOfEntity($entity)]);
+    }
+
+    /**
+     * Flush the current state of the repository into the file system.
+     * @todo    Make thread safe
+     */
+    public function flush() {
+        $entitiesToPersist = array();
+        foreach ($this->entities as $entity) {
+            // Validation must be done before checking for virtual entities.
+            // As even virtual entities must comply with the unique-key restrictions.
+            $this->validate($entity);
+            if ($entity->isVirtual()) {
+                continue;
+            }
+            $entitiesToPersist[] = $entity;
+        }
+
+        $dataSet = new \Cx\Core_Modules\Listing\Model\Entity\DataSet();
+        $dataSet->add('data', $entitiesToPersist);
+        $dataSet->add('meta', $this->getMetaDefinition());
+        $dataSet->save($this->repositoryPath);
+    }
+
+    /**
+     * Generate meta definition to be stored in the repository
+     * @access private
+     * @return array    Meta definition
+     */
+    private function getMetaDefinition() {
+        return array(
+            'auto_increment'    => $this->entityAutoIncrement,
+            'identifier'        => $this->entityIdentifier,
+            'unique_keys'       => $this->entityUniqueKeys,
+        );
+    }
+
+    /**
+     * Validate the entity to comply with any unique-key constraints
+     * @param   YamlEntity The entity to validate
+     * @access private
+     */
+    private function validate($entity) {
+        foreach ($this->entityUniqueKeys as $uniqueKey) {
+            $value = call_user_func(array($entity, "get".ucfirst($uniqueKey)));
+            $matchedEntities = $this->findBy(array($uniqueKey => $value));
+
+            // if no other entities use the same uniqueKey then we're good
+            if (!count($matchedEntities)) {
+                continue;
+            }
+            if (count($matchedEntities) == 1) {
+                // it's fine in case the entity with the same uniqueKey is actually the entity we are validating
+                if ($this->getIdentifierOfEntity(current($matchedEntities)) == $this->getIdentifierOfEntity($entity)) {
+                    continue;
+                }
+            }
+
+            throw new YamlRepositoryException("Attribute $uniqueKey ($value) of entity #{$this->getIdentifierOfEntity($entity)} must be unique!");
+        }
+    }
+
+    /**
+     * Generate and get a new primary identifier
+     * @return integer New primary identifier
+     * @access private
+     */
+    private function getNewEntityIdentifier() {
+        return ++$this->entityAutoIncrement;
+    }
+
+    /**
+     * Get the primary identifier of an entity
+     * @param   YamlEntity $entity The entity of which to get the primary identifier from
+     * @access private
+     * @return  integer The primary identifier of the supplied entity
+     */
+    private function getIdentifierOfEntity($entity) {
+        return call_user_func(array($entity, "get".ucfirst($this->entityIdentifier)));
+    }
+}
+
---
+++ core/Model/Model/Entity/YamlEntity.class.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * YAML Entity
+ *
+ * A entity that is handled by a YAML repository.
+ *
+ * @copyright   CONTREXX CMS - COMVATION AG
+ * @author      Thomas Däppen <thomas.daeppen@comvation.com>
+ * @package     contrexx
+ * @subpackage  core_model
+ */
+
+namespace Cx\Core\Model\Model\Entity;
+
+/**
+ * YAML Entity Exception
+ *
+ * @copyright   CONTREXX CMS - COMVATION AG
+ * @author      Thomas Däppen <thomas.daeppen@comvation.com>
+ * @package     contrexx
+ * @subpackage  core_model
+ */
+class YamlEntityException extends \Exception {};
+
+/**
+ * YAML Entity
+ *
+ * A entity that is handled by a YAML repository.
+ *
+ * @copyright   CONTREXX CMS - COMVATION AG
+ * @author      Thomas Däppen <thomas.daeppen@comvation.com>
+ * @package     contrexx
+ * @subpackage  core_model
+ */
+class YamlEntity {
+    /**
+     * Defines if an entity is virtual and therefore not persistable.
+     * Defaults to FALSE - not virtual.
+     * @var boolean
+     */
+    protected $virtual = false;
+
+    /**
+     * Set the virtuality of the entity
+     * @param   boolean $virtual    TRUE to set the entity as virtual or otherwise to FALSE 
+     */
+    public function setVirtual($virtual) {
+        $this->virtual = $virtual;
+    }
+
+    /**
+     * Returns the virtuality of the entity
+     * @return  boolean TRUE if the entity is virtual, otherwise FALSE
+     */
+    public function isVirtual() {
+        return $this->virtual;
+    }
+}
+
