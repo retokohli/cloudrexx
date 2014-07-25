0d518ae77d77d99747c7aabff2da1d748cde5768
Santhanam ss4u <santhanam@softsolutions4u.info>
2014-07-25 09:59:42 +0000

    Cache : Fixed the issue that memcache could not activated in backend.
    git-svn-id: http://svn.contrexxlabs.com/contrexx/trunk@31996 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- core_modules/Cache/Controller/CacheLib.class.php
+++ core_modules/Cache/Controller/CacheLib.class.php
@@ -167,9 +167,9 @@ class CacheLib
         }
         
         // Memcache
-        if (
-            $this->isInstalled(self::CACHE_ENGINE_MEMCACHE) &&
-            $_CONFIG['cacheUserCache'] == self::CACHE_ENGINE_MEMCACHE
+        if (   $this->isInstalled(self::CACHE_ENGINE_MEMCACHE)
+            && (\Env::get('cx')->getMode() == \Cx\Core\Core\Controller\Cx::MODE_BACKEND
+            || $_CONFIG['cacheUserCache'] == self::CACHE_ENGINE_MEMCACHE)
         ) {
             $memcacheConfiguration = $this->getMemcacheConfiguration();
             unset($this->memcache); // needed for reinitialization
