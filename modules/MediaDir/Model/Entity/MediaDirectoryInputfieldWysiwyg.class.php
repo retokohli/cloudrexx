9a46cd95161dc41894e3e82d0a05b249a1417772
Santhanam ss4u <santhanam@softsolutions4u.info>
2014-08-01 12:59:41 +0000

    Mediadir: fixed the issue with the multi-language fields(text , text area and wysiwyg editor) #2252
    git-svn-id: http://svn.contrexxlabs.com/contrexx/trunk@32113 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- modules/MediaDir/Model/Entity/MediaDirectoryInputfieldWysiwyg.class.php
+++ modules/MediaDir/Model/Entity/MediaDirectoryInputfieldWysiwyg.class.php
@@ -99,7 +99,7 @@ class MediaDirectoryInputfieldWysiwyg extends \Cx\Modules\MediaDir\Controller\Me
 
                 if ($objInit->mode == 'backend') {
                     //$strInputfield = '<span id="'.$this->moduleName.'Inputfield_'.$intId.'_Minimized" style="display: block;"><textarea name="'.$this->moduleName.'Inputfield['.$intId.'][0]" id="'.$this->moduleName.'Inputfield_'.$intId.'_0" style="width: 300px; height: 60px;" onfocus="this.select();" />'.$arrValue[0].'</textarea>&nbsp;<a href="javascript:ExpandMinimizeMultiple(\''.$intId.'\');">'.$_ARRAYLANG['TXT_MEDIADIR_MORE'].'&nbsp;&raquo;</a></span>';
-                    $strInputfield = '<span id="'.$this->moduleNameLC.'Inputfield_'.$intId.'_ELEMENT_Minimized" style="display: block;">'.new \Cx\Core\Wysiwyg\Wysiwyg($this->moduleNameLC.'Inputfield['.$intId.'][0]', $arrValue[0]).'&nbsp;<a href="javascript:updateWysiwygEditor(\''.$intId.'\', \'ELEMENT\');javascript:ExpandMinimizeMultiple(\''.$intId.'\', \'ELEMENT\');">'.$_ARRAYLANG['TXT_MEDIADIR_MORE'].'&nbsp;&raquo;</a></span>';
+                    $strInputfield = '<span id="'.$this->moduleNameLC.'Inputfield_'.$intId.'_ELEMENT_Minimized" style="display: block;">'.new \Cx\Core\Wysiwyg\Wysiwyg($this->moduleNameLC.'Inputfield['.$intId.'][0]', $arrValue[0]).'&nbsp;<a href="javascript:javascript:ExpandMinimizeMultiple(\''.$intId.'\', \'ELEMENT\');">'.$_ARRAYLANG['TXT_MEDIADIR_MORE'].'&nbsp;&raquo;</a></span>';
 
                     $strInputfield .= '<span id="'.$this->moduleNameLC.'Inputfield_'.$intId.'_ELEMENT_Expanded" style="display: none;">';
                     foreach ($this->arrFrontendLanguages as $key => $arrLang) {
@@ -107,30 +107,28 @@ class MediaDirectoryInputfieldWysiwyg extends \Cx\Modules\MediaDir\Controller\Me
 
                         $strInputfield .=  new \Cx\Core\Wysiwyg\Wysiwyg($this->moduleNameLC.'Inputfield['.$intId.']['.$intLangId.']', $arrValue[$intLangId]).'&nbsp;'.$arrLang['name'].'<br />';
                     }
-                    $strInputfield .=  "&nbsp;<a href=\"javascript:updateWysiwygEditor('".$intId."', 'ELEMENT');javascript:ExpandMinimizeMultiple('".$intId."', 'ELEMENT');\">&laquo;&nbsp;".$_ARRAYLANG['TXT_MEDIADIR_MINIMIZE']."</a>";
-                    
-                    $strInputfield .= '<textarea name="'.$this->moduleNameLC.'Inputfield['.$intId.'][old]" style="display: none;" onfocus="this.select();" />'.$arrValue[0].'</textarea>';
+                    $strInputfield .=  "&nbsp;<a href=\"javascript:javascript:ExpandMinimizeMultiple('".$intId."', 'ELEMENT');\">&laquo;&nbsp;".$_ARRAYLANG['TXT_MEDIADIR_MINIMIZE']."</a>";
+                                        
                     $strInputfield .= '</span>';
                 } else {
                     //$strInputfield = '<textarea name="'.$this->moduleName.'Inputfield['.$intId.'][0]" id="'.$this->moduleName.'Inputfield_'.$intId.'_0" class="'.$this->moduleName.'InputfieldTextarea" onfocus="this.select();" />'.$arrValue[0].'</textarea>';
                     //$strInputfield = '<span id="'.$this->moduleName.'Inputfield_'.$intId.'_Minimized" style="display: block; float: left;" class="'.$this->moduleName.'GroupMultilang"><textarea name="'.$this->moduleName.'Inputfield['.$intId.'][0]" id="'.$this->moduleName.'Inputfield_'.$intId.'_0" class="'.$this->moduleName.'InputfieldTextarea '.$strInfoClass.'" '.$arrInfoValue[0].' onfocus="this.select();" />'.$arrValue[0].'</textarea>&nbsp;<a href="javascript:ExpandMinimize(\''.$intId.'\');">'.$_ARRAYLANG['TXT_MEDIADIR_MORE'].'&nbsp;&raquo;</a></span>';
-                    $strInputfield = '<span class="editorFix"><span id="'.$this->moduleNameLC.'Inputfield_'.$intId.'_Minimized" style="display: block;" class="'.$this->moduleNameLC.'GroupMultilang">'.new \Cx\Core\Wysiwyg\Wysiwyg($this->moduleNameLC.'Inputfield['.$intId.'][0]', $arrValue[0]).'&nbsp;<a href="javascript:updateWysiwygEditor(\''.$intId.'\', \'ELEMENT\');javascript:ExpandMinimizeMultiple(\''.$intId.'\', \'ELEMENT\');">'.$_ARRAYLANG['TXT_MEDIADIR_MORE'].'&nbsp;&raquo;</a></span>';
+                    $strInputfield = '<span class="editorFix"><span id="'.$this->moduleNameLC.'Inputfield_'.$intId.'_Minimized" style="display: block;" class="'.$this->moduleNameLC.'GroupMultilang">'.new \Cx\Core\Wysiwyg\Wysiwyg($this->moduleNameLC.'Inputfield['.$intId.'][0]', $arrValue[0]).'&nbsp;<a href="javascript:javascript:ExpandMinimizeMultiple(\''.$intId.'\', \'ELEMENT\');">'.$_ARRAYLANG['TXT_MEDIADIR_MORE'].'&nbsp;&raquo;</a></span>';
 
                     $strInputfield .= '<span id="'.$this->moduleNameLC.'Inputfield_'.$intId.'_Expanded" style="display: none;" class="'.$this->moduleNameLC.'GroupMultilang">';
                     foreach ($this->arrFrontendLanguages as $key => $arrLang) {
                         $intLangId = $arrLang['id'];
 
                         if (($key+1) == count($this->arrFrontendLanguages)) {
-                            $minimize = "&nbsp;<a href=\"javascript:updateWysiwygEditor('".$intId."', 'ELEMENT');javascript:ExpandMinimizeMultiple('".$intId."', 'ELEMENT');\">&laquo;&nbsp;".$_ARRAYLANG['TXT_MEDIADIR_MINIMIZE']."</a>";
+                            $minimize = "&nbsp;<a href=\"javascript:javascript:ExpandMinimizeMultiple('".$intId."', 'ELEMENT');\">&laquo;&nbsp;".$_ARRAYLANG['TXT_MEDIADIR_MINIMIZE']."</a>";
                         } else {
                             $minimize = "";
                         }
 
                         //$strInputfield .= '<textarea name="'.$this->moduleName.'Inputfield['.$intId.']['.$intLangId.']" id="'.$this->moduleName.'Inputfield_'.$intId.'_'.$intLangId.'" class="'.$this->moduleName.'InputfieldTextarea '.$strInfoClass.'" '.$arrInfoValue[$intLangId].' onfocus="this.select();" />'.$arrValue[$intLangId].'</textarea>&nbsp;'.$arrLang['name'].'<a href="javascript:ExpandMinimize(\''.$intId.'\');">&nbsp;'.$minimize.'</a><br />';
 
-                        $strInputfield .=  new \Cx\Core\Wysiwyg\Wysiwyg($this->moduleNameLC.'Inputfield['.$intId.']['.$intLangId.']', $arrValue[$intLangId]).'&nbsp;'.$arrLang['name'].'<a href="javascript:updateWysiwygEditor(\''.$intId.'\', \'ELEMENT\');javascript:ExpandMinimizeMultiple(\''.$intId.'\', \'ELEMENT\');">&nbsp;'.$minimize.'</a><br />';
-                    }
-                    $strInputfield .= '<textarea name="'.$this->moduleNameLC.'Inputfield['.$intId.'][old]" style="display: none;" onfocus="this.select();" />'.$arrValue[0].'</textarea>';
+                        $strInputfield .=  new \Cx\Core\Wysiwyg\Wysiwyg($this->moduleNameLC.'Inputfield['.$intId.']['.$intLangId.']', $arrValue[$intLangId]).'&nbsp;'.$arrLang['name'].'<a href="javascript:javascript:ExpandMinimizeMultiple(\''.$intId.'\', \'ELEMENT\');">&nbsp;'.$minimize.'</a><br />';
+                    }                    
                     $strInputfield .= '</span></span>';
                 }
                 return $strInputfield;
@@ -248,23 +246,8 @@ class MediaDirectoryInputfieldWysiwyg extends \Cx\Modules\MediaDir\Controller\Me
 
 
     function getJavascriptCheck()
-    {
-        $strJavascriptCheck = <<<EOF
-
-            case 'wysiwyg':
-                //we update here beause of bug #2126 
-                updateWysiwygEditor(
-                    //faster than a regex in JS
-                    parseInt(
-                        inputFields[field][0].substr(
-                            inputFields[field][0].indexOf('[')+1
-                        )
-                    ),
-                    'ELEMENT'
-                );
-                break;
-EOF;
-        return $strJavascriptCheck;
+    {             
+        return null;
     }
 
 
