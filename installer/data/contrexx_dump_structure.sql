295727eedbc75da4e9cd30eb887f9427349b2767
svnbot <svnbot@comvation.com>
2014-08-11 13:00:16 +0000

    (AUTOMATIC) update trunk's installer dumps from contrexxlabs_contrexx_trunk
    git-svn-id: http://svn.contrexxlabs.com/contrexx/trunk@32235 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- installer/data/contrexx_dump_structure.sql
+++ installer/data/contrexx_dump_structure.sql
@@ -338,7 +338,7 @@ CREATE TABLE `contrexx_core_module_multisite_website` (
   `name` varchar(200) NOT NULL,
   `codeBase` varchar(50) NOT NULL,
   `language` varchar(50) NOT NULL,
-  `status` int(11) NOT NULL,
+  `status` varchar(15) NOT NULL,
   `websiteServiceServerId` int(11) default NULL,
   `secretKey` varchar(255) NOT NULL,
   `ipAddress` varchar(45) NOT NULL,
