67918e00de3efe8d1cea92885687542d4731850c
Santhanam ss4u <santhanam@softsolutions4u.info>
2014-08-12 13:03:00 +0000

    Workbench: - Fixed the issue of finding the testing folder. - Added test case for add website.
    git-svn-id: http://svn.contrexxlabs.com/contrexx/trunk@32245 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

---
+++ core_modules/MultiSite/Testing/UnitTest/WebsiteTest.php
@@ -0,0 +1,15 @@
+<?php
+include_once(ASCMS_TEST_PATH.'/testCases/DoctrineTestCase.php');
+
+/**
+ * Test cases for the class Cx\Core_Modules\MultiSite\Model\Entity\Website
+ *
+ * @author ss4u <ss4u.comvation@gmail.com>
+ */
+class WebsiteTest extends DoctrineTestCase {
+    
+    function testAddWebsite() {
+        
+        return true;        
+    }
+}
