1db15f62f235a53de8f16a64898f8b2450e7a2ee
Thomas Däppen <thomas.daeppen@cloudrexx.com>
2014-08-17 18:39:16 +0000

    Cx\Modules\Pim: added new module Pim
    git-svn-id: http://svn.contrexxlabs.com/contrexx/trunk@32289 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

---
+++ modules/Pim/Controller/DefaultController.class.php
@@ -0,0 +1,94 @@
+<?php
+
+/**
+ * DefaultController
+ *
+ * @copyright   Comvation AG
+ * @author      Comvation Development Team <info@comvation.com>
+ * @package     contrexx
+ * @subpackage  module_pim
+ */
+
+namespace Cx\Modules\Pim\Controller;
+
+/**
+ * 
+ * DefaultController for displaying all the orders.
+ *
+ * @copyright   Comvation AG
+ * @author      Comvation Development Team <info@comvation.com>
+ * @package     contrexx
+ * @subpackage  module_pim
+ */
+class DefaultController extends \Cx\Core\Core\Model\Entity\Controller {
+    
+    /**
+     * Em instance
+     * @var \Doctrine\ORM\EntityManager em
+     */
+    protected $em;
+    
+    /**
+     * Sigma template instance
+     * @var Cx\Core\Html\Sigma  $template
+     */
+    protected $template;
+    
+    /**
+     * ProductRepository instance 
+     * @var \Cx\Modules\Product\Model\Repository\ProductRepository $productRepository
+     */
+    protected $productRepository;
+    
+    /**
+     * module name
+     * @var string $moduleName
+     */
+    public $moduleName = 'Pim';
+    
+    /**
+     * module name for language placeholder
+     * @var string $moduleNameLang
+     */
+    public $moduleNameLang = 'PIM';
+
+    /**
+     * Controller for the Backend Orders views
+     * 
+     * @param \Cx\Core\Core\Model\Entity\SystemComponentController $systemComponentController the system component controller object
+     * @param \Cx\Core\Core\Controller\Cx                          $cx                        the cx object
+     * @param \Cx\Core\Html\Sigma                                  $template                  the template object
+     * @param string                                               $submenu                   the submenu name
+     */
+    public function __construct(\Cx\Core\Core\Model\Entity\SystemComponentController $systemComponentController, \Cx\Core\Core\Controller\Cx $cx, \Cx\Core\Html\Sigma $template, $submenu = null) {
+        parent::__construct($systemComponentController, $cx);
+        
+        $this->template          = $template;
+        $this->em                = $this->cx->getDb()->getEntityManager();
+        $this->productRepository   = $this->em->getRepository('Cx\Modules\Pim\Model\Entity\Product');
+        
+        $this->showProducts();
+    }
+
+    public function showProducts() 
+    {
+        global $_ARRAYLANG;
+        
+        $products = $this->productRepository->findAll();
+        if (empty($products)) {
+            $products = new \Cx\Modules\Pim\Model\Entity\Product();
+        }
+        $view = new \Cx\Core\Html\Controller\ViewGenerator($products, array(
+            'header'    => $_ARRAYLANG['TXT_MODULE_ORDER_ACT_DEFAULT'],
+            'functions' => array(
+                'add'       => true,
+                'edit'      => true,
+                'delete'    => true,
+                'sorting'   => true,
+                'paging'    => true,
+                'filtering' => false,
+                )
+            ));
+        $this->template->setVariable('PRODUCTS_CONTENT', $view->render());
+    }
+}
---
+++ modules/Pim/lang/en/backend.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @copyright   CONTREXX CMS - COMVATION AG
+ * @author      Comvation Development Team <info@comvation.com>
+ * @access      public
+ * @package     contrexx
+ * @subpackage  modules_pim
+ */
+global $_ARRAYLANG;
+
+// Here come the ACTs:
+$_ARRAYLANG['TXT_MODULE_PIM_PRODUCTS'] = 'Products';
+$_ARRAYLANG['TXT_MODULE_PIM_ACT_DEFAULT'] = 'Products';
