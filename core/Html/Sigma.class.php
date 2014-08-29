4f30325464e2da36d1ce92238d933a2a279378db
Thomas Däppen <thomas.daeppen@cloudrexx.com>
2014-08-29 10:15:10 +0000

    Cx\Core\Html: migrated HTML_Template_Sigma to version 1.3.0 (PHP5 compatability)
    git-svn-id: http://svn.contrexxlabs.com/contrexx/trunk@32549 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- core/Html/Sigma.class.php
+++ core/Html/Sigma.class.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * Sigma
  *
@@ -9,11 +8,6 @@
  * @subpackage  core_html
  */
 
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 namespace Cx\Core\Html;
 
 /**
@@ -21,35 +15,78 @@ namespace Cx\Core\Html;
  *
  * @copyright   CONTREXX CMS - COMVATION AG
  * @author Michael Ritter <michael.ritter@comvation.com>
+ * @author Thomas Däppen <thomas.daeppen@comvation.com>
  * @package     contrexx
  * @subpackage  core_html
  */
 class Sigma extends \HTML_Template_Sigma {
+
+    protected $restoreFileRoot = null;
     
     public function __construct($root = '', $cacheRoot = '') {
         parent::__construct($root, $cacheRoot);
         $this->setErrorHandling(PEAR_ERROR_DIE);
     }
     
-    /**
-     * Reads the file and returns its content
-     *
-     * @param    string    filename
-     * @return   string    file content (or error object)
-     * @access   private
-     */
-    function _getFile($filename)
-    {
-        $filename = \Env::get('ClassLoader')->getFilePath($filename);
-        if (!($fh = @fopen($filename, 'r'))) {
-            return $this->raiseError($this->errorMessage(SIGMA_TPL_NOT_FOUND, $filename), SIGMA_TPL_NOT_FOUND);
-        }
-        $content = fread($fh, filesize($filename));
-        fclose($fh);
-        return $content;
-    }
-    
     function getRoot() {
         return $this->fileRoot;
+    }
+
+    function loadTemplateFile($filename, $removeUnknownVariables = true, $removeEmptyBlocks = true) {
+        $this->mapCustomizing($filename);
+        $return = parent::loadTemplateFile($filename, $removeUnknownVariables, $removeEmptyBlocks);
+        $this->unmapCustomizing();
+        return $return;
+    }
+
+    function addBlockfile($placeholder, $block, $filename) {
+        $this->mapCustomizing($filename);
+        $return = parent::addBlockfile($placeholder, $block, $filename);
+        $this->unmapCustomizing();
+        return $return;
+    }
+
+    function replaceBlockfile($block, $filename, $keepContent = false) {
+        $this->mapCustomizing($filename);
+        $return = parent::replaceBlockfile($block, $filename, $keepContent);
+        $this->unmapCustomizing();
+        return $return;
+    }
+
+    /**
+     * The customizing mechanism does not apply to method _getCached().
+     * Therefore it is not overwritten.
+     */
+    /** function _getCached($filename, $block = '__global__', $placeholder = '') {} */
+
+    /**
+     * Detects if $filename is customized.
+     * If so, it causes \HTML_Template_Sigma to load the customized version
+     * of the file.
+     * @param   string $filename    The filename passed by the overwritten methods of \HTML_Template_Sigma
+     */
+    protected function mapCustomizing($filename) {
+        // check if template is customized
+        $filePath = \Env::get('ClassLoader')->getFilePath($this->fileRoot . $filename);
+        if ($filePath != $this->fileRoot . $filename) {
+            // backup original fileRoot
+            $this->restoreFileRoot = $this->fileRoot;
+
+            // point fileRoot to customizing path
+            $newFileRoot = substr($filePath, 0, -strlen($filename));
+            $this->fileRoot = $newFileRoot;
+        }
+    }
+
+    /**
+     * In case a customized version of a file has been loaded.
+     * This method does revert \HTML_Template_Sigma so that is will
+     * continue to load regular files without customizings.
+     */
+    protected function unmapCustomizing() {
+        if ($this->restoreFileRoot) {
+            $this->fileRoot = $this->restoreFileRoot;
+            $this->restoreFileRoot = null;
+        }
     }
 }
