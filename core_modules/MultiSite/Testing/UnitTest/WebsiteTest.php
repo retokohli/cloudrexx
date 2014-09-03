64523b51db39cee0c97927d3ed8a183b3c216e0d
Santhanam ss4u <santhanam@softsolutions4u.info>
2014-09-03 12:38:27 +0000

    PHPUnit Test - Added test case for the \Cx\Core_Modules\MultiSite\Model\Entity\Website class.
    git-svn-id: http://svn.contrexxlabs.com/contrexx/trunk@32631 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- core_modules/MultiSite/Testing/UnitTest/WebsiteTest.php
+++ core_modules/MultiSite/Testing/UnitTest/WebsiteTest.php
@@ -10,6 +10,25 @@ class WebsiteTest extends DoctrineTestCase {
     
     function testAddWebsite() {
         
-        return true;        
+        $objFWUser   = \FWUser::getFWUserObject();
+        $objUser     = $objFWUser->objUser->getUser(contrexx_input2raw(1));        
+        $websiteName = 'mytestwebsite'. \Cx\Core\Setting\Controller\Setting::getValue('multiSiteDomain');
+        
+        // check website is manager
+        $basepath = \Cx\Core\Setting\Controller\Setting::getValue('websitePath');
+        $websiteServiceServer = null;
+        
+        if (\Cx\Core\Setting\Controller\Setting::getValue('mode') == ComponentController::MODE_MANAGER) {
+            //get default service server
+            $defaultWebsiteServiceServer = \Env::get('em')->getRepository('Cx\Core_Modules\MultiSite\Model\Entity\WebsiteServiceServer')
+            ->findBy(array('isDefault' => 1));
+            $websiteServiceServer = $defaultWebsiteServiceServer[0];
+        }
+        
+        $objWebsite = new \Cx\Core_Modules\MultiSite\Model\Entity\Website($basepath, $websiteName, $websiteServiceServer, $objUser, false);
+        \Env::get('em')->persist($objWebsite);        
+        \Env::get('em')->flush();
+        
+        return $objWebsite->setup(array('subscription' => 'Trail'));        
     }
 }
