2e0f2b406902aa8ccb704663b9af1b392f203ebb
Santhanam ss4u <santhanam@softsolutions4u.info>
2014-09-04 10:18:01 +0000

    PHPUnit : Added test commends to the Multisite add Website test.
    git-svn-id: http://svn.contrexxlabs.com/contrexx/trunk@32640 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- core_modules/MultiSite/Testing/UnitTest/WebsiteTest.php
+++ core_modules/MultiSite/Testing/UnitTest/WebsiteTest.php
@@ -8,27 +8,42 @@ include_once(ASCMS_TEST_PATH.'/testCases/DoctrineTestCase.php');
  */
 class WebsiteTest extends DoctrineTestCase {
     
+    /**
+     * Test function to adding a website
+     */
     function testAddWebsite() {
         
         $objFWUser   = \FWUser::getFWUserObject();
-        $objUser     = $objFWUser->objUser->getUser(contrexx_input2raw(1));        
-        $websiteName = 'mytestwebsite'. \Cx\Core\Setting\Controller\Setting::getValue('multiSiteDomain');
+        
+        /**
+         * Creating website requires 2 parameters
+         * 1. User object 
+         * 2. Website name
+         */
+        $objUser     = $objFWUser->objUser->getUser(contrexx_input2raw(1));        // get the user object
+        $websiteName = 'mytestwebsite'. \Cx\Core\Setting\Controller\Setting::getValue('multiSiteDomain'); // website name + multisite domain name
         
         // check website is manager
         $basepath = \Cx\Core\Setting\Controller\Setting::getValue('websitePath');
         $websiteServiceServer = null;
         
-        if (\Cx\Core\Setting\Controller\Setting::getValue('mode') == ComponentController::MODE_MANAGER) {
+        // When current server is a website manager then it will request the service server to create the website.
+        // So we need to fetch the default website website service server.
+        if (\Cx\Core\Setting\Controller\Setting::getValue('mode') == \Cx\Core_Modules\MultiSite\Controller\ComponentController::MODE_MANAGER) {
             //get default service server
-            $defaultWebsiteServiceServer = \Env::get('em')->getRepository('Cx\Core_Modules\MultiSite\Model\Entity\WebsiteServiceServer')
+            $defaultWebsiteServiceServer = self::$em->getRepository('Cx\Core_Modules\MultiSite\Model\Entity\WebsiteServiceServer')
             ->findBy(array('isDefault' => 1));
             $websiteServiceServer = $defaultWebsiteServiceServer[0];
         }
         
+        /**
+         * create new website object and flush into database
+         */
         $objWebsite = new \Cx\Core_Modules\MultiSite\Model\Entity\Website($basepath, $websiteName, $websiteServiceServer, $objUser, false);
-        \Env::get('em')->persist($objWebsite);        
-        \Env::get('em')->flush();
+        self::$em->persist($objWebsite);        
+        self::$em->flush();
         
+        // configure the website
         return $objWebsite->setup(array('subscription' => 'Trail'));        
     }
 }
