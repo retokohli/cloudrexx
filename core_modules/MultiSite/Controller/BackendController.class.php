fe697a724a9d4593918d8a4ae40e138fabae9a91
Sathish ss4u <sathishkumar@softsolutions4u.info>
2014-09-08 13:06:52 +0000

    MultiSite: Populate Config to Service Services
    git-svn-id: http://svn.contrexxlabs.com/contrexx/trunk@32715 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- core_modules/MultiSite/Controller/BackendController.class.php
+++ core_modules/MultiSite/Controller/BackendController.class.php
@@ -435,7 +435,7 @@ class BackendController extends \Cx\Core\Core\Model\Entity\SystemComponentBacken
 //       Once implemented, those options must be read-only or not getting listed at all
                     $_ARRAYLANG['TXT_CORE_MODULE_MULTISITE'] .($mode == ComponentController::MODE_SERVICE ? ' - TODO: The configuration options below must be set remotely by the Website Manager! (except for option "Subscription controller")' : ''),
                     'Setup',
-                    'TXT_CORE_MODULE_MULTISITE_', $mode == ComponentController::MODE_SERVICE ? 'true': ''
+                    'TXT_CORE_MODULE_MULTISITE_', $mode == ComponentController::MODE_SERVICE ? true : ''
                 );
             }
             
