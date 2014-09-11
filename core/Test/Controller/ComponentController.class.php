6d510de664e883356d7b55cccf717179fab6e31e
Santhanam ss4u <santhanam@softsolutions4u.info>
2014-09-11 07:20:18 +0000

    PHPUnit: Added Routing component and added component controller class for Test component.
    git-svn-id: http://svn.contrexxlabs.com/contrexx/trunk@32750 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

---
+++ core/Test/Controller/ComponentController.class.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Main controller for Test
+ * 
+ * @copyright   Comvation AG
+ * @author      Project Team SS4U <info@comvation.com>
+ * @package     contrexx
+ * @subpackage  core_test
+ */
+
+namespace Cx\Core\Test\Controller;
+
+/** 
+ * Main controller for Test
+ * 
+ * @copyright   Comvation AG
+ * @author      Project Team SS4U <info@comvation.com>
+ * @package     contrexx
+ * @subpackage  core_test
+ */
+class ComponentController extends \Cx\Core\Core\Model\Entity\SystemComponentController {
+    public function getControllerClasses() {
+        // Return an empty array here to let the component handler know that there
+        // does not exist a backend, nor a frontend controller of this component.
+        return array();
+    }    
+}
