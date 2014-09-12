3ba3a690b86d6d1a5e6007a6773d7fe404658ac1
Santhanam ss4u <santhanam@softsolutions4u.info>
2014-09-12 10:53:08 +0000

    ContentManager : bug fix #2279 (It is not possible to add aliases to a page).
    git-svn-id: http://svn.contrexxlabs.com/contrexx/branches/contrexx_3_2@32782 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- core/ContentManager/Controller/JsonPage.class.php
+++ core/ContentManager/Controller/JsonPage.class.php
@@ -495,6 +495,26 @@ class JsonPage implements JsonAdapter {
         
         $this->em->flush();
         
+        // bug fix #2279
+        // could not save alias after running $this->em->clear()
+        // Aliases are only updated in the editing mode.
+        if (!empty($pageArray)) {
+            // Only users with publish rights can create aliases.
+            if (\Permission::checkAccess(115, 'static', true) && \Permission::checkAccess(78, 'static', true)) {
+                // Aliases are updated after persist.
+                $data = array();
+                $data['alias'] = $pageArray['alias'];
+                $aliases = $page->getAliases();
+                $page->updateFromArray($data);
+                if ($aliases != $page->getAliases()) {
+                    $reload = true;
+                }
+            } else {
+                // Users without permission shouldn't see the aliasses anyway
+                //$this->messages[] = $_CORELANG['TXT_CORE_ALIAS_CREATION_DENIED'];
+            }
+        }
+        
         // this fixes log version number skipping
         $this->em->clear();
         $logs = $this->logRepo->getLogEntries($page);
@@ -576,24 +596,6 @@ class JsonPage implements JsonAdapter {
             $this->em->flush();
         }
         
-        // Aliases are only updated in the editing mode.
-        if (!empty($pageArray)) {
-            // Only users with publish rights can create aliases.
-            if (\Permission::checkAccess(115, 'static', true) && \Permission::checkAccess(78, 'static', true)) {
-                // Aliases are updated after persist.
-                $data = array();
-                $data['alias'] = $pageArray['alias'];
-                $aliases = $page->getAliases();
-                $page->updateFromArray($data);
-                if ($aliases != $page->getAliases()) {
-                    $reload = true;
-                }
-            } else {
-                // Users without permission shouldn't see the aliasses anyway
-                //$this->messages[] = $_CORELANG['TXT_CORE_ALIAS_CREATION_DENIED'];
-            }
-        }
-
         // get version
         // if it is a draft, don't take the last one
         $version = $page->getVersion()->getVersion();
