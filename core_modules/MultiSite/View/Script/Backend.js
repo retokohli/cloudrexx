fe62815963864fa9dd7997cbb40060b07bad783a
Rajesh ss4u <rajeshp@softsolutions4u.info>
2014-10-01 13:44:45 +0000

    MultiSite: Website Status,implemented loading animation and overlay effect
    git-svn-id: http://svn.contrexxlabs.com/contrexx/trunk@33010 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- core_modules/MultiSite/View/Script/Backend.js
+++ core_modules/MultiSite/View/Script/Backend.js
@@ -1,5 +1,7 @@
 (function($) {
     cx.ready(function() {
+        $('#instance_table').append('<div id ="load-lock"></div>');
+        
         $(".defaultCodeBase").change(function() {
             domainUrl = cx.variables.get('baseUrl', 'MultiSite') + cx.variables.get('cadminPath', 'contrexx') + "index.php?cmd=JsonData&object=MultiSite&act=updateDefaultCodeBase";
             cx.jQuery.ajax({
@@ -10,22 +12,27 @@
                 },
                 type: "POST",
                
-                success: function(response) { 
+                success: function(response) {
                     if (response.data) {
                         cx.tools.StatusMessage.showMessage(response.data, null,2000);
                     }
                 }
-                
+
             });
         });
-         $('.changeWebsiteStatus').focus(function() {
+        $('.changeWebsiteStatus').focus(function() {
             //Store old value
-            $(this).data('lastValue',$(this).val());
+            $(this).data('lastValue', $(this).val());
+            
+            cx.bind("loadingStart", cx.lock, "websitestatus");
+            cx.bind("loadingEnd", cx.unlock, "websitestatus");
             //changing dropdown value
         }).change(function() {
             domainUrl = cx.variables.get('baseUrl', 'MultiSite') + cx.variables.get('cadminPath', 'contrexx') + "index.php?cmd=JsonData&object=MultiSite&act=updateWebsiteState";
             var websiteDetails = $(this).attr('data-websiteDetails').split("-");
             if (confirm("Please confirm to change the state of website " + websiteDetails[1] + ' to ' + $(this).val())) {
+                cx.trigger("loadingStart", "websitestatus", {});
+                cx.tools.StatusMessage.showMessage("<div id=\"loading\">" + cx.jQuery('#loading').html() + "</div>");
                 cx.jQuery.ajax({
                     dataType: "json",
                     url: domainUrl,
@@ -38,12 +45,26 @@
                         if (response.data) {
                             cx.tools.StatusMessage.showMessage(response.data, null, 2000);
                         }
+                        cx.trigger("loadingEnd", "websitestatus", {});
                     }
 
                 });
             }else{
-                 $(this).val($(this).data('lastValue'));
+                $(this).val($(this).data('lastValue'));
             }
         });
+/**
+         * Locks the website status in order to prevent user input
+         */
+        cx.lock = function() {
+            cx.jQuery("#load-lock").show();
+        };
+
+        /**
+         * Unlocks the website status in order to allow user input
+         */
+        cx.unlock = function() {
+           cx.jQuery("#load-lock").hide();
+        };
     });
 })(jQuery);
