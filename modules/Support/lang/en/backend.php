f211e00692ee2e0bcf56d80ff711a05027f7b828
Raj ss4u <rajkumar@softsolutions4u.info>
2014-10-29 13:56:15 +0000

    Support:  - new support component created.  - implemented the feedback form.
    git-svn-id: http://svn.contrexxlabs.com/contrexx/trunk@33278 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

---
+++ modules/Support/lang/en/backend.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * @copyright   CONTREXX CMS - COMVATION AG
+ * @author      Project Team SS4U <info@comvation.com>
+ * @access      public
+ * @package     contrexx
+ * @subpackage  modules_support
+ */
+global $_ARRAYLANG;
+
+//Here come the ACTs:
+$_ARRAYLANG['TXT_MODULE_SUPPORT_ACT_DEFAULT'] = 'Support';
+
+//feedback
+$_ARRAYLANG['TXT_SUPPORT_FEEDBACK_SELECT_FEEDBACK'] = 'Please select';
+$_ARRAYLANG['TXT_SUPPORT_FEEDBACK_SUBJECT'] = 'Concerning';
+$_ARRAYLANG['TXT_SUPPORT_FEEDBACK_COMMENTS'] = 'Comments';
+$_ARRAYLANG['TXT_SUPPORT_FEEDBACK_BUG_REPORT'] = 'Report error';
+$_ARRAYLANG['TXT_SUPPORT_FEEDBACK_FEATURE_REQUEST'] = 'Request feature';
+$_ARRAYLANG['TXT_SUPPORT_FEEDBACK_HAVE_QUESTION'] = 'Question' ;
+$_ARRAYLANG['TXT_SUPPORT_FEEDBACK'] = 'Feedback';
+$_ARRAYLANG['TXT_SUPPORT_FEEDBACK_TYPE'] = 'Type';
+$_ARRAYLANG['TXT_SUPPORT_FEEDBACK_FAQ'] = 'Frequently asked questions (FAQ)';
+$_ARRAYLANG['TXT_SUPPORT_FEEDBACK_BUG_REPORT'] = 'Report error';
+$_ARRAYLANG['TXT_SUPPORT_FEEDBACK_FEATURE_REQUEST'] = 'Request feature';
+$_ARRAYLANG['TXT_SUPPORT_FEEDBACK_HAVE_QUESTION'] = 'Question' ;
+$_ARRAYLANG['TXT_SUPPORT_ERROR_MSG_FIELDS_EMPTY'] = 'Please enter values in the mandatory fields!';
+$_ARRAYLANG['TXT_SUPPORT_FEEDBACK_EMAIL_SEND_FAILED'] = 'Request could not be sent.';
+$_ARRAYLANG['TXT_SUPPORT_FEEDBACK_EMAIL_SEND_SUCESSFULLY'] = 'Request successfully sent.';
+$_ARRAYLANG['TXT_SUPPORT_CONTACT_TITLE'] = 'Contact details';
+$_ARRAYLANG['TXT_SUPPORT_USER_NAME'] = 'User Name';
+$_ARRAYLANG['TXT_SUPPORT_USER_EMAIL'] = 'e-mail';
+$_ARRAYLANG['TXT_SUPPORT_FEEDBACK_MAIL'] = 'Feedback Mail';
+$_ARRAYLANG['TXT_SUPPORT_FEEDBACK_COMMENTS'] = 'Comments';
+$_ARRAYLANG['TXT_SUPPORT_FEEDBACK_URL'] = 'URL';
+$_ARRAYLANG['TXT_SUPPORT_FEEDBACK_TOPIC'] = 'Feedback topic';
+
