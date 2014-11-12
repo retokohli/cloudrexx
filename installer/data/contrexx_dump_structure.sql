66be9965de93fa3ea8a5d16cb36686c61a6a016b
svnbot <svnbot@comvation.com>
2014-11-12 13:50:31 +0000

    (AUTOMATIC) update trunk's installer dumps from contrexxlabs_contrexx_trunk
    git-svn-id: http://svn.contrexxlabs.com/contrexx/trunk@33490 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- installer/data/contrexx_dump_structure.sql
+++ installer/data/contrexx_dump_structure.sql
@@ -3548,15 +3548,14 @@ CREATE TABLE `contrexx_module_order_invoice` (
 SET character_set_client = @saved_cs_client;
 SET @saved_cs_client     = @@character_set_client;
 SET character_set_client = utf8;
-CREATE TABLE `contrexx_module_order_invoiceitem` (
+CREATE TABLE `contrexx_module_order_invoice_item` (
   `id` int(11) NOT NULL auto_increment,
   `invoice_id` int(11) default NULL,
   `description` varchar(255) NOT NULL,
   `price` decimal(10,0) NOT NULL,
   PRIMARY KEY  (`id`),
   KEY `IDX_ED28B9B52989F1FD` (`invoice_id`),
-  CONSTRAINT `contrexx_module_order_invoiceitem_ibfk_2` FOREIGN KEY (`invoice_id`) REFERENCES `contrexx_module_order_invoice` (`id`),
-  CONSTRAINT `contrexx_module_order_invoiceitem_ibfk_1` FOREIGN KEY (`invoice_id`) REFERENCES `contrexx_module_order_invoice` (`id`)
+  CONSTRAINT `contrexx_module_order_invoice_item_ibfk_1` FOREIGN KEY (`invoice_id`) REFERENCES `contrexx_module_order_invoice` (`id`)
 ) ENGINE=InnoDB;
 SET character_set_client = @saved_cs_client;
 SET @saved_cs_client     = @@character_set_client;
