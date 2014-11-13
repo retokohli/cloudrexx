db7e1090a71f7ca006c83cd848a3c06e3edac90d
Michael Ritter <michael.ritter@cloudrexx.com>
2014-11-13 10:03:52 +0000

    core myAdmin - update - Disabled RSS feed on dashboard (we don't want ET to call home)
    git-svn-id: http://svn.contrexxlabs.com/contrexx/branches/contrexx_4_0@33499 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- core/myAdmin.class.php
+++ core/myAdmin.class.php
@@ -176,7 +176,7 @@ class myAdminManager {
             $objTemplate->hideBlock('stats_javascript');
         }
 
-        $objRss = new XML_RSS('http://www.contrexx.com/feed/news_headlines_de.xml?version=' . $_CONFIG['coreCmsVersion']);
+        /*$objRss = new XML_RSS('http://www.contrexx.com/feed/news_headlines_de.xml?version=' . $_CONFIG['coreCmsVersion']);
         $objRss->parse();
         $arrItems = $objRss->getItems();
         if (!empty($arrItems) && ($_CONFIG['dashboardNews'] == 'on')) {
@@ -185,9 +185,9 @@ class myAdminManager {
                 'NEWS_LINK'  => $arrItems[0]['link'],
             ));
             $objTemplate->parse('news');
-        } else {
+        } else {*/
             $objTemplate->hideBlock('news');
-        }
+        //}
     }
 
     private function getStatistics()
