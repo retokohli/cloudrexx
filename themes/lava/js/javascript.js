c67ca7c96f7df9a4a3f850d9659c9d3d7af73a62
ss4u <comvation@softsolutions4u.info>
2014-11-18 12:32:34 +0000

    View: lava  - Imported lava template into themes folder.  - Migrated html pages into bootstrap version 3.2.0
    git-svn-id: http://svn.contrexxlabs.com/contrexx/trunk@33578 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

---
+++ themes/lava/js/javascript.js
@@ -0,0 +1,28 @@
+(function ($) {    
+    $(document).ready(function () {        
+        $('ul.sf-menu').superfish({
+            delay: 100
+        });
+        $(".starter_active").addClass("active");
+        if ($('#subnavigation li.level-2').length == 0) {
+            $('#subnavigation').hide();
+        }
+        $('#subnavigation li.active').parents('ul.menu').siblings('a').removeClass('inactive').addClass('active').parent().removeClass('inactive').addClass('active');
+        if ($('#subnavigation li.level-2:visible:last ul:visible').length == 0) {
+            $('#subnavigation li.level-2 > a:visible:last').addClass('no-border');
+        }
+        if ($('#subnavigation li:visible:last').hasClass('level-3')) {
+            $('#subnavigation li:visible:last').parent().addClass('no-border');
+        }
+        if ($('#subnavigation li:visible:last').hasClass('level-4')) {
+            $('#subnavigation li:visible:last').parent().parent().parent().addClass('no-border');
+        }
+        $('#header').carousel({
+            interval: 6000
+        });
+        if($('#subnavigation li.level-2').length != 0){
+            $('h3.submenu-title').css('display','block');
+            $('.menu-break').css('display','block');
+        }
+    });
+})(jQuery);
