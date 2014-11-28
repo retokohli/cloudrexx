80a1734b8dfd38b26ebbcf08ad5833edbb4ec491
Raj ss4u <rajkumar@softsolutions4u.info>
2014-11-28 13:15:34 +0000

    NetManager  - Added new component as NetManager.  - Moved the section Administration > Global Configuration > Domains to Administration > Domains .
    git-svn-id: http://svn.contrexxlabs.com/contrexx/trunk@33762 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

---
+++ core/NetManager/lang/de/backend.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * This is the english language file for backend mode.
+ * This file is included by Contrexx and all entries are set as placeholder
+ * values for backend ACT template by SystemComponentBackendController
+ *
+ * @copyright   CONTREXX CMS - Comvation AG Thun
+ * @author      Project Team SS4U <info@comvation.com>
+ * @package     contrexx
+ * @subpackage  core_netmanager
+ */
+
+global $_ARRAYLANG;
+
+// Let's start with module info:
+$_ARRAYLANG['TXT_CORE_NETMANAGER'] = 'Domains';
+$_ARRAYLANG['TXT_CORE_NETMANAGER_DESCRIPTION'] = 'This module shows the all Domain Alias';
+
+// Here come the ACTs:
+$_ARRAYLANG['TXT_CORE_NETMANAGER_ACT_DEFAULT'] = 'Overview';
+$_ARRAYLANG['TXT_CORE_NETMANAGER_ENTITY'] = 'Domain';
