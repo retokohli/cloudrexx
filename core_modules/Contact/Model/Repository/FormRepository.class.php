5b62c3304e20e8825a3f841b71143c263a2b8788
Sathish ss4u <sathishkumar@softsolutions4u.info>
2014-12-03 12:00:49 +0000

    Contact: Implement model event triggers(Bug fix)
     - Implemented doctrine model for database table contrexx_module_contact_form. 
     - Before triggering the model events, instantiate the doctrine entity of the related contact form and passed it as event argument.
    
    
    git-svn-id: http://svn.contrexxlabs.com/contrexx/trunk@33827 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

---
+++ core_modules/Contact/Model/Repository/FormRepository.class.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Class FormRepository
+ *
+ * @copyright   CONTREXX CMS - COMVATION AG
+ * @author      ss4u <ss4ugroup@gmail.com>
+ * @package     contrexx
+ * @subpackage  coremodule_contact
+ */
+
+namespace Cx\Core_Modules\Contact\Model\Repository;
+
+/**
+ * Class FormRepository
+ * 
+ * @copyright   CONTREXX CMS - COMVATION AG
+ * @author      ss4u <ss4ugroup@gmail.com>
+ * @package     contrexx
+ * @subpackage  coremodule_contact
+ */
+class FormRepository extends \Doctrine\ORM\EntityRepository {}
+
