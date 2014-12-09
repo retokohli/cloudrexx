b6ebe893bb0328c34d20397b53ecd1804ee17f0d
ss4u <comvation@softsolutions4u.info>
2014-12-09 13:35:59 +0000

    View: Create theme Highwind -Worked on the mobile navigation css and home page design.
    git-svn-id: http://svn.contrexxlabs.com/contrexx/trunk@33943 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- themes/highwind/javascript.js
+++ themes/highwind/javascript.js
@@ -1,31 +1,6 @@
 (function($) {
     $(document).ready(function() {
-        /*Header trasparent START here*/
-//        var fadeSpeed = 10, fadeTo = 0.6, topDistance = 30;
-//        var topbarME = function() {
-//            $('.header').fadeTo(fadeSpeed, 1);
-//        }, topbarML = function() {
-//            $('.header').fadeTo(fadeSpeed, fadeTo);
-//        };
-//        var inside = false;
-//        $(window).scroll(function() {
-//            position = $(window).scrollTop();
-//            if (position > topDistance && !inside) {
-//
-//                topbarML();
-//                $('.header').bind('mouseenter', topbarME);
-//                $('.header').bind('mouseleave', topbarML);
-//                inside = true;
-//            }
-//
-//            else if (position < topDistance) {
-//                topbarME();
-//                $('.header').unbind('mouseenter', topbarME);
-//                $('.header').unbind('mouseleave', topbarML);
-//                inside = false;
-//            }
-//        });
-        /*Header trasparent END here*/
+
         /*Mobile naviagtion START here*/
         function navHide() {
             $('section, footer').animate({right: '0'}, 10);
@@ -87,49 +62,70 @@
         /* Metanavigation End */
         /* Navigation Start */
         $('#navigation-wrapper .navigation > li').hover(function() {
-            
-        $('.navigation li.level-1:first').addClass('first');
-        $('.navigation li.level-1:last').addClass('last');
-        $('.navigation li.level-1').children('ul').each(function() {
-            $(this).children('li:last').addClass('last');
-        });
-        $('.navigation li.active').parents('ul.menu').siblings('a').removeClass('starter_normal').addClass('starter_active').parent().removeClass('starter_normal').addClass('starter_active');
-        if ($('#subnavigation li.level-2').length == 0) {
-            $('#subnavigation').hide();
-        }
-        $('#subnavigation li.active').parents('ul.menu').siblings('a').removeClass('inactive').addClass('active').parent().removeClass('inactive').addClass('active');
-        if ($('#subnavigation li.level-2:visible:last ul:visible').length == 0) {
-            $('#subnavigation li.level-2 > a:visible:last').addClass('no-border');
-        }
-        if ($('#subnavigation li:visible:last').hasClass('level-3')) {
-            $('#subnavigation li:visible:last').parent().addClass('no-border');
-        }
-        if ($('#subnavigation li:visible:last').hasClass('level-4')) {
-            $('#subnavigation li:visible:last').parent().parent().parent().addClass('no-border');
-        }
-        /* Navigation End */
-        /* Shop Start */
-        $('#shop-categories li a:last').addClass('last');
-        $('#shop-currencies a:last').addClass('last');
-        /* Shop End */
-        $('#back-top').click(function() {
-            $('html, body').animate({
-                scrollTop: 0
-            }, 400);
-            return false;
-        });
-        $(".login-toggle a.open").click(function() {
-            $('.login-toggle').removeClass('open');
-            $('.login-toggle').addClass('open');
-            $(".login").slideDown();
-            return false;
-        });
-        $(".login-toggle a.hidetext").click(function() {
-            $('.login-toggle').removeClass('open');
-            $(".login").slideUp();
-            return false;
+
+            $('.navigation li.level-1:first').addClass('first');
+            $('.navigation li.level-1:last').addClass('last');
+            $('.navigation li.level-1').children('ul').each(function() {
+                $(this).children('li:last').addClass('last');
+            });
+            $('.navigation li.active').parents('ul.menu').siblings('a').removeClass('starter_normal').addClass('starter_active').parent().removeClass('starter_normal').addClass('starter_active');
+            if ($('#subnavigation li.level-2').length == 0) {
+                $('#subnavigation').hide();
+            }
+            $('#subnavigation li.active').parents('ul.menu').siblings('a').removeClass('inactive').addClass('active').parent().removeClass('inactive').addClass('active');
+            if ($('#subnavigation li.level-2:visible:last ul:visible').length == 0) {
+                $('#subnavigation li.level-2 > a:visible:last').addClass('no-border');
+            }
+            if ($('#subnavigation li:visible:last').hasClass('level-3')) {
+                $('#subnavigation li:visible:last').parent().addClass('no-border');
+            }
+            if ($('#subnavigation li:visible:last').hasClass('level-4')) {
+                $('#subnavigation li:visible:last').parent().parent().parent().addClass('no-border');
+            }
+            /* Navigation End */
+            /* Shop Start */
+            $('#shop-categories li a:last').addClass('last');
+            $('#shop-currencies a:last').addClass('last');
+            /* Shop End */
+            $('#back-top').click(function() {
+                $('html, body').animate({
+                    scrollTop: 0
+                }, 400);
+                return false;
+            });
+            $(".login-toggle a.open").click(function() {
+                $('.login-toggle').removeClass('open');
+                $('.login-toggle').addClass('open');
+                $(".login").slideDown();
+                return false;
+            });
+            $(".login-toggle a.hidetext").click(function() {
+                $('.login-toggle').removeClass('open');
+                $(".login").slideUp();
+                return false;
+            });
+
         });
         // Add .parent class to appropriate menu items
-        jQuery( 'ul.menu' ).parent().addClass( 'parent' );
+        $('ul.menu').parent().addClass('parent');
+
+        /**
+         * Scroll to top
+         */
+        $(function() {
+            $('.back-to-top').click(function() {
+                $('body,html').animate({
+                    scrollTop: 0
+                }, 800);
+                return false;
+            });
+        });
+        $(window).scroll(function() {
+            if ($(this).scrollTop() > 200) {
+                $('.back-to-top').fadeIn();
+            } else {
+                $('.back-to-top').fadeOut();
+            }
+        });
     });
 })(jQuery);
