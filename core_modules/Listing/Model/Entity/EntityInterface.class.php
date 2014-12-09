aee83e796c58bad447f37d38ed5f084a3e71807c
Vinoth ss4u <vinoth@softsolutions4u.info>
2014-12-09 12:32:18 +0000

    MultiSite: Synchronization between manager and service servers -Implemented the functionality in EntityInterface::export(), to export as entity object from the given array.
    git-svn-id: http://svn.contrexxlabs.com/contrexx/trunk@33938 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- core_modules/Listing/Model/Entity/EntityInterface.class.php
+++ core_modules/Listing/Model/Entity/EntityInterface.class.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * Entity interface
+ * EntityInterface Class
  *
  * @copyright   CONTREXX CMS - COMVATION AG
  * @author      Project Team SS4U <info@comvation.com>
@@ -12,7 +12,10 @@
 namespace Cx\Core_Modules\Listing\Model\Entity;
 
 /**
- * Entity interface
+ * EntityInterface Class
+ * 
+ * This class used to convert the entity objects into array and array
+ * into entity objects
  *
  * @copyright   CONTREXX CMS - COMVATION AG
  * @author      Project Team SS4U <info@comvation.com>
@@ -31,69 +34,88 @@ class EntityInterface implements Exportable, Importable {
     }
 
     /**
-     * To Export the array as objects.
+     * This function is used to convert the array into entity objects.
      * 
-     * @param array $data
+     * @param  array $data
      * 
-     * @return array return as object array
+     * @return array return as entity object array
      */
     public function export($data) {
-        $associationMappings = \Env::get('em')->getClassMetadata($this->entityClass)->getAssociationMappings();
-
-        $repo = \Env::get('em')->getRepository($this->entityClass);
-        foreach ($data as $key => $value) {
-            $entity = null;
-            if (!empty($value['id'])) {
-                $entity = $repo->findOneBy(array('id' => $value['id']));
-            }
-            if (!$entity) {
-                $entity = new $this->entityClass();
-            }
-            foreach ($value as $field => $methodValue) {
-                if (array_key_exists($field, $associationMappings)) {
-                    continue;
-                }
-
-                $methodNameToSetAssociation = 'set' . ucfirst($field);
-                $entity->$methodNameToSetAssociation($methodValue);
-            }
-            $entities[] = $entity;
+        if (empty($data)) {
+            return;
         }
+        $em = \Env::get('em');
+        $entityClassMetaData = $em->getClassMetadata($this->entityClass);
+        $repository = $em->getRepository($this->entityClass);
+
+        $entityArray = current($data);
+        
+        $entityObj = null;
+        $primaryKeyName = $entityClassMetaData->getSingleIdentifierFieldName();
+
+        if (!empty($entityArray[$primaryKeyName])) {
+            $entityObj = $repository->findOneBy(array($primaryKeyName => $entityArray[$primaryKeyName]));
+        }
+        if (!$entityObj) {
+            $entityObj = new $this->entityClass();
+        }
+        //get the association mappings
+        $associationMappings = $entityClassMetaData->getAssociationMappings();
+        //class methods
+        $classMethods = get_class_methods($entityObj);
+
+        foreach ($entityArray as $field => $methodValue) {
+            if (!array_key_exists($field, $associationMappings)) {
+                $entityObj->{'set' . ucfirst($field)}($methodValue);
+            } else {
+                if ($entityClassMetaData->isSingleValuedAssociation($field) && in_array('set' . ucfirst($field), $classMethods)) {
+                    $col = $associationMappings[$field]['joinColumns'][0]['referencedColumnName'];
+                    $association = \Env::get('em')->getRepository($associationMappings[$field]['targetEntity'])->findOneBy(array($col => $methodValue));
+                    if ($association) {
+                        $entityObj->{'set' . ucfirst($field)}($association);
+                    }
+                }
+            }
+        }
+        $entities[] = $entityObj;
         return $entities;
     }
 
     /**
      * set the entity class
      * 
-     * @param type $entityClass
+     * @param string $entityClass
      */
     public function setEntityClass($entityClass) {
         $this->entityClass = $entityClass;
     }
 
     /**
-     * To import an array for the given entity object
+     * This function is used to convert the entity object into array.
      * 
-     * @param  object $object
-     * @return array return as array
+     * @param  object $object entity object
+     * @return array  return as array
      */
     public function import($object) {
+
+        if (!is_object($object)) {
+            return;
+        }
 
         $em = \Env::get('em');
         $data = array();
 
         $entityClassMetaData = $em->getClassMetadata(get_class($object));
         $associationMappings = $entityClassMetaData->getAssociationMappings();
-
-        foreach ($associationMappings as $field => $associationMapping) {
-            if ($entityClassMetaData->isSingleValuedAssociation($field) && in_array('set' . ucfirst($field), get_class_methods($object))
-            ) {
-                $associationObj = $object->{'get' . ucfirst($field)}();
-                $primaryKeyName = $em->getClassMetadata(get_class($associationObj))->getSingleIdentifierFieldName();
-                $data[$field] = $associationObj->{'get' . ucfirst($primaryKeyName)}();
+        if (!empty($associationMappings)) {
+            foreach ($associationMappings as $field => $associationMapping) {
+                if ($entityClassMetaData->isSingleValuedAssociation($field) && in_array('set' . ucfirst($field), get_class_methods($object))) {
+                    $associationObj = $object->{'get' . ucfirst($field)}();
+                    $primaryKeyName = $em->getClassMetadata(get_class($associationObj))->getSingleIdentifierFieldName();
+                    $data[$field] = $associationObj->{'get' . ucfirst($primaryKeyName)}();
+                }
             }
         }
-
         foreach ($entityClassMetaData->getColumnNames() as $column) {
             $field = $entityClassMetaData->getFieldName($column);
             $value = $entityClassMetaData->getFieldValue($object, $field);
