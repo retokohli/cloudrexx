960ad1f0db208bf86b46f33cb140d7c9e684dc28
ss4u <comvation@softsolutions4u.info>
2014-12-11 13:00:51 +0000

    view: klasik
     - Done css styles for static subpages.
     - Done script and css design for desktop and mobile view navigation.
     - Added preview.gif image.
    
    git-svn-id: http://svn.contrexxlabs.com/contrexx/trunk@34001 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- themes/klasik/javascript.js
+++ themes/klasik/javascript.js
@@ -1,5 +1,14 @@
 (function($) {
     $(document).ready(function() {
+        jQuery("ul.sf-menu").supersubs({
+            minWidth: 10, // requires em unit.
+            maxWidth: 13, // requires em unit.
+            extraWidth: 3	// extra width can ensure lines don't sometimes turn over due to slight browser differences in how they round-off values
+                    // due to slight rounding differences and font-family 
+        }).superfish();
+        jQuery('#topnav').tinyNav({
+            active: 'starter_active'
+        });
         /*Header trasparent START here*/
         var fadeSpeed = 10, fadeTo = 0.6, topDistance = 30;
         var topbarME = function() {
