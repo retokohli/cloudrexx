fa7ac69cc919ae419ecd02a8f08551724ef33453
Robin Glauser <robin.glauser@cloudrexx.com>
2015-03-30 14:54:32 +0000

    Access: Adding docblocks to AccessEventListener
    git-svn-id: http://svn.contrexxlabs.com/contrexx/trunk@35099 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- core_modules/Access/Model/Event/AccessEventListener.class.php
+++ core_modules/Access/Model/Event/AccessEventListener.class.php
@@ -1,8 +1,13 @@
 <?php
+
 /**
- * @copyright   Comvation AG 
- * @author Robin Glauser <robin.glauser@comvation.com>
+ * Class AccessEventListener
+ *
+ * @copyright   CONTREXX CMS - Comvation AG Thun
+ * @author      Robin Glauser <robin.glauser@comvation.com>
  * @package     contrexx
+ * @subpackage  coremodule_access
+ * @version     1.0.0
  */
 
 namespace Cx\Core_Modules\Access\Model\Event;
@@ -12,35 +17,45 @@ use Cx\Core\Event\Model\Entity\EventListener;
 use Cx\Core_Modules\MediaBrowser\Controller\MediaBrowserConfiguration;
 use Cx\Core\Model\Model\Entity\MediaType;
 
-class AccessEventListener implements EventListener {
+class AccessEventListener implements EventListener
+{
 
     /**
      * @var Cx
      */
     protected $cx;
 
-    function __construct(Cx $cx)
-    {
+    /**
+     * @param Cx $cx
+     */
+    function __construct(Cx $cx) {
         $this->cx = $cx;
     }
 
-
-    public function onEvent($eventName, array $eventArgs)
-    {
+    /**
+     * @param       $eventName
+     * @param array $eventArgs
+     */
+    public function onEvent($eventName, array $eventArgs) {
         $this->$eventName(current($eventArgs));
     }
 
-    public function LoadMediaTypes(MediaBrowserConfiguration $mediaBrowserConfiguration)
-    {
+    /**
+     * @param MediaBrowserConfiguration $mediaBrowserConfiguration
+     */
+    public function LoadMediaTypes(
+        MediaBrowserConfiguration $mediaBrowserConfiguration
+    ) {
         global $_ARRAYLANG;
-        $mediaType = new MediaType();
-        $mediaType->setName('access');
-        $mediaType->setHumanName($_ARRAYLANG['TXT_USER_ADMINISTRATION']);
-        $mediaType->setDirectory(array(
-            $this->cx->getWebsiteImagesAccessPath(),
-            $this->cx->getWebsiteImagesAccessWebPath(),
-        ));
-        $mediaType->setAccessIds(array(18));
+        $mediaType = new MediaType(
+            'access',
+            $_ARRAYLANG['TXT_USER_ADMINISTRATION'],
+            array(
+                $this->cx->getWebsiteImagesAccessPath(),
+                $this->cx->getWebsiteImagesAccessWebPath(),
+            ),
+            array(18)
+        );
         $mediaBrowserConfiguration->addMediaType($mediaType);
     }
 
