7864932ece7ca01d9cbf2360fab618aee32c161d
Robin Glauser <robin.glauser@cloudrexx.com>
2015-03-30 14:59:51 +0000

    Media: Adding docblocks to MediaEventListener
    git-svn-id: http://svn.contrexxlabs.com/contrexx/trunk@35101 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- core_modules/Media/Model/Event/MediaEventListener.class.php
+++ core_modules/Media/Model/Event/MediaEventListener.class.php
@@ -1,41 +1,49 @@
 <?php
-/**
- * @copyright   Comvation AG 
- * @author Robin Glauser <robin.glauser@comvation.com>
- * @package     contrexx
- */
-
 
 /**
- * @copyright   Comvation AG
- * @author Robin Glauser <robin.glauser@comvation.com>
+ * Class MediaEventListener
+ *
+ * @copyright   CONTREXX CMS - Comvation AG Thun
+ * @author      Robin Glauser <robin.glauser@comvation.com>
  * @package     contrexx
+ * @subpackage  coremodule_media
+ * @version     1.0.0
  */
 
 namespace Cx\Core_Modules\Media\Model\Event;
 
 use Cx\Core\Core\Controller\Cx;
+use Cx\Core\Event\Model\Entity\EventListener;
 use Cx\Core_Modules\MediaBrowser\Controller\MediaBrowserConfiguration;
 use Cx\Core\Model\Model\Entity\MediaType;
 
-class MediaEventListener implements \Cx\Core\Event\Model\Entity\EventListener {
+class MediaEventListener implements EventListener {
 
     /**
      * @var Cx
      */
     protected $cx;
 
+    /**
+     * @param Cx $cx
+     */
     function __construct(Cx $cx)
     {
         $this->cx = $cx;
     }
 
-
+    /**
+     * @param       $eventName
+     * @param array $eventArgs
+     */
     public function onEvent($eventName, array $eventArgs)
     {
         $this->$eventName(current($eventArgs));
     }
 
+    /**
+     * @param MediaBrowserConfiguration $mediaBrowserConfiguration
+     */
     public function LoadMediaTypes(MediaBrowserConfiguration $mediaBrowserConfiguration)
     {
         global $_ARRAYLANG;
