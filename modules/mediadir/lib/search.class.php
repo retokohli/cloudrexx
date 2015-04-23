1b4428ffc9b84039de770806dafd83ecfeb3beec
Robin Glauser <robin.glauser@cloudrexx.com>
2015-04-23 15:39:08 +0000

    Mediadir: Fixed #2552
    git-svn-id: http://svn.contrexxlabs.com/contrexx/branches/contrexx_4_0@35278 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- modules/mediadir/lib/search.class.php
+++ modules/mediadir/lib/search.class.php
@@ -91,9 +91,9 @@ class mediaDirectorySearch extends mediaDirectoryLibrary
 <form method="get" action="$strSearchFormAction">
 <input name="section" value="$strSectionValue" type="hidden" />
 <input name="type" value="normal" type="hidden" />
-<input type="hidden" value="Suchen" name="search">
 $strSearchFormCmd
-<input name="term" class="$strInputfieldSearch" value="$strSearchFormTerm" onfocus="this.select();" type="text" /><input class="$strButtonSearch" value="$strTextSearch" name="search" type="submit">
+<input name="term" class="$strInputfieldSearch" value="$strSearchFormTerm" onfocus="this.select();" type="text" />
+<input class="$strButtonSearch" value="$strTextSearch" name="search" type="submit">
 </form>
 </div>
 EOF;
@@ -105,9 +105,9 @@ EOF;
 <div class="normal">
 <input name="section" value="$strSectionValue" type="hidden" />
 <input name="type" value="exp" type="hidden" />
-<input type="hidden" value="Suchen" name="search">
 $strSearchFormCmd
-<p><label>$strTextSearchterm</label><input name="term" class="$strInputfieldSearch" value="$strSearchFormTerm" onfocus="this.select();" type="text" /></p>
+<p><label>$strTextSearchterm</label>
+<input name="term" class="$strInputfieldSearch" value="$strSearchFormTerm" onfocus="this.select();" type="text" /></p>
 </div>
 <div class="expanded">
 $strExpandedInputfields
