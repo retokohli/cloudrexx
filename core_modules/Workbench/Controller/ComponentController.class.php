fbddfc45871f2ffc1bf81d8fec6d441daa708afb
Dhivya ss4u <dhivya@softsolutions4u.info>
2015-06-01 12:15:01 +0000

    Permissions: Command mode
    - Implemented SystemComponentController::hasAccessToExecuteCommand() to check the access permission.
    - Removed parent::executeCommand($command, $arguments) in the ComponentController::executeCommand() for all Component. 
    
    git-svn-id: http://svn.contrexxlabs.com/contrexx/trunk@35536 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- core_modules/Workbench/Controller/ComponentController.class.php
+++ core_modules/Workbench/Controller/ComponentController.class.php
@@ -39,9 +39,8 @@ class ComponentController extends \Cx\Core\Core\Model\Entity\SystemComponentCont
         }
     }
 
-    public function executeCommand($command, $arguments) {
-        parent::executeCommand($command, $arguments);
-        
+    public function executeCommand($command, $arguments)
+    {
         new \Cx\Core_Modules\Workbench\Model\Entity\ConsoleInterface(array_merge(array($command), $arguments), $this->cx);
     }
 
