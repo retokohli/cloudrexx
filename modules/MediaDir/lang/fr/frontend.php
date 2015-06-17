adecec21dbbf1b479c81772214aa58ec4b8bc70e
Adrian Berger <adrian.berger@cloudrexx.com>
2015-06-17 14:42:35 +0000

    MediaDir: fix tipo mistake "googe" and remove redundant placeholder
    git-svn-id: http://svn.contrexxlabs.com/contrexx/trunk@35643 5ce06e5d-2b28-0410-aa6a-b3cd9cf52d8a

--- modules/MediaDir/lang/fr/frontend.php
+++ modules/MediaDir/lang/fr/frontend.php
@@ -47,7 +47,7 @@ $_ARRAYLANG['TXT_MEDIADIR_VOTING_CORRUPT'] = "Bewertung fehlgeschlagen. Entweder
 $_ARRAYLANG['TXT_MEDIADIR_AT'] = "um";
 $_ARRAYLANG['TXT_MEDIADIR_COMMENT_ADD_SUCCESSFULL'] = "Ihr Kommentar wurde erfolgreich hinzugefügt.";
 $_ARRAYLANG['TXT_MEDIADIR_COMMENT_ADD_CORRUPT'] = "Beim Übermitteln der Daten ist eine Fehler aufgetreten, daher konnte ihr Kommentar nicht gespeichert werden.";
-$_ARRAYLANG['TXT_MEDIADIR_GOOGLEMAPS_LINK'] = "Link zu Googe Maps";
+$_ARRAYLANG['TXT_MEDIADIR_GOOGLEMAPS_LINK'] = "Link zu Google Maps";
 
 $_ARRAYLANG['TXT_MEDIADIR_SUCCESSFULLY_ADDED'] = "ajouté avec succès.";
 $_ARRAYLANG['TXT_MEDIADIR_CORRUPT_ADDED'] = "N'a pu être ajouté.";
@@ -61,7 +61,6 @@ $_ARRAYLANG['TXT_MEDIADIR_COULD_NOT_SEND_ACTIVATION_MAIL'] = "Registrierungsfehl
 $_ARRAYLANG['TXT_MEDIADIR_COULD_NOT_SEND_EMAIL'] = "Konnte keine Bestätigung per Mail senden.<br />Für weitere Informationen wenden Sie sich bitte an den Administrator %EMAIL%. ";
 $_ARRAYLANG['TXT_MEDIADIR_ID_OR_SEARCH_TERM'] = "Eintrag suchen (ID oder Suchbegriff)";
 
-$_ARRAYLANG['TXT_MEDIADIR_GOOGLEMAPS_LINK'] = 'Link zu Googe Maps';
 $_ARRAYLANG['TXT_MEDIADIR_DISPLAYNAME'] = "Nom d'affichage";
 $_ARRAYLANG['TXT_MEDIADIR_TRANSLATION_STATUS'] = 'Freigegebene Profilsprachen';
 $_ARRAYLANG['TXT_MEDIADIR_READY_TO_CONFIRM'] = 'Freigeben';
