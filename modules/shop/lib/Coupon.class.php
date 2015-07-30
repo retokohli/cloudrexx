a5a0fd797231a9be0e07334f673d7076dfa9313f
sebastian-brand <sebastian.brand@cloudrexx.com>
2015-07-30 17:14:02 +0200

    Shop (bugfix #2600): - the value uses will be now display correctly - the value discount_amount will be now formatted correctly

--- modules/shop/lib/Coupon.class.php
+++ modules/shop/lib/Coupon.class.php
@@ -1317,7 +1317,7 @@ DBG::log("Coupon::getByOrderId($order_id): ERROR: Query failed");
                     $attribute_discount_rate),
             'SHOP_DISCOUNT_COUPON_AMOUNT' =>
                 Html::getInputText('discount_amount',
-                    number_format($objCouponEdit->discount_amount, 2), false,
+                    number_format($objCouponEdit->discount_amount, 2, '.', ''), false,
                     $attribute_discount_amount),
             'SHOP_DISCOUNT_COUPON_USES' =>
                 Html::getInputText('uses',
