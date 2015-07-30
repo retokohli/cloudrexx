604c34e69e20c63fbd72a137588fad23eeba776b
sebastian-brand <sebastian.brand@cloudrexx.com>
2015-07-30 17:25:38 +0200

    Shop (bugfix #2615): - get the FWUser object over the dedicated method and not over the global   variable

--- modules/shop/lib/Customer.class.php
+++ modules/shop/lib/Customer.class.php
@@ -660,9 +660,7 @@ class Customer extends User
      */
     static function updatePassword($email, $password)
     {
-        global $objFWUser;
-
-        $objUser = $objFWUser->objUser->getUsers(
+        $objUser = \FWUser::getFWUserObject()->objUser->getUsers( 
             array('email' => $email));
         if (!$objUser) return false;
         $objUser->setPassword($password);
