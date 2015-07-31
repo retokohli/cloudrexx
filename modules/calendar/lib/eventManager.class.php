fd85ca5bb698d3bdc3958a3636d8fcb03732b1df
sebastian-brand <sebastian.brand@cloudrexx.com>
2015-07-31 09:39:53 +0200

    Calendar (bugfix #2642): - fixed the issue in displaying repeat event when setting event end date   to a specific date

--- modules/calendar/lib/eventManager.class.php
+++ modules/calendar/lib/eventManager.class.php
@@ -1421,10 +1421,11 @@ class CalendarEventManager extends CalendarLibrary
                 $isAllowedEvent = (boolean) $objCloneEvent->seriesData['seriesPatternEnd']; 
                 break;
             case 3:
-                if($objCloneEvent->startDate <= $objCloneEvent->seriesData['seriesPatternEnd']) {
+                if($objCloneEvent->startDate <= $objCloneEvent->seriesData['seriesPatternEndDate']) {
                     $getNextEvent = true;
                 } else {
-                    $getNextEvent = false;
+                    // don't show the event when startdate is greater then seriesPatternEndDate 
+                    $isAllowedEvent = $getNextEvent = false; 
                 }
                 break;
         }
