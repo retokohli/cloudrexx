826bd6afbac7ca959c27cd32b1f3fbcf742a2476
Nick Brönnimann <nick.broennimann@cloudrexx.com>
2015-08-28 10:59:04 +0200

    ComponentManger: CLX-654 removed unused code
    (cherry picked from commit ec4cac818b496709662663b2fc2d8ebe025cb372)

---
+++ core/ComponentManager/lang/de/frontend.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * @copyright   CONTREXX CMS - COMVATION AG
+ * @author      Comvation Development Team <info@comvation.com>
+ * @access      public
+ * @package     contrexx
+ * @subpackage  core_componentmanager
+ */
+global $_ARRAYLANG;
+$_ARRAYLANG['TXT_COMPONENTMANGER_COMPONENT_INSTALLATION_INSTRUCTION'] = "<p>Diese Komponente ist zurzeit nicht installiert.</p><ol><li>Um die Komponente zu installieren, melden Sie sich im <a href=”https://[[DOMAIN]]/cadmin” target=”_blank” title=”Backend”>Backend</a> an</li><li>Wechseln Sie zu 'Administration/Anwendungen'</li><li>Wählen Sie bei der Gewünschten Anwendung ([[COMPONENT_NAME]]) die Checkbox aus und klicken Sie am Ende  der Seite auf Inhaltsseiten generieren.</li></ol>";
