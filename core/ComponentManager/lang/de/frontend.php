d3c467f17aba221fbdff196eb18df154dd01e926
Nick Brönnimann <nick.broennimann@cloudrexx.com>
2015-08-31 10:29:23 +0200

    ComponentManager: CLX-654 optimized the installation instructions
    (cherry picked from commit ab4b18582dbb8bdd1ba78bd6f38a6da8075b28de)

--- core/ComponentManager/lang/de/frontend.php
+++ core/ComponentManager/lang/de/frontend.php
@@ -8,4 +8,7 @@
  * @subpackage  core_componentmanager
  */
 global $_ARRAYLANG;
-$_ARRAYLANG['TXT_COMPONENTMANGER_COMPONENT_INSTALLATION_INSTRUCTION'] = "<p>Diese Komponente ist zurzeit nicht installiert.</p><ol><li>Um die Komponente zu installieren, melden Sie sich im <a href=”https://[[DOMAIN]]/cadmin” target=”_blank” title=”Backend”>Backend</a> an</li><li>Wechseln Sie zu 'Administration/Anwendungen'</li><li>Wählen Sie bei der Gewünschten Anwendung ([[COMPONENT_NAME]]) die Checkbox aus und klicken Sie am Ende  der Seite auf Inhaltsseiten generieren.</li></ol>";
+$_ARRAYLANG['TXT_MODULE_INSTALLATION_DUIDE'] = 'Diese Anwendung ist zurzeit nicht installiert.
+Um die Anwendung zu installieren, melden Sie sich im %s an.
+Wechseln Sie zu %s.
+Wählen Sie bei der Gewünschten Anwendung (%s) die Checkbox aus und klicken Sie am Ende  der Seite auf Inhaltsseiten generieren.';
