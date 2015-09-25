df4b1fb28a293576d38fadf9ef55d055cd83c155
Adrian Berger <adrian.berger@cloudrexx.com>
2015-09-25 11:08:36 +0200

    Session: fix array matching is_callable

--- core/session.class.php
+++ core/session.class.php
@@ -272,7 +272,8 @@ class cmsSession extends RecursiveArrayAccess implements SessionHandlerInterface
                         self::updateToDb($sessionValue);
                     } else {
                         if ($this->isDirty($lockKey)) {
-                            if (is_callable($sessionValue)) {
+                            // is_callable() can return true for type array, so we need to check that it is not an array
+                            if (!is_array($sessionValue) && is_callable($sessionValue)) {
                                 \DBG::dump('Function for session index '. $lockKey .' can not be stored, saving functions in session is not supported. Please use json instead');
                                 $this->releaseLock($lockKey);
                                 continue;
@@ -859,7 +860,8 @@ class cmsSession extends RecursiveArrayAccess implements SessionHandlerInterface
                 if (is_a($value, 'Cx\Core\Model\RecursiveArrayAccess')) {
                     $serializedValue = '';
                 } else {
-                    if (is_callable($value)) {
+                    // is_callable() can return true for type array, so we need to check that it is not an array
+                    if (!is_array($value) && is_callable($value)) {
                         \DBG::dump('Function for session index '. $key .' can not be stored, saving functions in session is not supported. Please use json instead');
                         continue;
                     }
