11783059cf042ad283339c27d2a386290e2db55e
Raj-ss4u <rajkumar@softsolutions4u.info>
2015-09-30 17:32:09 +0530

    Core HTML: Allow table sorting  - Added the german interface text for exception messages.

--- core/Html/lang/de/backend.php
+++ core/Html/lang/de/backend.php
@@ -13,3 +13,6 @@
 global $_ARRAYLANG;
 
 $_ARRAYLANG['TXT_CORE_HTML_UPDATE_SORT_ORDER_FAILED'] = 'Das System konnte die Sortierung nicht speichern.';
+$_ARRAYLANG['TXT_CORE_HTML_SORTING_ENTITY_NOT_FOUND_ERROR'] = 'Die Entity <strong>%s</strong> wurde in der Komponente <strong>%s</strong> nicht gefunden.';
+$_ARRAYLANG['TXT_CORE_HTML_SORTING_GETTER_SETTER_NOT_FOUND_ERROR'] = 'Die Entity <strong>%s</strong> hat keine Getter und Setter für die Eigenschaften <strong>%s</strong> oder <strong>%s</strong>';
+$_ARRAYLANG['TXT_CORE_HTML_SORTING_NO_ENTITY_FOUND_ERROR'] = 'Keine Einträge zum sortieren gefunden.';
