74be98a252fcdc8e45a7237a1bf711378a081277
Raj-ss4u <rajkumar@softsolutions4u.info>
2015-10-05 13:21:48 +0530

    Core HTML: Allow table sorting  - Fixed the issue of wrong column gets update after the row drag and drop.

--- core/BackendTable.class.php
+++ core/BackendTable.class.php
@@ -91,10 +91,6 @@ class BackendTable extends HTML_Table {
                         //Add the additional attribute id, for getting the updated sort order after the row sorting
                         $this->updateRowAttributes($row, array('id' => 'sortingOrder_' . $data), true);
                     }
-                    if (!empty($sortField) && $header === $sortField) {
-                        //Add the additional attribute class, to display the updated sort order after the row sorting
-                        $this->updateColAttributes($col, array('class' => 'sortBy' . $sortField));
-                    }
                     $encode = true;
                     if (
                         isset($options['fields']) &&
@@ -105,6 +101,10 @@ class BackendTable extends HTML_Table {
                         continue;
                     }
                     
+                    if (!empty($sortField) && $header === $sortField) {
+                        //Add the additional attribute class, to display the updated sort order after the row sorting
+                        $this->updateColAttributes($col, array('class' => 'sortBy' . $sortField));
+                    }
                     $origHeader = $header;
                     if(isset($options['fields'][$header]['sorting'])) {
                         $sorting = $options['fields'][$header]['sorting'];
