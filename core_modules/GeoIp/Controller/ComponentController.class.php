7407ebea93f9107f1e65589626222785af5181eb
Raj-ss4u <rajkumar@softsolutions4u.info>
2015-12-15 18:37:30 +0530

    Implement parsing algorithm  - Implemented the parsing algorithm in the GeoIp component

--- core_modules/GeoIp/Controller/ComponentController.class.php
+++ core_modules/GeoIp/Controller/ComponentController.class.php
@@ -56,4 +56,74 @@ class ComponentController extends \Cx\Core\Core\Model\Entity\SystemComponentCont
     public function getControllerClasses() {
         return array('Backend', 'Default');
     }
+    
+    /**
+     * Do GeoIp processing after content is loaded from DB
+     * 
+     * @param \Cx\Core\ContentManager\Model\Entity\Page $page the resolved page
+     */
+    public function postContentLoad(\Cx\Core\ContentManager\Model\Entity\Page $page)
+    {
+        switch ($this->cx->getMode()) {
+            case \Cx\Core\Core\Controller\Cx::MODE_FRONTEND:
+                \Cx\Core\Setting\Controller\Setting::init('GeoIp', 'config', 'Yaml');
+                $serviceStatus = \Cx\Core\Setting\Controller\Setting::getValue('serviceStatus', 'GeoIp');
+                
+                //If GeoIp processing is deactivated, skip the process
+                if (empty($serviceStatus)) {
+                    return;
+                }
+                
+                //Get the country name and code by using the ipaddress through GeoIp2 library
+                spl_autoload_register(array($this, 'loadGeoIpClass'));
+                $countryDb          = $this->getDirectory().'/Data/GeoLite2-Country.mmdb';
+                $availableLocale    = array('de', 'en', 'fr', 'ru', 'es', 'ja', 'pt-BR', 'zh-CN');
+                $frontendLangLocale = \FWLanguage::getLanguageCodeById(FRONTEND_LANG_ID);
+                $locale             = in_array($frontendLangLocale, $availableLocale) ? $frontendLangLocale : 'en';
+                try {
+                    $reader = new \GeoIp2\Database\Reader($countryDb, array($locale));
+                    $record = $reader->country($_SERVER['REMOTE_ADDR']);
+                    $countryName = $record->country->name;
+                    $countryCode = $record->country->isoCode;
+                } catch (\Exception $e) {
+                    \DBG::log($e->getMessage());
+                    return;
+                }
+
+                //Parse the country name and code
+                $objTemplate = $this->cx->getTemplate();
+                $objTemplate->setVariable(array(
+                    'GEOIP_COUNTRY_NAME' => $countryName,
+                    'GEOIP_COUNTRY_CODE' => $countryCode
+                ));
+
+                //Set the country name and code as cx.variables
+                $objJS = \ContrexxJavascript::getInstance();
+                $objJS->setVariable(array(
+                    'countryName'   => $countryName,
+                    'countryCode'   => $countryCode
+                ), 'geoIp');
+            break;
+        }
+    }
+    
+    /**
+     * Load the GeoIp library classes
+     * 
+     * @param type $className namespace of the class
+     * 
+     * @return null
+     */
+    public function loadGeoIpClass($className) 
+    {
+        $parts = explode('\\', $className);
+        if (!in_array($parts[0], array('GeoIp2', 'MaxMind'))) {
+            return;
+        }
+        if ($parts[0] == 'MaxMind') {
+            array_unshift($parts, 'MaxMind'); // add virtual name MaxMind in the begining
+        }
+        $filePath = $this->cx->getCodeBaseLibraryPath() . '/' . implode('/', $parts) . '.php';
+        $this->cx->getClassLoader()->loadFile($filePath);
+    }
 }
