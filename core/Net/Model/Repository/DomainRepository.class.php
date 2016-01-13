50e77146e69f0d4a2546c0a37ec6070cebf7a15f
Michael Ritter <michael.ritter@cloudrexx.com>
2016-01-13 18:09:27 +0100

    core Net (bugfix): Improves draft fix and exception message

--- core/Net/Model/Repository/DomainRepository.class.php
+++ core/Net/Model/Repository/DomainRepository.class.php
@@ -67,7 +67,7 @@ class DomainRepository extends \Cx\Core\Model\Controller\YamlRepository {
         parent::__construct(\Env::get('cx')->getWebsiteConfigPath() . '/DomainRepository.yml');
         
         //Initialize the Hostname Domain
-        $hostName = current($this->findBy(array('name' => $_SERVER['SERVER_NAME'])));
+        $hostName = $this->findOneBy(array('name' => $_SERVER['SERVER_NAME']));
         if (!$hostName) {
             $hostName = new \Cx\Core\Net\Model\Entity\Domain($_SERVER['SERVER_NAME']);
             $hostName->setVirtual(true);
@@ -82,7 +82,7 @@ class DomainRepository extends \Cx\Core\Model\Controller\YamlRepository {
         // This else statement can be removed as soon as YamlRepository can
         // handle ID changes of virtual entities.
         else {
-            throw new \Exception('Duplicate entry for this domain');
+            throw new \Exception('Duplicate entry for this domain, see http://bugs.cloudrexx.com/cloudrexx/ticket/2763');
         }
         
         $hostName->setVirtual(true);
