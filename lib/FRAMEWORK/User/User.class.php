39ab4b871a5c97686a4b86306c9d214517c4023c
Michael Ritter <michael.ritter@cloudrexx.com>
2016-03-04 14:31:56 +0100

    core DateTime (update): Adds first version of DateTime component

--- lib/FRAMEWORK/User/User.class.php
+++ lib/FRAMEWORK/User/User.class.php
@@ -2451,7 +2451,15 @@ class User extends User_Profile
         }
         return $result->fields['id'];
     }
-
+    
+    /**
+     * Returns this user's timezone
+     * @todo Implement a way to detect the real timezone
+     * @return \DateTimeZone User's timezone
+     */
+    public function getTimezone() {
+        return new \DateTimeZone('Europe/Paris');
+    }
 
     /**
      * Tries to form a valid and unique username from the words given
