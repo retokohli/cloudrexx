7ed7f5d81a121794ea819f083d32df36ac4c4e75
Raj-ss4u <rajkumar@softsolutions4u.info>
2016-04-15 17:42:32 +0530

    WYSIWYG templates provided by the webdesign template  - Merged the template defined in the Wysiwyg page(under the Administration tab) with the template present in the default themes file Wysiwyg.yml if the file Wysiwyg.yml exists in the default theme.

--- core/Wysiwyg/ckeditor.config.js.php
+++ core/Wysiwyg/ckeditor.config.js.php
@@ -59,6 +59,20 @@ if (!empty($pageId) && $pageId != 'new') {
 }
 
 $ymlOption = $wysiwyg->getCustomCSSVariables($skinId);
+//Get the Active template from the Wysiwyg page(under the Administration tab)
+$templates  = json_decode($wysiwyg->getWysiwygTempaltes(), true);
+//get the Wysiwyg templates from default themes
+$themeRepo      = new \Cx\Core\View\Model\Repository\ThemeRepository();
+$theme          = $themeRepo->getDefaultTheme();
+$themesFilePath = new \Cx\Core\ViewManager\Model\Entity\ViewManagerFile($theme->getFoldername() . '/Wysiwyg.yml');
+$fileSystem     = $cx->getMediaSourceManager()->getMediaType('themes')->getFileSystem();
+if ($fileSystem->fileExists($themesFilePath)) {
+    $objYaml   = new \Symfony\Component\Yaml\Yaml();
+    $themeInfo = $objYaml->load($fileSystem->readFile($themesFilePath));
+    //If the Wysiwyg template exists in default themes
+    //then merge those templates with the active templates present in the Wysiwyg page
+    $templates = !empty($themeInfo['Templates']) ? array_merge($templates, $themeInfo['Templates']) : $templates;
+}
 ?>
 //if the wysiwyg css not defined in the session, then load the css variables and put it into the session
 if(!cx.variables.get('css', 'wysiwyg')) {
@@ -150,7 +164,7 @@ CKEDITOR.editorConfig = function( config )
 
 //loading the templates
 CKEDITOR.on('instanceReady',function(){
-    var loadingTemplates = <?php echo $wysiwyg->getWysiwygTempaltes();?>;
+    var loadingTemplates = <?php echo json_encode($templates);?>;
     for(var instanceName in CKEDITOR.instances) {
         //console.log( CKEDITOR.instances[instanceName] );
         loadingTemplates.button = CKEDITOR.instances[instanceName].getCommand("templates") //Reference to Template-Button
