fd95e17f12ed45ee1813ff01d8988db0aea368e2
Raj-ss4u <rajkumar@softsolutions4u.info>
2016-04-19 12:27:11 +0530

    MediaDir: Disable latest entries in overview section of MediaDir  - Updated the language interface text.  - Used 'settingsLatestNumHeadlines' instead of 'settingsLatestNumOverview' in the method Cx\Modules\MediaDir\Controller\MediaDirectoryPlaceholders::getLatestPlacholder().

--- modules/MediaDir/Controller/MediaDirectoryPlaceholders.class.php
+++ modules/MediaDir/Controller/MediaDirectoryPlaceholders.class.php
@@ -79,11 +79,11 @@ class MediaDirectoryPlaceholders extends MediaDirectoryLibrary
     function getLatestPlacholder()
     {
         $this->strPlaceholder = null;
-        
-        $intLimitEnd = ($this->arrSettings['showLatestEntriesInOverview'] == 1)
-                        ? intval($this->arrSettings['settingsLatestNumOverview'])
+
+        $intLimitEnd = ($this->arrSettings['showLatestEntriesInWebdesignTmpl'] == 1)
+                        ? intval($this->arrSettings['settingsLatestNumHeadlines'])
                         : null;
-        
+
         $objEntries = new MediaDirectoryEntry($this->moduleName); 
         $objEntries->getEntries(null,null,null,null,true,null,1,null,$intLimitEnd);  
         
