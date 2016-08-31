ef663a654104ccb15c8d495a570bba84cd6e961d
Raj-ss4u <rajkumar@softsolutions4u.info>
2016-08-31 18:21:49 +0530

    Access: Fix bug #2826: Profile attribute placeholders cannot be used in signup mail  - Fixed the signup mail issues.

--- core_modules/Access/Controller/Access.class.php
+++ core_modules/Access/Controller/Access.class.php
@@ -762,6 +762,7 @@ class Access extends \Cx\Core_Modules\Access\Controller\AccessLib
 
             $isTextMail  = in_array($objUserMail->getFormat(), array('multipart', 'text'));
             $isHtmlMail  = in_array($objUserMail->getFormat(), array('multipart', 'html'));
+            $protocol    = \Cx\Core\Routing\Url::fromRequest()->getProtocol();
             $searchTerms = array(
                 '[[HOST]]',
                 '[[USERNAME]]',
@@ -773,35 +774,51 @@ class Access extends \Cx\Core_Modules\Access\Controller\AccessLib
             $replaceTextTerms = array(
                 $_CONFIG['domainUrl'],
                 $objUser->getUsername(),
-                'http://'.$_CONFIG['domainUrl'].CONTREXX_SCRIPT_PATH.'?section=Access&cmd=signup&u='.($objUser->getId()).'&k='.$objUser->getRestoreKey(),
-                'http://'.$_CONFIG['domainUrl'],
+                $protocol.'://'.$_CONFIG['domainUrl'].CONTREXX_SCRIPT_PATH.'?section=Access&cmd=signup&u='.($objUser->getId()).'&k='.$objUser->getRestoreKey(),
+                $protocol.'://'.$_CONFIG['domainUrl'],
                 $objUserMail->getSenderName(),
-                'http://'.$_CONFIG['domainUrl'].ASCMS_PATH_OFFSET.ASCMS_BACKEND_PATH.'/index.php?cmd=Access&act=user&tpl=modify&id='.$objUser->getId()
+                $protocol.'://'.$_CONFIG['domainUrl'].ASCMS_PATH_OFFSET.ASCMS_BACKEND_PATH.'/index.php?cmd=Access&act=user&tpl=modify&id='.$objUser->getId()
             );
             $replaceHtmlTerms = array(
                 $_CONFIG['domainUrl'],
                 contrexx_raw2xhtml($objUser->getUsername()),
-                'http://'.$_CONFIG['domainUrl'].CONTREXX_SCRIPT_PATH.'?section=Access&cmd=signup&u='.($objUser->getId()).'&k='.$objUser->getRestoreKey(),
-                'http://'.$_CONFIG['domainUrl'],
+                $protocol.'://'.$_CONFIG['domainUrl'].CONTREXX_SCRIPT_PATH.'?section=Access&cmd=signup&u='.($objUser->getId()).'&k='.$objUser->getRestoreKey(),
+                $protocol.'://'.$_CONFIG['domainUrl'],
                 contrexx_raw2xhtml($objUserMail->getSenderName()),
-                'http://'.$_CONFIG['domainUrl'].ASCMS_PATH_OFFSET.ASCMS_BACKEND_PATH.'/index.php?cmd=Access&act=user&tpl=modify&id='.$objUser->getId()
+                $protocol.'://'.$_CONFIG['domainUrl'].ASCMS_PATH_OFFSET.ASCMS_BACKEND_PATH.'/index.php?cmd=Access&act=user&tpl=modify&id='.$objUser->getId()
             );
             if ($mail2load == 'reg_confirm') {
+                $imagePath = $protocol.'://'.$_CONFIG['domainUrl']
+                    . \Cx\Core\Core\Controller\Cx::instanciate()
+                    ->getWebsiteImagesAccessProfileWebPath().'/';
                 $objUser->objAttribute->first();
                 while (!$objUser->objAttribute->EOF) {
                     $objAttribute = $objUser->objAttribute->getById(
                         $objUser->objAttribute->getId()
                     );
 
-                    $placeholderName = strtoupper($objUser->objAttribute->getId());
-                    $searchTerms[]   = '[[USER_' . $placeholderName . ']]';
-                    $replaceTextTerms[] = $objUser->getProfileAttribute($objAttribute->getId());
-                    if ($isHtmlMail && $objUser->objAttribute->getType() == 'text') {
-                        $replaceHtmlTerms[] = contrexx_raw2xhtml(
-                            $objUser->getProfileAttribute($objAttribute->getId())
-                        );
+                    $placeholderName  = strtoupper($objUser->objAttribute->getId());
+                    $searchTerms[]    = '[[USER_' . $placeholderName . ']]';
+                    $placeholderValue = $this->parseAttribute($objUser, $objAttribute->getId(), 0, false, true);
+                    if (
+                        $objAttribute->getType() == 'image' &&
+                        $objAttribute->getId() == 'picture'
+                    ) {
+                        $path = $imagePath.'0_noavatar.gif';
+                        $imgName = $objUser->getProfileAttribute($objAttribute->getId());
+                        if (\Cx\Lib\FileSystem\FileSystem::exists($imagePath . $imgName)) {
+                            $path = $imagePath . $imgName;
+                        }
+                        $replaceHtmlTerms[] = \Html::getImageByPath($path, 'alt="'.$objUser->getEmail().'"');
+                        $replaceTextTerms[] = $path;
                     } else {
-                        $replaceHtmlTerms[] = $objUser->getProfileAttribute($objAttribute->getId());
+                        if (in_array($objUser->objAttribute->getType(), array('text', 'menu'))) {
+                            $replaceTextTerms[] = html_entity_decode($placeholderValue, ENT_QUOTES, CONTREXX_CHARSET);
+                            $replaceHtmlTerms[] = html_entity_decode($placeholderValue, ENT_QUOTES, CONTREXX_CHARSET);
+                        } else {
+                            $replaceTextTerms[] = $placeholderValue;
+                            $replaceHtmlTerms[] = $placeholderValue;
+                        }
                     }
                     $objUser->objAttribute->next();
                 }
