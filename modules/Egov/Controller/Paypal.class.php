c5718fc7cc5b10d08f504cba0e1dd49dff4137b4
SanthanaKumar <santhanam@softsolutions4u.info>
2016-09-14 10:49:08 +0530

    Core: PHP7 compatibility - Migrate dropped function usages - Used string function instead of preg_* to get faster results.

--- modules/Egov/Controller/Paypal.class.php
+++ modules/Egov/Controller/Paypal.class.php
@@ -202,7 +202,7 @@ class Paypal {
 
       }
 
-      if (preg_match("/VERIFIED/i",$this->ipn_response)) {
+      if (stripos('VERIFIED', $this->ipn_response) !== false) {
 
          // Valid IPN transaction.
          $this->log_ipn_results(true);
