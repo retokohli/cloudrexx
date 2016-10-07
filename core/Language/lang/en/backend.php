ad8af994253b740c6062cfee4b5a8fe840889903
Manuel Schenk <manuel.schenk@comvation.com>
2016-10-07 10:27:11 +0200

    core Language (update): Cleanup auto generated code by workbench

--- core/Language/lang/en/backend.php
+++ core/Language/lang/en/backend.php
@@ -38,17 +38,9 @@
 
 global $_ARRAYLANG;
 
-// Let's start with module info:
+// General module info:
 $_ARRAYLANG['TXT_CORE_LANGUAGE'] = 'Language';
-$_ARRAYLANG['TXT_CORE_LANGUAGE_DESCRIPTION'] = 'This is a new module with some sample content to show how to start.';
+$_ARRAYLANG['TXT_CORE_LANGUAGE_DESCRIPTION'] = 'Handles language versions by their country and/or the effective language.';
 
-// Here come the ACTs:
-$_ARRAYLANG['TXT_CORE_LANGUAGE_ACT_DEFAULT'] = 'Overview';
-$_ARRAYLANG['TXT_CORE_LANGUAGE_ACT_SETTINGS'] = 'Settings';
-$_ARRAYLANG['TXT_CORE_LANGUAGE_ACT_SETTINGS_DEFAULT'] = 'Mailing';
-$_ARRAYLANG['TXT_CORE_LANGUAGE_ACT_SETTINGS_HELP'] = 'Help';
-
-// Now our content specific values:
-$_ARRAYLANG['TXT_CORE_LANGUAGE_CONGRATULATIONS'] = 'Overview';
-$_ARRAYLANG['TXT_CORE_LANGUAGE_SUCCESSFUL_CREATION'] = 'This is the Overview/Dashboard of your new Component. More tabs will be generated if you add entities to this component.';
-$_ARRAYLANG['TXT_CORE_LANGUAGE_EXAMPLE_TEMPLATE'] = 'This is the default template for this component, located in View/Template/Backend/Default.html. In order to add entities, place your YAML files in Model/Yaml folder and execute ./workbench.bat db update. Then add a language file entry for your entity.';
+// Module ACTs:
+$_ARRAYLANG['TXT_CORE_LANGUAGE_ACT_DEFAULT'] = 'Frontend';
