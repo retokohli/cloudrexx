9ef02742ac2810563ac55f6e223d0789698b767d
Reto Kohli <reto.kohli@comvation.com>
2016-11-28 16:23:44 +0100

    Remove unused uses, make property protected

--- core/ContentManager/Model/Entity/Translation.class.php
+++ core/ContentManager/Model/Entity/Translation.class.php
@@ -2,11 +2,6 @@
 
 namespace Cx\Core\ContentManager\Model\Entity;
 
-use Doctrine\ORM\Mapping\Table;
-use Doctrine\ORM\Mapping\Index;
-use Doctrine\ORM\Mapping\UniqueConstraint;
-use Doctrine\ORM\Mapping\Entity;
-
 /**
  * Cx\Core\ContentManager\Model\Entity\Translation
  */
@@ -15,7 +10,7 @@ class Translation extends \Gedmo\Translatable\Entity\AbstractTranslation
     /**
      * @var integer $id
      */
-    private $id;
+    protected $id;
 
     /**
      * All required columns are mapped through inherited superclass
