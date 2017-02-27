dc536b51ed6e13e508f979a26d807dbbbf67d140
Michael Ritter <michael.ritter@cloudrexx.com>
2017-02-27 17:38:11 +0100

    core_modules Widget (bugfix): Fix parsing of not-yet-Widgets

--- core_modules/Widget/Controller/ComponentController.class.php
+++ core_modules/Widget/Controller/ComponentController.class.php
@@ -70,7 +70,7 @@ class ComponentController extends \Cx\Core\Core\Model\Entity\SystemComponentCont
      * @param \Cx\Core\ContentManager\Model\Entity\Page $page       The resolved page
      */
     public function postContentLoad(\Cx\Core\ContentManager\Model\Entity\Page $page) {
-        $template = new \Cx\Core\Html\Sigma();
+        $template = new \Cx\Core_Modules\Widget\Model\Entity\Sigma();
         $template->setTemplate($page->getContent());
         $this->parseWidgets($template, 'ContentManager', 'Page', $page->getId());
         $page->setContent($template->get());
