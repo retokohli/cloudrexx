90e0721adfe6b589187e72252ad4bf6e89f3b2c7
Thomas Däppen <thomas.daeppen@cloudrexx.com>
2017-03-10 16:17:23 +0100

    MediaBrowser (bugfix): List protected content pages in MediaBrowser

--- core_modules/MediaBrowser/Controller/JsonMediaBrowser.class.php
+++ core_modules/MediaBrowser/Controller/JsonMediaBrowser.class.php
@@ -158,7 +158,7 @@ class JsonMediaBrowser extends SystemComponentController implements JsonAdapter
     public function getSites() {
         $pageTree = new MediaBrowserPageTree(
             $this->cx->getDb()->getEntityManager(), $this->cx->getLicense(), 0, null, FRONTEND_LANG_ID
-            , null, false, false, false
+            , null, false, true, false
         );
         $pageTree->render();
         return $pageTree->getFlatTree();
