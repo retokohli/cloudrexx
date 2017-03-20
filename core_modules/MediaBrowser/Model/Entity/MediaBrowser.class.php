43856de662590171a6be18b8d75f4043c78191dd
Manuel Schenk <manuel.schenk@comvation.com>
2017-03-20 17:42:08 +0100

    core_module MediaBrowser: Corrects datatypes and improves documentation

--- core_modules/MediaBrowser/Model/Entity/MediaBrowser.class.php
+++ core_modules/MediaBrowser/Model/Entity/MediaBrowser.class.php
@@ -72,30 +72,16 @@ class MediaBrowser extends EntityBase
      */
     protected $entity;
 
+
     /**
      * Create new instance of mediabrowser and register in componentcontroller.
      *
-     * @throws \Exception
+     * @param SystemComponentController $systemComponentController Component which will be instantiated with the MediaBrowser
+     * @param string $entity Entity which will be instantiated with the MediaBrowser
+     * @throws \Exception If no $systemComponentController is provided and none can be found
      */
     function __construct(SystemComponentController $systemComponentController = null, $entity = '')
     {
-        if (!empty($systemComponentController)) {
-            $this->systemComponentController = $systemComponentController;
-        } else {
-            $traces = debug_backtrace(DEBUG_BACKTRACE_PROVIDE_OBJECT, 2);
-            $trace = end($traces);
-            if (empty($trace['class'])) {
-                throw new \Exception('No class found in backtrace');
-            }
-            $matches = array();
-            preg_match(
-                '/Cx\\\\(?:Core|Core_Modules|Modules)\\\\([^\\\\]*)\\\\/',
-                $trace['class'],
-                $matches
-            );
-            $this->systemComponentController = $matches[0];
-        }
-
         $this->entity = $entity;
 
         $this->getComponentController()->addMediaBrowser($this);
@@ -104,8 +90,24 @@ class MediaBrowser extends EntityBase
             'data-cx-mb',
             'class' => "mediabrowser-button button"
         );
-    }
 
+        if (!empty($systemComponentController)) {
+            $this->systemComponentController = $systemComponentController;
+            return;
+        }
+        $traces = debug_backtrace(DEBUG_BACKTRACE_PROVIDE_OBJECT, 2);
+        $trace = end($traces);
+        if (empty($trace['class'])) {
+            throw new \Exception('No class found in backtrace');
+        }
+        $matches = array();
+        preg_match(
+            '/Cx\\\\(?:Core|Core_Modules|Modules)\\\\([^\\\\]*)\\\\/',
+            $trace['class'],
+            $matches
+        );
+        $this->systemComponentController = $this->getComponent($matches[1]);
+    }
 
     /**
      * Set a mediabrowser option
