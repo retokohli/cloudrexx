675b889cdea3ea3aea5a9b23b4129c7d45832790
Raj-ss4u <rajkumar@softsolutions4u.info>
2017-04-12 18:23:39 +0530

    Access: Migrate separate EsiParameter parsing  - Removed the method EsiWidgetController::getWidget() and modified the method EsiWidgetController::parseWidget() in the Access component.

--- core_modules/Access/Controller/EsiWidgetController.class.php
+++ core_modules/Access/Controller/EsiWidgetController.class.php
@@ -47,21 +47,35 @@ namespace Cx\Core_Modules\Access\Controller;
 class EsiWidgetController extends \Cx\Core_Modules\Widget\Controller\EsiWidgetController {
     /**
      * Parses a widget
-     * @param string $name Widget name
-     * @param \Cx\Core\Html\Sigma Widget template
-     * @param \Cx\Core\Routing\Model\Entity\Response $response Current response
-     * @param array $params Array of params
+     *
+     * @param string                                 $name     Widget name
+     * @param \Cx\Core\Html\Sigma                    $template WidgetTemplate
+     * @param \Cx\Core\Routing\Model\Entity\Response $response Response object
+     * @param array                                  $params   Get parameters
+     *
+     * @return null
      */
     public function parseWidget($name, $template, $response, $params)
     {
-
-        $langId = \FWLanguage::getLangIdByIso639_1($params['lang']);
-        $template->setVariable(\Env::get('init')->getComponentSpecificLanguageData('Access', true, $langId));
-        $template->setVariable(\Env::get('init')->getComponentSpecificLanguageData('Core', true, $langId));
+        $template->setVariable(
+            \Env::get('init')->getComponentSpecificLanguageData(
+                'Access',
+                true,
+                $params['lang']
+            )
+        );
+        $template->setVariable(
+            \Env::get('init')->getComponentSpecificLanguageData(
+                'Core',
+                true,
+                $params['lang']
+            )
+        );
 
         if (preg_match('/^access_logged_(in|out)\d{0,2}/', $name)) {
             $this->getComponent('Session')->getSession();
             \FWUser::parseLoggedInOutBlocks($template);
+            return;
         }
 
         $objAccessBlocks = new AccessBlocks($template);
@@ -89,6 +103,32 @@ class EsiWidgetController extends \Cx\Core_Modules\Widget\Controller\EsiWidgetCo
             } else {
                 $template->hideBlock($name);
             }
+
+            // of the users who's last activity was within 3600s
+            // take the one with the lowest last_activity
+            $objFWUser = \FWUser::getFWUserObject();
+            $filter = array(
+                'active'    => true,
+                'last_activity' => array(
+                    '>' => (time()-3600)
+                )
+            );
+            $objUser = $objFWUser->objUser->getUsers(
+                $filter,
+                null,
+                array('last_activity' => 'asc'),
+                null,
+                1
+            );
+            if (!$objUser) {
+                return;
+            }
+
+            // and user_from_above.last_activity + 3600s = cache timeout
+            $cacheTimeout = $objUser->getLastActivityTime() + 3600;
+            $dateTime     = new \DateTime('@' . $cacheTimeout);
+            $response->setExpirationDate($dateTime);
+            return;
         }
 
         //Parse the last active users
@@ -115,6 +155,7 @@ class EsiWidgetController extends \Cx\Core_Modules\Widget\Controller\EsiWidgetCo
             } else {
                 $template->hideBlock($name);
             }
+            return;
         }
 
         //Parse the latest registered users
@@ -141,85 +182,39 @@ class EsiWidgetController extends \Cx\Core_Modules\Widget\Controller\EsiWidgetCo
             } else {
                 $template->hideBlock($name);
             }
+            return;
         }
 
         //Parse the birthday users
-        if ($name == 'access_birthday_member_list') {
-            if (
-                \FWUser::showBirthdayUsers() &&
-                $objAccessBlocks->isSomeonesBirthdayToday() &&
-                (
-                    $template->blockExists('access_birthday_female_members') ||
-                    $template->blockExists('access_birthday_male_members') ||
-                    $template->blockExists('access_birthday_members')
-                )
-            ) {
-                if ($template->blockExists('access_birthday_female_members')) {
-                    $objAccessBlocks->setBirthdayUsers('female');
-                }
+        if ($name !== 'access_birthday_member_list') {
+            return;
+        }
 
-                if ($template->blockExists('access_birthday_male_members')) {
-                    $objAccessBlocks->setBirthdayUsers('male');
-                }
-
-                if ($template->blockExists('access_birthday_members')) {
-                    $objAccessBlocks->setBirthdayUsers();
-                }
-            } else {
-                $template->hideBlock($name);
+        if (
+            \FWUser::showBirthdayUsers() &&
+            $objAccessBlocks->isSomeonesBirthdayToday() &&
+            (
+                $template->blockExists('access_birthday_female_members') ||
+                $template->blockExists('access_birthday_male_members') ||
+                $template->blockExists('access_birthday_members')
+            )
+        ) {
+            if ($template->blockExists('access_birthday_female_members')) {
+                $objAccessBlocks->setBirthdayUsers('female');
             }
+
+            if ($template->blockExists('access_birthday_male_members')) {
+                $objAccessBlocks->setBirthdayUsers('male');
+            }
+
+            if ($template->blockExists('access_birthday_members')) {
+                $objAccessBlocks->setBirthdayUsers();
+            }
+        } else {
+            $template->hideBlock($name);
         }
-    }
-
-    /**
-     * Returns the content of a widget
-     *
-     * @param array $params JsonAdapter parameters
-     *
-     * @return array Content in an associative array
-     */
-    public function getWidget($params)
-    {
-        if (!isset($params['get']['name'])) {
-            return parent::getWidget($params);
-        }
-
-        switch ($params['get']['name']) {
-            case 'access_birthday_member_list':
-                $dateTime = new \DateTime();
-                $dateTime->setTime(23, 59, 59);
-                $params['response']->setExpirationDate($dateTime);
-                break;
-            case 'access_currently_online_member_list':
-                // of the users who's last activity was within 3600s
-                // take the one with the lowest last_activity
-                $objFWUser = \FWUser::getFWUserObject();
-                $filter = array(
-                    'active'    => true,
-                    'last_activity' => array(
-                        '>' => (time()-3600)
-                    )
-                );
-                $objUser = $objFWUser->objUser->getUsers(
-                    $filter,
-                    null,
-                    array(
-                        'last_activity'    => 'asc',
-                    ),
-                    null,
-                    1
-                );
-                if (!$objUser) {
-                    break;
-                }
-
-                // and user_from_above.last_activity + 3600s = cache timeout
-                $cacheTimeout = $objUser->getLastActivityTime() + 3600;
-                $dateTime = new \DateTime('@' . $cacheTimeout);
-                $params['response']->setExpirationDate($dateTime);
-                break; 
-        }
-
-        return parent::getWidget($params);
+        $dateTime = new \DateTime();
+        $dateTime->setTime(23, 59, 59);
+        $response->setExpirationDate($dateTime);
     }
 }
