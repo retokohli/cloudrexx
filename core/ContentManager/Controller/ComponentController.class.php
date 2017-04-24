bd839c9eb8764bfd9ecfca005ef4b3a3164345cb
vinoth_ss4u <vinoth@softsolutions4u.info>
2017-04-24 18:01:53 +0530

    ContentManager: Migrate component "ContentManager"  - Fixed inline comment.

--- core/ContentManager/Controller/ComponentController.class.php
+++ core/ContentManager/Controller/ComponentController.class.php
@@ -82,7 +82,7 @@ class ComponentController extends \Cx\Core\Core\Model\Entity\SystemComponentCont
     public function postInit(\Cx\Core\Core\Controller\Cx $cx)
     {
         //Creating widget only for the frontend placeholder/template block.
-        //But the placeholder CONTENT_TEXT was used in both the frontend and
+        //But the placeholder CONTENT_TITLE was used in both the frontend and
         //backend templates. To avoid creating a widget for the backend placeholder,
         //we will call the return if the mode is backend.
         if($this->cx->getMode() === \Cx\Core\Core\Controller\Cx::MODE_BACKEND) {
