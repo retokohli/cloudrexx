8b51bcbadd996b09ed303a0634f4a3efc6ecc22c
Michael Ritter <michael.ritter@cloudrexx.com>
2017-04-28 17:12:17 +0200

    core View (bugfix): Adds missing raw2xhtml (again!)

--- core/View/Controller/ComponentController.class.php
+++ core/View/Controller/ComponentController.class.php
@@ -86,7 +86,7 @@ class ComponentController extends \Cx\Core\Core\Model\Entity\SystemComponentCont
             }
             $widgetUrl = clone $currentUrl;
             $widgetUrl->setParam($view, $active);
-            $content = (string) $widgetUrl;
+            $content = contrexx_raw2xhtml((string) $widgetUrl);
 
             $widgetController->registerWidget(
                 new \Cx\Core_Modules\Widget\Model\Entity\FinalStringWidget(
