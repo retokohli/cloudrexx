dafc3347f3d8f4c49b8b57f324f7c31534d9f770
Michael Ritter <michael.ritter@cloudrexx.com>
2017-05-22 16:34:01 +0200

    core View (update): Adds generic parse target

--- core_modules/Widget/Model/Entity/WidgetParseTarget.class.php
+++ core_modules/Widget/Model/Entity/WidgetParseTarget.class.php
@@ -40,7 +40,7 @@ namespace Cx\Core_Modules\Widget\Model\Entity;
  * @package cloudrexx
  * @subpackage coremodules_widget
  */
-abstract class WidgetParseTarget extends \Cx\Model\Base\EntityBase {
+abstract class WidgetParseTarget extends \Cx\Core\View\Model\Entity\ParseTarget {
 
     /**
      * Returns the content for a widget
