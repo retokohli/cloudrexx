retokohli/cloudrexx

commit dbe85c9ccc6fe35f47819a7b67b41acc85fb5f87

Core HTML: Allow table sorting  - Removed the insert query of the table 'contrexx_settings' from the installer data file 'contrexx_dump_data.sql'.  - Migrated the method 'updateOrder' from 'JsonHtmlController.class.php' to 'ViewGeneratorJsonController.class.php'.  - Modified the files ViewGenerator.class.php, Backend.js and BackendTable.class.php.

core/BackendTable.class.php
@@ -428,7 +428,7 @@ class BackendTable extends HTML_Table {
      */
     function toHtml()
     {
-        $this->altRowAttributes(2, array('class' => 'row1'), array('class' => 'row2'), true);
+        $this->altRowAttributes(1, array('class' => 'row1'), array('class' => 'row2'), true);
         $strHtml = '';
         $tabs = $this->_getTabs();
         $tab = $this->_getTab();

core/Html/Controller/ComponentController.class.php
@@ -28,7 +28,7 @@ class ComponentController extends \Cx\Core\Core\Model\Entity\SystemComponentCont
     public function getControllerClasses() {
         // Return an empty array here to let the component handler know that there
         // does not exist a backend, nor a frontend controller of this component.
-        return array('JsonHtml', 'ViewGeneratorJson');
+        return array('ViewGeneratorJson');
     }
 
     /**
@@ -37,6 +37,6 @@ class ComponentController extends \Cx\Core\Core\Model\Entity\SystemComponentCont
      * @return array List of ComponentController classes
      */
     public function getControllersAccessableByJson() {
-        return array('JsonHtmlController', 'ViewGeneratorJsonController');
+        return array('ViewGeneratorJsonController');
     }
 }

core/Html/Controller/JsonHtmlController.class.php
@@ -1,206 +1,0 @@
-<?php
-/**
- * Specific JsonHtmlController for this Component. Use this to easily handle the ajax request
- *
- * @copyright   Cloudrexx AG
- * @author      Project Team SS4U <info@comvation.com>
- * @package     contrexx
- * @subpackage  core_html
- */
-
-namespace Cx\Core\Html\Controller;
-
-/**
- * Specific JsonHtmlController for this Component. Use this to easily handle the ajax request
- *
- * @copyright   Cloudrexx AG
- * @author      Project Team SS4U <info@comvation.com>
- * @package     contrexx
- * @subpackage  core_html
- */
-class JsonHtmlController extends    \Cx\Core\Core\Model\Entity\Controller 
-                         implements \Cx\Core\Json\JsonAdapter {
-    /**
-     * Returns the internal name used as identifier for this adapter
-     * 
-     * @return String Name of this adapter
-     */
-    public function getName() {
-        return 'Html';
-    }
-    
-    /**
-     * Returns an array of method names accessable from a JSON request
-     * 
-     * @return array List of method names
-     */
-    public function getAccessableMethods() {
-        return array(
-            'updateOrder' => new \Cx\Core_Modules\Access\Model\Entity\Permission(array('http', 'https'), array('post'), true)
-        );
-    }
-    
-    /**
-     * Returns all messages as string
-     * 
-     * @return String HTML encoded error messages
-     */
-    public function getMessagesAsString() {
-        return '';
-    }
-    
-    /**
-     * Returns default permission as object
-     * @return Object
-     */
-    public function getDefaultPermissions() {
-        return null;
-    }
-    
-    /**
-     * Update the sort order in DB
-     * 
-     * @param array $params
-     * 
-     * @global array  $_ARRAYLANG
-     * @global object $objInit
-     * 
-     * @return array
-     * @throws \Exception
-     */
-    public function updateOrder($params) {
-        global $_ARRAYLANG, $objInit;
-        
-        //get the language interface text
-        $langData   = $objInit->loadLanguageData('Html');
-        $_ARRAYLANG = array_merge($_ARRAYLANG, $langData);
-        
-        $post = is_array($params['post']) ? $params['post'] : array();
-        if (    empty($post)
-            ||  empty($post['prePosition'])
-            ||  empty($post['curPosition'])
-            ||  empty($post['sortField'])
-            ||  empty($post['component'])
-            ||  empty($post['entity'])
-        ) {
-            throw new \Exception($_ARRAYLANG['TXT_CORE_HTML_UPDATE_SORT_ORDER_FAILED']);
-        }
-        
-        //Get all the 'POST' values
-        $sortField       = !empty($post['sortField'])
-                           ? contrexx_input2raw($post['sortField'])
-                           : '';
-        $sortOrder       = !empty($post['sortOrder'])
-                           ? contrexx_input2raw($post['sortOrder'])
-                           : '';
-        $componentName   = !empty($post['component'])
-                           ? contrexx_input2raw($post['component'])
-                           : '';
-        $entityName      = !empty($post['entity'])
-                           ? contrexx_input2raw($post['entity'])
-                           : '';
-        $pagingPosition  = !empty($post['pagingPosition'])
-                           ? contrexx_input2int($post['pagingPosition'])
-                           : 0;
-        $currentPosition = isset($post['curPosition']) 
-                           ? contrexx_input2int($post['curPosition'])
-                           : 0;
-        $prePosition     = isset($post['prePosition']) 
-                           ? contrexx_input2int($post['prePosition'])
-                           : 0;
-        $updatedOrder    = (    isset($post['sortingOrder']) 
-                            &&  is_array($post['sortingOrder'])
-                           )
-                           ? array_map('contrexx_input2int', $post['sortingOrder'])
-                           : array();
-
-        $em = $this->cx->getDb()->getEntityManager();
-        $componentRepo   = $em->getRepository('Cx\Core\Core\Model\Entity\SystemComponent');
-        $objComponent    = $componentRepo->findOneBy(array('name' => $componentName));
-        $entityNameSpace = $objComponent->getNamespace() . '\\Model\\Entity\\' . $entityName;
-        //check whether the entity namespace is a valid one or not
-        if (!in_array($entityNameSpace, $objComponent->getEntityClasses())) {
-            throw new \Exception($_ARRAYLANG['TXT_CORE_HTML_UPDATE_SORT_ORDER_FAILED']);
-        }
-        
-        $entityObject = $em->getClassMetadata($entityNameSpace);
-        $classMethods = get_class_methods($entityObject->newInstance());
-        $primaryKeyName = $entityObject->getSingleIdentifierFieldName();
-        //check whether the updating entity set/get method is a valid one or not
-        if (    !in_array('set'.ucfirst($sortField), $classMethods)
-            ||  !in_array('get'.ucfirst($sortField), $classMethods)
-            ||  !in_array('get'.ucfirst($primaryKeyName), $classMethods)
-        ) {
-            throw new \Exception($_ARRAYLANG['TXT_CORE_HTML_UPDATE_SORT_ORDER_FAILED']);
-        }
-        
-        //update the entities order field in DB
-        $oldPosition = $pagingPosition + $prePosition;
-        $newPosition = $pagingPosition + $currentPosition;
-
-        $min = min(array($newPosition, $oldPosition));
-        $max = max(array($newPosition, $oldPosition));
-
-        $offset = $min - 1;
-        $limit  = ($max - $min) + 1;
-
-        $qb = $em->createQueryBuilder();
-        $qb ->select('e')
-            ->from($entityNameSpace, 'e')
-            ->orderBy('e.' . $sortField, $sortOrder);
-        if (empty($updatedOrder)) {
-            $qb->setFirstResult($offset)
-               ->setMaxResults($limit);
-        }
-        $entities = $qb->getQuery()->getResult();
-
-        if ($entities) {
-            if (    ($oldPosition > $newPosition && empty($updatedOrder))
-                || (!empty($updatedOrder) && $sortOrder == 'DESC')
-            ) {
-                krsort($entities);
-            }
-            $i = 1;
-            $recordCount = count($entities);
-            $orderFieldSetMethodName = 'set'.ucfirst($sortField);
-            $orderFieldGetMethodName = 'get'.ucfirst($sortField);
-            $primaryGetMethodName    = 'get'.ucfirst($primaryKeyName);
-            foreach ($entities as $entity) {
-                if (!empty($updatedOrder)) {
-                    //If the same 'order' field value is repeated,
-                    //we need to update all the entries.
-                    $id = $entity->$primaryGetMethodName();
-                    if (in_array($id, $updatedOrder)) {
-                        $order   = array_search($id, $updatedOrder);
-                        $orderNo = $pagingPosition + $order + 1;
-                        if ($sortOrder == 'DESC') {
-                            $orderNo = $recordCount - ($pagingPosition + $order);
-                        }
-                        $entity->$orderFieldSetMethodName($orderNo);
-                    } else {
-                        $entity->$orderFieldSetMethodName($i);
-                    }
-                } else {
-                    //If the same 'order' field value is not repeated,
-                    //we need to update all the entries between dragged and dropped position
-                    $currentOrder = $entity->$orderFieldGetMethodName();
-                    if ($i == 1) {
-                        $firstResult = $entity;
-                        $sortOrder   = $currentOrder;
-                        $i++;
-                        continue;
-                    } else if ($i == count($entities)) {
-                        $firstResult->$orderFieldSetMethodName($currentOrder);
-                        $entity->$orderFieldSetMethodName($sortOrder);
-                        continue;
-                    }
-                    $entity->$orderFieldSetMethodName($sortOrder);
-                    $sortOrder = $currentOrder;
-                }
-                $i++;
-            }
-            $em->flush();
-        }
-        return array('status' => 'success', 'recordCount' => $recordCount);
-    }
-}

core/Html/Controller/ViewGenerator.class.php
@@ -82,9 +82,6 @@ class ViewGenerator {
         $this->viewId = static::$increment++;
         try {
             
-            //initialize the row sorting functionality
-            $this->getSortingOption($object);
-            
             $cx = \Cx\Core\Core\Controller\Cx::instanciate();
             \JS::registerCSS($cx->getCoreFolderName() . '/Html/View/Style/Backend.css');
             $entityWithNS = $this->findEntityClass($object);
@@ -102,6 +99,10 @@ class ViewGenerator {
             if (empty($this->options)) {
                 $this->options = $options[''];
             }
+            
+            //initialize the row sorting functionality
+            $this->getSortingOption($object);
+            
             if (
                 (!isset($_POST['vg_increment_number']) || $_POST['vg_increment_number'] != $this->viewId) &&
                 (!isset($_GET['vg_increment_number']) || $_GET['vg_increment_number'] != $this->viewId)

core/Html/Controller/ViewGeneratorJsonController.class.php
@@ -49,6 +49,7 @@ class ViewGeneratorJsonController extends \Cx\Core\Core\Model\Entity\Controller 
                     return false;
                 }
             ),
+            'updateOrder' => new \Cx\Core_Modules\Access\Model\Entity\Permission(array('http', 'https'), array('post'), true)
         );
     }
 
@@ -113,5 +114,149 @@ class ViewGeneratorJsonController extends \Cx\Core\Core\Model\Entity\Controller 
             $options->toArray() // must be array and not recursiveArrayAccess object
         );
         return $entityClassObjectView->render();
+    }
+    
+    /**
+     * Update the sort order in DB
+     * 
+     * @param array $params supplied arguments from JsonData-request
+     * 
+     * @return array it contains status and record count
+     * @throws \Exception
+     */
+    public function updateOrder($params) {
+        global $_ARRAYLANG, $objInit;
+        
+        //get the language interface text
+        $langData   = $objInit->loadLanguageData($this->getName());
+        $_ARRAYLANG = array_merge($_ARRAYLANG, $langData);
+        
+        $post = is_array($params['post']) ? $params['post'] : array();
+        if (    empty($post)
+            ||  empty($post['prePosition'])
+            ||  empty($post['curPosition'])
+            ||  empty($post['sortField'])
+            ||  empty($post['component'])
+            ||  empty($post['entity'])
+        ) {
+            throw new \Exception($_ARRAYLANG['TXT_CORE_HTML_UPDATE_SORT_ORDER_FAILED']);
+        }
+        
+        //Get all the 'POST' values
+        $sortField       = !empty($post['sortField'])
+                           ? contrexx_input2raw($post['sortField'])
+                           : '';
+        $sortOrder       = !empty($post['sortOrder'])
+                           ? contrexx_input2raw($post['sortOrder'])
+                           : '';
+        $componentName   = !empty($post['component'])
+                           ? contrexx_input2raw($post['component'])
+                           : '';
+        $entityName      = !empty($post['entity'])
+                           ? contrexx_input2raw($post['entity'])
+                           : '';
+        $pagingPosition  = !empty($post['pagingPosition'])
+                           ? contrexx_input2int($post['pagingPosition'])
+                           : 0;
+        $currentPosition = isset($post['curPosition']) 
+                           ? contrexx_input2int($post['curPosition'])
+                           : 0;
+        $prePosition     = isset($post['prePosition']) 
+                           ? contrexx_input2int($post['prePosition'])
+                           : 0;
+        $updatedOrder    = (    isset($post['sortingOrder']) 
+                            &&  is_array($post['sortingOrder'])
+                           )
+                           ? array_map('contrexx_input2int', $post['sortingOrder'])
+                           : array();
+
+        $em = $this->cx->getDb()->getEntityManager();
+        $componentRepo   = $em->getRepository('Cx\Core\Core\Model\Entity\SystemComponent');
+        $objComponent    = $componentRepo->findOneBy(array('name' => $componentName));
+        $entityNameSpace = $objComponent->getNamespace() . '\\Model\\Entity\\' . $entityName;
+        //check whether the entity namespace is a valid one or not
+        if (!in_array($entityNameSpace, $objComponent->getEntityClasses())) {
+            throw new \Exception($_ARRAYLANG['TXT_CORE_HTML_UPDATE_SORT_ORDER_FAILED']);
+        }
+        
+        $entityObject = $em->getClassMetadata($entityNameSpace);
+        $classMethods = get_class_methods($entityObject->newInstance());
+        $primaryKeyName = $entityObject->getSingleIdentifierFieldName();
+        //check whether the updating entity set/get method is a valid one or not
+        if (    !in_array('set'.ucfirst($sortField), $classMethods)
+            ||  !in_array('get'.ucfirst($sortField), $classMethods)
+            ||  !in_array('get'.ucfirst($primaryKeyName), $classMethods)
+        ) {
+            throw new \Exception($_ARRAYLANG['TXT_CORE_HTML_UPDATE_SORT_ORDER_FAILED']);
+        }
+        
+        //update the entities order field in DB
+        $oldPosition = $pagingPosition + $prePosition;
+        $newPosition = $pagingPosition + $currentPosition;
+
+        $min = min(array($newPosition, $oldPosition));
+        $max = max(array($newPosition, $oldPosition));
+
+        $offset = $min - 1;
+        $limit  = ($max - $min) + 1;
+
+        $qb = $em->createQueryBuilder();
+        $qb ->select('e')
+            ->from($entityNameSpace, 'e')
+            ->orderBy('e.' . $sortField, $sortOrder);
+        if (empty($updatedOrder)) {
+            $qb->setFirstResult($offset)
+               ->setMaxResults($limit);
+        }
+        $entities = $qb->getQuery()->getResult();
+
+        if ($entities) {
+            if (    ($oldPosition > $newPosition && empty($updatedOrder))
+                || (!empty($updatedOrder) && $sortOrder == 'DESC')
+            ) {
+                krsort($entities);
+            }
+            $i = 1;
+            $recordCount = count($entities);
+            $orderFieldSetMethodName = 'set'.ucfirst($sortField);
+            $orderFieldGetMethodName = 'get'.ucfirst($sortField);
+            $primaryGetMethodName    = 'get'.ucfirst($primaryKeyName);
+            foreach ($entities as $entity) {
+                if (!empty($updatedOrder)) {
+                    //If the same 'order' field value is repeated,
+                    //we need to update all the entries.
+                    $id = $entity->$primaryGetMethodName();
+                    if (in_array($id, $updatedOrder)) {
+                        $order   = array_search($id, $updatedOrder);
+                        $orderNo = $pagingPosition + $order + 1;
+                        if ($sortOrder == 'DESC') {
+                            $orderNo = $recordCount - ($pagingPosition + $order);
+                        }
+                        $entity->$orderFieldSetMethodName($orderNo);
+                    } else {
+                        $entity->$orderFieldSetMethodName($i);
+                    }
+                } else {
+                    //If the same 'order' field value is not repeated,
+                    //we need to update all the entries between dragged and dropped position
+                    $currentOrder = $entity->$orderFieldGetMethodName();
+                    if ($i == 1) {
+                        $firstResult = $entity;
+                        $sortOrder   = $currentOrder;
+                        $i++;
+                        continue;
+                    } else if ($i == count($entities)) {
+                        $firstResult->$orderFieldSetMethodName($currentOrder);
+                        $entity->$orderFieldSetMethodName($sortOrder);
+                        continue;
+                    }
+                    $entity->$orderFieldSetMethodName($sortOrder);
+                    $sortOrder = $currentOrder;
+                }
+                $i++;
+            }
+            $em->flush();
+        }
+        return array('status' => 'success', 'recordCount' => $recordCount);
     }
 }

core/Html/View/Script/Backend.js
@@ -37,10 +37,10 @@ cx.jQuery(function(jQuery) {
             }
 
             var that   = this,
-                sortTd = jQuery('table.sortable tbody > tr:not(:nth-child(1), :nth-child(2), :last-child) > td.sortBy'),
+                sortTd = jQuery('table.sortable tbody > tr:not(:nth-child(1), :last-child) > td.sortBy'),
                 updatedOrder  = jQuery('.sortable tbody').sortable('serialize'), recordCount,
-                currentIndex  = ui.item.index() - 1,
-                previousIndex = jQuery(ui.item).data('pIndex') - 1,
+                currentIndex  = ui.item.index(),
+                previousIndex = jQuery(ui.item).data('pIndex'),
                 repeat = isOrderNoRepeat(sortTd, previousIndex, currentIndex),
                 data   = 'sortOrder=' + sortOrder + '&curPosition=' + currentIndex
                        + '&prePosition=' + previousIndex

commit f4ee028b06b4d44610a728576608eaa2fb411c18

core Html (bugfix): Fix for head row bug introduced in dbe85c9ccc6fe35f47819a7b67b41acc85fb5f87

core/BackendTable.class.php
@@ -226,6 +226,7 @@ class BackendTable extends HTML_Table {
                 $row++;
             }
             // adjust colspan of master-table-header-row
+            $this->altRowAttributes(1, array('class' => 'row1'), array('class' => 'row2'), true);
             if ($hasMasterTableHeader) {
                 $this->setCellAttributes(0, 0, array('colspan' => $col + is_array($options['functions'])));
                 $this->updateRowAttributes(1, array('class' => 'row3'), true);
@@ -524,7 +525,6 @@ class BackendTable extends HTML_Table {
      */
     function toHtml()
     {
-        $this->altRowAttributes(1, array('class' => 'row1'), array('class' => 'row2'), true);
         $strHtml = '';
         $tabs = $this->_getTabs();
         $tab = $this->_getTab();