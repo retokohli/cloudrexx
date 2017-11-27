82d99c32a15d6400a12a061556d9f94d4073de3a
Adrian Berger <adrian.berger@comvation.com>
2017-11-27 17:36:25 +0100

    Newsletter: Add db options for crm association

--- installer/data/contrexx_dump_structure.sql
+++ installer/data/contrexx_dump_structure.sql
@@ -2913,7 +2913,7 @@ CREATE TABLE `contrexx_module_newsletter_rel_cat_news` (
 CREATE TABLE `contrexx_module_newsletter_rel_crm_membership_newsletter` (
   `membership_id` int(10) unsigned NOT NULL,
   `newsletter_id` int(10) unsigned NOT NULL,
-  `type` enum('include', 'exclude') NOT NULL,
+  `type` enum('associate', 'include', 'exclude') NOT NULL,
   UNIQUE KEY `uniq` (`membership_id`,`newsletter_id`,`type`)
 ) ENGINE=InnoDB;
 CREATE TABLE `contrexx_module_newsletter_rel_user_cat` (
@@ -2948,7 +2948,7 @@ CREATE TABLE `contrexx_module_newsletter_tmp_sending` (
   `newsletter` int(11) NOT NULL DEFAULT '0',
   `email` varchar(255) NOT NULL DEFAULT '',
   `sendt` tinyint(1) NOT NULL DEFAULT '0',
-  `type` enum('access','newsletter','core') NOT NULL DEFAULT 'newsletter',
+  `type` enum('access','newsletter','core','crm') NOT NULL DEFAULT 'newsletter',
   `code` varchar(10) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `unique_email` (`newsletter`,`email`),
