96fb71f6bc8e26ab941577dfbd61ac3214a76ae6
Michael Ritter <michael.ritter@cloudrexx.com>
2017-12-22 13:34:58 +0100

    modules MediaDir (update): Add placeholders for lat & long

--- modules/MediaDir/Model/Entity/MediaDirectoryInputfieldGoogleMap.class.php
+++ modules/MediaDir/Model/Entity/MediaDirectoryInputfieldGoogleMap.class.php
@@ -45,7 +45,14 @@ namespace Cx\Modules\MediaDir\Model\Entity;
  */
 class MediaDirectoryInputfieldGoogleMap extends \Cx\Modules\MediaDir\Controller\MediaDirectoryLibrary implements Inputfield
 {
-    public $arrPlaceholders = array('TXT_MEDIADIR_INPUTFIELD_NAME','MEDIADIR_INPUTFIELD_VALUE','MEDIADIR_INPUTFIELD_LINK', 'MEDIADIR_INPUTFIELD_LINK_HREF');
+    public $arrPlaceholders = array(
+        'TXT_MEDIADIR_INPUTFIELD_NAME',
+        'MEDIADIR_INPUTFIELD_VALUE',
+        'MEDIADIR_INPUTFIELD_LINK',
+        'MEDIADIR_INPUTFIELD_LINK_HREF',
+        'MEDIADIR_INPUTFIELD_MAP_LAT',
+        'MEDIADIR_INPUTFIELD_MAP_LONG',
+    );
 
     private $imagePath;
     private $imageWebPath;
@@ -301,6 +308,8 @@ EOF;
             $arrContent[$this->moduleLangVar.'_INPUTFIELD_VALUE'] = $objGoogleMap->getMap();
             $arrContent[$this->moduleLangVar.'_INPUTFIELD_LINK'] = $strValueLink;
             $arrContent[$this->moduleLangVar.'_INPUTFIELD_LINK_HREF'] = $strValueLinkHref;
+            $arrContent[$this->moduleLangVar.'_INPUTFIELD_MAP_LAT'] = $strValueLat;
+            $arrContent[$this->moduleLangVar.'_INPUTFIELD_MAP_LONG'] = $strValueLon;
         } else {
             $arrContent = null;
         }
