d33f825e3df4c96592163038bc50f224c2c93282
Thomas Däppen <thomas.daeppen@cloudrexx.com>
2018-01-25 21:31:56 +0100

    Wysiwyg: dropped orphaned code

--- core/Wysiwyg/ckeditor.config.js.php
+++ core/Wysiwyg/ckeditor.config.js.php
@@ -121,16 +121,10 @@ CKEDITOR.editorConfig = function( config )
 CKEDITOR.on('instanceReady',function(){
     var loadingTemplates = <?php echo $wysiwyg->getWysiwygTempaltes();?>;
     for(var instanceName in CKEDITOR.instances) {
-        //console.log( CKEDITOR.instances[instanceName] );
         loadingTemplates.button = CKEDITOR.instances[instanceName].getCommand("templates") //Reference to Template-Button
 
         // Define Standard-Path
-        //var path = CKEDITOR.plugins.getPath('templates')
-        //var defaultPath = path.split("lib/ckeditor/")[0]+"customizing/lib/ckeditor"+path.split("lib/ckeditor")[1]+"templates/"
-        //var defaultPath = path.split("lib/ckeditor")[0] //Path to Templates-Folder
-        //var defaultPath = "/"
         loadingTemplates.load = (function(){
-            //this.defaultPath = defaultPath;
             if (typeof this.button != 'undefined') {
                 this.button.setState(CKEDITOR.TRISTATE_DISABLED) // Disable "Template"-Button
             }
@@ -213,7 +207,6 @@ cx.bind("loadingEnd", function(myArgs) {
         var data = myArgs['data'];
         if(data.hasOwnProperty('wysiwygCssReload') && (data.wysiwygCssReload).hasOwnProperty('css')) {
             for(var instanceName in CKEDITOR.instances) {
-                //CKEDITOR.instances[instanceName].config.contentsCss =  data.wysiwygCssReload.css;
                 var is_same = (data.wysiwygCssReload.css).equals(cx.variables.get('css', 'wysiwyg')) && cx.variables.get('css', 'wysiwyg').every(function(element, index) {
                     return element === data.wysiwygCssReload.css[index];
                 });
