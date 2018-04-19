4dc275c4c2cb84c44659dfc4a1f44c0b4f357566
Michael Ritter <michael.ritter@cloudrexx.com>
2018-04-19 14:19:26 +0200

    fix(user): remove unused custom profile attributes (CLX-2077)

--- installer/data/contrexx_dump_data.sql
+++ installer/data/contrexx_dump_data.sql
@@ -412,15 +412,6 @@ INSERT INTO `contrexx_access_settings` (`key`, `value`, `status`) VALUES ('user_
 INSERT INTO `contrexx_access_settings` (`key`, `value`, `status`) VALUES ('user_config_email_access','',1);
 INSERT INTO `contrexx_access_settings` (`key`, `value`, `status`) VALUES ('user_config_profile_access','',1);
 INSERT INTO `contrexx_access_settings` (`key`, `value`, `status`) VALUES ('user_delete_account','',1);
-INSERT INTO `contrexx_access_user_attribute` (`id`, `parent_id`, `type`, `mandatory`, `sort_type`, `order_id`, `access_special`, `access_id`) VALUES (1,NULL,'textarea','0','asc',0,'',56);
-INSERT INTO `contrexx_access_user_attribute` (`id`, `parent_id`, `type`, `mandatory`, `sort_type`, `order_id`, `access_special`, `access_id`) VALUES (2,NULL,'text','0','asc',0,'',57);
-INSERT INTO `contrexx_access_user_attribute` (`id`, `parent_id`, `type`, `mandatory`, `sort_type`, `order_id`, `access_special`, `access_id`) VALUES (3,NULL,'uri','0','asc',0,'',0);
-INSERT INTO `contrexx_access_user_attribute_name` (`attribute_id`, `lang_id`, `name`) VALUES (1,1,'Notizen');
-INSERT INTO `contrexx_access_user_attribute_name` (`attribute_id`, `lang_id`, `name`) VALUES (1,2,'Notes');
-INSERT INTO `contrexx_access_user_attribute_name` (`attribute_id`, `lang_id`, `name`) VALUES (2,1,'Kundenrabattgruppe');
-INSERT INTO `contrexx_access_user_attribute_name` (`attribute_id`, `lang_id`, `name`) VALUES (2,2,'Discount group');
-INSERT INTO `contrexx_access_user_attribute_name` (`attribute_id`, `lang_id`, `name`) VALUES (3,1,'Facebook');
-INSERT INTO `contrexx_access_user_attribute_name` (`attribute_id`, `lang_id`, `name`) VALUES (3,2,'Facebook');
 INSERT INTO `contrexx_access_user_groups` (`group_id`, `group_name`, `group_description`, `is_active`, `type`, `homepage`) VALUES (1,'Manager','Administrator',1,'backend','');
 INSERT INTO `contrexx_access_user_groups` (`group_id`, `group_name`, `group_description`, `is_active`, `type`, `homepage`) VALUES (3,'Community','Community',1,'frontend','');
 INSERT INTO `contrexx_access_user_groups` (`group_id`, `group_name`, `group_description`, `is_active`, `type`, `homepage`) VALUES (9,'Moderator','Inhaltspflege und Statistiken',1,'backend','');
