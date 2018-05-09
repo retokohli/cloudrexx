0cb87737f7d5212b185f5dab83097db399eee0ad
Michael Ritter <michael.ritter@cloudrexx.com>
2018-05-09 11:31:15 +0200

    fix(news): replace ip by unique user hash (CLX-2110)

--- core_modules/News/Controller/NewsLibrary.class.php
+++ core_modules/News/Controller/NewsLibrary.class.php
@@ -2854,9 +2854,19 @@ EOF;
         }
 
         //Now check database (make sure the user didn't delete the cookie
-        $objResult = $objDatabase->SelectLimit("SELECT 1 FROM `".DBPREFIX."module_news_comments`
-                                                 WHERE  `ip_address` = '".contrexx_input2db($_SERVER['REMOTE_ADDR'])."'
-                                                        AND `date` > ".(time() - intval($this->arrSettings['news_comments_timeout'])));
+        $cx = \Cx\Core\Core\Controller\Cx::instanciate();
+        $ipAddress = $cx->getComponent(
+            'Stats'
+        )->getCounterInstance()->getUniqueUserId();
+        $objResult = $objDatabase->SelectLimit('
+            SELECT
+                1
+            FROM
+                `' . DBPREFIX . 'module_news_comments`
+            WHERE
+                `ip_address` = "' . $ipAddress . '" AND
+                `date` > ' . (time() - intval($this->arrSettings['news_comments_timeout']))
+        );
         if ($objResult && !$objResult->EOF) {
             return true;
         }
