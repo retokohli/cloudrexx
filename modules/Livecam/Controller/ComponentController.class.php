48b4148c9ecbacd91b1f5afcf0732107dcddaadc
Sam Hawkes <sam.hawkes@comvation.com>
2018-08-10 10:14:36 +0200

    fix(livecam): fix docblocks (CLX-2233)

--- modules/Livecam/Controller/ComponentController.class.php
+++ modules/Livecam/Controller/ComponentController.class.php
@@ -93,9 +93,13 @@ class ComponentController
     }
 
     /**
-     * Init code after system initialization for example widgets
+     * Do something after system initialization
      *
-     * @param \Cx\Core\Core\Controller\Cx $cx
+     * USE CAREFULLY, DO NOT DO ANYTHING COSTLY HERE!
+     * CALCULATE YOUR STUFF AS LATE AS POSSIBLE.
+     * This event must be registered in the postInit-Hook definition
+     * file config/postInitHooks.yml.
+     * @param \Cx\Core\Core\Controller\Cx   $cx The instance of \Cx\Core\Core\Controller\Cx
      */
     public function postInit(\Cx\Core\Core\Controller\Cx $cx)
     {
