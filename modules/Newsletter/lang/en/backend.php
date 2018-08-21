275881cc3e9c26b68d36554f7cd7e45db4383b1d
Michael Ritter <michael.ritter@cloudrexx.com>
2018-08-21 11:45:01 +0200

    style(newsletter): optimize setting description (CLX-2128)

--- modules/Newsletter/lang/en/backend.php
+++ modules/Newsletter/lang/en/backend.php
@@ -510,4 +510,4 @@ $_ARRAYLANG['TXT_NEWSLETTER_CRM_MEMBERSHIP_EXCLUDE_TXT'] = 'Don\'t send campaign
 $_ARRAYLANG['TXT_NEWSLETTER_STATISTICS'] = 'Enable statistics';
 $_ARRAYLANG['TXT_NEWSLETTER_STATISTICS_TOOLTIP'] = 'Please note that the information tracked by this metric could contain personal data which is prohibited by law in most countries without explicit consent of the affected person.';
 $_ARRAYLANG['TXT_NEWSLETTER_STATISTICS_DROP'] = 'Do you want to delete the tracked data?';
-$_ARRAYLANG['TXT_NEWSLETTER_CONFIRM_LINK_VALIDITY_HOUR'] = 'Registration confirmation link valid in hours';
+$_ARRAYLANG['TXT_NEWSLETTER_CONFIRM_LINK_VALIDITY_HOUR'] = 'Registration confirmation link validity in hours';
