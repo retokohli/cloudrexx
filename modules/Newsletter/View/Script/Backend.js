d0fdc0ad0e2979ea961f100c79469b0f76894076
Michael Ritter <michael.ritter@cloudrexx.com>
2018-08-22 17:13:27 +0200

    fix(newsletter): cleanup JS code and fix active lists (CLX-2129)

--- modules/Newsletter/View/Script/Backend.js
+++ modules/Newsletter/View/Script/Backend.js
@@ -1,35 +1,45 @@
 cx.jQuery(document).ready(function(){
-  var consentConfirmDiv = jQuery('#consentConfirmdiv'),
-      consentCheckbox   = jQuery('#consentConfirm'),
-      errorMsg          = cx.variables.get('NEWSLETTER_CONSENT_CONFIRM_ERROR', 'Newsletter');
+    var consentConfirmDiv = jQuery("#consentConfirmdiv"),
+        consentCheckbox   = jQuery("#consentConfirm"),
+        errorMsg          = cx.variables.get("NEWSLETTER_CONSENT_CONFIRM_ERROR", "Newsletter");
 
-  // Checks a url values to hide a consent confirm div during edit by default
-  if (jQuery('form[name="userAddEdit"]').length && (jQuery('#editUser').val() != 0)) {
-    consentConfirmDiv.addClass('inactive');
-    consentCheckbox.prop('checked', true);
-  }
-
-  jQuery('input[name^="newsletter_recipient_associated_list"]').change(function(){
-    if (consentConfirmDiv.hasClass('inactive')) {
-      consentCheckbox.prop('checked', false);
-      consentConfirmDiv.removeClass('inactive');
-      consentConfirmDiv.addClass('active');
+    // Checks a url values to hide a consent confirm div during edit by default
+    if (jQuery("form[name=\"userAddEdit\"]").length && (jQuery("#editUser").val() != 0)) {
+        consentConfirmDiv.addClass("inactive");
+        consentCheckbox.prop("checked", true);
     }
-  });
 
-  jQuery('input[name="imported"]').click(function(e){
-    if (!jQuery('#consentConfirmImport').is(':checked')) {
-      e.preventDefault();
-      showErrorMsg(errorMsg);
-    }
-  })
+    jQuery("input[name^=\"newsletter_recipient_associated_list\"]").each(function(index, el) {
+        var element = jQuery(el);
+        if (element.is(":checked")) {
+            element.addClass("pre-checked");
+        }
+    });
 
-  jQuery('input[name="newsletter_recipient_save"], input[name="newsletter_import_plain"]').click(function(e){
-    if (!consentCheckbox.is(':checked')) {
-      e.preventDefault();
-      showErrorMsg(errorMsg);
-    }
-  });
+    jQuery("input[name^=\"newsletter_recipient_associated_list\"]").change(function(){
+        if (jQuery(this).hasClass("pre-checked")) {
+            return;
+        }
+        if (consentConfirmDiv.hasClass("inactive")) {
+            consentCheckbox.prop("checked", false);
+            consentConfirmDiv.removeClass("inactive");
+            consentConfirmDiv.addClass("active");
+        }
+    });
+
+    jQuery("input[name=\"imported\"]").click(function(e){
+        if (!jQuery("#consentConfirmImport").is(":checked")) {
+            e.preventDefault();
+            showErrorMsg(errorMsg);
+        }
+    })
+
+    jQuery("input[name=\"newsletter_recipient_save\"], input[name=\"newsletter_import_plain\"]").click(function(e){
+        if (!consentCheckbox.is(":checked")) {
+            e.preventDefault();
+            showErrorMsg(errorMsg);
+        }
+    });
 });
 
 
@@ -38,8 +48,8 @@ cx.jQuery(document).ready(function(){
  */
 function showErrorMsg(errorMsg)
 {
- if (jQuery('.consentError').length === 0) {
-    jQuery('#subnavbar_level1').removeClass('no_margin');
-    jQuery('#subnavbar_level1').after('<br><div class="consentError" id="alertbox">' + errorMsg +'</div>');
-  }
+    if (jQuery(".consentError").length === 0) {
+        jQuery("#subnavbar_level1").removeClass("no_margin");
+        jQuery("#subnavbar_level1").after("<br><div class=\"consentError\" id=\"alertbox\">" + errorMsg +"</div>");
+    }
 }
