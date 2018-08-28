d9335f6b91d82d4f600e41cb88332164785e8358
git <reto.kohli@comvation.com>
2018-08-28 09:50:09 +0200

    chore(indexer): add test/sample code (CHDIR-25)

---
+++ core_modules/IndexerDocx/Testing/TestIndexerDocx.php
@@ -0,0 +1,6 @@
+<?php
+
+// TEST ONLY
+// Integrate a call like this in your code in order to trigger indexing:
+//    $this->cx->getEvents()->triggerEvent('MediaSource:Edit',
+//        ['path' => $this->cx->getWebsiteDocumentRootPath() . '/sample.docx']);
