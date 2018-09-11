19018864844959bab691f3898b6360e6f7ada5f8
DeepakRajanM <deepak@softsolutions4u.info>
2018-09-11 12:47:27 +0530

    fix(downloads): removed storing of function reference into a variable (CLX-2226)

--- modules/Downloads/Controller/Download.class.php
+++ modules/Downloads/Controller/Download.class.php
@@ -1492,11 +1492,19 @@ class Download {
                 return false;
             }
         }
-        $urlCallBack = function ($value) {
-            return preg_match('#^[a-z]+://$#i', $value);
-        };
 
-        if (($this->type == 'url') && array_search('1', array_map($urlCallBack, $this->sources))) {
+        if (
+            ($this->type == 'url') &&
+            array_search(
+                '1',
+                array_map(
+                    function ($value) {
+                        return preg_match('#^[a-z]+://$#i', $value);
+                    },
+                    $this->sources
+                )
+            )
+        ) {
             $this->error_msg[] = $_ARRAYLANG['TXT_DOWNLOADS_SET_SOURCE_MANDATORY'];
             return false;
         }
