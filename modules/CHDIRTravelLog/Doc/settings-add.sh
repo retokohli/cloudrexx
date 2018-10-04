ca11a9f356de7b2a35d58a77d5562ad10f8e8a88
git <reto.kohli@comvation.com>
2018-10-04 13:24:27 +0200

    fix(chdirtravellog): add settings and language entries (CHDIR-23)

--- modules/CHDIRTravelLog/Doc/settings-add.sh
+++ modules/CHDIRTravelLog/Doc/settings-add.sh
@@ -1,7 +1,12 @@
+# Path: modules/CHDIRTravelLog/Doc/settings-add.sh
 ./cx Setting add CHDIRTravelLog -group=config -engine=FileSystem project_name 'GAN16' 1 text ''
-./cx Setting add CHDIRTravelLog -group=config -engine=FileSystem data_folder '/travellog/' 2 text ''
-./cx Setting add CHDIRTravelLog -group=config -engine=FileSystem pdf_folder '/travellog/pdf/' 3 text ''
+./cx Setting add CHDIRTravelLog -group=config -engine=FileSystem data_folder '/media/Travellog/' 2 text ''
+./cx Setting add CHDIRTravelLog -group=config -engine=FileSystem pdf_folder '/media/Travellog/PDF/' 3 text ''
 ./cx Setting add CHDIRTravelLog -group=config -engine=FileSystem csv_delimiter ';' 4 text ''
+# TODO: Not added correctly:
 ./cx Setting add CHDIRTravelLog -group=config -engine=FileSystem csv_enclosure '"' 5 text ''
-./cx Setting add CHDIRTravelLog -group=config -engine=FileSystem csv_escape '\\' 6 text ''
-./cx Setting add CHDIRTravelLog -group=config -engine=FileSystem last_sync_time '1519911128' 7 text ''
+# TODO: Not added correctly:
+./cx Setting add CHDIRTravelLog -group=config -engine=FileSystem csv_escape \\ 6 text ''
+# The sync time may inititally be reset in order to trigger the update
+#./cx Setting add CHDIRTravelLog -group=config -engine=FileSystem last_sync_time '1519911128' 7 text ''
+./cx Setting add CHDIRTravelLog -group=config -engine=FileSystem last_sync_time '0' 7 text ''
