f16947a62494abcfa21dca33a4ae03b052bae757
git <reto.kohli@comvation.com>
2018-10-05 10:59:02 +0200

    feat(chdirtravellog): translate language entries (CHDIR-23)

---
+++ modules/CHDIRTravelLog/lang/de/backend.php
@@ -0,0 +1,39 @@
+<?php declare(strict_types=1);
+/**
+ * Cloudrexx App by Comvation AG
+ *
+ * PHP Version 7.1 - 7.2
+ *
+ * @category  CloudrexxApp
+ * @package   CHDIRTravelLog
+ * @author    Comvation AG <info@comvation.com>
+ * @copyright 2018 ch-direct
+ * @link      https://www.comvation.com/
+ *
+ * Unauthorized copying, changing or deleting
+ * of any file from this app is strictly prohibited
+ *
+ * Authorized copying, changing or deleting
+ * can only be allowed by a separate contract
+ */
+
+/**
+ * @author      Reto Kohli <reto.kohli@comvation.com>
+ * @package     cloudrexx
+ * @subpackage  module_chdirtravellog
+ */
+global $_ARRAYLANG;
+$_ARRAYLANG['TXT_MODULE_CHDIRTRAVELLOG'] = 'TravelLog';
+$_ARRAYLANG['TXT_MODULE_CHDIRTRAVELLOG_DESCRIPTION'] = 'Reiseblätter und verwandte Verbindungen durchsuchen und exportieren';
+$_ARRAYLANG['TXT_MODULE_CHDIRTRAVELLOG_ACT_SETTINGS'] = 'Einstellungen';
+$_ARRAYLANG['TXT_MODULE_CHDIRTRAVELLOG_ACT_SETTINGS_DEFAULT'] = 'Allgemein';
+$_ARRAYLANG['TXT_MODULE_CHDIRTRAVELLOG_ACT_DEFAULT'] = 'TODO: Removeme';
+$_ARRAYLANG['TXT_MODULE_CHDIRTRAVELLOG_ACT_SETTINGS_HELP'] = 'TODO: Removeme';
+$_ARRAYLANG['TXT_MODULE_CHDIRTRAVELLOG_CSV_DELIMITER'] = 'CSV Import: Trennzeichen';
+$_ARRAYLANG['TXT_MODULE_CHDIRTRAVELLOG_CSV_ENCLOSURE'] = 'CSV Import: Umschliessung';
+$_ARRAYLANG['TXT_MODULE_CHDIRTRAVELLOG_CSV_ESCAPE'] = 'CSV Import: Escape';
+$_ARRAYLANG['TXT_MODULE_CHDIRTRAVELLOG_DATA_FOLDER'] = 'Verzeichnis: Datenablage';
+$_ARRAYLANG['TXT_MODULE_CHDIRTRAVELLOG_LAST_SYNC_TIME'] = 'Zeitstempel der letzten Synchronisation (nicht ändern!)';
+$_ARRAYLANG['TXT_MODULE_CHDIRTRAVELLOG_PDF_FOLDER'] = 'Verzeichnis: PDF Dokumente';
+$_ARRAYLANG['TXT_MODULE_CHDIRTRAVELLOG_PROJECT_NAME'] = 'Projekt Name: Vorgabe';
+$_ARRAYLANG['TXT_MODULE_CHDIRTRAVELLOG_PROJECT_NAMES'] = 'Projekt Name: Verfügbare Optionen (kommagetrennt)';
