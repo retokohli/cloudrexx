056f560ec6c394bf0dc5bdf4da01b645aad35f19
git <reto.kohli@comvation.com>
2018-10-08 10:27:28 +0200

    docs(customizing): add notes for running the export (CHDIR-19)

--- customizing/ch-direct.org-user-import/start.php
+++ customizing/ch-direct.org-user-import/start.php
@@ -6,8 +6,16 @@
   </head>
   <body>
     <?php
-//    Run URL:
-//        http://chdirect.org.lvh.me/customizing/ch-direct.org-user-import/start.php
+//  Run URL:
+//    http://chdirect.org.lvh.me/customizing/ch-direct.org-user-import/start.php
+//
+//  Run Notes
+//      - Import the old access/user and newsletter tables
+//          - Recommended: Create a separate database, and update your config/configuration.php:
+//              $_DBCONFIG['database'] = 'voev'; // Database name
+//      - Run this script
+//      - Import the dump from the export folder into the target database
+//          - Restore your configuration when done, if necessary
     require_once dirname(dirname(dirname(__FILE__))) . '/core/Core/init.php';
     //\DBG::activate(DBG_PHP | DBG_LOG_FILE);
     \DBG::activate(DBG_PHP|DBG_DB_ERROR);
