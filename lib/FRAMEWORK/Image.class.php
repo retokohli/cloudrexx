d6def53cffa5b2bfb7edca775a68e4613a29425a
DeepakRajanM <deepak@softsolutions4u.info>
2018-11-02 14:39:06 +0530

    feat(lib): extended image class with fixImageOrientation method (CLX-2323)

--- lib/FRAMEWORK/Image.class.php
+++ lib/FRAMEWORK/Image.class.php
@@ -922,7 +922,46 @@ class ImageManager
         throw new Exception('Is not a valid image');
     }
 
+    /**
+     * Fix image orientation based on its exif-data
+     *
+     * @param string $filePath File path
+     */
+    public function fixImageOrientation($filePath)
+    {
+        // The method exif_read_data() reads a header information from an image.
+        // It returns an associated array where the array indexes are the header
+        // names and the array values are the values associated with those headers.
+        // If no data can be returned, exif_read_data() will return FALSE.
+        $exif = exif_read_data($filePath);
+        if (empty($exif['Orientation'])) {
+            return;
+        }
 
+        $this->loadImage($filePath);
+        // The information from the accelerometer is stored in the orientation
+        // field of the Exchangeable Image File Format (EXIF) metadata.
+        // If the image has rotated, EXIF data should have 'Orientation' in its
+        // header information. The orientation always in 3, 6 or 8. Here orientation
+        // 3 is for 360 degree rotated image, orientation 6 is for 0 degree rotated
+        // image and orientation 8 is for 180 degree rotated image.
+        try {
+            switch ($exif['Orientation']) {
+                case 3:
+                    $this->rotateImage(180);
+                    break;
+                case 6:
+                    $this->rotateImage(-90);
+                    break;
+                case 8:
+                    $this->rotateImage(90);
+                    break;
+                default :
+                    return;
+            }
+            $this->saveNewImage($filePath, true);
+        } catch (\Exception $ex) {
+            \DBG::msg($ex->getMessage());
+        }
+    }
 }
-
-?>
