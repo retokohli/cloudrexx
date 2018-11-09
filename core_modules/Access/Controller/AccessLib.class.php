a608257f771143f5264c17f07c2001e4e3de2596
Michael Ritter <michael.ritter@cloudrexx.com>
2018-11-09 15:46:52 +0100

    fix(access): re-add missing HTML attribute (CLX-2388)

--- core_modules/Access/Controller/AccessLib.class.php
+++ core_modules/Access/Controller/AccessLib.class.php
@@ -211,7 +211,7 @@ class AccessLib
         $this->arrAttributeTypeTemplates = array(
             'textarea'        => '<textarea name="[NAME]" rows="1" cols="1">[VALUE]</textarea>',
             'text'            => '<input type="text" name="[NAME]" value="[VALUE]" autocomplete="foobar" />',
-            'password'        => '<input type="text" value="" onkeyup="if (this.value == \'\') { this.setAttribute(\'type\', \'text\'); } else { this.setAttribute(\'type\', \'password\'); }" style="text-security: disc; -webkit-text-security: disc;" />',
+            'password'        => '<input type="text" name="[NAME]" value="" onkeyup="if (this.value == \'\') { this.setAttribute(\'type\', \'text\'); } else { this.setAttribute(\'type\', \'password\'); }" style="text-security: disc; -webkit-text-security: disc;" />',
             'checkbox'        => '<input type="hidden" name="[NAME]" /><input type="checkbox" name="[NAME]" value="1" [CHECKED] />',
             'menu'            => '<select name="[NAME]"[STYLE]>[VALUE]</select>',
             'menu_option'     => '<option value="[VALUE]"[SELECTED][STYLE]>[VALUE_TXT]</option>',
