0140a4c28265b298f5002e8c9c10c17e6145614b
Michael Ritter <michael.ritter@cloudrexx.com>
2018-12-07 11:27:16 +0100

    Revert "fix(html): add missing param (CLX-2183)"
    This reverts commit 7b12aa85b966d2c706b597fd2af2700681ec71c7.
    We intentionally parse block __global__ in this case!

--- core/Html/Sigma.class.php
+++ core/Html/Sigma.class.php
@@ -286,7 +286,7 @@ class Sigma extends \HTML_Template_Sigma {
         // This may lead to too much data in $this->_functions but
         // Sigma simply does str_replace() which never matches.
         $func_bkp = $this->_functions;
-        $ret = $this->_buildBlockVariables($block);
+        $ret = $this->_buildBlockVariables();
         $this->_functions = $func_bkp + $this->_functions;
         return $ret;
     }
