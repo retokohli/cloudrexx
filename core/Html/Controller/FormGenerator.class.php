3e3f0c2663abafe487160bbed18e176006605100
Sam Hawkes <sam.hawkes@comvation.com>
2019-01-21 16:19:33 +0100

    feat(html): add callback for attribute value (CLX-2471)

--- core/Html/Controller/FormGenerator.class.php
+++ core/Html/Controller/FormGenerator.class.php
@@ -207,6 +207,47 @@ class FormGenerator {
      */
     public function getDataElement($name, $type, $length, $value, &$options, $entityId) {
         global $_ARRAYLANG, $_CORELANG;
+
+        if (isset($options['valueCallback'])) {
+            $valueCallback = $options['valueCallback'];
+            $vgId = null;
+            if (
+                isset($options['functions']) &&
+                isset($options['functions']['vg_increment_number'])
+            ) {
+                $vgId = $options['functions']['vg_increment_number'];
+            }
+            if (
+                is_array($valueCallback) &&
+                isset($valueCallback['adapter']) &&
+                isset($valueCallback['method'])
+            ) {
+                $json = new \Cx\Core\Json\JsonData();
+                $jsonResult = $json->data(
+                    $valueCallback['adapter'],
+                    $valueCallback['method'],
+                    array(
+                        'data' => $value,
+                        'name' => $name,
+                        'rows' => array(),
+                        'options' => $options,
+                        'vgId' => $this->vgId,
+                    )
+                );
+                if ($jsonResult['status'] == 'success') {
+                    $value = $jsonResult['data'];
+                }
+            } else if (is_callable($valueCallback)) {
+                $value = $valueCallback(
+                    $value,
+                    $name,
+                    array(),
+                    $options,
+                    $this->vgId
+                );
+            }
+        }
+
         if (isset($options['formfield'])) {
             $formFieldGenerator = $options['formfield'];
             $formField = '';
