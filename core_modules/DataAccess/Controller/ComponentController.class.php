e2ce45e12c714388a498a92287e31f0e0a9db232
Sam Hawkes <sam.hawkes@comvation.com>
2019-03-08 14:54:14 +0100

    refactor(dataaccess): add json controller for data access (CLX-2605)

--- core_modules/DataAccess/Controller/ComponentController.class.php
+++ core_modules/DataAccess/Controller/ComponentController.class.php
@@ -54,9 +54,23 @@ class ComponentController extends \Cx\Core\Core\Model\Entity\SystemComponentCont
      */
     public function getControllerClasses()
     {
-        return array('JsonOutput', 'CliOutput', 'Backend');
+        return array('JsonOutput', 'CliOutput', 'Backend', 'JsonDataAccess');
     }
-    
+
+    /**
+     * Returns a list of JsonAdapter class names
+     *
+     * The array values might be a class name without namespace. In that case
+     * the namespace \Cx\{component_type}\{component_name}\Controller is used.
+     * If the array value starts with a backslash, no namespace is added.
+     *
+     * Avoid calculation of anything, just return an array!
+     * @return array List of ComponentController classes
+     */
+    public function getControllersAccessableByJson() {
+        return array('JsonDataAccessController');
+    }
+
     /**
      * Returns a list of command mode commands provided by this component
      * @return array List of command names
---
+++ core_modules/DataAccess/Controller/JsonDataAccessController.class.php
@@ -0,0 +1,88 @@
+<?php
+/**
+ * Cloudrexx
+ *
+ * @link      http://www.cloudrexx.com
+ * @copyright Cloudrexx AG 2007-2019
+ *
+ * According to our dual licensing model, this program can be used either
+ * under the terms of the GNU Affero General Public License, version 3,
+ * or under a proprietary license.
+ *
+ * The texts of the GNU Affero General Public License with an additional
+ * permission and of our proprietary license can be found at and
+ * in the LICENSE file you have received along with this program.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * "Cloudrexx" is a registered trademark of Cloudrexx AG.
+ * The licensing of the program under the AGPLv3 does not imply a
+ * trademark license. Therefore any rights, title and interest in
+ * our trademarks remain entirely with us.
+ */
+
+/**
+ * JsonController for DataAccess
+ *
+ * @copyright   Cloudrexx AG
+ * @author      Sam Hawkes <info@cloudrexx.com>
+ * @package     cloudrexx
+ * @subpackage  core_modules_dataaccess
+ * @version     5.0.0
+ */
+namespace Cx\Core_Modules\DataAccess\Controller;
+
+/**
+ * JsonController for DataAccess
+ *
+ * @copyright   Cloudrexx AG
+ * @author      Sam Hawkes <info@cloudrexx.com>
+ * @package     cloudrexx
+ * @subpackage  core_modules_dataaccess
+ * @version     5.0.0
+ */
+class JsonDataAccessController
+    extends \Cx\Core\Core\Model\Entity\Controller
+    implements \Cx\Core\Json\JsonAdapter
+{
+    /**
+     * List of messages
+     * @var array
+     */
+    protected $messages = array();
+
+    public function getName() {}
+
+    public function getAccessableMethods() {}
+
+    public function getMessagesAsString() {}
+
+    public function getDefaultPermissions() {}
+
+    public function getAccessCondition() {}
+
+    public function getAllowedOutputMethods() {}
+
+    public function getDataAccessPermission() {}
+
+    public function getDataAccessPermissionId() {}
+
+    public function getFieldListSearch($args) {}
+
+    public function getDataAccessSearch($args) {}
+
+    public function getDataAccessReadOnlySearch($args) {}
+
+    protected function getDataAccessValues($apiKeyId) {}
+
+    protected function getSelectedDataAccessValues($apiKeyId) {}
+
+    protected function getAllDataAccessValues() {}
+
+    protected function getSearch($name, $data) {}
+
+    public function storeSelectedDataAccess($args) {}
+}
