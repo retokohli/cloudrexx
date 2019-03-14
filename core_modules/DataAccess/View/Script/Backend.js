e6e0bf42cea9d0f173c5a4f92f135daf5b0f8c41
Sam Hawkes <sam.hawkes@comvation.com>
2019-03-14 11:19:42 +0100

    fix(dataaccess): outsource code to add generate button (CLX-2605)

--- core_modules/DataAccess/View/Script/Backend.js
+++ core_modules/DataAccess/View/Script/Backend.js
@@ -1,16 +1,29 @@
 cx.jQuery(document).ready(function() {
+    addGenerateButton();
+    initializeConditions();
+});
 
+function addGenerateButton() {
     let inputField = cx.jQuery('#form-0-apiKey');
 
-    if (undefined !== inputField.val() && !inputField.val().length) {
-        // Show btn
-        const btnText = cx.variables.get('TXT_CORE_MODULE_DATA_ACCESS_GENERATE_BTN', 'DataAccess');
-        let btn = '<button id="generate-api-key">' + btnText + '</button>';
-        inputField.after(btn);
+    if (undefined === inputField.val() || inputField.val().length) {
+        return;
+    }
 
-        // Generate API-Key on click.
-        cx.jQuery('#generate-api-key').click(function (event) {
-            event.preventDefault();
+    // Show btn
+    const btnText = cx.variables.get('TXT_CORE_MODULE_DATA_ACCESS_GENERATE_BTN', 'DataAccess');
+    let btn = '<button id="generate-api-key">' + btnText + '</button>';
+    inputField.after(btn);
+
+    // Generate API-Key on click.
+    cx.jQuery('#generate-api-key').click(function (event) {
+        event.preventDefault();
+
+        let apiKey = Math.random().toString(36).substring(7);
+        inputField.val(apiKey);
+        cx.jQuery('#generate-api-key').hide();
+    });
+}
 
 function initializeConditions() {
     let conditionWrapper = cx.jQuery('#form-0-accessCondition');
