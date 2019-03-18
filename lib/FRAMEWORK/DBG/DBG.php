621d8b883f07f0e89f06681486356fa96f5783d1
Thomas Däppen <thomas.daeppen@cloudrexx.com>
2019-03-18 11:24:30 +0100

    refactor(dbg): improve readability of log format (CLX-2615)

--- lib/FRAMEWORK/DBG/DBG.php
+++ lib/FRAMEWORK/DBG/DBG.php
@@ -864,14 +864,21 @@ class DBG
                 $outputModuleStr
             ) {
                 $parsingTime = $cx->stopTimer();
-                \DBG::log(
-                    '(Cx: ' . $cx->getId() .
-                    ') Request parsing completed after ' . $parsingTime  .
-                    ' "' . $cachedStr . '" "' . $requestInfo . '" "' .
-                    $requestIp . '" "' . $requestHost . '" "' .
-                    $requestUserAgent . '" "' . memory_get_peak_usage(true) .
-                    '" "' . $userHash . '"' . $outputModuleStr
+                $format = '(Cx: %1$s) Request parsing completed after %2$s "%3$s" "%4$s" "%5$s" "%6$s" "%7$s" "%8$s" "%9$s"%10$s';
+                $log = sprintf(
+                    $format,
+                    $cx->getId(),
+                    $parsingTime,
+                    $cachedStr,
+                    $requestInfo,
+                    $requestIp,
+                    $requestHost,
+                    $requestUserAgent,
+                    memory_get_peak_usage(true),
+                    $userHash,
+                    $outputModuleStr
                 );
+                \DBG::log($log);
             }
         );
     }
