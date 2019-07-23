c11531258fae8b42b1e6428a6b0c713d4412768f
Thomas Däppen <thomas.daeppen@cloudrexx.com>
2019-07-23 16:13:42 +0200

    fix(newsletter): drop orphaned code (CLX-2895)

--- modules/Newsletter/Controller/Newsletter.class.php
+++ modules/Newsletter/Controller/Newsletter.class.php
@@ -1109,7 +1109,7 @@ class Newsletter extends NewsletterLib
      */
     public static function displayInBrowser()
     {
-        global $objDatabase, $_ARRAYLANG, $_CONFIG;
+        global $objDatabase, $_CONFIG;
 
         $id    = !empty($_GET['id'])    ? contrexx_input2raw($_GET['id'])    : '';
         $email = !empty($_GET['email']) ? contrexx_input2raw($_GET['email']) : '';
@@ -1218,10 +1218,6 @@ class Newsletter extends NewsletterLib
             $fax            = contrexx_raw2xhtml($objResult->fields['fax']);
             $website        = contrexx_raw2xhtml($objResult->fields['uri']);
             $birthday       = contrexx_raw2xhtml($objResult->fields['birthday']);
-
-            // unsubscribe and profile links have been removed from browser-view - 12/20/12 TD
-            //$unsubscribe        = '<a href="'.\Cx\Core\Routing\Url::fromModuleAndCmd('Newsletter', 'unsubscribe', '', array('code' => $code, 'mail' => $email)).'">'.$_ARRAYLANG['TXT_UNSUBSCRIBE'].'</a>';
-            //$profile            = '<a href="'.\Cx\Core\Routing\Url::fromModuleAndCmd('Newsletter', 'profile', '', array('code' => $code, 'mail' => $email)).'">'.$_ARRAYLANG['TXT_EDIT_PROFILE'].'</a>';
         } elseif ($objUser = \FWUser::getFWUserObject()->objUser->getUsers(array('email' => contrexx_raw2db($email), 'active' => 1), null, null, null, 1)) {
             $sex            = $objUser->objAttribute->getById($objUser->getProfileAttribute('gender'))->getName();
             $salutation     = contrexx_raw2xhtml($objUser->objAttribute->getById('title_'.$objUser->getProfileAttribute('title'))->getName());
@@ -1239,10 +1235,6 @@ class Newsletter extends NewsletterLib
             $fax            = contrexx_raw2xhtml($objUser->getProfileAttribute('phone_fax'));
             $website        = contrexx_raw2xhtml($objUser->getProfileAttribute('website'));
             $birthday       = date(ASCMS_DATE_FORMAT_DATE, $objUser->getProfileAttribute('birthday'));
-
-            // unsubscribe and profile links have been removed from browser-view - 12/20/12 TD
-            //$unsubscribe = '<a href="'.\Cx\Core\Routing\Url::fromModuleAndCmd('Newsletter', 'unsubscribe', '', array('code' => $code, 'mail' => $email)).'">'.$_ARRAYLANG['TXT_UNSUBSCRIBE'].'</a>';
-            //$profile     = '<a href="'.\Cx\Core\Routing\Url::fromModuleAndCmd('Newsletter', 'profile', '', array('code' => $code, 'mail' => $email)).'">'.$_ARRAYLANG['TXT_EDIT_PROFILE'].'</a>';
         } elseif ($crmUser->load($crmId)) {
 
             $objAttribute = \FWUser::getFWUserObject()->objUser->objAttribute
