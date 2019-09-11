62b317843c64b20d839abc41b7f8bccac6719a83
Thomas Däppen <thomas.daeppen@cloudrexx.com>
2019-09-11 17:04:20 +0200

    fix(calendar): return early (CLX-2990)

--- modules/Calendar/Controller/CalendarLibrary.class.php
+++ modules/Calendar/Controller/CalendarLibrary.class.php
@@ -300,12 +300,12 @@ class CalendarLibrary
                 } else {
                     $eventId = intval($_GET['id']);
                 }                       
-                
                 $objEvent = new \Cx\Modules\Calendar\Controller\CalendarEvent($eventId);
-                
-                if($objEvent->author != $intUserId) {
-                    $strStatus = 'no_access';
+                if ($objEvent->author == $intUserId) {
+                    return;
                 }
+
+                $strStatus = 'no_access';
                 break;
             
             case 'my_events':
