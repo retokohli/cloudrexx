3c353bfe288576e5834f0ce30d7f32bfd3f43c14
Sam Hawkes <sam.hawkes@comvation.com>
2019-10-01 13:23:08 +0200

    feat(lib): implement search map method (SEIL-184)

--- lib/googleServices/googleMap.class.php
+++ lib/googleServices/googleMap.class.php
@@ -46,6 +46,11 @@ class googleMap
 {
     private $apiKey;
 
+    /**
+     * @var bool if the google map js is already loaded
+     */
+    protected static $jsLoaded = false;
+
     private $mapModus = 'overview';
     private $mapDimensions;
     private $mapZoom = 1;
@@ -149,7 +154,7 @@ class googleMap
 
 
 
-    function addMapMarker($id, $lon, $lat, $info, $hideInfo=true, $click=null, $mouseover=null, $mouseout=null, $icon=null)
+    function addMapMarker($id, $lon, $lat, $info = '', $hideInfo=true, $click=null, $mouseover=null, $mouseout=null, $icon=null)
     {
         $this->mapMarkers[$id]['lon'] = $lon;
         $this->mapMarkers[$id]['lat'] = $lat;
@@ -253,10 +258,160 @@ EOF;
         return $layer;
     }
 
-
-
+    /**
+     * Get a script tag to instantiate a Google Map with search functions. To
+     * use the search fields, define a \ContrexxJavascript variable with an array
+     * and the search fields IDs. For example:
+     * array(
+     *     'long' => 'long-search-field-id',
+     *     'lat' => 'lat-search-field-id',
+     *     'zoom' => 'zoom-search-field-id',
+     *     'street' => 'street-search-field-id',
+     *     'zip' => 'zip-search-field-id,
+     *     'city' => 'city-search-field-id
+     * );
+     *
+     * @return string
+     */
     private function getSearchMap()
     {
-        return "not yet implemented";
+        \JS::activate('cx');
+
+        $layer = '<div id="'.$this->mapId.'" '.$this->mapClass.' '.$this->mapDimensions.'></div>';
+
+        $this->loadMapMarkers();
+        $map = 'map_'.$this->mapIndex;
+        $jsScriptTag = $this->getScriptTagToLoadGoogleMaps();
+
+        $layer .= <<<EOF
+$jsScriptTag
+<script>
+//<![CDATA[
+var $map;
+
+cx.ready(function() {
+    const elZoom = document.getElementById(
+        cx.variables.get('map_search_field_ids', 'map_$this->mapIndex').zoom
+    );
+    const elLon = document.getElementById(
+        cx.variables.get('map_search_field_ids', 'map_$this->mapIndex').long
+    );
+    const elLat = document.getElementById(
+        cx.variables.get('map_search_field_ids', 'map_$this->mapIndex').lat
+    );
+
+    var $map = new google.maps.Map(document.getElementById("$this->mapId"));
+    cx.variables.set('map_{$this->mapIndex}', $map, '$this->mapId');
+    $this->mapCenter
+
+    $map.setCenter(center);
+    
+    $map.setZoom($this->mapZoom);
+
+    $map.setMapTypeId(google.maps.MapTypeId.$this->mapType);
+
+    var mapMarkers = cx.variables.get('map_{$this->mapIndex}_markers', '$this->mapId');
+    for (var key in mapMarkers) {
+        if (!mapMarkers.hasOwnProperty(key)) {
+            continue;
+        }
+
+        var mapMarker = mapMarkers[key];
+        mapMarker.marker = new google.maps.Marker({
+            position: new google.maps.LatLng(mapMarker.lat, mapMarker.lon),
+            map: map_$this->mapIndex,
+            draggable:true,
+            animation: google.maps.Animation.DROP
+        });
+        
+        google.maps.event.addListener(mapMarker.marker, 'dragend', function(event) {
+            if (event.latLng.lat()) {
+               elLat.value = event.latLng.lat();
+            }
+            if(event.latLng.lng()){
+               elLon.value = event.latLng.lng();
+            }
+            $map.setCenter(new google.maps.LatLng(event.latLng.lat(), event.latLng.lng()));
+        });
+        
+        // Use the same ID for the search btn as for the marker
+        document.getElementById(key).addEventListener('click', function() {
+            const elStreet = document.getElementById(
+                cx.variables.get('map_search_field_ids', 'map_$this->mapIndex').street
+            );
+            const elZip = document.getElementById(
+                cx.variables.get('map_search_field_ids', 'map_$this->mapIndex').zip
+            );
+            const elCity = document.getElementById(
+                cx.variables.get('map_search_field_ids', 'map_$this->mapIndex').city
+            );
+            var address =  elStreet.value + " " + elZip.value + " " + elCity.value + " CH";
+        
+            var geocoder = new google.maps.Geocoder();
+            
+            if (geocoder) {
+                geocoder.geocode( { 'address': address}, function(results, status) {
+                    if (status == google.maps.GeocoderStatus.OK) {
+                        setPosition(results[0].geometry.location, $map, '$this->mapIndex', mapMarker.marker);
+                        $map.setCenter(results[0].geometry.location);
+                    }
+                });
+            }
+        });
+        
+        google.maps.event.addListener($map, "click", function(event) {
+            setPosition(event.latLng, $map, '$this->mapIndex', mapMarker.marker);
+        });
+
+        // in selection mode, we want only one marker.
+        // therefore, do not process any more markers
+        break;
+    }
+
+    google.maps.event.addListener($map, "idle", function() {
+        elZoom.value = $map.getZoom();
+    });
+    
+});
+
+function setPosition(position, map, mapIndex, marker) {
+    const elZoom = document.getElementById(
+        cx.variables.get('map_search_field_ids', 'map_' + mapIndex).zoom
+    );
+    const elLon = document.getElementById(
+        cx.variables.get('map_search_field_ids', 'map_' + mapIndex).long
+    );
+    const elLat = document.getElementById(
+        cx.variables.get('map_search_field_ids', 'map_' + mapIndex).lat
+    );
+    if (!marker) {
+        marker = new google.maps.Marker({
+            map: map
+        });
+    }
+    marker.set('position', position);
+    elZoom.value = map.getZoom();
+    elLon.value = position.lng();
+    elLat.value = position.lat();
+}
+//]]>
+</script>
+EOF;
+        return $layer;
+    }
+
+    /**
+     * Get the script tag to load the google map js or an empty string. If the
+     * script is already loaded, return an empty string
+     *
+     * @return string script tag or empty string
+     */
+    protected function getScriptTagToLoadGoogleMaps() {
+        if (static::$jsLoaded) {
+            return '<script src="https://maps.googleapis.com/maps/api/js?key='
+                .$this->apiKey.'&sensor=false&v=3"></script>';
+        }
+        static::$jsLoaded = true;
+        return '';
     }
 }
