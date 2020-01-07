688d384278c1786e641cb5095b24b97fe7cc0c98
Mirjam Doyon <mirjam.doyon@comvation.com>
2020-01-07 16:07:09 +0100

    feat(doctrinerepository): add method to get versionnumber (CLX-3103)

--- core/DataSource/Model/Entity/DoctrineRepository.class.php
+++ core/DataSource/Model/Entity/DoctrineRepository.class.php
@@ -531,5 +531,27 @@ class DoctrineRepository extends DataSource
         return $this->getType() == 'doctrineRepository' &&
             is_a($this->getIdentifier(), 'Gedmo\Loggable\Loggable', true);
     }
+
+    /**
+     * @inheritDoc
+     *
+     * If DataSource is Doctrine and versionable, the Versionnumber can never be less than 1.
+     */
+    public function getCurrentVersion(array $elementId) : int {
+
+//        $objectClass = $this->getIdentifier();
+        $em = $this->cx->getDb()->getEntityManager();
+        $repo = $em->getRepository($this->getIdentifier());
+
+//        if (!$repo) {
+//            throw new \Exception('Repository not found!');
+//        }
+
+        $currentVersion = $repo->findOneBy(array('objectId' => $elementId), array('version' => 'desc'));
+
+        return $currentVersion ? $currentVersion->getVersion() : 0;
+
+
+    }
 }
 
