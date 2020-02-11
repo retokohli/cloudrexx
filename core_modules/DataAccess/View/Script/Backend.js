5848e18c0c09d523c659cf372dc24dd5394ac510
Michael Ritter <michael.ritter@cloudrexx.com>
2020-02-11 16:18:30 +0100

    refactor(dataaccess): create longer API keys (CLX-2605)

--- core_modules/DataAccess/View/Script/Backend.js
+++ core_modules/DataAccess/View/Script/Backend.js
@@ -6,6 +6,18 @@ cx.jQuery(document).ready(function() {
     loadJsonMethods();
 });
 
+// This and the next method are copied from https://stackoverflow.com/a/27747377
+function dec2hex (dec) {
+  return ('0' + dec.toString(16)).substr(-2)
+}
+
+// generateId :: Integer -> String
+function generateId (len) {
+  var arr = new Uint8Array((len || 40) / 2)
+  window.crypto.getRandomValues(arr)
+  return Array.from(arr, dec2hex).join('')
+}
+
 function addGenerateButton() {
     let inputField = cx.jQuery('#form-0-apiKey');
 
@@ -22,8 +34,7 @@ function addGenerateButton() {
     cx.jQuery('#generate-api-key').click(function (event) {
         event.preventDefault();
 
-        let apiKey = Math.random().toString(36).substring(7);
-        inputField.val(apiKey);
+        inputField.val(generateId(20));
         cx.jQuery('#generate-api-key').hide();
     });
 }
