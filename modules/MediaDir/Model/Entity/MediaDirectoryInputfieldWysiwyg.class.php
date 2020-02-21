50f6654c336dbbd12d1b0d0c92419d289cd42f02
Thomas Däppen <thomas.daeppen@cloudrexx.com>
2020-02-21 16:12:58 +0100

    fix(mediadir): fix toggling of wysiwyg field type in frontend (CLX-3108)
    This fixes the wrong commit 757daf74d982101db16246430c774aeea98625cc

--- modules/MediaDir/Model/Entity/MediaDirectoryInputfieldWysiwyg.class.php
+++ modules/MediaDir/Model/Entity/MediaDirectoryInputfieldWysiwyg.class.php
@@ -136,20 +136,14 @@ class MediaDirectoryInputfieldWysiwyg extends \Cx\Modules\MediaDir\Controller\Me
 
                     $strInputfield .= '</span>';
                 } else {
-                    $strInputfield = '<span class="editorFix"><span id="'.$this->moduleNameLC.'Inputfield_'.$intId.'_Minimized" style="display: block;" class="'.$this->moduleNameLC.'GroupMultilang">'.new \Cx\Core\Wysiwyg\Wysiwyg($this->moduleNameLC.'Inputfield['.$intId.'][0]', contrexx_raw2xhtml($arrValue[0])).'&nbsp;<a href="javascript:ExpandMinimizeMultiple(\''.$intId.'\', \'ELEMENT\');">'.$_ARRAYLANG['TXT_MEDIADIR_MORE'].'&nbsp;&raquo;</a></span>';
+                    $strInputfield = '<span class="editorFix"><span id="'.$this->moduleNameLC.'Inputfield_'.$intId.'_ELEMENT_Minimized" style="display: block;" class="'.$this->moduleNameLC.'GroupMultilang">'.new \Cx\Core\Wysiwyg\Wysiwyg($this->moduleNameLC.'Inputfield['.$intId.'][0]', contrexx_raw2xhtml($arrValue[0])).'&nbsp;<a href="javascript:ExpandMinimizeMultiple(\''.$intId.'\', \'ELEMENT\');">'.$_ARRAYLANG['TXT_MEDIADIR_MORE'].'&nbsp;&raquo;</a></span>';
 
-                    $strInputfield .= '<span id="'.$this->moduleNameLC.'Inputfield_'.$intId.'_Expanded" style="display: none;" class="'.$this->moduleNameLC.'GroupMultilang">';
+                    $strInputfield .= '<span id="'.$this->moduleNameLC.'Inputfield_'.$intId.'_ELEMENT_Expanded" style="display: none;" class="'.$this->moduleNameLC.'GroupMultilang">';
                     foreach ($this->arrFrontendLanguages as $key => $arrLang) {
                         $intLangId = $arrLang['id'];
-
-                        if (($key+1) == count($this->arrFrontendLanguages)) {
-                            $minimize = "&nbsp;<a href=\"javascript:ExpandMinimizeMultiple('".$intId."', 'ELEMENT');\">&laquo;&nbsp;".$_ARRAYLANG['TXT_MEDIADIR_MINIMIZE']."</a>";
-                        } else {
-                            $minimize = "";
-                        }
-
-                        $strInputfield .=  new \Cx\Core\Wysiwyg\Wysiwyg($this->moduleNameLC.'Inputfield['.$intId.']['.$intLangId.']', contrexx_raw2xhtml($arrValue[$intLangId])).'&nbsp;'.$arrLang['name'].'<a href="javascript:ExpandMinimizeMultiple(\''.$intId.'\', \'ELEMENT\');">&nbsp;'.$minimize.'</a><br />';
+                        $strInputfield .=  new \Cx\Core\Wysiwyg\Wysiwyg($this->moduleNameLC.'Inputfield['.$intId.']['.$intLangId.']', contrexx_raw2xhtml($arrValue[$intLangId])).'&nbsp;'.$arrLang['name'].'<br />';
                     }
+                    $strInputfield .= '<a href="javascript:ExpandMinimizeMultiple(\''.$intId.'\', \'ELEMENT\');">&nbsp;'.$_ARRAYLANG['TXT_MEDIADIR_MINIMIZE'].'</a>';
                     $strInputfield .= '</span></span>';
                 }
                 return $strInputfield;
