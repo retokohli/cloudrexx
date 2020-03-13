65c62b26abb48cb958c3625c308900783a6ce0d0
Michael Ritter <michael.ritter@cloudrexx.com>
2020-03-13 16:36:37 +0100

    refactor(model): simplify password type registering (CLX-3164)

--- core/Model/Db.class.php
+++ core/Model/Db.class.php
@@ -428,7 +428,7 @@ namespace Cx\Core\Model {
 
             //resolve enum, set errors
             $conn = $em->getConnection();
-            foreach (array('enum', 'timestamp') as $type) {
+            foreach (array('enum', 'timestamp', 'password') as $type) {
                 \Doctrine\DBAL\Types\Type::addType(
                     $type,
                     'Cx\Core\Model\Model\Entity\\' . ucfirst($type) . 'Type'
@@ -443,10 +443,6 @@ namespace Cx\Core\Model {
                 'string'
             );
             \Cx\Core\Model\Controller\YamlDriver::registerKnownEnumTypes($conn);
-            \Doctrine\DBAL\Types\Type::addType(
-                'password',
-                'Cx\Core\Model\Model\Entity\PasswordType'
-            );
 
             $this->em = $em;
             return $this->em;
