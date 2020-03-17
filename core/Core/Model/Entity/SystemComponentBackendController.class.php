retokohli/cloudrexx

commit b68cba9bdfdaf0a26197e8702b89fbc71c7be8aa

refactor(core): improve loading of template for overview (CLX-3184)

core/Core/Model/Entity/SystemComponentBackendController.class.php
@@ -105,7 +105,8 @@ class SystemComponentBackendController extends Controller {
             $testFilename = 'Default';
         }
         foreach ($cmd as $index=>$name) {
-            if ($index == 0) {
+            // always load Default.html for overview page
+            if (empty($name)) {
                 continue;
             }
 

commit a4dae3ff1ddb31e45b6903ef4b953d3dba6003b6

fix(core): fix custom template for second level backend (CLX-3252)
This fixes a bug introduced in b68cba9bdfd

core/Core/Model/Entity/SystemComponentBackendController.class.php
@@ -108,7 +108,7 @@ class SystemComponentBackendController extends Controller {
         }
         foreach ($cmd as $index=>$name) {
             // always load Default.html for overview page
-            if (empty($name)) {
+            if (empty($name) || $index == 0) {
                 continue;
             }
 